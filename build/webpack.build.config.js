533bb7b5e4cc316745d47e6ea5c26658719c64e1
John Leider <john.j.leider@gmail.com>
2016-09-11 21:21:10 -0400

    updated build

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   devtool: false,
@@ -27,18 +26,10 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel',
         exclude: /node_modules/
-      },
-      {
-        test: /\.styl$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: "vue-style-loader",
-          loader: ['css-loader', 'stylus-loader']
-        })
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('stylus.css'),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     }),
@@ -50,12 +41,5 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
-  ],
-  vue: {
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 3 versions']
-      })
-    ]
-  }
+  ]
 }
