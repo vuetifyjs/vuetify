fc94cbe884d7120853d86613944f303a73242d81
John Leider <john.j.leider@gmail.com>
2016-09-13 18:39:34 -0400

    cleaned up modal directive

--- src/directives/modal.js
+++ src/directives/modal.js
@@ -1,12 +1,36 @@
-export default {
-  bind (el, binding, vnode) {
-    el.setAttribute('data-modal', binding.arg)
+var defaults = {}
 
-    el.onclick = e => {
-      e.preventDefault()
-      
-      vnode.context.$vuetify.bus.pub(`modal:open:${binding.arg}`)
-    }
+function directive (el, binding, v) {
+  let config = {}
+
+  Object.assign(
+    config,
+    defaults,
+    binding.modifiers,
+    { value: binding.arg },
+    binding.value || {}
+  )
+
+  el.setAttribute('data-modal', config.value)
+
+  el.onclick = e => {
+    e.preventDefault()
+    
+    v.context.$vuetify.bus.pub(`modal:open:${config.value}`)
+  }
+}
+
+export default {
+  bind (el, binding, v) {
+    directive(el, binding, v)
+  },
+
+  updated (el, binding, v) {
+    directive(el, binding, v)
+  },
+
+  componentUpdated (el, binding, v) {
+    directive(el, binding, v)
   },
 
   unbind (el) {
