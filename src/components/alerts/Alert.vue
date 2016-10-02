d0f8c99c35876d098dce7d2f879061f8e0746800
John Leider <john.j.leider@gmail.com>
2016-10-02 19:16:01 -0400

    new alerts component

---
+++ src/components/alerts/Alert.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  div(
+    class="alert"
+    v-bind:class="classes"
+  )
+    slot
+</template>
+
+<script>
+  export default {
+    props: {
+      error: {
+        type: Boolean,
+        default: false
+      },
+
+      info: {
+        type: Boolean,
+        default: false
+      },
+
+      success: {
+        type: Boolean,
+        default: false
+      },
+
+      warning: {
+        type: Boolean,
+        default: false
+      }
+    },
+
+    computed: {
+      classes () {
+        return {
+          'alert--error': this.error,
+          'alert--info': this.info,
+          'alert--success': this.success,
+          'alert--warning': this.warning,
+        }
+      }
+    }
+  }
+</script>
---
+++ src/components/alerts/index.js
@@ -0,0 +1,5 @@
+import Alert from './Alert.vue'
+
+export default {
+  Alert
+}
