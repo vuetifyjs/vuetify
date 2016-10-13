29cf518ae0280b02411563f4fe1e518ba1524fa6
John Leider <john.j.leider@gmail.com>
2016-10-13 18:51:35 -0400

    [build] 0.4.7

---
+++ build/release.sh
@@ -0,0 +1,17 @@
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure (y/n)" -n 1 -r
+echo #
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  git push
+  npm publish
+fi
