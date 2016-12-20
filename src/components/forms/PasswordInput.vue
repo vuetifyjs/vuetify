d1278430c651a6893c6f30c5fdb89900e886e7b0
Vyacheslav Shindin <russian.procoder@gmail.com>
2016-12-20 11:27:43 +0700

    Create form/PasswordInput.vue
    Create form/PasswordInput.vue

---
+++ src/components/forms/PasswordInput.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+  div(
+    class="input-group"
+    v-bind:class="classes"
+  )
+    label(
+      v-bind:for="id"
+      v-html="label"
+    )
+    input(
+      type="password"
+      v-bind:name="name"
+      v-bind:id="id"
+      v-bind:placeholder="placeholder"
+      v-bind:value="value"
+      v-on:blur="focused = false"
+      v-on:input="$emit('input', $event.target.value)"
+      v-on:focus="focused = true"
+      ref="input"
+    )
+</template>
+
+<script>
+  export default {
+    name: 'password-input',
+
+    data () {
+      return {
+        focused: false
+      }
+    },
+
+    computed: {
+      classes () {
+        return {
+          'input-group--focused': this.focused,
+          'input-group--dirty': this.value || this.placeholder || (this.$refs.input && this.$refs.input.value)
+        }
+      }
+    },
+
+    props: {
+      label: String,
+
+      id: String,
+
+      name: String,
+
+      placeholder: String,
+
+      value: {
+        required: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  input
+    &[type=password]
+      border-bottom: 1px solid #ddd
+      height: 3rem
+      width: 100%
+      position: relative
+
+      &:focus
+        outline: none
+</style>
