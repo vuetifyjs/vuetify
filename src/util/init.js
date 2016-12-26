ec3fdf691941beaf7e6e0aa46b7f64924e72d547
John Leider <john.j.leider@gmail.com>
2016-12-26 16:14:42 -0500

    updated conditional

--- src/util/init.js
+++ src/util/init.js
@@ -3,7 +3,7 @@ import Bus from './bus'
 export default function () {
   const click = e => Bus.pub('body:click', e)
 
-  if (window.orientation !== 'undefined') {
+  if (typeof window.orientation !== 'undefined') {
     document.body.addEventListener('touchstart', click, false)
   } else {
     document.body.addEventListener('click', click, false)
