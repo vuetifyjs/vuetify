19e70fadb6cb6cfb9bce7c728e840d55ade7d102
John Leider <john.j.leider@gmail.com>
2017-01-02 12:09:57 -0500

    added new state check for load

--- src/util/load.js
+++ src/util/load.js
@@ -1,7 +1,13 @@
-export default function (cb) {
+function load (cb) {
   if (document.readyState === 'complete') {
     return setTimeout(cb, 0)
   }
 
+  if (document.readyState === 'interactive') {
+    return setTimeout(() => load(cb), 500)
+  }
+
   document.addEventListener('DOMContentLoaded', cb)
 }
+
+export default load
