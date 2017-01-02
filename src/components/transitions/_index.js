c680fdf0f5c1383c3d149365e965acdf0ca4c3e1
John Leider <john.j.leider@gmail.com>
2017-01-02 13:47:57 -0500

    added new transitions for tabs

--- src/components/transitions/_index.js
+++ src/components/transitions/_index.js
@@ -3,9 +3,13 @@ import { createSimpleTransition } from '../../util/helpers'
 const SlideXTransition = createSimpleTransition('slide-x-transition')
 const SlideYTransition = createSimpleTransition('slide-y-transition')
 const ScaleTransition = createSimpleTransition('scale-transition')
+const TabTransition = createSimpleTransition('tab-transition')
+const TabReverseTransition = createSimpleTransition('tab-reverse-transition')
 
 export default {
   SlideXTransition,
   SlideYTransition,
-  ScaleTransition
+  ScaleTransition,
+  TabTransition,
+  TabReverseTransition
 }
