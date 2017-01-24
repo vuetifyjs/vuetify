7fecbdf13327315b00b0ad0dcbf0d729ca2d6010
John Leider <john.j.leider@gmail.com>
2017-01-23 21:18:38 -0500

    added btn-toggle component

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,7 +1,9 @@
 import Btn from './Button.vue'
 import BtnDropdown from './ButtonDropdown.vue'
+import BtnToggle from './ButtonToggle.vue'
 
 export default {
   Btn,
-  BtnDropdown
+  BtnDropdown,
+  BtnToggle
 }
