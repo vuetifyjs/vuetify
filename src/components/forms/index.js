85d0cd6db4440bfb099218148c362864455d8241
David Berube <d@berube.co>
2017-02-06 01:19:17 -0500

    Added textarea component

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,11 +1,13 @@
 import Checkbox from './Checkbox.vue'
 import Radio from './Radio.vue'
 import Select from './Select.vue'
-import TextInput from './TextInput.vue'
+import TextInput from './TextInput.vue',
+import Textarea from './Textarea.vue',
 
 export default {
   Checkbox,
   Radio,
   Select,
-  TextInput
+  TextInput,
+  Textarea
 }
