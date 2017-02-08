b5b0c5faf9d66049f138bdcddd3a6d5f395aaa27
David Graham <prograhammer@gmail.com>
2017-02-08 06:13:34 -0600

    fix transition, scroll, positioning

--- src/components/menus/Menu.vue
+++ src/components/menus/Menu.vue
@@ -36,14 +36,10 @@
     data () {
       return {
         dimensions: {
-          activator: {},
-          content: {},
-          list: {},
-          item: {}
-        },
-        position: {
-          top: 0,
-          left: 0
+          activator: { top: 0, left: 0, bottom: 0, right: 0, height: 0, width: 0, offsetTop: 0 },
+          content: { top: 0, left: 0, bottom: 0, right: 0, height: 0, width: 0, offsetTop: 0 },
+          list: null,
+          selected: null
         },
         minWidth: 'auto'
       }
@@ -74,17 +70,65 @@
     computed: {
       direction () {
         return {
-          'vertical': (this.bottom || this.auto) ? 'bottom' : 'top',
-          'horizontal': (this.right || this.auto) ? 'right' : 'left'
+          'vert': (this.bottom || this.auto) ? 'bottom' : 'top',
+          'horiz': (this.right || this.auto) ? 'right' : 'left'
         }
       },
 
       offset () {
+        const { activator: a, content: c } = this.dimensions
+        const { pageYOffset: pageY, pageXOffset: pageX } = window
+
         return {
-          'top': this.offsetY ? -this.dimensions.activator.height : 0,
-          'left': this.offsetX ? -this.dimensions.activator.width : 0,
-          'bottom': this.offsetY ? this.dimensions.activator.height : 0,
-          'right': this.offsetX ? this.dimensions.activator.width : 0
+          'top': this.offsetY ? -c.height + pageY : a.height - c.height + pageY,
+          'left': this.offsetX ? -c.width + pageX : a.width - c.width + pageX,
+          'bottom': this.offsetY ? a.height + pageY : pageY,
+          'right': this.offsetX ? a.width + pageX : pageX
+        }
+      },
+
+      autoOffset () {
+        if (!(this.auto && this.dimensions.selected)) return 0
+
+        const { activator: a, content: c, selected: s, list } = this.dimensions
+        const offsetBottom = list.height - s.height - s.offsetTop
+        const scrollMiddle = (c.height - s.height) / 2
+
+        let auto = (a.height - c.height) / 2
+        if (s.offsetTop < scrollMiddle) auto += scrollMiddle - s.offsetTop
+        if (offsetBottom < scrollMiddle) auto += offsetBottom - scrollMiddle
+
+        return auto
+      },
+
+      offscreen () {
+        const { activator: a, content: c } = this.dimensions
+        const top = a.top + this.offset[this.direction.vert] + this.autoOffset
+        const left = a.left + this.offset[this.direction.horiz]
+        const { pageYOffset: pageY, pageXOffset: pageX } = window
+        const { innerHeight: innerH, innerWidth: innerW } = window
+
+        return {
+          'vert': top + c.height - pageY > innerH
+            ? innerH - (top + c.height - pageY)
+            : top - pageY < 0
+              ? pageY - top
+              : 0,
+          'horiz': left + c.width - pageX > innerW
+            ? innerW - (left + c.width - pageX)
+            : left - pageX < 0
+              ? pageX - left
+              : 0
+        }
+      },
+
+      position () {
+        const { vert, horiz } = this.direction
+        const a = this.dimensions.activator
+
+        return {
+          top: a.top + this.offset[vert] + this.autoOffset + this.offscreen.vert,
+          left: a.left + this.offset[horiz] + this.offscreen.horiz
         }
       },
 
@@ -99,134 +143,64 @@
     },
 
     mounted () {
-      // Move content to beginning of the document (easier to re-position later).
+      // Move content to beginning of the document (for more functionality).
       document.body.insertBefore(this.$refs.content, document.body.firstChild)
     },
 
     methods: {
       activate () {
-        this.minWidth = this.$el.clientWidth
+        // Get measurements before transitions mess with them.
+        this.updateDimensions()
+
         this.$nextTick(() => {
           this.isActive = true
-          this.updateDimensions()
-          this.updatePosition()
         })
       },
 
       updateDimensions () {
-        this.$refs.content.style.display = 'block'
+        this.sneakPeek()
+
+        this.minWidth = this.$el.clientWidth
         this.dimensions = {
           'activator': this.rect(this.$refs.activator),
           'content': this.rect(this.$refs.content),
           'list': this.rect(this.$refs.content, '.list'),
-          'item': this.rect(this.$refs.content, '.list__item')
-        }
-        this.$refs.content.style.display = 'none'
-      },
-
-      updatePosition () {
-        const { vertical, horizontal } = this.direction
-        this.position.top = this.computePosition(vertical)
-        this.position.left = this.computePosition(horizontal)
-        this.fixOffScreen()
-      },
-
-      computeAuto (dir) {
-        if (!this.isVertical(dir)) return 0
-
-        const { activator, content, list, item } = this.dimensions
-        const selected = this.$refs.content.querySelector('.list__tile--active')
-
-        let offset = this.isReversed(dir)
-          ? 0
-          : (activator.height - content.height) / 2
-
-        if (!selected) return offset
-
-        const offsetTop = selected.parentElement.offsetTop
-        const offsetBottom = list.height - item.height - offsetTop
-        const scrollHeight = content.height - item.height
-        const scrollToMiddle = scrollHeight / 2
-
-        if (offsetTop < scrollToMiddle) offset += scrollToMiddle - offsetTop
-        if (offsetBottom < scrollToMiddle) offset += offsetBottom - scrollToMiddle
-
-        this.scrollToItem(dir, offsetTop, scrollHeight)
-
-        // Todo: Temporary out-of-bound fix. Need to discuss.
-        return this.isReversed(dir) ? 0 : offset
-      },
-
-      scrollToItem (dir, offsetTop, scrollHeight) {
-        let scroll = offsetTop - (scrollHeight / 2)
-
-        // Todo: Temporary out-of-bound fix. Need to discuss.
-        if (this.isReversed(dir)) {
-          scroll = dir === 'top' ? offsetTop - scrollHeight : offsetTop
+          'selected': this.rect(this.$refs.content, '.list__tile--active', 'parent')
         }
 
-        this.$refs.content.scrollTop = scroll
+        this.updateScroll()
+        this.sneakPeekOff()
       },
 
-      computePosition (dir) {
-        const { activator, content } = this.dimensions
-        const isVertical = this.isVertical(dir)
-        const coord = isVertical ? 'top' : 'left'
-        const dimen = isVertical ? 'height' : 'width'
-        const winScroll = isVertical ? window['pageYOffset'] : window['pageXOffset']
-        const offset = this.auto ? this.computeAuto(dir) : this.offset[dir]
+      updateScroll () {
+        if (!(this.auto && this.dimensions.selected)) return
 
-        // Adjust for 'top' or 'left' direction (inverse direction)
-        const dirAdjust = this.isTopOrLeft(dir)
-          ? activator[dimen] - content[dimen]
-          : 0
+        const { content: c, selected: s } = this.dimensions
+        const scrollMiddle = (c.height - s.height) / 2
 
-        return activator[coord] + offset + dirAdjust + winScroll
-      },
-
-      fixOffScreen () {
-        const content = this.dimensions.content
-        const { vertical, horizontal } = this.direction
-        const top = this.position.top - window['pageYOffset']
-        const left = this.position.left - window['pageXOffset']
-
-        if (top < 0 || top + content['height'] > window['innerHeight']) {
-          this.position.top = this.computePosition(this.reverse(vertical))
-        }
-
-        if (left < 0 || left + content['width'] > window['innerWidth']) {
-          this.position.left = this.computePosition(this.reverse(horizontal))
-        }
-      },
-
-      isReversed (dir) {
-        return this.isVertical(dir)
-          ? this.direction['vertical'] !== dir
-          : this.direction['horizontal'] !== dir
+        this.$refs.content.scrollTop = s.offsetTop - scrollMiddle + this.offscreen.vert
       },
 
       // Utils
       // ====================
 
-      rect (el, classSelector = '') {
-        if (!classSelector) return el.getBoundingClientRect()
-        const descendant = el.querySelector(classSelector)
-        return descendant ? descendant.getBoundingClientRect() : null
+      rect (el, selector, getParent = false) {
+        el = selector ? el.querySelector(selector) : el
+        el = el && getParent ? el.parentElement : el
+
+        return el
+          ? Object.assign(el.getBoundingClientRect(), { 'offsetTop': el.offsetTop })
+          : null
       },
 
-      isVertical (dir) {
-        return ['top', 'bottom'].includes(dir)
+      sneakPeek () {
+        this.$refs.content.style.opacity = 0
+        this.$refs.content.style.display = 'inline-block'
       },
 
-      isTopOrLeft (dir) {
-        return ['top', 'left'].includes(dir)
-      },
-
-      reverse (dir) {
-        if (dir === 'top') return 'bottom'
-        if (dir === 'bottom') return 'top'
-        if (dir === 'left') return 'right'
-        if (dir === 'right') return 'left'
+      sneakPeekOff () {
+        this.$refs.content.style.display = 'none'
+        this.$refs.content.style.opacity = null
       }
     }
   }
