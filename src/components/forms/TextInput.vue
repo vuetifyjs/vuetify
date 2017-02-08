01fa41564d823af31f2e2b6ae816c79d045619b5
John Leider <john.j.leider@gmail.com>
2017-02-08 18:29:07 -0500

    added auto conversion of type

--- src/components/forms/TextInput.vue
+++ src/components/forms/TextInput.vue
@@ -29,7 +29,7 @@
     data () {
       return {
         focused: false,
-        inputValue: this.value
+        inputValue: this.value.toString()
       }
     },
 
@@ -60,7 +60,6 @@
       },
 
       value: {
-        type: String,
         required: false
       }
     },
