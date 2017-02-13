66917bf8050613d458e8e4a9788b703afc70c02d
John Leider <john.j.leider@gmail.com>
2017-02-13 08:42:37 -0500

    Update index.js

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,8 +1,8 @@
 import Checkbox from './Checkbox.vue'
 import Radio from './Radio.vue'
 import Select from './Select.vue'
-import TextInput from './TextInput.vue',
-import Textarea from './Textarea.vue',
+import TextInput from './TextInput.vue'
+import Textarea from './Textarea.vue'
 
 export default {
   Checkbox,
