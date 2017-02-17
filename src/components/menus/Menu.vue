ef4c5f6edb06bed934fd8fd58becf95860317291
David Graham <prograhammer@gmail.com>
2017-02-16 20:13:37 -0600

    menu cleanup

--- src/components/menus/Menu.vue
+++ src/components/menus/Menu.vue
@@ -18,7 +18,6 @@
       div(
         ref="content"
         class="menu__content"
-        v-on:click="isActive = false"
         v-show="isActive"
         v-bind:style="styles"
       )
@@ -39,37 +38,37 @@
         dimensions: {
           activator: {
             cssTop: 0, cssLeft: 0,
-            top: 0, left: 0, bottom: 0, right: 0, height: 0, width: 0, offsetTop: 0
+            top: 0, left: 0, bottom: 0, right: 0, width: 0, height: 0, offsetTop: 0
           },
           content: {
             cssTop: 0, cssLeft: 0,
-            top: 0, left: 0, bottom: 0, right: 0, height: 0, width: 0, offsetTop: 0
+            top: 0, left: 0, bottom: 0, right: 0, width: 0, height: 0, offsetTop: 0
           },
           list: null,
           selected: null
         },
         minWidth: 'auto',
-        autoNudgeX: -16,
-        autoNudgeY: -8
+        nudgeX: -16,
+        nudgeY: -16
       }
     },
 
     props: {
-      auto: Boolean,
+      top: Boolean,
       left: Boolean,
       bottom: Boolean,
       right: Boolean,
+      auto: Boolean,
+      offsetX: Boolean,
+      offsetY: Boolean,
       maxHeight: {
         type: [String, Number],
         default: 'auto'
       },
-      offsetX: Boolean,
-      offsetY: Boolean,
       origin: {
         type: String,
         default: 'top left'
       },
-      top: Boolean,
       transition: {
         type: String,
         default: 'v-menu-transition'
@@ -78,9 +77,7 @@
 
     watch: {
       isActive () {
-        if (this.isActive) {
-          this.activate()
-        }
+        if (this.isActive) this.updateDimensions()
 
         this.$emit('input', this.isActive)
       }
@@ -88,110 +85,103 @@
 
     computed: {
       direction () {
-        const { edgeDistance: edge } = this
+        const { left, top, auto, screenDistance } = this
         const { content: c } = this.dimensions
-        let vert = this.top && !this.auto ? 'top' : 'bottom'
-        let horiz = this.left && !this.auto ? 'left' : 'right'
+        let horiz = left && !auto ? 'left' : 'right'
+        let vert = top && !auto ? 'top' : 'bottom'
 
-        // Flip direction, if needed, to where there's more room from the screen edge.
-        vert = !this.auto && c.height > edge[vert] ? edge.maxVertDir : vert
-        horiz = !this.auto && c.width > edge[horiz] ? edge.maxHorizDir : horiz
+        // Flip direction, if needed, to where there's more room from the window edge.
+        horiz = !auto && c.width > screenDistance[horiz] ? screenDistance.horizMaxDir : horiz
+        vert = !auto && c.height > screenDistance[vert] ? screenDistance.vertMaxDir : vert
 
-        return { vert, horiz }
+        return { horiz, vert }
       },
 
       offset () {
         const { activator: a, content: c } = this.dimensions
-        const y = a.top - c.top    // <-- Start with the diff between content and activator to
-        const x = a.left - c.left  // <-- shift content to activator's position.
+        const { direction, offsetX, offsetY, offsetAuto } = this
+        const x = a.left - c.left  // <-- Start with the diff between content and activator
+        const y = a.top - c.top    // <-- to shift content to activator's position.
 
         return {
-          'top': this.offsetY ? y - c.height : y + a.height - c.height,
-          'left': this.offsetX ? x - c.width : x + a.width - c.width,
-          'bottom': this.offsetY ? y + a.height : y,
-          'right': this.offsetX ? x + a.width : x
+          horiz: direction.horiz === 'left'
+            ? offsetX ? x - c.width : x + a.width - c.width + offsetAuto.horiz   // left
+            : offsetX ? x + a.width : x + offsetAuto.horiz,                      // right
+          vert: direction.vert === 'top'
+            ? offsetY ? y - c.height : y + a.height - c.height + offsetAuto.vert // top
+            : offsetY ? y + a.height : y + offsetAuto.vert                       // bottom
         }
       },
 
-      autoOffsetY () {
-        if (!this.auto || !this.dimensions.selected) return this.auto ? this.autoNudgeY - 8 : 0
+      offsetAuto () {
+        if (!this.auto) return { horiz: 0, vert: 0 }
+        if (!this.dimensions.selected) return { horiz: this.nudgeX, vert: this.nudgeY }
 
         const { activator: a, content: c, selected: s, list } = this.dimensions
         const offsetBottom = list.height - s.height - s.offsetTop
         const scrollMiddle = (c.height - s.height) / 2
-        let auto = (a.height - c.height) / 2 + this.autoNudgeY
+        const horiz = this.nudgeX
+        let vert = (a.height - c.height + this.nudgeY) / 2
 
-        if (s.offsetTop < scrollMiddle) auto += scrollMiddle - s.offsetTop
-        if (offsetBottom < scrollMiddle) auto += offsetBottom - scrollMiddle
+        vert += s.offsetTop < scrollMiddle ? scrollMiddle - s.offsetTop : 0
+        vert += offsetBottom < scrollMiddle ? offsetBottom - scrollMiddle : 0
 
-        return auto
+        return { horiz, vert }
       },
 
-      autoOffsetX () {
-        return this.auto ? this.autoNudgeX : 0
-      },
-
-      edgeDistance () {
+      screenDistance () {
         const { activator: a } = this.dimensions
         const { innerHeight: innerH, innerWidth: innerW } = this.window
-        let edge = {}
+        const x = this.offsetX ? a.right : a.left  // <-- Determine which edge of the
+        const y = this.offsetY ? a.bottom : a.top  // <-- activator to measure distance from.
+        let distance = {}
 
-        edge = { top: a.top, left: a.left, bottom: innerH - a.bottom, right: innerW - a.right }
-        edge.maxVert = edge.top > edge.bottom ? edge.top : edge.bottom
-        edge.maxHoriz = edge.left > edge.right ? edge.left : edge.right
-        edge.maxVertDir = edge.top > edge.bottom ? 'top' : 'bottom'
-        edge.maxHorizDir = edge.left > edge.right ? 'left' : 'right'
+        distance = { top: y, left: x, bottom: innerH - y, right: innerW - x }
+        distance.horizMax = distance.left > distance.right ? distance.left : distance.right
+        distance.horizMaxDir = distance.left > distance.right ? 'left' : 'right'
+        distance.vertMax = distance.top > distance.bottom ? distance.top : distance.bottom
+        distance.vertMaxDir = distance.top > distance.bottom ? 'top' : 'bottom'
 
-        return edge
+        return distance
       },
 
-      offscreen () {
+      screenOverflow () {
         const { content: c } = this.dimensions
-        const top = c.top + this.offset[this.direction.vert] + this.autoOffsetY
-        const left = c.left + this.offset[this.direction.horiz] + this.autoOffsetX
+        const left = c.left + this.offset.horiz
+        const top = c.top + this.offset.vert
 
         return {
-          'vert': this.auto && top + c.height > this.window.innerHeight
-            ? this.window.innerHeight - (top + c.height)
-            : this.auto && top < 0
-              ? -top
-              : 0,
           'horiz': this.auto && left + c.width > this.window.innerWidth
-            ? this.window.innerWidth - (left + c.width)
+            ? (left + c.width) - this.window.innerWidth
             : this.auto && left < 0
-              ? -left
+              ? left
+              : 0,
+          'vert': this.auto && top + c.height > this.window.innerHeight
+            ? (top + c.height) - this.window.innerHeight
+            : this.auto && top < 0
+              ? top
               : 0
         }
       },
 
       position () {
-        const { vert, horiz } = this.direction
         const { content: c } = this.dimensions
 
         return {
-          top: c.cssTop + this.offset[vert] + this.autoOffsetY + this.offscreen.vert,
-          left: c.cssLeft + this.offset[horiz] + this.autoOffsetX + this.offscreen.horiz
+          left: c.cssLeft + this.offset.horiz - this.screenOverflow.horiz,
+          top: c.cssTop + this.offset.vert - this.screenOverflow.vert
         }
       },
 
       styles () {
         return {
-          top: `${this.position.top}px`,
-          left: `${this.position.left}px`
+          left: `${this.position.left}px`,
+          top: `${this.position.top}px`
         }
       }
     },
 
     methods: {
-      activate () {
-        // Get measurements before transitions mess with them.
-        this.updateDimensions()
-
-        this.$nextTick(() => {
-          this.isActive = true
-        })
-      },
-
       updateDimensions () {
         this.sneakPeek()
         this.updateMaxMin()
@@ -213,36 +203,37 @@
       },
 
       updateMaxMin () {
-        const { $el, $refs, maxHeight, auto, autoNudgeX } = this
+        const { $el, $refs, maxHeight, auto, nudgeX } = this
 
-        $refs.content.style.minWidth = `${$el.clientWidth + Math.abs(auto ? autoNudgeX : 0)}px`
+        $refs.content.style.minWidth = `${$el.clientWidth + Math.abs(auto ? nudgeX : 0)}px`
         $refs.content.style.maxHeight = null  // <-- TODO: This is a temporary fix.
         $refs.content.style.maxHeight = isNaN(maxHeight) ? maxHeight : `${maxHeight}px`
       },
 
       fixOffscreen () {
-        const { $refs, edgeDistance: edge } = this
+        const { $refs, screenDistance } = this
         const { vert } = this.direction
 
-        // If not auto, reduce height to the max vertical distance to a screen edge.
-        if (!this.auto && this.dimensions.content.height > edge[vert]) {
-          $refs.content.style.maxHeight = `${edge.maxVert}px`
+        // If not auto, reduce height to the max vertical distance to a window edge.
+        if (!this.auto && this.dimensions.content.height > screenDistance[vert]) {
+          $refs.content.style.maxHeight = `${screenDistance.vertMax}px`
           this.dimensions.content.height = $refs.content.getBoundingClientRect().height
         }
       },
 
       updateScroll () {
-        if (!(this.auto && this.dimensions.selected)) return
+        if (!this.auto || !this.dimensions.selected) return
 
         const { content: c, selected: s, list: l } = this.dimensions
         const scrollMiddle = (c.height - s.height) / 2
         const scrollMax = l.height - c.height
         let offsetTop = s.offsetTop - scrollMiddle
 
-        if (this.offscreen.vert && offsetTop > scrollMax) offsetTop = scrollMax
-        if (this.offscreen.vert && offsetTop < 0) offsetTop = 0
+        offsetTop = this.screenOverflow.vert && offsetTop > scrollMax ? scrollMax : offsetTop
+        offsetTop = this.screenOverflow.vert && offsetTop < 0 ? 0 : offsetTop
+        offsetTop -= this.screenOverflow.vert
 
-        this.$refs.content.scrollTop = offsetTop + this.offscreen.vert
+        this.$refs.content.scrollTop = offsetTop
       },
 
       // Utils
@@ -254,12 +245,12 @@
 
         if (!el) return null
 
-        const { top, bottom, right, left, width, height } = el.getBoundingClientRect()
-        const cssTop = parseInt(el.style.top) || 0
+        const { top, left, bottom, right, width, height } = el.getBoundingClientRect()
         const cssLeft = parseInt(el.style.left) || 0
+        const cssTop = parseInt(el.style.top) || 0
         const offsetTop = el.offsetTop
 
-        return { cssTop, cssLeft, top, bottom, right, left, width, height, offsetTop }
+        return { cssTop, cssLeft, top, left, bottom, right, width, height, offsetTop }
       },
 
       sneakPeek (on = true) {
