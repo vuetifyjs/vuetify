6e7e2a613969a3f7ca94154f4aa57bc7f52dcd78
John Leider <john.j.leider@gmail.com>
2017-02-25 11:45:18 -0500

    slider updates

--- src/components/sliders/Slider.js
+++ src/components/sliders/Slider.js
@@ -1,213 +1,173 @@
-<template lang="pug">
-  div(
-    v-bind:class="classes"
-    role="slider"
-    v-on:mouseup="sliderMove"
-    v-click-outside
-  )
-    v-icon(
-      v-if="prependIcon" 
-      class="input-group__prepend-icon"
-    ) {{ prependIcon }}
-    label(
-      v-if="label" 
-      v-html="label"
-    )
-    div(class="slider")
-      div(
-        class="slider__track__container"
-        ref="track"
-      )
-        div(
-          class="slider__track"
-          v-bind:style="trackStyles"
-        )
-        div(
-          class="slider__track-fill"
-          v-bind:style="trackFillStyles"
-        )
-      div(
-        class="slider__ticks-container"
-        v-bind:style="tickContainerStyles"
-        v-if="tickInterval"
-      )
-        div(
-          class="slider__ticks"
-          v-bind:style="tickStyles"
-        )
-      div(
-        v-bind:class="thumbContainerClasses"
-        v-on:touchstart.stop="onMouseDown"
-        v-on:mousedown.stop="onMouseDown"
-        v-bind:style="thumbStyles"
-        ref="thumb"
-      )
-        div(class="slider__thumb")
-        v-scale-transition(
-          origin="bottom center"
-          v-if="thumbLabel"
-        )
-          div(
-            class="slider__thumb--label__container"
-            v-if="isActive"
-          )
-            div(class="slider__thumb--label")
-              span {{ inputValue }}
-</template>
+import Input from '../../mixins/input'
 
-<script>
-  import { addOnceEventListener } from '../../util/helpers'
+export default {
+  name: 'slider',
 
-  export default {
-    name: 'slider',
+  mixins: [Input],
 
-    data () {
-      return {
-        isActive: false,
-        inputWidth: 0
-      }
+  data () {
+    return {
+      isActive: false,
+      inputWidth: 0
+    }
+  },
+
+  props: {
+    inverted: Boolean,
+    min: {
+      type: [Number, String],
+      default: 0
     },
-
-    props: {
-      dark: Boolean,
-      disabled: Boolean,
-      label: String,
-      light: Boolean,
-      inverted: Boolean,
-      min: {
-        type: [Number, String],
-        default: 0
-      },
-      max: {
-        type: [Number, String],
-        default: 100
-      },
-      prependIcon: String,
-      stepSize: {
-        type: [Number, String],
-        default: 1
-      },
-      tickInterval: [Number, String],
-      thumbLabel: Boolean,
-      value: [Number, String],
-      vertical: Boolean
+    max: {
+      type: [Number, String],
+      default: 100
     },
-
-    watch: {
-      value () {
-        this.inputValue = this.value
-      }
+    stepSize: {
+      type: [Number, String],
+      default: 1
     },
+    tickInterval: [Number, String],
+    thumbLabel: Boolean,
+    value: [Number, String],
+    vertical: Boolean
+  },
 
-    computed: {
-      classes () {
-        return {
-          'input-group input-group--slider': true,
-          'input-group--active': this.isActive,
-          'input-group--dark': this.dark,
-          'input-group--dirty': this.inputValue > this.min,
-          'input-group--disabled': this.disabled,
-          'input-group--light': this.light,
-          'input-group--prepend-icon': this.prependIcon,
-          'input-group--ticks': this.tickInterval
-        }
-      },
-      inputValue: {
-        get () {
-          return this.value
-        },
-        set (val) {
-          // Do not re-calc width if not needed, causes jump
-          if (val !== Math.round(this.inputWidth)) {
-            this.inputWidth = (100 * (val / this.max))
-          }
-
-          let value = Math.round(val)
-
-          value = value < this.min ? 0 : value > this.max ? this.max : value
-
-          this.$emit('input', value)
-        }
-      },
-      thumbContainerClasses () {
-        return {
-          'slider__thumb-container': true,
-          'slider__thumb-container--label': this.thumbLabel
-        }
-      },
-      thumbStyles () {
-        return {
-          left: `${this.inputWidth}%`
-        }
-      },
-      tickContainerStyles () {
-        return {
-          transform: `translate3d(-${this.tickInterval / 2 * this.stepSize}%, -50%, 0)`
-        }
-      },
-      tickStyles () {
-        return {
-          backgroundSize: `${this.tickInterval * this.stepSize}% 2px`,
-          transform: `translate3d(${this.tickInterval / 2 * this.stepSize}%, 0, 0)`
-        }
-      },
-      trackStyles () {
-        const scaleX = this.calculateScale(1 - (this.inputWidth / 100))
-        const translateX = this.inputWidth < 1 && !this.thumbLabel ? `${8}px` : 0
-        return {
-          transform: `scaleX(${scaleX}) translateX(${translateX})`
-        }
-      },
-      trackFillStyles () {
-        const scaleX = this.calculateScale(this.inputWidth / 100)
-        const translateX = this.inputWidth > 99 && !this.thumbLabel ? `${-8}px` : 0
-        return {
-          transform: `scaleX(${scaleX}) translateX(${translateX})`
-        }
-      }
-    },
-
-    mounted () {
+  watch: {
+    value () {
       this.inputValue = this.value
+    }
+  },
+
+  computed: {
+    classes () {
+      return {
+        'input-group input-group--slider': true,
+        'input-group--active': this.isActive,
+        'input-group--dirty': this.inputValue > this.min,
+        'input-group--disabled': this.disabled,
+        'input-group--ticks': this.tickInterval
+      }
     },
-
-    methods: {
-      calculateScale (scale) {
-        if (scale < .02 && !this.thumbLabel) {
-          return 0
+    inputValue: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        // Do not re-calc width if not needed, causes jump
+        if (val !== Math.round(this.inputWidth)) {
+          this.inputWidth = (100 * (val / this.max))
         }
 
-        return this.disabled ? scale - .015 : scale
-      },
-      onMouseDown (e) {
-        this.isActive = true
-        document.addEventListener('touchmove', this.onMouseMove, false)
-        document.addEventListener('pointermove', this.onMouseMove, false)
-        document.addEventListener('mouseup', this.onMouseUp, false)
-      },
-      onMouseUp (e) {
-        this.isActive = false
-        document.removeEventListener('pointermove', this.onMouseMove, false)
-        document.removeEventListener('touchmove', this.onMouseMove, false)
-      },
-      onMouseMove (e) {
-        let { left: offsetLeft, width: trackWidth } = this.$refs.track.getBoundingClientRect()
-        let { width: thumbWidth } = this.$refs.thumb.getBoundingClientRect()
-        const clientX = 'touches' in e ? e.touches[0].clientX : e.clientX
-        let left = (
-          ((clientX - offsetLeft - thumbWidth)
-          / trackWidth) * 100
-        )
+        let value = Math.round(val)
 
-        left = left < 0 ? 0 : left > 100 ? 100 : left
+        value = value < this.min ? 0 : value > this.max ? this.max : value
 
-        this.inputValue = this.max * (left / 100)
-      },
-      sliderMove (e) {
-        if (!this.isActive) {
-          this.onMouseMove(e)
-        }
+        this.$emit('input', value)
+      }
+    },
+    thumbContainerClasses () {
+      return {
+        'slider__thumb-container': true,
+        'slider__thumb-container--label': this.thumbLabel
+      }
+    },
+    thumbStyles () {
+      return {
+        left: `${this.inputWidth}%`
+      }
+    },
+    tickContainerStyles () {
+      return {
+        transform: `translate3d(-${this.tickInterval / 2 * this.stepSize}%, -50%, 0)`
+      }
+    },
+    tickStyles () {
+      return {
+        backgroundSize: `${this.tickInterval * this.stepSize}% 2px`,
+        transform: `translate3d(${this.tickInterval / 2 * this.stepSize}%, 0, 0)`
+      }
+    },
+    trackStyles () {
+      const scaleX = this.calculateScale(1 - (this.inputWidth / 100))
+      const translateX = this.inputWidth < 1 && !this.thumbLabel ? `${8}px` : 0
+      return {
+        transform: `scaleX(${scaleX}) translateX(${translateX})`
+      }
+    },
+    trackFillStyles () {
+      const scaleX = this.calculateScale(this.inputWidth / 100)
+      const translateX = this.inputWidth > 99 && !this.thumbLabel ? `${-8}px` : 0
+      return {
+        transform: `scaleX(${scaleX}) translateX(${translateX})`
       }
     }
+  },
+
+  mounted () {
+    this.inputValue = this.value
+  },
+
+  methods: {
+    calculateScale (scale) {
+      if (scale < 0.02 && !this.thumbLabel) {
+        return 0
+      }
+
+      return this.disabled ? scale - 0.015 : scale
+    },
+    onMouseDown (e) {
+      this.isActive = true
+      document.addEventListener('touchmove', this.onMouseMove, false)
+      document.addEventListener('pointermove', this.onMouseMove, false)
+      document.addEventListener('mouseup', this.onMouseUp, false)
+    },
+    onMouseUp (e) {
+      this.isActive = false
+      document.removeEventListener('pointermove', this.onMouseMove, false)
+      document.removeEventListener('touchmove', this.onMouseMove, false)
+    },
+    onMouseMove (e) {
+      const { left: offsetLeft, width: trackWidth } = this.$refs.track.getBoundingClientRect()
+      const { width: thumbWidth } = this.$refs.thumb.getBoundingClientRect()
+      const clientX = 'touches' in e ? e.touches[0].clientX : e.clientX
+      let left = (
+        ((clientX - offsetLeft - thumbWidth) / trackWidth) * 100
+      )
+
+      left = left < 0 ? 0 : left > 100 ? 100 : left
+
+      this.inputValue = this.max * (left / 100)
+    },
+    sliderMove (e) {
+      if (!this.isActive) {
+        this.onMouseMove(e)
+      }
+    }
+  },
+
+  render (h) {
+    const slider = h('div', { 'class': 'slider' })
+    // const sliderTrackContainer = h('div', {'class': 'slider__track__container', ref:"track"})
+    // const sliderTrack = h('div', {style: this.trackStyles})
+    // const sliderTrackFill = h('div', {style: this.trackFillStyles})
+    // const ticksContainer = h('div', {'class': 'slider__ticks-container', style: this.tickContainerStyles}) // v-if
+    // const sliderTicks = h('div', {'class': 'slider__ticks', style: this.tickStyles})
+    // const thumbContainer = h('div', {
+    //   'class': this.thumbContainerClasses,
+    //   style: this.thumbStyles,
+    //   on: {
+    //     touchstart: this.onMouseDown,
+    //     mousedown: this.onMouseDown
+    //   },
+    //   ref: 'thumb'
+    // })
+    // const thumbContainer = h('div', {
+    //   'class': 'slider__thumb'
+    // })
+    // // v-scale transition if thumbLabel origin bottom center
+    // const thumbLabelContainer = h('div', {'class': 'slider__thumb--label__container'}) // v-if isActive
+    // const thumbLabel = h('div', {'class': 'slider__thumb--label'}) // span with {{ inputValue }}
+
+    return this.genInputGroup(h, [slider])
   }
-</script>
+}
