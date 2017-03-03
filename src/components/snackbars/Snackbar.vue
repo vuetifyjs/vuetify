20f6d7ee62279abdb62d7d78a734531838a596be
Marco Ferreira <marco@altitude.co>
2017-03-03 19:31:02 -0300

    Add 'timeout' option and resizing hack

--- src/components/snackbars/Snackbar.vue
+++ src/components/snackbars/Snackbar.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
 
-div(v-bind:class='[{snack: true}, snackClasses]')
+div(v-bind:class='[{"snack": true}, snackClasses]' ref="snack")
     v-slide-y-transition
-        div( class='snack--body' v-show="isActive" )
-            div( class='snack--content' )
+        div( v-bind:class='[{"snack--body": true}, bodyClasses]' v-show="isActive" )
+            div( class='snack--content' id='snack--content')
                 slot
 </template>
 
 <script>
 import Toggleable from '../../mixins/toggleable'
+import { debounce } from '../../util/helpers'
 
 export default {
     name: 'snackbar',
@@ -20,10 +21,23 @@ export default {
         right: Boolean,
         top: Boolean,
         bottom: Boolean,
-        timeout: Number
+        timeout: {
+            type: Number,
+            default: 6000
+        }
+    },
+
+    data () {
+        return {
+            textHeight: null,
+            setup: false
+        }
     },
 
     computed: {
+        multipleLines () {
+			return this.textHeight > 26
+		},
 		snackClasses () {
             // default to bottom
             let bottom = !this.left && !this.right && !this.top ? true : this.bottom
@@ -34,21 +48,67 @@ export default {
 				'snack--position-top': this.top,
 				'snack--position-bottom': bottom
 			}
-		}
+		},
+        bodyClasses () {
+            return {
+                'snack--body-line': !this.multipleLines,
+				'snack--body-lines': this.multipleLines
+            }
+        }
 	},
 
     watch: {
         isActive () {
             if (this.isActive)
-                setTimeout(() => this.isActive = false, this.timeout || 6000)
+                setTimeout(() => this.isActive = false, this.timeout)
         }
     },
 
 	methods: {
-		activate () {
+        activate () {
 			this.isActive = true
-		}
-	}
+		},
+        deactivate () {
+			this.isActive = false
+		},
+        setContentHeight () {
+            if (this.textHeight !== document.getElementById('snack--content').clientHeight) {
+                this.textHeight = document.getElementById('snack--content').clientHeight
+                console.log('textHeight changed to', this.textHeight)
+            }
+        },
+        resize () {
+            if (this.isActive) {
+                this.setContentHeight()
+            }
+            else {
+                this.getContentHeightHack()
+            }
+        },
+        getContentHeightHack () {
+            this.$refs.snack.style.opacity = 0
+    		this.activate()
+    		this.setContentHeight()
+    		setTimeout(() => {
+    			this.deactivate()
+    		}, 300)
+    		setTimeout(() => {
+    			this.$refs.snack.style.opacity = 1
+    		}, 600)
+        }
+	},
+
+    updated () {
+        if (!this.setup || this.textHeight === 0) {
+            this.resize()
+            this.setup = true
+        }
+	},
+
+    mounted () {
+        this.getContentHeightHack()
+        window.addEventListener('resize', debounce(this.resize, 200), false)
+    }
 
 }
 
