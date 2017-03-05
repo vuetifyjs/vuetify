9b3aad1dc8bf4e07b2f50b16452e86a4d5d7005b
John Leider <john.j.leider@gmail.com>
2017-03-04 21:52:08 -0500

    updated snackbars

---
+++ src/components/snackbars/Snackbar.js
@@ -0,0 +1,57 @@
+import Toggleable from '../../mixins/toggleable'
+
+export default {
+  name: 'snackbar',
+
+  mixins: [Toggleable],
+
+  props: {
+    bottom: Boolean,
+    left: Boolean,
+    multiLine: Boolean,
+    right: Boolean,
+    top: Boolean,
+    timeout: {
+      type: Number,
+      default: 6000
+    }
+  },
+
+  computed: {
+    classes () {
+      return {
+        'snack': true,
+        'snack--bottom': this.bottom || !this.top,
+        'snack--left': this.left,
+        'snack--right': this.right,
+        'snack--top': this.top,
+        'snack--multi-line': this.multiLine
+      }
+    },
+    computedTransition () {
+      return this.top ? 'v-slide-y-transition' : 'v-slide-y-reverse-transition'
+    }
+  },
+
+  watch: {
+    isActive () {
+      if (this.isActive) {
+        setTimeout(() => (this.isActive = false), this.timeout)
+      }
+    }
+  },
+
+  render (h) {
+    const content = h('div', {
+      'class': 'snack__content',
+      directives: [{
+        name: 'show',
+        value: this.isActive
+      }]
+    }, [this.$slots.default])
+
+    return h('div', {
+      'class': this.classes
+    }, [h(this.computedTransition, {}, [content])])
+  }
+}
