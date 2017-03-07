f009d931483620a7b47773826ca05503502242d2
John Leider <john.j.leider@gmail.com>
2017-03-06 22:14:45 -0500

    menu fixes

--- src/components/menus/Menu.js
+++ src/components/menus/Menu.js
@@ -1,4 +1,5 @@
 import Toggleable from '../../mixins/toggleable'
+import { addOnceEventListener } from '../../util/helpers'
 
 export default {
   name: 'menu',
@@ -77,19 +78,21 @@ export default {
 
   watch: {
     isActive (val) {
-      if (val) this.activate()
-      else this.isContentActive = false
+      if (val) {
+        this.setDirection()
+          .then(this.updatePosition)
+          .then(() => (this.isContentActive = true))
+      } else {
+        this.isContentActive = false
+      }
     }
   },
 
-  methods: {
-    activate () {
-      this.window = window
-      this.setDirection()
-      this.$nextTick(() => { this.updatePosition() })
-      setTimeout(() => { this.isContentActive = true }, 50)
-    },
+  mounted () {
+    this.window = window
+  },
 
+  methods: {
     setDirection (horiz = '', vert = '') {
       this.direction = {
         horiz: horiz || (this.left && !this.auto ? 'left' : 'right'),
@@ -101,31 +104,36 @@ export default {
       this.position.left = this.direction.vert === 'left' ? 'auto' : '0px'
       this.position.bottom = this.direction.vert === 'bottom' ? 'auto' : '0px'
       this.position.right = this.direction.vert === 'right' ? 'auto' : '0px'
+
+      return Promise.resolve()
     },
 
     updatePosition () {
       this.updateDimensions()
+      return new Promise((resolve, reject) => {
+        this.$nextTick(this.assignPosition)
+        setTimeout(resolve, 0)
+      })
+    },
 
-      const { horiz, vert } = this.direction
-      const { offset } = this
-
-      this.position.left = horiz === 'left' ? 'auto' : `${offset.horiz}px`
-      this.position.top = vert === 'top' ? 'auto' : `${offset.vert}px`
-      this.position.right = horiz === 'right' ? 'auto' : `${-offset.horiz}px`
-      this.position.bottom = vert === 'bottom' ? 'auto' : `${-offset.vert}px`
+    assignPosition () {
+      this.position.left = this.direction.horiz === 'left' ? 'auto' : `${this.offset.horiz}px`
+      this.position.top = this.direction.vert === 'top' ? 'auto' : `${this.offset.vert}px`
+      this.position.right = this.direction.horiz === 'right' ? 'auto' : `${-this.offset.horiz}px`
+      this.position.bottom = this.direction.vert === 'bottom' ? 'auto' : `${-this.offset.vert}px`
     },
 
     updateDimensions () {
-      this.sneakPeek()
-      const { activator: a, content: c } = this.$refs
-
+      this.$refs.content.style.display = 'inline-block'
       this.dimensions = {
-        'activator': a.children ? this.measure(a.children[0]) : this.measure(a),
-        'content': this.measure(c),
-        'list': this.measure(c, '.list'),
-        'selected': this.measure(c, '.list__tile--active', 'parent')
+        activator: this.$refs.activator.children
+          ? this.measure(this.$refs.activator.children[0])
+          : this.measure(this.$refs.activator),
+        content: {},
+        list: {},
+        selected: {}
       }
-      this.sneakPeek(false)
+      this.$refs.content.style.display = 'none'
     },
 
     genActivator (h) {
@@ -148,16 +156,11 @@ export default {
     genTransition (h) {
       const data = {
         props: {
-          name: 'menu-transition',
           origin: this.origin
-        },
-        on: {
-          // 'before-enter': () => { this.beforeEnter() }
-          // 'enter': (el, done) => { this.enter() }
         }
       }
 
-      return h('transition', data, [this.genContent(h)])
+      return h('v-menu-transition', data, [this.genContent(h)])
     },
 
     genContent (h) {
@@ -168,21 +171,13 @@ export default {
           name: 'show',
           value: this.isContentActive
         }],
-        'class': { 'menu__content': true },
+        'class': 'menu__content',
         on: {
           click: () => { if (this.closeOnClick) this.isActive = false }
         }
       }
 
       return h('div', data, [this.$slots.default])
-    },
-
-    beforeEnter () {
-
-    },
-
-    enter (el, done) {
-
     },
 
     // Utils
@@ -193,18 +188,7 @@ export default {
       el = el && getParent ? el.parentElement : el
 
       if (!el) return null
-      const { top, left, bottom, right, width, height } = el.getBoundingClientRect()
-      return { top, left, bottom, right, width, height, offsetTop: el.offsetTop }
-    },
-
-    sneakPeek (on = true) {
-      if (on) {
-        // this.$refs.content.style.opacity = 0
-        this.$refs.content.style.display = 'inline-block'
-      } else {
-        this.$refs.content.style.display = 'none'
-        // this.$refs.content.style.opacity = null
-      }
+      return Object.assign(el.getBoundingClientRect(), { offsetTop: el.offsetTop })
     }
   },
 
