3cf5a4a5b4435c7036e1bb3a4373a9ff3ba34495
John Leider <john.j.leider@gmail.com>
2017-03-11 20:02:42 -0500

    cleaned up chip and list styles for new avatar component

--- src/components/lists/index.js
+++ src/components/lists/index.js
@@ -7,7 +7,7 @@ import ListTileAction from './ListTileAction'
 
 const ListItem = createSimpleFunctional('list__item', 'li')
 const ListTileActionText = createSimpleFunctional('list__tile__action-text', 'span')
-const ListTileAvatar = createSimpleFunctional('list__tile__avatar', 'div')
+const ListTileAvatar = createSimpleFunctional('list__tile__avatar', 'v-avatar')
 const ListTileContent = createSimpleFunctional('list__tile__content', 'div')
 const ListTileTitle = createSimpleFunctional('list__tile__title', 'div')
 const ListTileSubTitle = createSimpleFunctional('list__tile__sub-title', 'div')
