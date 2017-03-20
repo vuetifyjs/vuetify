63846082baada67e50414c64461af428feea4e87
John Leider <john.j.leider@gmail.com>
2017-03-20 18:48:37 -0400

    converted app to functional render fn

---
+++ src/components/app/App.js
@@ -0,0 +1,40 @@
+export default {
+  functional: true,
+
+  name: 'app',
+
+  props: {
+    footer: Boolean,
+    leftFixedSidebar: Boolean,
+    leftSidebar: Boolean,
+    id: {
+      type: String,
+      default: 'app'
+    },
+    rightFixedSidebar: Boolean,
+    rightSidebar: Boolean,
+    topFixedToolbar: Boolean,
+    topToolbar: Boolean,
+    sidebarUnderToolbar: Boolean
+  },
+
+  render (h, context) {
+    return h('div', {
+      'class': {
+        'with': true,
+        'left-fixed-sidebar': context.props.leftFixedSidebar,
+        'left-sidebar': context.props.leftSidebar,
+        'bottom-footer': context.props.footer,
+        'right-fixed-sidebar': context.props.rightFixedSidebar,
+        'right-sidebar': context.props.rightSidebar,
+        'top-fixed-toolbar': context.props.topFixedToolbar,
+        'top-toolbar': context.props.topToolbar,
+        'sidebar-under-toolbar': context.props.sidebarUnderToolbar
+      },
+      domProps: {
+        id: context.props.id,
+        'data-app': true
+      }
+    }, [context.children])
+  }
+}
