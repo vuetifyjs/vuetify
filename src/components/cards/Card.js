d510daf85c6333222fedd586064e20390a1189fe
Marco Ferreira <marco@altitude.co>
2017-03-24 15:09:30 -0300

    rolling back to a functional component

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,4 +1,6 @@
 export default {
+  functional: true,
+
   name: 'card',
 
   props: {
@@ -9,32 +11,22 @@ export default {
     horizontal: Boolean,
     img: String,
     hover: Boolean,
-    raised: Boolean
-  },
-
-  computed: {
-    classes () {
-      return {
-        'card': true,
-        'card--horizontal': this.horizontal,
-        'card--hover': this.hover,
-        'card--raised': this.raised,
-      }
-    },
+    raised: Boolean,
   },
 
   render (h, context) {
-    const data = {
-      'class': this.classes,
-      'style': {
-        height: this.height,
-      }
+    context.data.style = context.style || {}
+    context.data.class = context.data.class || []
+    context.data.style.height = context.props.height
+    context.data.class.push('card')
+    context.props.horizontal && context.data.class.push('card--horizontal')
+    context.props.hover && context.data.class.push('card--hover')
+    context.props.raised && context.data.class.push('card--raised')
+
+    if (context.props.img) {
+      context.data.style.background = `url(${context.props.img}) center center / cover no-repeat`
     }
 
-    if (this.img) {
-      data.style.background = `url(${this.img}) center center / cover no-repeat`
-    }
-
-    return h('div', data, this.$slots.default)
+    return h('div', context.data, context.children)
   }
 }
