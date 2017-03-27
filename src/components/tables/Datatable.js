15b2338636c13d4288b18b8d10a15bc5d252aa17
John Leider <john.j.leider@gmail.com>
2017-03-26 20:41:31 -0400

    finished initial elements

--- src/components/tables/Datatable.js
+++ src/components/tables/Datatable.js
@@ -21,8 +21,7 @@ export default {
 
   computed: {
     filteredItems () {
-      if (!this.sorting) return this.items
-
+      if (this.sorting === null) return this.items
       return this.items.sort((a, b) => {
         const sortA = a[Object.keys(a)[this.sorting]]
         const sortB = b[Object.keys(b)[this.sorting]]
@@ -41,23 +40,8 @@ export default {
   },
 
   methods: {
-    genHeader (item, index) {
-      const beingSorted = this.sorting === index
-      const icon = beingSorted && this.asc
-        ? 'arrow_upward'
-        : 'arrow_downward'
-
-      return this.$createElement('th', {
-        'class': {
-          'active': beingSorted
-        },
-        on: {
-          click: () => this.sort(index)
-        }
-      }, [this.$createElement('v-icon', icon), item])
-    },
     sort (index) {
-      if (!this.sorting) {
+      if (this.sorting === null) {
         this.sorting = index
         this.asc = true
       } else if (this.sorting === index && this.asc) {
@@ -69,18 +53,89 @@ export default {
         this.sorting = null
         this.asc = null
       }
+    },
+
+    genNextIcon () {
+      return this.$createElement('v-icon', {
+
+      }, 'chevron_right')
+    },
+    genPrevIcon () {
+      return this.$createElement('v-icon', {
+
+      }, 'chevron_left')
+    },
+    genHeader (item, index) {
+      const beingSorted = this.sorting === index
+      const icon = beingSorted && this.asc
+        ? 'arrow_upward'
+        : 'arrow_downward'
+
+      return this.$createElement('th', {
+        'class': {
+          'active': beingSorted
+        },
+        on: { click: () => this.sort(index) }
+      }, [
+        this.$createElement('v-icon', icon),
+        this.$scopedSlots.headers ? this.$scopedSlots.headers({ item }) : item
+      ])
+    },
+    genSelect () {
+      return this.$createElement('div', {}, 'Rows per page:')
+    },
+    genPagination () {
+      return this.$createElement('div', {}, '1-10 of 100')
+    },
+    genActions () {
+      return [this.$createElement('div', {
+        'class': 'datatable__actions'
+      }, [
+        this.genSelect(),
+        this.genPagination(),
+        this.genPrevIcon(),
+        this.genNextIcon()
+      ])]
+    },
+    genTR (children) {
+      return this.$createElement('tr', {}, children)
+    },
+    genTHead () {
+      const children = this.headers.map((o, i) => this.genHeader(o, i))
+      const checkbox = this.$createElement('v-checkbox', {
+        props: {
+          'hide-details': true,
+          primary: true
+        }
+      })
+
+      children.unshift(this.$createElement('th', [checkbox]))
+
+      return this.$createElement('thead', [this.genTR(children)])
+    },
+    genTBody () {
+      return this.$createElement('tbody', this.filteredItems.map(item => this.$scopedSlots.items({ item })))
+    },
+    genTFoot () {
+      return this.$createElement('tfoot', [
+        this.genTR([
+          this.$createElement('td', {
+            domProps: { colspan: '100%' }
+          }, this.genActions())
+        ])
+      ])
     }
   },
 
   render (h) {
-    const head = h('thead', [
-      h('tr', this.headers.map((o, i) => this.genHeader(o, i)))
+    return h('v-table-overflow', {}, [
+      h('table', {
+        'class': 'datatable'
+      }, [
+        this.genTHead(),
+        this.genTBody(),
+        this.genTFoot()
+      ])
     ])
-
-    const body = h('tbody', this.filteredItems.map(i => this.$scopedSlots.items({ item: i })))
-
-    return h('table', {
-      'class': 'datatable'
-    }, [head, body])
   }
 }
