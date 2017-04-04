ef33c16476d9574869bd2787382a370d1cdecf8f
John Leider <john.j.leider@gmail.com>
2017-04-03 20:40:40 -0400

    enhancement for #289

--- src/mixins/route-link.js
+++ src/mixins/route-link.js
@@ -2,7 +2,14 @@ export default {
   props: {
     append: Boolean,
     disabled: Boolean,
+    exact: {
+      type: Boolean,
+      default () {
+        return this.href === '/'
+      }
+    },
     href: [String, Object],
+    to: [String, Object],
     nuxt: Boolean,
     replace: Boolean,
     ripple: Boolean,
@@ -14,32 +21,31 @@ export default {
     click () {},
     generateRouteLink () {
       let tag
+      const options = this.to || this.href
 
       const data = {
         attrs: {},
         class: this.classes,
         props: {},
-        directives: [
-          {
-            name: 'ripple',
-            value: this.ripple || false
-          }
-        ]
+        directives: [{
+          name: 'ripple',
+          value: this.ripple || false
+        }]
       }
 
-      if (this.href && this.router) {
+      if (options && this.router) {
         tag = this.nuxt ? 'nuxt-link' : 'router-link'
-        data.props.to = this.href
-        data.props.exact = this.href === '/'
+        data.props.to = options
+        data.props.exact = this.exact
         data.props.activeClass = this.activeClass
         data.props.append = this.append
         data.props.replace = this.replace
         data.nativeOn = { click: this.click }
       } else {
         tag = this.tag || 'a'
-        
+
         if (tag === 'a') {
-          data.attrs.href = this.href || 'javascript:;'
+          data.attrs.href = options || 'javascript:;'
         }
 
         data.on = { click: this.click }
