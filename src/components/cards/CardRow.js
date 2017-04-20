e12ec14d66352b3e0da1b4d4b74e86cc7b80f1e9
Marco Ferreira <m@rco.cc>
2017-04-19 22:56:03 -0300

    Moving action buttons stacking logic into card-row (#397)
    * converting Modal into Dialog
    
    * adding styles and cleaning component
    
    * adding stacked actions styling and functionality
    
    * add 'stacked' css class to the component root
    
    * adding fullscreen styles
    
    * enabling optional 'overlay' prop
    
    * only execute resize math when active
    
    * enabling transitions
    
    * enabling transition origin
    
    * fix overlay content positioning
    
    * remove legacy file
    
    * import dialogs styles
    
    * fixes: dialog not pushed to the top when not using overlay
    
    * applying review tweaks
    
    * overlay 'on' by default and cleanup
    
    * target only component .card
    
    * adding car-actions mixin
    
    * cleanup
    
    * added activator
    
    * stacked layout styling fix
    
    * add minimum screen edge margin
    
    * prevent resize() to be called before dialog is fully displayed
    
    * Moving action buttons stacking logic into card-row
    
    card-row is not a render function, moved stacked buttons styles to cards.styl
    
    * cleanup

---
+++ src/components/cards/CardRow.js
@@ -0,0 +1,89 @@
+export default {
+  name: 'card-row',
+
+  props: {
+    actions: Boolean,
+
+    height: {
+      type: String,
+      default: 'auto'
+    },
+
+    img: String
+  },
+
+  data () {
+    return {
+      stackedActions: false
+    }
+  },
+
+  computed: {
+    classes () {
+      return {
+        'card__row': true,
+        'card__row--actions': this.actions,
+        'card__row--actions-stacked': this.stackedActions,
+      }
+    },
+
+    styles () {
+      const styles = {
+        height: this.height
+      }
+
+      if (this.img) {
+        styles.background = `url(${this.img}) center center / cover no-repeat`
+      }
+
+      return styles
+    }
+  },
+
+  methods: {
+    resize () {
+      let btns = this.$refs.actions.children
+
+      let maxButtonWidth = (this.$parent.$refs.dialog.offsetWidth - 8 - (8 * btns.length)) / btns.length
+      let shouldStack = false
+
+      for (let i = btns.length; i--;) {
+        if (btns[i].children.length) {
+          let span = btns[i].children[0]
+          if (span.scrollWidth > maxButtonWidth) {
+            shouldStack = true
+            break
+          }
+        }
+      }
+
+      this.stackedActions = shouldStack
+    }
+  },
+
+  mounted () {
+    if (this.actions) {
+      this.$vuetify.load(() => {
+        window.addEventListener('resize', this.resize, false)
+        this.$refs.actions.addEventListener('transitionend', this.resize, false)
+      })
+    }
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('resize', this.resize, false)
+    this.$refs.actions.removeEventListener('transitionend', this.resize, false)
+  },
+
+  render (h) {
+    let data = {
+      'class': this.classes,
+      style: this.styles
+    }
+
+    if (this.actions) data.ref = 'actions'
+
+    return h('div', data, this.$slots.default)
+  },
+
+}
