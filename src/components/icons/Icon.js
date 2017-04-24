b94084928844d232f050093027ce0edaf5a558f1
John Leider <john.j.leider@gmail.com>
2017-04-24 15:31:02 -0400

    init

--- src/components/icons/Icon.js
+++ src/components/icons/Icon.js
@@ -2,6 +2,7 @@ export default {
   name: 'icon',
 
   props: {
+    light: Boolean,
     large: Boolean,
     left: Boolean,
     medium: Boolean,
@@ -12,12 +13,14 @@ export default {
   computed: {
     classes () {
       return {
-        'material-icons icon': true,
+        'icon--dark': !this.light,
         'icon--large': this.large,
         'icon--left': this.left,
+        'icon--light': this.light,
         'icon--medium': this.medium,
         'icon--right': this.right,
-        'icon--x-large': this.xLarge
+        'icon--x-large': this.xLarge,
+        'material-icons icon': true
       }
     }
   },
