b9ec471185637425805f2b2cc78caee85d68f40c
Albert Kaaman <albert@kaaman.nu>
2017-06-09 20:11:30 +0200

    Datatable slots: headers/headerCell (#738)
    * data table items slot can now accept a tr
    
    * data table headers slot now works like items slot, added headerCell slot for old behaviour

--- src/components/tables/mixins/head.js
+++ src/components/tables/mixins/head.js
@@ -1,56 +1,66 @@
 export default {
   methods: {
     genTHead () {
-      const children = this.headers.map(o => this.genHeader(o))
-      const checkbox = this.$createElement('v-checkbox', {
-        props: {
-          dark: this.dark,
-          light: this.light,
-          primary: this.selectAll === 'primary' || this.selectAll === true,
-          secondary: this.selectAll === 'secondary',
-          success: this.selectAll === 'success',
-          info: this.selectAll === 'info',
-          warning: this.selectAll === 'warning',
-          error: this.selectAll === 'error',
-          hideDetails: true,
-          inputValue: this.all,
-          indeterminate: this.indeterminate
-        },
-        on: { change: this.toggle }
-      })
+      let children = []
 
-      this.selectAll !== false && children.unshift(this.$createElement('th', [checkbox]))
+      if (this.$scopedSlots.headers) {
+        const row = this.$scopedSlots.headers({ headers: this.headers })
 
-      return this.$createElement('thead', [this.genTR(children)])
+        children = row.length && row[0].tag === 'tr' ? row : this.genTR(row)
+      } else {
+        const row = this.headers.map(o => this.genHeader(o))
+        const checkbox = this.$createElement('v-checkbox', {
+          props: {
+            dark: this.dark,
+            light: this.light,
+            primary: this.selectAll === 'primary' || this.selectAll === true,
+            secondary: this.selectAll === 'secondary',
+            success: this.selectAll === 'success',
+            info: this.selectAll === 'info',
+            warning: this.selectAll === 'warning',
+            error: this.selectAll === 'error',
+            hideDetails: true,
+            inputValue: this.all,
+            indeterminate: this.indeterminate
+          },
+          on: { change: this.toggle }
+        })
+
+        this.selectAll && row.unshift(this.$createElement('th', [checkbox]))
+
+        children = this.genTR(row)
+      }
+
+      return this.$createElement('thead', [children])
     },
-    genHeader (item) {
+    genHeader (header) {
       const array = [
-        this.$scopedSlots.headers
-          ? this.$scopedSlots.headers({ item })
-          : item[this.headerText]
+        this.$scopedSlots.headerCell
+          ? this.$scopedSlots.headerCell({ header })
+          : header[this.headerText]
       ]
 
-      return this.$createElement('th', ...this.genHeaderData(item, array))
+      return this.$createElement('th', ...this.genHeaderData(header, array))
     },
-    genHeaderData (item, children) {
+    genHeaderData (header, children) {
       let beingSorted = false
       const classes = ['column']
       const data = {}
 
-      if ('sortable' in item && item.sortable || !('sortable' in item)) {
-        data.on = { click: () => this.sort(item.value) }
-        !('value' in item) && console.warn('Data table headers must have a value property that corresponds to a value in the v-model array')
+      if ('sortable' in header && header.sortable || !('sortable' in header)) {
+        data.on = { click: () => this.sort(header.value) }
+        !('value' in header) && console.warn('Data table headers must have a value property that corresponds to a value in the v-model array')
 
         classes.push('sortable')
         const icon = this.$createElement('v-icon', 'arrow_upward')
-        item.left && children.push(icon) || children.unshift(icon)
+        header.left && children.push(icon) || children.unshift(icon)
 
-        beingSorted = this.computedPagination.sortBy === item.value
+        beingSorted = this.computedPagination.sortBy === header.value
         beingSorted && classes.push('active')
         beingSorted && this.computedPagination.descending && classes.push('desc') || classes.push('asc')
       }
 
-      item.left && classes.push('text-xs-left') || classes.push('text-xs-right')
+      header.left && classes.push('text-xs-left') || classes.push('text-xs-right')
 
       data.class = classes
 
