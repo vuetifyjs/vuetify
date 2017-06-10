3185a18bf14364f04d68a3a944eb6695ad9536d5
nekosaur <albert@kaaman.nu>
2017-06-10 12:53:10 +0200

    updated Alert tests, added Snackbar tests

--- test/unit/specs/Alert.spec.js
+++ test/unit/specs/Alert.spec.js
@@ -6,22 +6,12 @@ import Alert from 'src/components/alerts/Alert'
 import Icon from 'src/components/icons/Icon'
 
 Alert.components = { 'v-icon': Icon }
-Vue.use(Vuetify)
 
 describe('Alert.vue', () => {
   it('should have an alert class', () => {
     const wrapper = mount(Alert)
 
     expect(wrapper.hasClass('alert')).toBe(true)
-  })
-
-  it('should render correct contents', () => {
-    const wrapper = mount(Alert)
-    const renderer = createRenderer()
-
-    renderer.renderToString(wrapper.vm, (err, str) => {
-      expect(str).toMatchSnapshot()
-    })
   })
 
   it('should have a close icon', () => {
@@ -37,18 +27,43 @@ describe('Alert.vue', () => {
     expect(wrapper.data().isActive).toBe(false)
   })
 
-  it('should close when close icon is clicked', () => {
-    let isActive = true
+  it('should emit input:false when close icon is clicked', () => {
     const wrapper = mount(Alert, {
       propsData: {
-        value: isActive,
+        value: true,
         dismissible: true
       }
     })
     const icon = wrapper.find('.alert__dismissible')[0]
+    const input = jest.fn()
+    wrapper.instance().$on('input', input)
     icon.dispatch('click')
 
-    expect(isActive).toBe(false)
+    expect(input).toBeCalledWith(false)
+  })
+
+  it('should not be visible after being dismissed', done => {
+    const wrapper = mount(Alert, {
+      propsData: {
+        value: true,
+        dismissible: true
+      }
+    })
+
+    wrapper.vm.$on('input', (val) => {
+      wrapper.setProps({
+        'value': false
+      })
+      wrapper.update()
+
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.hasStyle('display', 'none')).toBe(true)
+        done()
+      })
+    })
+
+    const icon = wrapper.find('.alert__dismissible')[0]
+    icon.dispatch('click')
   })
 
   it('should have a custom icon', () => {
