0c32a7965fa02ff4cb484b6b522638cdbbee37fa
John Leider <jleider@fastforwardacademy.com>
2017-06-14 13:48:37 -0400

    card tweaks

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -3,13 +3,51 @@ import {
   createSimpleFunctional
 } from '../../util/helpers'
 
-const CardText = createSimpleFunctional('card__text')
-const CardTitle = createSimpleFunctional('card__title')
 const CardActions = createSimpleFunctional('card__actions')
+const CardText = createSimpleFunctional('card__text')
+
+const CardMedia = {
+  name: 'card-media',
+
+  props: {
+    height: {
+      type: [Number, String],
+      default: 'auto'
+    }
+  },
+
+  render (h) {
+    const data = {
+      'class': 'card__media',
+      style: {
+        height: !isNaN(this.height) ? `${this.height}px` : this.height
+      }
+    }
+
+    return h('div', data, this.$slots.default)
+  }
+}
+
+const CardTitle = {
+  functional: true,
+
+  props: {
+    primary: Boolean
+  },
+
+  render (h, { data, props, children }) {
+    data.staticClass = (`card__title ${data.staticClass || ''}`).trim()
+
+    if (props.primary) data.staticClass += ' card__title--primary'
+
+    return h('div', data, children)
+  }
+}
 
 export default {
   Card,
   CardActions,
+  CardMedia,
   CardText,
   CardTitle
 }
--- src/util/helpers.js
+++ src/util/helpers.js
@@ -3,7 +3,7 @@ export function createSimpleFunctional (c, el = 'div') {
     functional: true,
 
     render: (h, { data, children }) => {
-      data.staticClass = data.staticClass ? `${c} ${data.staticClass}` : c
+      data.staticClass = data.staticClass ? `${c} ${data.staticClass || ''}` : c
 
       return h(el, data, children)
     }
