8221c7567d44d72d8c93a287cfe62ce0e6c3fae4
nekosaur <albert@kaaman.nu>
2017-06-23 11:36:29 +0200

    more tab updates

--- src/components/tabs/TabsBar.js
+++ src/components/tabs/TabsBar.js
@@ -1,45 +1,57 @@
 export default {
   name: 'tabs-bar',
 
+  inject: ['isScrollable'],
+
   data () {
     return {
       resizeDebounce: null,
       isOverflowing: false,
-      scrollOffset: 0
+      scrollOffset: 0,
+      itemOffset: 0
     }
-  },
-
-  props: {
-    mobile: Boolean
   },
 
   computed: {
     classes () {
       return {
-        'tabs__bar': true,
-        'tabs__bar--mobile': this.mobile
+        'tabs__bar': true
       }
     },
     containerClasses () {
       return {
-        'tabs__container': true,
-        'tabs__container--overflow': this.isOverflowing
+        'tabs__container': true
+      }
+    },
+    wrapperClasses () {
+      return {
+        'tabs__wrapper': true,
+        'tabs__wrapper--scrollable': this.isScrollable,
+        'tabs__wrapper--overflow': this.isOverflowing
       }
     },
     containerStyles () {
-      // fix min/max values
       return {
         'transform': `translateX(-${this.scrollOffset}px)`
       }
+    },
+    leftIconVisible () {
+      return this.isScrollable && this.isOverflowing && this.scrollOffset > 0
+    },
+    rightIconVisible () {
+      if (!this.isScrollable || !this.isOverflowing) return
+
+      const remaining = this.$refs.container.scrollWidth - (this.scrollOffset + this.$refs.container.clientWidth)
+      return remaining > (this.$refs.container.children[this.itemOffset].clientWidth * 0.25)
     }
   },
 
   methods: {
     scrollLeft () {
-      this.scrollOffset -= 150
+      this.scrollOffset = this.newOffsetLeft()
     },
     scrollRight () {
-      this.scrollOffset += 150
+      this.scrollOffset = this.newOffsetRight()
     },
     resize () {
       clearTimeout(this.resizeDebounce)
@@ -47,12 +59,46 @@ export default {
       this.resizeDebounce = setTimeout(() => {
         this.isOverflowing = this.$refs.container.clientWidth < this.$refs.container.scrollWidth
       }, 50)
+    },
+    newOffsetLeft () {
+      const items = this.$refs.container.children
+      let offset = 0
+
+      for (let i = this.itemOffset - 1; i >= 0; i--) {
+        if (!items[i].classList.contains('tabs__slider')) {
+          if (offset + items[i].clientWidth >= this.$refs.container.clientWidth) {
+            this.itemOffset = i
+            return this.scrollOffset - offset
+          }
+          offset += items[i].clientWidth
+        }
+      }
+
+      this.itemOffset = 0
+      return 0
+    },
+    newOffsetRight () {
+      const items = this.$refs.container.children
+      let offset = this.scrollOffset
+
+      for (let i = this.itemOffset; i < items.length; i++) {
+        if (!items[i].classList.contains('tabs__slider')) {
+          if (offset + items[i].clientWidth > this.scrollOffset + this.$refs.container.clientWidth) {
+            this.itemOffset = i
+            return offset
+          }
+          offset += items[i].clientWidth
+        }
+      }
+
+      return 0
     }
   },
 
   mounted () {
     this.$vuetify.load(() => {
       window.addEventListener('resize', this.resize, { passive: true })
+      this.resize()
     })
   },
 
@@ -71,10 +117,9 @@ export default {
       props: {
         left: true
       },
-      directives: [{
-        name: 'ripple',
-        value: ''
-      }],
+      style: {
+        display: this.leftIconVisible ? 'inline-flex' : 'none'
+      },
       on: {
         click: this.scrollLeft
       }
@@ -84,19 +129,18 @@ export default {
       props: {
         right: true
       },
-      directives: [{
-        name: 'ripple',
-        value: ''
-      }],
+      style: {
+        display: this.rightIconVisible ? 'inline-flex' : 'none'
+      },
       on: {
         click: this.scrollRight
       }
     }, 'chevron_right')
 
-    const wrapper = h('div', { class: 'tabs__wrapper' }, [container])
+    const wrapper = h('div', { class: this.wrapperClasses }, [container])
 
     return h('div', {
       'class': this.classes
-    }, [this.isOverflowing && left, wrapper, this.isOverflowing && right])
+    }, [wrapper, left, right])
   }
 }
