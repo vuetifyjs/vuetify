3f43268759f78001a6e9263704444a38768f7b92
nekosaur <albert@kaaman.nu>
2017-06-25 15:27:34 +0200

    some code cleanup

--- src/components/buttons/Morph.js
+++ src/components/buttons/Morph.js
@@ -2,9 +2,11 @@ import BezierEasing from 'bezier-easing'
 import Toggleable from '../../mixins/toggleable'
 import Fab from './mixins/fab'
 
+// const easeInOutCubic = BezierEasing(0.645, 0.045, 0.355, 1)
 const easeInCubic = BezierEasing(0.55, 0.055, 0.675, 0.19)
 const easeOutCubic = BezierEasing(0.215, 0.61, 0.355, 1)
-const easeInOutCubic = BezierEasing(0.645, 0.045, 0.355, 1)
+const easeInQuart = BezierEasing(0.895, 0.03, 0.685, 0.22)
+const easeOutQuart = BezierEasing(0.165, 0.84, 0.44, 1)
 
 export default {
   name: 'morph',
@@ -19,17 +21,13 @@ export default {
   },
 
   props: {
-    transition: {
-      type: String,
-      default: 'morph-transition'
-    },
-    activatorTransition: {
-      type: String,
-      default: 'activator-morph-transition'
-    },
     toolbar: Boolean,
     sheet: Boolean,
-    sheetHeight: Number
+    sheetHeight: Number,
+    closeOnOutsideClick: {
+      type: Boolean,
+      default: true
+    }
   },
 
   computed: {
@@ -55,7 +53,6 @@ export default {
 
       // Move content to parent element, if sheet
       if (this.sheet) {
-        console.log(this.$refs.content)
         this.$el.parentNode.insertBefore(this.$refs.content.$el, this.$el.parentNode.firstChild)
       }
     })
@@ -70,25 +67,21 @@ export default {
       clearTimeout(this.resizeDebounce)
 
       this.resizeDebounce = setTimeout(() => {
-        console.log(this)
         this.containerWidth = this.$el.parentNode.clientWidth
       }, 50)
     },
     beforeEnterTransition (el) {
-      console.log('before enter')
       const left = this.containerWidth - ((el.clientWidth / 2) + 16)
       const target = left - this.containerWidth / 2
 
       el.querySelector('.btn__content').style.transition = 'none'
       el.querySelector('.btn__content').style.opacity = 0
       el.style.transition = 'none'
-      el.style.zIndex = 0
       el.style.boxShadow = 'none'
       this.addTransform(el, `translateX(-${target}px)`)
       this.addTransform(el, `translateY(${this.toolbar ? 16 : this.sheetHeight / 2}px)`)
     },
     enterTransition (el, done) {
-      console.log('enter')
       const left = this.containerWidth - ((el.clientWidth / 2) + 16)
       const targetHorizontal = left - this.containerWidth / 2
       const targetVertical = this.toolbar ? 16 : this.sheetHeight / 2
@@ -101,33 +94,28 @@ export default {
         }, 500),
         this.animate(d => this.opacity(el, 0, 1, d), 300, 100),
         this.animate(p => this.horizontal(el, -targetHorizontal, 0, p), 300, 200, easeOutCubic),
-        this.animate(p => this.vertical(el, targetVertical, 0, p), 300, 200, easeInCubic),
+        this.animate(p => this.vertical(el, targetVertical, 0, p), 300, 200, easeInCubic)
       ]).then(() => done())
     },
     afterEnterTransition (el) {
-      console.log('after enter')
-      //el.style.transition = 'none'
       this.$el.style['-webkit-clip-path'] = 'none'
     },
     beforeLeaveTransition (el) {
       el.style.transition = 'none'
       el.querySelector('.btn__content').style.opacity = 0
-
     },
     leaveTransition (el, done) {
-      console.log('leave')
       const left = this.containerWidth - ((el.clientWidth / 2) + 16)
-      const targetHorizontal = left - this.containerWidth / 2
+      const targetHorizontal = left - (this.containerWidth * 0.75)
       const targetVertical = this.toolbar ? 16 : this.sheetHeight / 2
 
       Promise.all([
-        this.animate(d => this.opacity(el, 1, 0, d), 200, 100),
+        this.animate(d => this.opacity(el, 1, 0, d), 200, 150),
         this.animate(p => this.horizontal(el, 0, -targetHorizontal, p), 300, 0, easeInCubic),
-        this.animate(p => this.vertical(el, 0, targetVertical, p), 300, 0, easeOutCubic),
+        this.animate(p => this.vertical(el, 0, targetVertical, p), 300, 0, easeOutCubic)
       ]).then(() => done())
     },
     afterLeaveTransition (el) {
-      console.log('after leave')
       this.$el.style.transition = 'none'
     },
     timeout (fn, delay) {
@@ -163,12 +151,10 @@ export default {
     },
     height (el, start, end, delta) {
       const value = start + (delta * (end - start))
-
       el.style.height = `${value}px`
     },
     opacity (el, start, end, delta) {
       const value = start + (delta * (end - start))
-      //el.style.opacity = `${target > 0 ? target * delta : 1 * (1 - delta)})}`
       el.style.opacity = `${value}`
     },
     scale (el, start, end, delta) {
@@ -202,11 +188,10 @@ export default {
 
   render (h) {
     let children = []
+    const directives = this.closeOnOutsideClick ? [{ name: 'click-outside' }] : []
     const data = {
       'class': this.classes,
-      directives: [{
-        name: 'click-outside'
-      }],
+      directives,
       on: {
         click: () => (this.isActive = !this.isActive)
       }
@@ -234,13 +219,10 @@ export default {
       },
       on: {
         beforeLeave: (el) => {
-          console.log('before leave')
           el.style.opacity = 1
           el.style.clipPath = `circle(100%)`
         },
         leave: (el, done) => {
-          console.log('leave group')
-
           Promise.all([
             this.timeout(() => {
               el.querySelectorAll('.btn__content').forEach(e => e.style.opacity = 0)
@@ -250,44 +232,41 @@ export default {
             this.animate(d => {
               const value = this.interpolate(this.containerWidth / 2, 28, d)
               el.style.clipPath = `circle(${value}px)`
-            }, 400, easeOutCubic),
+            }, 400, easeOutQuart),
             this.sheet && this.animate(d => this.height(el, this.sheetHeight, 0, d), 300, 200, easeInCubic)
           ]).then(() => done())
         },
         afterLeave: (el) => {
-          console.log('after leave')
         },
         beforeEnter: (el) => {
-          el.style.zIndex = 5
           el.style.transition = 'none'
           el.style.opacity = 0
-          el.style.clipPath = 'circle(10%)'
+          el.style.clipPath = 'circle(1%)'
           el.querySelectorAll('.btn__content').forEach(e => e.style.opacity = 0)
+          this.addTransform(el, `translateX(${(this.containerWidth / 2) - 56}px)`)
           this.sheet && (el.style.height = '0px')
         },
         enter: (el, done) => {
-          console.log('enter group')
-
           Promise.all([
-            this.timeout(() => el.style.opacity = 1, 300),
-            this.animate(d => this.horizontal(el, this.containerWidth / 4, 0, d), 200, 300, easeOutCubic),
+            this.animate(d => this.opacity(el, 0, 1, d), 400, 0, easeInCubic),
+            this.animate(d => this.horizontal(el, (this.containerWidth / 2) - 56, 0, d), 300, 200, easeOutCubic),
             this.animate(d => {
-              const value = 10 + (d * (100 - 10))
+              const value = this.interpolate(28, this.containerWidth / 2, d)
               el.style.clipPath = `circle(${value}%)`
-            }, 300, 200, easeInCubic),
+            }, 400, 150, easeInQuart),
             this.timeout(() => {
               el.querySelectorAll('.btn__content').forEach(e => e.style.opacity = 1)
             }, 450),
-            this.sheet && this.animate(d => this.height(el, 0, this.sheetHeight, d), 300, 100, easeOutCubic)
+            this.sheet && this.animate(d => this.height(el, 0, this.sheetHeight, d), 350, 50, easeOutCubic)
           ]).then(() => done())
         },
         afterEnter: (el) => {
-
         }
       }
     }, children)
 
-    const activatorTransition = h('transition', {
+    const activator = h('transition', {
+      ref: 'activator',
       props: {
         css: false,
         tag: 'div'
@@ -299,15 +278,11 @@ export default {
         beforeLeave: this.beforeLeaveTransition,
         leave: this.leaveTransition,
         afterLeave: this.afterLeaveTransition
-      }
-    }, [!this.isActive && this.$slots.activator.map((e, i) => { e.key = i; return e })])
-
-    const activator = h('div', {
-      ref: 'activator',
+      },
       class: {
         'morph--activator': true
       }
-    }, [activatorTransition])
+    }, [!this.isActive && this.$slots.activator.map((e, i) => { e.key = i; return e })])
 
     return h('div', data, [activator, transition])
   }
