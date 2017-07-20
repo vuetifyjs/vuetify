212f8ec38446f1c5be9d8a17f48cbcd74a089316
Marco Ferreira <m@rco.cc>
2017-07-21 00:02:56 +0200

    init tests

---
+++ src/components/cards/VCard.spec.js
@@ -0,0 +1,63 @@
+import { mount } from 'avoriaz'
+import Vue from 'vue/dist/vue.common'
+import VCard from 'src/components/cards/VCard'
+
+describe('VCard.vue', () => {
+  it('should render component and match snapshot', () => {
+    const wrapper = mount(VCard)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a flat card', () => {
+    const wrapper = mount(VCard, {
+      propsData: {
+        flat: true
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a raised card', () => {
+    const wrapper = mount(VCard, {
+      propsData: {
+        raised: true
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a tile card', () => {
+    const wrapper = mount(VCard, {
+      propsData: {
+        tile: true
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a card with custom height', () => {
+    const wrapper = mount(VCard, {
+      propsData: {
+        height: '400px'
+      }
+    })
+
+    // expect(wrapper.hasStyle('height', '400px')).to.equal(true)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  // it('should render a tile card', () => {
+  //   const wrapper = mount(VCard, {
+  //     propsData: {
+  //       tile: true
+  //     }
+  //   })
+
+  //   expect(wrapper.hasStyle('border-radius', '0')).to.equal(true)
+  //   expect(wrapper.html()).toMatchSnapshot()
+  // })
+})
