175cc1a38068f1666d5700a88102413305ae16e7
John Leider <jleider@fastforwardacademy.com>
2017-07-21 13:41:35 -0400

    test for #1081

--- src/components/selects/VSelect.spec.js
+++ src/components/selects/VSelect.spec.js
@@ -2,7 +2,20 @@ import { test } from '~util/testing'
 import VSelect from '~components/selects/VSelect'
 
 test('VSelect.js', ({ mount }) => {
-  it('should be in an error state when required and blurred with empty', () => {
-    console.log(VSelect)
+  it('should return numeric 0', () => {
+    const item = { value: 0, text: '0' }
+    const wrapper = mount(VSelect, {
+      propsData: {
+        value: null,
+        items: [item],
+        multiple: true
+      }
+    })
+
+    const change = jest.fn()
+    wrapper.instance().$on('change', change)
+    wrapper.instance().selectItem(item)
+
+    expect(change).toBeCalledWith([0])
   })
 })
