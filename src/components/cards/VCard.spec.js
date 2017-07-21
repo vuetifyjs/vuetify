13e8a5b77678980e128f96346e72bcc96b83efc6
Marco Ferreira <m@rco.cc>
2017-07-22 00:44:04 +0200

    snapshot tests

--- src/components/cards/VCard.spec.js
+++ src/components/cards/VCard.spec.js
@@ -1,31 +1,54 @@
-import { mount } from 'avoriaz'
-import Vue from 'vue/dist/vue.common'
-import VCard from 'src/components/cards/VCard'
+import { test } from '~util/testing'
+import VCard from '~components/cards/VCard'
 
-describe('VCard.vue', () => {
+test('VCard.js', ({ mount, functionalContext }) => {
+// describe('VCard.vue', () => {
   it('should render component and match snapshot', () => {
-    const wrapper = mount(VCard)
+    const wrapper = mount(VCard, functionalContext())
 
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render a flat card', () => {
-    const wrapper = mount(VCard, {
+    const wrapper = mount(VCard, functionalContext({
       propsData: {
         flat: true
       }
-    })
+    }))
 
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render a raised card', () => {
-    const wrapper = mount(VCard, {
+    const wrapper = mount(VCard, functionalContext({
       propsData: {
         raised: true
       }
-    })
+    }))
 
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a tile card', () => {
+    const wrapper = mount(VCard, functionalContext({
+      propsData: {
+        tile: true
+      }
+    }))
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a card with custom height', () => {
+    const wrapper = mount(VCard, functionalContext({
+      propsData: {
+        height: '400px'
+      }
+    }))
+
+    console.log(wrapper.hasStyle('height', '400px'))
+
+    expect(wrapper.hasStyle('height', '400px')).toBe(true)
     expect(wrapper.html()).toMatchSnapshot()
   })
 
@@ -36,28 +59,7 @@ describe('VCard.vue', () => {
       }
     })
 
+    expect(wrapper.hasClass('card--tile')).toBe(true)
     expect(wrapper.html()).toMatchSnapshot()
   })
-
-  it('should render a card with custom height', () => {
-    const wrapper = mount(VCard, {
-      propsData: {
-        height: '400px'
-      }
-    })
-
-    // expect(wrapper.hasStyle('height', '400px')).to.equal(true)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  // it('should render a tile card', () => {
-  //   const wrapper = mount(VCard, {
-  //     propsData: {
-  //       tile: true
-  //     }
-  //   })
-
-  //   expect(wrapper.hasStyle('border-radius', '0')).to.equal(true)
-  //   expect(wrapper.html()).toMatchSnapshot()
-  // })
 })
