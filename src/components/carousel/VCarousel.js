e00ab2f952dd77aa7980392a7b79089c8bcabde6
John Leider <john.j.leider@gmail.com>
2017-07-23 16:08:52 -0400

    enhancements for #946

--- src/components/carousel/VCarousel.js
+++ src/components/carousel/VCarousel.js
@@ -1,11 +1,12 @@
 import Bootable from '~mixins/bootable'
 import VBtn from '~components/buttons/VBtn'
 import VIcon from '~components/icons/VIcon'
+import Themeable from '~mixins/themeable'
 
 export default {
   name: 'v-carousel',
 
-  mixins: [Bootable],
+  mixins: [Bootable, Themeable],
 
   data () {
     return {
@@ -28,7 +29,15 @@ export default {
     interval: {
       type: Number,
       default: 6000
-    }
+    },
+    leftControlIcon: {
+      type: [Boolean, String],
+      default: 'chevron_left'
+    },
+    rightControlIcon: {
+      type: [Boolean, String],
+      default: 'chevron_right'
+    },
   },
 
   computed: {
@@ -68,17 +77,20 @@ export default {
         staticClass: 'carousel__controls'
       }, this.genItems())
     },
-    genIcon (direction, fn) {
+    genIcon (direction, icon, fn) {
+      if (!icon) return null
+
       return this.$createElement('div', {
         staticClass: `carousel__${direction}`
       }, [
         this.$createElement(VBtn, {
           props: {
             icon: true,
-            dark: true
+            dark: this.dark || !this.light,
+            light: this.light
           },
           on: { click: fn }
-        }, [this.$createElement(VIcon, `chevron_${direction}`)])
+        }, [this.$createElement(VIcon, icon)])
       ])
     },
     genItems () {
@@ -90,7 +102,8 @@ export default {
           },
           props: {
             icon: true,
-            dark: true
+            dark: this.dark || !this.light,
+            light: this.light
           },
           key: index,
           on: { click: this.select.bind(this, index) }
@@ -142,8 +155,8 @@ export default {
         }
       }]
     }, [
-      this.genIcon('left', this.prev),
-      this.genIcon('right', this.next),
+      this.genIcon('left', this.leftControlIcon, this.prev),
+      this.genIcon('right', this.rightControlIcon, this.next),
       this.genControls(),
       this.$slots.default
     ])
