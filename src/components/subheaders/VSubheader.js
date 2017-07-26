b7b4451aaac9283c5ca0c033134d703808d74854
Alex Kozack <cawa-93@users.noreply.github.com>
2017-07-26 15:02:16 +0300

    Fix support dark theme
    Fix by analogy with the <v-divider>
    https://github.com/vuetifyjs/vuetify/blob/16515c3ebb9734218ef3d833b6c5cc56a7cc65c7/src/components/dividers/VDivider.js#L16

--- src/components/subheaders/VSubheader.js
+++ src/components/subheaders/VSubheader.js
@@ -13,7 +13,7 @@ export default {
     data.staticClass = (`subheader ${data.staticClass || ''}`).trim()
 
     if (props.inset) data.staticClass += ' subheader--inset'
-    if (!props.dark) data.staticClass += ' theme--light'
+    if (props.light) data.staticClass += ' theme--light'
     if (props.dark) data.staticClass += ' theme--dark'
 
     return h('li', data, children)
