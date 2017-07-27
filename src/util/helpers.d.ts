70487beb98b8c0e64fc3fc9ce1be62f96130b8db
Isaac Lyman <isaaclyman@gmail.com>
2017-07-27 09:19:06 -0600

    Add TypeScript definitions for Vuetify plugin and createSimpleTransition

---
+++ src/util/helpers.d.ts
@@ -0,0 +1,5 @@
+declare module 'vuetify/src/util/helpers' {
+    import { FunctionalComponentOptions } from 'vue'
+
+    export function createSimpleTransition(name: string): FunctionalComponentOptions
+}
