7337259b7a3e756c3b778122b94aca28747eee04
Jay <4063783+Phlow2001@users.noreply.github.com>
2017-07-30 13:41:15 -0400

    Check Vue version satisfied minimum requirements, in CDN use-case

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-require('./stylus/main.styl')
+﻿require('./stylus/main.styl')
 
 import Components from './components'
 import * as Directives from './directives'
 import Load from './util/load'
+import semver from 'semver'
 
 function plugin (Vue) {
   Object.keys(Components).forEach(key => {
@@ -18,7 +19,16 @@ function plugin (Vue) {
   }
 }
 
+function checkVueVersion () {
+  if (!semver.satisfies(window.Vue.version, '>=2.4.0')) {
+    console.error('Vuetify requires Vue version >= 2.4.0')
+  }
+}
+
 if (typeof window !== 'undefined' && window.Vue) {
+  if (window.Vue.version) {
+    checkVueVersion()
+  }
   window.Vue.use(plugin)
 }
 
