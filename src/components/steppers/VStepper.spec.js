e38e648fda59a65a72d985d35ee57c8b1c079f92
John Leider <john.j.leider@gmail.com>
2017-08-01 21:57:03 -0400

    removed spec

--- src/components/steppers/VStepper.spec.js
+++
@@ -1,9 +0,0 @@
-// import { test } from '~util/testing'
-// import VStepper from '~components/steppers/VStepper'
-// import VStepperContent from '~components/steppers/VStepperContent'
-
-// test('VStepper.js', ({ mount }) => {
-//   it('should handle dynamic steps', () => {
-//     const wrapper = mount(VStepper)
-//   })
-// })
