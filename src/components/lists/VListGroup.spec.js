b3e4347afc86938ae791a14c5049fb0edbee2ef0
Marco Ferreira <m@rco.cc>
2017-08-07 01:21:04 +0200

    add snapshots for List, ListGroup and ListTileAction

---
+++ src/components/lists/VListGroup.spec.js
@@ -0,0 +1,44 @@
+import VListGroup from '~components/lists/VListGroup'
+import { test } from '~util/testing'
+
+test('VListGroup.js', ({ mount }) => {
+  it('should render component and match snapshot', () => {
+    const wrapper = mount(VListGroup)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a lazy component and match snapshot', () => {
+    const wrapper = mount(VListGroup, {
+      propsData: {
+        lazy: true
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a component with no padding for action icon and match snapshot', () => {
+    const wrapper = mount(VListGroup, {
+      propsData: {
+        noAction: true
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render a component with route namespace and match snapshot', () => {
+    const $route = { path: '' }
+    const wrapper = mount(VListGroup, {
+      propsData: {
+        group: 'listGroup'
+      },
+      globals: {
+        $route
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
