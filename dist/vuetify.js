6ba9dc1e52a1a266140e3ea0e67a17c258fda612
John Leider <john.j.leider@gmail.com>
2017-08-13 09:01:29 -0400

    [build] 0.14.11

--- dist/vuetify.js
+++ dist/vuetify.js
@@ -2514,7 +2514,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 module.exports = {
 	"name": "vuetify",
-	"version": "0.14.9",
+	"version": "0.14.10",
 	"author": {
 		"name": "John Leider",
 		"email": "john.j.leider@gmail.com"
