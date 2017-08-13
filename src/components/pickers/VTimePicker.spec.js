465fb6818a21ab496a2777a8829685ab8fc45a62
Albert Kaaman <albert@kaaman.nu>
2017-08-13 15:06:28 +0200

    fixed bug where VTimePicker did not set am/pm indicator correctly (#1336)

--- src/components/pickers/VTimePicker.spec.js
+++ src/components/pickers/VTimePicker.spec.js
@@ -21,21 +21,14 @@ test('VTimePicker.js', ({ mount }) => {
   })
 
   it('should accept a date object for a value', () => {
-    const now = new Date(2017, 7, 1, 0, 0, 0, 0)
+    const now = new Date('2017-01-01 12:00 AM')
     const wrapper = mount(VTimePicker, {
       propsData: {
         value: now
       }
     })
-    let hour = now.getHours()
-    let period = 'am'
-    const minutes = now.getMinutes()
-    if (hour > 12) {
-      hour -= 12
-      period = 'pm'
-    }
 
-    expect(wrapper.vm.inputTime).toBe(`${hour}:${minutes}${period}`)
+    expect(wrapper.vm.inputTime).toEqual('12:00am')
     expect(wrapper.html()).toMatchSnapshot()
   })
 
@@ -63,5 +56,45 @@ test('VTimePicker.js', ({ mount }) => {
     const ampm = wrapper.instance().inputTime.match(/(am|pm)/)
 
     expect(ampm).toBe(null)
+  })
+
+  it('should set picker to pm when given Date after noon', () => {
+    const wrapper = mount(VTimePicker, {
+      propsData: {
+        value: new Date('2017-01-01 12:00 PM')
+      }
+    })
+
+    expect(wrapper.data().period).toEqual('pm')
+  })
+
+  it('should set picker to pm when given string with PM in it', () => {
+    const wrapper = mount(VTimePicker, {
+      propsData: {
+        value: '1:00 PM'
+      }
+    })
+
+    expect(wrapper.data().period).toEqual('pm')
+  })
+
+  it('should set picker to pm when given string with pm in it', () => {
+    const wrapper = mount(VTimePicker, {
+      propsData: {
+        value: '1:00 pm'
+      }
+    })
+
+    expect(wrapper.data().period).toEqual('pm')
+  })
+
+  it('should set picker to am when given Date before noon', () => {
+    const wrapper = mount(VTimePicker, {
+      propsData: {
+        value: new Date('2017-01-01 1:00 AM')
+      }
+    })
+
+    expect(wrapper.data().period).toEqual('am')
   })
 })
