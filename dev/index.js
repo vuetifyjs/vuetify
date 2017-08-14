7c965d7da885115f67d4d40a1925ccc958583218
nekosaur <albert@kaaman.nu>
2017-08-14 18:04:03 +0200

    fixed path bugs, added initial onResize call to resize directive

--- dev/index.js
+++ dev/index.js
@@ -1,22 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 
-import {
-  VApp,
-  VAlert,
-  VAvatar,
-  VBottomNav,
-  VBreadcrumbs,
-  VBreadcrumbsItem,
-  VBtn
-} from 'vuetify/components'
-
-Vue.use(VApp)
-Vue.use(VAlert)
-Vue.use(VAvatar)
-Vue.use(VBottomNav)
-Vue.use(VBreadcrumbs)
-Vue.use(VBreadcrumbsItem)
-Vue.use(VBtn)
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
 
 new Vue(App).$mount('#app')
