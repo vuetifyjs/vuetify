498f440e3d77109eab3460f9c20b1736c9e46635
John Leider <john.j.leider@gmail.com>
2017-08-14 20:09:49 -0400

    updated/fixed v-list-group

--- src/components/VList/VListGroup.spec.js
+++ src/components/VList/VListGroup.spec.js
@@ -1,17 +1,24 @@
-import { VListGroup } from '~components/VList'
+import { VList, VListGroup } from '~components/VList'
 import { test } from '~util/testing'
 
 test('VListGroup.js', ({ mount }) => {
   it('should render component and match snapshot', () => {
-    const wrapper = mount(VListGroup)
+    const wrapper = mount(VList, {
+      slots: {
+        default: [VListGroup]
+      }
+    })
 
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render a lazy component and match snapshot', () => {
-    const wrapper = mount(VListGroup, {
+    const wrapper = mount(VList, {
       propsData: {
         lazy: true
+      },
+      slots: {
+        default: [VListGroup]
       }
     })
 
@@ -19,9 +26,12 @@ test('VListGroup.js', ({ mount }) => {
   })
 
   it('should render a component with no padding for action icon and match snapshot', () => {
-    const wrapper = mount(VListGroup, {
+    const wrapper = mount(VList, {
       propsData: {
         noAction: true
+      },
+      slots: {
+        default: [VListGroup]
       }
     })
 
@@ -30,9 +40,12 @@ test('VListGroup.js', ({ mount }) => {
 
   it('should render a component with route namespace and match snapshot', () => {
     const $route = { path: '' }
-    const wrapper = mount(VListGroup, {
+    const wrapper = mount(VList, {
       propsData: {
         group: 'listGroup'
+      },
+      slots: {
+        default: [VListGroup]
       },
       globals: {
         $route
