a2ca591bc0c46975af4b9b393e7d456433b52549
Albert Kaaman <albert@kaaman.nu>
2017-08-19 15:00:25 +0200

    enhancement/pagination (#1417)
    * refactored VPagination to resize list automatically. Also added totalVisible prop for manual control
    
    * updated tests
    
    * added theme support for VPagination
    
    * added test

--- src/components/VPagination/VPagination.spec.js
+++ src/components/VPagination/VPagination.spec.js
@@ -2,13 +2,16 @@ import { test } from '~util/testing'
 import VPagination from './VPagination'
 
 test('VPagination.vue', ({ mount }) => {
-  it('emits an event when next or previous is clicked', () => {
+  it('emits an event when next or previous is clicked', async () => {
+    jest.useFakeTimers()
     const wrapper = mount(VPagination, {
-      attachToDocument: true,
       propsData: {
         length: 5
       }
     })
+    jest.runAllTimers()
+
+    await wrapper.vm.$nextTick()
 
     const previous = jest.fn()
     const next = jest.fn()
@@ -23,5 +26,54 @@ test('VPagination.vue', ({ mount }) => {
     expect(next).toBeCalled()
     expect(previous).toBeCalled()
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should only render start and end of range if length is big', async () => {
+    jest.useFakeTimers()
+    const wrapper = mount(VPagination, {
+      propsData: {
+        length: 100
+      }
+    })
+    jest.runAllTimers()
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.find('.pagination__more').length).toEqual(1)
+  })
+
+  it('should only render middle of range if length is big and value is somewhere in the middle', async () => {
+    jest.useFakeTimers()
+    const wrapper = mount(VPagination, {
+      propsData: {
+        length: 100,
+        value: 50
+      }
+    })
+    jest.runAllTimers()
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.find('.pagination__more').length).toEqual(2)
+  })
+
+  it('should use totalVisible prop if defined', async () => {
+    jest.useFakeTimers()
+    const wrapper = mount(VPagination, {
+      propsData: {
+        length: 100,
+        value: 50,
+        totalVisible: 10
+      }
+    })
+    jest.runAllTimers()
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.find('.pagination__more').length).toEqual(2)
+    expect(wrapper.find('.pagination__item').length).toEqual(8)
   })
 })
