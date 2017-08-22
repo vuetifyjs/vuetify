b37dd303a260a9777bb32f4c63a9b0370198592c
jk <jacek@karczmarczyk.pl>
2017-08-22 09:58:51 +0200

    minor refactoring to please codebeat

--- src/components/VDatePicker/mixins/month-table.js
+++ src/components/VDatePicker/mixins/month-table.js
@@ -13,82 +13,76 @@ export default {
     touch (value) {
       this.tableDate = new Date(this.tableYear + value, 0)
     },
+    genTouch () {
+      return {
+        name: 'touch',
+        value: {
+          left: e => (e.offsetX < -15) && this.touch(1),
+          right: e => (e.offsetX > 15) && this.touch(-1)
+        }
+      }
+    },
     genTable () {
-      const children = []
-      const data = {
-        'class': 'picker--date__table',
-        directives: [
-          {
-            name: 'touch',
-            value: {
-              left: (e) => (e.offsetX < -15) && this.touch(1),
-              right: (e) => (e.offsetX > 15) && this.touch(-1)
-            }
-          }
-        ]
-      }
-
-      if (this.scrollable) {
-        data.on = { wheel: this.wheelScroll }
-      }
-
-      children.push(this.$createElement('table', {
-        key: this.tableYear
-      }, [this.genTBody()]))
-
-      return this.$createElement('div', data, [
+      return this.$createElement('div', {
+        staticClass: 'picker--date__table',
+        on: this.scrollable ? { wheel: this.wheelScroll } : undefined,
+        directives: [this.genTouch()]
+      }, [
         this.$createElement('transition', {
           props: { name: this.computedTransition }
-        }, children)
+        }, [
+          this.$createElement('table', {
+            key: this.tableYear
+          }, [
+            this.genTBody()
+          ])
+        ])
+      ])
+    },
+    genTD (month) {
+      const date = new Date(this.tableYear, month, 1, 12, 0, 0, 0)
+      const monthName = date.toLocaleString(this.locale, { month: 'short' })
+      return this.$createElement('td', [
+        this.$createElement('button', {
+          'class': {
+            'btn': true,
+            'btn--raised': this.isActive(month),
+            'btn--flat': !this.isActive(month),
+            'btn--active': this.isActive(month),
+            'btn--current': this.isCurrent(month),
+            'btn--light': this.dark,
+            'btn--disabled': !this.isAllowed(date)
+          },
+          attrs: {
+            type: 'button'
+          },
+          domProps: {
+            innerHTML: `<span class="btn__content">${monthName}</span>`
+          },
+          on: {
+            click: () => {
+              const tableYear = this.tableYear
+              month = month < 9 ? `0${month + 1}` : (month + 1)
+
+              this.inputDate = `${tableYear}-${month}-01T12:00:00`
+              this.$nextTick(() => (this.autosave && this.save()))
+            }
+          }
+        })
       ])
     },
     genTBody () {
       const children = []
-      const cols = 3
+      const cols = Array(3).fill(null)
+      const rows = 12 / cols.length
 
-      for (let row = 0; row < 12 / cols; row++) {
-        const rows = []
-        for (let col = 0; col < cols; col++) {
-          const i = row * cols + col
-          const date = new Date(this.tableYear, i, 1, 12, 0, 0, 0)
-          const month = date.toLocaleString(this.locale, { month: 'short' })
-          rows.push(this.$createElement('td', [
-            this.$createElement('button', {
-              'class': {
-                'btn': true,
-                'btn--raised': this.isActive(i),
-                'btn--flat': !this.isActive(i),
-                'btn--active': this.isActive(i),
-                'btn--current': this.isCurrent(i),
-                'btn--light': !this.dark,
-                'btn--disabled': !this.isAllowed(date)
-              },
-              attrs: {
-                type: 'button'
-              },
-              domProps: {
-                innerHTML: `<span class="btn__content">${month}</span>`
-              },
-              on: {
-                click: () => {
-                  const tableYear = this.tableYear
-                  let month = i + 1
-                  month = month < 10 ? `0${month}` : (month)
-
-                  this.inputDate = `${tableYear}-${month}-01T12:00:00`
-                  this.$nextTick(() => (this.autosave && this.save()))
-                }
-              }
-            })
-          ]))
-        }
-        children.push(this.genTR(rows))
+      for (let row = 0; row < rows; row++) {
+        children.push(this.$createElement('tr', cols.map((_, col) => {
+          return this.genTD(row * cols.length + col)
+        })))
       }
 
       return this.$createElement('tbody', children)
-    },
-    genTR (children = [], data = {}) {
-      return [this.$createElement('tr', data, children)]
     },
     isActive (i) {
       return this.tableYear === this.year &&
