23d67cecbd020c89a5efbb93d4834e79a91a8cb2
Albert Kaaman <albert@kaaman.nu>
2017-08-24 17:03:45 +0200

    enhancement/dialog-scroll (#1457)
    * trying out scroll solution for dialog
    
    * added ie11 support
    
    * focus dialog content when activated
    
    * updated scroll checking, added mobile check
    
    * fixed tests

--- src/mixins/overlayable.js
+++ src/mixins/overlayable.js
@@ -66,6 +66,83 @@ export default {
         } catch (e) {}
       }, this.overlayTransitionDuration)
     },
+    scrollListener (e) {
+      if (e.type === 'keydown') {
+        const up = [38, 33, 36]
+        const down = [40, 34, 35]
+
+        if (up.includes(e.keyCode)) {
+          e.deltaY = -1
+        } else if (down.includes(e.keyCode)) {
+          e.deltaY = 1
+        } else {
+          return
+        }
+      }
+
+      if (e.target === this.overlay ||
+        (e.type !== 'keydown' && e.target === document.body) ||
+        this.checkPath(e)) e.preventDefault()
+    },
+    composedPath (e) {
+      const path = []
+      let el = e.target
+
+      while (el) {
+        path.push(el)
+
+        if (el.tagName === 'HTML') {
+          path.push(document)
+          path.push(window)
+
+          return path
+        }
+
+        el = el.parentElement
+      }
+    },
+    hasScrollbar (el) {
+      const style = window.getComputedStyle(el)
+      return ['auto', 'scroll'].includes(style['overflow-y'])
+    },
+    shouldScroll (el, delta) {
+      if (el.scrollTop === 0 && delta < 0) return true
+      else if (el.scrollTop + el.clientHeight === el.scrollHeight && delta > 0) return true
+      else return false
+    },
+    isInside (el, parent) {
+      if (el === parent) {
+        return true
+      } else if (el === null || el === document.body) {
+        return false
+      } else {
+        return this.isInside(el.parentNode, parent)
+      }
+    },
+    checkPath (e) {
+      const path = e.path || this.composedPath(e)
+      const delta = e.deltaY || -e.wheelDelta
+
+      if (e.type === 'keydown' && path[0] === document.body) {
+        const dialog = this.$refs.content.querySelector('.dialog')
+        const selected = window.getSelection().anchorNode
+        if (this.hasScrollbar(dialog) && this.isInside(selected, dialog)) {
+          return this.shouldScroll(dialog, delta)
+        }
+        return true
+      }
+
+      for (let i = 0; i < path.length; i++) {
+        const el = path[i]
+
+        if (el === document) return true
+        else if (this.hasScrollbar(el)) {
+          return this.shouldScroll(el, delta)
+        } else if (el === this.$refs.content) return true
+      }
+
+      return true
+    },
     hideScroll () {
       // Check documentElement first for IE11
       // this.overlayOffset = document.documentElement &&
@@ -74,10 +151,21 @@ export default {
 
       // document.body.style.top = `-${this.overlayOffset}px`
       // document.body.style.position = 'fixed'
-      document.documentElement.style.overflow = 'hidden'
+
+      if (this.$vuetify.breakpoint.mdAndDown) {
+        document.documentElement.style.overflow = 'hidden'
+      } else {
+        window.addEventListener('mousewheel', this.scrollListener)
+        window.addEventListener('keydown', this.scrollListener)
+      }
     },
     showScroll () {
-      document.documentElement.removeAttribute('style')
+      if (this.$vuetify.breakpoint.mdAndDown) {
+        document.documentElement.removeAttribute('style')
+      } else {
+        window.removeEventListener('mousewheel', this.scrollListener)
+        window.removeEventListener('keydown', this.scrollListener)
+      }
 
       // if (!this.overlayOffset) return
       // document.body.scrollTop = this.overlayOffset
