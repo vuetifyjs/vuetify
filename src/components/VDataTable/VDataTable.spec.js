24c2b74639d4cf7d39dcfe1af7ac477be6b5ea34
nekosaur <albert@kaaman.nu>
2017-08-31 18:41:36 +0200

    Fixed #1544 VDataTable should now emit totalItems correctly

--- src/components/VDataTable/VDataTable.spec.js
+++ src/components/VDataTable/VDataTable.spec.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import { test } from '~util/testing'
 import VDataTable from './VDataTable'
 
@@ -30,4 +31,37 @@ test('VDataTable.js', ({ mount }) => {
     expect('Application is missing <v-app> component.').toHaveBeenTipped()
     // Also expect tests to not crash :)
   })
+
+/* avoriaz needs to support scoped slots
+  it('should emit pagination object with correct items length', async () => {
+    const vm = new Vue()
+    const wrapper = mount(VDataTable, {
+      propsData: {
+        pagination: {},
+        headers: [
+          { text: 'First', value: 'first' },
+          { text: 'Second', value: 'first' }
+        ],
+        items: [
+          { first: 1, second: 'foo' },
+          { first: 2, second: 'bar' },
+          { first: 3, second: 'baz' }
+        ]
+      },
+      scopedSlots: {
+        items: (props) => vm.createElement('td', [props.item.first])
+      }
+    })
+
+    const change = jest.fn()
+    wrapper.instance().$on('update:pagination', change)
+
+    wrapper.setProps({ search: 'f' })
+
+    await wrapper.vm.$nextTick()
+
+    expect('Application is missing <v-app> component.').toHaveBeenTipped()
+    expect(change).toBeCalledWith({ page: 1, totalItems: 1 })
+  })
+*/
 })
