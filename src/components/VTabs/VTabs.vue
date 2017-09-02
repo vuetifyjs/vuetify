8d431f1acf184c9a31c15d372212ca9d969ff1f9
John Leider <john.j.leider@gmail.com>
2017-09-02 18:36:02 -0400

    fixes #1574

--- src/components/VTabs/VTabs.vue
+++ src/components/VTabs/VTabs.vue
@@ -76,7 +76,6 @@
       },
       activeIndex () {
         this.updateTabs()
-        this.$emit('input', this.target)
         this.isBooted = true
       },
       tabItems (newItems, oldItems) {
@@ -171,6 +170,8 @@
           const nextIndex = this.content.findIndex(o => o.id === target)
           this.reverse = nextIndex < this.activeIndex
           setActiveIndex(nextIndex)
+
+          this.$emit('input', this.target)
         })
       },
       updateTabs () {
