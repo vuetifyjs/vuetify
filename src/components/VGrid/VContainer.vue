62535c1bca35efffe76ec851eaffe85a8b0ed643
nekosaur <albert@kaaman.nu>
2017-09-02 10:29:29 +0200

    Disregard previous VGrid bug fix. This one should hopefully do it

--- src/components/VGrid/VContainer.vue
+++ src/components/VGrid/VContainer.vue
@@ -1,10 +1,7 @@
 <script>
-  export default {
-    name: 'v-grid',
-    render (h) {
-      return h('div')
-    }
-  }
+  import Grid from './grid'
+
+  export default Grid('container')
 </script>
 
 <style lang="stylus" src="../../stylus/components/_grid.styl"></style>
--- src/components/VGrid/index.js
+++ src/components/VGrid/index.js
@@ -1,41 +1,20 @@
 import {
   createSimpleFunctional
 } from '../../util/helpers'
-import VGrid from './VGrid'
+import VContainer from './VContainer'
+import VFlex from './VFlex'
+import VLayout from './VLayout'
 
-const Grid = (name) => ({
-  name: `v-${name}`,
-
-  functional: true,
-
-  props: {
-    id: String
-  },
-
-  render: (h, { props, data, children }) => {
-    data.staticClass = (`${name} ${data.staticClass || ''}`).trim()
-
-    if (data.attrs) {
-      data.staticClass += ` ${Object.keys(data.attrs).join(' ')}`
-      delete data.attrs
-    }
-
-    if (props.id) {
-      data.domProps = data.domProps || {}
-      data.domProps.id = props.id
-    }
-
-    return h('div', data, children)
-  }
-})
-
-export const VContainer = Grid('container')
-export const VFlex = Grid('flex')
-export const VLayout = Grid('layout')
 export const VSpacer = createSimpleFunctional('spacer')
+export {
+  VContainer,
+  VFlex,
+  VLayout
+}
+
+const VGrid = {}
 
 VGrid.install = function install (Vue) {
-  Vue.component(VGrid.name, VGrid)
   Vue.component(VContainer.name, VContainer)
   Vue.component(VFlex.name, VFlex)
   Vue.component(VLayout.name, VLayout)
