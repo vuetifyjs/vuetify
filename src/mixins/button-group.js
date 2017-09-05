85bae7dd711002533eb210c049a09c2bd8746980
Kael <kaelwd@gmail.com>
2017-09-06 05:29:30 +1000

    Allow any data type in button values
    See #1596

--- src/mixins/button-group.js
+++ src/mixins/button-group.js
@@ -8,47 +8,45 @@ export default {
 
   methods: {
     getValue (i) {
-      return ('value' in this.buttons[i]) && !!this.buttons[i].value
-        ? this.buttons[i].value
-        : i
+      return this.buttons[i].value == null ? i : this.buttons[i].value
     },
     update () {
       const selected = []
 
-      this.buttons
-        .forEach((elm, i) => {
-          // Fix for testing, dataset does not exist on elm?
-          if (!elm.dataset) elm.dataset = {}
+      this.buttons.forEach((button, i) => {
+        const elm = button.$el
 
-          elm.removeAttribute('data-only-child')
+        // Fix for testing, dataset does not exist on elm?
+        if (!elm.dataset) elm.dataset = {}
 
-          if (this.isSelected(i)) {
-            elm.setAttribute('data-selected', true)
-            elm.classList.add('btn--active')
-            selected.push(i)
-          } else {
-            elm.removeAttribute('data-selected')
-            elm.classList.remove('btn--active')
-          }
+        elm.removeAttribute('data-only-child')
 
-          elm.dataset.index = i
-        })
+        if (this.isSelected(i)) {
+          elm.setAttribute('data-selected', true)
+          elm.classList.add('btn--active')
+          selected.push(i)
+        } else {
+          elm.removeAttribute('data-selected')
+          elm.classList.remove('btn--active')
+        }
+
+        elm.dataset.index = i
+      })
 
       if (selected.length === 1) {
-        this.buttons[selected[0]].setAttribute('data-only-child', true)
+        this.buttons[selected[0]].$el.setAttribute('data-only-child', true)
       }
     }
   },
 
   mounted () {
-    const options = { passive: true }
     this.$vuetify.load(() => {
-      this.buttons = this.$slots.default
-        .filter(vnode => vnode.tag !== undefined)
-        .map((vnode, i) => {
+      this.buttons = this.$children
+        .filter(button => button.$vnode.tag !== undefined)
+        .map((button, i) => {
           this.listeners.push(this.updateValue.bind(this, i))
-          vnode.elm.addEventListener('click', this.listeners[i], options)
-          return vnode.elm
+          button.$on('click', this.listeners[i])
+          return button
         })
 
       this.update()
@@ -56,8 +54,8 @@ export default {
   },
 
   beforeDestroy () {
-    this.buttons.forEach((elm, i) => {
-      elm.removeEventListener('click', this.listeners[i])
+    this.buttons.forEach((button, i) => {
+      button.$off('click', this.listeners[i])
     })
   }
 }
