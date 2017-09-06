add9c95b1f9a548ed63972ed0b3d907990e5332a
Kael <kaelwd@gmail.com>
2017-09-06 19:54:18 +1000

    Add DOM fallback for getValue()

--- src/mixins/button-group.js
+++ src/mixins/button-group.js
@@ -8,30 +8,40 @@ export default {
 
   methods: {
     getValue (i) {
-      return this.buttons[i].value == null ? i : this.buttons[i].value
+      if (this.buttons[i].value != null) {
+        return this.buttons[i].value
+      }
+
+      // Fix for testing, this should always be false in the browser
+      if (this.buttons[i].$el.value != null && this.buttons[i].$el.value !== '') {
+        return this.buttons[i].$el.value
+      }
+
+      return i
     },
     update () {
       const selected = []
 
-      this.buttons.forEach((button, i) => {
-        const elm = button.$el
+      this.buttons
+        .forEach((button, i) => {
+          const elm = button.$el
 
-        // Fix for testing, dataset does not exist on elm?
-        if (!elm.dataset) elm.dataset = {}
+          // Fix for testing, dataset does not exist on elm?
+          if (!elm.dataset) elm.dataset = {}
 
-        elm.removeAttribute('data-only-child')
+          elm.removeAttribute('data-only-child')
 
-        if (this.isSelected(i)) {
-          elm.setAttribute('data-selected', true)
-          elm.classList.add('btn--active')
-          selected.push(i)
-        } else {
-          elm.removeAttribute('data-selected')
-          elm.classList.remove('btn--active')
-        }
+          if (this.isSelected(i)) {
+            elm.setAttribute('data-selected', true)
+            elm.classList.add('btn--active')
+            selected.push(i)
+          } else {
+            elm.removeAttribute('data-selected')
+            elm.classList.remove('btn--active')
+          }
 
-        elm.dataset.index = i
-      })
+          elm.dataset.index = i
+        })
 
       if (selected.length === 1) {
         this.buttons[selected[0]].$el.setAttribute('data-only-child', true)
@@ -42,12 +52,11 @@ export default {
   mounted () {
     this.$vuetify.load(() => {
       this.buttons = this.$children
-        .filter(button => button.$vnode.tag !== undefined)
-        .map((button, i) => {
-          this.listeners.push(this.updateValue.bind(this, i))
-          button.$on('click', this.listeners[i])
-          return button
-        })
+
+      this.buttons.forEach((button, i) => {
+        this.listeners.push(this.updateValue.bind(this, i))
+        button.$on('click', this.listeners[i])
+      })
 
       this.update()
     })
