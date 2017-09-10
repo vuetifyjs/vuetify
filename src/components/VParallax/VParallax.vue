76a80c338b68eb1f09ca03e52f23bcf4ff74093a
John Leider <john.j.leider@gmail.com>
2017-09-10 10:54:50 -0400

    fixed issues with parallax not properly loading

--- src/components/VParallax/VParallax.vue
+++ src/components/VParallax/VParallax.vue
@@ -38,6 +38,19 @@
     },
 
     methods: {
+      init () {
+        if (!this.$refs.img) return
+
+        if (this.$refs.img.complete) {
+          this.translate()
+          this.listeners()
+        } else {
+          this.$refs.img.addEventListener('load', () => {
+            this.translate()
+            this.listeners()
+          }, false)
+        }
+      },
       objHeight () {
         return this.$refs.img.naturalHeight
       },
@@ -52,9 +65,6 @@
       }, [
         h('img', {
           staticClass: 'parallax__image',
-          'class': {
-            'parallax__image--jumbotron': this.jumbotron
-          },
           style: this.styles,
           attrs: {
             src: this.src
@@ -64,29 +74,16 @@
       ])
 
       const content = h('div', {
-        staticClass: 'parallax__content',
-        style: {
-          minHeight: isNaN(this.height) ? this.height : `${this.height}px`
-        }
+        staticClass: 'parallax__content'
       }, this.$slots.default)
-
-      let directives = this.directives()
-
-      if (this.jumbotron) {
-        directives = directives.filter(d => d.name !== 'scroll')
-      }
 
       return h('div', {
         staticClass: 'parallax',
         style: {
-          minHeight: isNaN(this.height) ? this.height : `${this.height}px`
+          height: `${parseInt(this.normalizedHeight)}px`
         },
-        directives,
         on: this.$listeners
-      }, [
-        container,
-        content
-      ])
+      }, [container, content])
     }
   }
 </script>
