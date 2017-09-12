72f58304d61297e5ce9b4f5d76cd1c606a3b7d84
Jacek Karczmarczyk <jkarczm@gmail.com>
2017-09-12 23:26:36 +0200

    Update VTextField.spec.js

--- src/components/VTextField/VTextField.spec.js
+++ src/components/VTextField/VTextField.spec.js
@@ -166,7 +166,6 @@ test('VTextField.js', ({ mount }) => {
   })
 
   it('should not clear input if not clearble and has appended icon (without callback)', async () => {
-    let called = 0
     const wrapper = mount(VTextField, {
       propsData: {
         value: 'foo',
@@ -178,7 +177,6 @@ test('VTextField.js', ({ mount }) => {
     icon.trigger('click')
     await wrapper.vm.$nextTick()
     expect(wrapper.vm.inputValue).toBe('foo')
-    expect(called).toBe(0)
   })
 
   it('should start validating on blur', async () => {
