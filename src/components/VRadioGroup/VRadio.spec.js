3b601d2d3ed6d612708e9083b1d0cf5cba129aae
jk <jacek@karczmarczyk.pl>
2017-09-13 21:38:57 +0200

    vradio tests updated (added 'provide' and snapshots)

--- src/components/VRadioGroup/VRadio.spec.js
+++ src/components/VRadioGroup/VRadio.spec.js
@@ -3,10 +3,14 @@ import { /* VRadioGroup, */ VRadio } from '~components/VRadioGroup'
 
 test('VRadio.vue', ({ mount }) => {
   it('should advise about v-radio-group being necessary', () => {
-    mount(VRadio)
+    mount(VRadio, {
+      provide: {
+        name: () => 'name',
+        isMandatory: () => false
+      }
+    })
 
     expect('immediate parent of v-radio-group').toHaveBeenTipped()
-    expect('Injection').toHaveBeenWarned() // TODO: testing library needs injection mock support
   })
 
   // TODO: Enable test when there's a way to test $parent.$vnode.tag
@@ -23,6 +27,10 @@ test('VRadio.vue', ({ mount }) => {
     const wrapper = mount(VRadio, {
       data: {
         isActive: false
+      },
+      provide: {
+        name: () => 'name',
+        isMandatory: () => false
       }
     })
 
@@ -33,9 +41,9 @@ test('VRadio.vue', ({ mount }) => {
     wrapper.setData({ 'isActive': true })
     inputGroup = wrapper.find('.input-group')[0]
     expect(inputGroup.getAttribute('aria-checked')).toBe('true')
+    expect(wrapper.html()).toMatchSnapshot()
 
     expect('immediate parent of v-radio-group').toHaveBeenTipped()
-    expect('Injection').toHaveBeenWarned() // TODO: testing library needs injection mock support
   })
 
   it('should render aria-label attribute with label value on input group', () => {
@@ -43,27 +51,50 @@ test('VRadio.vue', ({ mount }) => {
       propsData: {
         label: 'Test'
       },
-      attrs: {}
+      attrs: {},
+      provide: {
+        name: () => 'name',
+        isMandatory: () => false
+      }
     })
 
     const inputGroup = wrapper.find('.input-group')[0]
     expect(inputGroup.getAttribute('aria-label')).toBe('Test')
+    expect(wrapper.html()).toMatchSnapshot()
 
     expect('immediate parent of v-radio-group').toHaveBeenTipped()
-    expect('Injection').toHaveBeenWarned() // TODO: testing library needs injection mock support
   })
 
   it('should not render aria-label attribute with no label value on input group', () => {
     const wrapper = mount(VRadio, {
       propsData: {
         label: null
+      },
+      provide: {
+        name: () => 'name',
+        isMandatory: () => false
       }
     })
 
     const inputGroup = wrapper.find('.input-group')[0]
     expect(inputGroup.element.getAttribute('aria-label')).toBeFalsy()
+    expect(wrapper.html()).toMatchSnapshot()
 
     expect('immediate parent of v-radio-group').toHaveBeenTipped()
-    expect('Injection').toHaveBeenWarned() // TODO: testing library needs injection mock support
+  })
+
+  it('should render proper input name', () => {
+    const wrapper = mount(VRadio, {
+      provide: {
+        name: () => 'name',
+        isMandatory: () => false
+      }
+    })
+
+    const input = wrapper.find('input')[0]
+    expect(input.getAttribute('name')).toBe('name')
+    expect(wrapper.html()).toMatchSnapshot()
+
+    expect('immediate parent of v-radio-group').toHaveBeenTipped()
   })
 })
