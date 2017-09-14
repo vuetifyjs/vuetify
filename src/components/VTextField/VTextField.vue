vuetifyjs/vuetify

commit d43bfb4b67007bbf71ae3352b839781be2e11aca

npm packages update, linting, updated tests (#1717)
* Update VTextField.vue

* bumped versions of everything

* avoriaz: hasAttribute => getAttribute

* removed ' is readonly' check - SHOULD IT BE REMOVED?

* lint

* linting

* downgraded postcss-loader, added eslint plugins

src/components/VTextField/VTextField.vue
@@ -164,7 +164,7 @@
             tabindex: this.tabindex,
             'aria-label': (!this.$attrs || !this.$attrs.id) && this.label // Label `for` will be set if we have an id
           },
-          on: Object.assign(listeners, {
+          on: Object.assign({}, listeners, {
             blur: this.blur,
             input: this.onInput,
             focus: this.focus

src/components/VApp/VApp.spec.js
@@ -27,7 +27,7 @@ test('VApp.js', ({ mount }) => {
   it('should have data-app attribute', () => {
     const wrapper = mount(VApp)
 
-    expect(wrapper.hasAttribute('data-app', 'true')).toBe(true)
+    expect(wrapper.getAttribute('data-app')).toBe('true')
     expect(wrapper.html()).toMatchSnapshot()
   })
 
@@ -38,7 +38,7 @@ test('VApp.js', ({ mount }) => {
       }
     })
 
-    expect(wrapper.hasAttribute('id', 'inspire')).toBe(true)
+    expect(wrapper.getAttribute('id')).toBe('inspire')
     expect(wrapper.html()).toMatchSnapshot()
   })
 })

src/components/VBreadcrumbs/VBreadcrumbs.spec.js
@@ -26,7 +26,7 @@ test('VBreadcrumbs.js', ({ mount }) => {
   //   })
 
   //   const item = wrapper.find(VBreadcrumbsItem)[0]
-  //   expect(item.hasAttribute('data-divider', '/')).toBe(true)
+  //   expect(item.getAttribute('data-divider')).toBe('/')
   //   expect(wrapper.html()).toMatchSnapshot()
   })
 })

src/components/VBtn/VBtn.spec.js
@@ -22,7 +22,7 @@ describe('VBtn.js', () => {
     })
 
     expect(wrapper.is('a')).toBe(true)
-    expect(wrapper.hasAttribute('href', 'http://www.google.com')).toBe(true)
+    expect(wrapper.getAttribute('href')).toBe('http://www.google.com')
     expect(wrapper.html()).toMatchSnapshot()
   })
 

src/components/VBtn/VBtn.vue
@@ -72,8 +72,8 @@
           'secondary': this.secondary && !this.outline && !this.flat,
           'success': this.success && !this.outline && !this.flat,
           'info': this.info && !this.outline && !this.flat,
-          'warning':this.warning && !this.outline && !this.flat,
-          'error':this.error && !this.outline && !this.flat,
+          'warning': this.warning && !this.outline && !this.flat,
+          'error': this.error && !this.outline && !this.flat,
           'primary--text': !this.disabled && this.primary && (this.outline || this.flat),
           'secondary--text': !this.disabled && this.secondary && (this.outline || this.flat),
           'success--text': !this.disabled && this.success && (this.outline || this.flat),

src/components/VBtnToggle/VBtnToggle.spec.js
@@ -184,6 +184,6 @@ test('VBtnToggle.vue', () => {
 
     const btn = wrapper.find('.btn')[0]
 
-    expect(btn.hasAttribute('data-only-child', 'true')).toBe(true)
+    expect(btn.getAttribute('data-only-child')).toBe('true')
   })
 })

src/components/VBtnToggle/VBtnToggle.vue
@@ -1,7 +1,4 @@
 <script>
-  import VBtn from '../VBtn'
-  import VIcon from '../VIcon'
-
   import ButtonGroup from '../../mixins/button-group'
   import Themeable from '../../mixins/themeable'
 

src/components/VCarousel/VCarousel.vue
@@ -58,10 +58,9 @@
         })
 
         this.items.forEach(i => i.open(
-            this.items[this.inputValue]._uid,
-            this.reverse
-          )
-        )
+          this.items[this.inputValue]._uid,
+          this.reverse
+        ))
 
         this.$emit('input', this.inputValue)
         this.restartTimeout()

commit 96cb27df164395be81cf9f127bc7bd2f9c864a42

Fix an issue where the change event would fire multiple times
Caused by #1717

src/components/VTextField/VTextField.vue
@@ -147,7 +147,7 @@
       },
       genInput () {
         const tag = this.multiLine || this.textarea ? 'textarea' : 'input'
-        const listeners = this.$listeners || {}
+        const listeners = Object.assign({}, this.$listeners)
         delete listeners['change'] // Change should not be bound externally
 
         const data = {
@@ -164,7 +164,7 @@
             tabindex: this.tabindex,
             'aria-label': (!this.$attrs || !this.$attrs.id) && this.label // Label `for` will be set if we have an id
           },
-          on: Object.assign({}, listeners, {
+          on: Object.assign(listeners, {
             blur: this.blur,
             input: this.onInput,
             focus: this.focus