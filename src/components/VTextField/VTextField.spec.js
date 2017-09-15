vuetifyjs/vuetify

commit d43bfb4b67007bbf71ae3352b839781be2e11aca

npm packages update, linting, updated tests (#1717)
* Update VTextField.vue

* bumped versions of everything

* avoriaz: hasAttribute => getAttribute

* removed ' is readonly' check - SHOULD IT BE REMOVED?

* lint

* linting

* downgraded postcss-loader, added eslint plugins

src/components/VTextField/VTextField.vue
@@ -164,7 +164,7 @@
             tabindex: this.tabindex,
             'aria-label': (!this.$attrs || !this.$attrs.id) && this.label // Label `for` will be set if we have an id
           },
-          on: Object.assign(listeners, {
+          on: Object.assign({}, listeners, {
             blur: this.blur,
             input: this.onInput,
             focus: this.focus

src/components/VTextField/VTextField.spec.js
@@ -32,8 +32,7 @@ test('VTextField.js', ({ mount }) => {
     })
 
     const inputGroup = wrapper.find('input')[0]
-    expect(inputGroup.hasAttribute('aria-label', 'Test')).toBe(true)
-    expect(`$attrs is readonly`).toHaveBeenWarned()
+    expect(inputGroup.getAttribute('aria-label')).toBe('Test')
   })
 
   it('should not render aria-label attribute on text field element with no label value or id', () => {
@@ -46,7 +45,6 @@ test('VTextField.js', ({ mount }) => {
 
     const inputGroup = wrapper.find('input')[0]
     expect(inputGroup.element.getAttribute('aria-label')).toBeFalsy()
-    expect(`$attrs is readonly`).toHaveBeenWarned()
   })
 
   it('should not render aria-label attribute on text field element with id', () => {
@@ -61,7 +59,6 @@ test('VTextField.js', ({ mount }) => {
 
     const inputGroup = wrapper.find('input')[0]
     expect(inputGroup.element.getAttribute('aria-label')).toBeFalsy()
-    expect(`$attrs is readonly`).toHaveBeenWarned()
   })
 
   it('should start out as invalid', () => {
@@ -123,7 +120,7 @@ test('VTextField.js', ({ mount }) => {
 
     const input = wrapper.find('input')[0]
 
-    expect(input.hasAttribute('readonly', 'readonly')).toBe(true)
+    expect(input.getAttribute('readonly')).toBe('readonly')
     expect(wrapper.html()).toMatchSnapshot()
   })
 

src/components/VApp/VApp.spec.js
@@ -27,7 +27,7 @@ test('VApp.js', ({ mount }) => {
   it('should have data-app attribute', () => {
     const wrapper = mount(VApp)
 
-    expect(wrapper.hasAttribute('data-app', 'true')).toBe(true)
+    expect(wrapper.getAttribute('data-app')).toBe('true')
     expect(wrapper.html()).toMatchSnapshot()
   })
 
@@ -38,7 +38,7 @@ test('VApp.js', ({ mount }) => {
       }
     })
 
-    expect(wrapper.hasAttribute('id', 'inspire')).toBe(true)
+    expect(wrapper.getAttribute('id')).toBe('inspire')
     expect(wrapper.html()).toMatchSnapshot()
   })
 })

src/components/VBreadcrumbs/VBreadcrumbs.spec.js
@@ -26,7 +26,7 @@ test('VBreadcrumbs.js', ({ mount }) => {
   //   })
 
   //   const item = wrapper.find(VBreadcrumbsItem)[0]
-  //   expect(item.hasAttribute('data-divider', '/')).toBe(true)
+  //   expect(item.getAttribute('data-divider')).toBe('/')
   //   expect(wrapper.html()).toMatchSnapshot()
   })
 })

src/components/VBtn/VBtn.spec.js
@@ -22,7 +22,7 @@ describe('VBtn.js', () => {
     })
 
     expect(wrapper.is('a')).toBe(true)
-    expect(wrapper.hasAttribute('href', 'http://www.google.com')).toBe(true)
+    expect(wrapper.getAttribute('href')).toBe('http://www.google.com')
     expect(wrapper.html()).toMatchSnapshot()
   })
 

src/components/VBtn/VBtn.vue
@@ -72,8 +72,8 @@
           'secondary': this.secondary && !this.outline && !this.flat,
           'success': this.success && !this.outline && !this.flat,
           'info': this.info && !this.outline && !this.flat,
-          'warning':this.warning && !this.outline && !this.flat,
-          'error':this.error && !this.outline && !this.flat,
+          'warning': this.warning && !this.outline && !this.flat,
+          'error': this.error && !this.outline && !this.flat,
           'primary--text': !this.disabled && this.primary && (this.outline || this.flat),
           'secondary--text': !this.disabled && this.secondary && (this.outline || this.flat),
           'success--text': !this.disabled && this.success && (this.outline || this.flat),

src/components/VBtnToggle/VBtnToggle.spec.js
@@ -184,6 +184,6 @@ test('VBtnToggle.vue', () => {
 
     const btn = wrapper.find('.btn')[0]
 
-    expect(btn.hasAttribute('data-only-child', 'true')).toBe(true)
+    expect(btn.getAttribute('data-only-child')).toBe('true')
   })
 })

src/components/VBtnToggle/VBtnToggle.vue
@@ -1,7 +1,4 @@
 <script>
-  import VBtn from '../VBtn'
-  import VIcon from '../VIcon'
-
   import ButtonGroup from '../../mixins/button-group'
   import Themeable from '../../mixins/themeable'
 

commit 69f269b2e5d5fc86a24d72f69969481fa6cf1a21

Fix an issue where the VTextField's change event would fire multiple times (#1730)
* Fix an issue where the change event would fire multiple times

Caused by #1717

* Create test case

src/components/VTextField/VTextField.vue
@@ -147,7 +147,7 @@
       },
       genInput () {
         const tag = this.multiLine || this.textarea ? 'textarea' : 'input'
-        const listeners = this.$listeners || {}
+        const listeners = Object.assign({}, this.$listeners)
         delete listeners['change'] // Change should not be bound externally
 
         const data = {
@@ -164,7 +164,7 @@
             tabindex: this.tabindex,
             'aria-label': (!this.$attrs || !this.$attrs.id) && this.label // Label `for` will be set if we have an id
           },
-          on: Object.assign({}, listeners, {
+          on: Object.assign(listeners, {
             blur: this.blur,
             input: this.onInput,
             focus: this.focus

src/components/VTextField/VTextField.spec.js
@@ -203,4 +203,35 @@ test('VTextField.js', ({ mount }) => {
 
     expect(input.element.value).toEqual('fgh')
   })
+
+  it('should fire a single change event on blur', async () => {
+    let value = 'asd'
+    const change = jest.fn()
+
+    const component = {
+      render (h) {
+        return h(VTextField, {
+          on: {
+            input: (i) => value = i,
+            change
+          },
+          props: { value }
+        })
+      }
+    }
+    const wrapper = mount(component)
+
+    const input = wrapper.find('input')[0]
+
+    input.trigger('focus')
+    input.element.value = 'fgh'
+    input.trigger('input')
+
+    await wrapper.vm.$nextTick()
+    input.trigger('blur')
+    await wrapper.vm.$nextTick()
+
+    expect(change).toBeCalledWith('fgh')
+    expect(change.mock.calls.length).toBe(1)
+  })
 })