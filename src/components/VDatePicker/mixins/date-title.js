16f6d0e16840ddd1a913676133087c0e61c282eb
Jacek Karczmarczyk <jkarczm@gmail.com>
2017-09-15 18:39:37 +0200

    month picker (#1435)
    * copied from VDatePicker
    
    * month picker - WIP
    
    * button styling
    
    * button styling
    
    * fixed swipe
    
    * fixed title with long month name
    
    * removed day
    
    * fixed formatting
    
    * removed usused props
    
    * test
    
    * month-title/date-title common code
    
    * month-year/date-year common code
    
    * month-header/date-header common code
    
    * removed duplicated code for VDatePicker and VMonthPicker
    
    * removed duplicated code for VDatePicker and VMonthPicker
    
    * splitted code into shorter methods
    
    * fixed btn styling, code cleanup
    
    * fixed btn:active:hover styling
    
    * minor refactoring to please codebeat
    
    * fixed bug when clicked more times on same month
    
    * fixed disabled dates styling
    
    * common base code for allowedDates for VMonthPicker and VDatePicker
    
    * bug in firstAllowedDate
    
    * snapshot tests for VDatePicker and VMonthPicker
    
    * month picker integrated into datepicker [WIP]
    
    * month picker integrated into datepicker [WIP]
    
    * month picker integrated into datepicker [WIP]
    
    * month picker integrated into datepicker [WIP]
    
    * allowed-dates functionality for month picker
    
    * fixes/tweaks/cleanups
    
    * type prop (not ready for 'year' yet)
    
    * cleanup
    
    * added btn--date-picker class to month button
    
    * added possibility of inheriting themes from app, fixed picker/time-picker/date-picker styles
    
    * removed theme-inheritable
    
    * fixes locale formatting
    
    * fixes #1649
    
    * fixes #1651
    
    * missing button--active class for current && active day
    
    * lint
    
    * vradio tests updated (added 'provide' and snapshots)
    
    * Revert "vradio tests updated (added 'provide' and snapshots)"
    
    This reverts commit 369660fcff74463c26056e42e297861abea8c6d1.

--- src/components/VDatePicker/mixins/date-title.js
+++ src/components/VDatePicker/mixins/date-title.js
@@ -10,72 +10,63 @@ export default {
         }, this.yearIcon)
         : null
     },
-    genTitle () {
-      let date = new Date(this.year, this.month, this.day)
 
-      // Workaround for #1409
-      date.setHours(1)
+    getYearBtn () {
+      const titleDate = new Date(this.year, this.month, this.day, 12)
+      return this.$createElement('div', {
+        'class': {
+          'picker--date__title-year': true,
+          'active': this.activePicker === 'YEAR'
+        },
+        on: {
+          click: e => {
+            e.stopPropagation()
+            this.activePicker = 'YEAR'
+          }
+        }
+      }, [
+        this.supportsLocaleFormat
+          ? titleDate.toLocaleDateString(this.locale, { year: 'numeric' })
+          : this.year,
+        this.genYearIcon()
+      ])
+    },
 
-      if (typeof this.titleDateFormat === 'function') {
-        date = this.titleDateFormat(date)
-      } else if (this.supportsLocaleFormat) {
-        date = date.toLocaleDateString(this.locale, this.titleDateFormat)
-      } else if ('toLocaleDateString' in Date.prototype) {
-        date = date.toLocaleDateString(this.locale)
-      } else {
-        date = date.toISOString().substr(0, 10)
-      }
-
-      if (this.landscape) {
-        if (date.indexOf(',') > -1) date = date.replace(',', ',<br>')
-        else if (date.indexOf(' ') > -1) date = date.replace(' ', '<br>')
-      }
-
-      const text = this.$createElement('transition', {
+    genTitleText (title) {
+      return this.$createElement('transition', {
         props: {
           name: 'slide-x-transition',
           mode: 'out-in'
         }
       }, [
         this.$createElement('div', {
-          domProps: { innerHTML: date },
-          key: date
+          domProps: { innerHTML: title },
+          key: title
         })
       ])
+    },
 
-      const titleDate = new Date(this.year, this.month, this.day, 12)
+    genTitleDate (title) {
+      return this.$createElement('div', {
+        staticClass: 'picker--date__title-date',
+        'class': {
+          'active': this.activePicker === this.type.toUpperCase()
+        },
+        on: {
+          click: e => {
+            e.stopPropagation()
+            this.activePicker = this.type.toUpperCase()
+          }
+        }
+      }, [this.genTitleText(title)])
+    },
+
+    genTitle (title) {
       return this.$createElement('div', {
         'class': 'picker__title'
       }, [
-        this.$createElement('div', {
-          'class': {
-            'picker--date__title-year': true,
-            'active': this.isSelected
-          },
-          on: {
-            click: e => {
-              e.stopPropagation()
-              this.isSelected = true
-            }
-          }
-        }, [
-          this.supportsLocaleFormat
-            ? titleDate.toLocaleDateString(this.locale, { year: 'numeric' })
-            : this.year,
-          this.genYearIcon()
-        ]),
-        this.$createElement('div', {
-          'class': {
-            'picker--date__title-date': true,
-            'active': !this.isSelected
-          },
-          on: {
-            click: e => {
-              e.stopPropagation()
-              this.isSelected = false
-            }
-          }
-        }, [text])
+        this.getYearBtn(),
+        this.genTitleDate(title)
       ])
     }
   }
---
+++ src/components/VDatePicker/mixins/month-table.js
@@ -0,0 +1,84 @@
+export default {
+  methods: {
+    monthWheelScroll (e) {
+      e.preventDefault()
+
+      let year = this.tableYear
+
+      if (e.deltaY < 0) year++
+      else year--
+
+      this.tableDate = new Date(year, 0)
+    },
+    monthClick (month) {
+      const tableYear = this.tableYear
+      const monthStr = month < 9 ? `0${month + 1}` : (month + 1)
+      const day = this.day < 10 ? `0${this.day}` : this.day
+
+      this.inputDate = `${tableYear}-${monthStr}-${day}T12:00:00`
+      if (this.type === 'date') {
+        this.activePicker = 'DATE'
+      } else {
+        this.$nextTick(() => (this.autosave && this.save()))
+      }
+    },
+    monthGenTD (month) {
+      const date = new Date(this.tableYear, month, 1, 12, 0, 0, 0)
+      let monthName
+
+      if (typeof this.monthFormat === 'function') {
+        monthName = this.monthFormat(date)
+      } else if (this.supportsLocaleFormat) {
+        monthName = date.toLocaleDateString(this.locale, this.monthFormat)
+      } else {
+        monthName = date.getMonth() + 1
+        if (monthName < 10) {
+          monthName = `0${monthName}`
+        }
+      }
+
+      return this.$createElement('td', [
+        this.$createElement('button', {
+          'class': {
+            'btn btn--date-picker': true,
+            'btn--raised': this.monthIsActive(month),
+            'btn--flat': !this.monthIsActive(month),
+            'btn--active': this.monthIsActive(month),
+            'btn--outline': this.monthIsCurrent(month) && !this.monthIsActive(month),
+            'btn--disabled': this.type === 'month' && !this.isAllowed(date)
+          },
+          attrs: {
+            type: 'button'
+          },
+          domProps: {
+            innerHTML: `<span class="btn__content">${monthName}</span>`
+          },
+          on: {
+            click: () => this.monthClick(month)
+          }
+        })
+      ])
+    },
+    monthGenTBody () {
+      const children = []
+      const cols = Array(3).fill(null)
+      const rows = 12 / cols.length
+
+      for (let row = 0; row < rows; row++) {
+        children.push(this.$createElement('tr', cols.map((_, col) => {
+          return this.monthGenTD(row * cols.length + col)
+        })))
+      }
+
+      return this.$createElement('tbody', children)
+    },
+    monthIsActive (i) {
+      return this.tableYear === this.year &&
+        this.month === i
+    },
+    monthIsCurrent (i) {
+      return this.currentYear === this.tableYear &&
+        this.currentMonth === i
+    }
+  }
+}
