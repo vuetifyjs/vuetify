7b20967e3627bf515d8b3b75abb19341791def6d
nekosaur <albert@kaaman.nu>
2017-09-16 22:43:59 +0200

    fixed babel path transform

--- build/babel-transform-stylus-paths.js
+++ build/babel-transform-stylus-paths.js
@@ -5,7 +5,11 @@ var wrapListener = require('babel-plugin-detective/wrap-listener');
 module.exports = wrapListener(listener, 'transform-stylus-paths');
 
 function listener(path, file, opts) {
+  const regex = /((?:\.\.\/)+)/gi
   if (path.isLiteral() && path.node.value.endsWith('.styl')) {
-    path.node.value = path.node.value.replace('../../', '../../../src/')
+    const matches = regex.exec(path.node.value)
+    if (!matches) return
+
+    path.node.value = path.node.value.replace(matches[0], `${matches[0]}../src/`)
   }
 }
