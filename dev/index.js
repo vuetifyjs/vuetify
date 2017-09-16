0ed3fefd440d1e733dc99d8f67ce2025e4ba0d64
jk <jacek@karczmarczyk.pl>
2017-09-16 04:51:52 +0200

    Revert "dev/index.js with router"
    This reverts commit 84e833569af674da900b12ed593aa1d03f346070.

--- dev/index.js
+++ dev/index.js
@@ -4,12 +4,4 @@ import App from './App'
 import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
-
-import router from './router'
-
-new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
+new Vue(App).$mount('#app')
