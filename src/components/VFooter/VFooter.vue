549a372a903b3b6bb211dce9e8a16dafa9a909e5
Nicolas <nico@beerfactory.org>
2017-09-17 02:03:22 +0200

    Add themeable mixin to VFooter (#1768)
    * Add themeable to VFooter
    
    * Add themeable mixin

--- src/components/VFooter/VFooter.vue
+++ src/components/VFooter/VFooter.vue
@@ -1,10 +1,11 @@
 <script>
   import Applicationable from '../../mixins/applicationable'
+  import Themeable from '../../mixins/themeable'
 
   export default {
     name: 'v-footer',
 
-    mixins: [Applicationable],
+    mixins: [Applicationable, Themeable],
 
     props: {
       absolute: Boolean,
@@ -49,7 +50,9 @@
         staticClass: 'footer',
         'class': {
           'footer--absolute': this.absolute,
-          'footer--fixed': this.fixed
+          'footer--fixed': this.fixed,
+          'theme--dark': this.dark,
+          'theme--light': this.light
         },
         style: {
           paddingLeft: `${this.paddingLeft}px`,
