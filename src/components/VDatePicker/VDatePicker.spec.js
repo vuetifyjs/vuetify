738fa035d03bebb5357bc65729097aa863408234
Jacek Karczmarczyk <jk@drukcd.pl>
2017-10-03 03:35:08 +0700

    updated snapshots

--- src/components/VDatePicker/VDatePicker.spec.js
+++ src/components/VDatePicker/VDatePicker.spec.js
@@ -51,7 +51,7 @@ test('VDatePicker.js', ({ mount }) => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
-  it.skip('should match snapshot with allowed dates', () => {
+  it('should match snapshot with allowed dates', () => {
     const wrapper = mount(VDatePicker, {
       propsData: {
         value: '2013-05-07',
@@ -62,7 +62,7 @@ test('VDatePicker.js', ({ mount }) => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
-  it.skip('should match snapshot with allowed dates and pick-month prop', () => {
+  it('should match snapshot with allowed dates and pick-month prop', () => {
     const wrapper = mount(VDatePicker, {
       propsData: {
         value: '2013-05',
