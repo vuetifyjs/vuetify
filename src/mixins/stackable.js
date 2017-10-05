4ca8a25c12d9b86c5586e1a9cf60cdcaeba1831d
John Kielkopf <john@webifi.com>
2017-10-05 12:50:29 -0500

    Fix close animation

--- src/mixins/stackable.js
+++ src/mixins/stackable.js
@@ -4,17 +4,18 @@ export function factory (opts = { minZIndex: 5, stackClass: 'unspecified' }) {
   return {
     computed: {
       activeZIndex () {
+        const content = opts.content || this.$refs.content
         if (!this.isActive) {
-          // Return zero if we're not active
-          return 0
+          // Return current zindex if not active
+          return getZIndex(content)
         }
         // Return max current z-index (excluding self) + 2 (2 to leave room for an overlay below, if needed)
-        return this.getMaxZIndex((opts.exclude || (() => [this.$refs.content]))()) + 2
+        return this.getMaxZIndex((opts.exclude || (() => [content]))()) + 2
       }
     },
     methods: {
       getMaxZIndex (exclude = []) {
-        // start with lowest allowed z-index or z-index of parent, whichever is greater
+        // start with lowest allowed z-index or z-index of component, whichever is greater
         const zis = [opts.minZIndex, this.$el ? getZIndex(this.$el) : 0]
         // get z-index for all active dialogs
         const activeElements = document.getElementsByClassName(opts.stackClass)
