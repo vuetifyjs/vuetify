a040f4fe969a24b42f93f41f9b741d461698fa0d
Jacek Karczmarczyk <jk@drukcd.pl>
2017-10-08 11:26:26 +0700

    test

---
+++ src/components/VToolbar/VToolbar.spec.js
@@ -0,0 +1,15 @@
+import { test } from '~util/testing'
+import { mount } from 'avoriaz'
+import VToolbar from '~components/VToolbar'
+
+test('VToolbar.vue', () => {
+  it('should render a colored toolbar', () => {
+    const wrapper = mount(VToolbar, {
+      propsData: {
+        color: 'blue lighten-1'
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
