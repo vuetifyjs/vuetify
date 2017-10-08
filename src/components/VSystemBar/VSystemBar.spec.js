dd21d9a8db58815c13f2dd857e6d91ebe4888018
Jacek Karczmarczyk <jk@drukcd.pl>
2017-10-08 11:34:55 +0700

    test

--- src/components/VSystemBar/VSystemBar.spec.js
+++ src/components/VSystemBar/VSystemBar.spec.js
@@ -3,13 +3,14 @@ import { mount } from 'avoriaz'
 import VSystemBar from '~components/VSystemBar'
 
 test('VSystemBar.vue', () => {
-  it('should render a colored toolbar', () => {
+  it('should render a colored system bar', () => {
     const wrapper = mount(VSystemBar, {
       propsData: {
         color: 'blue lighten-1'
       }
     })
 
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.element.classList).toContain('blue')
+    expect(wrapper.element.classList).toContain('lighten-1')
   })
 })
