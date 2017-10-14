9db604a5f585b5c2b093ec73577274de5e96bc82
John Leider <john.j.leider@gmail.com>
2017-10-13 20:07:28 -0400

    fix(v-select): adjusted selected items functionality
    Fixed a bug where `v-select` with the **combobox** prop was always displaying as dirty

--- src/components/VSelect/VSelect.js
+++ src/components/VSelect/VSelect.js
@@ -482,7 +482,7 @@ export default {
 
       // Combobox is the single version
       // of a taggable select element
-      if (this.combobox) return (this.selectedItems = [val || ''])
+      if (this.combobox) return (this.selectedItems = val ? [val] : [])
 
       let selectedItems = this.computedItems.filter(i => {
         if (!this.isMultiple) {
