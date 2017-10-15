5168d648d62e61265c133353936e4fb0695d9725
John Leider <john.j.leider@gmail.com>
2017-10-14 22:26:05 -0400

    feat(v-dialog): changed defaults for `v-dialog` props
    changed defaults for the **max-width** and **width** prop. removing deprecation of the **width**
    prop.
    
    resolves #2098. breaks old default width for `v-dialog`, '290px'

--- src/components/VDialog/VDialog.js
+++ src/components/VDialog/VDialog.js
@@ -37,13 +37,16 @@ export default {
     fullWidth: Boolean,
     maxWidth: {
       type: [String, Number],
-      default: 290
+      default: 'none'
     },
     origin: {
       type: String,
       default: 'center center'
     },
-    width: [String, Number],
+    width: {
+      type: [String, Number],
+      default: 'auto'
+    },
     scrollable: Boolean,
     transition: {
       type: [String, Boolean],
@@ -133,16 +136,9 @@ export default {
     }
 
     if (!this.fullscreen) {
-      let width = this.maxWidth
-
-      if (this.width) {
-        console.log('The {width} property is being deprecated, please use {max-width}.')
-        // TODO: Deprecate
-        width = this.width
-      }
-
       data.style = {
-        maxWidth: isNaN(width) ? width : `${width}px`
+        maxWidth: isNaN(this.maxWidth) ? this.maxWidth : `${this.maxWidth}px`,
+        width: isNaN(this.width) ? this.width : `${this.width}px`
       }
     }
 
