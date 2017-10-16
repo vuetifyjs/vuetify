c48353172a05bdda6ae66d3c68f72f70dfbce4c8
Jacek Karczmarczyk <jk@drukcd.pl>
2017-10-16 19:43:00 +0700

    fix: don't update inputDate if new date is not allowed
    fixes #2106

--- src/components/VDatePicker/mixins/date-years.js
+++ src/components/VDatePicker/mixins/date-years.js
@@ -12,10 +12,15 @@ export default {
         this.inputDate = `${year}`
         this.$nextTick(() => (this.autosave && this.save()))
       } else if (this.type === 'month') {
-        this.inputDate = this.sanitizeDateString(`${year}-${this.month + 1}`, 'month')
+        const date = this.sanitizeDateString(`${year}-${this.month + 1}`, 'month')
+        if (this.isAllowed(date)) this.inputDate = date
+        this.tableDate = `${year}`
         this.activePicker = 'MONTH'
       } else {
-        this.inputDate = this.sanitizeDateString(`${year}-${this.tableMonth + 1}-${this.day}`, 'date')
+        const date = this.sanitizeDateString(`${year}-${this.tableMonth + 1}-${this.day}`, 'date')
+        if (this.isAllowed(date)) this.inputDate = date
+        this.tableDate = `${year}-${this.tableMonth + 1}`
+        this.inputDate = date
         this.activePicker = 'MONTH'
       }
     },
