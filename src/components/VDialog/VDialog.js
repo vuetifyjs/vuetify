b84cbca797ff2be03383c58782460c4652a61b61
Kael <kaelwd@gmail.com>
2017-10-17 00:31:51 +1100

    fix(VDialog): don't create overlay on initially open fullscreen dialogs
    See https://codepen.io/anon/pen/rGqXoP?editors=1010
    
    (cherry picked from commit 993bcb33ee3533e7d10d829a9c68c4603f27bbc8)

--- src/components/VDialog/VDialog.js
+++ src/components/VDialog/VDialog.js
@@ -73,12 +73,12 @@ export default {
   watch: {
     isActive (val) {
       if (val) {
-        !this.fullscreen && !this.hideOverlay && this.genOverlay()
-        this.fullscreen && this.hideScroll()
-        this.$refs.content.focus()
+        this.init()
       } else {
         if (!this.fullscreen) this.removeOverlay()
         else this.showScroll()
+
+        this.unbind()
       }
     }
   },
@@ -99,7 +99,9 @@ export default {
       return !this.persistent && getZIndex(this.$refs.content) >= this.getMaxZIndex()
     },
     init () {
-      this.isActive && this.genOverlay()
+      !this.fullscreen && !this.hideOverlay && this.genOverlay()
+      this.fullscreen && this.hideScroll()
+      this.$refs.content.focus()
 
       if (this.$listeners.keydown) this.bind()
     },
