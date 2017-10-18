1d5f2f2aaef6aea34d103b74518e3f159de55bf2
Jacek Karczmarczyk <jk@drukcd.pl>
2017-10-18 14:39:06 +0700

    test: remove test settings

--- src/components/Vuetify/mixins/theme.js
+++ src/components/Vuetify/mixins/theme.js
@@ -1,5 +1,5 @@
 const LIGHT_THEME_DEFAULTS = {
-  primary: 'red',
+  primary: '#1976D2',
   secondary: '#424242',
   accent: '#82B1FF',
   error: '#FF5252',
@@ -8,15 +8,7 @@ const LIGHT_THEME_DEFAULTS = {
   warning: '#FFC107'
 }
 
-const DARK_THEME_DEFAULTS = {
-  primary: 'green',
-  secondary: '#424242',
-  accent: '#82B1FF',
-  error: '#FF5252',
-  info: '#2196F3',
-  success: '#4CAF50',
-  warning: '#FFC107'
-}
+const DARK_THEME_DEFAULTS = LIGHT_THEME_DEFAULTS
 
 export default function (opts) {
   const dark = opts.theme && opts.theme.dark
