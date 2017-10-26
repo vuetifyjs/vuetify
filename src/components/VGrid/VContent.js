vuetifyjs/vuetify

commit 3744603d3107c7e1a244f8b9224b723d6e32a87f

fix: remove necessity of <main> (#2298)
* fix: remove the necessity of adding main tag

* fix: backwards compatibility

* Deprecate wrapping v-content in a <main> tag

* Include the offending <main> in the console warning

* Turns out I'm retarded

src/components/VGrid/VContent.js
@@ -18,12 +18,19 @@ export default {
     }
   },
 
+  mounted () {
+    // TODO: Deprecate
+    if (this.$el.parentElement.tagName === 'MAIN') {
+      console.warn('v-content no longer needs to be wrapped in a <main> tag', this.$el.parentElement)
+    }
+  },
+
   render (h) {
     const data = {
       staticClass: 'content',
       style: this.styles
     }
 
-    return h('div', data, this.$slots.default)
+    return h('main', data, this.$slots.default)
   }
 }

src/stylus/components/_app.styl
@@ -7,9 +7,11 @@
   min-height: 100vh
   flex-direction: column
 
-  > main
-    display: flex
+  // TODO: Deprecate
+  > main:not(.content)
     flex: 1 0 auto
+    display: flex
+    flex-direction: column
 
 application($material)
   background: $material.background

src/stylus/components/_content.styl
@@ -1,6 +1,7 @@
 @import '../bootstrap'
 
 .content
-  flex: 1 1 auto
+  flex: 1 0 auto
   max-width: 100%
   transition: .3s padding $transition.swing
+  will-change: padding

src/stylus/elements/_global.styl
@@ -22,10 +22,6 @@ header
   width: 100%
   z-index: 1
 
-main
-  transition: .3s padding $transition.swing
-  will-change: padding-left
-
 ::-ms-clear
 ::-ms-reveal
   display: none

src/stylus/components/_navigation-drawer.styl
@@ -25,7 +25,6 @@ navigation-drawer($material)
 
     &__tile__sub-title
       color: $material.text.secondary
-      
 
   .list--group__header--active
     .list__tile, + .list--group
@@ -59,17 +58,6 @@ theme(navigation-drawer, "navigation-drawer")
     &:after
       left: 0
       right: initial
-
-  // Remove animation when booting up
-  &:not(.navigation-drawer--is-booted)
-    transition: none !important
-    z-index: -1
-
-    + .toolbar, ~ main, ~ .footer
-      transition: none !important
-
-      @media $display-breakpoints.sm-and-down
-        padding-left: 0 !important
 
   &--close:not(.navigation--permanent)
     &.navigation-drawer:not(.navigation-drawer--right)
@@ -114,11 +102,6 @@ theme(navigation-drawer, "navigation-drawer")
 
     &:not(.navigation-drawer--close)
       elevation(16)
-
-  /** Changed nested components */
-  ~ toolbar
-    + main
-      min-height: "calc(100vh - %s)" % $toolbar-height
 
   .list
     background: inherit

commit e64985dece5ce4123d75ff51941165f567c00506

fix(layout): fix layout regression from #2298

src/components/VGrid/VContent.js
@@ -3,6 +3,13 @@ require('../../stylus/components/_content.styl')
 export default {
   name: 'v-content',
 
+  props: {
+    tag: {
+      type: String,
+      default: 'main'
+    }
+  },
+
   computed: {
     styles () {
       const {
@@ -31,6 +38,10 @@ export default {
       style: this.styles
     }
 
-    return h('main', data, this.$slots.default)
+    return h('div', {
+      staticClass: 'content--wrap'
+    }, [
+      h(this.tag, data, this.$slots.default)
+    ])
   }
 }

src/stylus/components/_app.styl
@@ -4,14 +4,12 @@
 .application
   -webkit-backface-visibility: hidden
   display: flex
-  min-height: 100vh
   flex-direction: column
 
   // TODO: Deprecate
   > main:not(.content)
     flex: 1 0 auto
     display: flex
-    flex-direction: column
 
 application($material)
   background: $material.background

src/stylus/components/_content.styl
@@ -1,7 +1,12 @@
 @import '../bootstrap'
 
 .content
-  flex: 1 0 auto
+  flex: 1 1 auto
   max-width: 100%
+  min-height: 100vh
   transition: .3s padding $transition.swing
   will-change: padding
+
+  &--wrap
+    flex: 1 0 auto
+    display: flex

src/stylus/elements/_global.styl
@@ -1,9 +1,3 @@
-html, body
-  height: 100%
-  min-height: 100%
-  position: relative
-  width: 100%
-
 html
   font-size: $font-size-root
   text-rendering: optimizeLegibility