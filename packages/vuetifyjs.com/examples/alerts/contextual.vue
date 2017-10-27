66bb3bdc15f52871f48e1c0c59bba155d6cc728a
John Leider <john.j.leider@gmail.com>
2017-10-27 08:28:24 -0400

    updated alert example to show new prop

--- packages/vuetifyjs.com/examples/alerts/contextual.vue
+++ packages/vuetifyjs.com/examples/alerts/contextual.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <v-alert color="success" icon="check_circle" value="true">
+    <v-alert type="success" value="true">
       This is a success alert.
     </v-alert>
 
-    <v-alert color="info" icon="info" value="true">
+    <v-alert type="info" value="true">
       This is a info alert.
     </v-alert>
 
-    <v-alert color="warning" icon="priority_high" value="true">
+    <v-alert type="warning" value="true">
       This is a warning alert.
     </v-alert>
 
-    <v-alert color="error" icon="warning" value="true">
+    <v-alert type="error" value="true">
       This is a error alert.
     </v-alert>
   </div>
