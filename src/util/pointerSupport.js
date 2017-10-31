61463f159d976ee0e6ada33e1eda8e620b69fc93
jacekkarczmarczyk <jkarczm@gmail.com>
2017-10-31 12:49:29 +0700

    cleanup

--- src/util/pointerSupport.js
+++ src/util/pointerSupport.js
@@ -6,15 +6,10 @@ import { addOnceEventListener } from './helpers'
 export default {
   mounted () {
     const setupPointerSupport = (propName, eventName) => {
-      const load = () => {
-        const app = document.querySelector('[data-app]') || document.body
-        const className = 'application--' + propName.replace(/([A-Z])/g, '-$1').toLowerCase()
-        app.classList.add(className)
-      }
-
       addOnceEventListener(window, eventName, () => {
         this.$vuetify[propName] = true
-        this.$vuetify.load(load)
+        const className = 'application--' + propName.replace(/([A-Z])/g, '-$1').toLowerCase()
+        document.querySelector('[data-app]').classList.add(className)
       })
     }
 
