9aad97a612b7ec04f57c5d6862d5d5976406af7b
Kael <kaelwd@gmail.com>
2017-11-07 14:17:47 +1100

    chore: Add .env file to store dev server config

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.config')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
