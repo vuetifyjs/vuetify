2a60fdd2356f772f53b58c3e694b52de27aac471
John Leider <john.j.leider@gmail.com>
2017-11-16 21:40:44 -0500

    added ssr toc

--- packages/vuetifyjs.com/pages/guides/SSRPage.vue
+++ packages/vuetifyjs.com/pages/guides/SSRPage.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   text-page(:data="$data")
-    section#introduction
-      section-heading(value="Guides.SSR.introductionHeader")
-      section-text(value="Guides.SSR.introductionText1")
-      h3.mb-3 {{ $t('Guides.SSR.introductionSubHeader') }}
+    section#structure
+      section-heading(value="Guides.SSR.structureHeader")
+      section-text(value="Guides.SSR.structureText1")
+      h3.mb-3 {{ $t('Guides.SSR.structureSubHeader') }}
       markup(lang="cli")
         | / project
         |   / assets
@@ -14,7 +14,8 @@
         |   / router
         |   / static
         |   / store
-      section-text(value="Guides.SSR.introductionText2")
+      section-text(value="Guides.SSR.structureText2")
+
     section#application
       section-heading(value="Guides.SSR.applicationHeader")
       section-text(value="Guides.SSR.applicationText1")
@@ -35,6 +36,7 @@
       section-text(value="Guides.SSR.applicationText3")
       h3.mb-3 {{ $t('Guides.SSR.applicationSubHeader2') }}
       section-text(value="Guides.SSR.applicationText4")
+
     section#routing
       section-heading(value="Guides.SSR.routingHeader")
       section-text(value="Guides.SSR.routingText1")
@@ -44,6 +46,7 @@
         |   route('/inspire', 'Inspire')
         |]
       section-text(value="Guides.SSR.routingText2")
+
     section#state-control
       section-heading(value="Guides.SSR.stateControlHeader")
       section-text(value="Guides.SSR.stateControlText1")
@@ -54,12 +57,15 @@
       section-text(value="Guides.SSR.stateControlText2")
       section-text(value="Guides.SSR.stateControlText3")
       section-text(value="Guides.SSR.stateControlText4")
+
     section#meta-data
       section-heading(value="Guides.SSR.metaDataHeader")
       section-text(value="Guides.SSR.metaDataText")
+
     section#redirects
       section-heading(value="Guides.SSR.redirectsHeader")
       section-text(value="Guides.SSR.redirectsText")
+
     section#web-app-support
       section-heading(value="Guides.SSR.webAppSupportHeader")
       section-text(value="Guides.SSR.webAppSupportText")
@@ -70,7 +76,8 @@
     name: 'ssr-page',
     data: () => ({
       header: "Guides.SSR.header",
-      headerText: "Guides.SSR.headerText"
+      headerText: "Guides.SSR.headerText",
+      toc: 'Guides.SSR.toc'
     })
   }
 </script>
