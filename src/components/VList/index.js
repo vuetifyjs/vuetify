b5bdb697455d5e3299ca0de06f4d0ff3daf5dfe3
John Leider <john.j.leider@gmail.com>
2017-11-17 20:53:58 -0500

    fix(v-list-tile-avatar): refactored v-list-tile-avatar
    `v-avatar` and `v-list-tile-avatar` now accept the **colorable** prop. refactored the list tile
    avatar to be a bit more flexible, also adding the ability to change size.
    
    fixes #1805

--- src/components/VList/index.js
+++ src/components/VList/index.js
@@ -4,10 +4,10 @@ import VList from './VList'
 import VListGroup from './VListGroup'
 import VListTile from './VListTile'
 import VListTileAction from './VListTileAction'
+import VListTileAvatar from './VListTileAvatar'
 
 export { VList, VListGroup, VListTile, VListTileAction }
 export const VListTileActionText = createSimpleFunctional('list__tile__action-text', 'span')
-export const VListTileAvatar = createSimpleFunctional('list__tile__avatar', 'v-avatar')
 export const VListTileContent = createSimpleFunctional('list__tile__content', 'div')
 export const VListTileTitle = createSimpleFunctional('list__tile__title', 'div')
 export const VListTileSubTitle = createSimpleFunctional('list__tile__sub-title', 'div')
@@ -18,8 +18,8 @@ VList.install = function install (Vue) {
   Vue.component(VListGroup.name, VListGroup)
   Vue.component(VListTile.name, VListTile)
   Vue.component(VListTileAction.name, VListTileAction)
+  Vue.component(VListTileAvatar.name, VListTileAvatar)
   Vue.component('v-list-tile-action-text', VListTileActionText)
-  Vue.component('v-list-tile-avatar', VListTileAvatar)
   Vue.component('v-list-tile-content', VListTileContent)
   Vue.component('v-list-tile-sub-title', VListTileSubTitle)
   Vue.component('v-list-tile-title', VListTileTitle)
