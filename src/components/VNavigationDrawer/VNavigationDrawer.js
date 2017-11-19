743050378ef09208ff44b5a3125a89908956a0c0
Kael <kaelwd@gmail.com>
2017-11-20 03:55:56 +1100

    fix(drawer): convert isMobile to a computed property (#2569)
    * fix(drawer): convert isMobile to a computed property
    
    Fixes #2559
    
    * fix: stay closed when temporary is toggled while mobile

--- src/components/VNavigationDrawer/VNavigationDrawer.js
+++ src/components/VNavigationDrawer/VNavigationDrawer.js
@@ -22,7 +22,6 @@ export default {
   data () {
     return {
       isActive: false,
-      isMobile: null,
       touchArea: {
         left: 0,
         right: 0
@@ -84,6 +83,11 @@ export default {
         'theme--dark': this.dark,
         'theme--light': this.light
       }
+    },
+    isMobile () {
+      return !this.permanent &&
+        !this.temporary &&
+        this.$vuetify.breakpoint.width < parseInt(this.mobileBreakPoint, 10)
     },
     marginTop () {
       if (!this.app) return 0
@@ -164,7 +168,8 @@ export default {
         this.removeOverlay()
 
       if (prev == null ||
-        this.resizeIsDisabled
+        this.resizeIsDisabled ||
+        !this.reactsToMobile
       ) return
 
       this.isActive = !val
@@ -177,9 +182,6 @@ export default {
       // enable the drawer
       if (val) {
         this.isActive = true
-        this.isMobile = false
-      } else {
-        this.checkIfMobile()
       }
       this.updateApplication()
     },
@@ -204,9 +206,7 @@ export default {
     }
   },
 
-  beforeMount () {
-    this.checkIfMobile()
-
+  created () {
     if (this.permanent) {
       this.isActive = true
     } else if (this.stateless ||
@@ -234,27 +234,12 @@ export default {
         right: parentRect.right - 50
       }
     },
-    checkIfMobile () {
-      if (this.permanent ||
-        this.temporary
-      ) return
-
-      this.isMobile = window.innerWidth < parseInt(this.mobileBreakPoint, 10)
-    },
     closeConditional () {
       return this.reactsToClick
     },
     genDirectives () {
       const directives = [
-        { name: 'click-outside', value: this.closeConditional },
-        {
-          name: 'resize',
-          value: {
-            debounce: 200,
-            quiet: true,
-            value: this.onResize
-          }
-        }
+        { name: 'click-outside', value: this.closeConditional }
       ]
 
       !this.touchless && directives.push({
@@ -267,9 +252,6 @@ export default {
       })
 
       return directives
-    },
-    onResize () {
-      this.checkIfMobile()
     },
     swipeRight (e) {
       if (this.isActive && !this.right) return
