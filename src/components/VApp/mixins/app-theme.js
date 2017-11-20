9cfd48c2c370fb3cb0f640d5a619f06593cbdb42
John Leider <john.j.leider@gmail.com>
2017-11-19 23:18:19 -0500

    revert(theme): removing .application scoped anchor colors

--- src/components/VApp/mixins/app-theme.js
+++ src/components/VApp/mixins/app-theme.js
@@ -44,7 +44,7 @@ export default {
       return colors.join('')
     },
     genAnchorColor (color) {
-      return `.application a{color: ${color};}`
+      return `a{color: ${color};}`
     },
     genBackgroundColor (key, value) {
       return `.${key}{background-color:${value} !important;border-color:${value} !important;}`
