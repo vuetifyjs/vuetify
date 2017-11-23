827cbc760be1ba9a8cd9ffef7748390c70852654
jacekkarczmarczyk <jkarczm@gmail.com>
2017-11-23 22:51:05 +0700

    test: appliationable, bootable, routable

---
+++ src/mixins/applicationable.spec.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import { test } from '~util/testing'
+import { mount } from 'avoriaz'
+import Applicationable from '~mixins/applicationable'
+
+test('applicationable.js', () => {
+  it('should update application on mount', async () => {
+    const updateApplication = jest.fn()
+    const wrapper = mount({
+      mixins: [ Applicationable ],
+      methods: { updateApplication },
+      render: h => h('div')
+    })
+
+    expect(updateApplication.mock.calls.length).toBe(1)
+  })
+
+  it('should update application on app prop change', async () => {
+    const updateApplication = jest.fn()
+    const wrapper = mount({
+      mixins: [ Applicationable ],
+      methods: { updateApplication },
+      render: h => h('div')
+    })
+
+    wrapper.setProps({ app: true })
+    expect(updateApplication.mock.calls.length).toBe(2)
+  })
+})
