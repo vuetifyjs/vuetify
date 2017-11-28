64b982c61cfae6d1d39606690cef23fef4da89b1
jacekkarczmarczyk <jkarczm@gmail.com>
2017-11-29 00:06:50 +0700

    v-date-picker-title

---
+++ src/components/VDatePicker/VDatePickerTitle.js
@@ -0,0 +1,92 @@
+require('../../stylus/components/_date-picker-title.styl')
+
+import VIcon from '../VIcon'
+
+export default {
+  name: 'v-date-picker-title',
+
+  components: {
+    VIcon
+  },
+
+  props: {
+    date: {
+      type: String,
+      default: ''
+    },
+    value: Boolean,
+    year: {
+      type: [Number, String],
+      default: ''
+    },
+    yearIcon: {
+      type: String
+    }
+  },
+
+  methods: {
+    genYearIcon () {
+      return this.$createElement('v-icon', {
+        props: {
+          dark: true
+        }
+      }, this.yearIcon)
+    },
+
+    getYearBtn () {
+      return this.$createElement('div', {
+        staticClass: 'picker__title__btn date-picker-title__year',
+        'class': {
+          'active': this.value
+        },
+        on: {
+          click: e => {
+            e.stopPropagation()
+            this.$emit('input', !this.value)
+          }
+        }
+      }, [
+        this.year,
+        this.yearIcon ? this.genYearIcon() : null
+      ])
+    },
+
+    genTitleText () {
+      return this.$createElement('transition', {
+        props: {
+          name: 'slide-y-reverse-transition',
+          mode: 'out-in'
+        }
+      }, [
+        this.$createElement('div', {
+          domProps: { innerHTML: this.date },
+          key: this.date
+        })
+      ])
+    },
+
+    genTitleDate (title) {
+      return this.$createElement('div', {
+        staticClass: 'picker__title__btn date-picker-title__date',
+        'class': {
+          'active': !this.value
+        },
+        on: this.value ? {
+          click: e => {
+            e.stopPropagation()
+            this.$emit('input', !this.value)
+          }
+        } : {}
+      }, [this.genTitleText(title)])
+    }
+  },
+
+  render (h) {
+    return h('div', {
+      staticClass: 'date-picker-title'
+    }, [
+      this.getYearBtn(),
+      this.genTitleDate()
+    ])
+  }
+}
---
+++ src/components/VDatePicker/VDatePickerTitle.spec.js
@@ -0,0 +1,63 @@
+import { VDatePickerTitle } from '~components/VDatePicker'
+import { test } from '~util/testing'
+import { mount } from 'avoriaz'
+
+test('VDatePickerTitle.js', ({ mount }) => {
+  it('should render component and match snapshot', () => {
+    const wrapper = mount(VDatePickerTitle, {
+      propsData: {
+        year: '1234',
+        date: '2005-11-01'
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render component when selecting year and match snapshot', () => {
+    const wrapper = mount(VDatePickerTitle, {
+      propsData: {
+        year: '1234',
+        date: '2005-11-01',
+        value: true
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render year icon', () => {
+    const wrapper = mount(VDatePickerTitle, {
+      propsData: {
+        year: '1234',
+        yearIcon: 'year',
+        date: '2005-11-01'
+      }
+    })
+
+    expect(wrapper.find('.date-picker-title__year')[0].html()).toMatchSnapshot()
+  })
+
+  it('should emit input event on year/date click', () => {
+    const wrapper = mount(VDatePickerTitle, {
+      propsData: {
+        year: '1234',
+        yearIcon: 'year',
+        date: '2005-11-01'
+      }
+    })
+
+    const input = jest.fn(value => wrapper.setProps({ value }))
+    wrapper.vm.$on('input', input)
+
+    wrapper.find('.date-picker-title__date')[0].trigger('click')
+    expect(input).not.toBeCalled()
+    wrapper.find('.date-picker-title__year')[0].trigger('click')
+    expect(input).toBeCalledWith(true)
+    wrapper.find('.date-picker-title__date')[0].trigger('click')
+    expect(input).toBeCalledWith(false)
+    wrapper.find('.date-picker-title__year')[0].trigger('click')
+    wrapper.find('.date-picker-title__year')[0].trigger('click')
+    expect(input).toBeCalledWith(false)
+  })
+})
