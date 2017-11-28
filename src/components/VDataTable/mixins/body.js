1bc517ea80393df62b1abf742d6dabd689089b07
John Leider <john.j.leider@gmail.com>
2017-11-27 20:23:00 -0500

    chore(v-data-table): removed dev code

--- src/components/VDataTable/mixins/body.js
+++ src/components/VDataTable/mixins/body.js
@@ -31,7 +31,6 @@ export default {
       return this.genTR([transition], { class: 'datatable__expand-row' })
     },
     genFilteredItems () {
-      console.log(this.$scopedSlots.items)
       if (!this.$scopedSlots.items) {
         return null
       }
