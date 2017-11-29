e8bb2a7c1004cf20725da83e288fef0087381848
Jacek Karczmarczyk <jk@drukcd.pl>
2017-11-29 22:54:39 +0700

    fixed extra small grid size

--- packages/vuetifyjs.com/examples/grid-lists/default.vue
+++ packages/vuetifyjs.com/examples/grid-lists/default.vue
@@ -33,7 +33,7 @@
     data: () => ({
       size: 'sm',
       items: [
-        { text: 'Extra small (1px)', value: 'xs' },
+        { text: 'Extra small (2px)', value: 'xs' },
         { text: 'Small (4px)', value: 'sm' },
         { text: 'Medium (8px)', value: 'md' },
         { text: 'Large (16px)', value: 'lg' },
