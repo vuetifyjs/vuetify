3dbcc9d9ce9cc3212568943c4215a93fdfc548f4
John Leider <john.j.leider@gmail.com>
2017-11-28 20:28:12 -0500

    fixed motion page

--- packages/vuetifyjs.com/pages/motion/TransitionsPage.vue
+++ packages/vuetifyjs.com/pages/motion/TransitionsPage.vue
@@ -1,58 +1,44 @@
 <template lang="pug">
-  text-page(v-bind:data="$data" id="transitions-page")
-    section#examples
-      section-head(value="Motion.Transitions.examplesHeader")
-      component-example(
-        :header="`#${i + 1} ${$t(`Examples.Transitions.${example.file}.header`)}`"
-        :new-in="example.new"
-        :file="`transitions/${example.file}`"
-        :id="`example-${i + 1}`"
-        :key="i"
-        v-for="(example, i) in examples"
-      )
-        div(
-          slot="desc"
-          v-html="`${$t(`Examples.Transitions.${example.file}.desc`)}`"
-        )
-
-    section#create-your-own
-      section-head(value="Motion.Transitions.createYourOwnHeader")
-      section-text(value="Motion.Transitions.createYourOwnText1")
-      markup(lang="javascript")
-        |import { createSimpleTransition } from '../../node_modules/vuetify/src/util/helpers'
-        |&nbsp;
-        |const myTransition = createSimpleTransition('my-transition')
-        |&nbsp;
-        |Vue.component('my-transition', myTransition)
-      section-text(value="Motion.Transitions.createYourOwnText2")
-      markup(lang="stylus")
-        |.fade-transition
-        |   &-leave-active
-        |     position: absolute
-        |&nbsp;
-        |   &-enter-active, &-leave, &-leave-to
-        |     transition: $primary-transition
-        |&nbsp;
-        |   &-enter, &-leave-to
-        |     opacity: 0
-
+  component-page(:data="data")
+    template(slot="bottom")
+      section#create-your-own
+        section-head(value="Motion.Transitions.createYourOwnHeader")
+        section-text(value="Motion.Transitions.createYourOwnText1")
+        markup(lang="javascript")
+          |import { createSimpleTransition } from '../../node_modules/vuetify/src/util/helpers'
+          |&nbsp;
+          |const myTransition = createSimpleTransition('my-transition')
+          |&nbsp;
+          |Vue.component('my-transition', myTransition)
+        section-text(value="Motion.Transitions.createYourOwnText2")
+        markup(lang="stylus")
+          |.fade-transition
+          |   &-leave-active
+          |     position: absolute
+          |&nbsp;
+          |   &-enter-active, &-leave, &-leave-to
+          |     transition: $primary-transition
+          |&nbsp;
+          |   &-enter, &-leave-to
+          |     opacity: 0
 </template>
 
 <script>
   export default {
-    name: 'transitions-page',
-
-    data: () => ({
-      header: 'Motion.Transitions.header',
-      headerText: 'Motion.Transitions.headerText',
-      examples: [
-        { file: 'slideXTransitions' },
-        { file: 'slideYTransitions' },
-        { file: 'scaleTransition' },
-        { file: 'fadeTransition' },
-        { file: 'customOrigin' }
-      ],
-      toc: 'Motion.Transitions.toc'
-    })
+    computed: {
+      components () {
+        return this.$t('Motion.Transitions.components')
+      },
+      data () {
+        return {
+          folder: 'transitions',
+          components: this.components,
+          examples: this.examples
+        }
+      },
+      examples () {
+        return this.$t('Motion.Transitions.examples')[0]
+      }
+    }
   }
 </script>
