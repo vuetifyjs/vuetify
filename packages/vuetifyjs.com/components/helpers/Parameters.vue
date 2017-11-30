bd875bc5b39ec902d0c6238d84836c5a03a078dc
John Leider <john.j.leider@gmail.com>
2017-11-30 16:31:40 -0500

    refactored process for parsing props

--- packages/vuetifyjs.com/components/helpers/Parameters.vue
+++ packages/vuetifyjs.com/components/helpers/Parameters.vue
@@ -1,118 +1,71 @@
 <template lang="pug">
-  v-card.component-parameters
-    v-card-title
-      v-select(
-        label="Component"
-        hide-details
-        single-line
-        v-bind:items="components"
-        v-model="currentComponent"
-        auto
-        :disabled="components.length < 2"
+  v-data-table(
+    :headers="headers"
+    :search="search"
+    :items="computedItems"
+    :pagination.sync="pagination"
+  ).component-parameters
+    template(slot="items" slot-scope="{ item }")
+      td(
+        v-for="(opt, i) in item"
+        :key="i"
+        v-html="opt"
       )
-      v-spacer
-      v-spacer.hidden-sm-and-down
-      v-text-field(
-        append-icon="search"
-        label="Search..."
-        single-line
-        hide-details
-        v-model="search"
-      )
-    v-data-table(
-      v-bind:headers="headers"
-      v-bind:search="search"
-      v-bind:items="parsedItems"
-      v-bind:pagination.sync="pagination"
-    )
-      template(slot="items" slot-scope="{ item }")
-        td(
-          v-for="(opt, i) in item"
-          v-bind:key="i"
-          v-html="opt"
-        )
 </template>
 
 <script>
-  import { capitalize, uppercase } from '@/util/helpers'
+  import { capitalize } from '@/util/helpers'
 
   export default {
-    data () {
-      return {
-        currentComponent: this.components[0],
-        pagination: {
-          rowsPerPage: 10
-        },
-        search: '',
-        shared: {}
+    data: () => ({
+      pagination: {
+        rowsPerPage: 10
       }
-    },
+    }),
 
     props: {
-      api: {
-        type: Object,
-        default: () => ({})
-      },
-      components: {
-        type: Array,
-        default: () => ([])
-      },
-      id: String,
+      target: String,
       headers: {
         type: Array,
         default: () => ([])
       },
+      items: {
+        type: Array,
+        default: () => ([])
+      },
       namespace: String,
+      search: String,
       type: String
     },
 
     computed: {
-      extra () {
-        const component = capitalize(this.$route.params.component)
-        const section = capitalize(this.$route.params.section)
-        const props = `${section}.${component}.extra`
+      computedItems () {
+        return this.items.map(item => {
+          const newItem = {}
 
-        return this.$te(props)
-          ? this.$t(props)[0]
-          : {}
-      },
-      items () {
-        const component = this.api[this.currentComponent] || {
-          props: [],
-          slots: [],
-          scopedSlots: []
-        }
+          Object.keys(item).map(key => {
+            const fn = this[`gen${capitalize(key)}`]
 
-        let items = component[this.type] || []
+            if (fn) {
+              newItem[key] = fn(item[key])
+            } else {
+              newItem[key] = item[key]
+            }
+          })
 
-        if (this.type === 'slots' && component.scopedSlots) {
-          items = items.concat(component.scopedSlots)
-        }
+          newItem.desc = this.genDescription(item.name)
 
-        const extra = this.extra[this.currentComponent]
-        if (extra && extra[this.type]) {
-          items = items.concat(extra[this.type])
-        }
-
-        return items
-      },
-      parsedItems () {
-        return this[`gen${this.type}`]()
+          return newItem
+        })
       }
     },
 
     methods: {
-      genDefault () {
-        return Object.assign({}, {
-          props: [],
-          slots: []
-        })
-      },
-      genDescription (item) {
+      genDescription (name) {
         let description = ''
-        const specialLevelDesc = `Components.${this.namespace}.special.${this.type}.${this.currentComponent}.${item.name}`
-        const componentLevelDesc = `Components.${this.namespace}.${this.type}.${item.name}`
-        const genericDesc = `Generic.${this.uppercase(this.type)}.${item.name}`
+        const specialLevelDesc = `Components.${this.namespace}.special.${this.type}.${this.target}.${name}`
+        const componentLevelDesc = `Components.${this.namespace}.${this.type}.${name}`
+        const genericDesc = `Generic.${capitalize(this.type)}.${name}`
 
         if (this.$te(specialLevelDesc)) {
           description = this.$t(specialLevelDesc)
@@ -123,26 +76,6 @@
         }
 
         return description
-      },
-      genprops () {
-        return this.items.map(item => {
-          return {
-            name: this.genName(item.name),
-            type: this.genType(item.type),
-            default: item.default,
-            description: this.genDescription(item)
-          }
-        })
-      },
-      genslots () {
-        return this.items.map(item => {
-          return {
-            name: this.genName((item || '').slice()),
-            description: this.genDescription({
-              name: item
-            })
-          }
-        })
       },
       genName (name) {
         name = name || ''
@@ -156,11 +89,6 @@
         return type.map(t => {
           return this.$t(`Generic.Types.${t}`)
         }).join(', ')
-      },
-      uppercase (str) {
-        str = str || ''
-
-        return str.substr(0, 1).toUpperCase() + str.slice(1)
       }
     }
   }
@@ -168,7 +96,6 @@
 
 <style lang="stylus">
   .component-parameters
-    box-shadow: none
     code
       white-space: nowrap
 </style>
