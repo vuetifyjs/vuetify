b5093f54d1358e66b6f6f9aa6327fcb1b98382c7
nekifirus <nekifirus@gmail.com>
2017-11-30 15:54:07 +0500

    Update isValueEvent.spec.js

--- src/util/isValueEvent.spec.js
+++ src/util/isValueEvent.spec.js
@@ -6,15 +6,4 @@ test('VTimePicker/util/isValueEvent.js', ({ mount }) => {
     expect(isValueEvent(3, null)).toBe(false)
     expect(isValueEvent('3', null)).toBe(false)
   })
-
-  it('should check if the date is event when events values is array', () => {
-    expect(isValueEvent(3, [-1, 1, 3, 5])).toBe(true)
-    expect(isValueEvent(-1, ['-1', 1, 3, 5])).toBe(false)
-  })
-
-  it('should check if the date is event when events values is function', () => {
-    const fn = value => value.toString()[0] === '1'
-    expect(isValueEvent(13, fn)).toBe(true)
-    expect(isValueEvent(23, fn)).toBe(false)
-  })
 })
