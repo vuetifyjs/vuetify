642145f6cf86dead74cb085e905b4b567d4283a0
John Leider <john.j.leider@gmail.com>
2017-12-01 09:25:59 -0500

    added display page

---
+++ packages/vuetifyjs.com/i18n/en-US/layout/Display.js
@@ -0,0 +1,47 @@
+export default {
+  header: 'Display',
+  headerText: 'The display helpers allow you to control the display of content. This includes being conditionally visible based upon the current viewport, or the actual element display type.',
+  toc: [
+    {
+      text: 'Introduction',
+      href: 'introduction'
+    },
+    {
+      text: 'Visibility',
+      href: 'visibility'
+    },
+    {
+      text: 'Display',
+      href: 'display'
+    },
+    {
+      text: 'Examples',
+      href: 'examples'
+    }
+  ],
+  visibilityHeader: 'Visibility',
+  visibilityText: 'Conditionally display an element based upon the current **viewport**. These classes can be applied using the following format `hidden-{breakpoint}-{condition}`',
+  breakpointText: 'The _breakpoint_ sets the viewport size:',
+  breakpoints: [
+    '`xs` - extra small devices',
+    '`sm` - small devices',
+    '`md` - medium devices',
+    '`lg` - large devices',
+    '`xl` - extra large',
+  ],
+  conditionText: 'The _condition_ applies the class base on:',
+  conditions: [
+    '`only` - hide the element only on `xs` through `xl` breakpoints',
+    '`and-down` - hide the element on the specified breakpoint and down `sm` through `lg` breakpoints',
+    '`and-up` - hide the element on the specified breakpoint and up `sm` through `lg` breakpoints'
+  ],
+  displayHeader: 'Display',
+  displayText: 'Specify the elements `display` property. These classes can be applied using the following format `d-{display}`.',
+  displays: [
+    '`d-inline-flex` - sets the element display property to `inline-flex`',
+    '`d-flex` - sets the element display property to `flex`',
+    '`d-inline-block` - sets the element display property to `inline-block`',
+    '`d-block` - sets the element display property to `block`',
+    '`d-inline` - sets the element display property to `inline`'
+  ]
+}
---
+++ packages/vuetifyjs.com/pages/layout/DisplayPage.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  doc-view
+    template(slot-scope="{ namespace }")
+      section#grid
+        grid
+
+      section#visibility
+        section-head(:value="`${namespace}.visibilityHeader`")
+        section-text(:value="`${namespace}.visibilityText`")
+        section-text(:value="`${namespace}.breakpointText`")
+        ul.browser-list.pb-2
+          li(
+            v-for="item in breakpoints"
+            :key="item"
+          )
+            markdown(:source="item")
+        section-text(:value="`${namespace}.conditionText`")
+        ul.browser-list.pb-2
+          li(
+            v-for="item in conditions"
+            :key="item"
+          )
+            markdown(:source="item")
+
+      section#display
+        section-head(:value="`${namespace}.displayHeader`")
+        section-text(:value="`${namespace}.displayText`")
+        ul.browser-list.pb-2
+          li(
+            v-for="item in displays"
+            :key="item"
+          )
+            markdown(:source="item")
+
+      section#examples
+        section-head(value="Generic.Pages.examples")
+        example(
+          readonly
+          file="display/hiddenElements"
+        )
+</template>
+
+<script>
+  export default {
+    computed: {
+      breakpoints () {
+        return this.$t('Layout.Display.breakpoints')
+      },
+      conditions () {
+        return this.$t('Layout.Display.conditions')
+      },
+      displays () {
+        return this.$t('Layout.Display.displays')
+      }
+    }
+  }
+</script>
