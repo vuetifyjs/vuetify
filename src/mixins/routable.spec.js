752d82d1d0be2d28ae42f37b3571f0be5ea8fe1d
jacekkarczmarczyk <jkarczm@gmail.com>
2017-12-01 21:55:47 +0700

    fixed test name

--- src/mixins/routable.spec.js
+++ src/mixins/routable.spec.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { test } from '~util/testing'
 import Routable from '~mixins/routable'
 
-test('applicationable.js', ({ mount }) => {
+test('routable.js', ({ mount }) => {
   it('should generate exact route link with to="/" and undefined exact', async () => {
     const wrapper = mount({
       mixins: [ Routable ],
