f74711451819c28de7d6f517e54cc8c7805069b0
John Leider <john.j.leider@gmail.com>
2017-12-01 20:38:52 -0500

    added missing component

--- packages/vuetifyjs.com/i18n/en-US/components/Tabs.js
+++ packages/vuetifyjs.com/i18n/en-US/components/Tabs.js
@@ -2,6 +2,7 @@ export default {
   header: 'Tab',
   headerText: 'The <code>v-tabs</code> component is used for hiding content behind a selectable item. This can also be used as a pseudo-navigation for a page, where the tabs are links and the tab-items are the content.',
   components: [
+    'v-tabs',
     'v-tabs-bar',
     'v-tabs-slider',
     'v-tabs-item',
