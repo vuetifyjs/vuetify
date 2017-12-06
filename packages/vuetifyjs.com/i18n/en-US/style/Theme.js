428196d3cfe56eead16f5c0bf9a5a5285912b5cf
Jacek Karczmarczyk <jk@drukcd.pl>
2017-12-06 23:30:20 +0700

    src/util/colors example
    fixes #88

--- packages/vuetifyjs.com/i18n/en-US/style/Theme.js
+++ packages/vuetifyjs.com/i18n/en-US/style/Theme.js
@@ -7,8 +7,9 @@ export default {
   customizingHeader: 'Customizing',
   customizingText1: 'By default, Vuetify has a standard theme applied for all components.',
   customizingText2: 'This can be easily changed. Simply pass a **theme** property to the <code>Vue.use</code> function. You can choose to modify all or only some of the theme properties, with the remaining inheriting from the default.',
-  customizingText3: 'Under the hood, Vuetify will generate css classes based upon these values that will be accessible in the DOM. These classes will follow the same markup as other helper classes, <code>primary</code> or <code>secondary--text</code> for example.',
-  customizingText4: 'These values will also be made available on the instance **$vuetify** object under the **theme** property. This allows you to _dynamically_ modify your theme. Behind the scenes, Vuetify will regenerate and update your theme classes, seemlessly updating your application.',
+  customizingText3: 'You can also use the pre-defined material colors.',
+  customizingText4: 'Under the hood, Vuetify will generate css classes based upon these values that will be accessible in the DOM. These classes will follow the same markup as other helper classes, <code>primary</code> or <code>secondary--text</code> for example.',
+  customizingText5: 'These values will also be made available on the instance **$vuetify** object under the **theme** property. This allows you to _dynamically_ modify your theme. Behind the scenes, Vuetify will regenerate and update your theme classes, seemlessly updating your application.',
   toc: [
     {
       text: 'Theme',
