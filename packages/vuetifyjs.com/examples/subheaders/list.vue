b4a269da692e355992522cb606b5850b36a14a42
Jacek Karczmarczyk <jk@drukcd.pl>
2017-12-06 23:40:41 +0700

    fixed subheaders colors
    fixes #77

--- packages/vuetifyjs.com/examples/subheaders/list.vue
+++ packages/vuetifyjs.com/examples/subheaders/list.vue
@@ -38,31 +38,31 @@
           {
             avatar: '/static/doc-images/lists/1.jpg',
             title: 'Brunch this weekend?',
-            subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
           },
           { divider: true, inset: true },
           {
             avatar: '/static/doc-images/lists/2.jpg',
             title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-            subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
           },
           { divider: true, inset: true },
           {
             avatar: '/static/doc-images/lists/3.jpg',
             title: 'Oui oui',
-            subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
           },
           { divider: true, inset: true },
           {
             avatar: '/static/doc-images/lists/4.jpg',
             title: 'Birthday gift',
-            subtitle: "<span class='grey--text text--darken-2'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
           },
           { divider: true, inset: true },
           {
             avatar: '/static/doc-images/lists/5.jpg',
             title: 'Recipe to try',
-            subtitle: "<span class='grey--text text--darken-2'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
           },
         ]
       }
