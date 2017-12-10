e4c1166e092abc59e835f6bfd0b4a3b925613df5
Kael <kaelwd@gmail.com>
2017-12-10 18:49:16 +1100

    fix(Vicon): merge custom inline styles
    Fixes #2764

--- src/components/VIcon/VIcon.js
+++ src/components/VIcon/VIcon.js
@@ -44,7 +44,7 @@ export default {
       case props.xLarge: fontSize = SIZE_MAP.xLarge
         break
     }
-    data.style = { fontSize }
+    data.style = { fontSize, ...data.style }
 
     let iconName = ''
     if (children.length) {
