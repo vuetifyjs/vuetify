ffee1c90d675fe5e164bc185e910d675b8eadcc6
John Leider <john@vuetifyjs.com>
2017-12-23 13:48:16 -0500

    Merge pull request #130 from Larkenx/bug/grid-offset-example
    fixing grid offset example not displaying as expected with the given labels

--- packages/vuetifyjs.com/examples/grid/offset.vue
+++ packages/vuetifyjs.com/examples/grid/offset.vue
@@ -6,17 +6,17 @@
           <v-card-text>xs10 offset-xs1</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs7 offset-xs0 offset-md2 offset-lg5>
+      <v-flex xs7 offset-xs5>
         <v-card dark color="secondary">
           <v-card-text>xs7 offset-xs5</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm5 md2>
+      <v-flex xs5>
         <v-card dark color="primary">
           <v-card-text>xs5</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm5 md3 offset-xs0 offset-lg2>
+      <v-flex xs5 offset-xs2>
         <v-card dark color="green">
           <v-card-text>xs5 offset-xs2</v-card-text>
         </v-card>
