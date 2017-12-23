e29634ebfd0255409bf45bd3ed213c8666a76188
John Leider <john.j.leider@gmail.com>
2017-12-23 15:25:39 -0500

    fixes 53

--- packages/vuetifyjs.com/examples/selection-controls/checkboxesBoolean.vue
+++ packages/vuetifyjs.com/examples/selection-controls/checkboxesBoolean.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
-    <v-checkbox v-bind:label="`Checkbox 1: ${ex1.toString()}`" v-model="ex1" light></v-checkbox>
-    <v-checkbox v-bind:label="`Checkbox 2: ${ex2.toString()}`" v-model="ex2" light></v-checkbox>
+    <v-checkbox v-bind:label="`Checkbox 1: ${ex1.toString()}`" v-model="ex1"></v-checkbox>
+    <v-checkbox v-bind:label="`Checkbox 2: ${ex2.toString()}`" v-model="ex2"></v-checkbox>
   </v-container>
 </template>
 
