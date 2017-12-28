d31f5f389d2fb1902d571165c19587ef4be7630e
Kael <kaelwd@gmail.com>
2017-12-28 14:27:08 +1100

    simplify inline checkbox example

--- packages/vuetifyjs.com/examples/selection-controls/checkboxesInlineTexfield.vue
+++ packages/vuetifyjs.com/examples/selection-controls/checkboxesInlineTexfield.vue
@@ -1,44 +1,23 @@
 <template>
   <v-card>
     <v-card-text>
-      <v-container fluid grid-list-md>
-        <v-layout align-center>
-          <v-flex class="shrink">
-            <v-checkbox
-              v-model="ex5"
-              hide-details
-            ></v-checkbox>
-          </v-flex>
-          <v-flex>
-            <v-text-field label="Include files"></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout align-center>
-          <v-flex class="shrink">
-            <v-checkbox
-              v-model="ex6"
-              hide-details
-            ></v-checkbox>
-          </v-flex>
-          <v-flex>
-            <v-text-field
-              label="I only work if you check the box"
-              :disabled="!ex6"
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
-      </v-container>
+      <v-layout align-center>
+        <v-checkbox v-model="includeFiles" hide-details class="shrink mr-2"></v-checkbox>
+        <v-text-field label="Include files"></v-text-field>
+      </v-layout>
+      <v-layout align-center>
+        <v-checkbox v-model="enabled" hide-details class="shrink mr-2"></v-checkbox>
+        <v-text-field label="I only work if you check the box" :disabled="!enabled"></v-text-field>
+      </v-layout>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        ex5: true,
-        ex6: false
-      }
-    }
+    data: () => ({
+      includeFiles: true,
+      enabled: false
+    })
   }
 </script>
