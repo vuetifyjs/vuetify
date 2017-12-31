99efcccfacc4ef2f8201bdb38a422d0b869bf4ee
Kael <kaelwd@gmail.com>
2018-01-01 03:41:16 +1100

    fix(VNavigationDrawer): only close on route change when mobile/temporary
    fixes #2894

--- src/components/VNavigationDrawer/VNavigationDrawer.js
+++ src/components/VNavigationDrawer/VNavigationDrawer.js
@@ -148,8 +148,7 @@ export default {
     },
     reactsToRoute () {
       return !this.disableRouteWatcher &&
-        !this.stateless &&
-        !this.permanent
+        (this.temporary || this.isMobile)
     },
     resizeIsDisabled () {
       return this.disableResizeWatcher || this.stateless
