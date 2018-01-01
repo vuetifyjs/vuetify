c0732773e3d6203db143f7ef727e3101ac73ff09
Jacek Karczmarczyk <jk@drukcd.pl>
2018-01-02 00:00:03 +0700

    added append/prepend icon info, fixed 'right' prop location

--- packages/vuetifyjs.com/i18n/en-US/generic/Props.js
+++ packages/vuetifyjs.com/i18n/en-US/generic/Props.js
@@ -18,7 +18,6 @@ export default {
   overlap: 'Component will overlap the slotted content',
   persistent: 'Clicking outside will not dismiss the dialog',
   returnMaskedValue: 'Returns the unmodified masked string',
-  right: 'Aligns tabs to the right',
   ripple: 'Applies the `v-ripple` directive',
   shift: 'Hide text of button when not active',
   singleLine: 'Label does not move on focus/dirty',
