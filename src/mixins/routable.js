ec9fcae39d982eddb0b51f866684eb85682c8ba8
Jacek Karczmarczyk <jkarczm@gmail.com>
2018-01-01 21:29:39 +0700

    fix: do not apply ripple on disabled routable (#2899)

--- src/mixins/routable.js
+++ src/mixins/routable.js
@@ -34,7 +34,7 @@ export default {
         props: {},
         directives: [{
           name: 'ripple',
-          value: this.ripple || false
+          value: (this.ripple && !this.disabled) || false
         }],
         on: {
           ...(this.$listeners || {}),
