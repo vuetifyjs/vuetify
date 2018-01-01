d9f5fd578320c307f270404d5ac6b3b25dca39a4
Kael <kaelwd@gmail.com>
2018-01-02 07:20:23 +1100

    fix a bunch of import paths and some formatting

--- packages/vuetifyjs.com/pages/motion/TransitionsPage.vue
+++ packages/vuetifyjs.com/pages/motion/TransitionsPage.vue
@@ -4,20 +4,20 @@
       section-head(value="Motion.Transitions.createYourOwnHeader")
       section-text(value="Motion.Transitions.createYourOwnText1")
       markup(lang="javascript")
-        |import { createSimpleTransition } from '../../node_modules/vuetify/src/util/helpers'
-        |&nbsp;
-        |const myTransition = createSimpleTransition('my-transition')
-        |&nbsp;
-        |Vue.component('my-transition', myTransition)
+        | import { createSimpleTransition } from 'vuetify/es5/util/helpers'
+        | &nbsp;
+        | const myTransition = createSimpleTransition('my-transition')
+        | &nbsp;
+        | Vue.component('my-transition', myTransition)
       section-text(value="Motion.Transitions.createYourOwnText2")
       markup(lang="stylus")
-        |.fade-transition
+        | .fade-transition
         |   &-leave-active
         |     position: absolute
-        |&nbsp;
+        | &nbsp;
         |   &-enter-active, &-leave, &-leave-to
         |     transition: $primary-transition
-        |&nbsp;
+        | &nbsp;
         |   &-enter, &-leave-to
         |     opacity: 0
 </template>
--- packages/vuetifyjs.com/pages/style/ThemePage.vue
+++ packages/vuetifyjs.com/pages/style/ThemePage.vue
@@ -6,7 +6,7 @@
         section-text(:value="`${namespace}.lightAndDarkText1`")
         markup(lang="html")
           |&lt;v-app dark&gt;
-          |...
+          | ...
           |&lt;/v-app&gt;
         section-text(:value="`${namespace}.lightAndDarkText2`")
 
@@ -14,7 +14,7 @@
         section-head(:value="`${namespace}.customizingHeader`")
         section-text(:value="`${namespace}.customizingText1`")
         markup(lang="javascript")
-          |{
+          | {
           |   primary: '#1976D2',
           |   secondary: '#424242',
           |   accent: '#82B1FF',
@@ -22,33 +22,33 @@
           |   info: '#2196F3',
           |   success: '#4CAF50',
           |   warning: '#FFC107'
-          |}
+          | }
         section-text(:value="`${namespace}.customizingText2`")
         markup(lang="javascript")
-          |Vue.use(Vuetify, {
+          | Vue.use(Vuetify, {
           |   theme: {
           |     primary: '#3f51b5',
           |     secondary: '#b0bec5',
           |     accent: '#8c9eff',
           |     error: '#b71c1c'
           |   }
-          |})
+          | })
         section-text(:value="`${namespace}.customizingText3`")
         markup(lang="javascript")
-          |import colors from 'vuetify/es5/util/colors'
+          | import colors from 'vuetify/es5/util/colors'
           |
-          |Vue.use(Vuetify, {
+          | Vue.use(Vuetify, {
           |   theme: {
           |     primary: colors.purple.base,
           |     secondary: colors.gray.darken1,
           |     accent: colors.shades.black,
           |     error: colors.red.accent3
           |   }
-          |})
+          | })
         section-text(:value="`${namespace}.customizingText4`")
         section-text(:value="`${namespace}.customizingText5`")
         markup(lang="javascript")
-          |this.$vuetify.theme.primary = '#4caf50'
+          | this.$vuetify.theme.primary = '#4caf50'
 
       section#stylus-guide
         section-head(:value="`${namespace}.stylusHeader`")
@@ -56,35 +56,35 @@
         h3.mb-3 {{ $t(`${namespace}.stylusHeader2`) }}
         section-text(:value="`${namespace}.stylusText2`")
         markup(lang="cli")
-          |$ npm i stylus stylus-loader style-loader css-loader --save-dev
+          | $ npm i stylus stylus-loader style-loader css-loader --save-dev
           |// or
-          |$ yarn add stylus stylus-loader style-loader css-loader --dev
+          | $ yarn add -D stylus stylus-loader style-loader css-loader
         section-text(:value="`${namespace}.stylusText3`")
         markup(lang="javascript")
-          |module: {
-          |  rules: [
-          |    {
-          |      test: /\.styl$/,
-          |      loader: ['style-loader', 'css-loader', 'stylus-loader']
-          |    }
-          |  ]
-          |}
+          | module: {
+          |   rules: [
+          |     {
+          |       test: /\.styl$/,
+          |       loader: ['style-loader', 'css-loader', 'stylus-loader']
+          |     }
+          |   ]
+          | }
         section-text(:value="`${namespace}.stylusText4`")
         markup(lang="stylus")
-          |// main.styl
-          |@require '../../node_modules/vuetify/src/stylus/main'
+          | // main.styl
+          | @import '~vuetify/src/stylus/main'
         section-text(:value="`${namespace}.stylusText5`")
         markup(lang="javascript")
-          |// app.js
-          |import('./stylus/main.styl')
+          | // app.js
+          | import './stylus/main.styl'
         section-text(:value="`${namespace}.stylusText6`")
         h3.mb-3 {{ $t(`${namespace}.stylusHeader3`) }}
         section-text(:value="`${namespace}.stylusText7`")
         markup(lang="stylus")
-          |$body-font-family = 'Open Sans'
-          |$alert-font-size = 18px
-          |&nbsp;
-          |@import '../../node_modules/vuetify/src/stylus/main'
-          |// For a-la-carte
-          |@import '../../node_modules/vuetify/src/stylus/app'
+          | $body-font-family = 'Open Sans'
+          | $alert-font-size = 18px
+          | &nbsp;
+          | @import '~vuetify/src/stylus/main'
+          | // For a-la-carte
+          | @import '~vuetify/src/stylus/app'
 </template>
