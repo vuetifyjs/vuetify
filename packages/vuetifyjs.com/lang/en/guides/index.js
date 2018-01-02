798efe6369b354c033f28cc3df80cc14589057de
Kael <kaelwd@gmail.com>
2018-01-02 19:26:03 +1100

    remove duplicated language files

--- packages/vuetifyjs.com/lang/en/guides/index.js
+++
@@ -1,6 +0,0 @@
-import ServerSideRendering from './ServerSideRendering'
-import ALaCarte from './ALaCarte'
-
-export default {
-  ServerSideRendering, ALaCarte
-}
--- packages/vuetifyjs.com/lang/en/vuetify/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home'
-import PremadeThemes from './PremadeThemes'
-
-export default {
-  Home,
-  PremadeThemes
-}
