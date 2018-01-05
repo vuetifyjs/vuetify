6b7567793a21636a2de11aedf758d360d5a0ae1d
John Leider <john.j.leider@gmail.com>
2018-01-05 18:44:34 -0500

    updates from doc release

--- packages/vuetifyjs.com/store/objects/app-drawer-items.js
+++ packages/vuetifyjs.com/store/objects/app-drawer-items.js
@@ -117,7 +117,8 @@ export default [
     ]
   },
   { href: '/pre-made-themes', title: 'Pre-made themes', icon: 'mdi-theme-light-dark' },
-  { href: '/store', title: 'Store', icon: 'mdi-store', badge: 'coming soon' },
+  // { href: '/store', title: 'Store', icon: 'mdi-store', badge: 'coming soon' },
+  { href: 'https://vuetify.threadless.com/', title: 'Store', icon: 'mdi-store', target: '_blank' },
   { divider: true },
   { header: 'Additional resources' },
   {
