c1bd31b5fcca7342317cce372cabc4c3f87285d7
John Leider <john@vuetifyjs.com>
2018-01-04 20:23:48 -0500

    Merge pull request #168 from vuetifyjs/enhancement/grid
    enhancement/grid

--- packages/vuetifyjs.com/examples/grid/grid.vue
+++ packages/vuetifyjs.com/examples/grid/grid.vue
@@ -6,27 +6,27 @@
           <v-card-text class="px-0">12</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs6 v-for="i in 2" :key="i">
+      <v-flex xs6 v-for="i in 2" :key="`6${i}`">
         <v-card dark color="secondary">
           <v-card-text class="px-0">6</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs4 v-for="i in 3" :key="i">
+      <v-flex xs4 v-for="i in 3" :key="`4${i}`">
         <v-card dark color="primary">
           <v-card-text class="px-0">4</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs3 v-for="i in 4" :key="i">
+      <v-flex xs3 v-for="i in 4" :key="`3${i}`">
         <v-card dark color="secondary">
           <v-card-text class="px-0">3</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs2 v-for="i in 6" :key="i">
+      <v-flex xs2 v-for="i in 6" :key="`2${i}`">
         <v-card dark color="primary">
           <v-card-text class="px-0">2</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs1 v-for="i in 12" :key="i">
+      <v-flex xs1 v-for="i in 12" :key="`1${i}`">
         <v-card dark color="secondary">
           <v-card-text class="px-0">1</v-card-text>
         </v-card>
