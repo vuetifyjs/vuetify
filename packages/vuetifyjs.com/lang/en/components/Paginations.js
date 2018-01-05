45baf36f3b92ddd8a45bdbad160e0b3366b577e4
John Leider <john@vuetifyjs.com>
2018-01-04 20:24:44 -0500

    Merge pull request #164 from bad4iz/feature/fix_en-EN__attribute_allocated
    fix en-EN: `attribute`

--- packages/vuetifyjs.com/lang/en/components/Paginations.js
+++ packages/vuetifyjs.com/lang/en/components/Paginations.js
@@ -1,6 +1,6 @@
 export default {
   header: 'Pagination',
-  headerText: 'The `v-pagination` component is used to separate long sets of data so that it is easier for a user to consume information. Depending on the length provided, the pagination component will automatically scale. To maintain the current page, simply supply a v-model attribute.',
+  headerText: 'The `v-pagination` component is used to separate long sets of data so that it is easier for a user to consume information. Depending on the length provided, the pagination component will automatically scale. To maintain the current page, simply supply a `v-model` attribute.',
   components: ['v-pagination'],
   examples: [{
     short: {
