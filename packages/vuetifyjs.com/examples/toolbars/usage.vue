c64bbf24e33e927a07d85e97a089a494ec362b7c
nekosaur <albert@kaaman.nu>
2018-01-06 13:06:27 +0100

    fix: updated toolbar page, added example

--- packages/vuetifyjs.com/examples/toolbars/usage.vue
+++ packages/vuetifyjs.com/examples/toolbars/usage.vue
@@ -1,8 +1,8 @@
 <template>
   <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
     <v-toolbar-title>Title</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn flat>Link One</v-btn>
       <v-btn flat>Link Two</v-btn>
