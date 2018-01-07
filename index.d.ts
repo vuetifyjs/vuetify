b09176ed284cc75f24e9430d79d076f1580ce965
Kael <kaelwd@gmail.com>
2018-01-08 01:55:40 +1100

    Add darken/lighten variations to theme colors (#2872)
    * feat(theme): add darken/lighten variations to theme colors + auto text
    
    * Remove automatic text color
    
    It was too ambitious for the timeframe, can revisit at some future time
    
    * Allow the user to minify or cache the theme stylesheet
    
    * Update VApp snapshots
    
    * refactor: use consoleWarn instead of throwing a rangeError
    
    * style: remove semicolon

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,77 @@
-import "vuetify/src/util/helpers";
-import {PluginFunction} from "vue";
+import 'vuetify/src/util/helpers'
+import { PluginFunction } from 'vue'
 
 declare class Vuetify {
-  static install: PluginFunction<never>;
+  static install: PluginFunction<never>
 }
 
-declare class VuetifyApplication {
-  bar: number;
-  bottom: number;
-  left: number;
-  right: number;
-  top: number;
+declare interface VuetifyApplication {
+  bar: number
+  bottom: number
+  left: number
+  right: number
+  top: number
 }
 
-declare class VuetifyBreakpoint {
-  height: number;
-  lg: boolean;
-  lgAndDown: boolean;
-  lgAndUp: boolean;
-  lgOnly: boolean;
-  md: boolean;
-  mdAndDown: boolean;
-  mdAndUp: boolean;
-  mdOnly: boolean;
-  name: string;
-  sm: boolean;
-  smAndDown: boolean;
-  smAndUp: boolean;
-  smOnly: boolean;
-  width: number;
-  xl: boolean;
-  xlOnly: boolean;
-  xs: boolean;
-  xsOnly: boolean;
+declare interface VuetifyBreakpoint {
+  height: number
+  lg: boolean
+  lgAndDown: boolean
+  lgAndUp: boolean
+  lgOnly: boolean
+  md: boolean
+  mdAndDown: boolean
+  mdAndUp: boolean
+  mdOnly: boolean
+  name: string
+  sm: boolean
+  smAndDown: boolean
+  smAndUp: boolean
+  smOnly: boolean
+  width: number
+  xl: boolean
+  xlOnly: boolean
+  xs: boolean
+  xsOnly: boolean
 }
 
-declare class VuetifyTheme {
-  primary: string;
-  accent: string;
-  secondary: string;
-  info: string;
-  warning: string;
-  error: string;
-  success: string;
+declare interface VuetifyTheme {
+  primary: VuetifyThemeItem
+  accent: VuetifyThemeItem
+  secondary: VuetifyThemeItem
+  info: VuetifyThemeItem
+  warning: VuetifyThemeItem
+  error: VuetifyThemeItem
+  success: VuetifyThemeItem
+
+  [name: string]: VuetifyThemeItem
 }
 
-declare class VuetifyObject {
-  application: VuetifyApplication;
-  breakpoint: VuetifyBreakpoint;
-  dark: boolean;
-  theme: VuetifyTheme;
+declare type VuetifyThemeItem = string | number
+
+declare interface VuetifyThemeCache {
+  get: (parsedTheme: VuetifyTheme) => string | null
+  set: (parsedTheme: VuetifyTheme, css: string) => void
 }
 
-declare module "vue/types/vue" {
+declare interface VuetifyOptions {
+  themeVariations?: string[]
+  minifyTheme?: (css: string) => string
+  themeCache?: VuetifyThemeCache
+}
+
+declare interface VuetifyObject {
+  application: VuetifyApplication
+  breakpoint: VuetifyBreakpoint
+  dark: boolean
+  theme: VuetifyTheme
+  options: VuetifyOptions
+}
+
+declare module 'vue/types/vue' {
   interface Vue {
-    $vuetify: VuetifyObject;
+    $vuetify: VuetifyObject
   }
 }
 
-export default Vuetify;
+export default Vuetify
