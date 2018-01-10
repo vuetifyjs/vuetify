d4984909c6c0c73656a41a94a6847ea24f0f0ccb
Евгений Плотников <bad4iz@gmail.com>
2018-01-11 00:09:57 +0400

    Merge pull request #189 from bad4iz/feature/fix_lang_en_mixins
    fix lang en mixins

--- packages/vuetifyjs.com/lang/en/mixins/Menuable.js
+++ packages/vuetifyjs.com/lang/en/mixins/Menuable.js
@@ -2,8 +2,8 @@ export default {
   props: {
     activator: 'Designate a custom activator when the activator slot is not used. String can be any valid querySelector and Object can be any valid Node',
     allowOverflow: 'Removes overflow re-positioning for the content',
-    maxWidth: `Sets the maximum width for the content`,
-    minWidth: `Sets the minimum width for the content`,
+    maxWidth: 'Sets the maximum width for the content',
+    minWidth: 'Sets the minimum width for the content',
     nudgeBottom: 'Nudge the content to the bottom',
     nudgeLeft: 'Nudge the content to the left',
     nudgeRight: 'Nudge the content to the right',
