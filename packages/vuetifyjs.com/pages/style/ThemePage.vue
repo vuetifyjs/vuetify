1f867b31f6ebc7969fceb34d18f61d67827e426a
Alexandre <alexandre.bonneau@linuxfr.eu>
2018-01-13 23:01:06 -1000

    Fix the colors property name `gray` to `grey` (#200)
    Signed-off-by: Alexandre Bonneau <alexandre.bonneau@linuxfr.eu>

--- packages/vuetifyjs.com/pages/style/ThemePage.vue
+++ packages/vuetifyjs.com/pages/style/ThemePage.vue
@@ -40,7 +40,7 @@
           | Vue.use(Vuetify, {
           |   theme: {
           |     primary: colors.purple.base,
-          |     secondary: colors.gray.darken1,
+          |     secondary: colors.grey.darken1,
           |     accent: colors.shades.black,
           |     error: colors.red.accent3
           |   }
