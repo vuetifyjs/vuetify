dfebfbabb7b980d3a3f7a029df7e8d3c29c20d43
Евгений Плотников <bad4iz@gmail.com>
2018-01-22 06:39:04 +0400

    Merge pull request #224 from ElderJames/master
    Chinese Translation for Components Pages

--- packages/vuetifyjs.com/lang/zh_Hans/components/Toolbars.js
+++ packages/vuetifyjs.com/lang/zh_Hans/components/Toolbars.js
@@ -1,6 +1,6 @@
 export default {
-  header: 'Toolbar',
-  headerText: 'The `v-toolbar` component is pivotal to any gui, as it generally is the primary source of site navigation. The toolbar component works great in conjunction with a navigation drawer for hiding links and presenting an activator to open the sidebar on mobile.',
+  header: '工具栏',
+  headerText: '`v-toolbar`组件对于任何GUI都很重要，因为它通常是网站导航的主要来源。工具栏组件可以与导航抽屉一起很好地工作，用于隐藏链接并呈现激活器以在移动设备上打开侧边栏。',
   components: [
     'v-toolbar',
     'v-toolbar-title',
@@ -10,75 +10,75 @@ export default {
   ],
   examples: [{
     default: {
-      header: 'Default',
-      desc: 'A toolbar is a flexible container that can be utilized in a number of ways. By default, the toolbar is 64px high on desktop, 56px high on mobile portrait and 48px high on mobile landscape. These can be overwritten using the `dense` and `prominent` props.'
+      header: '默认情况',
+      desc: '工具栏是一个灵活的容器，可以通过多种方式使用。默认情况下，桌面上的工具栏高为64px，移动人像的高为56px，移动风景的高为48px，这些可以用`dense`和`prominent`属性来重写。'
     },
     appBar: {
-      header: 'App bar',
-      desc: 'An App bar is the primary toolbar of your application. It can contain icons, menus and more.'
+      header: '应用程序栏',
+      desc: '应用程序栏是应用程序主要的主要工具栏，它可以包含图标、菜单等等。'
     },
     appBarExtension: {
-      header: 'App bar with extension',
-      desc: 'An extension can be used that creates a much more prominent bar. If a title is placed within the extension, it will automatically be shifted to the right to match the normal location it would be at in the main bar.'
+      header: '带扩展的应用程序栏',
+      desc: '扩展可以用来创建更突出的栏，如果标题放置在扩展内，它将自动移到右侧，与主栏中的正常位置相匹配。'
     },
     columnWidthWithHierarchy: {
-      header: 'Column width with hierarchy',
-      desc: 'Toolbars come in 2 variants, light and dark. Light toolbars have dark tinted buttons and dark text whereas Dark toolbars have white tinted buttons and white text.'
+      header: '具有层次结构的列宽',
+      desc: '工具栏有两个变种，明亮和暗黑。明亮工具栏有深色的按钮和黑暗的文字，而暗黑工具栏有白色的按钮和白色的文字。'
     },
     flexibleAndCard: {
-      header: 'Flexible toolbar and card toolbar',
-      desc: 'With a few custom styles, you can easily create your amazing user interfaces.'
+      header: '灵活的工具栏和卡片工具栏',
+      desc: '通过一些自定义样式，您可以轻松创建令人惊叹的用户界面。'
     },
     floatingWithSearch: {
-      header: 'Floating with search',
-      desc: 'A floating toolbar can be placed over the content that it references.'
+      header: '搜索时浮动',
+      desc: '可以将浮动工具栏放置在其引用的内容上。'
     },
     variations: {
-      header: 'Variations',
-      desc: 'An app-bar has multiple variations that can be applied with themes and helper classes. These range from light and dark themes, colored and transparent.',
+      header: '变体',
+      desc: '一个应用程序栏有多个可以应用主题和辅助类的变种，范围从明亮和暗黑的主题、色彩和透明度。',
       uninverted: true
     },
     prominent: {
-      header: 'Prominent toolbars',
-      desc: 'Prominent toolbars add a slot where you can placed elements in the extended area. The content and extension areas will be locked to 64px in height.'
+      header: '突出的工具栏',
+      desc: '突出的工具栏添加了一个插槽，你可以在扩展区域放置元素，内容和扩展区域将被锁定到64x的高度。'
     },
     dense: {
-      header: 'Dense toolbars',
-      desc: 'Dense toolbars reduce their height to 48px. This is also the default behavior for mobile landscape orientation.'
+      header: '密集的工具栏',
+      desc: '密集工具栏降低其高度未48px,这也是移动设备横向定位的默认行为。'
     },
     denseProminent: {
-      header: 'Dense prominent toolbars',
-      desc: 'You can combine both the prominent and dense to extend a smaller variant of the toolbar.'
+      header: '密集突出的工具栏',
+      desc: '您可以结合突出和密集来扩展工具栏的较小变体。'
     },
     scrolling: {
-      header: 'Scrolling techniques',
-      desc: 'A toolbar can transition off screen when scrolling. Here we demonstrate how you can specify the scroll target (default is window). Keep in mind, for this example there is special markup that will not be required in your application.'
+      header: '滚动屏幕',
+      desc: '滚动时，工具栏可以过渡到屏幕之外，这里我们演示如何指定滚动目标（默认是窗口）。请记住，在这个例子中，有一些特殊的标记在您的应用程序中不需要。'
     },
     appBarMenu: {
-      header: 'App bar with menu',
-      desc: 'You can add menu components to a toolbar.'
+      header: '应用程序栏与菜单',
+      desc: '您可以将菜单组件添加到工具栏。'
     },
     systemBarsStatusWindow: {
-      header: 'System bars - status/window',
-      desc: 'System bars are useful for notification icons on mobile and desktop. For live examples of, head to the pre-defined layouts.'
+      header: '系统栏——状态/窗口',
+      desc: '系统栏对于移动设备和桌面上的通知图标很有用，对于现场示例，请前往预定义的布局。'
     }
   }],
   props: {
-    card: 'Inherits border radius when used within `v-card`',
-    clippedLeft: 'Designates that the applications `v-navigation-drawer` is clipped on the left side of the toolbar',
-    clippedRight: 'Designates that the applications `v-navigation-drawer` is clipped on the right side of the toolbar',
-    dense: 'Reduces the height of the toolbar content and extension',
-    extended: 'Force the toolbar to generate the extension without using the slot',
-    flat: 'Removes the toolbar box-shadow',
-    floating: 'Makes the toolbar float inline',
-    height: 'Designates a specific height for the toolbar',
-    lightsOut: 'Reduces the system bar opacity',
-    manualScroll: 'Manually apply the **scroll-off-screen** functionality',
-    prominent: 'Increases the height of the toolbar content and extension',
-    scrollOffScreen: 'Will transition the toolbar off screen when scrolling down',
-    scrollTarget: 'Assign the scroll target for scroll-off-screen',
-    scrollThreshold: 'The amount of scroll distance down before the toolbar uses scroll-off-screen',
-    status: 'Reduces the system bar height',
-    window: 'Increases the system bar height'
+    card: '在`v-card`中使用时继承边框半径',
+    clippedLeft: '指定`v-navigation-drawer`应用程序在工具栏的左侧被剪切',
+    clippedRight: '指定`v-navigation-drawer`应用程序在工具栏的右侧被剪切',
+    dense: '减小工具栏内容和扩展的高度',
+    extended: '强制工具栏生成扩展名而不使用插槽',
+    flat: '移除工具栏的盒子阴影',
+    floating: '使工具栏内联浮动',
+    height: '为工具栏指定一个特定的高度',
+    lightsOut: '降低系统栏不透明度',
+    manualScroll: '手动应用**滚动屏幕**功能',
+    prominent: '增加工具栏内容和扩展的高度',
+    scrollOffScreen: '当向下滚动时工具栏会过渡到屏幕外。',
+    scrollTarget: '为滚动屏幕指定滚动目标',
+    scrollThreshold: '工具栏使用滚动屏幕之前滚动的距离',
+    status: '降低系统栏高度',
+    window: '增加系统栏高度'
   }
 }
