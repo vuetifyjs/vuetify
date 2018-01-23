c25cf5df53d8005564fbcc38474861c59284fe22
John Leider <john.j.leider@gmail.com>
2018-01-23 09:49:35 -0500

    added loading indicator

--- packages/vuetifyjs.com/components/store/StoreHeader.vue
+++ packages/vuetifyjs.com/components/store/StoreHeader.vue
@@ -2,11 +2,17 @@
   v-container.text-xs-center#store-header
     v-layout
       v-flex(xs12)
-        img(
-          src="/static/doc-images/logo.svg"
-          height="125px"
-          alt="Vuetify"
+        v-progress-circular(
+          :indeterminate="loading"
+          size="200"
+          :width="loading ? 1 : 0"
+          color="secondary"
         ).mb-3
+          img(
+            src="/static/doc-images/logo.svg"
+            height="125px"
+            alt="Vuetify"
+          )
 
         h1(v-text="header").display-1.primary--text.mb-3
         p(v-text="subHeader").subheading
@@ -20,6 +26,10 @@
       header: {
         type: String,
         default: undefined
+      },
+      loading: {
+        type: Boolean,
+        default: false
       },
       subHeader: {
         type: String,
