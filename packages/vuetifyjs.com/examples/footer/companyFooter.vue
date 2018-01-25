f93be3c3807d75d5eadafacca2a21d3c16264847
John Leider <john@vuetifyjs.com>
2018-01-24 20:34:11 -0500

    Merge pull request #231 from vuetifyjs/footers-new
    Footers new

---
+++ packages/vuetifyjs.com/examples/footer/companyFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer height="auto" class="grey darken-3">
+    <v-layout row wrap justify-center>
+      <v-btn
+        color="white"
+        flat
+        v-for="link in links"
+        :key="link"
+      >
+        {{ link }}
+      </v-btn>
+      <v-flex xs12 py-3 text-xs-center white--text>
+        &copy; 2018 <strong>vuetifyjs.com</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: ['Home', 'About Us', 'Team', 'Services', 'Blog', 'Contact Us']
+    })
+  }
+</script>
---
+++ packages/vuetifyjs.com/examples/footer/tealFooter.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-footer height="auto">
+    <v-card flat tile>
+      <v-card-title class="teal white--text">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+        <v-spacer></v-spacer>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          icon
+          dark
+          class="mx-3"
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text class="grey lighten-3">
+        <v-layout>
+          <v-flex
+            v-for="(col, i) in rows"
+            :key="i"
+            xs3
+          >
+            <span class="body-2" v-text="col.title.toUpperCase()"/>
+            <div
+              v-for="(child, i) in col.children"
+              :key="i"
+              v-text="child"
+            />
+          </v-flex>
+          <v-flex xs3 layout column>
+            <span class="body-2">CONTACT</span>
+            <div>
+              <v-icon size="18px" class="mr-3">fa-home</v-icon>
+              New York, NY 10012, US
+            </div>
+            <div>
+              <v-icon size="18px" class="mr-3">fa-envelope</v-icon>
+              info@example.com
+            </div>
+            <div>
+              <v-icon size="18px" class="mr-3">fa-phone</v-icon>
+              + 01 234 567 88
+            </div>
+            <div>
+              <v-icon size="18px" class="mr-3">fa-print</v-icon>
+              + 01 234 567 89
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+      <v-card-actions class="grey lighten-2 justify-center">
+        &copy; 2018 <strong>vuetifyjs.com</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: ['fa-facebook', 'fa-twitter', 'fa-google-plus', 'fa-linkedin', 'fa-instagram'],
+      rows: [
+        {
+          title: 'Company Name',
+          children: ['Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit']
+        },
+        {
+          title: 'Products',
+          children: ['MDBootstrap', 'MDWordPress', 'BrandFlow', 'Bootstrap Angular']
+        },
+        {
+          title: 'Useful Links',
+          children: ['Your account', 'Become an Affiliate', 'Shipping Rates', 'Helper']
+        }
+      ]
+    })
+  }
+</script>
