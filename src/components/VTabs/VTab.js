603f757a37f804ba072408d2bd929419017dc639
John Leider <john.j.leider@gmail.com>
2018-01-28 20:00:36 -0500

    fix(v-tab): changed how active link is derived
    the current logic was very hacky and caused issues when building static sites, such as with nuxt
    
    fixes #3010

--- src/components/VTabs/VTab.js
+++ src/components/VTabs/VTab.js
@@ -4,6 +4,9 @@ import {
   inject as RegistrableInject
 } from '../../mixins/registrable'
 
+// Utilities
+import { getObjectValueByPath } from '../../util/helpers'
+
 export default {
   name: 'v-tab',
 
@@ -52,14 +55,12 @@ export default {
   },
 
   watch: {
-    $route: {
-      immediate: true,
-      handler: 'onRouteChange'
-    }
+    $route: 'onRouteChange'
   },
 
   mounted () {
     this.tabs.register(this)
+    this.onRouteChange()
   },
 
   beforeDestroy () {
@@ -80,13 +81,13 @@ export default {
       this.to || this.tabClick(this)
     },
     onRouteChange () {
-      if (!this.to) return
+      if (!this.to || !this.$refs.link) return
 
-      this.$nextTick(() => {
-        if (this.$el.firstChild.className.indexOf(this.activeClass) > -1) {
-          this.tabClick(this)
-        }
-      })
+      const path = `_vnode.data.class.${this.activeClass}`
+
+      if (getObjectValueByPath(this.$refs.link, path)) {
+        this.tabClick(this)
+      }
     },
     toggle (action) {
       this.isActive = (action === this) || (action === this.action)
@@ -100,6 +101,8 @@ export default {
     // If disabled, use div as anchor tags do not support
     // being disabled
     const tag = this.disabled ? 'div' : link.tag
+
+    data.ref = 'link'
 
     return h('div', {
       staticClass: 'tabs__div'
