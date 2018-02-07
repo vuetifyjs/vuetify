5ea6bf8cbcd8029382ffb9f9f1d6f96937a30b19
John Leider <john.j.leider@gmail.com>
2018-02-07 07:46:55 -0500

    fix(grid-example): updated markup to fix text wrapping
    fixes #233

--- packages/vuetifyjs.com/examples/grid/offset.vue
+++ packages/vuetifyjs.com/examples/grid/offset.vue
@@ -8,17 +8,17 @@
       </v-flex>
       <v-flex xs7 offset-xs0 offset-md2 offset-lg5>
         <v-card dark color="secondary">
-          <v-card-text>xs7 offset-(xs5|md2|lg5)</v-card-text>
+          <v-card-text>xs7 offset-(xs5 | md2 | lg5)</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm5 md2>
+      <v-flex xs12 sm5 md3>
         <v-card dark color="primary">
-          <v-card-text>(xs12|sm5|md2)</v-card-text>
+          <v-card-text>(xs12 | sm5 | md3)</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm5 md3 offset-xs0 offset-lg2>
+      <v-flex xs12 sm5 md5 offset-xs0 offset-lg2>
         <v-card dark color="green">
-          <v-card-text>(xs12|sm5|md3) offset-(xs0|lg2)</v-card-text>
+          <v-card-text>(xs12 | sm5 | md5) offset-(xs0 | lg2)</v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
