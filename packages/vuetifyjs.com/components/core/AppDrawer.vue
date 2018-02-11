a13fa83fca26a12923b06461f564aebc92603ca0
John Leider <john.j.leider@gmail.com>
2018-02-10 19:41:16 -0500

    doc updates

--- packages/vuetifyjs.com/components/core/AppDrawer.vue
+++ packages/vuetifyjs.com/components/core/AppDrawer.vue
@@ -21,16 +21,7 @@
             :src="`/static/doc-images/${diamond.src}`"
             :alt="diamond.title"
           )
-      v-btn(
-        dark
-        style="background-color: #F96854 !important;"
-        active-class=""
-        href="https://www.patreon.com/bePatron?u=6223325"
-        target="_blank"
-        rel="noopener"
-      )
-        img(src="/static/doc-images/patreon-dark.png" width="24px").mr-2
-        span(v-text="$t('Vuetify.AppDrawer.becomeASponsor')").caption
+      patreon-btn
     v-container(fluid)
       v-text-field(
         placeholder="Search"
---
+++ packages/vuetifyjs.com/components/misc/SupportVuetify.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+    class="mb-5"
+    href="https://www.patreon.com/bePatron?u=6223325"
+    rel="noopener"
+    target="_blank"
+  >
+    <v-card-text>
+      <v-layout align-center>
+        <v-flex display-1 patreon mr-4>
+          PATREON
+        </v-flex>
+        <v-flex patreon-bar mr-4 />
+        <v-flex display-1 patreon-caption pb-1>
+          Support Vuetify
+        </v-flex>
+        <img src="/static/doc-images/logo.svg" width="50px" alt="">
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style lang="stylus" scoped>
+  .patreon
+    color: #052D49
+    flex: 0 0 auto
+    font-weight: 900
+    letter-spacing: 6px !important
+
+  .patreon-bar
+    background-color: #052D49
+    height: 80px
+    flex: 0 1 6px
+
+  .patreon-caption
+    color: #052D49
+</style>
