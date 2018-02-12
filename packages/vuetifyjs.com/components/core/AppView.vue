d06696a432d37f96ec09618667b1e34ef573041a
John Leider <john.j.leider@gmail.com>
2018-02-11 19:50:34 -0500

    added new footer

--- packages/vuetifyjs.com/components/core/AppView.vue
+++ packages/vuetifyjs.com/components/core/AppView.vue
@@ -2,4 +2,16 @@
   v-content
     v-fade-transition(mode="out-in")
       router-view(:key="$route.path")
+    app-footer
 </template>
+
+<script>
+  // Components
+  import AppFooter from '@/components/core/AppFooter'
+
+  export default {
+    components: {
+      AppFooter
+    }
+  }
+</script>
