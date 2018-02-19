e446cd5f691ce4cc8e238a4aeb5ec3530ae8047e
John Leider <john@vuetifyjs.com>
2018-02-19 16:24:01 -0500

    fix(snackbars): fixed snackbars not being shown on docs
    fixes #179

--- packages/vuetifyjs.com/components/helpers/Example.vue
+++ packages/vuetifyjs.com/components/helpers/Example.vue
@@ -219,6 +219,9 @@
 <style lang="stylus">
   @import '~vuetify/src/stylus/settings/_variables.styl'
 
+  #snackbars .component-example .application--example
+    z-index: auto
+
   .component-example
     // margin-bottom: 32px
 
