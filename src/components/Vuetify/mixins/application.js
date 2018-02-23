d785ff1d6cd4dcb072cf32090e45f5a7b2d32ea8
John Leider <john.j.leider@gmail.com>
2018-02-23 16:00:18 -0500

    fix(applicationable): updated process
    Will now store reference to the components that bind to ensure no unbinding component can overwrite
    a new bind
    
    fixes #3347

--- src/components/Vuetify/mixins/application.js
+++ src/components/Vuetify/mixins/application.js
@@ -4,5 +4,29 @@ export default {
   footer: 0,
   left: 0,
   right: 0,
-  top: 0
+  top: 0,
+  components: {
+    bar: {},
+    bottom: {},
+    footer: {},
+    left: {},
+    right: {},
+    top: {}
+  },
+  bind (uid, target, value) {
+    if (!this.components[target]) return
+
+    this.components[target] = { [uid]: value }
+    this.update(target)
+  },
+  unbind (uid, target) {
+    if (this.components[target][uid] == null) return
+
+    delete this.components[target][uid]
+    this.update(target)
+  },
+  update (target) {
+    this[target] = Object.values(this.components[target])
+      .reduce((acc, cur) => (acc + cur), 0)
+  }
 }
