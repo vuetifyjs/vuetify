bbd04d7925635caebe8bd09ae5df4a10f54c4b59
Jente Van Heuverswyn <jente.vanheuverswyn@orbitgis.com>
2018-03-03 10:42:08 +0100

    Fix code style, Add interface for colors with accents

--- src/util/colors.d.ts
+++ src/util/colors.d.ts
@@ -1,45 +1,49 @@
 declare module 'vuetify/es5/util/colors' {
-  interface Color {
-    base: string;
-    lighten5: string;
-    lighten4: string;
-    lighten3: string;
-    lighten2: string;
-    lighten1: string;
-    darken1: string;
-    darken2: string;
-    darken3: string;
-    darken4: string;
-    accent1?: string;
-    accent2?: string;
-    accent3?: string;
-    accent4?: string;
-  }
+	interface BaseColor {
+		base: string
+		lighten5: string
+		lighten4: string
+		lighten3: string
+		lighten2: string
+		lighten1: string
+		darken1: string
+		darken2: string
+		darken3: string
+		darken4: string
+	}
 
-  interface Shade {
-    black: string;
-    white: string;
-    transparent: string;
-  }
+	interface Color extends BaseColor {
+		accent1?: string
+		accent2?: string
+		accent3?: string
+		accent4?: string
+	}
 
-  export const red: Color;
-  export const pink: Color;
-  export const purple: Color;
-  export const deepPurple: Color;
-  export const indigo: Color;
-  export const blue: Color;
-  export const lightBlue: Color;
-  export const cyan: Color;
-  export const teal: Color;
-  export const green: Color;
-  export const lightGreen: Color;
-  export const lime: Color;
-  export const yellow: Color;
-  export const amber: Color;
-  export const orange: Color;
-  export const deepOrange: Color;
-  export const brown: Color;
-  export const blueGrey: Color;
-  export const grey: Color;
-  export const shades: Shade;
+	interface Shade {
+		black: string
+		white: string
+		transparent: string
+	}
+
+	export const red: Color
+	export const pink: Color
+	export const purple: Color
+	export const deepPurple: Color
+	export const indigo: Color
+	export const blue: Color
+	export const lightBlue: Color
+	export const cyan: Color
+	export const teal: Color
+	export const green: Color
+	export const lightGreen: Color
+	export const lime: Color
+	export const yellow: Color
+	export const amber: Color
+	export const orange: Color
+	export const deepOrange: Color
+	export const brown: BaseColor
+	export const blueGrey: BaseColor
+	export const grey: BaseColor
+	export const shades: Shade
 }
+
