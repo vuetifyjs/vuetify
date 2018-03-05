51a7dee0761d89958e780397e107ed933edf1c32
John Leider <john.j.leider@gmail.com>
2018-03-04 21:34:57 -0500

    feat($home-page): changed layout

--- packages/vuetifyjs.com/components/misc/index.js
+++ packages/vuetifyjs.com/components/misc/index.js
@@ -1,6 +1,5 @@
 import CtaBtn from './CtaBtn'
 import Grid from './Grid'
-import HomeFooter from './HomeFooter'
 import MaskTable from './MaskTable'
 import PatreonBtn from './PatreonBtn'
 import SupportVuetify from './SupportVuetify'
@@ -9,7 +8,6 @@ import TemplateList from './TemplateList'
 export default {
   CtaBtn,
   Grid,
-  HomeFooter,
   MaskTable,
   PatreonBtn,
   SupportVuetify,
