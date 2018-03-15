2a3f0250fcf98511502406749d11b875c2cb093a
Paul Whittemore <dev@appurist.com>
2018-03-15 15:37:56 -0300

    Simplify the iconfont set selection dramatically.
    Just use { iconfont: 'fa' } for font-awesome, and/or override individual icons.

--- src/components/Vuetify/mixins/icons.js
+++ src/components/Vuetify/mixins/icons.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-multi-spaces */
-
 // Maps internal Vuetify icon names to actual Material Design icon names.
 const ICONS_MATERIAL = {
   'cancel': 'cancel',
   'close': 'close',
-  'delete': 'cancel',  // delete (e.g. v-chip close)
+  'delete': 'cancel', // delete (e.g. v-chip close)
   'success': 'check_circle',
   'info': 'info',
   'warning': 'priority_high',
@@ -21,19 +19,11 @@ const ICONS_MATERIAL = {
   'subgroup': 'arrow_drop_down'
 }
 
-// Provides iconfont meta configuration for Material Design icons.
-const ICONFONT_MATERIAL = {
-  name: 'Material Design',
-  iconType: 'material-design',
-  prefix: ['mdi', 'mdi_', 'mdi-'],
-  placement: 'auto' // 'auto', 'class', 'text' for inner text content,or 'data-icon'
-}
-
 // Maps internal Vuetify icon names to actual Font-Awesome icon names.
 const ICONS_FONTAWESOME = {
   'cancel': 'fas fa-times-circle',
   'close': 'fas fa-times',
-  'delete': 'fas fa-times-circle',  // delete (e.g. v-chip close)
+  'delete': 'fas fa-times-circle', // delete (e.g. v-chip close)
   'success': 'fas fa-check-circle',
   'info': 'fas fa-info-circle',
   'warning': 'fas fa-exclamation',
@@ -43,31 +33,15 @@ const ICONS_FONTAWESOME = {
   'checked': 'fas fa-check-square',
   'unchecked': 'far fa-square', // note 'far'
   'indeterminate': 'fas fa-minus-square',
-  'dot': 'fas fa-circle',   // for carousel
+  'dot': 'fas fa-circle', // for carousel
   'sort': 'fas fa-sort-up',
   'expand': 'fas fa-chevron-down',
   'append': 'fas fa-keyboard_arrow_down',
   'subgroup': 'fas fa-caret-down'
 }
 
-// Provides iconfont meta configuration for Material Design icons.
-const ICONFONT_FONTAWESOME = {
-  name: 'Font Awesome',
-  iconType: 'fa',
-  prefix: ['fa', 'fas', 'far', 'fal', 'fab'],
-  placement: 'auto' // 'auto', 'class', 'text' for inner text content,or 'data-icon'
-}
-
-export default function icons (iconfont = {}, icons = {}) {
-  let iconfontToUse = ICONFONT_MATERIAL
-  let iconsToUse = ICONS_MATERIAL
-  if ((iconfont.name && iconfont.name === 'Font Awesome') ||
-      (iconfont.iconType && iconfont.iconType === 'fa')) {
-    iconfontToUse = ICONFONT_FONTAWESOME
-    iconsToUse = ICONS_FONTAWESOME
-  }
-
-  let combinedIcons = Object.assign({}, iconsToUse, icons)
-  combinedIcons.iconfont = Object.assign({},  iconfontToUse, iconfont)
-  return combinedIcons
+export default function icons (iconfont = 'md', icons = {}) {
+  const iconSet = iconfont.toLowerCase()
+  const iconsToUse = (iconSet === 'fa') ? ICONS_FONTAWESOME : ICONS_MATERIAL
+  return Object.assign({}, iconsToUse, icons)
 }
