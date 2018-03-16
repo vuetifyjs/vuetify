ba537c4e2589eb482a792f7ccc8ad9b3c65539fa
Paul Whittemore <dev@appurist.com>
2018-03-16 17:06:09 -0300

    Fixed the test for the 'icon--checkbox' class.
    Resolves the remaining failing testcases.

--- src/components/VCheckbox/VCheckbox.js
+++ src/components/VCheckbox/VCheckbox.js
@@ -65,7 +65,7 @@ export default {
       h(VIcon, {
         staticClass: 'icon--selection-control',
         'class': {
-          'icon--checkbox': this.icon === this.$vuetify.icons.checked
+          'icon--checkbox': this.icon === 'icon$checked'
         },
         key: this.icon,
         on: Object.assign({
