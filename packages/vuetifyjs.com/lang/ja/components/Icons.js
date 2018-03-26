58135fc4320014c0d6a31251f54b9921b53ff8ae
Евгений Плотников <info@bad4iz.ru>
2018-03-26 12:47:53 +0400

    Merge pull request #379 from YukaKadokura/translate_iconpage_into_ja
    Japanese translation for Components/Icon

--- packages/vuetifyjs.com/lang/ja/components/Icons.js
+++ packages/vuetifyjs.com/lang/ja/components/Icons.js
@@ -1,36 +1,36 @@
 export default {
   header: 'Icon',
-  headerText: 'The `v-icon` component provides a large set of glyphs to provide context to various aspects of your application. Vuetify icons utilize Google\'s Material Icons font library. For a list of all available icons, visit the official <a href="https://material.io/icons/" target="_blank" rel="noopener">Material Icons</a> page.',
+  headerText: '`v-icon`コンポーネントは、あなたのアプリケーションの様々な場面に応じて多くのアイコンを提供します。Vuetifyのアイコンは、GoogleのMaterial Iconsライブラリを使用します。 全ての利用可能なアイコンのリストは、公式の<a href="https://material.io/icons/" target="_blank" rel="noopener">Material Icons</a>ページをご覧ください。',
   components: ['v-icon'],
   examples: [{
     standard: {
       header: 'Standard',
-      desc: 'Icons come in two themes (light and dark), and four different sizes (standard, medium, large, and x-large).'
+      desc: 'アイコンは2つのテーマ(light、dark)と、4つの異なるサイズ(standard、medium、large、x-large)で表現されます。'
     },
     fontAwesome: {
       header: 'Font Awesome',
-      desc: '[Font Awesome](http://fontawesome.io/icons/) is also supported. Simply use the `fa-` prefixed icon name. Please note that you still need to include the Font Awesome icons in your project.'
+      desc: '[Font Awesome](http://fontawesome.io/icons/) のアイコンの利用をサポートしています。アイコン名に`fa-`プレフィックスを付与するだけで使用できます。あなたのプロジェクトにFont Awesomeを導入しておく必要があるので注意して下さい。'
     },
     mdi: {
       header: 'Material Design Icons',
-      desc: '[Material Design Icons](https://materialdesignicons.com/) is also supported. Simply use the `mdi-` prefixed icon name. Please note that you still need to include the MDI icons in your project.'
+      desc: '[Material Design Icons](https://materialdesignicons.com/)のアイコンの利用をサポートしています。アイコン名に`mdi-`プレフィックスを付与するだけで使用できます。あなたのプロジェクトにMaterial Design Iconsを導入しておく必要があるので注意して下さい。'
     },
     color: {
-      header: 'Color',
-      desc: 'Using color helpers you can change the color of an icon from the standard dark and light themes.'
+      header: '色',
+      desc: 'カラーヘルパーを使用すると、アイコンの色を標準のdark、lightテーマから変更できます。'
     },
     buttons: {
-      header: 'Buttons',
-      desc: 'Icons can be used inside of buttons to add emphasis to the action.'
+      header: 'ボタン',
+      desc: 'アイコンをボタン内部で使用し、ボタンを強調させるための動きを付けることができます。'
     }
   }],
   props: {
     disabled: 'Mixins.Input.props.disabled',
-    large: 'Makes the icon large **(36px)**',
-    left: 'Places icon on the left, when used inside a button',
-    medium: 'Makes the icon medium **(28px)**',
-    right: 'Places icon on the right, when used inside a button',
-    small: 'Makes the icon small **(16px)**',
-    xLarge: 'Makes the icon extra large **(40px)**'
+    large: 'アイコンのサイズを大 **(36px)** にします。',
+    left: 'ボタン内部でアイコンを使用する場合に、アイコンを左に配置します。',
+    medium: 'アイコンのサイズを中 **(28px)** にします。',
+    right: 'ボタン内部でアイコンを使用する場合に、アイコンを右に配置します。',
+    small: 'アイコンのサイズを小 **(16px)** にします。',
+    xLarge: 'アイコンのサイズを最大 **(40px)** にします。'
   }
 }
