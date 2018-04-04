4df52fd65e0928c6a03fdd26c744684adc0ac913
John Leider <john.j.leider@gmail.com>
2018-04-04 18:27:34 -0400

    test(v-select-list): added unit tests

--- test/unit/components/VSelect/VSelectList.spec.js
+++ test/unit/components/VSelect/VSelectList.spec.js
@@ -36,4 +36,53 @@ test('VSelect', ({ mount, compileToFunctions }) => {
     expect(divider.children.length).toBe(1)
     expect(divider.children[0].text).toBe('foobar')
   })
+
+  it('should use no-data slot', () => {
+    const wrapper = mount(VSelectList, {
+      slots: {
+        'no-data': [{
+          render: h => h('div', 'foo')
+        }]
+      }
+    })
+    expect(wrapper.vm.$slots['no-data'].length).toBe(1)
+  })
+
+  it('should generate children', () => {
+    const wrapper = mount(VSelectList, {
+      propsData: {
+        items: [
+          { header: true },
+          { divider: true },
+          'foo'
+        ]
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should return defined item value', () => {
+    const wrapper = mount(VSelectList, {
+      propsData: {
+        itemValue: 'foo'
+      }
+    })
+
+    const getValue = wrapper.vm.getValue
+
+    expect(getValue({ fizz: 'buzz' })).toEqual({ fizz: 'buzz' })
+
+    wrapper.setProps({ itemValue: 'fizz' })
+
+    expect(getValue({ fizz: 'buzz' })).toEqual('buzz')
+  })
+
+  it('should use supplied item if cannot find path', () => {
+    const wrapper = mount(VSelectList)
+    const getPropertyFromItem = wrapper.vm.getPropertyFromItem
+
+    expect(getPropertyFromItem({ foo: true }, 'foo.bar')).toEqual({ foo: true })
+    expect(getPropertyFromItem({ foo: { bar: true }}, 'foo.bar')).toBe(true)
+  })
 })
