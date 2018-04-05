973afd50e5d18d5400981453846d300587cd8c8e
Pedro Vieira <peluprvi@gmail.com>
2018-04-04 22:46:47 -0300

    refactor (VSubheader): Add v- prefix on subheader classes (#3773)

--- src/components/VSubheader/VSubheader.js
+++ src/components/VSubheader/VSubheader.js
@@ -14,9 +14,9 @@ export default {
   },
 
   render (h, { data, children, props }) {
-    data.staticClass = (`subheader ${data.staticClass || ''}`).trim()
+    data.staticClass = (`v-subheader ${data.staticClass || ''}`).trim()
 
-    if (props.inset) data.staticClass += ' subheader--inset'
+    if (props.inset) data.staticClass += ' v-subheader--inset'
     if (props.light) data.staticClass += ' theme--light'
     if (props.dark) data.staticClass += ' theme--dark'
 
