240ab4707bf4219dd39c7f333e6e5bd233cd8a9b
John Leider <john.j.leider@gmail.com>
2018-04-12 20:40:51 -0400

    updated indentation

--- packages/vuetifyjs.com/examples/navigation-drawers/combined.vue
+++ packages/vuetifyjs.com/examples/navigation-drawers/combined.vue
@@ -15,33 +15,33 @@
         value="true"
       >
         <v-toolbar flat class="transparent">
-            <v-list class="pa-0">
-              <v-list-tile avatar>
-                <v-list-tile-avatar>
-                  <img src="https://randomuser.me/api/portraits/men/85.jpg" >
-                </v-list-tile-avatar>
-                <v-list-tile-content>
-                  <v-list-tile-title>John Leider</v-list-tile-title>
-                </v-list-tile-content>
-                <v-list-tile-action>
-                  <v-btn icon @click.native.stop="mini = !mini">
-                    <v-icon>chevron_left</v-icon>
-                  </v-btn>
-                </v-list-tile-action>
-              </v-list-tile>
-            </v-list>
-          </v-toolbar>
-          <v-list class="pt-0" dense>
-            <v-divider></v-divider>
-            <v-list-tile v-for="item in items" :key="item.title" @click="">
-              <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg" >
+              </v-list-tile-avatar>
               <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                <v-list-tile-title>John Leider</v-list-tile-title>
               </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn icon @click.native.stop="mini = !mini">
+                  <v-icon>chevron_left</v-icon>
+                </v-btn>
+              </v-list-tile-action>
             </v-list-tile>
           </v-list>
+        </v-toolbar>
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in items" :key="item.title" @click="">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
       </v-navigation-drawer>
       <v-list class="grow">
         <v-list-tile
