6f29f7576b3dc745dc539ba984d582754d0d9ee9
John Leider <john@vuetifyjs.com>
2018-04-15 11:24:27 -0400

    Merge pull request #394 from YukaKadokura/translation_components_into_ja
    Translation components(Footer, Forms, Dialogs) into ja

--- packages/vuetifyjs.com/lang/ja/components/Dialogs.js
+++ packages/vuetifyjs.com/lang/ja/components/Dialogs.js
@@ -1,54 +1,61 @@
 export default {
   header: 'Dialog',
-  headerText: 'The `v-dialog` component inform users about a specific task and may contain critical information, require decisions, or involve multiple tasks. Use dialogs sparingly because they are interruptive.',
+  headerText:
+    '`v-dialog`コンポーネントは、特定のタスクについてユーザに通知し、重要な情報を含んだり意思決定を必要とするような複数のタスクを伴う可能性があります。割り込み的な要素ですので、ダイアログは控えめに使用してください。 ',
   components: ['v-dialog'],
-  examples: [{
-    simple: {
-      header: 'Simple dialogs',
-      desc: 'Choosing an option immediately commits the option and closes the menu. Touching outside of the dialog, or pressing Back, cancels the action and closes the dialog.',
-      uninverted: true
-    },
-    withoutActivator: {
-      header: 'Without activator',
-      desc: 'If for some reason you are unable to use the activator slot, be sure to add the `.stop` modifier to the event that triggers the dialog.',
-      uninverted: true
-    },
-    modal: {
-      header: 'Modal',
-      desc: 'Similar to a Simple Dialog, except that it\'s not dismissed when touching outside.',
-      uninverted: true
-    },
-    fullscreen: {
-      header: 'Fullscreen',
-      desc: 'Due to limited space, full-screen dialogs may be more appropriate for mobile devices than dialogs used on devices with larger screens.',
-      uninverted: true
-    },
-    form: {
-      header: 'Form',
-      desc: 'Just a simple example of a form in a dialog.',
-      uninverted: true
-    },
-    scrollable: {
-      header: 'Scrollable',
-      desc: 'Example of a dialog with scrollable content.',
-      uninverted: true
-    },
-    overflowed: {
-      header: 'Overflowed',
-      desc: 'Modals that do not fit within the available window space will scroll the container.',
-      uninverted: true
+  examples: [
+    {
+      simple: {
+        header: 'シンプルなダイアログ',
+        desc:
+          'オプションを選択するとすぐにオプションがコミットされ、メニューが閉じます。ダイアログの外側に触れる、または戻るを押すと、アクションがキャンセルされ、ダイアログが閉じます。',
+        uninverted: true
+      },
+      withoutActivator: {
+        header: 'activatorを使用しない場合',
+        desc:
+          '何らかの理由で `activator` slotを使用しない場合は、ダイアログをトリガするイベントに `.stop`修飾子を必ず追加してください。',
+        uninverted: true
+      },
+      modal: {
+        header: 'モーダル',
+        desc: 'シンプルなダイアログに似ていますが、モーダルの外側に触れてもモーダルは閉じません。',
+        uninverted: true
+      },
+      fullscreen: {
+        header: 'フルスクリーンのダイアログ',
+        desc:
+          'スペースが限られているため、モバイルデバイスにとって、フルスクリーンのダイアログは大画面のデバイスで使用されるダイアログよりも適している可能性があります。',
+        uninverted: true
+      },
+      form: {
+        header: 'ダイアログ内のフォーム',
+        desc: 'ダイアログ内のフォームのシンプルな例です。',
+        uninverted: true
+      },
+      scrollable: {
+        header: 'スクロール可能なダイアログ',
+        desc: 'スクロール可能なコンテンツを持つダイアログの例です。',
+        uninverted: true
+      },
+      overflowed: {
+        header: 'オーバーフロー',
+        desc: '利用可能なウィンドウスペースに収まらないモーダルは、コンテナでスクロールされます。',
+        uninverted: true
+      }
     }
-  }],
+  ],
   props: {
-    disabled: 'Disabled the ability to open the dialog',
-    fullWidth: 'Specifies the modal to force 100% width',
-    fullscreen: 'Changes layout for fullscreen display',
-    hideOverlay: 'Hide the display of the overlay',
+    disabled: 'ダイアログを開く機能を無効にします。',
+    fullWidth: 'モーダルの幅が100%となるよう強制的に指定します。',
+    fullscreen: 'フルスクリーン表示になるようレイアウトを変更します。',
+    hideOverlay: '前面以外の表示を隠します。Hide the display of the overlay',
     lazy: 'Mixins.Bootable.props.lazy',
-    maxWidth: 'The maximum width of the content',
+    maxWidth: 'コンテンツの最大幅を指定します。',
     origin: 'Mixins.Transitionable.props.origin',
-    persistent: 'Clicking outside will not dismiss the dialog',
-    scrollable: 'When set to true, expects a card, card-title, card-text and card-actions. Additionally card-text should have specified height. Will set card-text to overflow-y',
-    width: 'Sets the dialog width'
+    persistent: 'ダイアログの外側をクリックしてもダイアログを消しません。',
+    scrollable:
+      'trueを設定すると、 `card` 、 `card-title` 、 `card-text` 、 `card-action` が必要です。さらに、card-textの高さを設定する必要があります。 `card-text` は、スタイルの `overflow-y` を設定します。',
+    width: 'ダイアログの幅を設定します。'
   }
 }
