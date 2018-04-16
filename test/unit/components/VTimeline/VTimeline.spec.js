2d396ad2067c173e7bef6998e2e23daf7c442e12
abhaywawale <abhaybwawale@gmail.com>
2018-04-16 18:08:07 +0530

    deleted unnecessary props

--- test/unit/components/VTimeline/VTimeline.spec.js
+++ test/unit/components/VTimeline/VTimeline.spec.js
@@ -6,19 +6,19 @@ test('VTimeline.js', ({ mount, compileToFunctions }) => {
   it('should render a div with class timeline', () => {
     const wrapper = mount(VTimeline)
     expect(wrapper.is('div')).toBe(true)
-    expect(wrapper.hasClass('timeline')).toBe(true)
+    expect(wrapper.hasClass('v-timeline')).toBe(true)
   })
 
   it('should render one ul with timeline__container class and match snapshot', () => {
     const wrapper = mount(VTimeline)
-    const listElements = wrapper.find('.timeline ul')
+    const listElements = wrapper.find('.v-timeline ul')
 
     expect(listElements).toHaveLength(1)
 
     const ul = wrapper.first('ul')
 
     expect(ul.is('ul')).toBe(true)
-    expect(ul.hasClass('timeline__container')).toBe(true)
+    expect(ul.hasClass('v-timeline__container')).toBe(true)
     expect(wrapper.html()).toMatchSnapshot()
   })
 
@@ -28,7 +28,7 @@ test('VTimeline.js', ({ mount, compileToFunctions }) => {
         light: true
       }
     })
-    const div = wrapper.find('.timeline')[0]
+    const div = wrapper.find('.v-timeline')[0]
     expect(div.hasClass('theme--light')).toBe(true)
   })
 
@@ -38,25 +38,7 @@ test('VTimeline.js', ({ mount, compileToFunctions }) => {
         dark: true
       }
     })
-    const div = wrapper.find('.timeline')[0]
+    const div = wrapper.find('.v-timeline')[0]
     expect(div.hasClass('theme--dark')).toBe(true)
-  })
-
-  it('should inject slot to children', () => {
-    const { render } = compileToFunctions(`
-      <v-timeline>
-        <v-timeline-item/>
-      </v-timeline>
-    `)
-    const component = Vue.component('test', {
-      components: {
-        VTimeline,
-        VTimelineItem
-      },
-      render
-    })
-    const wrapper = mount(component)
-
-    expect(wrapper.html()).toMatchSnapshot()
   })
 })
