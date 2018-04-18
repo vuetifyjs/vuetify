f784eb7d6b05f37b2d4e8904f327840813e96e65
Dmitry Sadovsky <dmitry.sadovskiy@insales.ru>
2018-04-18 17:55:59 +0300

    test coverage

--- test/unit/components/VDataTable/VDataTable.spec.js
+++ test/unit/components/VDataTable/VDataTable.spec.js
@@ -382,4 +382,56 @@ test('VDataTable.vue', ({ mount, compileToFunctions }) => {
     expect(wrapper.find('tr.nested')[0].element.parentElement.tagName).toBe('TBODY')
     expect('Unable to locate target [data-app]').toHaveBeenTipped()
   })
+
+  it('should render expand slot as indirect child', async () => {
+    const vm = new Vue()
+
+    const items = (props) => {
+      return vm.$createElement('tr',
+        {
+          on: {
+            click: () => props.expanded = !props.expanded
+          },
+          attrs: { class: 'item' }
+        },
+        [ vm.$createElement('td', [props.item.name]) ]
+      )
+    }
+
+    const expand = (props) => {
+      return  props.item.ingredients.map((ingredient) => {
+            return vm.$createElement('tr',
+              {
+                attrs: { class: 'nested' }
+              },
+              [ vm.$createElement('td', [ingredient.name], { key: ingredient.id }) ]
+            )
+          }
+        )
+    }
+
+    const wrapper = mount(Vue.component('test', {
+        render (h) {
+          return h(VDataTable, {
+            props: {
+              noWrappedExpandSlots: false,
+              itemKey: 'name',
+              items: this.items,
+              headers: this.headers
+            },
+            scopedSlots: {
+              items,
+              expand
+            }
+          })
+        },
+        data: noWrappedExpandSlotsData
+    }))
+
+
+    wrapper.find('tr.item')[0].element.click()
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('tr.nested')[0].element.parentElement.tagName).toBe('DIV')
+    expect('Unable to locate target [data-app]').toHaveBeenTipped()
+  })
 })
