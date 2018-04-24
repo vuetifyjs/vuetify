8d752b80df84b933f39ddc099efe6f575d812a59
Zohir Tamda <35851048+Dob6458@users.noreply.github.com>
2018-04-24 17:33:12 +0200

    fix(subheaders): add missing spacer to list example (#430)

--- packages/vuetifyjs.com/examples/subheaders/list.vue
+++ packages/vuetifyjs.com/examples/subheaders/list.vue
@@ -5,6 +5,7 @@
         <v-toolbar color="cyan" dark>
           <v-toolbar-side-icon></v-toolbar-side-icon>
           <v-toolbar-title>Inbox</v-toolbar-title>
+          <v-spacer></v-spacer>
           <v-btn icon>
             <v-icon>search</v-icon>
           </v-btn>
