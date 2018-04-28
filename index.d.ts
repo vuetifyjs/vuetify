2d69b1eb83100a502234af423231a8776be6f5ff
Kael <kaelwd@gmail.com>
2018-04-29 04:42:25 +1000

    feat(theme): allow a nonce to be specified for the stylesheet (#3952)
    * feat(theme): allow a nonce to be specified for the stylesheet
    
    resolves #3950
    
    * test: add test for nonce

--- index.d.ts
+++ index.d.ts
@@ -130,6 +130,8 @@ declare interface VuetifyOptions {
   themeVariations: string[]
   minifyTheme: ((css: string) => string) | null
   themeCache: VuetifyThemeCache | null
+  /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src#Unsafe_inline_script */
+  cspNonce: string | null
 }
 
 declare type VuetifyGoToEasing =
