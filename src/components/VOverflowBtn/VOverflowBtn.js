c15d0306230725262afad7980df04125ab3b749d
Kael <kaelwd@gmail.com>
2018-05-06 02:34:13 +1000

    feat: add button

--- src/components/VOverflowBtn/VOverflowBtn.js
+++ src/components/VOverflowBtn/VOverflowBtn.js
@@ -9,6 +9,9 @@ import VTextField from '../VTextField/VTextField'
 /* eslint-disable */
 import VBtn from '../VBtn'
 
+import { consoleWarn } from '../../util/console'
+import VSelect from '@/components/VSelect/VSelect'
+
 export default {
   name: 'v-overflow-btn',
 
@@ -35,7 +38,20 @@ export default {
   },
 
   methods: {
+    genSelections () {
+      // Override VAutocomplete's override
+      return VSelect.methods.genSelections.call(this)
+    },
+    genCommaSelection (item) {
+      if (this.segmented) {
+        return this.genSegmentedBtn(item)
+      } else {
+        return VAutocomplete.methods.genCommaSelection.call(this, item)
+      }
+    },
     genInput () {
+      // if (this.segmented) return this.$createElement()
+
       const input = VTextField.methods.genInput.call(this)
 
       input.data.domProps.value = this.internalSearch
@@ -45,10 +61,27 @@ export default {
     },
     genLabel () {
       const label = VTextField.methods.genLabel.call(this)
+      if (!label) return label
 
       delete label.data.style.position
 
       return label
-    }
+    },
+    genSegmentedBtn (item) {
+      if (!item.text || !item.callback) {
+        consoleWarn('When using \'segmented\' prop without a selection slot, items must contain both a text and callback property', this)
+        return null
+      }
+
+      return this.$createElement(VBtn, {
+        props: { flat: true },
+        on: {
+          click (e) {
+            e.stopPropagation()
+            item.callback(e)
+          }
+        }
+      }, [item.text])
+    },
   }
 }
