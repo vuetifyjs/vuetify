65341efee354ef0060bcf5d2f855dea3e00617e1
John Leider <john.j.leider@gmail.com>
2018-05-08 19:37:27 -0400

    feat(typography): add font weights

--- packages/vuetifyjs.com/pages/style/TypographyPage.vue
+++ packages/vuetifyjs.com/pages/style/TypographyPage.vue
@@ -1,74 +1,106 @@
 <template lang="pug">
   doc-view#typography-page
     template(slot-scope="{ namespace }")
-      section#examples
-        section-head(:value="`${namespace}.examplesHeader`")
-        div(class="component-example")
-          div(class="component-example__container")
-            dl(class="headings")
-              dd
-                div(class="display-4 grey--text text--darken-1") Light 112sp
-                strong .display-4
-                span font-weight 300
-              dd
-                div(class="display-3 grey--text text--darken-1") Regular 56sp
-                strong .display-3
-                span font-weight 400
-              dd
-                div(class="display-2 grey--text text--darken-1") Regular 45sp
-                strong .display-2
-                span font-weight 400
-              dd
-                div(class="display-1 grey--text text--darken-1") Regular 34sp
-                strong .display-1
-                span font-weight 400
-              dd
-                div(class="headline grey--text text--darken-1") Regular 24sp
-                strong .headline
-                span font-weight 400
-              dd
-                div(class="title grey--text text--darken-1") Medium 20sp
-                strong .title
-                span font-weight 500
-              dd
-                div(class="subheading grey--text text--darken-1") Regular 16sp
-                strong .subheading
-                span font-weight 400
-              dd
-                div(class="body-2 grey--text text--darken-1") Medium 14sp
-                strong .body-2
-                span font-weight 500
-              dd
-                div(class="body-1 grey--text text--darken-1") Regular 14sp
-                strong .body-1
-                span font-weight 400
-              dd
-                div(class="caption grey--text text--darken-1") Regular 12sp
-                strong .caption
-                span font-weight 400
+      section WIP
+      section#font-size
+        v-card(
+          color="teal"
+          dark
+          flat
+        ).pa-5.mb-3
+          v-layout(
+            v-for="(text, i) in texts"
+            :key="`${i}-text`"
+            align-center
+            justify-start
+          ).mb-3
+            div(style="min-width: 85px;").mr-3
+              code .{{ text[0] }}
+            v-flex(:class="`${text[0]}`") {{ text[1] }}
+
+      section#font-weights
+        v-card(
+          flat
+        ).pa-5
+          div(
+            v-for="(weight, i) in weights"
+            :key="`${i}-weight`"
+          ).mb-3
+            div(:class="`${weight[1]} my-2 display-2`") {{ weight[0] }}
+            kbd .{{ weight[1] }}
+          div(
+            v-for="(weight, i) in italics"
+            :key="`${i}-italic`"
+          ).mb-3
+            div(:class="`${weight[1]} my-2 display-2 font-italic`") {{ weight[0] }}
+            kbd .{{ weight[1] }}.font-italic
+
+      section#typeface.hidden-sm-and-down
+        v-card(
+          flat
+        ).pa-5
+          v-layout(
+            align-center
+            justify-space-between
+            wrap
+            v-for="(type, i) in typefaces"
+            :key="i"
+          ).my-3
+            v-flex(:class="type[1]" v-html="type[0]")
+            v-flex.hidden-md-and-down.text-xs-right.text-uppercase.grey--text {{ type[2] }}
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      texts: [
+        ['display-4', 'Light 112sp', '300'],
+        ['display-3', 'Regular 56sp', '400'],
+        ['display-2', 'Regular 45sp', '400'],
+        ['display-1', 'Regular 34sp', '400'],
+        ['headline', 'Regular 24sp', '400'],
+        ['title', 'Medium 20sp', '500'],
+        ['subheading', 'Regular 16sp', '400'],
+        ['body-2', 'Medium 14sp', '500'],
+        ['body-1', 'Regular 14sp', '400'],
+        ['caption', 'Regular 12sp', '400']
+      ],
+      weights: [
+        ['Roboto Thin', 'font-weight-thin', '100'],
+        ['Roboto Light', 'font-weight-light', '300'],
+        ['Roboto Regular', 'font-weight-regular', '400'],
+        ['Roboto Medium', 'font-weight-medium', '500'],
+        ['Roboto Bold', 'font-weight-bold', '700'],
+        ['Roboto Black', 'font-weight-black', '900']
+      ],
+      italics: [
+        ['Roboto Thin Italic', 'font-weight-thin', '100'],
+        ['Roboto Light Italic', 'font-weight-light', '300'],
+        ['Roboto Regular Italic', 'font-weight-regular', '400'],
+        ['Roboto Medium Italic', 'font-weight-medium', '500'],
+        ['Roboto Bold Italic', 'font-weight-bold', '700'],
+        ['Roboto Black Italic', 'font-weight-black', '900']
+      ],
+      typefaces: [
+        ['Quantum Mechanics', 'display-3', 'Regular'],
+        ['6.626069x10<sup class="display-2 font-weight-light mt-4">-34</sup>', 'd-flex align-start display-4 font-weight-thin', 'Thin'],
+        ['One hundred percent cotton bond', 'display-1 font-italic font-weight-bold', 'Bold italic'],
+        ['Quasiparticles', 'display-4 font-weight-bold', 'Bold'],
+        ['It became the non-relativistic limit of quantum field theory', 'title', 'Condensed'],
+        ['PAPERCRAFT', 'display-4 font-weight-light font-italic', 'Light italic'],
+        ['Probabilistic wave - particle wavefunction orbital path', 'title font-italic font-weight-medium', 'Medium italic'],
+        ['ENTANGLED', 'display-4 font-weight-black', 'Black'],
+        ['Cardstock 80lb ultra-bright orange', 'display-1 font-weight-medium', 'Medium'],
+        ['STATIONERY', 'display-4 font-weight-thin', 'Thin'],
+        ['POSITION, MOMENTUM & SPIN', 'display-2 font-weight-light', 'Condensed Light']
+      ]
+    })
+  }
+</script>
 
 <style lang="stylus">
   #typography-page
-    .component-example__container
-      > div, dl
-        width: 100%
-        &:after
-          content:""
-          display: table
-          clear: both
-
-      .headings
-        dt, dd
-          float: left
-
-        dd
-          margin: 0 0 2rem
-          clear: both
-
-          > div
-            margin-bottom: 16px
-
-          strong
-            margin-right: 16px
+    #typeface
+      div
+        word-break: break-all
 </style>
