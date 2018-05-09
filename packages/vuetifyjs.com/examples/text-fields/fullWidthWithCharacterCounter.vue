87b51f53dc5221e826faf490a85fd740b878a84a
John Leider <john.j.leider@gmail.com>
2018-05-08 21:12:46 -0400

    refactor(full-width): update example

--- packages/vuetifyjs.com/examples/text-fields/fullWidthWithCharacterCounter.vue
+++ packages/vuetifyjs.com/examples/text-fields/fullWidthWithCharacterCounter.vue
@@ -9,24 +9,19 @@
             <v-spacer></v-spacer>
             <v-icon color="white">send</v-icon>
           </v-toolbar>
-          <v-container fluid class="pa-0 mt-2">
-            <v-layout wrap>
-              <v-flex xs2>
-                <v-subheader>To</v-subheader>
-              </v-flex>
-              <v-flex xs10 class="text-xs-right">
-                <v-chip>
-                  <v-avatar>
-                    <img src="https://randomuser.me/api/portraits/men/92.jpg" >
-                  </v-avatar>
-                  Trevor Hansen
-                </v-chip>
-                <v-chip>
-                  <v-avatar>
-                    <img src="https://randomuser.me/api/portraits/men/91.jpg" >
-                  </v-avatar>
-                  Alex Nelson
-                </v-chip>
+          <v-container fluid class="pa-0">
+            <v-layout wrap align-center>
+              <v-flex xs12>
+                <v-autocomplete
+                  :items="['Trevor Handsen', 'Alex Nelson']"
+                  chips
+                  label="To"
+                  full-width
+                  hide-selected
+                  multiple
+                  single-line
+                >
+                </v-autocomplete>
               </v-flex>
               <v-flex xs12>
                 <v-divider></v-divider>
@@ -40,15 +35,14 @@
               </v-flex>
               <v-flex xs12>
                 <v-divider></v-divider>
-                <v-text-field
+                <v-textarea
                   v-model="title"
                   label="Message"
                   counter
                   max="120"
                   full-width
-                  multi-line
                   single-line
-                ></v-text-field>
+                ></v-textarea>
               </v-flex>
             </v-layout>
           </v-container>
