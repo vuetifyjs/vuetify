9ef5bb48179a4d0b5b021b156c27a97cfe82d36f
Kael <kaelwd@gmail.com>
2018-05-11 02:45:16 +1000

    chore(tests): return correct exit code when jest fails

--- build/run-tests.js
+++ build/run-tests.js
@@ -1,12 +1,16 @@
-"use strict"
-const spawn = require("child_process").spawn
+'use strict'
+const spawn = require('child_process').spawn
 
 const args = process.argv.slice(2)
 
+let child
+
 if (process.platform === 'win32') {
   // yarn test -i
-  spawn('yarn.cmd', ['test:win32', ...args], { stdio: 'inherit' })
+  child = spawn('yarn.cmd', ['test:win32', ...args], { stdio: 'inherit' })
 } else {
   // yarn test
-  spawn('yarn', ['test:unix', ...args], { stdio: 'inherit' })
+  child = spawn('yarn', ['test:unix', ...args], { stdio: 'inherit' })
 }
+
+child.on('exit', process.exit)
