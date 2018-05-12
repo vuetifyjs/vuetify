d98f6a94e53b00504fadc8a56952acdca27a790a
nekosaur <albert@kaaman.nu>
2018-05-12 14:17:09 +0200

    feat(v-timeline): added left and right props, and opposite slot

--- src/components/VTimeline/VTimelineItem.js
+++ src/components/VTimeline/VTimelineItem.js
@@ -13,7 +13,9 @@ export default {
   props: {
     icon: [Boolean, String],
     color: String,
-    iconColor: String
+    iconColor: String,
+    left: Boolean,
+    right: Boolean
   },
 
   methods: {
@@ -22,32 +24,43 @@ export default {
         staticClass: 'v-timeline-item__body'
       }, this.$slots.default)
     },
+    genIcon () {
+      return this.$createElement(VIcon, {
+        props: {
+          color: this.iconColor
+        }
+      }, this.icon)
+    },
     genBtn () {
       return this.$createElement(VBtn, {
+        staticClass: 'v-timeline-item__btn',
         props: {
           absolute: true,
           color: this.color,
-          fab: true,
-          relative: true
+          fab: true
         }
-      }, [
-        this.icon
-          ? this.$createElement(VIcon, {
-            props: {
-              color: this.iconColor
-            }
-          }, this.icon)
-          : null
-      ])
+      }, [this.icon ? this.genIcon() : null])
+    },
+    genOpposite () {
+      if (this.$slots.opposite) {
+        return this.$createElement('div', {
+          staticClass: 'v-timeline-item__opposite'
+        }, [this.$slots.opposite])
+      }
     }
   },
 
   render (h) {
     return h('div', {
-      staticClass: 'v-timeline-item'
+      staticClass: 'v-timeline-item',
+      class: {
+        'v-timeline-item--left': this.left,
+        'v-timeline-item--right': this.right
+      }
     }, [
       this.genBtn(),
-      this.genBody()
+      this.genBody(),
+      this.genOpposite()
     ])
   }
 }
