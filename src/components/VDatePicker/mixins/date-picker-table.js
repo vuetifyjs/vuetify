10c2ce82b7a8655403e2b67da8d54ca62b2dd17b
Jacek Karczmarczyk <jk@drukcd.pl>
2018-06-05 12:47:57 +0700

    fix(v-date-picker): disallow selecting disabled dates with keyboard
    fixes #4151

--- src/components/VDatePicker/mixins/date-picker-table.js
+++ src/components/VDatePicker/mixins/date-picker-table.js
@@ -60,17 +60,17 @@ export default {
   },
 
   methods: {
-    genButtonClasses (value, isDisabled, isFloating) {
+    genButtonClasses (value, isAllowed, isFloating) {
       const isSelected = value === this.value
       const isCurrent = value === this.current
 
       const classes = {
         'v-btn--active': isSelected,
         'v-btn--flat': !isSelected,
-        'v-btn--icon': isSelected && !isDisabled && isFloating,
+        'v-btn--icon': isSelected && isAllowed && isFloating,
         'v-btn--floating': isFloating,
         'v-btn--depressed': !isFloating && isSelected,
-        'v-btn--disabled': isDisabled || (this.disabled && isSelected),
+        'v-btn--disabled': !isAllowed || (this.disabled && isSelected),
         'v-btn--outline': isCurrent && !isSelected,
         ...this.themeClasses
       }
@@ -80,19 +80,19 @@ export default {
       return classes
     },
     genButton (value, isFloating) {
-      const isDisabled = !isDateAllowed(value, this.min, this.max, this.allowedDates)
+      const isAllowed = isDateAllowed(value, this.min, this.max, this.allowedDates)
 
       return this.$createElement('button', {
         staticClass: 'v-btn',
-        'class': this.genButtonClasses(value, isDisabled, isFloating),
+        'class': this.genButtonClasses(value, isAllowed, isFloating),
         attrs: {
           type: 'button'
         },
         domProps: {
-          disabled: isDisabled,
+          disabled: !isAllowed,
           innerHTML: `<div class="v-btn__content">${this.formatter(value)}</div>`
         },
-        on: isDisabled ? {} : {
+        on: (this.disabled || !isAllowed) ? {} : {
           click: () => this.$emit('input', value)
         }
       })
