5f119c2cb8de11305e0f48e64eca4ea75ecb653b
nekosaur <albert@kaaman.nu>
2018-06-18 11:01:15 +0200

    chore: fix merge errors

--- src/components/VIcon/VIcon.ts
+++ src/components/VIcon/VIcon.ts
@@ -5,7 +5,7 @@ import Colorable from '../../mixins/colorable'
 
 import {
   convertToUnit,
-  remapInternalIcon
+  getObjectValueByPath
 } from '../../util/helpers'
 
 import { VNode, VNodeChildren } from 'vue'
@@ -23,7 +23,29 @@ function isFontAwesome5 (iconType: string): boolean {
   return ['fas', 'far', 'fal', 'fab'].some(val => iconType.includes(val))
 }
 
-export default {
+const ICONS_PREFIX = '$vuetify.icons.'
+
+// This remaps internal names like '$vuetify.icons.cancel' to the current name
+// for that icon. Note the parent component is needed for $vuetify because
+// VIcon is a functional component. This function only looks at the
+// immediate parent, so it won't remap for a nested functional components.
+function remapInternalIcon (parent: object, iconName: string): string {
+  if (!iconName.startsWith(ICONS_PREFIX)) {
+    // return original icon name unchanged
+    return iconName
+  }
+
+  // Now look up icon indirection name, e.g. '$vuetify.icons.cancel':
+  return getObjectValueByPath(parent, iconName) || iconName
+}
+
+function keys<O> (o: O) {
+  return Object.keys(o) as (keyof O)[]
+}
+
+const addTextColorClassChecks = Colorable.options.methods.addTextColorClassChecks
+
+export default mixins(Colorable, Themeable).extend({
   name: 'v-icon',
 
   functional: true,
