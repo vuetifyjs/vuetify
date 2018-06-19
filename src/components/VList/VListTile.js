424999b716b35740a8429269111e52cf6baa9e58
Ilian Iliev <ilieff91@gmail.com>
2018-06-19 08:22:47 +0300

    feat(v-list-tile): add hover prop (#4330)

--- src/components/VList/VListTile.js
+++ src/components/VList/VListTile.js
@@ -32,6 +32,7 @@ export default {
     },
     avatar: Boolean,
     inactive: Boolean,
+    hover: Boolean,
     tag: String
   },
 
@@ -49,6 +50,7 @@ export default {
         'v-list__tile--link': this.isLink && !this.inactive,
         'v-list__tile--avatar': this.avatar,
         'v-list__tile--disabled': this.disabled,
+        'v-list__tile--hover': this.hover && !this.isLink && !this.inactive,
         'v-list__tile--active': !this.to && this.isActive,
         [this.activeClass]: this.isActive
       }
