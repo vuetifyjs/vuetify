88e0ead6cc80a7921629dffea975268b08cf7728
Jacek Karczmarczyk <jkarczm@gmail.com>
2018-06-20 23:00:53 +0700

    fix(VCard, converToUnit): set proper styles with falsy height prop (#4348)
    fixes #4344

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -221,10 +221,14 @@ export function filterChildren (array: VNode[] = [], tag: string): VNode[] {
   })
 }
 
-export function convertToUnit (str: string | number, unit = 'px'): string {
-  return isNaN(+str)
-    ? str as string // TODO: this is wrong but I cbf fixing it
-    : `${Number(str)}${unit}`
+export function convertToUnit (str: string | number | null | undefined, unit = 'px'): string | undefined {
+  if (str == null || str === '') {
+    return undefined
+  } else if (isNaN(+str!)) {
+    return String(str)
+  } else {
+    return `${Number(str)}${unit}`
+  }
 }
 
 export function kebabCase (str: string): string {
--- test/unit/util/helpers.spec.js
+++ test/unit/util/helpers.spec.js
@@ -1,5 +1,10 @@
 import { test } from '@/test'
-import { deepEqual, getNestedValue, getPropertyFromItem } from '@/util/helpers'
+import {
+  deepEqual,
+  getNestedValue,
+  getPropertyFromItem,
+  convertToUnit
+} from '@/util/helpers'
 
 test('helpers.js', () => {
   it('should pass comparison', () => {
@@ -147,5 +152,25 @@ test('helpers.js', () => {
     expect(getPropertyFromItem(obj, 'x.y')).toEqual('nested')
     expect(getPropertyFromItem(obj, ['x', 'y'])).toEqual('nested')
     expect(getPropertyFromItem(obj, ['x.y'])).toEqual('comp')
+  })
+
+  it('should return proper value in convertToUnit', () => {
+    expect(convertToUnit(undefined)).toBe(undefined)
+    expect(convertToUnit(null)).toBe(undefined)
+    expect(convertToUnit('')).toBe(undefined)
+
+    expect(convertToUnit(0)).toBe('0px')
+    expect(convertToUnit(3)).toBe('3px')
+    expect(convertToUnit(3.14)).toBe('3.14px')
+
+    expect(convertToUnit(0, 'em')).toBe('0em')
+    expect(convertToUnit(3, 'em')).toBe('3em')
+    expect(convertToUnit(3.14, 'em')).toBe('3.14em')
+
+    expect(convertToUnit('0vw')).toBe('0vw')
+    expect(convertToUnit('3vw')).toBe('3vw')
+    expect(convertToUnit('3.14vw')).toBe('3.14vw')
+
+    expect(convertToUnit('foo')).toBe('foo')
   })
 })
