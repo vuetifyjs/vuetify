4c4d975cdbf0977410a0b4dc7b1dd25c2021db16
nekosaur <albert@kaaman.nu>
2018-06-25 16:20:49 +0200

    refactor: table progress

--- src/components/VDataTable/VTableProgress.ts
+++ src/components/VDataTable/VTableProgress.ts
@@ -1,4 +1,4 @@
-import { VNode, CreateElement } from 'vue'
+import Vue, { VNode, CreateElement, ComponentOptions } from 'vue'
 import { injectOne } from '../../util/inject'
 import { DataTableProvide } from './VDataTable'
 
@@ -9,16 +9,22 @@ export default injectOne<DataTableProvide>()('dataTable').extend({
   name: 'v-table-progress',
 
   render (h: CreateElement): VNode {
+    const children = []
+
+    if (this.dataTable.loading) {
+      children.push(h(VRow, [
+        h(VProgressLinear as ComponentOptions<Vue>, {
+          props: {
+            height: 2,
+            active: true,
+            indeterminate: true
+          }
+        })
+      ]))
+    }
+
     return h('div', {
       staticClass: 'thead v-data-table__progress'
-    }, [this.dataTable.loading && h(VRow, [
-      h((VProgressLinear as any), { // TODO: change when converted
-        props: {
-          height: 2,
-          active: true,
-          indeterminate: true
-        }
-      })
-    ])])
+    }, children)
   }
 })
