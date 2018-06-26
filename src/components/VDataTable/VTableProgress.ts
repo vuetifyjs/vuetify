34fc50f75a95997def27f1c909afcfd32ec02757
nekosaur <albert@kaaman.nu>
2018-06-26 17:01:17 +0200

    feat: added height prop to table-progress

--- src/components/VDataTable/VTableProgress.ts
+++ src/components/VDataTable/VTableProgress.ts
@@ -1,4 +1,4 @@
-import Vue, { VNode, CreateElement, ComponentOptions } from 'vue'
+import Vue, { VNode, ComponentOptions } from 'vue'
 import { injectOne } from '../../util/inject'
 import { DataTableProvide } from './VDataTable'
 
@@ -8,14 +8,21 @@ import VRow from './VRow'
 export default injectOne<DataTableProvide>()('dataTable').extend({
   name: 'v-table-progress',
 
-  render (h: CreateElement): VNode {
+  props: {
+    height: {
+      type: Number,
+      default: 2
+    }
+  },
+
+  render (h): VNode {
     const children = []
 
     if (this.dataTable.loading) {
       children.push(h(VRow, [
         h(VProgressLinear as ComponentOptions<Vue>, {
           props: {
-            height: 2,
+            height: this.height,
             active: true,
             indeterminate: true
           }
