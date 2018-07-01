5149e9d4838ddbb9d3c45b81e6c876aa2bd89d45
John Leider <john.j.leider@gmail.com>
2018-07-01 16:33:17 -0400

    feat(dynamic-async): make components dynamic and async

--- packages/vuetifyjs.com/src/components/index.js
+++ packages/vuetifyjs.com/src/components/index.js
@@ -6,12 +6,17 @@ const requireComponent = require.context(
   '@/components', true, /\.vue$/
 )
 
+// Dynamically load all components
+// and lazily load them
+// https://vuejs.org/v2/guide/components-dynamic-async
 requireComponent.keys().forEach(fileName => {
-  const componentConfig = requireComponent(fileName)
-
   const componentName = upperFirst(
     camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, ''))
   )
 
-  Vue.component(componentName, componentConfig.default || componentConfig)
+  Vue.component(componentName, resolve => {
+    const componentConfig = requireComponent(fileName)
+
+    resolve(componentConfig.default || componentConfig)
+  })
 })
