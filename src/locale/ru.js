aa916253a22c3e7b7002e3c3a4609f8801537712
Илья <iliya-zelenko@mail.ru>
2018-07-01 04:37:09 +0300

    ru language

---
+++ src/locale/ru.js
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Элементов на странице:',
+    rowsPerPageAll: 'Все',
+    pageText: '{0}-{1} из {2}',
+    noResultsText: 'В результате поиска ничего не найдено',
+    nextPage: 'Следующая страница',
+    prevPage: 'Предыдущая страница'
+  },
+  dataTable: {
+    rowsPerPageText: 'Строк на странице:'
+  },
+  noDataText: 'Нет данных для отображения'
+}
