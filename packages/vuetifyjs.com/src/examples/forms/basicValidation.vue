a56be72c549798594aaa6a87b67381e72f3ca7ec
John Leider <john.j.leider@gmail.com>
2018-07-03 17:46:24 -0400

    fix(forms): simplify regexp
    whatever was there before was apparently wrong
    
    fixes #512

--- packages/vuetifyjs.com/src/examples/forms/basicValidation.vue
+++ packages/vuetifyjs.com/src/examples/forms/basicValidation.vue
@@ -28,7 +28,7 @@
       email: '',
       emailRules: [
         v => !!v || 'E-mail is required',
-        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
       ]
     })
   }
