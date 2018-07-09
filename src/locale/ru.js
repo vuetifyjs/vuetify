5c5dd95ad54a6b5f8018b7ec5a5d1bcabdc9e728
Kirill Romanov <djaler1@gmail.com>
2018-07-09 08:48:13 +0300

    feat: add russian locale (#4425)

---
+++ src/locale/ru.js
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Записей на странице:',
+    rowsPerPageAll: 'Все',
+    pageText: '{0}-{1} из {2}',
+    noResultsText: 'Не найдено подходящих записей',
+    nextPage: 'Следующая страница',
+    prevPage: 'Предыдущая страница'
+  },
+  dataTable: {
+    rowsPerPageText: 'Строк на странице:'
+  },
+  noDataText: 'Отсутствуют данные'
+}
