1767399be4d26f034032f421f054c0857fcb935d
Jacek Karczmarczyk <jk@drukcd.pl>
2018-07-10 18:58:49 +0200

    chore(createSimpleFunctional): cleanup

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -5,14 +5,9 @@ export function createSimpleFunctional (
   el = 'div',
   name?: string
 ): FunctionalComponentOptions {
-  name = name || c.replace(/__/g, '-')
-
-  // TODO: remove after close
-  // https://github.com/vuetifyjs/vuetify/issues/1561
-  name = name.split('-')[0] === 'v' ? name : `v-${name}`
-
   return {
-    name,
+    name: name || c.replace(/__/g, '-'),
+
     functional: true,
 
     render (h, { data, children }): VNode {
