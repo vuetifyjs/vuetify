b1d9053577eb64cfd947a222394ce2b02099b1e5
Enric Bisbe Gil <ebisbe@users.noreply.github.com>
2018-07-20 16:41:05 +0200

    Add rate this album to cards (#532)
    * Add rate this album to cards
    
    * Fix styles
    
    * Update styles
    
    * Lint files

--- packages/vuetifyjs.com/src/examples/cards/horizontal.vue
+++ packages/vuetifyjs.com/src/examples/cards/horizontal.vue
@@ -36,45 +36,67 @@
 
           <v-flex xs12>
             <v-card color="cyan darken-2" class="white--text">
-              <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs7>
+              <v-layout>
+                <v-flex xs5>
+                  <v-card-media
+                    src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+                    height="125px"
+                    contain
+                  ></v-card-media>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
                     <div>
                       <div class="headline">Supermodel</div>
                       <div>Foster the People</div>
+                      <div>(2014)</div>
                     </div>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-card-media
-                      src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-                      height="125px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                </v-layout>
-              </v-container>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-3">
+                Rate this album
+                <v-spacer></v-spacer>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+              </v-card-actions>
             </v-card>
           </v-flex>
 
           <v-flex xs12>
             <v-card color="purple" class="white--text">
-              <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs7>
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
                     <div>
                       <div class="headline">Halycon Days</div>
                       <div>Ellie Goulding</div>
+                      <div>(2013)</div>
                     </div>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-card-media
-                      src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-                      height="125px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                </v-layout>
-              </v-container>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-card-media
+                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                    height="125px"
+                    contain
+                  ></v-card-media>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-3">
+                Rate this album
+                <v-spacer></v-spacer>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+              </v-card-actions>
             </v-card>
           </v-flex>
         </v-layout>
