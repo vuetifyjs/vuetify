2fb2fb614b06bfbed50b0aa14cf8a96d4378c45c
Le_Futuriste <spamfree@matthieubessat.fr>
2018-07-23 15:08:03 +0200

    feat: add french locale (#4685)

---
+++ src/locale/fr.js
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Objet par page :',
+    rowsPerPageAll: 'Tout',
+    pageText: '{0}-{1} de {2}',
+    noResultsText: "Pas d'enregistrements corespondant trouvé",
+    nextPage: 'Page suivante',
+    prevPage: 'Page précédente'
+  },
+  dataTable: {
+    rowsPerPageText: 'Ligne par page :'
+  },
+  noDataText: 'Aucune données disponible'
+}
