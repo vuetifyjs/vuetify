1fda045231e2e2c74bc80a758d9cf815661a8714
Enric Bisbe Gil <enricu@gmail.com>
2018-07-27 07:32:00 +0200

    Remove unnecesary conditional

--- src/components/VList/VListTileAction.js
+++ src/components/VList/VListTileAction.js
@@ -7,7 +7,7 @@ export default {
   render (h, { data, children }) {
     data.staticClass = data.staticClass ? `v-list__tile__action ${data.staticClass}` : 'v-list__tile__action'
     const filteredChild = children.filter(VNode => VNode.isComment === false)
-    if ((filteredChild || []).length > 1) data.staticClass += ' v-list__tile__action--stack'
+    if (filteredChild.length > 1) data.staticClass += ' v-list__tile__action--stack'
 
     return h('div', data, children)
   }
