fbc5a35497bc37ab93fbcc7463eab5fcda44ad15
Enric Bisbe Gil <enricu@gmail.com>
2018-07-28 10:01:23 +0200

    Filter out VNodes with empty text

--- src/components/VList/VListTileAction.js
+++ src/components/VList/VListTileAction.js
@@ -6,7 +6,9 @@ export default {
 
   render (h, { data, children }) {
     data.staticClass = data.staticClass ? `v-list__tile__action ${data.staticClass}` : 'v-list__tile__action'
-    const filteredChild = children.filter(VNode => VNode.isComment === false)
+    const filteredChild = children.filter(VNode => {
+      return VNode.isComment === false && VNode.text !== ' '
+    })
     if (filteredChild.length > 1) data.staticClass += ' v-list__tile__action--stack'
 
     return h('div', data, children)
