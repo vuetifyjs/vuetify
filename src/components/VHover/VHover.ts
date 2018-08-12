6eb7b33866df9dc96bce4d687d91357847f501cf
Kael <kaelwd@gmail.com>
2018-08-13 02:53:30 +1000

    fix: check for multiple children, override incorrect types

--- src/components/VHover/VHover.ts
+++ src/components/VHover/VHover.ts
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue, { VNode } from 'vue'
 import { consoleWarn } from '../../util/console'
 
 export default Vue.extend({
@@ -8,17 +8,24 @@ export default Vue.extend({
 
   render (): any {
     if (!this.$scopedSlots.default) {
-      consoleWarn('v-hover is missing a default scopedSlot')
+      consoleWarn('v-hover is missing a default scopedSlot', this)
 
       return null
     }
 
-    const element = this.$scopedSlots.default({ hover: this.hover })
+    // TODO: types are wrong - https://github.com/vuejs/vue/pull/8644
+    const element = this.$scopedSlots.default({ hover: this.hover }) as any as VNode
 
-    element.data.on = element.data.on || {}
+    if (typeof element === 'string' || Array.isArray(element)) {
+      consoleWarn('v-hover should only contain a single element', this)
 
-    element.data.on.mouseenter = () => (this.hover = true)
-    element.data.on.mouseleave = () => (this.hover = false)
+      return null
+    }
+
+    element.data!.on = element.data!.on || {}
+
+    element.data!.on!.mouseenter = () => (this.hover = true)
+    element.data!.on!.mouseleave = () => (this.hover = false)
 
     return element
   }
