8a4b59d21b1e5853094a14cf46940c822387547b
John Leider <john.j.leider@gmail.com>
2018-08-13 14:48:22 -0400

    feat(transitions): spring cleaning
    added 4 new transitions, added default css transition for generic transitions, added transition
    helpers

--- src/components/transitions/index.js
+++ src/components/transitions/index.js
@@ -19,6 +19,10 @@ export const VDialogTransition = createSimpleTransition('dialog-transition')
 export const VDialogBottomTransition = createSimpleTransition('dialog-bottom-transition')
 export const VFadeTransition = createSimpleTransition('fade-transition')
 export const VScaleTransition = createSimpleTransition('scale-transition')
+export const VScrollXTransition = createSimpleTransition('scroll-x-transition')
+export const VScrollXReverseTransition = createSimpleTransition('scroll-x-reverse-transition')
+export const VScrollYTransition = createSimpleTransition('scroll-y-transition')
+export const VScrollYReverseTransition = createSimpleTransition('scroll-y-reverse-transition')
 export const VSlideXTransition = createSimpleTransition('slide-x-transition')
 export const VSlideXReverseTransition = createSimpleTransition('slide-x-reverse-transition')
 export const VSlideYTransition = createSimpleTransition('slide-y-transition')
@@ -39,6 +43,10 @@ export default {
     VFadeTransition,
     VMenuTransition,
     VScaleTransition,
+    VScrollXTransition,
+    VScrollXReverseTransition,
+    VScrollYTransition,
+    VScrollYReverseTransition,
     VSlideXTransition,
     VSlideXReverseTransition,
     VSlideYTransition,
