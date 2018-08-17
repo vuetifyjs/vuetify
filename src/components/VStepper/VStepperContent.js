67b3c976bb07aaf4f63a525681d85f27e6515560
bfis <b.fis@cern.ch>
2018-08-17 06:39:32 +0200

    fix(v-stepper-content): visibility desync (#4859)
    * fix(v-stepper-content): visibility desync
    
    * updated tests
    
    * fixed tests
    
    needs a booting sequence as the first `this.isActive = true` behaves differently
    
    * fixed tests
    
    `enter` & `leave` are only used when `isVertical` is set
    
    * fixed tests
    
    need to wait for the `watch` to be triggered
    
    * fixed tests
    
    `isVertical` is not prop but data

--- src/components/VStepper/VStepperContent.js
+++ src/components/VStepper/VStepperContent.js
@@ -103,7 +103,7 @@ export default {
       this.height = 0
 
       // Give the collapsing element time to collapse
-      setTimeout(() => (this.height = (scrollHeight || 'auto')), 450)
+      setTimeout(() => this.isActive && (this.height = (scrollHeight || 'auto')), 450)
     },
     leave () {
       this.height = this.$refs.wrapper.clientHeight
