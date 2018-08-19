219e0d2fb86000775e53af4a381f22411bd661d4
DCzajkowski <dare.czajkowski@gmail.com>
2018-08-19 21:32:58 +0200

    Allow for more customisability - New slot for the VDataTable (#4691)
    * Allow for more customisability - New slot for the VDataTable
    
    * Changed name of the slot and added a new one
    
    * Move slots around

--- src/mixins/data-iterable.js
+++ src/mixins/data-iterable.js
@@ -471,8 +471,10 @@ export default {
       return [this.$createElement('div', {
         'class': this.actionsClasses
       }, [
+        this.$slots['actions-prepend'] ? this.$createElement('div', {}, this.$slots['actions-prepend']) : null,
         this.rowsPerPageItems.length > 1 ? this.genSelect() : null,
-        rangeControls
+        rangeControls,
+        this.$slots['actions-append'] ? this.$createElement('div', {}, this.$slots['actions-append']) : null
       ])]
     }
   }
