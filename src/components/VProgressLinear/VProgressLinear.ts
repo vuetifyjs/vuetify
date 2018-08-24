8d5ee25d7d3a79ed072176a4b1b22c46feacaebd
Adam Kaput <adamkaput@gmail.com>
2018-08-25 01:49:54 +0200

    fix(VProgressCircular): add a11y to circular progressbars fix(VProgressLinear): add a11y to linear progressbars update snapshots

--- src/components/VProgressLinear/VProgressLinear.ts
+++ src/components/VProgressLinear/VProgressLinear.ts
@@ -134,6 +134,12 @@ export default mixins(Colorable).extend({
 
     return h('div', {
       staticClass: 'v-progress-linear',
+      attrs: {
+        'role': 'progressbar',
+        'aria-valuemin': 0,
+        'aria-valuemax': this.bufferValue,
+        'aria-valuenow': this.indeterminate ? undefined : this.value
+      },
       class: {
         'v-progress-linear--query': this.query
       },
