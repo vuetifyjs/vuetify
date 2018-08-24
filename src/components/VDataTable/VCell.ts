fbad45e5dc4fd2797b15af2471d6473127ec2485
nekosaur <albert@kaaman.nu>
2018-08-24 14:17:45 +0000

    refactor: v-cell rendering

--- src/components/VDataTable/VCell.ts
+++ src/components/VDataTable/VCell.ts
@@ -14,11 +14,14 @@ export default {
   render (h: CreateElement, context: any): VNode {
     const { data, slots, props } = context
     const resolvedSlots = slots()
-    const staticClass = `${props.head ? 'v-head' : 'v-cell'} ${data.staticClass || ''}`
 
     return h('div', {
-      staticClass,
-      class: data.class,
+      staticClass: data.staticClass,
+      class: {
+        'v-head': props.head,
+        'v-cell': !props.head,
+        ...data.class
+      },
       on: data.nativeOn
     }, resolvedSlots.default)
   }
