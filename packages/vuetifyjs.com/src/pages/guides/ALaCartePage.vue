3e6bcd1fa79ce3cffb9d65b7eb76b23e8617d323
Kael <kaelwd@gmail.com>
2018-10-11 17:04:21 +1100

    feat(alacarte): update guide for 1.3 and vuetify-loader

--- packages/vuetifyjs.com/src/pages/guides/ALaCartePage.vue
+++ packages/vuetifyjs.com/src/pages/guides/ALaCartePage.vue
@@ -2,229 +2,88 @@
   views-doc
     helpers-section-text(slot="sup" :value="`Guides.ALaCarte.headerText2`")
     template(slot-scope="{ namespace }")
-      textarea(
-        :style="{ position: 'absolute', left: '-1000px', top: '-1000px' }"
-        :value="copy"
-        ref="copy"
-      )
       section#importing-components
         helpers-section-head(:value="`${namespace}.importHeader`")
         helpers-section-text(:value="`${namespace}.importText1`")
-        helpers-markup(lang="js")
-          |// .babelrc
-          |["transform-imports", {
-          |  "vuetify": {
-          |    "transform": "vuetify/es5/components/${member}",
-          |    "preventFullImport": true
-          |  }
-          |}]
-        core-alert(:value="`${namespace}.alert2`" error)
-        helpers-markup(lang="js")
-          |// main.js
-          |import Vue from 'vue'
-          |import App from './App.vue'
-          |import {
-          |  Vuetify, // required
-          |  VApp, // required
-          |  VNavigationDrawer,
-          |  VFooter,
-          |  VToolbar,
-          |  transitions
-          |} from 'vuetify'
-          |import { Ripple } from 'vuetify/es5/directives'
-          |
-          |Vue.use(Vuetify, {
-          |   components: {
-          |     VApp,
-          |     VNavigationDrawer,
-          |     VFooter,
-          |     VToolbar,
-          |     transitions
-          |   },
-          |   directives: {
-          |     Ripple
-          |   }
-          |})
-        core-alert(:value="`${namespace}.alert3`" info)
-        helpers-section-text(:value="`${namespace}.importText2`")
-        helpers-markup(lang="js")
-          |// main.js
-          |// Without `transform-imports` package
-          |import Vue from 'vue'
-          |import VApp from 'vuetify/es5/components/VApp'
-          |import Vuetify from 'vuetify/es5/components/Vuetify'
-          |import transitions from 'vuetify/es5/components/transitions'
-          |import directives from 'vuetify/es5/directives'
-          |
-          |Vue.use(Vuetify, {
-          |   components: {
-          |      VApp,
-          |      transitions
-          |   },
-          |   directives
-          |})
-        helpers-section-text(:value="`${namespace}.importText3`")
-        helpers-markup(lang="js")
-          |// .vue files
-          |import * as VCard from 'vuetify/es5/components/VCard'
-          |
-          |export default {
-          |  components: {
-          |    ...VCard
-          |  }
-          |}
+        core-alert(:value="`${namespace}.webpackVersionWarning`" warning).text--primary
+        helpers-markup(lang="js").
+          // main.js
+          import Vue from 'vue'
+          import App from './App.vue'
+          import Vuetify, {
+            VApp, // required
+            VNavigationDrawer,
+            VFooter,
+            VToolbar,
+            transitions
+          } from 'vuetify/lib'
+          import { Ripple } from 'vuetify/lib/directives'
 
+          Vue.use(Vuetify, {
+             components: {
+               VApp,
+               VNavigationDrawer,
+               VFooter,
+               VToolbar,
+               transitions
+             },
+             directives: {
+               Ripple
+             }
+          })
+        core-alert(:value="`${namespace}.optionsDirectivesNotice`" info)
+        helpers-section-text(:value="`${namespace}.importText2`")
+        helpers-markup(lang="js").
+          // .vue files
+          import { VCard, VCardText, VCardMedia } from 'vuetify/lib'
+
+          export default {
+            components: {
+              VCard,
+              VCardText,
+              VCardMedia
+            }
+          }
       section#required-styles
         helpers-section-head(:value="`${namespace}.styleHeader`")
         helpers-section-text(:value="`${namespace}.styleText1`")
-        helpers-markup(lang="cli")
-          |$ npm install --save-dev stylus stylus-loader
-          |# or
-          |$ yarn add --dev stylus stylus-loader
+        helpers-markup(lang="cli").
+          $ npm install --save-dev stylus stylus-loader
+          # or
+          $ yarn add --dev stylus stylus-loader
         helpers-section-text(:value="`${namespace}.styleText2`")
-        helpers-markup(lang="js")
-          |// src/main.js
-          |require('vuetify/src/stylus/app.styl')
+        helpers-markup(lang="js").
+          // src/main.js
+          import 'vuetify/src/stylus/app.styl'
+        core-alert(:value="`${namespace}.stylusDetailNotice`" info)
 
-      section#ui-component-name-list
-        helpers-section-head(:value="`${namespace}.componentNameListHeader`")
-        helpers-section-text(:value="`${namespace}.componentNameListText1`")
-        v-card
-          v-card-title
-            v-spacer
-            v-text-field(
-              append-icon="search"
-              label="Search"
-              single-line
-              hide-details
-              v-model="search"
-            )
-          v-data-table(
-              :headers="headers"
-              :items="items"
-              :customFilter="customFilter"
-              :search="search"
-              v-model="selected"
-              item-key="name"
-              select-all
-              :rows-per-page-items="[10, 20, {text: '$vuetify.dataIterator.rowsPerPageAll', value: -1}]"
-              class="elevation-1"
-            )
-            template(slot="headerCell" slot-scope="props")
-              v-tooltip(lazy bottom)
-                span(slot="activator") {{ props.header.text }}
-                span {{ props.header.text }}
-            template(slot="items" slot-scope="props")
-              td
-                v-checkbox(
-                  primary
-                  hide-details
-                  v-model="props.selected"
-                )
-              td(class="text-xs-right")
-                code(v-if="props.item.name !== 'directives'") <{{ props.item.name }}>
-              td(class="text-xs-right") {{ props.item.component }}
-              td(class="text-xs-right") {{ props.item.group }}
-            template(slot="footer")
-              td(colspan="4")
-                v-layout(align-center)
-                  div
-                    v-switch(
-                      label="ES5"
-                      v-model="es5"
-                      hide-details
-                    )
-                  v-flex
-                    v-btn(
-                      color="primary"
-                      flat
-                      @click="copyMarkup"
-                      slot="activator"
-                    )
-                      | Copy markup
-                      v-icon(right) content_copy
+      section#vuetify-loader
+        helpers-section-head(:value="`${namespace}.loaderHeader`")
+        helpers-section-text(:value="`${namespace}.loaderText1`")
+        helpers-markup(lang="js").
+          // You still need to register Vuetify itself
+          import Vue from 'vue'
+          import Vuetify from 'vuetify/lib'
+          import 'vuetify/src/stylus/app.styl'
+
+          Vue.use(Vuetify)
+        helpers-markup(lang="js").
+          // webpack.config.js
+          const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+          module.exports = {
+            plugins: [
+              new VuetifyLoaderPlugin()
+            ]
+          }
+
+          // or vue.config.js
+          const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+          module.exports = {
+            configureWebpack: {
+              plugins: [
+                new VuetifyLoaderPlugin()
+              ]
+            }
+          }
+        helpers-section-text(:value="`${namespace}.loaderText2`")
 </template>
-
-<script>
-  import alacarteComponents from '@/util/alacarteComponents'
-
-  export default {
-    data () {
-      return {
-        search: '',
-        pagination: {},
-        selected: [{ name: 'v-app', component: 'VApp', group: 'VApp' }],
-        headers: [
-          { text: 'Markup', value: 'name' },
-          { text: 'Component', value: 'component' },
-          { text: 'Group', value: 'group' }
-        ],
-        es5: false,
-        // component list.
-        items: alacarteComponents()
-      }
-    },
-    computed: {
-      copy () {
-        const components = `components: {\n` + this.componentSelection.filter(name => name !== 'directives').map(name => `    ${name}`).join(',\n') + '\n  }'
-        const hasDirectives = this.componentSelection.includes('directives')
-        const directivesImport = hasDirectives ? `import directives from 'vuetify/es5/directives';\n` : ''
-        const items = (hasDirectives ? [components, 'directives'] : [components]).map(v => `  ${v}`).join(',\n')
-        const use = `Vue.use(Vuetify, {\n${items}\n});`
-        const imports = this.es5 ? this.es5Imports : this.es6Imports
-        return `import Vue from 'vue';\n${imports}\n${directivesImport}\n${use}`
-      },
-      es5Imports () {
-        const imports = this.componentSelection
-          .filter(name => name !== 'directives')
-          .map(name => `import ${name} from 'vuetify/es5/components/${name}';`).join('\n')
-
-        return `import Vuetify from 'vuetify/es5/components/Vuetify';\n${imports}`
-      },
-      es6Imports () {
-        const imports = this.componentSelection
-          .filter(name => name !== 'directives')
-          .map(name => `  ${name}`)
-          .join(',\n')
-
-        return `import {\n  Vuetify,\n${imports}\n} from 'vuetify';`
-      },
-      isGroup () {
-        const isGroup = {}
-        this.items.forEach(({ group }) => (isGroup[group] = (group in isGroup) ? isGroup[group] + 1 : 0))
-        return isGroup
-      },
-      isVAppSelected () {
-        return !!this.selected.find(item => item.name === 'v-app')
-      },
-      componentSelection () {
-        const names = [...new Set(this.selected.map(({group}) => group))]
-        names.sort((a, b) => {
-          a = a === 'VApp' ? '' : a
-          b = b === 'VApp' ? '' : b
-          return a < b ? -1 : a > b
-        })
-        return names
-      }
-    },
-    watch: {
-      isVAppSelected () {
-        this.isVAppSelected || alert('VApp component is required, removing it may cause your application not working properly!')
-      }
-    },
-    methods: {
-      customFilter (items, search, filter) {
-        if (!search) return items
-        search = search.toString().toLowerCase()
-
-        return items.filter(item => (
-          filter(item['name'], search) || filter(item['component'], search) || filter(item['group'], search)
-        ))
-      },
-      copyMarkup () {
-        this.$refs.copy.select()
-        document.execCommand('copy')
-      }
-    }
-  }
-</script>
