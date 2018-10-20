d812643802375170877dcb9dabae3efbb8c8bcd7
d0whc3r <d0whc3r@gmail.com>
2018-10-20 17:54:02 +0200

    feat: add catalan locale

---
+++ src/locale/ca.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Elements per pàgina:',
+    rowsPerPageAll: 'Tots',
+    pageText: '{0}-{1} de {2}',
+    noResultsText: 'No s\'ha trobat cap registre coincident',
+    nextPage: 'Pàgina següent',
+    prevPage: 'Pàgina anterior'
+  },
+  dataTable: {
+    rowsPerPageText: 'Files per pàgina:'
+  },
+  noDataText: 'No hi ha dades disponibles'
+}
