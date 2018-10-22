1933918719c3389b84765385281a3934cafc4f8a
Werner Mollentze <wernerm@users.noreply.github.com>
2018-10-22 20:24:10 +0200

    Added Afrikaans locale

---
+++ src/locale/af.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Aantal per bladsy:',
+    rowsPerPageAll: 'Alles',
+    pageText: '{0}-{1} van {2}',
+    noResultsText: 'Geen ooreenstemmende resultate is gevind nie',
+    nextPage: 'Volgende bladsy',
+    prevPage: 'Vorige bladsy'
+  },
+  dataTable: {
+    rowsPerPageText: 'Rye per bladsy:'
+  },
+  noDataText: 'Geen data is beskikbaar nie'
+}
