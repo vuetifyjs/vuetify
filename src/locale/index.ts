5798670ee92110ddfdac495856ad2ed816d00d38
Jacek Karczmarczyk <jk@drukcd.pl>
2018-10-25 15:51:37 +0700

    feat: index for src/locales

---
+++ src/locale/index.ts
@@ -0,0 +1,34 @@
+import de from './de'
+import el from './el'
+import en from './en'
+import fa from './fa'
+import fr from './fr'
+import hu from './hu'
+import it from './it'
+import nl from './nl'
+import pl from './pl'
+import pt from './pt'
+import ru from './ru'
+import srCyrl from './sr-Cyrl'
+import uk from './uk'
+import zhHans from './zh-Hans'
+import zhHant from './zh-Hant'
+
+export default {
+  de,
+  el,
+  en,
+  fa,
+  fr,
+  hu,
+  it,
+  nl,
+  pl,
+  pt,
+  ru,
+  srCyrl,
+  uk,
+  zhHans,
+  zhHant
+}
+
