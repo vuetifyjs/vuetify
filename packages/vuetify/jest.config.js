510fe324754abd032654e2dbf5bc9054dd9c2288
John Leider <john.j.leider@gmail.com>
2018-10-27 11:39:38 -0400

    fix(jest): configure from base

--- packages/vuetify/jest.config.js
+++ packages/vuetify/jest.config.js
@@ -1,38 +1,7 @@
+const base = require('../../jest.config')
+
 module.exports = {
-  verbose: false,
-  roots: [
-    '<rootDir>/test/unit',
-    '<rootDir>/test/integration'
-  ],
-  moduleFileExtensions: [
-    'ts',
-    'js'
-  ],
-  moduleDirectories: [
-    'node_modules'
-  ],
-  moduleNameMapper: {
-    '^@/test$': '<rootDir>/test/index.js',
-    '^@/test/(.*)$': '<rootDir>/test/$1',
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  transform: {
-    '\\.(styl)$': 'jest-css-modules',
-    '.*\\.(j|t)s$': 'ts-jest'
-  },
-  mapCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{js,ts,tsx}'
-  ],
-  transformIgnorePatterns: [
-    'node_modules/(?!vue-router)'
-  ],
-  snapshotSerializers: [
-    'jest-serializer-html'
-  ],
-  globals: {
-    'ts-jest': {
-      'useBabelrc': true
-    }
-  }
+  ...base,
+  name: 'Vuetify',
+  displayName: 'Vuetify'
 }
