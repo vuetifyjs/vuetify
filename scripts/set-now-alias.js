b01bf57712c0ec0b8e86f24480dd454cbfdc0288
Kael <kaelwd@gmail.com>
2018-11-01 01:37:15 +1100

    chore: remove files list from now config

--- scripts/set-now-alias.js
+++ scripts/set-now-alias.js
@@ -9,6 +9,23 @@ if (!alias) {
 }
 
 config.alias = alias
+config.files = [
+  'packages/api-generator/dist',
+  'packages/api-generator/package.json',
+  'packages/vuetify/dist',
+  'packages/vuetify/lib',
+  'packages/vuetify/es5',
+  'packages/vuetify/src',
+  'packages/vuetify/types',
+  'packages/vuetify/package.json',
+  'packages/vuetifyjs.com/dist',
+  'packages/vuetifyjs.com/src',
+  'packages/vuetifyjs.com/server.js',
+  'packages/vuetifyjs.com/package.json',
+  'package.json',
+  'yarn.lock'
+]
+
 fs.writeFileSync(
   require.resolve('../now.json'),
   JSON.stringify(config, null, 2)
