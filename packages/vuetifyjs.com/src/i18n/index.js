7e075571c0fdc3d6a2c2562f4c2a17619694df18
John Leider <john.j.leider@gmail.com>
2018-11-01 23:19:38 -0400

    docs: add lazy loading for languages

--- packages/vuetifyjs.com/src/i18n/index.js
+++ packages/vuetifyjs.com/src/i18n/index.js
@@ -1,30 +1,57 @@
-import { camelActual } from '@/util/helpers'
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import en from '@/lang/en'
-import * as langs from '@/lang'
+import axios from 'axios'
+import messages from '@/lang'
 
 Vue.use(VueI18n)
 
-export function createI18n (ssrContext) {
-  let locale = 'en'
+export function createI18n (ssrContext, router) {
+  const loadedLanguages = []
+  const globalLanguages = {}
 
+  let locale
   if (ssrContext && ssrContext.lang) {
     locale = ssrContext.lang
   } else if (typeof document !== 'undefined') {
     locale = document.documentElement.lang
   }
 
-  const localePath = camelActual(locale)
-  const messages = { en }
+  const i18n = new VueI18n({
+    locale,
+    messages,
+    fallbackLocale: 'en'
+  })
 
-  if (locale !== 'en' && langs[localePath]) {
-    messages[locale] = langs[localePath]
+  function setI18nLanguage (lang) {
+    i18n.locale = lang
+    axios.defaults.headers.common['Accept-Language'] = lang
+
+    if (!ssrContext) {
+      document.querySelector('html').setAttribute('lang', lang)
+    }
+
+    return lang
   }
 
-  return new VueI18n({
-    locale: 'en',
-    fallbackLocale: 'en',
-    messages
+  function loadLanguageAsync (lang) {
+    if (!loadedLanguages.includes(lang)) {
+      return import(/* webpackChunkName: "lang-[request]" */ `@/lang/${lang}`).then(msgs => {
+        loadedLanguages.push(lang)
+        globalLanguages[lang] = msgs.default
+        i18n.setLocaleMessage(lang, globalLanguages)
+        return Promise.resolve(setI18nLanguage(lang))
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+    return Promise.resolve(setI18nLanguage(lang))
+  }
+
+  router.beforeEach((to, from, next) => {
+    loadLanguageAsync(
+      to.params.lang
+    ).then(() => next())
   })
+
+  return i18n
 }
--- packages/vuetifyjs.com/src/lang/index.js
+++ packages/vuetifyjs.com/src/lang/index.js
@@ -1,17 +1,30 @@
-import en from './en'
-import esMX from './es-MX'
-import ja from './ja'
-import ko from './ko'
-import ptBR from './pt-BR'
-import ru from './ru'
-import zhHans from './zhHans'
+import camelCase from 'lodash/camelCase'
+import upperFirst from 'lodash/upperFirst'
 
-export {
-  en,
-  esMX,
-  ja,
-  ko,
-  ptBR,
-  ru,
-  zhHans
+const requireLang = require.context(
+  './',
+  true,
+  /\.json$/
+)
+
+const messages = {}
+
+for (const file of requireLang.keys()) {
+  if (file === './index.js') continue
+
+  const path = file.replace(/(\.\/|\.json$)/g, '').split('/')
+
+  path.reduce((o, s, i) => {
+    const prop = i !== 0
+      ? upperFirst(camelCase(s))
+      : s
+
+    o[prop] = i + 1 === path.length
+      ? requireLang(file)
+      : o[prop] || {}
+
+    return o[prop]
+  }, messages)
 }
+
+export default messages
