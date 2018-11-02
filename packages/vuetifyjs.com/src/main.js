c5f3a8a2c46275153a0dc09fc2b454832f7a6d5e
John Leider <john.j.leider@gmail.com>
2018-11-01 23:50:12 -0400

    docs: move no-ssr to plugins

--- packages/vuetifyjs.com/src/main.js
+++ packages/vuetifyjs.com/src/main.js
@@ -6,7 +6,6 @@ import 'event-source-polyfill'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import axios from 'axios'
-import NoSSR from 'vue-no-ssr'
 
 // Bootstrap
 import '@/components'
@@ -18,8 +17,6 @@ import { sync } from 'vuex-router-sync'
 
 // Application
 import App from './App.vue'
-
-Vue.component(NoSSR.name, NoSSR)
 
 Vue.config.performance = process.env.NODE_ENV === 'development'
 
--- packages/vuetifyjs.com/src/plugins/index.js
+++ packages/vuetifyjs.com/src/plugins/index.js
@@ -1,3 +1,4 @@
 // Make this dynamic
 import '@/plugins/axios'
+import '@/plugins/no-ssr'
 import '@/plugins/vuetify'
