f32c9d1d4c943be55fcb4ce398256d7388980764
John Leider <john.j.leider@gmail.com>
2018-11-05 22:23:24 -0500

    docs: add back shouldShowAd computed property
    was showing ad on store/home page

--- packages/vuetifyjs.com/src/components/core/Ad.vue
+++ packages/vuetifyjs.com/src/components/core/Ad.vue
@@ -1,63 +1,65 @@
 <template>
   <v-flex id="ad__container" shrink>
-    <ad-system-bar v-if="viewport === 'xs' && $vuetify.breakpoint.xsOnly">
-      <ad-shown :viewport="viewport" />
-    </ad-system-bar>
+    <template v-if="shouldShowAd">
+      <ad-system-bar v-if="viewport === 'xs' && $vuetify.breakpoint.xsOnly">
+        <ad-shown :viewport="viewport" />
+      </ad-system-bar>
 
-    <ad-bottom-nav v-else-if="viewport === 'sm' && $vuetify.breakpoint.smOnly">
-      <ad-shown :viewport="viewport" />
-    </ad-bottom-nav>
+      <ad-bottom-nav v-else-if="viewport === 'sm' && $vuetify.breakpoint.smOnly">
+        <ad-shown :viewport="viewport" />
+      </ad-bottom-nav>
 
-    <ad-drawer v-else-if="viewport === 'md' && $vuetify.breakpoint.mdAndUp">
-      <v-layout
-        column
-        fill-height
-      >
-        <v-flex shrink mb-3>
-          <no-ssr>
-            <core-table-of-contents
-              :offset="85"
-              :threshold="50"
-            />
-          </no-ssr>
-        </v-flex>
-
-        <v-flex
-          xs12
-          text-xs-center
+      <ad-drawer v-else-if="viewport === 'md' && $vuetify.breakpoint.mdAndUp">
+        <v-layout
+          column
+          fill-height
         >
-          <h4 class="caption font-weight-bold grey--text">Diamond Sponsors</h4>
-          <div class="my-3">
-            <div
-              v-for="diamond in diamonds"
-              :key="diamond.name"
-              class="text-xs-center mt-2"
-            >
-              <a
-                :href="diamond.href"
-                target="_blank"
-                rel="noopener"
-                @click="$ga.event('drawer sponsor click', 'click', diamond.name)"
-              >
-                <v-img
-                  :src="`https://cdn.vuetifyjs.com/images/${diamond.logo}`"
-                  :alt="diamond.Name"
-                  contain
-                  height="30"
-                />
-              </a>
-            </div>
-          </div>
-          <misc-sponsor-btn small />
-        </v-flex>
+          <v-flex shrink mb-3>
+            <no-ssr>
+              <core-table-of-contents
+                :offset="85"
+                :threshold="50"
+              />
+            </no-ssr>
+          </v-flex>
 
-        <ad-shown
-          :viewport="viewport"
-          class="mt-auto"
-          style="margin-bottom: 72px;"
-        />
-      </v-layout>
-    </ad-drawer>
+          <v-flex
+            xs12
+            text-xs-center
+          >
+            <h4 class="caption font-weight-bold grey--text">Diamond Sponsors</h4>
+            <div class="my-3">
+              <div
+                v-for="diamond in diamonds"
+                :key="diamond.name"
+                class="text-xs-center mt-2"
+              >
+                <a
+                  :href="diamond.href"
+                  target="_blank"
+                  rel="noopener"
+                  @click="$ga.event('drawer sponsor click', 'click', diamond.name)"
+                >
+                  <v-img
+                    :src="`https://cdn.vuetifyjs.com/images/${diamond.logo}`"
+                    :alt="diamond.Name"
+                    contain
+                    height="30"
+                  />
+                </a>
+              </div>
+            </div>
+            <misc-sponsor-btn small />
+          </v-flex>
+
+          <ad-shown
+            :viewport="viewport"
+            class="mt-auto"
+            style="margin-bottom: 72px;"
+          />
+        </v-layout>
+      </ad-drawer>
+    </template>
   </v-flex>
 </template>
 
@@ -86,6 +88,12 @@
       ...mapState('route', ['path', 'name']),
       diamonds () {
         return this.supporters.diamond
+      },
+      shouldShowAd () {
+        return (
+          this.name !== 'home/Home' &&
+          this.name.indexOf('store/') < 0
+        )
       }
     },
 
