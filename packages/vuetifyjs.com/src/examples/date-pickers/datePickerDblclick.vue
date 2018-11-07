242fb1acfb10c8bb072aa3ac93b2f940cbcf0aa5
Jacek Karczmarczyk <jk@drukcd.pl>
2018-11-07 16:10:38 +0700

    docs: example

---
+++ packages/vuetifyjs.com/src/examples/date-pickers/datePickerDblclick.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 sm6 class="my-2 px-1">
+      <v-date-picker
+        v-model="date"
+        full-width
+        @dblclick="dblclick"
+      ></v-date-picker>
+    </v-flex>
+    <v-flex xs12 sm6 class="my-2 px-1">
+      <v-subheader>Double click on any date in the picker</v-subheader>
+      <ul class="ma-3">
+        <li v-for="(clickedDate, index) in dates" :key="index">Clicked date: {{ clickedDate }}</li>
+      </ul>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      date: new Date().toISOString().substr(0, 10),
+      dates: []
+    }),
+    methods: {
+      dblclick (date) {
+        this.dates.push(date)
+      }
+    }
+  }
+</script>
