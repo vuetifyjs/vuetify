6c427a855efc4721ad779d61330f86488becf9ae
Kael <kaelwd@gmail.com>
2018-11-08 17:24:15 +1100

    chore(ci): build docs on now instead of travis
    building on travis seems to be slower, and a build failure should
    still be reported either way

--- scripts/set-now-alias.js
+++ scripts/set-now-alias.js
@@ -10,24 +10,6 @@ if (!alias) {
 }
 
 config.alias = alias
-config.files = [
-  'packages/api-generator/dist',
-  'packages/api-generator/package.json',
-  'packages/vuetify/dist',
-  'packages/vuetify/lib',
-  'packages/vuetify/es5',
-  'packages/vuetify/src',
-  'packages/vuetify/types',
-  'packages/vuetify/package.json',
-  'packages/vuetifyjs.com/dist',
-  'packages/vuetifyjs.com/src',
-  'packages/vuetifyjs.com/server.js',
-  'packages/vuetifyjs.com/package.json',
-  'package.json',
-  'lerna.json',
-  'yarn.lock'
-]
-pkg.scripts['now-build'] = "echo 'Built on Travis CI'"
 
 fs.writeFileSync(
   require.resolve('../now.json'),
