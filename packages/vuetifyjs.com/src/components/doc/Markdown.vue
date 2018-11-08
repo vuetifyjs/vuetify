98ae6c1642d29bc8088ff179df60a3db9bc63e68
John Leider <john.j.leider@gmail.com>
2018-11-08 17:47:54 -0500

    docs: improve how markdown handles external links

--- packages/vuetifyjs.com/src/components/doc/Markdown.vue
+++ packages/vuetifyjs.com/src/components/doc/Markdown.vue
@@ -1,4 +1,5 @@
 <script>
+  // Utilities
   import marked from 'marked'
 
   export default {
@@ -56,6 +57,21 @@
         code = code.map(c => `- ${c}\n`).join('')
       }
 
+      // Convert all markdown external links
+      // to have an icon and target blank
+      code = code.replace(/\[([^\]]*)\]\(([^)]*)\)/g, (match, p1, p2) => {
+        if (p2.indexOf('http') < 0) return match
+
+        return `
+          <a
+            href="${p2}"
+            target="_blank"
+            rel="noopener"
+            class="markdown--link"
+          >${p1}<i class="v-icon mdi mdi-open-in-new"></i></a>
+        `.trim()
+      })
+
       return h(props.tag, {
         staticClass: 'markdown',
         class: {
@@ -68,8 +84,19 @@
   }
 </script>
 
-<style>
+<style lang="stylus">
 h4.markdown > p {
   margin-bottom: 0;
+}
+.markdown--link {
+  text-decoration: none;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  .v-icon {
+    font-size: 16px;
+  }
 }
 </style>
