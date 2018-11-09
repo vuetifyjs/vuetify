8c0d88f271658ee3afe935787b44367a00205a78
Andrew Henry <andrew_henry@fcp.biz>
2018-11-09 14:03:09 -0500

    refactor(PM2 Configs): Moved PM2 configs to root

---
+++ ecosystem-win.config.js
@@ -0,0 +1,13 @@
+// PM2 process file
+// http://pm2.keymetrics.io/docs/usage/application-declaration/
+
+module.exports = {
+  apps: [
+    {
+      name: 'vuetify-docs',
+      script: 'server.js',
+      cwd: './packages/vuetifyjs.com/',
+      watch: true,
+    },
+  ],
+};
