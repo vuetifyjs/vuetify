4acd24d17b3adea6d4b5406260aee2569bc51d6c
pynnl <31667324+pynnl@users.noreply.github.com>
2018-11-10 21:16:21 -0500

    fix(v-menu): position problem with horizontal scrolling (#5527)
    * add scrollX for measuring rect
    
    * add getOffsetLeft(), move the logic from measure() to calcXOverflow()

--- packages/vuetify/src/mixins/menuable.js
+++ packages/vuetify/src/mixins/menuable.js
@@ -200,7 +200,7 @@ export default Vue.extend({
 
       if (left < 0) left = 12
 
-      return left
+      return left + this.getOffsetLeft()
     },
     calcYOverflow (top) {
       const documentHeight = this.getInnerHeight()
@@ -275,6 +275,12 @@ export default Vue.extend({
       if (!this.hasWindow) return 0
 
       return window.innerWidth
+    },
+    getOffsetLeft () {
+      if (!this.hasWindow) return 0
+
+      return window.pageXOffset ||
+        document.documentElement.scrollLeft
     },
     getOffsetTop () {
       if (!this.hasWindow) return 0
