bb9c0faf3257588866faa445bf2c5f2dabafba93
John Leider <john.j.leider@gmail.com>
2018-11-12 20:58:10 -0500

    docs: add supplemental section
    updated form components with mask table

---
+++ packages/vuetifyjs.com/src/components/doc/Supplemental.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <doc-subheading>Generic.Pages.supplemental</doc-subheading>
+    <core-tree :children="value" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: Array,
+        default: () => ([])
+      }
+    }
+  }
+</script>
