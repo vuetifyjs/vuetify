79957356b6576761f390c5682174bc8e4a358661
Enric Bisbe Gil <enricu@gmail.com>
2018-11-15 13:43:01 +0100

    Add spanish locale

---
+++ packages/vuetify/src/locale/es.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Elementos por página:',
+    rowsPerPageAll: 'Todos',
+    pageText: '{0}-{1} de {2}',
+    noResultsText: 'Sin datos para mostrar',
+    nextPage: 'Página Siguiente',
+    prevPage: 'Página Anterior'
+  },
+  dataTable: {
+    rowsPerPageText: 'Filas por página:'
+  },
+  noDataText: 'Sin datos'
+}
