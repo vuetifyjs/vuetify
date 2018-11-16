215e94d5d538fcc86e89442f0e42cdad5f49250a
Илья <iliyazelenkog@gmail.com>
2018-11-16 02:53:00 +0200

    Update menu-keyable.js

--- packages/vuetify/src/components/VMenu/mixins/menu-keyable.js
+++ packages/vuetify/src/components/VMenu/mixins/menu-keyable.js
@@ -54,10 +54,12 @@ export default {
       this.getTiles()
 
       if (e.keyCode === keyCodes.down && this.listIndex < this.tiles.length - 1) {
+        e.preventDefault()
         this.listIndex++
         // Allow user to set listIndex to -1 so
         // that the list can be un-highlighted
       } else if (e.keyCode === keyCodes.up && this.listIndex > -1) {
+        e.preventDefault()
         this.listIndex--
       } else if (e.keyCode === keyCodes.enter && this.listIndex !== -1) {
         this.tiles[this.listIndex].click()
