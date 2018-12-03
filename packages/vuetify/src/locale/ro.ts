4e63f67c18c8db0a3d3d5eef6cab13d58d16d88a
Sebastian Mares <818380+smares@users.noreply.github.com>
2018-12-03 22:43:11 +0100

    Create ro.ts
    Added Romanian translations

---
+++ packages/vuetify/src/locale/ro.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Elemente per pagină:',
+    rowsPerPageAll: 'Toate',
+    pageText: '{0}-{1} din {2}',
+    noResultsText: 'Nici un rezultat găsit',
+    nextPage: 'Pagina următoare',
+    prevPage: 'Pagina precedentă'
+  },
+  dataTable: {
+    rowsPerPageText: 'Rânduri per pagină:'
+  },
+  noDataText: 'Nu sunt date disponibile'
+}
