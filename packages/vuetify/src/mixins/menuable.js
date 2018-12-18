f561ca2c13748211905bc2af8ec5805d13929df8
Kael Watts-Deuchar <kaelwd@gmail.com>
2018-12-19 02:59:07 +1100

    fix(menuable): set correct position with left+attach+offset-x
    see a025093, #5318

--- packages/vuetify/src/mixins/menuable.js
+++ packages/vuetify/src/mixins/menuable.js
@@ -107,11 +107,11 @@ export default Vue.extend({
     computedLeft () {
       const a = this.dimensions.activator
       const c = this.dimensions.content
+      const activatorLeft = this.isAttached ? a.offsetLeft : a.left
       const minWidth = Math.max(a.width, c.width)
       let left = 0
 
-      if (this.isAttached) left += a.offsetLeft
-      else left += this.left ? a.left - (minWidth - a.width) : a.left
+      left += this.left ? activatorLeft - (minWidth - a.width) : activatorLeft
       if (this.offsetX) left += this.left ? -a.width : a.width
       if (this.nudgeLeft) left -= parseInt(this.nudgeLeft)
       if (this.nudgeRight) left += parseInt(this.nudgeRight)
