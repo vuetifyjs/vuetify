b6ca648c57486eb44ea743f41e8bb2e6ca0f62cf
John Leider <john.j.leider@gmail.com>
2018-12-20 23:02:38 -0500

    docs(Ecosystems): update language, add new items

--- packages/vuetifyjs.com/src/components/core/Ecosystems.vue
+++ packages/vuetifyjs.com/src/components/core/Ecosystems.vue
@@ -2,10 +2,9 @@
   <v-menu
     attach
     bottom
-    lazy
     left
     offset-y
-    max-height="500"
+    max-height="700"
   >
     <v-btn
       slot="activator"
@@ -75,8 +74,65 @@
 <script>
   export default {
     data: vm => ({
-      ecosystems: vm.$t('Vuetify.AppToolbar.ecosystems'),
-      socials: vm.$t('Vuetify.AppToolbar.socials')
+      ecosystems: [
+        {
+          text: vm.$t('Vuetify.AppToolbar.themeGenerator'),
+          icon: 'mdi-format-paint',
+          href: 'https://theme-generator.vuetifyjs.com'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.cognito'),
+          icon: 'mdi-server-network',
+          href: 'https://cognito.vuetifyjs.com'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.eslint'),
+          icon: 'mdi-eslint',
+          href: 'https://github.com/vuetifyjs/eslint-config-vuetify'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.reddit'),
+          href: 'https://www.reddit.com/r/vuetifyjs/',
+          icon: 'mdi-reddit'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.medium'),
+          href: 'https://medium.com/vuetify',
+          icon: 'mdi-medium'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.awesome'),
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+          icon: 'mdi-creation'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.codepen'),
+          href: 'https://template.vuetifyjs.com',
+          icon: 'mdi-codepen'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.latest'),
+          href: 'https://github.com/vuetifyjs/vuetify/releases',
+          icon: 'mdi-format-list-checks'
+        }
+      ],
+      socials: [
+        {
+          text: vm.$t('Vuetify.AppToolbar.github'),
+          href: 'https://github.com/vuetifyjs/vuetify',
+          icon: 'mdi-github-box'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.twitter'),
+          href: 'https://twitter.com/vuetifyjs',
+          icon: 'mdi-twitter-box'
+        },
+        {
+          text: vm.$t('Vuetify.AppToolbar.facebook'),
+          href: 'https://www.facebook.com/vuetifyjs',
+          icon: 'mdi-facebook-box'
+        }
+      ]
     })
   }
 </script>
