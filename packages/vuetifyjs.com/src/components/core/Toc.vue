c4c9241ae6cf6d2eb13896dd43ed29857d4ae299
John Leider <john.j.leider@gmail.com>
2018-12-27 11:02:26 -0500

    docs(Toc): wait for animationFrame before loading

--- packages/vuetifyjs.com/src/components/core/Toc.vue
+++ packages/vuetifyjs.com/src/components/core/Toc.vue
@@ -53,7 +53,7 @@
       isLoading: {
         immediate: true,
         handler (val) {
-          !val && this.genList()
+          !val && requestAnimationFrame(this.genList)
         }
       }
     },
