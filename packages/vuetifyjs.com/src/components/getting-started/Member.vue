c4b8a22027367cc16f6a805e73cb521300daa860
John Leider <john.j.leider@gmail.com>
2018-12-27 16:50:37 -0500

    docs(MeetTheTeam): add new page

---
+++ packages/vuetifyjs.com/src/components/getting-started/Member.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="mb-3 text-xs-center member">
+    <v-avatar
+      size="164"
+      class="elevation-4"
+      color="#DDF0FE"
+    >
+      <v-hover>
+        <v-img
+          slot-scope="{ hover }"
+          :src="`https://avataaars.io/?${value.avatar}`"
+        >
+          <v-sheet
+            :color="hover ? 'rgba(0, 0, 0, .72)' : 'transparent'"
+            class="transition-fast-in-fast-out"
+            dark
+            height="100%"
+            style="position: absolute; top: 0; left: 0;"
+            width="100%"
+          >
+            <v-layout
+              v-show="hover"
+              fill-height
+              align-center
+              justify-center
+              ma-0
+              wrap
+            >
+              <v-flex xs12>
+                <doc-markdown>{{ value.title }}</doc-markdown>
+              </v-flex>
+              <a
+                v-if="value.email"
+                :href="`mailto:${value.email}`"
+                :title="`${value.name}'s' Email`"
+                class="text-decoration-none mx-2"
+              >
+                <v-icon>mdi-email</v-icon>
+              </a>
+              <a
+                v-if="value.github"
+                :href="`https://github.com/${value.github}`"
+                :title="`${value.name}'s' Github Profile`"
+                class="text-decoration-none mx-2"
+              >
+                <v-icon>mdi-github-face</v-icon>
+              </a>
+            </v-layout>
+          </v-sheet>
+        </v-img>
+      </v-hover>
+    </v-avatar>
+    <h3
+      class="font-weight-light headline"
+      v-text="value.name"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  }
+</script>
+
+<style>
+.member h3 {
+  font-size: 24px;
+  font-weight: 500;
+}
+</style>
---
+++ packages/vuetifyjs.com/src/components/getting-started/Team.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout
+      justify-center
+      wrap
+    >
+      <v-flex
+        v-for="(member, i) in team"
+        :key="i"
+        xs6
+        sm4
+      >
+        <getting-started-member :value="member" />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      team: [
+        {
+          title: 'founder',
+          name: 'John Leider',
+          email: 'john@vuetifyjs.com',
+          github: 'johnleider',
+          avatar: 'avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=Blonde&facialHairType=Blank&clotheType=Hoodie&clotheColor=Heather&eyeType=Side&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light'
+        },
+        {
+          title: 'operations',
+          name: 'Ranee Leider',
+          email: 'ranee@vuetifyjs.com',
+          avatar: 'avatarStyle=Transparent&topType=LongHairStraight&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light'
+        },
+        {
+          title: 'assistant',
+          name: 'Rachel Luna',
+          email: 'rachel@vuetifyjs.com',
+          avatar: 'avatarStyle=Transparent&topType=LongHairCurvy&accessoriesType=Round&hairColor=BrownDark&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=Heather&eyeType=Side&eyebrowType=RaisedExcited&mouthType=Default&skinColor=Light'
+        },
+        {
+          title: 'core',
+          name: 'Kael Watts-Deuchar',
+          github: 'kaelwd',
+          avatar: 'avatarStyle=Transparent&topType=ShortHairShaggyMullet&accessoriesType=Blank&hairColor=BlondeGolden&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Heather&eyeType=Squint&eyebrowType=SadConcerned&mouthType=ScreamOpen&skinColor=Pale'
+        },
+        {
+          title: 'core',
+          name: 'Albert Kaaman',
+          github: 'nekosaur',
+          avatar: 'avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription02&hairColor=Auburn&facialHairType=MoustacheMagnum&facialHairColor=Auburn&clotheType=Hoodie&clotheColor=Black&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Tongue&skinColor=Pale'
+        },
+        {
+          title: 'core',
+          name: 'Jacek Karczmarczyk',
+          github: 'jacekkarczmarczyk',
+          avatar: 'avatarStyle=Transparent&topType=ShortHairSides&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=Blank&clotheType=Overall&clotheColor=Heather&eyeType=Squint&eyebrowType=Angry&mouthType=Serious&skinColor=Brown'
+        },
+        {
+          title: 'core',
+          name: 'Andrew Henry',
+          github: 'MajesticPotatoe',
+          avatar: 'avatarStyle=Transparent&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Heather&eyeType=Dizzy&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'
+        }
+      ]
+    })
+  }
+</script>
