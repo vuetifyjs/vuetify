d9acecc908bb2ef02d55fc2e1ae1c30d7e758ba3
John Leider <john.j.leider@gmail.com>
2019-01-02 01:25:32 -0500

    docs(Card): add new example

---
+++ packages/vuetifyjs.com/src/examples/cards/twitterCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card
+    class="mx-auto"
+    color="#26c6da"
+    dark
+    max-width="400"
+  >
+    <v-card-title>
+      <v-icon
+        large
+        left
+      >
+        mdi-twitter
+      </v-icon>
+      <span class="title font-weight-light">Twitter</span>
+    </v-card-title>
+
+    <v-card-text class="headline font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-tile class="grow">
+        <v-list-tile-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>Evan You</v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-layout
+          align-center
+          justify-end
+        >
+          <v-icon class="mr-1">mdi-heart</v-icon>
+          <span class="subheading mr-2">256</span>
+          <span class="mr-1">·</span>
+          <v-icon class="mr-1">mdi-share-variant</v-icon>
+          <span class="subheading">45</span>
+        </v-layout>
+      </v-list-tile>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      //
+    })
+  }
+</script>
