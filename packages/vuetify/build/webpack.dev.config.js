a079b18b58693a73c3a72ef45884531dbc65c7d6
Daan Roet <daanroet@gmail.com>
2019-01-04 16:43:40 +0100

    fix(eslint): use 2 space indent

--- packages/vuetify/build/webpack.dev.config.js
+++ packages/vuetify/build/webpack.dev.config.js
@@ -12,107 +12,107 @@ portfinder.basePort = 8080;
 const resolve = file => path.resolve(__dirname, file)
 
 module.exports = async () => {
-   return merge(baseWebpackConfig, {
-      devtool: 'source-map',
-      entry: ['babel-polyfill', './dev/index.js'],
-      output: {
-         filename: '[name].js',
-         path: resolve('../dev'),
-         publicPath: '/dev/',
-         library: 'Vuetify'
-      },
-      resolve: {
-         alias: {
-            vuetify: resolve('../src'),
-            'vue$': 'vue/dist/vue.esm.js'
-         }
-      },
-      module: {
-         rules: [
-            {
-               test: /\.vue$/,
-               loader: 'vue-loader',
-               options: {
-                  compilerOptions: {
-                     modules: [VuetifyProgressiveModule]
-                  }
-               }
-            },
-            {
-               test: /\.ts$/,
-               use: 'happypack/loader?id=ts',
-               exclude: /node_modules/
-            },
-            {
-               test: /\.js$/,
-               use: 'happypack/loader?id=js',
-               exclude: /node_modules/
-            },
-            {
-               test: /\.css$/,
-               use: [
-                  'style-loader',
-                  'css-loader'
-               ]
-            },
-            {
-               test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
-               oneOf: [
-                  {
-                     test: /\.(png|jpe?g|gif)$/,
-                     resourceQuery: /vuetify-preload/,
-                     use: [
-                        'vuetify-loader/progressive-loader',
-                        {
-                           loader: 'url-loader',
-                           options: { limit: 8000 }
-                        }
-                     ]
-                  },
-                  {
-                     loader: 'url-loader',
-                     options: {
-                        limit: 10000,
-                        name: 'img/[name].[hash:7].[ext]'
-                     }
-                  }
-               ]
+  return merge(baseWebpackConfig, {
+    devtool: 'source-map',
+    entry: ['babel-polyfill', './dev/index.js'],
+    output: {
+      filename: '[name].js',
+      path: resolve('../dev'),
+      publicPath: '/dev/',
+      library: 'Vuetify'
+    },
+    resolve: {
+      alias: {
+        vuetify: resolve('../src'),
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            compilerOptions: {
+              modules: [VuetifyProgressiveModule]
             }
-         ]
-      },
-      devServer: {
-         contentBase: resolve('../dev'),
-         publicPath: '/dev/',
-         host: process.env.HOST || 'localhost',
-         port: process.env.PORT || await portfinder.getPortPromise(),
-         disableHostCheck: true
-      },
-      plugins: [
-         new VueLoaderPlugin(),
-         new ForkTsCheckerWebpackPlugin({
-            checkSyntacticErrors: true,
-            tsconfig: resolve('../tsconfig.json')
-         }),
-         new HappyPack({
-            id: 'ts',
-            threadPool: happyThreadPool,
-            loaders: [
-               'babel-loader',
-               {
-                  loader: 'ts-loader',
-                  options: {
-                     appendTsSuffixTo: [/\.vue$/],
-                     happyPackMode: true
-                  }
-               },
-               'eslint-loader?cache=true?emitWarning=true'
-            ]
-         }),
-         new HappyPack({
-            id: 'js',
-            threadPool: happyThreadPool,
-            loaders: ['babel-loader', 'eslint-loader?cache=true?emitWarning=true']
-         })
+          }
+        },
+        {
+          test: /\.ts$/,
+          use: 'happypack/loader?id=ts',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.js$/,
+          use: 'happypack/loader?id=js',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+          oneOf: [
+            {
+              test: /\.(png|jpe?g|gif)$/,
+              resourceQuery: /vuetify-preload/,
+              use: [
+                'vuetify-loader/progressive-loader',
+                {
+                  loader: 'url-loader',
+                  options: { limit: 8000 }
+                }
+              ]
+            },
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10000,
+                name: 'img/[name].[hash:7].[ext]'
+              }
+            }
+          ]
+        }
       ]
-   })
+    },
+    devServer: {
+      contentBase: resolve('../dev'),
+      publicPath: '/dev/',
+      host: process.env.HOST || 'localhost',
+      port: process.env.PORT || await portfinder.getPortPromise(),
+      disableHostCheck: true
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new ForkTsCheckerWebpackPlugin({
+        checkSyntacticErrors: true,
+        tsconfig: resolve('../tsconfig.json')
+      }),
+      new HappyPack({
+        id: 'ts',
+        threadPool: happyThreadPool,
+        loaders: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+              happyPackMode: true
+            }
+          },
+          'eslint-loader?cache=true?emitWarning=true'
+        ]
+      }),
+      new HappyPack({
+        id: 'js',
+        threadPool: happyThreadPool,
+        loaders: ['babel-loader', 'eslint-loader?cache=true?emitWarning=true']
+      })
+    ]
+  })
 }
