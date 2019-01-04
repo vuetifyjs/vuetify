bb55d6b6b8a627e8f1d5d28059a91d71150b42c4
John Leider <john.j.leider@gmail.com>
2019-01-04 11:28:59 -0500

    docs: remove dynamic store registration of docs

--- packages/vuetifyjs.com/src/pages/documentation/Page.vue
+++ packages/vuetifyjs.com/src/pages/documentation/Page.vue
@@ -17,15 +17,8 @@
 </template>
 
 <script>
-  // https://ssr.vuejs.org/guide/data.html#store-code-splitting
-  import docModule from '@/store/modules/documentation'
-
   export default {
     name: 'Documentation',
-
-    asyncData ({ store }) {
-      store.registerModule('documentation', docModule)
-    },
 
     props: {
       // Provided by router
@@ -41,10 +34,6 @@
         type: String,
         default: undefined
       }
-    },
-
-    destroyed () {
-      this.$store.unregisterModule('documentation')
     }
   }
 </script>
