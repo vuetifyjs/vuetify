ceb811b6ab4f9d3f4d00fe1a5df91f43d85f84ca
sh7dm <d3dx12.xx@gmail.com>
2019-01-07 14:39:41 +0300

    feat(kitchen): add more avatar sizes

--- packages/kitchen/src/pan/v-avatar.vue
+++ packages/kitchen/src/pan/v-avatar.vue
@@ -77,32 +77,160 @@
           justify-space-around
           wrap
         >
-          <v-avatar color="indigo">
+          <v-avatar
+            size="32"
+            color="indigo">
             <v-icon dark>mdi-account-group</v-icon>
           </v-avatar>
-
-          <v-avatar>
+          <v-avatar
+            size="64"
+            color="indigo">
+            <v-icon dark>mdi-account-group</v-icon>
+          </v-avatar>
+          <v-avatar
+            size="128"
+            color="indigo">
+            <v-icon dark>mdi-account-group</v-icon>
+          </v-avatar>
+          <v-avatar
+            size="256"
+            color="indigo">
+            <v-icon dark>mdi-account-group</v-icon>
+          </v-avatar>
+        </v-layout>
+      </v-flex>
+      <v-flex>
+        <v-layout
+          align-center
+          justify-space-around
+          wrap
+        >
+          <v-avatar size="32">
             <img
               src="https://cdn.vuetifyjs.com/images/john.jpg"
               alt="John"
             >
           </v-avatar>
-
+          <v-avatar size="64">
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="John"
+            >
+          </v-avatar>
+          <v-avatar size="128">
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="John"
+            >
+          </v-avatar>
+          <v-avatar size="256">
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="John"
+            >
+          </v-avatar>
+        </v-layout>
+      </v-flex>
+      <v-flex>
+        <v-layout
+          align-center
+          justify-space-around
+          wrap
+        >
           <v-badge overlap>
             <span slot="badge">3</span>
 
             <v-avatar
               color="purple red--after"
+              size="32"
             >
               <v-icon dark>mdi-alarm</v-icon>
             </v-avatar>
           </v-badge>
+          <v-badge overlap>
+            <span slot="badge">3</span>
 
-          <v-avatar color="teal">
+            <v-avatar
+              color="purple red--after"
+              size="64"
+            >
+              <v-icon dark>mdi-alarm</v-icon>
+            </v-avatar>
+          </v-badge>
+          <v-badge overlap>
+            <span slot="badge">3</span>
+
+            <v-avatar
+              color="purple red--after"
+              size="128"
+            >
+              <v-icon dark>mdi-alarm</v-icon>
+            </v-avatar>
+          </v-badge>
+          <v-badge overlap>
+            <span slot="badge">3</span>
+
+            <v-avatar
+              color="purple red--after"
+              size="256"
+            >
+              <v-icon dark>mdi-alarm</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-layout>
+      </v-flex>
+      <v-flex>
+        <v-layout
+          align-center
+          justify-space-around
+          wrap
+        >
+          <v-avatar
+            size="32"
+            color="teal">
             <span class="white--text headline">C</span>
           </v-avatar>
-
-          <v-avatar color="red">
+          <v-avatar
+            size="64"
+            color="teal">
+            <span class="white--text headline">C</span>
+          </v-avatar>
+          <v-avatar
+            size="128"
+            color="teal">
+            <span class="white--text headline">C</span>
+          </v-avatar>
+          <v-avatar
+            size="256"
+            color="teal">
+            <span class="white--text headline">C</span>
+          </v-avatar>
+        </v-layout>
+      </v-flex>
+      <v-flex>
+        <v-layout
+          align-center
+          justify-space-around
+          wrap
+        >
+          <v-avatar
+            size="32"
+            color="red">
+            <span class="white--text headline">J</span>
+          </v-avatar>
+          <v-avatar
+            size="64"
+            color="red">
+            <span class="white--text headline">J</span>
+          </v-avatar>
+          <v-avatar
+            size="128"
+            color="red">
+            <span class="white--text headline">J</span>
+          </v-avatar>
+          <v-avatar
+            size="256"
+            color="red">
             <span class="white--text headline">J</span>
           </v-avatar>
         </v-layout>
