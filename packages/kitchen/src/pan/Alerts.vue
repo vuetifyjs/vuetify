829f44d908b207c59514b4db24c700aebb37906b
John Leider <john.j.leider@gmail.com>
2019-01-10 17:30:51 -0500

    docs(kitchen): update alerts kitchen

--- packages/kitchen/src/pan/Alerts.vue
+++ packages/kitchen/src/pan/Alerts.vue
@@ -1,47 +1,229 @@
 <template>
-  <v-container text-xs-center>
-    <v-layout
-      column
-      variations>
+  <v-container>
+    <v-layout column>
+      <!-- Dense -->
       <v-flex>
-        <h3 class="title grey--text mb-4 mt-5">Colors</h3>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Dense</h3>
         <v-alert
-          :value="true"
+          show
+          type="success"
+          dense
+        >
+          This is a success alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="info"
+          dense
+        >
+          This is an info alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="warning"
+          dense
+        >
+          This is a warning alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="error"
+          dense
+        >
+          This is an error alert.
+        </v-alert>
+      </v-flex>
+      <!-- Default -->
+      <v-flex>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Default</h3>
+        <v-alert
+          show
           type="success"
         >
           This is a success alert.
         </v-alert>
 
         <v-alert
-          :value="true"
+          show
           type="info"
         >
           This is a info alert.
         </v-alert>
 
         <v-alert
-          :value="true"
+          show
           type="warning"
         >
           This is a warning alert.
         </v-alert>
 
         <v-alert
-          :value="true"
+          show
           type="error"
         >
-          This is a error alert.
+          This is an error alert.
         </v-alert>
       </v-flex>
-
+      <!-- Prominent -->
       <v-flex>
-        <h3 class="title grey--text mb-4 mt-5">Closable</h3>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Prominent</h3>
+        <v-alert
+          show
+          type="success"
+          prominent
+        >
+          This is a success alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="info"
+          prominent
+        >
+          This is an info alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="warning"
+          prominent
+        >
+          This is a warning alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="error"
+          prominent
+        >
+          This is an error alert.
+        </v-alert>
+      </v-flex>
+      <!-- Border -->
+      <v-flex>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Border</h3>
+        <v-alert
+          show
+          type="success"
+          border="top"
+        >
+          This is a success alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="info"
+          border="right"
+        >
+          This is an info alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="warning"
+          border="bottom"
+        >
+          This is a warning alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="error"
+          border="left"
+        >
+          This is an error alert.
+        </v-alert>
+      </v-flex>
+      <!-- Colored Border -->
+      <v-flex>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Colored Border</h3>
+        <v-alert
+          show
+          type="success"
+          border="right"
+          colored-border
+        >
+          This is a success alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="info"
+          border="bottom"
+          colored-border
+        >
+          This is an info alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="warning"
+          border="left"
+          colored-border
+        >
+          This is a warning alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="error"
+          border="top"
+          colored-border
+        >
+          This is an error alert.
+        </v-alert>
+      </v-flex>
+      <!-- Outline -->
+      <v-flex>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Outline</h3>
+        <v-alert
+          show
+          type="success"
+          outlined
+        >
+          This is a success alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="info"
+          outlined
+        >
+          This is an info alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="warning"
+          outlined
+        >
+          This is a warning alert.
+        </v-alert>
+
+        <v-alert
+          show
+          type="error"
+          outlined
+        >
+          This is an error alert.
+        </v-alert>
+      </v-flex>
+      <!-- Closable -->
+      <v-flex>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Closable</h3>
         <v-alert
           v-model="alertDismissible"
           dismissible
-          type="success"
+          color="blue-grey"
+          dark
         >
-          This is a success alert that is closable.
+          <div>
+            <!-- eslint-disable max-len -->
+            Nullam vel sem. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Sed hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.
+          </div>
         </v-alert>
 
         <div class="text-xs-center">
@@ -55,29 +237,32 @@
           </v-btn>
         </div>
       </v-flex>
-
+      <!-- Custom icons -->
       <v-flex>
-        <h3 class="title grey--text mb-4 mt-5">Custom icons</h3>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Custom icons</h3>
         <v-alert
-          :value="true"
-          color="success"
+          show
+          color="purple"
+          dark
           icon="mdi-account-check"
         >
-          This is a success alert with a custom icon.
+          This is a purple alert with a custom icon.
         </v-alert>
 
         <v-alert
-          :value="true"
-          color="error"
+          show
+          type="error"
+          dense
         >
           This is an error alert with no icon.
         </v-alert>
       </v-flex>
-
+      <!-- CSS Transitions -->
       <v-flex>
-        <h3 class="title grey--text mb-4 mt-5">Custom transitions</h3>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">CSS transitions</h3>
         <div class="text-xs-center">
           <v-btn
+            class="mb-3"
             color="primary"
             @click="alertTransition = !alertTransition"
           >
@@ -87,45 +272,57 @@
         <v-alert
           v-model="alertTransition"
           type="success"
-          transition="scale-transition"
+          transition="fade-transition"
         >
           This is a success alert.
         </v-alert>
       </v-flex>
-
+      <!-- Slots -->
       <v-flex>
-        <h3 class="title grey--text mb-4 mt-5">Outline</h3>
+        <h3 class="title grey--text text-xs-center mb-4 mt-5">Slots</h3>
         <v-alert
-          :value="true"
-          color="success"
-          outline
+          v-model="alertTransition2"
+          class="mb-0"
+          show
+          color="blue darken-3"
+          dark
         >
-          This is a success alert.
+          <template slot="prepend">
+            <v-avatar
+              class="pa-2 mr-3"
+              color="white"
+              size="64"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+                height="100%"
+                contain
+              />
+            </v-avatar>
+          </template>
+          <div>
+            <!-- eslint-disable max-len -->
+            Nullam vel sem. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Sed hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.
+          </div>
+          <v-btn
+            slot="close"
+            slot-scope="{ toggle }"
+            icon
+            @click="toggle"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
         </v-alert>
-
-        <v-alert
-          :value="true"
-          color="info"
-          outline
-        >
-          This is an info alert.
-        </v-alert>
-
-        <v-alert
-          :value="true"
-          color="warning"
-          outline
-        >
-          This is a warning alert.
-        </v-alert>
-
-        <v-alert
-          :value="true"
-          color="error"
-          outline
-        >
-          This is a error alert.
-        </v-alert>
+        <div class="text-xs-center">
+          <v-btn
+            v-if="!alertTransition2"
+            color="primary"
+            dark
+            @click="alertTransition2 = true"
+          >
+            Reset
+          </v-btn>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
@@ -137,7 +334,14 @@ export default {
 
   data: () => ({
     alertDismissible: true,
-    alertTransition: true
+    alertTransition: true,
+    alertTransition2: true
   })
 }
 </script>
+
+<style>
+  .v-alert {
+    margin-bottom: 16px;
+  }
+</style>
--- packages/kitchen/src/plugins/vuetify.js
+++ packages/kitchen/src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.css'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'mdi'
