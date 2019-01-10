b1ade7c23f052bd3ff1baf84e3614ce4ef38957a
sh7dm <d3dx12.xx@gmail.com>
2019-01-10 21:17:13 +0300

    docs(VCalendar): add slots and event list examples

---
+++ packages/vuetifyjs.com/src/examples/calendars/eventList.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-calendar color="primary">
+        <template
+          slot="day"
+          slot-scope="day"
+        >
+          <div
+            v-for="event in events"
+            :key="event.name"
+          >
+            <span
+              v-if="day.date == event.date"
+            >
+              {{ event.name }}
+            </span>
+          </div>
+        </template>
+      </v-calendar>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      events: [
+        {
+          name: 'New Year',
+          date: '2019-01-01'
+        },
+        {
+          name: 'Conference',
+          date: '2019-01-21'
+        },
+        {
+          name: 'Hackathon',
+          date: '2019-02-01'
+        }
+      ]
+    })
+  }
+</script>
---
+++ packages/vuetifyjs.com/src/examples/calendars/slots.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-calendar color="primary">
+        <template
+          slot="day"
+          slot-scope="day"
+        >
+          <div v-if="day.present">
+            Today
+          </div>
+        </template>
+      </v-calendar>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({})
+  }
+</script>
