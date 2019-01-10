ab35e135063338aab469329bbe8d67e5648cfbdd
John Leider <john.j.leider@gmail.com>
2019-01-09 23:08:25 -0500

    refactor(VAlert): implement styles and cleanup file

--- packages/vuetify/src/components/VAlert/VAlert.ts
+++ packages/vuetify/src/components/VAlert/VAlert.ts
@@ -1,26 +1,36 @@
 // Styles
-import '../../stylus/components/_alerts.styl'
+import './VAlert.sass'
+
+// Extensions
+import VSheet from '../VSheet'
 
 // Components
 import VIcon from '../VIcon'
 
 // Mixins
-import Colorable from '../../mixins/colorable'
-import Toggleable from '../../mixins/toggleable'
+import { factory as ToggleableFactory } from '../../mixins/toggleable'
 import Transitionable from '../../mixins/transitionable'
 
 // Types
 import { VNode } from 'vue/types'
 import mixins from '../../util/mixins'
+import { breaking, deprecate } from '../../util/console'
 
 /* @vue/component */
-export default mixins(Colorable, Toggleable, Transitionable).extend({
+export default mixins(
+  VSheet,
+  ToggleableFactory('show'),
+  Transitionable
+).extend({
   name: 'v-alert',
 
   props: {
     dismissible: Boolean,
     icon: String,
     outline: Boolean,
+    outlined: Boolean,
+    prominent: Boolean,
+    show: Boolean,
     type: {
       type: String,
       validator (val: string) {
@@ -31,39 +41,30 @@ export default mixins(Colorable, Toggleable, Transitionable).extend({
           'warning'
         ].includes(val)
       }
+    },
+    /* @deprecated */
+    value: {
+      type: Boolean,
+      default: undefined
+    }
+  },
+
+  created () {
+    if (this.value !== undefined) {
+      breaking('value', 'show', this)
+    }
+
+    if (this.outline) {
+      deprecate('outline', 'outlined')
     }
   },
 
   computed: {
-    computedColor (): string {
-      return (this.type && !this.color) ? this.type : (this.color || 'error')
-    },
-    computedIcon (): string | void {
-      if (this.icon || !this.type) return this.icon
-
-      switch (this.type) {
-        case 'info': return '$vuetify.icons.info'
-        case 'error': return '$vuetify.icons.error'
-        case 'success': return '$vuetify.icons.success'
-        case 'warning': return '$vuetify.icons.warning'
-      }
-    }
-  },
-
-  methods: {
-    genIcon (): VNode | null {
-      if (!this.computedIcon) return null
-
-      return this.$createElement(VIcon, {
-        'class': 'v-alert__icon'
-      }, this.computedIcon)
-    },
-
-    genDismissible (): VNode | null {
+    __cachedDismissible (): VNode | null {
       if (!this.dismissible) return null
 
       return this.$createElement('a', {
-        'class': 'v-alert__dismissible',
+        staticClass: 'v-alert__dismissible',
         on: { click: () => { this.isActive = false } }
       }, [
         this.$createElement(VIcon, {
@@ -72,29 +73,73 @@ export default mixins(Colorable, Toggleable, Transitionable).extend({
           }
         }, '$vuetify.icons.cancel')
       ])
+    },
+    __cachedIcon (): VNode | null {
+      if (!this.computedIcon) return null
+
+      return this.$createElement(VIcon, {
+        staticClass: 'v-alert__icon'
+      }, this.computedIcon)
+    },
+    classes (): object {
+      return {
+        ...VSheet.options.computed.classes.call(this),
+        'v-alert--outline': this.hasOutline,
+        'v-alert--prominent': this.prominent
+      }
+    },
+    computedColor (): string {
+      return (this.type && !this.color) ? this.type : (this.color || 'info')
+    },
+    computedIcon (): string | void {
+      switch (this.type) {
+        case 'info': return '$vuetify.icons.info'
+        case 'error': return '$vuetify.icons.error'
+        case 'success': return '$vuetify.icons.success'
+        case 'warning': return '$vuetify.icons.warning'
+        default: return this.icon
+      }
+    },
+    hasOutline (): boolean {
+      return this.outline || this.outlined
+    }
+  },
+
+  methods: {
+    genAlert (): VNode {
+      const setColor = this.hasOutline ? this.setTextColor : this.setBackgroundColor
+      const children = [
+        this.$slots.prepend || this.__cachedIcon,
+        this.genContent(),
+        this.$scopedSlots.append
+          ? this.$scopedSlots.append({ toggle: this.toggle })
+          : this.__cachedDismissible
+      ]
+
+      return this.$createElement('div', setColor(this.computedColor, {
+        staticClass: 'v-alert',
+        class: this.classes,
+        style: this.styles,
+        directives: [{
+          name: 'show',
+          value: this.isActive
+        }]
+      }), children)
+    },
+    genContent (): VNode {
+      return this.$createElement('div', {
+        staticClass: 'v-alert__content'
+      }, this.$slots.default)
+    },
+    toggle () {
+      this.isActive = !this.isActive
     }
   },
 
   render (h): VNode {
-    const children = [
-      this.genIcon(),
-      h('div', this.$slots.default),
-      this.genDismissible()
-    ] as any
-    const setColor = this.outline ? this.setTextColor : this.setBackgroundColor
-    const alert = h('div', setColor(this.computedColor, {
-      staticClass: 'v-alert',
-      'class': {
-        'v-alert--outline': this.outline
-      },
-      directives: [{
-        name: 'show',
-        value: this.isActive
-      }],
-      on: this.$listeners
-    }), children)
+    const render = this.genAlert()
 
-    if (!this.transition) return alert
+    if (!this.transition) return render
 
     return h('transition', {
       props: {
@@ -102,6 +147,6 @@ export default mixins(Colorable, Toggleable, Transitionable).extend({
         origin: this.origin,
         mode: this.mode
       }
-    }, [alert])
+    }, [render])
   }
 })
