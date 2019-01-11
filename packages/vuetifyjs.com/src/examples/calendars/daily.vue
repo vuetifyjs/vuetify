a960a17444a4b591c3874d70ae6b5021a0374bee
sh7dm <d3dx12.xx@gmail.com>
2019-01-11 20:08:50 +0300

    docs(VCalendar): add daily example

---
+++ packages/vuetifyjs.com/src/examples/calendars/daily.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-calendar color="primary" type="day">
+        <template
+          slot="dayHeader"
+          slot-scope="day"
+        >
+          <div v-if="day.present" class="text-xs-center">
+            Today
+          </div>
+        </template>
+
+        <template
+          slot="interval"
+          slot-scope="date"
+        >
+          <div
+            class="text-xs-center"
+          >
+            {{ date.hour }} o'clock
+          </div>
+        </template>
+      </v-calendar>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({})
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .v-calendar {
+    min-height: 400px;
+  }
+</style>
