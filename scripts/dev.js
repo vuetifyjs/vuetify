077eafea6626514e4a1f2c7eac7eda3fac0fc4ef
Andrew Henry <AMajesticPotatoe@gmail.com>
2019-01-12 02:50:06 -0500

    chore(scripts): add dev alias

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,10 @@
 const shell = require('shelljs')
 
 const target = process.argv[2]
+const alias = {
+  docs: 'vuetifyjs.com'
+}
+target = alias[target] || target
 
 if (!target) {
   shell.exec('lerna run dev --scope vuetify --stream')
