f9a857886dd425011b39710be0e7e5a7e709d47e
sh7dm <d3dx12.xx@gmail.com>
2019-01-12 08:58:57 +0300

    test(VChip): convert to TS

--- packages/vuetify/src/components/VChip/__tests__/VChip.spec.ts
+++ packages/vuetify/src/components/VChip/__tests__/VChip.spec.ts
@@ -1,20 +1,51 @@
-import VChip from '../index'
-import { test } from '@/test'
+// Libraries
+import Vue from 'vue'
 
-test('VChip.vue', ({ mount, compileToFunctions }) => {
+// Plugins
+import Router from 'vue-router'
+
+// Components
+import VChip from '../VChip'
+
+// Utilities
+import {
+  createLocalVue,
+  mount,
+  Wrapper
+} from '@vue/test-utils'
+
+describe('VBtn.ts', () => {
+  let mountFunction: (options?: object) => Wrapper<Vue>
+  let router: Router
+  let localVue: typeof Vue
+
+  beforeEach(() => {
+    router = new Router()
+    localVue = createLocalVue()
+    localVue.use(Router)
+
+    mountFunction = (options = {}) => {
+      return mount(VChip, {
+        localVue,
+        router,
+        ...options
+      })
+    }
+  })
+
   it('should have a v-chip class', () => {
-    const wrapper = mount(VChip)
+    const wrapper = mountFunction()
 
-    expect(wrapper.hasClass('v-chip')).toBe(true)
+    expect(wrapper.classes()).toContain('v-chip')
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should be removable', () => {
-    const wrapper = mount(VChip, {
+    const wrapper = mountFunction({
       propsData: { close: true }
     })
 
-    const close = wrapper.find('.v-chip__close')[0]
+    const close = wrapper.find('.v-chip__close')
 
     const input = jest.fn(value => wrapper.setProps({ value }))
     wrapper.vm.$on('input', input)
@@ -27,7 +58,7 @@ test('VChip.vue', ({ mount, compileToFunctions }) => {
   })
 
   it('should render a colored chip', () => {
-    const wrapper = mount(VChip, {
+    const wrapper = mountFunction({
       propsData: {
         color: 'blue',
         textColor: 'green'
@@ -39,7 +70,7 @@ test('VChip.vue', ({ mount, compileToFunctions }) => {
   })
 
   it('should render a disabled chip', () => {
-    const wrapper = mount(VChip, {
+    const wrapper = mountFunction({
       propsData: {
         disabled: true
       }
@@ -50,11 +81,11 @@ test('VChip.vue', ({ mount, compileToFunctions }) => {
     wrapper.setProps({
       close: true
     })
-    expect(wrapper.find('.v-chip__close')).toHaveLength(1)
+    expect(wrapper.findAll('.v-chip__close')).toHaveLength(1)
   })
 
   it('should render a colored outline chip', () => {
-    const wrapper = mount(VChip, {
+    const wrapper = mountFunction({
       propsData: {
         outline: true,
         color: 'blue'
@@ -66,7 +97,7 @@ test('VChip.vue', ({ mount, compileToFunctions }) => {
   })
 
   it('should render a colored outline chip with text color', () => {
-    const wrapper = mount(VChip, {
+    const wrapper = mountFunction({
       propsData: {
         outline: true,
         color: 'blue',
