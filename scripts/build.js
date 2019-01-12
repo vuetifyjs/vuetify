e3bea99ef2e631840979daf853add404bc723bba
Andrew Henry <AMajesticPotatoe@gmail.com>
2019-01-12 02:48:27 -0500

    chore(scripts): update build alias

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,8 @@ const shell = require('shelljs')
 
 let target = process.argv[2]
 const alias = {
-  api: '@vuetify/api-generator'
+  api: '@vuetify/api-generator',
+  docs: 'vuetifyjs.com'
 }
 target = alias[target] || target
 
