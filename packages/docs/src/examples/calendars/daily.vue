1f3dd1ebfa0e57a74e199d55f5b4fc8bead6c672
John Leider <john@vuetifyjs.com>
2019-01-14 21:11:59 -0600

    feat(VCalendar): add new component (#6092)
    * feat(VCalendar): add new component
    
    * Remaining VCalendar source
    
    * Fix styl formatting and refactor VTimestampFormatter
    
    * Initial Calendar documentation
    
    * chore(docs): remove unused style
    
    * docs(VCalendar): remove twitterCard example
    
    * docs(VCalendar): add more relevant components to the up next section
    
    * docs(VCalendar): add slots and event list examples
    
    * Remove functions from docs & make start not required.
    
    * Add updateTimes function
    
    * Added move/next/prev functions, and more classes for styling calendars.
    
    * docs(VCalendar): add min-height to examples
    
    * docs(VCalendar): add weekly example
    
    * docs(VCalendar): add daily example
    
    * Added time helpers, examples, scrollToTime,
    
    * Fix now prop and add popover for events example
    
    * Cleaned up examples with John's guidance

---
+++ packages/docs/src/examples/calendars/daily.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-sheet height="400">
+        <v-calendar color="primary" type="day">
+          <template
+            slot="dayHeader"
+            slot-scope="{ present }"
+          >
+            <div v-if="present" class="text-xs-center">
+              Today
+            </div>
+          </template>
+
+          <template
+            slot="interval"
+            slot-scope="{ hour }"
+          >
+            <div
+              class="text-xs-center"
+            >
+              {{ hour }} o'clock
+            </div>
+          </template>
+        </v-calendar>
+      </v-sheet>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({})
+  }
+</script>
