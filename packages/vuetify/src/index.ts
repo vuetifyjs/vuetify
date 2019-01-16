361e00ee47fb488bacd2b5ad108f82d74773e4dc
John Leider <john.j.leider@gmail.com>
2019-01-16 09:21:27 -0500

    refactor: improve bootstrap process

--- packages/vuetify/src/index.ts
+++ packages/vuetify/src/index.ts
@@ -2,76 +2,43 @@ import {
   VuetifyService,
   VuetifyServiceInstance,
 } from 'vuetify/types/services'
-import Vue, { VueConstructor } from 'vue'
-import { install } from './install'
+import { VueConstructor } from 'vue'
 import defaultPreset from './presets/default'
+import { install } from './install'
+import { VuetifyPreset } from 'vuetify/types/presets'
 import * as services from './services'
-import { keys as objectKeys } from './util/helpers'
 
 export default class Vuetify {
   framework: Record<string, VuetifyServiceInstance> = {}
-  userPreset: any
-  rootInstance: Vue | undefined = undefined
+  installed: string[] = []
+  defaultPreset: VuetifyPreset = defaultPreset
+  userPreset?: Partial<VuetifyPreset>
 
   static install: (Vue: VueConstructor) => void
   static version: string
 
-  constructor (options?: any) {
-    this.registerPreset(options)
-    this.registerServices()
-  }
-
-  get preset () {
-    return this.mergeOptions(this.userPreset, defaultPreset)
-  }
-
-  registerPreset (preset: any) {
+  constructor (preset?: Partial<VuetifyPreset>) {
     this.userPreset = preset
+
+    this.use(services.Application)
+    this.use(services.Icons)
+    this.use(services.Theme)
   }
 
-  private mergeOptions (
-    preset: any,
-    defaultPreset: any
-  ): any {
-    if (preset == null) return defaultPreset
-
+  // TODO: Improve merge strategy
+  get options (): VuetifyPreset {
     return {
-      ssr: preset.ssr != undefined ? preset.ssr : defaultPreset.ssr,
-      locale: {
-        ...defaultPreset.locale,
-        ...preset.locale
-      },
-      icons: {
-        iconfont: (
-          preset.icons!.iconfont ||
-          defaultPreset.icons.iconfont
-        ),
-        values: {
-          ...defaultPreset.icons.values,
-          ...preset.icons!.values
-        }
-      },
-      theme: {
-        default: (
-          preset.theme!.default ||
-          defaultPreset.theme.default
-        ),
-        themes: {
-          ...defaultPreset.theme.themes,
-          ...preset.theme!.themes
-        }
-      }
+      ...this.defaultPreset,
+      ...this.userPreset
     }
   }
 
-  private registerServices () {
-    objectKeys(services).forEach(key => {
-      const service: VuetifyService = services[key]
-      const property = service.property
-      const options = this.preset[property]
+  use (service: VuetifyService) {
+    const property = service.property
 
-      this.framework[property] = new service(options)
-    })
+    if (this.installed.includes(property)) return
+
+    this.framework[property] = new service(this.options[property])
   }
 }
 
