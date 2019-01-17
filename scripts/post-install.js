682362c2d37aee8e1e12f98c2599e172d8f79adb
John Leider <john.j.leider@gmail.com>
2019-01-17 12:30:04 -0500

    chore(vuetify): add back local dev env

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -2,12 +2,21 @@ const shell = require('shelljs')
 const fs = require('fs')
 const path = require('path')
 const resolve = target => path.resolve(__dirname, target)
-const targetFolder = '../packages/playground/src/views'
-const targetFile = `${targetFolder}/Playground.vue`
+const playgroundTargetFolder = '../packages/playground/src/views'
+const playgroundTargetFile = `${playgroundTargetFolder}/Playground.vue`
+const devTargetFolder = '../packages/vuetify/dev'
+const devTargetFile = `${devTargetFolder}/Playground.vue`
 
-if (!fs.existsSync(resolve(targetFile))) {
+if (!fs.existsSync(resolve(playgroundTargetFile))) {
   shell.cp(
-    resolve(`${targetFolder}/Playground.template.vue`),
-    resolve(targetFile)
+    resolve(`${playgroundTargetFolder}/Playground.template.vue`),
+    resolve(playgroundTargetFile)
+  )
+}
+
+if (!fs.existsSync(resolve(devTargetFile))) {
+  shell.cp(
+    resolve(`${devTargetFolder}/Playground.template.vue`),
+    resolve(devTargetFile)
   )
 }
