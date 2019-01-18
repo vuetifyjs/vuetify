d525273189d2ddf7ba3e60529d0a1a2b54f0429a
Andrew Henry <AMajesticPotatoe@gmail.com>
2019-01-18 09:06:45 -0500

    docs(example): fix lint errors

--- packages/docs/src/examples/data-tables/expand.vue
+++ packages/docs/src/examples/data-tables/expand.vue
@@ -4,14 +4,14 @@
       <v-toolbar-title>Expandable Table</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn color="primary" dark @click="expand = !expand">
-         {{ expand ? 'Close' : 'Keep' }} other rows
+        {{ expand ? 'Close' : 'Keep' }} other rows
       </v-btn>
     </v-toolbar>
     <v-data-table
       :headers="headers"
       :items="desserts"
-      item-key="name"
       :expand="expand"
+      item-key="name"
     >
       <template slot="items" slot-scope="props">
         <tr @click="props.expanded = !props.expanded">
