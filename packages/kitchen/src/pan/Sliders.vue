4f1c26181277c79d0d5000ebd31c1e2963c43b78
Albert Kaaman <albert@kaaman.nu>
2019-01-19 21:53:52 +0100

    refactor(VSlider): started work on refactoring VSlider for 2.0, and including vertical option

---
+++ packages/kitchen/src/pan/Sliders.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-layout row>
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Basic
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="basic" />
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Disabled
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="disabled" disabled />
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Read only
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="disabled" readonly />
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
+      <v-layout>
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Basic
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="basic" vertical />
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Disabled
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="disabled" vertical disabled />
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Read only
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="disabled" vertical readonly />
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
+      <v-layout>
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Label
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="basic" label="Label" />
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Inverse label
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="basic" label="Label" inverse-label />
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
+      <v-layout>
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Label
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="basic" label="Label" />
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <h3 class="title grey--text mb-4 mt-5 center">
+            Inverse label
+          </h3>
+          <v-layout row align-center>
+            <v-slider v-model="basic" label="Label" vertical inverse-label />
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Sliders',
+
+    data: () => ({
+      basic: 0,
+      disabled: 20
+    })
+  }
+</script>
+
+<style scoped>
+.center {
+  align-self: center;
+}
+</style>
