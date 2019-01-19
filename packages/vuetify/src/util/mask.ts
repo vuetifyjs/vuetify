02a14cd59bd977583f7093771f99b7433e175b48
sh7dm <d3dx12.xx@gmail.com>
2019-01-19 19:45:04 +0300

    style(mask): remove docblocks

--- packages/vuetify/src/util/mask.ts
+++ packages/vuetify/src/util/mask.ts
@@ -4,26 +4,10 @@ export interface MaskItem {
 }
 export type MaskType = '#' | 'A' | 'a' | 'N' | 'n' | 'X'
 
-/**
- * Default delimiter RegExp
- *
- * @type {RegExp}
- */
 export const defaultDelimiters = /[-!$%^&*()_+|~=`{}[\]:";'<>?,./\\ ]/
 
-/**
- *
- * @param {String} char
- *
- * @return {Boolean}
- */
 export const isMaskDelimiter = (char: string): boolean => char ? defaultDelimiters.test(char) : false
 
-/**
- * Mask keys
- *
- * @type {Object}
- */
 const allowedMasks: Record<MaskType, MaskItem> = {
   '#': {
     test: (char: string): boolean => Boolean(char.match(/[0-9]/)),
@@ -51,52 +35,17 @@ const allowedMasks: Record<MaskType, MaskItem> = {
   }
 }
 
-/**
- * Is Character mask
- *
- * @param  {String} char
- *
- * @return {Boolean}
- */
 const isMask = (char: string): boolean => allowedMasks.hasOwnProperty(char)
 
-/**
- * Automatically convert char case
- *
- * @param  {String} mask
- * @param  {String} char
- *
- * @return {String}
- */
 const convert = (mask: MaskType, char: string): string => {
   return allowedMasks[mask].convert ? allowedMasks[mask].convert(char) : char
 }
 
-/**
- * Mask Validation
- *
- * @param  {String} mask
- * @param  {String} char
- *
- * @return {Boolean}
- */
 const maskValidates = (mask: MaskType, char: string): boolean => {
   if (char == null || !isMask(mask)) return false
   return allowedMasks[mask].test(char)
 }
 
-/**
- * Mask Text
- *
- * Takes a string or an array of characters
- * and returns a masked string
- *
- * @param {*} text
- * @param {Array|String} masked
- * @param {Boolean} [dontFillMaskBlanks]
- *
- * @return {String}
- */
 export const maskText = (text: string | any[], masked: string | any[], dontFillMaskBlanks: boolean): string => {
   if (text == null) return ''
   text = String(text)
@@ -135,13 +84,6 @@ export const maskText = (text: string | any[], masked: string | any[], dontFillM
   return newText
 }
 
-/**
- * Unmask Text
- *
- * @param {String} text
- *
- * @return {String}
- */
 export const unmaskText = (text: string): string => {
   return text ? String(text).replace(new RegExp(defaultDelimiters, 'g'), '') : text
 }
