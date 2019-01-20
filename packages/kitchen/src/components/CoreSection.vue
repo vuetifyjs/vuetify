be3cd08cfacd5dcece826f8030a112f55aaba191
sh7dm <d3dx12.xx@gmail.com>
2019-01-20 09:18:48 +0300

    docs(kitchen): add CoreSection component

---
+++ packages/kitchen/src/components/CoreSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-flex :align-self-center="center">
+    <slot>
+      Content
+    </slot>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    name: 'CoreTitle',
+    props: [ 'center' ]
+  }
+</script>
