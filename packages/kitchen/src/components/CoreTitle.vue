f2d6974d30059054ecf2af60ea6a0c120422a888
sh7dm <d3dx12.xx@gmail.com>
2019-01-20 09:17:05 +0300

    docs(kitchen): add fallback content to CoreTitle component

--- packages/kitchen/src/components/CoreTitle.vue
+++ packages/kitchen/src/components/CoreTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <v-flex align-self-center>
-    <h3 class="title grey--text mb-4 mt-5 center"><slot /></h3>
+    <h3 class="title grey--text mb-4 mt-5 center"><slot>Title</slot></h3>
   </v-flex>
 </template>
 
