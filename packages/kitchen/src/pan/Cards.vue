7e09750a99f91887f6456ab03a0d624e1c034e4b
sh7dm <d3dx12.xx@gmail.com>
2019-01-23 16:57:22 +0300

    docs(kitchen): fix lint errors

--- packages/kitchen/src/pan/Cards.vue
+++ packages/kitchen/src/pan/Cards.vue
@@ -181,8 +181,7 @@
           <v-img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
             height="200px"
-          >
-          </v-img>
+          />
 
           <v-card-title primary-title>
             <div>
@@ -194,7 +193,7 @@
           <v-card-actions>
             <v-btn flat>Share</v-btn>
             <v-btn flat color="purple">Explore</v-btn>
-            <v-spacer></v-spacer>
+            <v-spacer />
             <v-btn icon @click="show = !show">
               <v-icon>{{ show ? 'mdi-chevron-down' : 'mdi-chevron-up' }}</v-icon>
             </v-btn>
@@ -202,7 +201,10 @@
 
           <v-slide-y-transition>
             <v-card-text v-show="show">
-              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier,
+              not with all the bed making you'll be doing. Then we'll go with that data file!
+              Hey, you add a one and two zeros to that or we walk!
+              You're going to do his laundry? I've got to find a way to escape.
             </v-card-text>
           </v-slide-y-transition>
         </v-card>
