cfa0a7f78b9f615958ac2948799c834571904926
Dmitry Sharshakov <sh7dm@outlook.com>
2019-01-29 17:45:49 +0300

    docs(kitchen): add VChipGroup pan (#6286)
    ## Description
    Add VChipGroup pan.
    
    ## Motivation and Context
    New component needs a pan. :shallow_pan_of_food: 
    
    ## Types of changes
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] Improvement/refactoring (non-breaking change that doesn't add any feature but make things better)
    
    ## Checklist:
    - [x] The PR title is no longer than 64 characters.
    - [ ] The PR is submitted to the correct branch (`master` for bug fixes and documentation updates, `dev` for new features and breaking changes).
    - [x] My code follows the code style of this project.
    - [x] I've added relevant changes to the documentation (applies to new features and breaking changes in core library)

---
+++ packages/kitchen/src/pan/Chip Groups.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <main-header>Item Groups</main-header>
+
+      <core-title>Simple</core-title>
+      <core-section center>
+        <v-chip-group>
+          <v-chip
+            v-for="n in 3"
+            :key="n"
+          >
+            Example Chip
+          </v-chip>
+        </v-chip-group>
+      </core-section>
+
+      <core-title>Multiple</core-title>
+      <core-section center>
+        <v-chip-group multiple>
+          <v-chip
+            v-for="n in 3"
+            :key="n"
+          >
+            Example Chip
+          </v-chip>
+        </v-chip-group>
+      </core-section>
+
+      <core-title>Active class</core-title>
+      <core-section center>
+        <v-chip-group active-class="success">
+          <v-chip
+            v-for="n in 3"
+            :key="n"
+          >
+            Example Chip
+          </v-chip>
+        </v-chip-group>
+      </core-section>
+
+      <core-title>Column</core-title>
+      <core-section style="max-width: 300px;" center>
+        <v-chip-group column multiple>
+          <v-chip
+            v-for="n in 3"
+            :key="n"
+          >
+            Example Chip
+          </v-chip>
+        </v-chip-group>
+      </core-section>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'ChipGroups',
+
+    data: () => ({})
+  }
+</script>
