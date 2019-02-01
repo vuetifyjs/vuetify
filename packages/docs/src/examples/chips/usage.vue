48e1eebfcaec5102e74350ad2d1a6b93aecae2ba
John Leider <john.j.leider@gmail.com>
2019-02-01 15:19:56 -0500

    docs(VChip): add new usage example, add missing prop defs

--- packages/docs/src/examples/chips/usage.vue
+++ packages/docs/src/examples/chips/usage.vue
@@ -1,139 +1,49 @@
 <template>
-  <div>
-    <div class="text-xs-center py-4">
-      <v-chip
-        :close-icon="close === 'custom' ? 'mdi-close-outline' : undefined"
-        :close="!!close"
-        :color="color"
-        :filter="variant.includes('filter')"
-        :label="variant.includes('label')"
-        :link="variant.includes('link')"
-        :outlined="variant.includes('outlined')"
-        :pill="variant.includes('pill')"
-        :input-value="value"
+  <div
+    id="chip-usage-example"
+    class="text-xs-center"
+  >
+    <v-chip>Default</v-chip>
+
+    <v-chip
+      color="red lighten-2"
+      text-color="white"
+    >
+      <v-avatar
+        left
+        color="white"
+        class="text--primary"
       >
-        <v-avatar
-          v-if="avatar !== ''"
-          :color="avatar === 'letter' ? 'secondary' : ''"
-          left
-        >
-          <span
-            v-if="avatar === 'letter'"
-            class="white--text"
-          >V</span>
-          <v-img
-            v-else-if="avatar === 'img'"
-            src="https://cdn.vuetifyjs.com/images/lists/2.jpg"
-          ></v-img>
-          <v-icon v-else>mdi-account-circle</v-icon>
-        </v-avatar>
-        <v-icon
-          v-if="!!icon"
-          left
-        >
-          mdi-account-circle
-        </v-icon>
-        Chip Component
-      </v-chip>
-    </div>
+        V
+      </v-avatar>
+      Letter
+    </v-chip>
 
-    <v-card>
-      <v-card-text>
-        <span>Color</span>
-        <v-radio-group
-          v-model="color"
-          row
-        >
-          <v-radio label="default" value=""></v-radio>
-          <v-radio label="primary" value="primary"></v-radio>
-          <v-radio label="warning" value="warning"></v-radio>
-        </v-radio-group>
+    <v-chip
+      color="primary"
+      pill
+    >
+      Pill Chip
+      <v-avatar
+        color="primary darken-2"
+        right
+      >
+        $
+      </v-avatar>
+    </v-chip>
 
-        <span>Close</span>
-        <v-radio-group
-          v-model="close"
-          row
-        >
-          <v-radio label="none" value=""></v-radio>
-          <v-radio label="default" value="default"></v-radio>
-          <v-radio label="custom" value="custom"></v-radio>
-        </v-radio-group>
-
-        <span>Icon</span>
-        <v-radio-group
-          v-model="icon"
-          row
-        >
-          <v-radio
-            label="none"
-            value=""
-          ></v-radio>
-          <v-radio
-            label="icon"
-            value="mdi-check"
-          ></v-radio>
-        </v-radio-group>
-
-        <span>Avatar</span>
-        <v-radio-group
-          v-model="avatar"
-          row
-        >
-          <v-radio
-            label="none"
-            value=""
-          ></v-radio>
-          <v-radio
-            label="letter"
-            value="letter"
-          ></v-radio>
-          <v-radio
-            label="img"
-            value="img"
-          ></v-radio>
-          <v-radio
-            label="icon"
-            value="icon"
-          ></v-radio>
-        </v-radio-group>
-
-        <v-select
-          v-model="variant"
-          :items="['outlined', 'label', 'pill', 'filter', 'link']"
-          box
-          chips
-          deletable-chips
-          label="Variants"
-          multiple
-          small-chips
-        >
-
-        </v-select>
-
-        <span>State</span>
-        <v-switch v-model="value" label="active"></v-switch>
-      </v-card-text>
-    </v-card>
+    <v-chip
+      label
+      color="deep-purple accent-4"
+      text-color="white"
+    >
+      Label
+    </v-chip>
   </div>
 </template>
 
-<script>
-  export default {
-    data: () => ({
-      avatar: '',
-      close: '',
-      color: '',
-      icon: '',
-      value: false,
-      variant: []
-    }),
-
-    watch: {
-      variant (val) {
-        if (val.includes('filter')) {
-          this.value = true
-        }
-      }
-    }
+<style>
+  #chip-usage-example .v-chip {
+    margin: 8px;
   }
-</script>
+</style>
