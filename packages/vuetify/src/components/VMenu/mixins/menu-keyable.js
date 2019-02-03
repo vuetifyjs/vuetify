f7ccda630b4c0527ac4280082910ebbda63f0ac4
Kael <kaelwd@gmail.com>
2019-02-04 02:28:20 +1100

    fix(VMenu): don't prevent default on keydown unless action taken
    fixes #2988

--- packages/vuetify/src/components/VMenu/mixins/menu-keyable.js
+++ packages/vuetify/src/components/VMenu/mixins/menu-keyable.js
@@ -53,23 +53,19 @@ export default {
       }
     },
     changeListIndex (e) {
-      if ([
-        keyCodes.down,
-        keyCodes.up,
-        keyCodes.enter
-      ].includes(e.keyCode)
-      ) e.preventDefault()
-
       // For infinite scroll and autocomplete, re-evaluate children
       this.getTiles()
 
       if (e.keyCode === keyCodes.down && this.listIndex < this.tiles.length - 1) {
+        e.preventDefault()
         this.listIndex++
         // Allow user to set listIndex to -1 so
         // that the list can be un-highlighted
       } else if (e.keyCode === keyCodes.up && this.listIndex > -1) {
+        e.preventDefault()
         this.listIndex--
       } else if (e.keyCode === keyCodes.enter && this.listIndex !== -1) {
+        e.preventDefault()
         this.tiles[this.listIndex].click()
       }
     },
