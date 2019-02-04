e653ba6ee456b431264b7d0d542e2221458cab26
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-02-03 19:53:03 -0500

    feat(locale): add Slovenian locale

---
+++ packages/vuetify/src/locale/sl.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Predmetov na stran:',
+    rowsPerPageAll: 'Vse',
+    pageText: '{0}-{1} od {2}',
+    noResultsText: 'Ni iskanega zapisa',
+    nextPage: 'Naslednja stran',
+    prevPage: 'Prejšnja stran'
+  },
+  dataTable: {
+    rowsPerPageText: 'Vrstic na stran:'
+  },
+  noDataText: 'Ni podatkov'
+}
