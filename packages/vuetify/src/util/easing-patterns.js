9d73a99c50199cf362ee407578c534cb5ea0a0fc
John Leider <john.j.leider@gmail.com>
2019-02-03 22:18:24 -0500

    chore(easing-patterns): add deprecation notice

---
+++ packages/vuetify/src/util/easing-patterns.js
@@ -0,0 +1,5 @@
+import { deprecate } from './console'
+
+export * from '../components/Vuetify/goTo/easing-patterns'
+
+deprecate('vuetify/es5/util/easing-patterns', 'vuetify/es5/Vuetify/goTo/easing-patterns')
