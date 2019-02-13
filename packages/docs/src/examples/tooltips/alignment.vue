327e5d74621a9c56cc1323698702a70a4ff2c543
sh7dm <d3dx12.xx@gmail.com>
2019-02-13 18:13:29 +0300

    docs: fix props order in VTooltip examples to avoid lint warnings

--- packages/docs/src/examples/tooltips/alignment.vue
+++ packages/docs/src/examples/tooltips/alignment.vue
@@ -2,28 +2,28 @@
   <div class="text-xs-center">
     <v-tooltip left>
       <template #activator="data">
-        <v-btn v-on="data.on" color="primary" dark>Left</v-btn>
+        <v-btn color="primary" dark v-on="data.on">Left</v-btn>
       </template>
       <span>Left tooltip</span>
     </v-tooltip>
 
     <v-tooltip top>
       <template #activator="data">
-        <v-btn v-on="data.on" color="primary" dark>Top</v-btn>
+        <v-btn color="primary" dark v-on="data.on">Top</v-btn>
       </template>
       <span>Top tooltip</span>
     </v-tooltip>
 
     <v-tooltip bottom>
       <template #activator="data">
-        <v-btn v-on="data.on" color="primary" dark>Bottom</v-btn>
+        <v-btn color="primary" dark v-on="data.on">Bottom</v-btn>
       </template>
       <span>Bottom tooltip</span>
     </v-tooltip>
 
     <v-tooltip right>
       <template #activator="data">
-        <v-btn v-on="data.on" color="primary" dark>Right</v-btn>
+        <v-btn color="primary" dark v-on="data.on">Right</v-btn>
       </template>
       <span>Right tooltip</span>
     </v-tooltip>
