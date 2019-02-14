89678591b61ceead749cc9e7306d49373d717508
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-02-13 20:30:40 -0500

    docs(examples): convert transition slots

--- packages/docs/src/examples/transitions/customOrigin.vue
+++ packages/docs/src/examples/transitions/customOrigin.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="text-xs-center">
     <v-menu transition="scale-transition" origin="center center">
-      <v-btn slot="activator" dark color="primary">Scale Transition</v-btn>
+      <template #activator="{ on: menu }">
+        <v-btn
+          dark
+          color="primary"
+          v-on="{ ...menu }"
+        >
+          Scale Transition
+        </v-btn>
+      </template>
       <v-list>
         <v-list-tile v-for="n in 5" :key="n" @click="">
           <v-list-tile-title v-text="'Item ' + n"></v-list-tile-title>
--- packages/docs/src/examples/transitions/fadeTransition.vue
+++ packages/docs/src/examples/transitions/fadeTransition.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="text-xs-center">
     <v-menu transition="fade-transition">
-      <v-btn slot="activator" dark color="primary">Fade Transition</v-btn>
+      <template #activator="{ on: menu }">
+        <v-btn
+          dark
+          color="primary"
+          v-on="{ ...menu }"
+        >
+          Fade Transition
+        </v-btn>
+      </template>
       <v-list>
         <v-list-tile v-for="n in 5" :key="n" @click="">
           <v-list-tile-title v-text="'Item ' + n"></v-list-tile-title>
