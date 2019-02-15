ed306a2ea2cb9c1d85592912902c1e1493e7c1d3
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-02-14 21:48:32 -0500

    docs(examples): convert expansionpanel slots

--- packages/docs/src/examples/expansion-panels/customIcons.vue
+++ packages/docs/src/examples/expansion-panels/customIcons.vue
@@ -5,7 +5,9 @@
       :key="i"
       expand-icon="mdi-menu-down"
     >
-      <div slot="header">Item</div>
+      <template v-slot:header>
+        <div>Item</div>
+      </template>
       <v-card>
         <v-card-text class="grey lighten-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
       </v-card>
