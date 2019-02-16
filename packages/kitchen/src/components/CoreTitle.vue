b1038b380f4d8b4e4f5c63ffc2f7d74b0747b339
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-02-16 13:52:35 +0300

    docs: make core-title h4

--- packages/kitchen/src/components/CoreTitle.vue
+++ packages/kitchen/src/components/CoreTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <v-flex align-self-center>
-    <h3 class="title grey--text mb-4 mt-5"><slot>Title</slot></h3>
+    <h4 class="title grey--text mb-4 mt-5"><slot>Title</slot></h4>
   </v-flex>
 </template>
 
