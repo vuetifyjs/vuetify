ffcbc0d3455568f6bc7a4e1809b18b0b343dd2ae
sh7dm <d3dx12.xx@gmail.com>
2019-02-16 18:34:30 +0300

    feat(VBanner): add new component

---
+++ packages/kitchen/src/pan/Banners.vue
@@ -0,0 +1,225 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <main-header>Banners</main-header>
+
+      <sub-header>One-line</sub-header>
+
+      <core-title>Default</core-title>
+      <core-section>
+        <v-banner>
+          No Internet connection
+        </v-banner>
+      </core-section>
+
+      <core-title>With actions</core-title>
+      <core-section>
+        <v-banner>
+          No Internet connection
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With icon</core-title>
+      <core-section>
+        <v-banner icon="mdi-wifi-strength-alert-outline">
+          No Internet connection
+        </v-banner>
+      </core-section>
+
+      <core-title>With icon slot</core-title>
+      <core-section>
+        <v-banner>
+          <v-icon slot="icon" color="warning" size="40">mdi-wifi-strength-alert-outline</v-icon>
+          No Internet connection
+        </v-banner>
+      </core-section>
+
+      <core-title>With action and icon</core-title>
+      <core-section>
+        <v-banner icon="mdi-wifi-strength-alert-outline">
+          No Internet connection
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With actions and icon</core-title>
+      <core-section>
+        <v-banner icon="mdi-wifi-strength-alert-outline">
+          No Internet connection
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With click:icon event</core-title>
+      <core-section>
+        <v-banner icon="mdi-wifi-strength-alert-outline" @click:icon="alert">
+          No Internet connection
+        </v-banner>
+      </core-section>
+
+      <core-title>With click:icon event and icon slot</core-title>
+      <core-section>
+        <v-banner @click:icon="alert">
+          <v-icon slot="icon" color="warning" size="40">mdi-wifi-strength-alert-outline</v-icon>
+          No Internet connection
+        </v-banner>
+      </core-section>
+
+      <core-title>Visibility control</core-title>
+      <core-section>
+        <v-checkbox v-model="v1" label="Visible" />
+        <v-banner v-model="v1" icon="mdi-wifi-strength-alert-outline">
+          No Internet connection
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>Visibility control + transition</core-title>
+      <core-section>
+        <v-checkbox v-model="v2" label="Visible" />
+        <v-banner v-model="v2" transition="slide-y-transition" icon="mdi-wifi-strength-alert-outline">
+          No Internet connection
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <sub-header>Two-line</sub-header>
+
+      <core-title>Default</core-title>
+      <core-section>
+        <v-banner two-line>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+        </v-banner>
+      </core-section>
+
+      <core-title>With icon</core-title>
+      <core-section>
+        <v-banner two-line icon="mdi-wifi-strength-alert-outline">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+        </v-banner>
+      </core-section>
+
+      <core-title>With icon slot</core-title>
+      <core-section>
+        <v-banner two-line>
+          <v-icon slot="icon" size="40" color="warning">mdi-wifi-strength-alert-outline</v-icon>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With actions</core-title>
+      <core-section>
+        <v-banner two-line>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With actions and icon</core-title>
+      <core-section>
+        <v-banner two-line icon="mdi-wifi-strength-alert-outline">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With click:icon event</core-title>
+      <core-section>
+        <v-banner two-line icon="mdi-wifi-strength-alert-outline" @click:icon="alert">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>With click:icon event and icon slot</core-title>
+      <core-section>
+        <v-banner two-line @click:icon="alert">
+          <v-icon slot="icon" size="40" color="warning">mdi-wifi-strength-alert-outline</v-icon>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>Visibility control</core-title>
+      <core-section>
+        <v-checkbox v-model="v3" label="Visible" />
+        <v-banner two-line v-model="v3" icon="mdi-wifi-strength-alert-outline">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+
+      <core-title>Visibility control + transition</core-title>
+      <core-section>
+        <v-checkbox v-model="v4" label="Visible" />
+        <v-banner two-line transition="slide-y-transition" v-model="v4" icon="mdi-wifi-strength-alert-outline">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+          Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+          <template slot="actions">
+            <v-btn text color="accent">Retry</v-btn>
+            <v-btn outline color="error"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+          </template>
+        </v-banner>
+      </core-section>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Banners',
+
+    data: () => ({
+      v1: true,
+      v2: true,
+      v3: true,
+      v4: true
+    }),
+
+    methods: {
+      alert () {
+        alert('Clicked on icon')
+      }
+    }
+  }
+</script>
---
+++ packages/vuetify/src/components/VBanner/VBanner.ts
@@ -0,0 +1,129 @@
+// Styles
+import './VBanner.sass'
+
+// Components
+import VSheet from '../VSheet'
+import VIcon from '../VIcon'
+
+// Mixins
+import Toggleable from '../../mixins/toggleable'
+import Transitionable from '../../mixins/transitionable'
+
+// Types
+import { VNode, VNodeData, VNodeChildren } from 'vue/types'
+import mixins from '../../util/mixins'
+
+/* @vue/component */
+export default mixins(
+  VSheet,
+  Toggleable,
+  Transitionable
+).extend({
+  name: 'v-banner',
+
+  props: {
+    icon: String,
+    value: {
+      type: Boolean,
+      default: true
+    },
+    twoLine: {
+      type: Boolean,
+      default: false
+    },
+    threeLine: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    classes (): object {
+      return {
+        ...VSheet.options.computed.classes.call(this),
+        'v-banner--one-line': !(this.twoLine || this.threeLine),
+        'v-banner--two-line': this.twoLine,
+        'v-banner--three-line': this.threeLine
+      }
+    }
+  },
+
+  methods: {
+    /** @public */
+    toggle () {
+      this.isActive = !this.isActive
+    },
+    iconClick (e: MouseEvent) {
+      this.$emit('click:icon', e)
+    },
+    genIcon () {
+      let content
+
+      if (this.icon) {
+        content = this.$createElement(VIcon, {
+          props: {
+            size: 40
+          }
+        }, [ this.icon ])
+      } else {
+        content = this.$slots.icon
+      }
+
+      if (content) {
+        return this.$createElement('div', {
+          staticClass: 'v-banner__icon',
+          on: {
+            click: this.iconClick
+          }
+        }, [
+          content
+        ])
+      } else {
+        return undefined
+      }
+    },
+    genContent () {
+      return this.$createElement('div', { staticClass: 'v-banner__content' }, this.$slots.default)
+    },
+    genActions () {
+      if (this.$slots.actions) {
+        return this.$createElement('div', { staticClass: 'v-banner__actions' }, this.$slots.actions)
+      } else {
+        return undefined
+      }
+    },
+    genBanner () {
+      const data: VNodeData = {
+        staticClass: 'v-banner',
+        class: this.classes,
+        style: this.styles,
+        directives: [{
+          name: 'show',
+          value: this.isActive
+        }]
+      }
+
+      const children: VNodeChildren = [
+        this.genIcon(),
+        this.genContent(),
+        this.genActions()
+      ]
+
+      return this.$createElement('div', data, children)
+    }
+  },
+
+  render (h): VNode {
+    const render = this.genBanner()
+
+    if (!this.transition) return render
+
+    return h('transition', {
+      props: {
+        name: this.transition,
+        origin: this.origin,
+        mode: this.mode
+      }
+    }, [render])
+  }
+})
