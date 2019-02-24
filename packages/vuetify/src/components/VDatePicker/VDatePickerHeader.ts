814a3728f7eb86511296a6300fa11bfc9aba6d4b
Jacek Karczmarczyk <jkarczm@gmail.com>
2019-02-25 00:04:23 +0700

    feat(VDatePicker): use $vuetify.lang.current (#6320)
    * feat(VDatePicker): use vuetify.lang.current
    
    fixes #5618
    
    * feat(localable): new mixin with user defined or global locale

--- packages/vuetify/src/components/VDatePicker/VDatePickerHeader.ts
+++ packages/vuetify/src/components/VDatePicker/VDatePickerHeader.ts
@@ -6,6 +6,7 @@ import VIcon from '../VIcon'
 
 // Mixins
 import Colorable from '../../mixins/colorable'
+import Localable from '../../mixins/localable'
 import Themeable from '../../mixins/themeable'
 
 // Utils
@@ -19,6 +20,7 @@ import { PropValidator } from 'vue/types/options'
 
 export default mixins(
   Colorable,
+  Localable,
   Themeable
 /* @vue/component */
 ).extend({
@@ -27,10 +29,6 @@ export default mixins(
   props: {
     disabled: Boolean,
     format: Function as PropValidator<DatePickerFormatter | undefined>,
-    locale: {
-      type: String,
-      default: 'en-us'
-    },
     min: String,
     max: String,
     nextIcon: {
@@ -59,9 +57,9 @@ export default mixins(
       if (this.format) {
         return this.format
       } else if (String(this.value).split('-')[1]) {
-        return createNativeLocaleFormatter(this.locale, { month: 'long', year: 'numeric', timeZone: 'UTC' }, { length: 7 })
+        return createNativeLocaleFormatter(this.currentLocale, { month: 'long', year: 'numeric', timeZone: 'UTC' }, { length: 7 })
       } else {
-        return createNativeLocaleFormatter(this.locale, { year: 'numeric', timeZone: 'UTC' }, { length: 4 })
+        return createNativeLocaleFormatter(this.currentLocale, { year: 'numeric', timeZone: 'UTC' }, { length: 4 })
       }
     }
   },
