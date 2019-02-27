ffefa9dea07e704cec62213ef6dd75e2164a4958
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-02-26 21:34:25 -0500

    chore(menuable): remove unused test import

--- packages/vuetify/test/unit/mixins/menuable.spec.js
+++ packages/vuetify/test/unit/mixins/menuable.spec.js
@@ -1,6 +1,5 @@
 import { test } from '@/test'
 import Menuable from '@/mixins/menuable'
-import VBtn from '@/components/VBtn'
 
 test('menuable.js', ({ mount }) => {
   it('should bind custom activator', () => {
