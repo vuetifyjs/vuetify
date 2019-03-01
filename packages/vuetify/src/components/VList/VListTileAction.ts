7ca296b1fa9176a15da83e6f252fff7b7f84c874
John Leider <john.j.leider@gmail.com>
2019-02-28 20:54:54 -0500

    refactor(VList): remove the need to define left and right for actions/icons/avatars

--- packages/vuetify/src/components/VList/VListTileAction.ts
+++ packages/vuetify/src/components/VList/VListTileAction.ts
@@ -7,19 +7,12 @@ export default Vue.extend({
 
   functional: true,
 
-  props: {
-    left: Boolean,
-    right: Boolean
-  },
-
   render (h, { data, children = [], props }): VNode {
     data.staticClass = data.staticClass ? `v-list__tile__action ${data.staticClass}` : 'v-list__tile__action'
     const filteredChild = children.filter(VNode => {
       return VNode.isComment === false && VNode.text !== ' '
     })
     if (filteredChild.length > 1) data.staticClass += ' v-list__tile__action--stack'
-    if (props.left) data.staticClass += ' v-list__tile__action--left'
-    if (props.right) data.staticClass += ' v-list__tile__action--right'
 
     return h('div', data, children)
   }
--- packages/vuetify/src/components/VList/VListTileIcon.ts
+++ packages/vuetify/src/components/VList/VListTileIcon.ts
@@ -7,16 +7,8 @@ export default Vue.extend({
 
   functional: true,
 
-  props: {
-    left: Boolean,
-    right: Boolean
-  },
-
   render (h, { data, children, props }): VNode {
     data.staticClass = (`v-list__tile__icon ${data.staticClass || ''}`).trim()
-
-    if (props.left) data.staticClass += ' v-list__tile__icon--left'
-    if (props.right) data.staticClass += ' v-list__tile__icon--right'
 
     return h('div', data, children)
   }
