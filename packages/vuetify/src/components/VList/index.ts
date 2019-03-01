36f04893ed136439ba0412e6f0f3129df679ef35
John Leider <john.j.leider@gmail.com>
2019-02-28 20:45:19 -0500

    refactor(VList): specification updates

--- packages/vuetify/src/components/VList/index.ts
+++ packages/vuetify/src/components/VList/index.ts
@@ -10,7 +10,7 @@ import VListTileIcon from './VListTileIcon'
 export const VListTileActionText = createSimpleFunctional('v-list__tile__action-text', 'span')
 export const VListTileContent = createSimpleFunctional('v-list__tile__content', 'div')
 export const VListTileTitle = createSimpleFunctional('v-list__tile__title', 'div')
-export const VListTileSubTitle = createSimpleFunctional('v-list__tile__sub-title', 'div')
+export const VListTileSubTitle = createSimpleFunctional('v-list__tile__subtitle', 'div')
 
 export {
   VList,
