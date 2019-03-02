16cd8f1965d71c883535517271418dd5172f9daf
sh7dm <d3dx12.xx@gmail.com>
2019-03-02 09:42:45 +0300

    revert:test(VWindowItem): migrate rtl mock to mount fn
    This reverts commit 2cd50079e174236f25deb4a25a9eccde18f9f185.

--- packages/vuetify/src/components/VWindow/__tests__/VWindowItem.spec.ts
+++ packages/vuetify/src/components/VWindow/__tests__/VWindowItem.spec.ts
@@ -34,11 +34,6 @@ describe('VWindowItem.ts', () => {
       return mount(VWindowItem, {
         localVue,
         router,
-        mocks: {
-          $vuetify: {
-            rtl: false
-          }
-        },
         ...options
       })
     }
@@ -48,6 +43,11 @@ describe('VWindowItem.ts', () => {
     const wrapper = mount(VWindow, {
       slots: {
         default: [VWindowItem]
+      },
+      mocks: {
+        $vuetify: {
+          rtl: false
+        }
       }
     })
 
