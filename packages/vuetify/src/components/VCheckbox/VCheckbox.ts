d66255fa73d59da2e1833e31c59eef011d5bef05
sh7dm <d3dx12.xx@gmail.com>
2019-03-05 14:17:55 +0300

    chore: use this in data function

--- packages/vuetify/src/components/VCheckbox/VCheckbox.ts
+++ packages/vuetify/src/components/VCheckbox/VCheckbox.ts
@@ -31,10 +31,10 @@ export default Selectable.extend({
     }
   },
 
-  data: () => ({ inputIndeterminate: false }),
-
-  created () {
-    this.inputIndeterminate = this.indeterminate
+  data () {
+    return {
+      inputIndeterminate: this.indeterminate
+    }
   },
 
   computed: {
