e5edab764a5c550e95f10d38971426c9e6eeb847
sh7dm <d3dx12.xx@gmail.com>
2019-03-08 19:07:10 +0300

    docs(VAppBar): add an example for fade-img-on-scroll property

---
+++ packages/docs/src/examples/app-bars/img-fade.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="hide-overflow">
+    <v-app-bar
+      absolute
+      color="teal lighten-3"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      :imgProps="{ gradient: 'to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)' }"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-3"
+      class="scroll-y"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
