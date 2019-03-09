a73a571d6546a997ea0e2f0af73f46fd76edada0
sh7dm <d3dx12.xx@gmail.com>
2019-03-09 17:33:54 +0300

    docs(VAppBar): add more examples to kitchen

--- packages/kitchen/src/pan/App Bars.vue
+++ packages/kitchen/src/pan/App Bars.vue
@@ -1,13 +1,47 @@
 <template>
   <v-container>
     <v-layout column>
-
       <core-title>
         With icons
       </core-title>
       <core-section>
         <v-app-bar
           dark
+          color="primary"
+        >
+          <v-toolbar-side-icon />
+
+          <v-toolbar-title class="white--text">
+            Title
+          </v-toolbar-title>
+
+          <v-spacer />
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+      </core-section>
+
+      <core-title>
+        Dense
+      </core-title>
+      <core-section>
+        <v-app-bar
+          dark
+          dense
           color="primary"
         >
           <v-toolbar-side-icon />
@@ -223,8 +257,8 @@
         </v-app-bar>
 
         <v-sheet
-          class="scroll-y"
           id="scroll-1"
+          class="scroll-y"
           max-height="100"
         >
           <v-container style="height: 1500px;" />
@@ -278,8 +312,115 @@
         </v-app-bar>
 
         <v-sheet
-          class="scroll-y"
           id="scroll-2"
+          class="scroll-y"
+          max-height="100"
+        >
+          <v-container style="height: 1500px;" />
+        </v-sheet>
+      </core-section>
+
+      <core-title>
+        Shrink on scroll
+      </core-title>
+      <core-section>
+        <v-app-bar
+          color="primary"
+          dark
+          src="https://picsum.photos/1920/1080?random"
+          shrink-on-scroll
+          scroll-target="#scroll-3"
+          scroll-threshold="500"
+          extended
+        >
+          <template #img="{ props }">
+            <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)" />
+          </template>
+
+          <v-toolbar-side-icon />
+
+          <template #extension>
+            <v-toolbar-title
+              class="white--text"
+            >
+              Title
+            </v-toolbar-title>
+          </template>
+
+          <v-spacer />
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <v-sheet
+          id="scroll-3"
+          class="scroll-y"
+          max-height="100"
+        >
+          <v-container style="height: 1500px;" />
+        </v-sheet>
+      </core-section>
+
+      <core-title>
+        Collapsing on scroll
+      </core-title>
+      <core-section>
+        <v-app-bar
+          color="primary"
+          dark
+          src="https://picsum.photos/1920/1080?random"
+          collapse-on-scroll
+          scroll-target="#scroll-4"
+          scroll-threshold="500"
+        >
+          <template #img="{ props }">
+            <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)" />
+          </template>
+
+          <v-toolbar-title
+            class="white--text"
+          >
+            Title
+          </v-toolbar-title>
+
+          <v-toolbar-side-icon />
+
+          <v-spacer />
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <v-sheet
+          id="scroll-4"
+          class="scroll-y"
           max-height="100"
         >
           <v-container style="height: 1500px;" />
