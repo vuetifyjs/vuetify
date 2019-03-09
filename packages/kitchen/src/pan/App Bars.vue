7aa286a4ac9e8f6b9e1525b7357dcc0ae3291e25
sh7dm <d3dx12.xx@gmail.com>
2019-03-09 13:41:42 +0300

    docs(VAppBar): add image examples to kitchen

--- packages/kitchen/src/pan/App Bars.vue
+++ packages/kitchen/src/pan/App Bars.vue
@@ -146,8 +146,8 @@
           extended
           src="https://picsum.photos/1920/1080?random"
         >
-          <template #img="img">
-            <v-img v-bind="img" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)" />
+          <template #img="{ props }">
+            <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)" />
           </template>
 
           <v-toolbar-side-icon />
@@ -178,6 +178,112 @@
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </v-app-bar>
+      </core-section>
+
+      <core-title>
+        With extension and fading background image
+      </core-title>
+      <core-section>
+        <v-app-bar
+          color="primary"
+          dark
+          extended
+          src="https://picsum.photos/1920/1080?random"
+          fade-img-on-scroll
+          scroll-target="#scroll-1"
+          scroll-threshold="500"
+        >
+          <v-toolbar-side-icon />
+
+          <template #extension>
+            <v-toolbar-title
+              class="white--text"
+            >
+              Title
+            </v-toolbar-title>
+          </template>
+
+          <v-spacer />
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <v-sheet
+          class="scroll-y"
+          max-height="100"
+          id="scroll-1"
+        >
+          <v-container style="height: 1500px;"></v-container>
+        </v-sheet>
+      </core-section>
+
+      <core-title>
+        With extension and fading background image scoped slot
+      </core-title>
+      <core-section>
+        <v-app-bar
+          color="primary"
+          dark
+          extended
+          src="https://picsum.photos/1920/1080?random"
+          fade-img-on-scroll
+          scroll-target="#scroll-2"
+          scroll-threshold="500"
+        >
+          <template #img="{ props }">
+            <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)" />
+          </template>
+
+          <v-toolbar-side-icon />
+
+          <template #extension>
+            <v-toolbar-title
+              class="white--text"
+            >
+              Title
+            </v-toolbar-title>
+          </template>
+
+          <v-spacer />
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <v-sheet
+          class="scroll-y"
+          max-height="100"
+          id="scroll-2"
+        >
+          <v-container style="height: 1500px;"></v-container>
+        </v-sheet>
       </core-section>
     </v-layout>
   </v-container>
