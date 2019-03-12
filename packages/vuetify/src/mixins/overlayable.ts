950f07e29e017e957ec2e3b82597a790a6253fa7
Kael <kaelwd@gmail.com>
2019-03-13 04:31:43 +1100

    fix(overlayable): don't hide scrollbar on desktop browsers

--- packages/vuetify/src/mixins/overlayable.ts
+++ packages/vuetify/src/mixins/overlayable.ts
@@ -1,6 +1,9 @@
 // Styles
 import '../stylus/components/_overlay.styl'
 
+// Utilities
+import { keyCodes } from '../util/helpers'
+
 // Types
 import Vue from 'vue'
 
@@ -31,6 +34,7 @@ export default Vue.extend<Vue & Toggleable & Stackable & options>().extend({
   data () {
     return {
       overlay: null as HTMLElement | null,
+      overlayOffset: 0,
       overlayTimeout: undefined as number | undefined,
       overlayTransitionDuration: 500 + 150 // transition + delay
     }
@@ -112,20 +116,117 @@ export default Vue.extend<Vue & Toggleable & Stackable & options>().extend({
         this.overlayTimeout = undefined
       }, this.overlayTransitionDuration)
     },
+    scrollListener (e: WheelEvent & KeyboardEvent) {
+      if (e.type === 'keydown') {
+        if (
+          ['INPUT', 'TEXTAREA', 'SELECT'].includes((e.target as Element).tagName) ||
+          // https://github.com/vuetifyjs/vuetify/issues/4715
+          (e.target as HTMLElement).isContentEditable
+        ) return
+
+        const up = [keyCodes.up, keyCodes.pageup]
+        const down = [keyCodes.down, keyCodes.pagedown]
+
+        if (up.includes(e.keyCode)) {
+          (e as any).deltaY = -1
+        } else if (down.includes(e.keyCode)) {
+          (e as any).deltaY = 1
+        } else {
+          return
+        }
+      }
+
+      if (e.target === this.overlay ||
+        (e.type !== 'keydown' && e.target === document.body) ||
+        this.checkPath(e)) e.preventDefault()
+    },
+    hasScrollbar (el?: Element) {
+      if (!el || el.nodeType !== Node.ELEMENT_NODE) return false
+
+      const style = window.getComputedStyle(el)
+      return ['auto', 'scroll'].includes(style.overflowY!) && el.scrollHeight > el.clientHeight
+    },
+    shouldScroll (el: Element, delta: number) {
+      if (el.scrollTop === 0 && delta < 0) return true
+      return el.scrollTop + el.clientHeight === el.scrollHeight && delta > 0
+    },
+    isInside (el: Element, parent: Element): boolean {
+      if (el === parent) {
+        return true
+      } else if (el === null || el === document.body) {
+        return false
+      } else {
+        return this.isInside(el.parentNode as Element, parent)
+      }
+    },
+    checkPath (e: WheelEvent) {
+      const path = e.path || this.composedPath(e)
+      const delta = e.deltaY
+
+      if (e.type === 'keydown' && path[0] === document.body) {
+        const dialog = this.$refs.dialog
+        const selected = window.getSelection().anchorNode as Element
+        if (dialog && this.hasScrollbar(dialog) && this.isInside(selected, dialog)) {
+          return this.shouldScroll(dialog, delta)
+        }
+        return true
+      }
+
+      for (let index = 0; index < path.length; index++) {
+        const el = path[index]
+
+        if (el === document) return true
+        if (el === document.documentElement) return true
+        if (el === this.$refs.content) return true
+
+        if (this.hasScrollbar(el as Element)) return this.shouldScroll(el as Element, delta)
+      }
+
+      return true
+    },
+    /**
+     * Polyfill for Event.prototype.composedPath
+     */
+    composedPath (e: WheelEvent): EventTarget[] {
+      if (e.composedPath) return e.composedPath()
+
+      const path = []
+      let el = e.target as Element
+
+      while (el) {
+        path.push(el)
+
+        if (el.tagName === 'HTML') {
+          path.push(document)
+          path.push(window)
+
+          return path
+        }
+
+        el = el.parentElement!
+      }
+      return path
+    },
     hideScroll () {
       const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
-      const pageScroll = window.scrollY
-      document.documentElement.style.top = `${-pageScroll}px`
-      document.documentElement.style.paddingRight = `${scrollbarWidth}px`
-      document.documentElement.classList.add('v-overlay-visible')
+      if (!scrollbarWidth) {
+        const pageScroll = window.scrollY
+        document.documentElement.style.top = `${-pageScroll}px`
+        document.documentElement.classList.add('v-overlay-visible')
+      } else {
+        window.addEventListener('wheel', this.scrollListener as EventHandlerNonNull, { passive: false })
+        window.addEventListener('keydown', this.scrollListener as EventHandlerNonNull)
+      }
     },
     showScroll () {
       const pageScroll = -parseFloat(document.documentElement.style.top!)
-      document.documentElement.style.top = null
-      document.documentElement.style.height = null
-      document.documentElement.style.paddingRight = null
-      document.documentElement.classList.remove('v-overlay-visible')
-      window.scrollTo(window.scrollX, pageScroll)
+      if (pageScroll) {
+        document.documentElement.style.top = null
+        document.documentElement.classList.remove('v-overlay-visible')
+        window.scrollTo(window.scrollX, pageScroll)
+      }
+      window.removeEventListener('wheel', this.scrollListener as EventHandlerNonNull)
+      window.removeEventListener('keydown', this.scrollListener as EventHandlerNonNull)
     }
   }
 })
