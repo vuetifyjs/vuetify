a94e4d06f60b2b1d89a67392b1e2fc547b24be1a
John Leider <john.j.leider@gmail.com>
2019-03-13 12:22:27 -0400

    fix(VExpansionPanelItem): add to component export

--- packages/vuetify/src/components/VExpansionPanel/index.ts
+++ packages/vuetify/src/components/VExpansionPanel/index.ts
@@ -1,11 +1,13 @@
 import VExpansionPanel from './VExpansionPanel'
+import VExpansionPanelItem from './VExpansionPanelItem'
 import VExpansionPanelContent from './VExpansionPanelContent'
 
-export { VExpansionPanel, VExpansionPanelContent }
+export { VExpansionPanel, VExpansionPanelContent, VExpansionPanelItem }
 
 export default {
   $_vuetify_subcomponents: {
     VExpansionPanel,
-    VExpansionPanelContent
+    VExpansionPanelContent,
+    VExpansionPanelItem
   }
 }
