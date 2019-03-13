5851d3872cdb26f88466d29756d01fb395a865fb
John Leider <john.j.leider@gmail.com>
2019-03-13 18:46:38 -0400

    docs(VExpansionPanel): expand upon variants example

--- packages/docs/src/examples/expansion-panels/popout.vue
+++ packages/docs/src/examples/expansion-panels/popout.vue
@@ -32,6 +32,22 @@
           </v-expansion-panel-item>
         </v-expansion-panel>
       </v-flex>
+
+      <v-flex xs12>
+        <v-expansion-panel accordion>
+          <v-expansion-panel-item
+            v-for="(item,i) in 5"
+            :key="i"
+          >
+            <template v-slot:header>
+              <div>Item</div>
+            </template>
+            <v-card>
+              <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+            </v-card>
+          </v-expansion-panel-item>
+        </v-expansion-panel>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
