8f73fb323355c8bf854173ccb75def700f754b33
sh7dm <d3dx12.xx@gmail.com>
2019-03-14 16:33:05 +0300

    feat(VFeatureDiscovery): add new component
    Co-authored-by: Jamie Curnow <jamie@island-web.ca>

--- packages/vuetify/src/components/VFeatureDiscovery/VFeatureDiscovery.ts
+++ packages/vuetify/src/components/VFeatureDiscovery/VFeatureDiscovery.ts
@@ -1,40 +1,26 @@
 // Styles
-import './VFeatureDiscovery.sass'
+import './VFeatureDiscovery.styl'
 
 // Mixins
 import Colorable from '../../mixins/colorable'
-import Elevatable from '../../mixins/elevatable'
 import Toggleable from '../../mixins/toggleable'
 import Themeable from '../../mixins/themeable'
+import { factory as PositionableFactory } from '../../mixins/positionable'
 
 // Types
-import { VNode } from 'vue'
-import mixins, { ExtractVue } from '../../util/mixins'
+import { VNode, VNodeChildren } from 'vue'
+import mixins from '../../util/mixins'
 
 // Directives
 import ClickOutside from '../../directives/click-outside'
 
-// Helpers
-import { convertToUnit, keyCodes, getSlot } from '../../util/helpers'
-import { PropValidator } from 'vue/types/options'
-
-const doubledSqrt2 = 2.8284
-
-const baseMixins = mixins(
+export default mixins(
   Colorable,
-  Elevatable,
   Toggleable,
-  Themeable
-)
-
-interface options extends ExtractVue<typeof baseMixins> {
-  $refs: {
-    target: HTMLElement
-  }
-}
-
-/* @vue/component */
-export default baseMixins.extend<options>().extend({
+  Themeable,
+  PositionableFactory(['left', 'top'])
+  /* @vue/component */
+).extend({
   name: 'v-feature-discovery',
 
   directives: { ClickOutside },
@@ -46,280 +32,125 @@ export default baseMixins.extend<options>().extend({
       type: String,
       default: 'primary'
     },
-    highlightColor: {
-      type: String,
-      default: 'white'
-    },
-    size: {
-      default: 700,
+    height: {
+      default: 400,
       type: [Number, String],
-      validator: (v: string | number) => !isNaN(parseInt(v))
+      validator: (v: string | number): boolean => !isNaN(parseInt(v))
     },
-    target: {
-      default: null as string | HTMLElement | null
-    } as PropValidator<string | HTMLElement>,
     value: {
       default: true
-    },
-    noRipple: {
-      default: false,
-      type: Boolean
-    },
-    textColor: {
-      type: String,
-      default: 'white'
     }
   },
-
-  data: () => ({
-    rect: {
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      height: 0,
-      width: 0
-    },
-    targetEl: null as HTMLElement | null
-  }),
 
   computed: {
     classes (): object {
       return {
         'v-feature-discovery--flat': this.flat,
-        'v-feature-discovery--active': this.internalActive,
-        'v-feature-discovery--lr-shifted': this.leftShift !== 0,
-        'v-feature-discovery--no-ripple': this.noRipple,
+        'v-feature-discovery--active': this.isActive,
+        'v-feature-discovery--top-left': this.top && this.left,
+        'v-feature-discovery--top-right': this.top && !this.left,
+        'v-feature-discovery--bottom-left': !this.top && this.left,
+        'v-feature-discovery--bottom-right': !this.top && !this.left,
         ...this.themeClasses
       }
     },
-    computedLeft (): number {
-      return this.rect.left - (this.computedSize / 2) + (this.rect.width / 2)
-    },
-    computedRight (): number {
-      return window.innerWidth - this.rect.right - (this.computedSize / 2) + (this.rect.width / 2)
-    },
-    shiftedLeft (): number {
-      return this.computedLeft + this.leftShift
-    },
-    computedBottom (): number {
-      return window.innerHeight - this.rect.bottom - (this.computedSize / 2) + (this.rect.height / 2)
-    },
-    computedTop (): number {
-      return this.rect.top - (this.computedSize / 2) + (this.rect.height / 2)
-    },
-    shiftedTop (): number {
-      return this.computedTop + this.topShift
-    },
-    computedSize (): number {
-      return parseInt(this.size)
-    },
-    styles (): object {
+    OuterContainerClasses (): object {
       return {
-        left: convertToUnit(this.shiftedLeft),
-        top: convertToUnit(this.shiftedTop),
-        height: convertToUnit(this.computedSize),
-        width: convertToUnit(this.computedSize)
+        [`justify-${this.left ? 'start' : 'end'}`]: true,
+        [`align-${this.top ? 'baseline' : 'end'}`]: true
       }
     },
-    rectSize (): number {
-      return Math.max(this.rect.width, this.rect.height)
-    },
-    highlightSize (): number {
-      return Math.sqrt(2 * (this.rectSize + this.highlightPadding) ** 2)
-    },
-    highlightPadding (): number {
-      return this.rectSize / 3.5
-    },
-    baseShift (): number {
-      return this.computedSize / doubledSqrt2 * 0.75
-    },
-    leftShift (): number {
-      if (this.computedLeft === this.computedRight) return 0
-      if (this.computedLeft < 0 && this.computedLeft < this.computedRight) return this.baseShift
-      if (this.computedRight < 0) return -this.baseShift
-      return 0
-    },
-    topShift (): number {
-      if (this.computedTop === this.computedBottom) return 0
-      if (this.computedTop < 0 && this.computedTop < this.computedBottom) return this.baseShift
-      if (this.computedBottom < 0) return -this.baseShift
-      return 0
-    },
-    backdropStyle (): object {
-      const out = {
-        'transform-origin': `calc(50% - ${convertToUnit(this.leftShift)}) calc(50% - ${convertToUnit(this.topShift)})`
-      } as { [key: string]: any }
-
-      if (this.leftShift > 0) {
-        out['justify-content'] = 'flex-end'
-        out['padding-right'] = '15%'
-      }
-      if (this.leftShift < 0) {
-        out['justify-content'] = 'flex-start'
-        out['padding-left'] = '15%'
-      }
-
-      return out
-    },
-    contentStyle (): object {
-      let justify = 'space-between'
-      if (this.topShift > 0) justify = 'flex-end'
-      if (this.topShift < 0) justify = 'flex-start'
-
+    innerContentClasses (): any {
       return {
-        'justify-content': justify
-      }
-    },
-    attrs (): object {
-      return {
-        'aria-hidden': !this.internalActive
-      }
-    },
-    internalActive (): boolean {
-      return this.isActive && this.rect.bottom > 0 && window.innerHeight - this.rect.top > 0
-    }
-  },
-
-  watch: {
-    target () {
-      this.updateTarget()
-    },
-    targetEl () {
-      if (this.targetEl) {
-        const newTarget = this.targetEl.cloneNode(true) as HTMLElement
-        newTarget.style.cssText = window.getComputedStyle(this.targetEl).cssText
-        newTarget.style.color = ''
-        newTarget.style.webkitTextFillColor = ''
-        newTarget.style.cssFloat = ''
-        newTarget.style.margin = ''
-        newTarget.style.top = ''
-        newTarget.style.bottom = ''
-        newTarget.style.left = ''
-        newTarget.style.right = ''
-        newTarget.classList.add(this.color + '--text')
-        if (newTarget.style.position === 'fixed') {
-          newTarget.style.position = 'static'
+        title: {
+          'mr-4': this.left && !this.top,
+          'ml-4 mt-3': !this.left && !this.top,
+          'align-end': true
+        },
+        text: {
+          'align-center': true
+        },
+        actions: {
+          'mr-5': this.left && this.top,
+          'ml-5': !this.left && this.top
         }
-
-        while (this.$refs.target.firstChild) {
-          this.$refs.target.firstChild.remove()
-        }
-        this.$refs.target.appendChild(newTarget)
       }
     },
-    isActive (val: boolean) {
-      val && this.updateRect()
+    computedHeight (): number {
+      return parseInt(this.height)
     }
-  },
-
-  mounted () {
-    this.updateTarget()
-
-    window.addEventListener('scroll', this.updateRect)
-    window.addEventListener('resize', this.updateRect)
-    window.addEventListener('keyup', this.keyPress)
   },
 
   methods: {
-    keyPress (e: KeyboardEvent) {
-      if (this.closeConditional() && e.keyCode === keyCodes.esc) this.isActive = false
-    },
-    updateTarget () {
-      if (this.target instanceof HTMLElement) this.targetEl = this.target
-      else this.targetEl = document.querySelector(this.target)
-
-      if (!this.targetEl) return
-
-      this.rect = this.targetEl.getBoundingClientRect()
-    },
-    updateRect () {
-      if (!this.targetEl || !this.isActive) return
-
-      this.rect = this.targetEl.getBoundingClientRect()
-    },
     closeConditional (): boolean {
-      return !this.persistent && this.internalActive
-    },
-    genBackdrop (): VNode {
-      return this.$createElement('div', this.setBackgroundColor(this.color, {
-        staticClass: 'v-feature-discovery__backdrop',
-        class: this.elevationClasses,
-        style: this.backdropStyle
-      }), [ this.genContent() ])
+      return !this.persistent && this.isActive
     },
     genContent (): VNode {
-      return this.$createElement('div', this.setTextColor(this.textColor, {
-        staticClass: 'v-feature-discovery__content',
-        style: this.contentStyle
-      }), [
-        this.genWrapper(),
-        this.genActions()
-      ])
-    },
-    genActions (): VNode {
-      return this.$createElement('div', {
-        staticClass: 'v-feature-discovery__actions'
-      }, getSlot(this, 'actions', {
-        close: () => {
-          if (this.closeConditional()) this.isActive = false
-        }
-      }, true))
-    },
-    genWrapper (): VNode {
-      return this.$createElement('div', {
-        staticClass: 'v-feature-discovery__wrapper'
-      }, [
-        this.$createElement('div', {
-          staticClass: 'v-feature-discovery__title'
-        }, getSlot(this, 'title')),
-        this.$createElement('div', {
-          staticClass: 'v-feature-discovery__text'
-        }, getSlot(this, 'default'))
-      ])
-    },
-    genChildren (): VNode[] {
-      return [
-        this.genBackdrop(),
-        this.genHighlight()
-      ]
-    },
-    genHighlight (): VNode {
-      return this.$createElement('div', this.setTextColor(this.color, this.setBackgroundColor(this.highlightColor, {
-        staticClass: 'v-feature-discovery__highlight',
-        style: {
-          top: `calc(50% - (${convertToUnit(this.highlightSize)} / 2) - ${convertToUnit(this.topShift)})`,
-          left: `calc(50% - (${convertToUnit(this.highlightSize)} / 2) - ${convertToUnit(this.leftShift)})`,
-          height: convertToUnit(this.highlightSize),
-          width: convertToUnit(this.highlightSize)
+      return this.$createElement(
+        'div',
+        {
+          staticClass: 'v-feature-discovery-outer-container layout row wrap fill-height',
+          class: this.OuterContainerClasses
         },
-        attrs: {
-          'aria-hidden': true
-        }
-      })), [this.$createElement('div', {
-        ref: 'target',
-        staticClass: 'v-feature-discovery__target'
-      })])
+        [this.genContainer()]
+      )
+    },
+    genContainer (): VNode {
+      return this.$createElement(
+        'div',
+        {
+          staticClass: 'v-feature-discovery-container flex fill-height',
+          style: {
+            maxWidth: `${this.computedHeight - (this.computedHeight * 0.15)}px`,
+            maxHeight: `${this.computedHeight - (this.computedHeight * 0.2)}px`
+          }
+        },
+        [this.genInnerContainer()]
+      )
+    },
+    genInnerContainer (): VNode {
+      const children: VNodeChildren = this.genInnerContent()
+      return this.$createElement(
+        'div',
+        { staticClass: 'v-feature-discovery-inner-container layout pa-2 column fill-height' },
+        children
+      )
+    },
+    genInnerContent (): VNodeChildren {
+      const content = ['title', 'text', 'actions']
+      return content.map(x => {
+        return this.$createElement(
+          'div',
+          {
+            staticClass: `v-feature-discovery-${x} flex d-flex text-xs-center`,
+            'class': this.innerContentClasses[x]
+          },
+          [this.$slots[x]]
+        )
+      })
     }
   },
 
   render (h): VNode {
     const data = {
-      staticClass: 'v-feature-discovery',
-      directives: [
-        {
-          name: 'click-outside',
-          value: () => /* istanbul ignore next */ (this.isActive = false),
-          args: {
-            closeConditional: this.closeConditional
-          }
+      directives: [{
+        name: 'click-outside',
+        value: () => (this.isActive = false),
+        args: {
+          closeConditional: this.closeConditional
         }
-      ],
+      }] as any,
+      staticClass: 'v-feature-discovery',
       class: this.classes,
-      style: this.styles,
-      attrs: this.attrs
+      style: {
+        height: `${parseInt(this.computedHeight)}px`,
+        width: `${parseInt(this.computedHeight)}px`
+      },
+      ref: 'content'
     }
 
-    return h('div', data, this.genChildren())
+    const children = [this.genContent()]
+
+    return h('div', this.setBackgroundColor(this.color, data), children)
   }
 })
