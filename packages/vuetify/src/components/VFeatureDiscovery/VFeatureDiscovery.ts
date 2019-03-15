fbc57e2ed5f57f71cf7edb0e776e8ebb52abbac4
John Leider <john.j.leider@gmail.com>
2019-03-15 11:59:08 -0400

    refactor(VFeatureDiscovery): start to incorporate specification styling

--- packages/vuetify/src/components/VFeatureDiscovery/VFeatureDiscovery.ts
+++ packages/vuetify/src/components/VFeatureDiscovery/VFeatureDiscovery.ts
@@ -8,17 +8,17 @@ import Themeable from '../../mixins/themeable'
 import { factory as PositionableFactory } from '../../mixins/positionable'
 
 // Types
-import { VNode, VNodeChildren } from 'vue'
+import { VNode } from 'vue'
 import mixins from '../../util/mixins'
 
 // Directives
 import ClickOutside from '../../directives/click-outside'
+import { convertToUnit } from '../../util/helpers'
 
 export default mixins(
   Colorable,
   Toggleable,
-  Themeable,
-  PositionableFactory(['left', 'top'])
+  Themeable
 /* @vue/component */
 ).extend({
   name: 'v-feature-discovery',
@@ -35,122 +35,105 @@ export default mixins(
     size: {
       default: 400,
       type: [Number, String],
-      validator: (v: string | number): boolean => !isNaN(parseInt(v))
+      validator: (v: string | number) => !isNaN(parseInt(v))
+    },
+    target: {
+      default: null
     },
     value: {
       default: true
     }
   },
 
+  data: () => ({
+    rect: {
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: 0,
+      width: 0
+    }
+  }),
+
   computed: {
     classes (): object {
       return {
         'v-feature-discovery--flat': this.flat,
         'v-feature-discovery--active': this.isActive,
-        'v-feature-discovery--top-left': this.top && this.left,
-        'v-feature-discovery--top-right': this.top && !this.left,
-        'v-feature-discovery--bottom-left': !this.top && this.left,
-        'v-feature-discovery--bottom-right': !this.top && !this.left,
         ...this.themeClasses
       }
     },
-    OuterContainerClasses (): object {
-      return {
-        [`justify-${this.left ? 'start' : 'end'}`]: true,
-        [`align-${this.top ? 'baseline' : 'end'}`]: true
-      }
+    computedLeft (): number {
+      return this.rect.left - (this.computedSize / 2) + (this.rect.width / 2)
     },
-    innerContentClasses (): any {
-      return {
-        title: {
-          'mr-4': this.left && !this.top,
-          'ml-4 mt-3': !this.left && !this.top,
-          'align-end': true
-        },
-        text: {
-          'align-center': true
-        },
-        actions: {
-          'mr-5': this.left && this.top,
-          'ml-5': !this.left && this.top
-        }
-      }
+    computedTop (): number {
+      return this.rect.top - (this.computedSize / 2) + (this.rect.height / 2)
     },
     computedSize (): number {
       return parseInt(this.size)
+    },
+    styles (): object {
+      return {
+        left: convertToUnit(this.computedLeft),
+        top: convertToUnit(this.computedTop),
+        height: convertToUnit(this.computedSize),
+        width: convertToUnit(this.computedSize)
+      }
     }
+  },
+
+  mounted () {
+    if (!this.target) return
+
+    const target = document.querySelector(this.target)
+
+    this.rect = target.getBoundingClientRect()
+    target.style.zIndex = 11
   },
 
   methods: {
     closeConditional (): boolean {
       return !this.persistent && this.isActive
     },
-    genContent (): VNode {
-      return this.$createElement(
-        'div',
-        {
-          staticClass: 'v-feature-discovery-outer-container layout row wrap fill-height',
-          class: this.OuterContainerClasses
-        },
-        [this.genContainer()]
-      )
+    genBackdrop () {
+      return this.$createElement('div', this.setBackgroundColor(this.color, {
+        staticClass: 'v-feature-discovery__backdrop'
+      }), this.$slots.default)
     },
-    genContainer (): VNode {
-      return this.$createElement(
-        'div',
-        {
-          staticClass: 'v-feature-discovery-container flex fill-height',
-          style: {
-            maxWidth: `${this.computedSize - (this.computedSize * 0.15)}px`,
-            maxHeight: `${this.computedSize - (this.computedSize * 0.2)}px`
-          }
-        },
-        [this.genInnerContainer()]
-      )
+    genChildren () {
+      return [
+        this.genBackdrop(),
+        this.genHighlight()
+      ]
     },
-    genInnerContainer (): VNode {
-      const children: VNodeChildren = this.genInnerContent()
-      return this.$createElement(
-        'div',
-        { staticClass: 'v-feature-discovery-inner-container layout pa-2 column fill-height' },
-        children
-      )
-    },
-    genInnerContent (): VNodeChildren {
-      const content = ['title', 'text', 'actions']
-      return content.map(x => {
-        return this.$createElement(
-          'div',
-          {
-            staticClass: `v-feature-discovery-${x} flex d-flex text-xs-center`,
-            'class': this.innerContentClasses[x]
-          },
-          [this.$slots[x]]
-        )
-      })
+    genHighlight () {
+      return this.$createElement('div', {
+        staticClass: 'v-feature-discovery__highlight'
+      }, this.$slots.default)
     }
   },
 
   render (h): VNode {
     const data = {
-      directives: [{
-        name: 'click-outside',
-        value: () => /* istanbul ignore next */ (this.isActive = false),
-        args: {
-          closeConditional: this.closeConditional
-        }
-      }] as any,
       staticClass: 'v-feature-discovery',
+      directives: [
+        {
+          name: 'click-outside',
+          value: () => /* istanbul ignore next */ (this.isActive = false),
+          args: {
+            closeConditional: this.closeConditional
+          }
+        },
+        {
+          name: 'show',
+          value: this.isActive
+        }
+      ],
       class: this.classes,
-      style: {
-        height: `${parseInt(this.computedSize)}px`,
-        width: `${parseInt(this.computedSize)}px`
-      },
-      ref: 'content'
+      style: this.styles
     }
 
-    const children = [this.genContent()]
-
-    return h('div', this.setBackgroundColor(this.color, data), children)
+    return h('div', data, this.genChildren())
   }
 })
