8c9312362e653c7643290124c1b274390f819b6e
Boris Dayma <boris.dayma@gmail.com>
2019-03-22 06:55:05 -0500

    test(VSnackbar): migrate to vue-test-utils (#6796)

--- packages/vuetify/src/components/VSnackbar/__tests__/VSnackbar.spec.ts
+++ packages/vuetify/src/components/VSnackbar/__tests__/VSnackbar.spec.ts
@@ -1,49 +1,69 @@
-import { test } from '@/test'
-import VSnackbar from '@/components/VSnackbar'
+// Components
+import VSnackbar from '../VSnackbar'
 
-test('VSnackbar.vue', ({ mount }) => {
+// Utilities
+import {
+  mount,
+  Wrapper
+} from '@vue/test-utils'
+
+// Types
+import { ExtractVue } from '../../../util/mixins'
+
+describe('VSnackbar.ts', () => {
+  type Instance = ExtractVue<typeof VSnackbar>
+  let mountFunction: (options?: object) => Wrapper<Instance>
+
+  beforeEach(() => {
+    mountFunction = (options = {}) => {
+      return mount(VSnackbar, {
+        ...options
+      })
+    }
+  })
+
   it('should have a v-snack class', () => {
-    const wrapper = mount(VSnackbar, {
+    const wrapper = mountFunction({
       propsData: {
         value: true
       }
     })
 
-    expect(wrapper.hasClass('v-snack')).toBe(true)
+    expect(wrapper.classes()).toContain('v-snack')
   })
 
   it('should have a v-snack__wrapper with a color class', () => {
-    const wrapper = mount(VSnackbar, {
+    const wrapper = mountFunction({
       propsData: {
         value: true,
         color: 'orange lighten-2'
       }
     })
 
-    expect(wrapper.find('.v-snack__wrapper.orange')).toHaveLength(1)
-    expect(wrapper.find('.v-snack__wrapper.lighten-2')).toHaveLength(1)
+    expect(wrapper.findAll('.v-snack__wrapper.orange')).toHaveLength(1)
+    expect(wrapper.findAll('.v-snack__wrapper.lighten-2')).toHaveLength(1)
   })
 
   it('should have a v-snack__content class only when active', async () => {
-    const wrapper = mount(VSnackbar, {
+    const wrapper = mountFunction({
       propsData: {
         value: false,
         timeout: 1000
       }
     })
 
-    expect(wrapper.find('div .v-snack__content')).toHaveLength(0)
+    expect(wrapper.findAll('div .v-snack__content')).toHaveLength(0)
 
     wrapper.setProps({ value: true })
 
     await wrapper.vm.$nextTick()
 
-    expect(wrapper.find('div .v-snack__content')).toHaveLength(1)
+    expect(wrapper.findAll('div .v-snack__content')).toHaveLength(1)
   })
 
   it('should timeout correctly', async () => {
     jest.useFakeTimers()
-    const wrapper = mount(VSnackbar, {
+    const wrapper = mountFunction({
       propsData: {
         value: false,
         timeout: 3141
@@ -52,9 +72,9 @@ test('VSnackbar.vue', ({ mount }) => {
 
     const value = jest.fn()
 
-    wrapper.instance().$on('input', value)
+    wrapper.vm.$on('input', value)
     wrapper.setProps({ value: true })
-    wrapper.update()
+    // wrapper.update()
 
     await wrapper.vm.$nextTick()
 
@@ -65,13 +85,13 @@ test('VSnackbar.vue', ({ mount }) => {
 
     await wrapper.vm.$nextTick()
 
-    expect(wrapper.data().isActive).toBe(false)
-    expect(value).toBeCalledWith(false)
+    expect(wrapper.vm.isActive).toBe(false)
+    expect(value).toHaveBeenCalledWith(false)
   })
 
   it('should timeout correctly when initial value is true', async () => {
     jest.useFakeTimers()
-    const wrapper = mount(VSnackbar, {
+    const wrapper = mountFunction({
       propsData: {
         value: true,
         timeout: 3141
@@ -80,7 +100,7 @@ test('VSnackbar.vue', ({ mount }) => {
 
     const value = jest.fn()
 
-    wrapper.instance().$on('input', value)
+    wrapper.vm.$on('input', value)
 
     await wrapper.vm.$nextTick()
 
@@ -91,7 +111,7 @@ test('VSnackbar.vue', ({ mount }) => {
 
     await wrapper.vm.$nextTick()
 
-    expect(wrapper.data().isActive).toBe(false)
-    expect(value).toBeCalledWith(false)
+    expect(wrapper.vm.isActive).toBe(false)
+    expect(value).toHaveBeenCalledWith(false)
   })
 })
