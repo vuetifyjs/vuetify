9149a21c0a3f279f47f8f3301a849a2b84a405e8
Илья <iliyazelenkog@gmail.com>
2019-03-27 19:36:34 +0200

    refactor: delete unused variables (#6850)

--- packages/vuetify/build/babel-transform-stylus-paths.js
+++ packages/vuetify/build/babel-transform-stylus-paths.js
@@ -1,8 +1,6 @@
-var types = require('babel-types');
-var pathLib = require('path');
-var wrapListener = require('babel-plugin-detective/wrap-listener');
+const wrapListener = require('babel-plugin-detective/wrap-listener')
 
-module.exports = wrapListener(listener, 'transform-stylus-paths');
+module.exports = wrapListener(listener, 'transform-stylus-paths')
 
 function listener(path, file, opts) {
   const regex = /((?:\.\.?\/)+)/gi
