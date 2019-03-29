a63b300842c191c65ed646b61bb6f3bf4388c588
John Leider <john@vuetifyjs.com>
2019-03-28 21:34:37 -0400

    feat(VCard): update to md2 (#6788)
    * chore(VCard): move style files, update existing references
    
    * docs(VCard): clean-up examples
    
    * docs(VCard): add new example
    
    * refactor(VCard): convert styles to sass
    
    * chore(VCard): remove deprecated components
    
    * docs(VCard): remove deprecation notice
    
    * fix(VCard): **raised** prop functionality
    
    * feat(VCard): add **outlined** prop
    
    * docs(VCard): update usage example display
    
    * feat(VCard): add support for focus styles
    
    add more options for designating a link
    
    * docs(VCard): add new outlined example
    
    * fix(VCard): remove elevation when using the **outlined** prop
    
    * feat(VCardText): add **measurable** mixin
    
    * feat(loadable): add prop for loader height
    
    * feat(VCard): add **disabled** and **loading** functionality
    
    * fix(VCard): avoid rendering progress if not loading
    
    * fix(VCard): use correct cursor for chips in card links
    
    fixes #3925
    
    * refactor(VCard): better spec matching for title and text
    
    * docs(VCard): update examples, add new **infoCard**
    
    * chore(VCard): move tests to component folder
    
    * test(VCard, VCardText): covert to TS, replace old tests
    
    * test(VCard): make coverage 100%
    
    * refactor(VCard): updates from testing
    
    * docs(VCard): add new **loading** example
    
    * docs(VCard): move examples around, update language, add prop text
    
    * docs(VCard): add intermediate example

--- packages/docs/src/examples/cards/usage.vue
+++ packages/docs/src/examples/cards/usage.vue
@@ -1,34 +1,12 @@
 <template>
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-          aspect-ratio="2.75"
-        ></v-img>
-
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            <div> {{ card_text }} </div>
-          </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-btn text color="orange">Share</v-btn>
-          <v-btn text color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-card
+    max-width="344"
+    class="mx-auto"
+  >
+    <v-card-title>I'm a title</v-card-title>
+    <v-card-text>I'm card text</v-card-text>
+    <v-card-actions>
+      <v-btn text>Click</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
-
-<script>
-  export default {
-    data () {
-      return {
-        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
-      }
-    }
-  }
-</script>
