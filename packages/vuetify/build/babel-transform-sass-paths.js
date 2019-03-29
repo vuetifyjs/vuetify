70d343cac7f6ba33757e67db27544b77d809a6f6
Kevin Marrec <kevin@marrec.io>
2019-03-30 00:20:34 +0100

    fix sass resolve

--- packages/vuetify/build/babel-transform-sass-paths.js
+++ packages/vuetify/build/babel-transform-sass-paths.js
@@ -1,8 +1,6 @@
-var types = require('babel-types');
-var pathLib = require('path');
-var wrapListener = require('babel-plugin-detective/wrap-listener');
+const wrapListener = require('babel-plugin-detective/wrap-listener')
 
-module.exports = wrapListener(listener, 'transform-sass-paths');
+module.exports = wrapListener(listener, 'transform-sass-paths')
 
 function listener(path, file, opts) {
   const regex = /((?:\.\.?\/)+)/gi
@@ -10,8 +8,6 @@ function listener(path, file, opts) {
     const matches = regex.exec(path.node.value)
     if (!matches) return
     const m = matches[0].startsWith('./') ? matches[0].substr(2) : matches[0]
-    const folder = file.hub.file.opts.filename.split('/').slice(2, 3)[0]
-
-    path.node.value = path.node.value.replace(matches[0], `${m}../../../src/components/${folder}/`)
+    path.node.value = path.node.value.replace(matches[0], `${m}../src/`)
   }
 }
