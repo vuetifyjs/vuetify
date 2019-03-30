4343e71a207ab9b0acdb3a92baeaecad64494100
Kevin Marrec <kevin@marrec.io>
2019-03-30 17:34:46 +0100

    refactor

--- packages/vuetify/build/babel-transform-sass-paths.js
+++ packages/vuetify/build/babel-transform-sass-paths.js
@@ -1,20 +1,12 @@
-var wrapListener = require('babel-plugin-detective/wrap-listener')
+const { join, dirname, relative } = require('path')
+const wrapListener = require('babel-plugin-detective/wrap-listener')
 
 module.exports = wrapListener(listener, 'transform-sass-paths')
 
 function listener(path, file) {
   if (path.isLiteral() && path.node.value.endsWith('.sass')) {
-    let tmpPath = path.node.value.split('/')
-    let currentFolder = file.hub.file.opts.filename.split('/').slice(0, -1)
-
-    let relativePath = Array(currentFolder.length).fill('..').concat(currentFolder)
-
-    while (tmpPath[0].startsWith('.') && (tmpPath.shift() === '..')) {
-      relativePath.splice(-1)
-    }
-
-    const finalPath = relativePath.concat(tmpPath).join('/').replace(currentFolder[0], 'src')
-
-    path.node.value = finalPath
+    const from = dirname(file.hub.file.opts.filename)
+    const to = from.replace(from.split('/')[0], 'src')
+    path.node.value = join(relative(from, to), path.node.value)
   }
 }
