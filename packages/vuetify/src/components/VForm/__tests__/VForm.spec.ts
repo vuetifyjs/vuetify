d9b744ef9a28243cffee83a28e715002285c8c46
sh7dm <d3dx12.xx@gmail.com>
2019-04-01 16:22:27 +0300

    test(VForm): convert to TypeScript and @vue/test-utils

--- packages/vuetify/src/components/VForm/__tests__/VForm.spec.ts
+++ packages/vuetify/src/components/VForm/__tests__/VForm.spec.ts
@@ -1,18 +1,30 @@
 import Vue from 'vue'
-import { test } from '@/test'
-import VTextField from '@/components/VTextField'
-import VBtn from '@/components/VBtn'
-import VForm from '@/components/VForm'
+import {
+  mount,
+  MountOptions,
+  Wrapper
+} from '@vue/test-utils'
+import VTextField from '../../VTextField'
+import VBtn from '../../VBtn'
+import VForm from '../VForm'
 
 const inputOne = Vue.component('input-one', {
   render (h) {
     return h(VTextField, {
-      propsData: [(v) => !!v || 'Required']
+      props: [v => !!v || 'Required']
     })
   }
 })
 
-test('VForm.js', ({ mount }) => {
+describe('VForm.ts', () => {
+  type Instance = InstanceType<typeof VForm>
+  let mountFunction: (options?: MountOptions<Instance>) => Wrapper<Instance>
+  beforeEach(() => {
+    mountFunction = (options?: MountOptions<Instance>) => {
+      return mount(VForm, options)
+    }
+  })
+
   it('should pass on listeners to form element', async () => {
     const submit = jest.fn()
     const component = Vue.component('test', {
@@ -34,43 +46,43 @@ test('VForm.js', ({ mount }) => {
 
     const wrapper = mount(component)
 
-    const btn = wrapper.find('button')[0]
+    const btn = wrapper.find('button')
 
     btn.trigger('click')
 
-    expect(submit).toBeCalled()
+    expect(submit).toHaveBeenCalled()
   })
 
   it('should watch the error bag', async () => {
-    const wrapper = mount(VForm)
+    const wrapper = mountFunction()
 
     const input = jest.fn()
     wrapper.vm.$on('input', input)
 
     Vue.set(wrapper.vm.errorBag, 'foo', true)
     await Vue.nextTick()
-    expect(input).toBeCalledWith(false)
+    expect(input).toHaveBeenCalledWith(false)
 
     Vue.set(wrapper.vm.errorBag, 'foo', false)
     await Vue.nextTick()
-    expect(input).toBeCalledWith(true)
+    expect(input).toHaveBeenCalledWith(true)
   })
 
   it('should register input child', async () => {
-    const wrapper = mount(VForm, {
+    const wrapper = mountFunction({
       slots: {
         default: [VTextField]
       }
     })
 
     await wrapper.vm.$nextTick()
-    expect(wrapper.vm.inputs.length).toBe(1)
-    expect(Object.keys(wrapper.vm.errorBag).length).toBe(1)
+    expect(wrapper.vm.inputs).toHaveLength(1)
+    expect(Object.keys(wrapper.vm.errorBag)).toHaveLength(1)
   })
 
   // TODO: Figure out how to test this with the updated v-form
   it.skip('should only watch children if not lazy', async () => {
-    const wrapper = mount(VForm, {
+    const wrapper = mountFunction({
       propsData: {
         lazyValidation: true
       },
@@ -87,20 +99,20 @@ test('VForm.js', ({ mount }) => {
 
     // beware, depends on number of computeds in VTextField
     const watchers = 28
-    expect(input._watchers.length).toBe(watchers)
+    expect(input._watchers).toHaveLength(watchers)
     input.shouldValidate = false
     wrapper.vm.watchChild(input)
     await wrapper.vm.$nextTick()
 
-    expect(input._watchers.length).toBe(watchers + 1)
+    expect(input._watchers).toHaveLength(watchers + 1)
     input.shouldValidate = true
     await wrapper.vm.$nextTick()
 
-    expect(Object.keys(wrapper.vm.errorBag).length).toBe(1)
+    expect(Object.keys(wrapper.vm.errorBag)).toHaveLength(1)
   })
 
   it('should emit input when calling validate on lazy-validated form', async () => {
-    const wrapper = mount(VForm, {
+    const wrapper = mountFunction({
       propsData: {
         lazyValidation: true
       },
@@ -124,37 +136,37 @@ test('VForm.js', ({ mount }) => {
 
     await wrapper.vm.$nextTick()
 
-    expect(value).toBeCalledWith(false)
+    expect(value).toHaveBeenCalledWith(false)
   })
 
-  it('should reset validation', async () => {
-    const wrapper = mount(VForm, {
+  it('resetValidation should work', async () => {
+    const wrapper = mountFunction({
       slots: {
         default: [VTextField]
       }
     })
 
-    expect(Object.keys(wrapper.vm.errorBag).length).toBe(1)
+    expect(Object.keys(wrapper.vm.errorBag)).toHaveLength(1)
     wrapper.vm.reset()
 
-    expect(Object.keys(wrapper.vm.errorBag).length).toBe(1)
+    expect(Object.keys(wrapper.vm.errorBag)).toHaveLength(1)
 
     wrapper.setProps({ lazyValidation: true })
-    expect(Object.keys(wrapper.vm.errorBag).length).toBe(1)
+    expect(Object.keys(wrapper.vm.errorBag)).toHaveLength(1)
 
     wrapper.vm.reset()
     await new Promise(resolve => setTimeout(resolve, 0))
-    expect(Object.keys(wrapper.vm.errorBag).length).toBe(0)
+    expect(Object.keys(wrapper.vm.errorBag)).toHaveLength(0)
   })
 
   it('should register and unregister items', () => {
-    const wrapper = mount(VForm, {
+    const wrapper = mountFunction({
       slots: {
         default: [VTextField]
       }
     })
 
-    expect(wrapper.vm.inputs.length).toBe(1)
+    expect(wrapper.vm.inputs).toHaveLength(1)
 
     const input = wrapper.vm.inputs[0]
 
@@ -162,28 +174,28 @@ test('VForm.js', ({ mount }) => {
     // does not exist
     wrapper.vm.unregister({ _uid: input._uid + 1 })
 
-    expect(wrapper.vm.inputs.length).toBe(1)
+    expect(wrapper.vm.inputs).toHaveLength(1)
 
     wrapper.vm.unregister(input)
 
-    expect(wrapper.vm.inputs.length).toBe(0)
+    expect(wrapper.vm.inputs).toHaveLength(0)
 
     // Add back input
     wrapper.vm.register(input)
 
-    expect(wrapper.vm.inputs.length).toBe(1)
+    expect(wrapper.vm.inputs).toHaveLength(1)
 
     const shouldValidate = jest.fn()
     wrapper.vm.watchers[0].shouldValidate = shouldValidate
 
     wrapper.vm.unregister(input)
 
-    expect(shouldValidate).toBeCalled()
+    expect(shouldValidate).toHaveBeenCalled()
   })
 
   it('should reset validation', async () => {
     const resetErrorBag = jest.fn()
-    const wrapper = mount(VForm, {
+    const wrapper = mountFunction({
       methods: { resetErrorBag },
       slots: {
         default: [VTextField]
