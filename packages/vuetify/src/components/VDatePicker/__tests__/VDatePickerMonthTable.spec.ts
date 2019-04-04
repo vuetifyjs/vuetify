72deb0ca56aa584733161ba013d3c73ec8becad8
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-04-04 09:36:34 -0400

    test(VDatePicker): fix lint errors

--- packages/vuetify/src/components/VDatePicker/__tests__/VDatePickerMonthTable.spec.ts
+++ packages/vuetify/src/components/VDatePicker/__tests__/VDatePickerMonthTable.spec.ts
@@ -46,6 +46,7 @@ describe('VDatePickerMonthTable.ts', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
+  // eslint-disable-next-line jest/no-disabled-tests
   it.skip('should watch tableDate value and run transition', async () => { // TODO: make this one work
     const wrapper = mountFunction({
       propsData: {
@@ -62,6 +63,7 @@ describe('VDatePickerMonthTable.ts', () => {
     expect(wrapper.findAll('table').wrappers[0].element.className).toBe('tab-transition-enter tab-transition-enter-active')
   })
 
+  // eslint-disable-next-line jest/no-disabled-tests
   it.skip('should watch tableDate value and run reverse transition', async () => { // TODO: make this one work
     const wrapper = mountFunction({
       propsData: {
@@ -141,6 +143,7 @@ describe('VDatePickerMonthTable.ts', () => {
   })
 
   // TODO
+  // eslint-disable-next-line jest/no-disabled-tests
   it.skip('should emit tableDate event when swiped', () => {
     const wrapper = mountFunction({
       propsData: {
