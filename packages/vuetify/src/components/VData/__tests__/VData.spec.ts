f33840bd14ba923b39a308ec8dbb37ef541c3d1e
sh7dm <d3dx12.xx@gmail.com>
2019-04-04 20:51:13 +0300

    test(VData): add unit tests

---
+++ packages/vuetify/src/components/VData/__tests__/VData.spec.ts
@@ -0,0 +1,43 @@
+import VData from '../VData'
+import {
+  mount,
+  MountOptions,
+  Wrapper
+} from '@vue/test-utils'
+
+describe('VData.ts', () => {
+  type Instance = InstanceType<typeof VData>
+  let mountFunction: (options?: MountOptions<Instance>) => Wrapper<Instance>
+  beforeEach(() => {
+    mountFunction = (options?: MountOptions<Instance>) => {
+      return mount(VData, options)
+    }
+  })
+
+  it('should render with scopedProps', async () => {
+    const wrapper = mountFunction({
+      computed: {
+        scopedProps: () => 'test'
+      },
+      scopedSlots: {
+        default (data) {
+          return this.$createElement('div', [ data ])
+        }
+      }
+    })
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should compute length', () => {
+    const wrapper = mountFunction({
+      propsData: {
+        items: [ 'foo', 'bar' ]
+      }
+    })
+
+    expect(wrapper.vm.itemsLength).toBe(2)
+  })
+})
