9d31069874debd6e30279bb030c16a35be55bcd7
sh7dm <d3dx12.xx@gmail.com>
2019-04-05 18:38:44 +0300

    test(VData): more tests

--- packages/vuetify/src/components/VData/__tests__/VData.spec.ts
+++ packages/vuetify/src/components/VData/__tests__/VData.spec.ts
@@ -4,6 +4,7 @@ import {
   MountOptions,
   Wrapper
 } from '@vue/test-utils'
+import toHaveBeenWarnedInit from '../../../../test/util/to-have-been-warned'
 
 describe('VData.ts', () => {
   type Instance = InstanceType<typeof VData>
@@ -13,6 +14,8 @@ describe('VData.ts', () => {
       return mount(VData, options)
     }
   })
+
+  toHaveBeenWarnedInit()
 
   it('should render with scopedProps', async () => {
     const wrapper = mountFunction({
@@ -45,5 +48,169 @@ describe('VData.ts', () => {
     })
 
     expect(wrapper.vm.itemsLength).toBe(10)
+  })
+
+  it('should group items', () => {
+    const updateOptions = jest.fn()
+
+    const wrapper = mountFunction({
+      methods: {
+        updateOptions
+      }
+    })
+
+    wrapper.vm.group('foo')
+    expect(updateOptions.mock.calls[0][0]).toMatchSnapshot()
+
+    wrapper.vm.group('bar')
+    expect(updateOptions.mock.calls[1][0]).toMatchSnapshot()
+
+    wrapper.setProps({
+      groupBy: [ 'baz' ]
+    })
+    wrapper.vm.group('baz')
+    expect(updateOptions.mock.calls[2][0]).toMatchSnapshot()
+    wrapper.vm.group('qux')
+    expect(updateOptions.mock.calls[3][0]).toMatchSnapshot()
+  })
+
+  it('should sort items', () => {
+    const updateOptions = jest.fn()
+
+    const wrapper = mountFunction({
+      methods: {
+        updateOptions
+      }
+    })
+
+    wrapper.vm.sort('foo')
+    expect(updateOptions.mock.calls[0][0]).toMatchSnapshot()
+
+    wrapper.vm.sort('bar')
+    expect(updateOptions.mock.calls[1][0]).toMatchSnapshot()
+
+    wrapper.setProps({
+      sortBy: [ 'baz' ]
+    })
+    wrapper.vm.sort('baz')
+    expect(updateOptions.mock.calls[2][0]).toMatchSnapshot()
+    wrapper.vm.sort('qux')
+    expect(updateOptions.mock.calls[3][0]).toMatchSnapshot()
+  })
+
+  it('should sort items by array', () => {
+    const updateOptions = jest.fn()
+
+    const wrapper = mountFunction({
+      methods: {
+        updateOptions
+      }
+    })
+
+    wrapper.vm.sortArray([ 'foo', 'test' ])
+    expect(updateOptions.mock.calls[0][0]).toMatchSnapshot()
+
+    wrapper.vm.sortArray([ 'bar', 'test' ])
+    expect(updateOptions.mock.calls[1][0]).toMatchSnapshot()
+
+    wrapper.setProps({
+      sortBy: [ 'baz' ]
+    })
+    wrapper.vm.sortArray([ 'baz', 'test' ])
+    expect(updateOptions.mock.calls[2][0]).toMatchSnapshot()
+    wrapper.vm.sortArray([ 'qux', 'test' ])
+    expect(updateOptions.mock.calls[3][0]).toMatchSnapshot()
+
+    wrapper.vm.sort([ 'foo', 'test' ]) // sort should call sortArray
+    expect(updateOptions.mock.calls[0][0]).toMatchSnapshot()
+  })
+
+  it('watchers should work', () => { // eslint-disable-line max-statements
+    const updateOptions = jest.fn()
+    const multiSort = jest.fn()
+    const mustSort = jest.fn()
+    const itemsPerPage = jest.fn()
+    const page = jest.fn()
+
+    const wrapper = mountFunction({
+      methods: {
+        updateOptions
+      },
+      listeners: {
+        'update:multi-sort': multiSort,
+        'update:must-sort': mustSort,
+        'update:items-per-page': itemsPerPage,
+        'update:page': page
+      }
+    })
+
+    wrapper.setProps({
+      multiSort: true
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ multiSort: true })
+    wrapper.vm.internalOptions.multiSort = true
+    expect(multiSort).toHaveBeenLastCalledWith(true)
+
+    wrapper.setProps({
+      mustSort: true
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ mustSort: true })
+    wrapper.vm.internalOptions.mustSort = true
+    expect(mustSort).toHaveBeenLastCalledWith(true)
+
+    wrapper.setProps({
+      itemsPerPage: 1337
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ itemsPerPage: 1337 })
+    wrapper.vm.internalOptions.itemsPerPage = 1337
+    expect(itemsPerPage).toHaveBeenLastCalledWith(1337)
+
+    wrapper.setProps({
+      sortDesc: true
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ sortDesc: [ true ] })
+    wrapper.setProps({
+      sortDesc: [ false, true, true ]
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ sortDesc: [ false, true, true ] })
+
+    wrapper.setProps({
+      groupDesc: true
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ groupDesc: [ true ] })
+    wrapper.setProps({
+      groupDesc: [ false, true, true ]
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ groupDesc: [ false, true, true ] })
+
+    wrapper.setProps({
+      page: 123
+    })
+    expect(updateOptions).toHaveBeenLastCalledWith({ page: 123 })
+    wrapper.vm.internalOptions.page = 123
+    expect(page).toHaveBeenLastCalledWith(123)
+  })
+
+  it('should paginate items', () => {
+    const test = [ 'foo', 'bar', 'baz', 'qux', 'fizz', 'buzz', 'foobar', 'barbaz', 'fizzbuzz', 1, 2, 3, 4, 5 ]
+
+    const wrapper = mountFunction({
+      data: () => ({
+        pageStart: 1,
+        pageStop: 10
+      })
+    })
+
+    expect(wrapper.vm.paginateItems(test)).toEqual([ 'bar', 'baz', 'qux', 'fizz', 'buzz', 'foobar', 'barbaz', 'fizzbuzz', 1 ])
+    wrapper.vm.pageStart = 3
+    wrapper.vm.pageStop = 7
+    expect(wrapper.vm.paginateItems(test)).toEqual([ 'qux', 'fizz', 'buzz', 'foobar' ])
+    wrapper.vm.pageStart = 17
+    wrapper.vm.internalOptions.page = 10
+    expect(wrapper.vm.paginateItems(test)).toEqual([])
+    expect(wrapper.vm.internalOptions.page).toBe(1)
+
+    expect('[Vue warn]: The computed property "pageStart" is already defined in data.').toHaveBeenWarned()
+    expect('[Vue warn]: The computed property "pageStop" is already defined in data.').toHaveBeenWarned()
   })
 })
