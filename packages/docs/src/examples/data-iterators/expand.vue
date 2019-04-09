149f5f9df21fffab3928a6a14dd1ae55d9fa0e9d
nekosaur <albert@kaaman.nu>
2019-04-09 12:00:35 +0200

    docs(VDataIterator): updated expand example

--- packages/docs/src/examples/data-iterators/expand.vue
+++ packages/docs/src/examples/data-iterators/expand.vue
@@ -1,65 +1,67 @@
 <template>
   <v-container fluid grid-list-md>
-    <v-switch v-model="expand" :label="(expand) ? 'Expand Multiple' : 'Expand Single'"></v-switch>
+    <v-switch v-model="expand" label="Expand Single Item"></v-switch>
     <v-data-iterator
       :items="items"
       item-key="name"
-      :rows-per-page-items="rowsPerPageItems"
-      :pagination.sync="pagination"
-      content-tag="v-layout"
-      :expand="expand"
-      row
-      wrap
+      :items-per-page="4"
+      :single-expand="expand"
+      hide-default-footer
     >
-      <template v-slot:item="props">
-        <v-flex
-          xs12
-          sm6
-          md4
-          lg3
-        >
-          <v-card>
-            <v-card-title>
-              <h4>{{ props.item.name }}</h4>
-            </v-card-title>
-            <v-switch
-              v-model="props.expanded"
-              :label="(props.expanded) ? 'Expanded' : 'Closed'"
-              class="pl-3 mt-0"
-            ></v-switch>
-            <v-divider></v-divider>
-            <v-list v-if="props.expanded" dense>
-              <v-list-tile>
-                <v-list-tile-content>Calories:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Fat:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Carbs:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Protein:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Sodium:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Calcium:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Iron:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
-              </v-list-tile>
-            </v-list>
-          </v-card>
-        </v-flex>
+      <template v-slot:default="{ items, isExpanded, expand }">
+        <v-layout row wrap>
+          <v-flex
+            v-for="item in items"
+            :key="item.name"
+            xs12
+            sm6
+            md4
+            lg3
+          >
+            <v-card>
+              <v-card-title>
+                <h4>{{ item.name }}</h4>
+              </v-card-title>
+              <v-switch
+                :input-value="isExpanded(item)"
+                @change="(v) => expand(item, v)"
+                :label="isExpanded(item) ? 'Expanded' : 'Closed'"
+                class="pl-3 mt-0"
+              ></v-switch>
+              <v-divider></v-divider>
+              <v-list v-if="isExpanded(item)" dense>
+                <v-list-tile>
+                  <v-list-tile-content>Calories:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.calories }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Fat:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.fat }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Carbs:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.carbs }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Protein:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.protein }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Sodium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.sodium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Calcium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.calcium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Iron:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ item.iron }}</v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </v-layout>
       </template>
     </v-data-iterator>
   </v-container>
@@ -69,10 +71,6 @@
   export default {
     data: () => ({
       expand: false,
-      rowsPerPageItems: [4, 8, 12],
-      pagination: {
-        rowsPerPage: 4
-      },
       items: [
         {
           name: 'Frozen Yogurt',
