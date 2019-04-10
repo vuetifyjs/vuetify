3ec7322abe57ec81bdb9d1a993ae8a396277660d
John Leider <john.j.leider@gmail.com>
2019-04-10 00:01:55 -0400

    docs(CoreFileBtn): add support for source linking on pages
    clean-up display with md2 spec link

---
+++ packages/docs/src/components/core/FileBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-tooltip left bottom>
+    <v-btn
+      slot="activator"
+      :href="`https://github.com/vuetifyjs/vuetify/tree/master/packages/vuetify/src/${link}`"
+      icon
+      target="_blank"
+      rel="noopener"
+      aria-label="Source"
+    >
+      <v-icon>mdi-source-repository</v-icon>
+    </v-btn>
+    <span>Source</span>
+  </v-tooltip>
+</template>
+
+<script>
+  export default {
+    props: {
+      link: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
