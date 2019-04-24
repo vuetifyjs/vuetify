b15266232074bd62dab89a686049a60bf2472db0
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-04-23 23:35:58 -0400

    docs(VAlert): update dismissable example

---
+++ packages/docs/src/examples/alerts/simple/dismissible.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-alert
+      v-model="alert"
+      dismissible
+      type="info"
+    >
+      I'm a dismissable alert
+    </v-alert>
+    <div class="text-xs-center">
+      <v-btn
+        v-if="!alert"
+        color="primary"
+        @click="alert = true"
+      >
+        Reset
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        alert: true
+      }
+    }
+  }
+</script>
