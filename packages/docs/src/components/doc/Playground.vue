6fd90e66900b55c2a45cd8a3c5c46289d6a9f76f
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-04-23 21:40:00 -0400

    docs(VAlert): breakout playground section

---
+++ packages/docs/src/components/doc/Playground.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <doc-heading>Generic.Pages.playground</doc-heading>
+    <doc-example
+      :id="`usage-${-1}`"
+      :value="internalValue"
+    />
+  </div>
+</template>
+
+<script>
+  // Utilities
+  import kebabCase from 'lodash/kebabCase'
+  import {
+    mapGetters
+  } from 'vuex'
+
+  export default {
+    props: {
+      value: {
+        type: [Object, String],
+        default: undefined
+      }
+    },
+
+    computed: {
+      ...mapGetters('documentation', [
+        'namespace',
+        'page'
+      ]),
+      internalValue () {
+        return this.value === Object(this.value)
+          ? this.value
+          : { file: this.value }
+      }
+    },
+
+    methods: { kebabCase }
+  }
+</script>
