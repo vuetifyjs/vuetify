0feecf770822d3e698bfd8d11963763f5322fda3
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-04-23 23:45:40 -0400

    docs(VAlert): update icon example

--- packages/docs/src/examples/alerts/simple/icon.vue
+++ packages/docs/src/examples/alerts/simple/icon.vue
@@ -3,17 +3,8 @@
     <v-alert
       type="info"
       icon="new_releases"
-      prominent
     >
-      This is an info alert with a custom icon.
-    </v-alert>
-
-    <v-alert
-      type="error"
-      dense
-      :icon="false"
-    >
-      This is an error alert with no icon.
+      I'm an info alert with a custom icon.
     </v-alert>
   </div>
 </template>
