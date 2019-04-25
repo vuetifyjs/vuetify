6b192453bff5124d57c694332f6bc5e979019fd8
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-04-24 20:48:48 -0400

    docs(VAlert): add border example

---
+++ packages/docs/src/examples/alerts/simple/border.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-alert
+      border="top"
+      type="info"
+    >
+      I'm an alert with a border top
+    </v-alert>
+    <v-alert
+      border="right"
+      type="info"
+    >
+      I'm an alert with a border right
+    </v-alert>
+    <v-alert
+      border="bottom"
+      type="info"
+    >
+      I'm an alert with a border bottom
+    </v-alert>
+    <v-alert
+      border="left"
+      type="info"
+    >
+      I'm an alert with a border left
+    </v-alert>
+  </div>
+</template>
