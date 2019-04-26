d2a0db301ab234e9fea7399502a2851fd8d51f41
Andrew Henry <AMajesticPotatoe@gmail.com>
2019-04-26 14:28:34 -0400

    docs(VAlert): example update for 2.0 (#7073)
    * docs(VAlert): organize existing examples
    
    * docs(VAlert): breakout playground section
    
    * docs(VAlert): update example pathing
    
    * docs(VAlert): update description and usage
    
    * docs(VAlert): update dismissable example
    
    * docs(VAlert): update icon example
    
    * docs(VAlert): update outlined example
    
    * docs(VAlert): add type and twitter examples
    
    * docs(VAlert): update transition example
    
    * docs(VAlert): add border example
    
    * docs: remove dev code
    
    * docs(VAlert): update border and add colored-border
    
    * docs(VAlert): update border description
    
    * docs(VAlert): add dense example
    
    * docs(VAlert): update dismissible description
    
    * docs(VAlert): add prominent and text examples
    
    * docs(VAlert): fix dismissible example
    
    * docs(VAlert): update outline props to outlined

---
+++ packages/docs/src/examples/alerts/simple/border.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-alert border="top">
+      I'm an alert with a border top
+    </v-alert>
+    <v-alert
+      border="right"
+      color="info"
+    >
+      I'm an alert with a border right and color info
+    </v-alert>
+    <v-alert
+      border="bottom"
+      dark
+    >
+      I'm an alert with a border bottom and dark prop
+    </v-alert>
+    <v-alert
+      border="left"
+      type="info"
+    >
+      I'm an alert with a border left type info
+    </v-alert>
+  </div>
+</template>
