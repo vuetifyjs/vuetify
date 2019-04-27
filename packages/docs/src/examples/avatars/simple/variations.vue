d5a5d07e7ba765f7fabd9b9fe4bc91f1a16ac444
sh7dm <d3dx12.xx@gmail.com>
2019-04-27 16:14:44 +0300

    docs(VAvatar): reorg examples

---
+++ packages/docs/src/examples/avatars/simple/variations.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout
+    align-center
+    justify-space-around
+    wrap
+  >
+    <v-avatar color="indigo">
+      <v-icon dark>account_circle</v-icon>
+    </v-avatar>
+
+    <v-avatar>
+      <img
+        src="https://cdn.vuetifyjs.com/images/john.jpg"
+        alt="John"
+      >
+    </v-avatar>
+
+    <v-badge overlap>
+      <template v-slot:badge>
+        <span>3</span>
+      </template>
+
+      <v-avatar
+        color="purple red--after"
+      >
+        <v-icon dark>notifications</v-icon>
+      </v-avatar>
+    </v-badge>
+
+    <v-avatar color="teal">
+      <span class="white--text headline">C</span>
+    </v-avatar>
+
+    <v-avatar color="red">
+      <span class="white--text headline">J</span>
+    </v-avatar>
+  </v-layout>
+</template>
