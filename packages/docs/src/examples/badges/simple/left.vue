b893bb486640fcac2b6c5ccf22f39194ee8b1985
Dmitry Sharshakov <sh7dm@outlook.com>
2019-04-28 06:49:00 +0300

    docs(VBadge): update examples (#7087)
    * docs(VBadge): reorg examples
    
    * docs(VBadge): upgrade playground
    
    * docs(VBadge): add hover example
    
    * docs(VBadge): update usage, breakout playground
    
    * docs(VBadge): add bottom and left examples
    
    * docs(VBadge): move visibility to simple
    
    * docs(VBadge): add dynamic example

---
+++ packages/docs/src/examples/badges/simple/left.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-xs-center">
+    <v-badge
+      color="teal"
+      left
+    >
+      <template v-slot:badge>
+        <v-icon dark>
+          done
+        </v-icon>
+      </template>
+      <span>Badge w/ Left Prop</span>
+    </v-badge>
+  </div>
+</template>
