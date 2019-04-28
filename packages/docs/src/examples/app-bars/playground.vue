3bf8633606da11f1d11b8e334af61eda385a55a4
sh7dm <d3dx12.xx@gmail.com>
2019-04-28 08:51:57 +0300

    docs(VAppBar): split out playground

---
+++ packages/docs/src/examples/app-bars/playground.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-card class="hide-overflow">
+    <v-app-bar
+      absolute
+      scroll-target="#playground-example"
+      :color="color"
+      :elevate-on-scroll="elevateOnScroll"
+      :hide-on-scroll="hideOnScroll"
+      :fade-on-scroll="fadeOnScroll"
+      :fade-img-on-scroll="fadeImgOnScroll"
+      :inverted-scroll="invertedScroll"
+      :collapse="collapse"
+      :collapse-on-scroll="collapseOnScroll"
+      :shrink-on-scroll="shrinkOnScroll"
+      :extended="extended"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet
+      id="playground-example"
+      class="scroll-y"
+      max-height="600"
+    >
+      <v-container style="height: 1500px;">
+        <v-layout row wrap justify-space-around style="margin-top: 300px;">
+          <v-select
+            v-model="color"
+            label="Color"
+            :items="colors"
+          ></v-select>
+          <v-switch v-model="elevateOnScroll" label="Elevate-on-scroll"></v-switch>
+          <v-switch v-model="hideOnScroll" label="Hide-on-scroll"></v-switch>
+          <v-switch v-model="fadeOnScroll" label="Fade-on-scroll"></v-switch>
+          <v-switch v-model="fadeImgOnScroll" label="Fade-img-on-scroll"></v-switch>
+          <v-switch v-model="invertedScroll" label="Inverted scroll"></v-switch>
+          <v-switch v-model="collapse" label="Collapse"></v-switch>
+          <v-switch v-model="collapseOnScroll" label="Collapse-on-scroll"></v-switch>
+          <v-switch v-model="shrinkOnScroll" label="Shrink-on-scroll"></v-switch>
+          <v-switch v-model="extended" label="Extended"></v-switch>
+        </v-layout>
+      </v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      elevateOnScroll: false,
+      hideOnScroll: false,
+      fadeOnScroll: false,
+      fadeImgOnScroll: false,
+      invertedScroll: false,
+      collapse: false,
+      collapseOnScroll: false,
+      shrinkOnScroll: false,
+      extended: false,
+      color: 'accent',
+      colors: [ 'primary', 'accent', 'warning lighten-2', 'teal', 'orange' ]
+    })
+  }
+</script>
--- packages/docs/src/examples/app-bars/usage.vue
+++ packages/docs/src/examples/app-bars/usage.vue
@@ -1,78 +1,41 @@
 <template>
-  <v-card class="hide-overflow">
-    <v-app-bar
-      absolute
-      scroll-target="#playground-example"
-      :color="color"
-      :elevate-on-scroll="elevateOnScroll"
-      :hide-on-scroll="hideOnScroll"
-      :fade-on-scroll="fadeOnScroll"
-      :fade-img-on-scroll="fadeImgOnScroll"
-      :inverted-scroll="invertedScroll"
-      :collapse="collapse"
-      :collapse-on-scroll="collapseOnScroll"
-      :shrink-on-scroll="shrinkOnScroll"
-      :extended="extended"
+  <v-app-bar
+    color="deep-purple accent-4"
+    dark
+  >
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>Page title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-menu
+      left
+      bottom
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <template #activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
 
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>favorite</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-sheet
-      id="playground-example"
-      class="scroll-y"
-      max-height="600"
-    >
-      <v-container style="height: 1500px;">
-        <v-layout row wrap justify-space-around style="margin-top: 300px;">
-          <v-select
-            v-model="color"
-            label="Color"
-            :items="colors"
-          ></v-select>
-          <v-switch v-model="elevateOnScroll" label="Elevate-on-scroll"></v-switch>
-          <v-switch v-model="hideOnScroll" label="Hide-on-scroll"></v-switch>
-          <v-switch v-model="fadeOnScroll" label="Fade-on-scroll"></v-switch>
-          <v-switch v-model="fadeImgOnScroll" label="Fade-img-on-scroll"></v-switch>
-          <v-switch v-model="invertedScroll" label="Inverted scroll"></v-switch>
-          <v-switch v-model="collapse" label="Collapse"></v-switch>
-          <v-switch v-model="collapseOnScroll" label="Collapse-on-scroll"></v-switch>
-          <v-switch v-model="shrinkOnScroll" label="Shrink-on-scroll"></v-switch>
-          <v-switch v-model="extended" label="Extended"></v-switch>
-        </v-layout>
-      </v-container>
-    </v-sheet>
-  </v-card>
+      <v-list>
+        <v-list-tile
+          v-for="n in 5"
+          :key="n"
+          @click="() => {}"
+        >
+          <v-list-tile-item>Option {{ n }}</v-list-tile-item>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      elevateOnScroll: false,
-      hideOnScroll: false,
-      fadeOnScroll: false,
-      fadeImgOnScroll: false,
-      invertedScroll: false,
-      collapse: false,
-      collapseOnScroll: false,
-      shrinkOnScroll: false,
-      extended: false,
-      color: 'accent',
-      colors: [ 'primary', 'accent', 'warning lighten-2', 'teal', 'orange' ]
-    })
-  }
-</script>
