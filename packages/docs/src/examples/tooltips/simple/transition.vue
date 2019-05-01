9b90473d3bba6d334b8ffc144d33c521abd949d2
t-tajiri <swallow.ava@gmail.com>
2019-05-01 16:51:46 +0900

    docs(VToolTip): add transition example

---
+++ packages/docs/src/examples/tooltips/simple/transition.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-xs-center">
+    <v-tooltip
+      top
+      transition="scale-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" color="primary" dark v-on="on">Scale</v-btn>
+      </template>
+      <span>scale transtion</span>
+    </v-tooltip>
+
+    <v-tooltip
+      top
+      transition="fade-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" color="primary" dark v-on="on">Fade</v-btn>
+      </template>
+      <span>fade transtion</span>
+    </v-tooltip>
+  </div>
+</template>
