78c33cd6eb5e379b7aa1f97cde05ad3de8e91fa0
t-tajiri <swallow.ava@gmail.com>
2019-05-01 17:28:21 +0900

    docs(VToolTip): add nudge example

---
+++ packages/docs/src/examples/tooltips/simple/nudge.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-xs-center">
+    <v-tooltip
+      nudge-bottom="100"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" color="info" dark v-on="on">Nudge</v-btn>
+      </template>
+      <span>nudge message here</span>
+    </v-tooltip>
+  </div>
+</template>
