2589cd448a6928388e2161062f1385006dfc10a6
t-tajiri <swallow.ava@gmail.com>
2019-05-01 23:30:52 +0900

    docs(VTooltip): fix code format

--- packages/docs/src/examples/tooltips/simple/nudge.vue
+++ packages/docs/src/examples/tooltips/simple/nudge.vue
@@ -2,7 +2,14 @@
   <div class="text-xs-center">
     <v-tooltip nudge-bottom="100">
       <template v-slot:activator="{ on }">
-        <v-btn class="mx-2" color="info" dark v-on="on">Nudge</v-btn>
+        <v-btn
+          class="mx-2"
+          color="info"
+          dark
+          v-on="on"
+        >
+          Nudge
+        </v-btn>
       </template>
       <span>nudge message here</span>
     </v-tooltip>
