aecdaea105f4ddc7f52fb06eabf33c7a8d68a482
t-tajiri <swallow.ava@gmail.com>
2019-05-01 14:36:25 +0900

    docs(VTooltip): add color example

---
+++ packages/docs/src/examples/tooltips/simple/color.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-xs-center">
+    <v-tooltip top color="primary">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" color="primary" dark v-on="on">Button</v-btn>
+      </template>
+      <span>primary tooltip</span>
+    </v-tooltip>
+
+    <v-tooltip top color="red">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" dark color="red" v-on="on">Button</v-btn>
+      </template>
+      <span>red tooltip</span>
+    </v-tooltip>
+
+    <v-tooltip top color="purple">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" dark color="purple" v-on="on">Button</v-btn>
+      </template>
+      <span>purple tooltip</span>
+    </v-tooltip>
+
+    <v-tooltip top color="#FFA500">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" dark color="#FFA500" v-on="on">Button</v-btn>
+      </template>
+      <span>orange(#FFA500) tooltip</span>
+    </v-tooltip>
+  </div>
+</template>
