38e36bdab8b1962c02d03120f9f4dd14adb8de57
t-tajiri <swallow.ava@gmail.com>
2019-05-01 23:38:10 +0900

    docs(VTooltip): update usage example

--- packages/docs/src/examples/tooltips/usage.vue
+++ packages/docs/src/examples/tooltips/usage.vue
@@ -1,24 +1,14 @@
 <template>
-  <div class="text-xs-center d-flex align-center">
-    <v-tooltip bottom>
+  <div class="text-xs-center">
+    <v-tooltip top>
       <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Button</v-btn>
+        <v-btn
+          v-on="on"
+        >
+          Button
+        </v-btn>
       </template>
-      <span>Tooltip</span>
-    </v-tooltip>
-
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on }">
-        <v-icon color="primary" dark v-on="on">home</v-icon>
-      </template>
-      <span>Tooltip</span>
-    </v-tooltip>
-
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on }">
-        <span v-on="on">This text has a tooltip</span>
-      </template>
-      <span>Tooltip</span>
+      <span>I'm a basic tooltip component.</span>
     </v-tooltip>
   </div>
 </template>
