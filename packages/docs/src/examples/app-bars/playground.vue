acc819f377dd0bd333511c23381c67734b6819dc
John Leider <john.j.leider@gmail.com>
2019-05-06 14:03:34 -0400

    docs(VAppBar): update playground

--- packages/docs/src/examples/app-bars/playground.vue
+++ packages/docs/src/examples/app-bars/playground.vue
@@ -1,62 +1,141 @@
 <template>
-  <v-card class="hide-overflow">
-    <v-app-bar
-      absolute
-      scroll-target="#playground-example"
-      :color="color"
-      :elevate-on-scroll="elevateOnScroll"
-      :hide-on-scroll="hideOnScroll"
-      :fade-on-scroll="fadeOnScroll"
-      :fade-img-on-scroll="fadeImgOnScroll"
-      :inverted-scroll="invertedScroll"
-      :collapse="collapse"
-      :collapse-on-scroll="collapseOnScroll"
-      :shrink-on-scroll="shrinkOnScroll"
-      :extended="extended"
+  <div>
+    <v-layout
+      row
+      wrap
+      justify-center
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-flex
+        xs6
+        md4
+      >
+        <v-select
+          v-model="color"
+          label="Color"
+          :items="colors"
+        ></v-select>
+      </v-flex>
+      <v-flex xs12></v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="elevateOnScroll"
+          label="Elevate-on-scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="hideOnScroll"
+          label="Hide-on-scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="fadeOnScroll"
+          label="Fade-on-scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="fadeImgOnScroll"
+          label="Fade-img-on-scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="invertedScroll"
+          label="Inverted scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="collapse"
+          label="Collapse"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="collapseOnScroll"
+          label="Collapse-on-scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="shrinkOnScroll"
+          label="Shrink-on-scroll"
+        ></v-switch>
+      </v-flex>
+      <v-flex
+        xs3
+        md2
+      >
+        <v-switch
+          v-model="extended"
+          label="Extended"
+        ></v-switch>
+      </v-flex>
+    </v-layout>
+    <v-card class="hide-overflow">
+      <v-app-bar
+        absolute
+        dark
+        scroll-target="#playground-example"
+        :color="color"
+        :elevate-on-scroll="elevateOnScroll"
+        :hide-on-scroll="hideOnScroll"
+        :fade-on-scroll="fadeOnScroll"
+        :fade-img-on-scroll="fadeImgOnScroll"
+        :inverted-scroll="invertedScroll"
+        :collapse="collapse"
+        :collapse-on-scroll="collapseOnScroll"
+        :shrink-on-scroll="shrinkOnScroll"
+        :extended="extended"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+        <v-toolbar-title>Title</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet
+        id="playground-example"
+        class="scroll-y"
+        max-height="600"
+      >
+        <v-container style="height: 1500px;">
 
-      <v-btn icon>
-        <v-icon>favorite</v-icon>
-      </v-btn>
+        </v-container>
+      </v-sheet>
+    </v-card>
+  </div>
 
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-sheet
-      id="playground-example"
-      class="scroll-y"
-      max-height="600"
-    >
-      <v-container style="height: 1500px;">
-        <v-layout row wrap justify-space-around style="margin-top: 300px;">
-          <v-select
-            v-model="color"
-            label="Color"
-            :items="colors"
-          ></v-select>
-          <v-switch v-model="elevateOnScroll" label="Elevate-on-scroll"></v-switch>
-          <v-switch v-model="hideOnScroll" label="Hide-on-scroll"></v-switch>
-          <v-switch v-model="fadeOnScroll" label="Fade-on-scroll"></v-switch>
-          <v-switch v-model="fadeImgOnScroll" label="Fade-img-on-scroll"></v-switch>
-          <v-switch v-model="invertedScroll" label="Inverted scroll"></v-switch>
-          <v-switch v-model="collapse" label="Collapse"></v-switch>
-          <v-switch v-model="collapseOnScroll" label="Collapse-on-scroll"></v-switch>
-          <v-switch v-model="shrinkOnScroll" label="Shrink-on-scroll"></v-switch>
-          <v-switch v-model="extended" label="Extended"></v-switch>
-        </v-layout>
-      </v-container>
-    </v-sheet>
-  </v-card>
 </template>
 
 <script>
