bc770357edf8e5b8f5a138933b33b38778df57a6
xufei@ <xufei@shushi.pro>
2019-05-09 13:15:54 +0800

    fix: fix column slot param

--- packages/docs/src/examples/data-tables/crud.vue
+++ packages/docs/src/examples/data-tables/crud.vue
@@ -52,7 +52,7 @@
         </v-dialog>
       </v-toolbar>
     </template>
-    <template v-slot:item.column.action="{ item }">
+    <template v-slot:item.action="{ item }">
       <v-icon
         small
         class="mr-2"
