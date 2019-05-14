4966fae1992f077155d715e6449f17662a80a15c
sh7dm <d3dx12.xx@gmail.com>
2019-05-14 21:34:01 +0300

    docs(VBottomNav): add hideOnScroll example

---
+++ packages/docs/src/examples/bottom-navigation/intermediate/hideOnScroll.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card
+    class="hide-overflow mx-auto"
+    height="200"
+    max-width="500"
+  >
+    <v-bottom-navigation
+      scroll-target="#scroll-area-1"
+      hide-on-scroll
+      absolute
+      color="white"
+      horizontal
+    >
+      <v-btn text color="teal">
+        <span>Recents</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <v-btn text color="teal">
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn text color="teal">
+        <span>Nearby</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
+    <v-sheet
+      id="scroll-area-1"
+      class="scroll-y"
+      max-height="600"
+    >
+      <v-container style="height: 1500px;">
+
+      </v-container>
+    </v-sheet>
+  </v-card>
+</template>
