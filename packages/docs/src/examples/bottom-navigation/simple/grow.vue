672bfc4ae53d720350330c3616e00bee73029652
sh7dm <d3dx12.xx@gmail.com>
2019-05-14 21:02:11 +0300

    docs(VBottomNav): add grow example

---
+++ packages/docs/src/examples/bottom-navigation/simple/grow.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card
+    class="hide-overflow mx-auto"
+    height="200"
+    max-width="700"
+  >
+    <v-card-text class="text-xs-center">
+      <v-btn
+        text
+        color="primary"
+        @click="grow = !grow"
+      >
+        Grow
+      </v-btn>
+    </v-card-text>
+
+    <v-bottom-navigation
+      :grow="grow"
+      absolute
+      color="white"
+      horizontal
+    >
+      <v-btn text color="teal">
+        <span>Recents</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <v-btn text color="teal">
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn text color="teal">
+        <span>Nearby</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        grow: false
+      }
+    }
+  }
+</script>
