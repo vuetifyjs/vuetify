d45ed76915a9163d26ef7aba5b4a18eb6ea11713
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-05-17 19:16:51 +0300

    docs(VImg): update examples (#7228)
    * docs(VImg): sort examples
    
    * docs(VImg): move gradients example to intermediate
    
    * docs(VImg): simplify usage example
    
    * docs(VImg): add grid example
    
    * docs(VImg): add placeholder example
    
    * docs(VImg): add ratio description
    
    * docs(VImg): add description to usage example

---
+++ packages/docs/src/examples/images/intermediate/grid.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-container grid-list-sm fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="n in 9"
+              :key="n"
+              xs4
+              d-flex
+            >
+              <v-card flat tile class="d-flex">
+                <v-img
+                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                  aspect-ratio="1"
+                  class="grey lighten-2"
+                >
+                  <template v-slot:placeholder>
+                    <v-layout
+                      fill-height
+                      align-center
+                      justify-center
+                      ma-0
+                    >
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-layout>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
