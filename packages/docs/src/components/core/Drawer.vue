d06d8c99d2a5156492e36e01efd63bb3232a922b
John Leider <john.j.leider@gmail.com>
2019-05-20 11:04:30 -0400

    chore: update sponsors

--- packages/docs/src/components/core/Drawer.vue
+++ packages/docs/src/components/core/Drawer.vue
@@ -10,16 +10,10 @@
     >
       <div class="text-xs-center">
         <h4 class="body-2 font-weight-bold grey--text">Premiere sponsor</h4>
-        <supporters-patron
-          v-if="supporters.special && supporters.special.length"
-          :value="supporters.special[0]"
-          :href="supporters.special[0].href"
-          class="mb-4"
-          large
-        />
-        <!-- <span class="d-block mb-3 caption grey--text text--lighten-1">
+
+        <span class="d-block mb-3 caption grey--text text--lighten-1">
           One spot available
-        </span> -->
+        </span>
 
         <supporters-sponsor-btn
           large
