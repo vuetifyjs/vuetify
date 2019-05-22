9a643df2d073841d79a2c956a8afb1cbc3fdbd41
sh7dm <d3dx12.xx@gmail.com>
2019-05-22 17:30:55 +0300

    docs(VChip): add filter example

---
+++ packages/docs/src/examples/chips/simple/filter.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout justify-space-around align-center>
+    <v-chip
+      class="ma-2"
+      :input-value="active"
+      filter
+    >
+      I'm v-chip
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      :input-value="active"
+      filter
+      filter-icon="mdi-plus"
+    >
+      I'm v-chip
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      :input-value="active"
+      filter
+      filter-icon="mdi-minus"
+    >
+      I'm v-chip
+    </v-chip>
+
+    <v-switch label="Active" v-model="active"></v-switch>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      active: false
+    })
+  }
+</script>
