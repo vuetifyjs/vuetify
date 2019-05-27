d45102e0bbd9606624000f6101b5ff56917b6d12
nekosaur <albert@kaaman.nu>
2019-05-27 17:40:46 +0200

    docs(VTextField): update docs wrt mask removal

--- packages/docs/src/examples/text-fields/masks.vue
+++ packages/docs/src/examples/text-fields/masks.vue
@@ -4,15 +4,20 @@
       <v-text-field v-model="mask" label="Mask"></v-text-field>
     </v-card-text>
     <v-card-text>
-      <v-text-field v-model="value" :mask="mask" label="Value"></v-text-field>
+      <v-text-field v-model="value" v-mask="mask" label="Value"></v-text-field>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+  import { mask } from 'vue-the-mask'
+
   export default {
+    directives: {
+      mask
+    },
     data: () => ({
-      mask: 'credit-card',
+      mask: '####-####-####-####',
       value: '4444444444444444'
     })
   }
