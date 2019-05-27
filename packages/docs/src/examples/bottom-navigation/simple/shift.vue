64d46f47c70dd2557cbfe7406c3ec1cbf8cda07f
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-05-27 13:34:57 -0400

    docs(VBottomNavigation): split out color and shift, add horizontal

---
+++ packages/docs/src/examples/bottom-navigation/simple/shift.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-bottom-navigation
+    v-model="bottomNav"
+    dark
+    shift
+  >
+    <v-btn>
+      <span>Video</span>
+      <v-icon>ondemand_video</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Music</span>
+      <v-icon>music_note</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Book</span>
+      <v-icon>book</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Image</span>
+      <v-icon>image</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        bottomNav: 3
+      }
+    },
+
+    computed: {
+      color () {
+        switch (this.bottomNav) {
+          case 0: return 'blue-grey'
+          case 1: return 'teal'
+          case 2: return 'brown'
+          case 3: return 'indigo'
+        }
+      }
+    }
+  }
+</script>
