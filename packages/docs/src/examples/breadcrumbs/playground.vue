f35c579cb8180f1346434d1b7a9799176b6e67d8
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-05-27 23:04:42 +0300

    docs(VBreadcrumbs): update examples (#7297)
    * docs(VBreadcrumbs): sort docs examples
    
    * docs(VBreadcrumbs): add large example
    
    * docs(VBreadcrumbs): add divider example
    
    * docs(VBreadcrumbs): add playground

---
+++ packages/docs/src/examples/breadcrumbs/playground.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-layout justify-space-around wrap>
+      <v-flex xs12>
+        <v-select v-model="divider" :items="dividers" label="Divider"></v-select>
+      </v-flex>
+      <v-switch v-model="large" label="Large"></v-switch>
+      <v-switch v-model="customDiv" label="Custom divider"></v-switch>
+    </v-layout>
+
+    <v-breadcrumbs :items="items" :large="large" :divider="divider">
+      <template v-slot:divider v-if="customDiv">
+        <v-icon>chevron_right</v-icon>
+      </template>
+    </v-breadcrumbs>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          text: 'Dashboard',
+          disabled: false,
+          href: 'breadcrumbs_dashboard'
+        },
+        {
+          text: 'Link 1',
+          disabled: false,
+          href: 'breadcrumbs_link_1'
+        },
+        {
+          text: 'Link 2',
+          disabled: true,
+          href: 'breadcrumbs_link_2'
+        }
+      ],
+      divider: '/',
+      dividers: [ '/', '\\', '.', ';', '>', '-' ],
+      large: false,
+      customDiv: false
+    })
+  }
+</script>
