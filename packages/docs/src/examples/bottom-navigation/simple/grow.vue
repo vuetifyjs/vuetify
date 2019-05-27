654d2bbabdd883be8fdfc67de4adc2a8d85fb0cb
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-05-27 13:47:06 -0400

    docs(VBottomNavigation): simplify grow

--- packages/docs/src/examples/bottom-navigation/simple/grow.vue
+++ packages/docs/src/examples/bottom-navigation/simple/grow.vue
@@ -1,45 +1,31 @@
 <template>
-  <div class="hide-overflow">
-    <div class="text-xs-center mb-2">
-      <v-btn
-        text
-        color="teal"
-        @click="grow = !grow"
-      >
-        Grow
-      </v-btn>
-    </div>
+  <v-bottom-navigation
+    :value="activeBtn"
+    grow
+    color="teal"
+  >
+    <v-btn>
+      <span>Recents</span>
+      <v-icon>history</v-icon>
+    </v-btn>
 
-    <v-bottom-navigation
-      :value="activeBtn"
-      :grow="grow"
-      color="teal"
-      horizontal
-    >
-      <v-btn>
-        <span>Recents</span>
-        <v-icon>history</v-icon>
-      </v-btn>
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>favorite</v-icon>
+    </v-btn>
 
-      <v-btn>
-        <span>Favorites</span>
-        <v-icon>favorite</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Nearby</span>
-        <v-icon>place</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
-  </div>
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>place</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        activeBtn: 1,
-        grow: false
+        activeBtn: 1
       }
     }
   }
