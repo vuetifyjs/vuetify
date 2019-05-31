4ccbb7b485e6408e8d529a466ed3ecb1f3f8d09c
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-05-31 15:48:45 +0300

    docs(VBottomSheet): update examples (#7349)
    * docs(VBottomSheet): sort examples
    
    * docs(VBottomSheet): add persistent example
    
    * docs(VBottomSheet): add model example
    
    * docs(VBottomSheet): add playground
    
    * docs(VBottomSheet): move inset example to intermediate
    
    * docs(VBottomSheet): simplify prop examples
    
    * docs(VBottomSheet): move player and list to complex

---
+++ packages/docs/src/examples/bottom-sheets/playground.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="text-xs-center">
+    <v-layout row wrap justify-space-around>
+      <v-switch v-model="sheet" label="Model"></v-switch>
+      <v-switch v-model="inset" label="Inset"></v-switch>
+      <v-switch v-model="hideOverlay" label="Hide overlay"></v-switch>
+    </v-layout>
+    <v-bottom-sheet v-model="sheet" :inset="inset" :hide-overlay="hideOverlay">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="purple"
+          dark
+          v-on="on"
+        >
+          Open Playground
+        </v-btn>
+      </template>
+      <v-sheet class="text-xs-center" height="200px">
+        <v-btn
+          class="mt-4"
+          flat
+          color="red"
+          @click="sheet = !sheet"
+        >close</v-btn>
+        <div class="title">Active Playground Props</div>
+        <div v-if="sheet">v-model</div>
+        <div v-if="inset">inset</div>
+        <div v-if="hideOverlay">hide-overlay</div>
+      </v-sheet>
+    </v-bottom-sheet>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      sheet: false,
+      inset: false,
+      hideOverlay: false,
+      tiles: [
+        { img: 'keep.png', title: 'Keep' },
+        { img: 'inbox.png', title: 'Inbox' },
+        { img: 'hangouts.png', title: 'Hangouts' },
+        { img: 'messenger.png', title: 'Messenger' },
+        { img: 'google.png', title: 'Google+' }
+      ]
+    })
+  }
+</script>
