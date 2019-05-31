1da124e748b642402796b86fdfa207cfa12613e8
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-31 17:02:41 +0900

    [Locale] Add korean locale

---
+++ packages/vuetify/src/locale/ko.ts
@@ -0,0 +1,18 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: '1페이지 당 항목 수：',
+    rowsPerPageAll: '전체',
+    pageText: '{2}개 중 {0}-{1}',
+    noResultsText: '일치하는 항목이 없습니다.',
+    nextPage: '다음',
+    prevPage: '이전'
+  },
+  dataTable: {
+    rowsPerPageText: '1페이지 당 행 수：'
+  },
+  noDataText: '데이터가 없습니다.',
+  carousel: {
+    prev: '이전',
+    next: '다음'
+  }
+}
