2699fcd11f85d63b2337c5c0c1008aba95133bc2
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-05-31 12:11:09 -0700

    docs(VSubheader): Add inset example and description.

---
+++ packages/docs/src/examples/subheaders/simple/inset.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-card>
+      <v-subheader inset>Subheader</v-subheader>
+      <v-list>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>folder</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>List item</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>folder</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>List item</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-flex>
+</template>
