288f6b0919133ebde2f30c02a34c28af09f1d17d
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-05-31 22:32:46 -0700

    docs(VSubheader): Update inset example.

--- packages/docs/src/examples/subheaders/simple/inset.vue
+++ packages/docs/src/examples/subheaders/simple/inset.vue
@@ -1,25 +1,59 @@
 <template>
   <v-flex xs12 sm6 offset-sm3>
     <v-card>
-      <v-subheader inset>Subheader</v-subheader>
+      <v-subheader :inset="inset">Subheader</v-subheader>
+
       <v-list>
-        <v-list-item @click="">
-          <v-list-item-action>
-            <v-icon>folder</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>List item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="">
-          <v-list-item-action>
-            <v-icon>folder</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>List item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <v-template v-for="(item, index) in items">
+          <v-list-item
+            v-if="item.action"
+            :key="item.title"
+            @click=""
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.action }}</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider
+            v-else-if="item.divider"
+            :key="index"
+            :inset="inset"
+          ></v-divider>
+        </v-template>
       </v-list>
     </v-card>
   </v-flex>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      inset: true,
+      items: [
+        {
+          action: 'label',
+          title: 'list item'
+        },
+        {
+          divider: true
+        },
+        {
+          action: 'label',
+          title: 'list item',
+        },
+        {
+          divider: true
+        },
+        {
+          action: 'label',
+          title: 'list item',
+        }
+      ]
+    })
+  }
+</script>
