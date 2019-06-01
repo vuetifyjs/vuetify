d5076f59a61254a98b1f5f601a2beda836922d90
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-01 12:24:49 -0700

    docs(VSubheader): update examples for 2.0 (#7346)
    * docs(VSubheader): Reorganize existing examples
    
    * docs(VSubheader): Update usage and usage desc.
    
    * docs(VSubheader): Update usage example.
    
    * docs(VSubheader): Add inset example and description.
    
    * docs(VSubheader): Add playground.
    
    Add playground section and example.
    
    * docs(VSubheader): Update inset desc.
    
    * docs(VSubheader): Update playground example.
    
    * docs(VSubheader): Update inset example.
    
    * docs(VSubheader): Fix trailing commas
    
    Remove trailing commas and add key to template because travis said so.

---
+++ packages/docs/src/examples/subheaders/simple/inset.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-card>
+      <v-subheader :inset="inset">Subheader</v-subheader>
+
+      <v-list>
+        <v-template v-for="(item, index) in items" :key="index">
+          <v-list-item
+            v-if="item.action"
+            :key="item.title"
+            @click=""
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.action }}</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider
+            v-else-if="item.divider"
+            :key="index"
+            :inset="inset"
+          ></v-divider>
+        </v-template>
+      </v-list>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      inset: true,
+      items: [
+        {
+          action: 'label',
+          title: 'list item'
+        },
+        {
+          divider: true
+        },
+        {
+          action: 'label',
+          title: 'list item'
+        },
+        {
+          divider: true
+        },
+        {
+          action: 'label',
+          title: 'list item'
+        }
+      ]
+    })
+  }
+</script>
