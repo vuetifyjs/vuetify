20c57d151b19972f22878a897f450b1ee519a3be
sh7dm <d3dx12.xx@gmail.com>
2019-06-01 20:03:06 +0300

    style(VDatePicker): fix style in usage example

--- packages/docs/src/examples/date-pickers/usage.vue
+++ packages/docs/src/examples/date-pickers/usage.vue
@@ -1,14 +1,14 @@
 <template>
-    <v-layout row wrap justify-space-around>
-      <v-flex xs12>
-        <v-layout row wrap justify-space-around>
-          <v-checkbox v-model="landscape" hide-details label="Landscape"></v-checkbox>
-          <v-checkbox v-model="reactive" hide-details label="Reactive"></v-checkbox>
-        </v-layout>
-      </v-flex>
+  <v-layout row wrap justify-space-around>
+    <v-flex xs12>
+      <v-layout row wrap justify-space-around>
+        <v-checkbox v-model="landscape" hide-details label="Landscape"></v-checkbox>
+        <v-checkbox v-model="reactive" hide-details label="Reactive"></v-checkbox>
+      </v-layout>
+    </v-flex>
 
-      <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
-    </v-layout>
+    <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
+  </v-layout>
 </template>
 
 <script>
