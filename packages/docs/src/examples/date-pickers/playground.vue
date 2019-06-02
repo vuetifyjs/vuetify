565c4ada5ad6d57d6755d17f1c7bce4dae104f23
sh7dm <d3dx12.xx@gmail.com>
2019-06-02 14:03:32 +0300

    docs(VDatePicker): add playground

---
+++ packages/docs/src/examples/date-pickers/playground.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout row wrap justify-space-around>
+    <v-flex xs12>
+      <v-layout row wrap justify-space-around>
+        <v-switch v-model="landscape" label="Landscape"></v-switch>
+        <v-switch v-model="reactive" label="Reactive"></v-switch>
+      </v-layout>
+    </v-flex>
+
+    <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        picker: new Date().toISOString().substr(0, 10),
+        landscape: false,
+        reactive: false
+      }
+    }
+  }
+</script>
