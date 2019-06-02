28af0bf0cb9641b121281e3cf9d177b2331d5dc1
sh7dm <d3dx12.xx@gmail.com>
2019-06-02 11:20:56 +0300

    docs(VDatePicker): remove checkboxes from usage example

--- packages/docs/src/examples/date-pickers/usage.vue
+++ packages/docs/src/examples/date-pickers/usage.vue
@@ -1,13 +1,6 @@
 <template>
-  <v-layout row wrap justify-space-around>
-    <v-flex xs12>
-      <v-layout row wrap justify-space-around>
-        <v-checkbox v-model="landscape" hide-details label="Landscape"></v-checkbox>
-        <v-checkbox v-model="reactive" hide-details label="Reactive"></v-checkbox>
-      </v-layout>
-    </v-flex>
-
-    <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
+  <v-layout row justify-center>
+    <v-date-picker v-model="picker"></v-date-picker>
   </v-layout>
 </template>
 
@@ -15,9 +8,7 @@
   export default {
     data () {
       return {
-        picker: new Date().toISOString().substr(0, 10),
-        landscape: false,
-        reactive: false
+        picker: new Date().toISOString().substr(0, 10)
       }
     }
   }
