4f80534ffbd15e6a5e0601a853c8cadd0487c5d2
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-02 22:27:12 -0700

    docs(VFooter): Update pathing and add absolute.
    Update existing example paths, and add absolute example.

---
+++ packages/docs/src/examples/footer/simple/absolute.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card height="150">
+    <v-card-title>Card Component</v-card-title>
+
+    <v-footer
+      absolute
+      color="blue"
+      class="font-weight-medium"
+    >
+      <v-flex
+        py-2
+        text-xs-center
+        xs12
+      >
+        Absolute Footer
+      </v-flex>
+    </v-footer>
+  </v-card>
+</template>
