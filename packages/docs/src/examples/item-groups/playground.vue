d02f9a0c3d6aad1507b80707702c82986b765b86
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-06-04 03:19:19 +0300

    docs(VItemGroup): update examples (#7380)
    * docs(VItemGroup): sort existing examples
    
    * docs(VItemGroup): add multiple example
    
    * docs(VItemGroup): add mandatory example
    
    * docs(VItemGroup): add activeClass example
    
    * docs(VItemGroup): hyphenate active-class prop in example
    
    * docs(VItemGroup): add playground

---
+++ packages/docs/src/examples/item-groups/playground.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-layout column>
+    <v-layout row wrap justify-space-around>
+      <v-switch v-model="mandatory" label="Mandatory"></v-switch>
+      <v-switch v-model="multiple" label="Multiple"></v-switch>
+      <v-flex xs12>
+        <v-select v-model="type" :items="types" label="Item type"></v-select>
+      </v-flex>
+    </v-layout>
+
+    <v-item-group :mandatory="mandatory" :multiple="multiple">
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <v-flex
+            v-for="n in 3"
+            :key="n"
+            xs12
+            md4
+          >
+            <v-item>
+              <template slot-scope="{ active, toggle }">
+                <v-card
+                  :color="active ? 'primary' : ''"
+                  class="d-flex align-center"
+                  dark
+                  height="200"
+                  v-if="type === 'cards'"
+                  @click="toggle"
+                >
+                  <v-scroll-y-transition>
+                    <div
+                      v-if="active"
+                      class="display-3 text-xs-center"
+                    >
+                      Active
+                    </div>
+                  </v-scroll-y-transition>
+                </v-card>
+                <v-img
+                  src="https://picsum.photos/id/237/200/300"
+                  height="150"
+                  class="text-xs-right pa-2"
+                  v-else
+                  @click="toggle"
+                >
+                  <v-btn
+                    icon
+                    dark
+                  >
+                    <v-icon>
+                      {{ active ? 'mdi-heart' : 'mdi-heart-outline' }}
+                    </v-icon>
+                  </v-btn>
+                </v-img>
+              </template>
+            </v-item>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-item-group>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      types: [
+        'cards',
+        'images',
+      ],
+      type: 'cards',
+      mandatory: false,
+      multiple: true,
+    }),
+  }
+</script>
