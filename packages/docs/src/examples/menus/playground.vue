6e02dee7ff0d802fbd5e57deaa81036f9c891abc
sh7dm <d3dx12.xx@gmail.com>
2019-06-05 17:05:35 +0300

    docs(VMenu): add playground

---
+++ packages/docs/src/examples/menus/playground.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-layout column align-center>
+    <v-layout row wrap justify-space-around>
+      <v-switch v-model="disabled" class="ma-2" label="Disabled"></v-switch>
+      <v-switch v-model="absolute" class="ma-2" label="Absolute"></v-switch>
+      <v-switch v-model="openOnHover" class="ma-2" label="Open on hover"></v-switch>
+      <v-switch v-model="value" class="ma-2" label="Value"></v-switch>
+    </v-layout>
+
+    <v-menu
+      v-model="value"
+      :disabled="disabled"
+      :absolute="absolute"
+      :open-on-hover="openOnHover"
+      offset-y
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="primary"
+          dark
+          v-on="on"
+        >
+          Dropdown
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+      disabled: false,
+      absolute: false,
+      openOnHover: false,
+      value: false,
+    }),
+  }
+</script>
