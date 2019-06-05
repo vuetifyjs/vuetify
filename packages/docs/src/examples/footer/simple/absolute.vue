2da14f2611323da85d34c799725fa6bad8b946ef
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-04 22:44:14 -0700

    docs(VFooter): Add absolute and padless.
    Add absolute and padless footer examples.

--- packages/docs/src/examples/footer/simple/absolute.vue
+++ packages/docs/src/examples/footer/simple/absolute.vue
@@ -1,18 +1,14 @@
 <template>
   <v-card height="150">
-    <v-card-title>Card Component</v-card-title>
-
     <v-footer
       absolute
-      color="blue"
       class="font-weight-medium"
     >
       <v-flex
-        py-2
         text-xs-center
         xs12
       >
-        Absolute Footer
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
       </v-flex>
     </v-footer>
   </v-card>
