43d037a357eec122d8474f2330854f36911294c5
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-06 14:04:11 -0700

    docs(VHover): Update usage example/desc.

--- packages/docs/src/examples/hover/usage.vue
+++ packages/docs/src/examples/hover/usage.vue
@@ -4,44 +4,10 @@
       slot-scope="{ hover }"
       :elevation="hover ? 12 : 2"
       class="mx-auto"
-      width="344"
+      height="350"
+      width="350"
     >
-      <v-img
-        :aspect-ratio="16/9"
-        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-      ></v-img>
-      <v-card-title>
-        <div>
-          <span class="headline">Cafe Badilico</span>
-          <div class="d-flex">
-            <v-rating
-              :value="value"
-              color="amber"
-              dense
-              half-increments
-              readonly
-              size="14"
-            ></v-rating>
-            <div class="ml-2 grey--text text--darken-2">
-              <span>{{ value }}</span>
-              <span>({{ reviews }})</span>
-            </div>
-          </div>
-        </div>
-        <v-spacer></v-spacer>
-        <v-btn icon class="mr-0">
-          <v-icon>mdi-chevron-right</v-icon>
-        </v-btn>
-      </v-card-title>
+      <v-card-text class="my-3 text-xs-center title">Hover over me!</v-card-text>
     </v-card>
   </v-hover>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      reviews: 413,
-      value: 4.5,
-    }),
-  }
-</script>
