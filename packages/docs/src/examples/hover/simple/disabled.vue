7e1dbad5101a5727b392cc2adb7632152f092832
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-07 03:13:34 -0700

    docs(VHover): Add disabled example.

---
+++ packages/docs/src/examples/hover/simple/disabled.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-hover disabled>
+    <v-card
+      slot-scope="{ hover }"
+      :elevation="hover ? 12 : 2"
+      class="mx-auto"
+      height="350"
+      width="350"
+    >
+      <v-card-text class="my-3 text-xs-center title">Hover over me!</v-card-text>
+    </v-card>
+  </v-hover>
+</template>
