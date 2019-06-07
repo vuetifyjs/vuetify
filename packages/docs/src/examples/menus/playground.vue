c639818d1a2634c9363da24d8c92912653b8208b
sh7dm <d3dx12.xx@gmail.com>
2019-06-07 14:55:16 +0300

    docs(VMenu): add missing props to playground

--- packages/docs/src/examples/menus/playground.vue
+++ packages/docs/src/examples/menus/playground.vue
@@ -4,6 +4,10 @@
       <v-switch v-model="disabled" class="ma-2" label="Disabled"></v-switch>
       <v-switch v-model="absolute" class="ma-2" label="Absolute"></v-switch>
       <v-switch v-model="openOnHover" class="ma-2" label="Open on hover"></v-switch>
+      <v-switch v-model="closeOnClick" label="Close on click"></v-switch>
+      <v-switch v-model="closeOnContentClick" label="Close on content click"></v-switch>
+      <v-switch v-model="offsetX" label="X offset"></v-switch>
+      <v-switch v-model="offsetY" label="Y offset"></v-switch>
       <v-switch v-model="value" class="ma-2" label="Value"></v-switch>
     </v-layout>
 
@@ -12,7 +16,10 @@
       :disabled="disabled"
       :absolute="absolute"
       :open-on-hover="openOnHover"
-      offset-y
+      :close-on-click="closeOnClick"
+      :close-on-content-click="closeOnContentClick"
+      :offset-x="offsetX"
+      :offset-y="offsetY"
     >
       <template v-slot:activator="{ on }">
         <v-btn
@@ -49,6 +56,10 @@
       absolute: false,
       openOnHover: false,
       value: false,
+      closeOnClick: true,
+      closeOnContentClick: true,
+      offsetX: false,
+      offsetY: true,
     }),
   }
 </script>
