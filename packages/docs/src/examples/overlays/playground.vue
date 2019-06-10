4e1562101f58df31ded131b9b8c9d8d87eea1d5f
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-10 13:45:16 -0700

    docs(VOverlay): Add playground section.

---
+++ packages/docs/src/examples/overlays/playground.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-xs-center">
+    <v-btn
+      color="light-blue"
+      @click="overlay = !overlay"
+    >
+      Show Overlay
+    </v-btn>
+
+    <v-overlay :value="overlay">
+      <v-btn
+        icon
+        @click="overlay = false"
+      >
+        Close Overlay
+      </v-btn>
+    </v-overlay>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      absolute: false,
+      opacity: 0.46,
+      overlay: false,
+      zIndex: 5,
+    }),
+  }
+</script>
