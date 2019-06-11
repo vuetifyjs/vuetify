72c563c30ab338a6e4dc81a4a4af4eefb4ff568b
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-06-11 19:17:23 -0400

    docs(VNavigationDrawer): update playground

--- packages/docs/src/examples/navigation-drawers/playground.vue
+++ packages/docs/src/examples/navigation-drawers/playground.vue
@@ -4,42 +4,56 @@
       <v-flex xs12>
         <v-select v-model="color" :items="colors" label="Color"></v-select>
       </v-flex>
-      <v-switch v-model="permanent" class="ma-2" label="Permanent"></v-switch>
-      <v-switch v-model="right" class="ma-2" label="Right"></v-switch>
-      <v-switch v-model="floating" class="ma-2" label="Floating"></v-switch>
+      <v-switch v-model="drawer" class="ma-2" label="v-model"></v-switch>
       <v-switch v-model="miniVariant" class="ma-2" label="Mini variant"></v-switch>
       <v-switch v-model="expandOnHover" class="ma-2" label="Expand on hover"></v-switch>
       <v-switch v-model="background" class="ma-2" label="Background"></v-switch>
+      <v-switch v-model="dark" class="ma-2" label="Dark"></v-switch>
+      <v-switch v-model="permanent" class="ma-2" label="Permanent"></v-switch>
+      <v-switch v-model="temporary" class="ma-2" label="Temporary"></v-switch>
+      <v-switch v-model="absolute" class="ma-2" label="Absolute"></v-switch>
+      <v-switch v-model="bottom" class="ma-2" label="Bottom"></v-switch>
+      <v-switch v-model="right" class="ma-2" label="Right"></v-switch>
+      <v-flex xs12 text-xs-center pb-2>
+        <span>Position: </span>
+        <v-btn class="ma-2" @click="reset">Normal</v-btn>
+        <v-btn class="ma-2" @click="setBottom">Bottom</v-btn>
+        <v-btn class="ma-2" @click="setRight">Right</v-btn>
+      </v-flex>
     </v-layout>
     <v-card
       height="400"
     >
       <v-navigation-drawer
+        :v-model="drawer"
         :color="color"
-        :permanent="permanent"
         :right="right"
-        :floating="floating"
+        :bottom="bottom"
         :mini-variant="miniVariant"
         :expand-on-hover="expandOnHover"
         :src="bg"
+        :dark="dark"
+        :temporary="temporary"
+        :permanent="permanent"
+        :absolute="absolute"
       >
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title">
-              Application
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              subtext
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
         <v-list
           dense
           nav
+          class="py-0"
         >
+          <v-list-item two-line>
+            <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/men/81.jpg">
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Application</v-list-item-title>
+              <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
           <v-list-item
             v-for="item in items"
             :key="item.title"
@@ -63,12 +77,16 @@
   export default {
     data () {
       return {
+        drawer: true,
         items: [
           { title: 'Dashboard', icon: 'mdi-view-dashboard' },
           { title: 'Photos', icon: 'mdi-image' },
           { title: 'About', icon: 'mdi-help-box' },
         ],
+        dark: false,
         permanent: true,
+        temporary: false,
+        absolute: false,
         color: 'primary',
         colors: [
           'primary',
@@ -78,16 +96,29 @@
           'teal',
         ],
         right: false,
-        floating: false,
+        bottom: false,
         miniVariant: false,
         expandOnHover: false,
         background: false,
       }
     },
-
     computed: {
       bg () {
         return this.background ? 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg' : undefined
+      },
+    },
+    methods: {
+      reset () {
+        this.permanent = true
+        this.right = this.bottom = this.absolute = this.temporary = false
+      },
+      setRight () {
+        this.right = this.permanent = this.absolute = true
+        this.bottom = this.temporary = false
+      },
+      setBottom () {
+        this.bottom = this.temporary = this.absolute = true
+        this.right = this.permanent = false
       },
     },
   }
