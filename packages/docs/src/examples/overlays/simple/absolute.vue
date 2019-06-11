9cfa59c6eb2fac9f34289ffd9ec16993d75d5c8b
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-11 13:48:29 -0700

    docs(VOverlay): Add absolute example and desc.

---
+++ packages/docs/src/examples/overlays/simple/absolute.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout column align-center justify-center>
+    <v-card height="300" width="250">
+      <v-layout justify-center wrap>
+        <v-btn
+          color="green"
+          class="mt-5"
+          @click="overlay = !overlay"
+        >
+          Show Overlay
+        </v-btn>
+
+        <v-overlay
+          absolute
+          opacity="1"
+          :value="overlay"
+        >
+          <v-btn
+            green
+            @click="overlay = false"
+          >
+            Close Overlay
+          </v-btn>
+        </v-overlay>
+      </v-layout>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      overlay: false,
+    }),
+  }
+</script>
