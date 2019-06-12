9eb4343487af529cbc83ae6ea2dbe690ddabbada
John Leider <john.j.leider@gmail.com>
2019-06-12 15:50:08 -0400

    test(VMenu): expand a11y coverage

--- packages/vuetify/src/components/VMenu/__tests__/VMenu.spec.ts
+++ packages/vuetify/src/components/VMenu/__tests__/VMenu.spec.ts
@@ -1,14 +1,14 @@
 // Components
 import VMenu from '../VMenu'
-import VBtn from '../../VBtn/VBtn'
 import VCard from '../../VCard/VCard'
+import VListItem from '../../VList/VListItem'
 
 // Utilities
-import { compileToFunctions } from 'vue-template-compiler'
 import {
   mount,
   Wrapper,
 } from '@vue/test-utils'
+import { keyCodes } from '../../../util/helpers'
 
 describe('VMenu.ts', () => {
   type Instance = InstanceType<typeof VMenu>
@@ -26,7 +26,6 @@ describe('VMenu.ts', () => {
       })
     }
   })
-  // const wrapper = mountFunction()
 
   it('should work', async () => {
     const wrapper = mountFunction({
@@ -125,7 +124,6 @@ describe('VMenu.ts', () => {
   })
 
   it('should update position dynamically', async () => {
-    jest.useFakeTimers()
     const wrapper = mountFunction({
       propsData: {
         absolute: true,
@@ -139,16 +137,135 @@ describe('VMenu.ts', () => {
 
     // TODO replace with jest fakeTimers when it will support requestAnimationFrame: https://github.com/facebook/jest/pull/7776
     // See https://github.com/vuetifyjs/vuetify/pull/6330#issuecomment-460083547 for details
-    jest.runAllTimers()
-    await wrapper.vm.$nextTick()
     expect(content.attributes('style')).toMatchSnapshot()
 
     wrapper.setProps({
       positionX: 110,
       positionY: 220,
     })
-    jest.runAllTimers()
-    await wrapper.vm.$nextTick()
     expect(content.attributes('style')).toMatchSnapshot()
+  })
+
+  it('should select next and previous tiles and skip non links/disabled', () => {
+    const wrapper = mountFunction({
+      propsData: { eager: true },
+      scopedSlots: {
+        default () {
+          return this.$createElement('div', [
+            this.$createElement(VListItem, { props: { link: true } }),
+            this.$createElement(VListItem, { props: { link: true } }),
+            this.$createElement(VListItem),
+            this.$createElement(VListItem, { props: { link: true } }),
+          ])
+        },
+      },
+    })
+
+    wrapper.vm.getTiles()
+
+    expect(wrapper.vm.listIndex).toBe(-1)
+
+    wrapper.vm.nextTile()
+    expect(wrapper.vm.listIndex).toBe(0)
+
+    wrapper.vm.nextTile()
+    expect(wrapper.vm.listIndex).toBe(1)
+
+    wrapper.vm.nextTile()
+    expect(wrapper.vm.listIndex).toBe(3)
+
+    wrapper.vm.nextTile()
+    expect(wrapper.vm.listIndex).toBe(3)
+
+    wrapper.vm.prevTile()
+    expect(wrapper.vm.listIndex).toBe(1)
+
+    wrapper.vm.prevTile()
+    expect(wrapper.vm.listIndex).toBe(0)
+
+    wrapper.vm.prevTile()
+    expect(wrapper.vm.listIndex).toBe(0)
+
+    expect('Unable to locate target [data-app]').toHaveBeenTipped()
+  })
+
+  /* eslint-disable max-statements */
+  it('should change list index', async () => {
+    const event = (keyCode, shiftKey = false) => new KeyboardEvent('keydown', { keyCode, shiftKey })
+    const wrapper = mountFunction({
+      propsData: {
+        eager: true,
+        value: true,
+      },
+      scopedSlots: {
+        default () {
+          return this.$createElement('div', [
+            this.$createElement(VListItem, { props: { link: true } }),
+            this.$createElement(VListItem, { props: { link: true } }),
+          ])
+        },
+      },
+    })
+
+    // Move to next item
+    wrapper.vm.changeListIndex(event(keyCodes.tab))
+    expect(wrapper.vm.listIndex).toBe(0)
+
+    // When at index 0, shift tab should deactivate menu
+    // as it goes to the next dom tabindex
+    wrapper.vm.changeListIndex(event(keyCodes.tab, true))
+    await new Promise(resolve => window.requestAnimationFrame(resolve))
+    expect(wrapper.vm.isActive).toBe(false)
+
+    // Reset active state, setup listIndex
+    wrapper.setData({ isActive: true, listIndex: 1 })
+
+    // Move to prev item
+    wrapper.vm.changeListIndex(event(keyCodes.tab, true))
+    expect(wrapper.vm.listIndex).toBe(0)
+
+    // Move to next item
+    wrapper.vm.changeListIndex(event(keyCodes.down))
+    expect(wrapper.vm.listIndex).toBe(1)
+
+    // Move to prev item
+    wrapper.vm.changeListIndex(event(keyCodes.up))
+    expect(wrapper.vm.listIndex).toBe(0)
+
+    const onItemClick = jest.fn()
+    const items = wrapper.findAll('.v-list-item')
+    const item = items.at(0)
+    item.element.onclick = onItemClick
+
+    // Should invoke click on element
+    expect(item.element === wrapper.vm.activeTile).toBe(true)
+    wrapper.vm.changeListIndex(event(keyCodes.enter))
+    expect(onItemClick).toHaveBeenCalled()
+    item.element.onclick = null
+
+    // Reset active state
+    wrapper.setData({ isActive: true })
+
+    // Returns before preventDefault
+    const event1 = event(keyCodes.esc)
+    wrapper.vm.onKeyDown(event1) // use onKeyDown to get automatic menu deactivation
+    expect(event1.defaultPrevented).toBe(false)
+
+    await new Promise(resolve => setTimeout(resolve))
+
+    expect(wrapper.vm.isActive).toBe(false)
+
+    const event2 = event(keyCodes.esc)
+    wrapper.vm.changeListIndex(event2)
+    expect(event2.defaultPrevented).toBe(false)
+
+    expect('Unable to locate target [data-app]').toHaveBeenTipped()
+  })
+
+  it('should accept a custom role or use default', () => {
+    expect(mountFunction().vm.$refs.content.getAttribute('role')).toBe('menu')
+    expect(mountFunction({
+      attrs: { role: 'listbox' },
+    }).vm.$refs.content.getAttribute('role')).toBe('listbox')
   })
 })
