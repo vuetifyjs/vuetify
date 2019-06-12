6127d55e5f1aa7c75dcb79a5332b7162947658c4
sh7dm <d3dx12.xx@gmail.com>
2019-06-12 14:34:00 +0300

    docs(VOverflowBtn): add counter example

---
+++ packages/docs/src/examples/overflow-btns/simple/counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container id="dropdown-example-3">
+    <v-overflow-btn
+      class="my-2"
+      :items="dropdown_edit"
+      label="Editable Btn"
+      editable
+      counter
+      item-value="text"
+    ></v-overflow-btn>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dropdown_edit: [
+        { text: '100%' },
+        { text: '75%' },
+        { text: '50%' },
+        { text: '25%' },
+        { text: '0%' },
+      ],
+    }),
+  }
+</script>
