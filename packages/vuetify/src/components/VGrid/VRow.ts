d48fbf8c976a1f68ccf9df4612fe32eb65d88379
Kael <kaelwd@gmail.com>
2019-06-14 02:00:17 +1000

    feat(VRow): add breakpoint align/justify props

--- packages/vuetify/src/components/VGrid/VRow.ts
+++ packages/vuetify/src/components/VGrid/VRow.ts
@@ -1,7 +1,68 @@
-import Vue from 'vue'
+import Vue, { PropOptions } from 'vue'
 import mergeData from '../../util/mergeData'
+import { upperFirst } from '../../util/helpers'
+
+// no xs
+const breakpoints = ['sm', 'md', 'lg', 'xl']
 
 const ALIGNMENT = ['start', 'end', 'center']
+
+function makeProps (prefix: string, def: () => PropOptions) {
+  return breakpoints.reduce((props, val) => {
+    props[prefix + upperFirst(val)] = def()
+    return props
+  }, {} as Dictionary<PropOptions>)
+}
+
+const alignValidator = (str: any) => [...ALIGNMENT, 'baseline', 'stretch'].includes(str)
+const alignProps = makeProps('align', () => ({
+  type: String,
+  default: null,
+  validator: alignValidator
+}))
+
+const justifyValidator = (str: any) => [...ALIGNMENT, 'between', 'around'].includes(str)
+const justifyProps = makeProps('justify', () => ({
+  type: String,
+  default: null,
+  validator: justifyValidator
+}))
+
+const alignContentValidator = (str: any) => [...ALIGNMENT, 'between', 'around', 'stretch'].includes(str)
+const alignContentProps = makeProps('alignContent', () => ({
+  type: String,
+  default: null,
+  validator: alignContentValidator
+}))
+
+const propMap = {
+  align: Object.keys(alignProps),
+  justify: Object.keys(justifyProps),
+  alignContent: Object.keys(alignContentProps)
+}
+
+const classMap = {
+  align: 'align-items',
+  justify: 'justify-content',
+  alignContent: 'align-content'
+}
+
+function breakpointClass (type: keyof typeof propMap, prop: string, val: string) {
+  let className = classMap[type]
+  if (val == null) {
+    return undefined
+  }
+  if (prop) {
+    // alignSm -> Sm
+    const breakpoint = prop.replace(type, '')
+    className += `-${breakpoint}`
+  }
+  // .align-items-sm-center
+  className += `-${val}`
+  return className.toLowerCase()
+}
+
+const cache = new Map<string, any[]>()
 
 export default Vue.extend({
   name: 'v-row',
@@ -12,37 +73,61 @@ export default Vue.extend({
       default: 'div'
     },
     dense: Boolean,
-    noGutters: {
-      type: Boolean,
-      default: false
-    },
+    noGutters: Boolean,
     align: {
       type: String,
       default: null,
-      validator: (str: any) => [...ALIGNMENT, 'baseline', 'stretch'].includes(str)
+      validator: alignValidator
     },
+    ...alignProps,
     justify: {
       type: String,
       default: null,
-      validator: (str: any) => [...ALIGNMENT, 'between', 'around'].includes(str)
+      validator: justifyValidator
     },
+    ...justifyProps,
     alignContent: {
       type: String,
       default: null,
-      validator: (str: any) => [...ALIGNMENT, 'between', 'around', 'stretch'].includes(str)
-    }
+      validator: alignContentValidator
+    },
+    ...alignContentProps
   },
   render (h, { props, data, children }) {
+    // Super-fast memoization based on props, 5x faster than JSON.stringify
+    let cacheKey = ''
+    for (const prop in props) {
+      cacheKey += String((props as any)[prop])
+    }
+    let classList = cache.get(cacheKey)
+
+    if (!classList) {
+      classList = []
+      // Loop through `align`, `justify`, `alignContent` breakpoint props
+      let type: keyof typeof propMap
+      for (type in propMap) {
+        propMap[type].forEach(prop => {
+          const value: string = (props as any)[prop]
+          const className = breakpointClass(type, prop, value)
+          if (className) classList!.push(className)
+        })
+      }
+
+      classList.push({
+        'no-gutters': props.noGutters,
+        [`align-items-${props.align}`]: props.align,
+        [`justify-content-${props.justify}`]: props.justify,
+        [`align-content-${props.alignContent}`]: props.alignContent
+      })
+
+      cache.set(cacheKey, classList)
+    }
+
     return h(
       props.tag,
       mergeData(data, {
         staticClass: props.dense ? 'form-row' : 'row',
-        class: {
-          'no-gutters': props.noGutters,
-          [`align-items-${props.align}`]: props.align,
-          [`justify-content-${props.justify}`]: props.justify,
-          [`align-content-${props.alignContent}`]: props.alignContent
-        }
+        class: classList
       }),
       children
     )
