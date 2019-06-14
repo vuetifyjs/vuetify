8cbff1c04d1c6d0111d0dae5a819b434411a42c4
sh7dm <d3dx12.xx@gmail.com>
2019-06-14 16:47:49 +0300

    docs(VSelect): add menuProps example

---
+++ packages/docs/src/examples/selects/intermediate/menuProps.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout align-center>
+    <v-flex xs12>
+      <v-select
+        :items="items"
+        :menuProps="{ top: true, offsetY: true }"
+        label="Label"
+      ></v-select>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+    }),
+  }
+</script>
