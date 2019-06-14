2c4d2659d05320983a43196eca0cd9a6b5343fd6
Blaine Landowski <42892508+blalan05@users.noreply.github.com>
2019-06-14 08:33:26 -0500

    docs(VHover): update slot syntax (#7512)

--- packages/docs/src/examples/hover/usage.vue
+++ packages/docs/src/examples/hover/usage.vue
@@ -6,15 +6,16 @@
   >
     <v-flex xs12>
       <v-hover>
-        <v-card
-          slot-scope="{ hover }"
-          :elevation="hover ? 12 : 2"
-          class="mx-auto"
-          height="350"
-          max-width="350"
-        >
-          <v-card-text class="my-3 text-xs-center title">Hover over me!</v-card-text>
-        </v-card>
+        <template v-slot:default="{ hover }">
+          <v-card
+            :elevation="hover ? 12 : 2"
+            class="mx-auto"
+            height="350"
+            max-width="350"
+          >
+            <v-card-text class="my-3 text-xs-center title">Hover over me!</v-card-text>
+          </v-card>
+        </template>
       </v-hover>
     </v-flex>
   </v-layout>
