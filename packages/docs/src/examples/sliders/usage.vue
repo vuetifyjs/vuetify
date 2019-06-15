b1b227f693953c47b79340640e794d5b7bee4a2b
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-06-15 05:26:06 +0300

    docs(VSlider): update examples (#7506)
    * docs(VSlider): sort examples
    
    * docs(VSlider): add disabled example
    
    * docs(VSlider): add readonly example
    
    * docs(VSlider): add inverseLabel example
    
    * docs(VSlider): reduce usage to simple example
    
    * docs(VSlider): change label for usage
    
    * docs(VSlider): add playground

--- packages/docs/src/examples/sliders/usage.vue
+++ packages/docs/src/examples/sliders/usage.vue
@@ -9,78 +9,11 @@
     >
       <v-flex xs12>
         <v-slider
-          v-model="slider"
-        ></v-slider>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-slider
-          v-model="slider2"
-          label="Label"
-        ></v-slider>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-slider
-          disabled
-          label="Disabled"
-          value="30"
-        ></v-slider>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-slider
-          label="Readonly"
-          readonly
-          value="30"
-        ></v-slider>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-slider
-          v-model="slider3"
-          inverse-label
-          label="Inverse label"
-        ></v-slider>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-slider
           v-model="volume"
           append-icon="volume_up"
           prepend-icon="volume_down"
+          label="Volume"
         ></v-slider>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-range-slider
-          v-model="price"
-          :max="600"
-          :min="20"
-          :step="10"
-          hide-details
-        >
-          <template #prepend>
-            <v-text-field
-              v-model="price[0]"
-              class="pt-0 mt-0"
-              hide-details
-              single-line
-              type="number"
-              style="width: 60px"
-            ></v-text-field>
-          </template>
-          <template #append>
-            <v-text-field
-              v-model="price[1]"
-              class="pt-0 mt-0"
-              hide-details
-              single-line
-              type="number"
-              style="width: 60px"
-            ></v-text-field>
-          </template>
-        </v-range-slider>
       </v-flex>
     </v-layout>
   </v-container>
@@ -90,11 +23,7 @@
   export default {
     data () {
       return {
-        slider: 45,
-        slider2: 45,
-        slider3: 45,
         volume: 10,
-        price: [110, 440],
       }
     },
   }
