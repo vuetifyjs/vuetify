f6993351e19679933fc1a29b380c72ecf12278d4
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-17 19:02:46 -0700

    docs(VProgressLinear): Update descriptions.
    Update example descriptions. Update rounded example.

--- packages/docs/src/examples/progress-linear/simple/rounded.vue
+++ packages/docs/src/examples/progress-linear/simple/rounded.vue
@@ -1,66 +1,27 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-toolbar
-      color="deep-purple accent-4"
-      dark
-      prominent
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>My Files</v-toolbar-title>
-
-      <v-btn
-        absolute
-        bottom
-        color="white"
-        fab
-        left
-        light
-      >
-        <v-icon>mdi-plus</v-icon>
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-container style="height: 400px;">
-      <v-layout
-        align-content-center
-        justify-center
-        fill-height
-        wrap
-      >
-        <v-flex
-          xs12
-          subtitle-1
-          text-xs-center
-        >
-          Getting your files
-        </v-flex>
-        <v-flex xs6>
-          <v-progress-linear
-            color="deep-purple accent-4"
-            indeterminate
-            rounded
-            height="6"
-          ></v-progress-linear>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-card>
+  <div>
+    <v-progress-linear
+      color="red darken-2"
+      rounded
+      value="100"
+    ></v-progress-linear>
+    <br>
+    <v-progress-linear
+      color="indigo"
+      rounded
+      value="100"
+    ></v-progress-linear>
+    <br>
+    <v-progress-linear
+      color="teal"
+      rounded
+      value="100"
+    ></v-progress-linear>
+    <br>
+    <v-progress-linear
+      color="cyan darken-2"
+      rounded
+      value="100"
+    ></v-progress-linear>
+  </div>
 </template>
