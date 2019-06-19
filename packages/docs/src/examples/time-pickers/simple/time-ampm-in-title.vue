1b4c3c16bddc1541a486745e9a1eb9de046c0b4d
sh7dm <d3dx12.xx@gmail.com>
2019-06-19 13:41:06 +0300

    docs(VTimePicker\): add time-ampm-in-title example

---
+++ packages/docs/src/examples/time-pickers/simple/time-ampm-in-title.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout row wrap justify-space-around>
+    <v-time-picker v-model="picker" ampm-in-title></v-time-picker>
+    <v-time-picker v-model="picker" landscape ampm-in-title></v-time-picker>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        picker: null,
+      }
+    },
+  }
+</script>
