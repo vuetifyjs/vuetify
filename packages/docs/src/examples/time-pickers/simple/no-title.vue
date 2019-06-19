902f49d868d66f2393bba358605b2c6dfd0c49e7
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-06-19 17:50:05 +0300

    docs(VTimePicker): update examples (#7564)
    * docs(VTimePicker): sort examples
    
    * docs(VTimePicker): reduce usage example
    
    * docs(VTimePicker): add playground
    
    * chore: typo
    
    * docs(VTimePicker): fix layout in examples
    
    * docs(VTimePicker\): add time-ampm-in-title example
    
    * docs(VTimePicker): add time-no-title example
    
    * docs(VTimePicker): add time-disabled example
    
    * docs(VTimePicker): add time-readonly example
    
    * docs(VTimePicker): fix time-disabled example
    
    * docs(VTimePicker): unprefix all the examples
    
    * docs(VTimePicker): update titles
    
    * docs(VTimePicker): fix some layouts
    
    * docs(VTimePicker): add use-seconds example
    
    * docs(VTimePicker): add scrollable example
    
    * docs(VTimePicker): add range example

---
+++ packages/docs/src/examples/time-pickers/simple/no-title.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout row wrap justify-space-around>
+    <v-time-picker v-model="picker" no-title></v-time-picker>
+    <v-time-picker v-model="picker" landscape no-title></v-time-picker>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        picker: null,
+      }
+    },
+  }
+</script>
