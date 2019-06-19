75660b775668591c37353db8db6dca40503c3afb
Jacek Karczmarczyk <jk@drukcd.pl>
2019-06-19 22:10:13 +0700

    feat(rtl): rtl/ltr build

--- packages/vuetify/postcss.config.js
+++ packages/vuetify/postcss.config.js
@@ -1,12 +1,26 @@
 const autoprefixer = require('autoprefixer')
 const mqpacker = require('css-mqpacker')
 
+const dir = process.env.VUETIFY_DIRECTION;
+const vuetifyDirection = dir ? (css => css.walkRules(rule => {
+  const selectors = rule.selectors.filter(selector => {
+    return !selector.startsWith(`.application--is-${dir === 'rtl' ? 'ltr' : 'rtl'}`)
+  }).map(selector => selector.replace(`.application--is-${dir} `, ''))
+
+  if(selectors.length) {
+    rule.selectors = selectors
+  } else {
+    rule.remove()
+  }
+})) : (css => css)
+
 module.exports = ctx => ({
   plugins: [
     autoprefixer({
       browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all'],
       remove: false
     }),
-    mqpacker()
+    mqpacker(),
+    vuetifyDirection,
   ]
 })
