7caf9542209c1dcf3beccaf40f1bcf82dee74771
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-20 16:03:04 -0700

    docs(VTextarea): Add icons example.
    Add example and description for append-icon and prepend-icon props.

---
+++ packages/docs/src/examples/textarea/simple/icon.vue
@@ -0,0 +1,12 @@
+<v-container grid-list-md>
+  <v-textarea
+    append-icon="comment"
+    label="New message"
+    value="Enter your message..."
+  ></v-textarea>
+  <v-textarea
+    prepend-icon="comment"
+    label="New message"
+    value="Enter your message..."
+  ></v-textarea>
+</v-container>
