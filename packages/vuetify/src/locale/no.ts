b78fe28dc7c025391610f5195fe95e6830c44753
Johannes Røsvik <j.rosvik@gmail.com>
2019-06-20 09:12:27 +0200

    feat(locale): add Norwegian translation (#7587)

---
+++ packages/vuetify/src/locale/no.ts
@@ -0,0 +1,32 @@
+export default {
+  close: 'Lukk',
+  dataIterator: {
+    pageText: '{0}-{1} av {2}',
+    noResultsText: 'Fant ingen matchende elementer.',
+    loadingText: 'Laster elementer...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Rader per side:',
+    ariaLabel: {
+      sortDescending: ': Sortert synkende. Aktiver for å fjerne sortering.',
+      sortAscending: ': Sortert stigende. Aktiver for å sortere synkende.',
+      sortNone: ': Ikke sortert. Aktiver for å sortere stigende.',
+    },
+  },
+  dataFooter: {
+    itemsPerPageText: 'Elementer per side:',
+    itemsPerPageAll: 'Alle',
+    nextPage: 'Neste side',
+    prevPage: 'Forrige side',
+    firstPage: 'Første side',
+    lastPage: 'Siste side',
+  },
+  datePicker: {
+    itemsSelected: '{0} valgt',
+  },
+  noDataText: 'Ingen data er tilgjengelig',
+  carousel: {
+    prev: 'Forrige bilde',
+    next: 'Neste bilde',
+  },
+}
