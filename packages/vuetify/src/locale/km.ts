368cd77ba5e281aab848d64a1cccf32d44e24d0b
Thydaduong <thydaduong7@gmail.com>
2019-06-20 08:03:35 +0700

    And Khmer language
    Add Khmer locale for Cambodian users

---
+++ packages/vuetify/src/locale/km.ts
@@ -0,0 +1,19 @@
+
+export default {
+  dataIterator: {
+    rowsPerPageText: 'ចំនួនធាតុក្នុងមួយទំព័រ:',
+    rowsPerPageAll: 'ទាំងអស់',
+    pageText: '{0}-{1} នៃ {2}',
+    noResultsText: 'រកមិនឃើញកំណត់ត្រា',
+    nextPage: 'ទំព័របន្ទាប់',
+    prevPage: 'ទំព័រមុន'
+  },
+  dataTable: {
+    rowsPerPageText: 'ចំនួនជួរដេកក្នុងមួយទំព័រ:'
+  },
+  noDataText: 'មិនមានទិន្នន័យ',
+  carousel: {
+    prev: 'មុន',
+    next: 'បន្ទាប់'
+  }
+}
