37aa3402072aabf306f5a6e00906cf3b90769434
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-22 00:43:22 -0700

    docs(VTextarea): Add counter.
    Add counter example and description.

--- packages/docs/src/examples/textarea/simple/clearable.vue
+++ packages/docs/src/examples/textarea/simple/clearable.vue
@@ -1,8 +1,10 @@
-<v-container fluid grid-list-md>
-  <v-textarea
-    clearable
-    clear-icon="cancel"
-    label="Text"
-    value="This is clearable text."
-  ></v-textarea>
-</v-container>
+<template>
+  <v-container fluid grid-list-md>
+    <v-textarea
+      clearable
+      clear-icon="cancel"
+      label="Text"
+      value="This is clearable text."
+    ></v-textarea>
+  </v-container>
+</template>
---
+++ packages/docs/src/examples/textarea/simple/counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid grid-list-md>
+    <v-textarea
+      counter
+      label="Text"
+      :rules="rules"
+      placeholder="Type something..."
+    ></v-textarea>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      rules: [v => v.length <= 25 || 'Max 25 characters'],
+    }),
+  }
+</script>
