3e2e2922a8e982e68aebc23ab9f3e3ac96d768e4
John Leider <john.j.leider@gmail.com>
2019-06-24 16:47:22 -0400

    feat(VFileInput): extend v-text-field, minor clean-up

--- packages/vuetify/src/components/VFileInput/VFileInput.ts
+++ packages/vuetify/src/components/VFileInput/VFileInput.ts
@@ -1,13 +1,10 @@
-import VInput from '../VInput'
-import mixins, { ExtractVue } from '../../util/mixins'
+// Styles
+import './VFileInput.sass'
 
-interface options extends ExtractVue<typeof VInput> {
-  $refs: {
-    input: HTMLInputElement
-  }
-}
+// Extensions
+import VTextField from '../VTextField'
 
-export default mixins<options>(VInput).extend({
+export default VTextField.extend({
   name: 'v-file-input',
 
   props: {
@@ -24,77 +21,77 @@ export default mixins<options>(VInput).extend({
     placeholder: String,
   },
 
-  computed: {
-    text (): string | null {
-      if (!this.lazyValue || !this.lazyValue.length) return this.placeholder
+  data: () => ({
+    lazyFileValue: [] as any[],
+  }),
 
-      // TODO: use $t for X selected
-      return this.lazyValue.length > 1 ? `${this.lazyValue.length} files selected` : this.lazyValue[0].name
+  computed: {
+    classes (): object {
+      return {
+        ...VTextField.options.computed.classes.call(this),
+        'v-file-input': true,
+      }
+    },
+    isDirty (): boolean {
+      return this.lazyFileValue.length > 0
+    },
+    text (): string | null {
+      if (!this.isDirty) return this.placeholder
+
+      return this.lazyFileValue.map(file => file.name).join(', ')
     },
   },
 
   methods: {
-    genDefaultSlot () {
-      return [
-        this.genLabel(),
-        this.genInput(),
-        this.genText(),
-      ]
+    genIconSlot () {
+      const slot = []
+
+      if (this.isDirty) {
+        slot.push(this.genIcon('append', () => {
+          this.lazyFileValue = []
+          this.internalValue = null
+        }))
+      }
+
+      return this.genSlot('append', 'inner', slot)
     },
     genPrependSlot () {
       const icon = this.genIcon('prepend', () => {
         this.$refs.input.click()
       })
 
-      if (icon.data!.attrs) {
-        icon.data!.attrs.tabindex = 0
-      } else {
-        icon.data!.attrs = { tabindex: 0 }
-      }
+      icon.data!.attrs! = { tabindex: 0 }
 
       return this.genSlot('prepend', 'outer', [icon])
     },
-    genAppendSlot () {
-      const slot = []
+    genInput () {
+      const input = VTextField.options.methods.genInput.call(this)
 
-      // Append icon for text field was really
-      // an appended inner icon, v-text-field
-      // will overwrite this method in order to obtain
-      // backwards compat
-      if (this.$slots.append) {
-        slot.push(this.$slots.append)
-      } else if (this.clearable && this.appendIcon) {
-        slot.push(this.genIcon('append', () => {
-          this.lazyValue = null
-          this.$refs.input.value = ''
-        }))
+      input.data!.domProps!.type = 'file'
+      input.data!.attrs!.multiple = this.multiple
+      input.data!.attrs!.readonly = true
+      input.data!.attrs!['aria-readonly'] = String(this.readonly)
+      input.data!.on!.change = (e: any) => {
+        this.lazyFileValue = [...e.target.files]
       }
 
-      return this.genSlot('append', 'outer', slot)
-    },
-    genInput () {
-      return this.$createElement('input', {
-        ref: 'input',
-        attrs: {
-          'aria-label': !this.id && this.label, // Label `for` will be set if we have an id
-          ...this.$attrs,
-          disabled: this.disabled,
-          placeholder: this.placeholder,
-          readonly: this.readonly,
-          id: this.id,
-          hidden: true,
-          type: 'file',
-          multiple: this.multiple,
-        },
-        on: {
-          change: (e: any) => {
-            this.lazyValue = [...e.target.files]
-          },
-        },
-      })
+      return [
+        this.genText(),
+        input,
+      ]
     },
     genText () {
-      return this.$createElement('div', [this.text])
+      return this.$createElement('div', {
+        staticClass: 'v-file-input__text',
+        class: {
+          'v-file-input__text--placeholder': this.placeholder && !this.isDirty,
+        },
+        on: {
+          click: () => {
+            this.$refs.input.click()
+          },
+        },
+      }, [this.text])
     },
   },
 })
