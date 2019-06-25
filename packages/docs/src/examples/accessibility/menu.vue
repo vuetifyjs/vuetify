9bdd8564c9001aa434f5a1301cad8031f69b607d
John Leider <john.j.leider@gmail.com>
2019-06-25 14:24:26 -0400

    docs(Accessibility): add new page

---
+++ packages/docs/src/examples/accessibility/menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-xs-center">
+    <v-menu>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click me
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item @click="method">
+          <v-list-item-title>Option 1</v-list-item-title>
+        </v-list-item>
+        <v-list-item disabled>
+          <v-list-item-title>Option 2</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="method">
+          <v-list-item-title>Option 3</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      method () {
+        // Perform an action
+      },
+    },
+  }
+</script>
