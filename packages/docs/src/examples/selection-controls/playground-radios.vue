9ed1223fac7f76d478070e3270ab83b800438c3f
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-06-25 18:03:55 +0300

    docs(SelectionControls): update examples (#7637)
    * docs(SelectionControls): sort examples
    
    * chore: add missing files
    
    * docs(VCheckbox): add playground
    
    * chore: fix lint errors
    
    * docs(VRating): add playground
    
    * docs(VSwitch): add playground
    
    * docs(SelectionControls): update examples

---
+++ packages/docs/src/examples/selection-controls/playground-radios.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-layout row wrap justify-space-around>
+      <v-switch v-model="disabled" class="mx-2" label="Disabled"></v-switch>
+      <v-switch v-model="readonly" class="mx-2" label="Read-only"></v-switch>
+      <v-switch v-model="error" class="mx-2" label="Error"></v-switch>
+      <v-switch v-model="success" class="mx-2" label="Success"></v-switch>
+      <v-switch v-model="row" class="mx-2" label="Row"></v-switch>
+      <v-flex xs12>
+        <v-select v-model="color" :items="colors" label="Color"></v-select>
+      </v-flex>
+    </v-layout>
+
+    <v-radio-group
+      :disabled="disabled"
+      :readonly="readonly"
+      :error="error"
+      :success="success"
+      :row="row"
+      :color="color"
+    >
+      <v-radio label="I'm a radio button"></v-radio>
+      <v-radio label="I'm a radio button"></v-radio>
+      <v-radio label="I'm a radio button"></v-radio>
+    </v-radio-group>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      disabled: false,
+      readonly: false,
+      error: false,
+      success: false,
+      row: false,
+      color: 'accent',
+      colors: [
+        'accent',
+        'teal',
+        'blue',
+        'error',
+        'success',
+      ],
+    }),
+  }
+</script>
---
+++ packages/docs/src/examples/selection-controls/playground-switches.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-layout row wrap justify-space-around>
+      <v-checkbox v-model="disabled" class="mx-2" label="Disabled"></v-checkbox>
+      <v-checkbox v-model="readonly" class="mx-2" label="Read-only"></v-checkbox>
+      <v-checkbox v-model="loading" class="mx-2" label="Loading"></v-checkbox>
+      <v-checkbox v-model="flat" class="mx-2" label="Flat"></v-checkbox>
+      <v-checkbox v-model="inset" class="mx-2" label="Inset"></v-checkbox>
+      <v-checkbox v-model="error" class="mx-2" label="Error"></v-checkbox>
+      <v-checkbox v-model="success" class="mx-2" label="Success"></v-checkbox>
+      <v-flex xs12>
+        <v-select v-model="color" :items="colors" label="Color"></v-select>
+      </v-flex>
+    </v-layout>
+
+    <v-switch
+      :disabled="disabled"
+      :readonly="readonly"
+      :loading="loading"
+      :flat="flat"
+      :inset="inset"
+      :error="error"
+      :success="success"
+      :color="color"
+      label="I'm a switch"
+    ></v-switch>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      disabled: false,
+      readonly: false,
+      loading: false,
+      flat: false,
+      inset: false,
+      error: false,
+      success: false,
+      color: 'accent',
+      colors: [
+        'accent',
+        'teal',
+        'blue',
+        'error',
+        'success',
+      ],
+    }),
+  }
+</script>
