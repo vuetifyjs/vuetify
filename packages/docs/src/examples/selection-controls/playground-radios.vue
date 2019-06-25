f8502937ceae1a45d58fe35679941193ddfa58ec
John Leider <john.j.leider@gmail.com>
2019-06-25 11:02:44 -0400

    docs(SelectionControls): update examples

--- packages/docs/src/examples/selection-controls/playground-radios.vue
+++ packages/docs/src/examples/selection-controls/playground-radios.vue
@@ -3,8 +3,6 @@
     <v-layout row wrap justify-space-around>
       <v-switch v-model="disabled" class="mx-2" label="Disabled"></v-switch>
       <v-switch v-model="readonly" class="mx-2" label="Read-only"></v-switch>
-      <v-switch v-model="mandatory" class="mx-2" label="Mandatory"></v-switch>
-      <v-switch v-model="multiple" class="mx-2" label="Multiple"></v-switch>
       <v-switch v-model="error" class="mx-2" label="Error"></v-switch>
       <v-switch v-model="success" class="mx-2" label="Success"></v-switch>
       <v-switch v-model="row" class="mx-2" label="Row"></v-switch>
@@ -16,8 +14,6 @@
     <v-radio-group
       :disabled="disabled"
       :readonly="readonly"
-      :mandatory="mandatory"
-      :multiple="multiple"
       :error="error"
       :success="success"
       :row="row"
@@ -35,8 +31,6 @@
     data: () => ({
       disabled: false,
       readonly: false,
-      mandatory: false,
-      multiple: false,
       error: false,
       success: false,
       row: false,
