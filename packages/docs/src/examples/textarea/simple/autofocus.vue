7b3489eadc4e0d6ff04566b1f06322c7d839f87f
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-25 10:04:19 -0700

    chore(VTextarea): Remove autofocus.
    Remove autofocus example, description, and section.

--- packages/docs/src/examples/textarea/simple/autofocus.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container fluid grid-list-md>
-    <v-textarea
-      autofocus
-      label="Label"
-      placeholder="I am an autofocused textarea... "
-    ></v-textarea>
-  </v-container>
-</template>
