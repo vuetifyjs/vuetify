86e443a759315cc3225531d1623182424fd43108
John Leider <john.j.leider@gmail.com>
2019-06-26 15:52:45 -0400

    docs: add tidelift-enterprise page

---
+++ packages/docs/src/components/supporters/TideliftMoreDemo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="pa-3">
+    <v-btn
+      class="ma-1"
+      color="#F6914D"
+      height="32"
+      outlined
+      style="background-color: #FFF !important;"
+      width="192"
+    >
+      Learn More
+    </v-btn>
+    <v-btn
+      class="white--text ma-1"
+      color="#F6914D"
+      depressed
+      height="32"
+      width="192"
+    >
+      Request a Demo
+    </v-btn>
+  </div>
+</template>
