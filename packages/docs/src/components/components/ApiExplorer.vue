8cc4c6493602d9e5aec6ec14ab0208c4db8937f6
John Leider <john.j.leider@gmail.com>
2019-06-28 17:35:18 -0400

    docs: improve a11y

--- packages/docs/src/components/components/ApiExplorer.vue
+++ packages/docs/src/components/components/ApiExplorer.vue
@@ -33,7 +33,10 @@
             <v-icon v-text="props.item.icon" />
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="props.item.text" />
+            <v-list-item-title
+              :id="props.attrs['aria-labelledby']"
+              v-text="props.item.text"
+            />
             <v-list-item-subtitle v-text="props.item.subtext" />
           </v-list-item-content>
         </template>
@@ -45,12 +48,12 @@
       text-xs-center
       grey--text
     >
-      <h3
+      <h2
         class="display-1"
         v-text="$t('Components.ApiExplorer.search')"
       />
       <div v-text="$t('Components.ApiExplorer.or')" />
-      <h3
+      <h2
         class="display-1"
         v-text="$t('Components.ApiExplorer.browseCategories')"
       />
