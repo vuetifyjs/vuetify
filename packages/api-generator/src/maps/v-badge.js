1d2a78b5d308eb66ba5e3bee4613bd2344e49293
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-30 00:48:11 -0700

    docs(api): update vbadge maps

--- packages/api-generator/src/maps/v-badge.js
+++ packages/api-generator/src/maps/v-badge.js
@@ -1,5 +1,47 @@
 module.exports = {
   'v-badge': {
-    slots: ['badge', 'default']
-  }
+    props: [
+      {
+        name: 'bottom',
+        default: 'false',
+        type: 'Boolean',
+      },
+      {
+        name: 'color',
+        default: 'primary',
+        type: 'String',
+      },
+      {
+        name: 'left',
+        default: 'false',
+        type: 'Boolean',
+      },
+      {
+        name: 'mode',
+        default: 'undefined',
+        type: 'String',
+      },
+      {
+        name: 'origin',
+        default: 'undefined',
+        type: 'String',
+      },
+      {
+        name: 'overlap',
+        default: 'false',
+        type: 'Boolean',
+      },
+      {
+        name: 'transition',
+        default: 'fab-transition',
+        type: 'String',
+      },
+      {
+        name: 'value',
+        default: 'true',
+        type: 'Any', // @TODO Ask ?
+      },
+    ],
+    slots: ['badge', 'default'],
+  },
 }
