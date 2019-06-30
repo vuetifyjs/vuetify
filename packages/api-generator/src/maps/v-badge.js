b3620367ca14220cfc8610d3ac15fc762b817b37
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-30 10:50:53 -0700

    chore(vbadge): fix maps

--- packages/api-generator/src/maps/v-badge.js
+++ packages/api-generator/src/maps/v-badge.js
@@ -1,47 +1,5 @@
 module.exports = {
   'v-badge': {
-    props: [
-      {
-        name: 'bottom',
-        default: 'false',
-        type: 'Boolean',
-      },
-      {
-        name: 'color',
-        default: 'primary',
-        type: 'String',
-      },
-      {
-        name: 'left',
-        default: 'false',
-        type: 'Boolean',
-      },
-      {
-        name: 'mode',
-        default: 'undefined',
-        type: 'String',
-      },
-      {
-        name: 'origin',
-        default: 'undefined',
-        type: 'String',
-      },
-      {
-        name: 'overlap',
-        default: 'false',
-        type: 'Boolean',
-      },
-      {
-        name: 'transition',
-        default: 'fab-transition',
-        type: 'String',
-      },
-      {
-        name: 'value',
-        default: 'true',
-        type: 'Any', // @TODO Ask ?
-      },
-    ],
-    slots: ['badge', 'default'],
-  },
+    slots: ['badge', 'default']
+  }
 }
