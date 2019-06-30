2ba075879c4917831810a0bb41bb75a206c3b79d
sh7dm <d3dx12.xx@gmail.com>
2019-06-30 12:31:13 +0300

    docs(VBanner): add api map

---
+++ packages/api-generator/src/maps/v-banner.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'v-banner': {
+    slots: ['default', 'icon', 'actions'],
+    scopedSlots: [{
+      name: 'actions',
+      props: {
+        dismiss: '(): void'
+      }
+    }],
+    functions: [
+      {
+        name: 'toggle',
+        signature: '(): void'
+      }
+    ]
+  }
+}
