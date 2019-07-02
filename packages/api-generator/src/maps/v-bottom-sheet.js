4e7d0282c05392138d9a24640ca2e0839817aa5e
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-07-01 18:19:53 -0700

    docs(VBottomSheet): api update for 2.0 (#7710)
    * docs(VBottomSheet): update prop descriptions
    
    * docs(VBottomSheet): Add missing props.
    
    * docs(VBottomSheet): Add activator to scoped slots
    
    * chore(VBottomSheet): Update prop descriptions.

--- packages/api-generator/src/maps/v-bottom-sheet.js
+++ packages/api-generator/src/maps/v-bottom-sheet.js
@@ -1,5 +1,14 @@
 module.exports = {
   'v-bottom-sheet': {
-    slots: ['activator', 'default'],
+    slots: ['default'],
+    scopedSlots: [
+      {
+        name: 'activator',
+        props: {
+          on: '{ [eventName]: eventHandler }',
+          value: 'boolean',
+        },
+      },
+    ],
   },
 }
