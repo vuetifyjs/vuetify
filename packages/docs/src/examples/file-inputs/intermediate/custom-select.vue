1ead0ca67b0cea6b3f6f6e1196d082efc91ec6c7
sh7dm <d3dx12.xx@gmail.com>
2019-07-08 21:21:26 +0300

    docs(VFileInput): add custom-selection

---
+++ packages/docs/src/examples/file-inputs/intermediate/custom-select.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-file-input label="File input">
+    <template #selection="{ files }">
+      <v-chip v-for="file in files" :key="file.name">
+        {{ file.size }} B large file named {{ file.name }}
+      </v-chip>
+    </template>
+  </v-file-input>
+</template>
