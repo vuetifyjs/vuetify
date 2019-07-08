3bedf9c6f582efc959d4ac9b6ebd6192e4e755c2
John Leider <john.j.leider@gmail.com>
2019-07-08 09:49:47 -0400

    docs(Selects): add information regarding item-text/value
    placed an alert and updated the property to have a type for items
    
    resolves #7775

--- packages/api-generator/src/maps/v-select.js
+++ packages/api-generator/src/maps/v-select.js
@@ -1,5 +1,15 @@
 const { VSelect } = require('../variables')
 
+const ItemObject = {
+  text: "'string' | 'number' | object'",
+  value: "'string' | 'number' | object'",
+}
+
+VSelect.props.push({
+  name: 'items',
+  example: ItemObject,
+})
+
 module.exports = {
   'v-select': VSelect,
 }
