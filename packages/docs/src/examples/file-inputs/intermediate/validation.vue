a2336411baedb181ad21779b9296c8d5b666c974
sh7dm <d3dx12.xx@gmail.com>
2019-07-08 21:42:37 +0300

    docs(VFileInput): add validation example

---
+++ packages/docs/src/examples/file-inputs/intermediate/validation.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-file-input :rules="rules" label="File input"></v-file-input>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    rules: [
+      value => value[0].size < 2000000
+    ]
+  })
+}
+</script>
