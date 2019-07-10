7431416230fb65952db84a26a0f04b4268aaf4e7
Kael <kaelwd@gmail.com>
2019-07-10 23:37:07 +1000

    fix(build): remove mqpacker
    breaks ordering of grid breakpoints

--- packages/vuetify/postcss.config.js
+++ packages/vuetify/postcss.config.js
@@ -1,12 +1,10 @@
 const autoprefixer = require('autoprefixer')
-const mqpacker = require('css-mqpacker')
 
 module.exports = ctx => ({
   plugins: [
     autoprefixer({
       browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all'],
       remove: false
-    }),
-    mqpacker()
+    })
   ]
 })
