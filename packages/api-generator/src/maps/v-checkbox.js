f7cd0459f41ad71368d3cc3580681f41063bdc31
John Leider <john.j.leider@gmail.com>
2019-07-19 13:42:02 -0400

    fix(VCheckbox): normalize indeterminate behavior with native checkbox input

--- packages/api-generator/src/maps/v-checkbox.js
+++ packages/api-generator/src/maps/v-checkbox.js
@@ -3,5 +3,11 @@ const { inputSlots } = require('../helpers/variables')
 module.exports = {
   'v-checkbox': {
     slots: inputSlots.concat(['label']),
+    events: [
+      {
+        name: 'update:indeterminate',
+        value: 'boolean',
+      },
+    ],
   },
 }
