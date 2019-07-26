e45749b21974d030f719c431852e0f760215b6ee
Canh Kieu <minhcanhdn0704@gmail.com>
2019-07-26 10:56:11 +0700

    Create vi.ts language
    Add vietnamese language.

---
+++ packages/vuetify/src/locale/vi.ts
@@ -0,0 +1,39 @@
+export default {
+  close: 'Đóng',
+  dataIterator: {
+    pageText: '{0}-{1} của {2}',
+    noResultsText: 'Không tìm thấy dữ liệu phù hợp',
+    loadingText: 'Tải dữ liệu...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Dòng mỗi trang:',
+    ariaLabel: {
+      sortDescending: ': Sắp xếp giảm dần. Kích hoạt để tắt xắp xếp',
+      sortAscending: ': Sắp xếp tăng dần. Kích hoạt để sắp xếp giảm dần.',
+      sortNone: ': Tắt sắp xếp. Kích hoạt để sắp xếp tăng dần',
+    },
+  },
+  dataFooter: {
+    itemsPerPageText: 'Dòng mỗi trang:',
+    itemsPerPageAll: 'Tất cả',
+    nextPage: 'Trang tiếp',
+    prevPage: 'Trang trước',
+    firstPage: 'Trang đầu',
+    lastPage: 'Trang cuối',
+  },
+  datePicker: {
+    itemsSelected: 'Đã chọn {0}',
+  },
+  noDataText: 'Không có dữ liệu',
+  carousel: {
+    prev: 'Trước',
+    next: 'Tiếp',
+  },
+  calendar: {
+    moreEvents: 'còn {0}',
+  },
+  fileInput: {
+    counter: '{0} tập tin',
+    counterSize: '{0} tập tin (trong tổng {1})',
+  },
+}
