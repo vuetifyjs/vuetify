2de4e726b27c4910c8ad0d29cc7d625ba9209040
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-07-29 19:34:10 -0400

    docs(fab): fix extended toolbar examples
    closes #8155

--- packages/docs/src/examples/floating-action-buttons/usage.vue
+++ packages/docs/src/examples/floating-action-buttons/usage.vue
@@ -25,17 +25,19 @@
         <v-card>
           <v-toolbar extended>
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
-            <v-btn
-              color="pink"
-              dark
-              small
-              absolute
-              bottom
-              left
-              fab
-            >
-              <v-icon>add</v-icon>
-            </v-btn>
+            <template v-slot:extension>
+              <v-btn
+                color="pink"
+                dark
+                small
+                absolute
+                bottom
+                left
+                fab
+              >
+                <v-icon>add</v-icon>
+              </v-btn>
+            </template>
           </v-toolbar>
           <v-card-text style="height: 236px;" class="grey lighten-5"></v-card-text>
           <v-card-text style="height: 100px; position: relative"></v-card-text>
