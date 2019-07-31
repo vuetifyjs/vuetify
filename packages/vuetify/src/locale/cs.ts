5632a53488e7a760940bb829ed96b32e209a669d
Marek Lukas <tajnymag@gmail.com>
2019-07-31 19:50:04 +0200

    Added czech translation

---
+++ packages/vuetify/src/locale/cs.ts
@@ -0,0 +1,39 @@
+export default {
+  close: 'Zavřít',
+  dataIterator: {
+    pageText: '{0}-{1} z {2}',
+    noResultsText: 'Nenalezeny žádné záznamy',
+    loadingText: 'Načítám položky...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Řádků na stránku:',
+    ariaLabel: {
+      sortDescending: ': Řazeno sestupně. Aktivováním vypnete řazení.',
+      sortAscending: ': Řazeno vzestupně. Aktivováním se bude řadit sestupně.',
+      sortNone: ': Neseřazeno. Aktivováním se bude řadit vzestupně.',
+    },
+  },
+  dataFooter: {
+    itemsPerPageText: 'Položek na stránku:',
+    itemsPerPageAll: 'Vše',
+    nextPage: 'Další strana',
+    prevPage: 'Předchozí strana',
+    firstPage: 'První strana',
+    lastPage: 'Poslední strana',
+  },
+  datePicker: {
+    itemsSelected: '{0} vybráno',
+  },
+  noDataText: 'Nejsou dostupná žádná data',
+  carousel: {
+    prev: 'Předchozí obrázek',
+    next: 'Další obrázek',
+  },
+  calendar: {
+    moreEvents: '{0} dalších',
+  },
+  fileInput: {
+    counter: '{0} souborů',
+    counterSize: '{0} souborů ({1} celkem)',
+  },
+}
