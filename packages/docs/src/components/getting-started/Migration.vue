a7f398421b2a44fdff2a46e466f69f8bf6e17c06
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-08-06 15:17:15 -0400

    docs(migration): fix markdown code generation

--- packages/docs/src/components/getting-started/Migration.vue
+++ packages/docs/src/components/getting-started/Migration.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container px-0>
-    <doc-markdown>{{ migration }}</doc-markdown>
+    <doc-markdown :code="migration" />
   </v-container>
 </template>
 
@@ -8,23 +8,23 @@
   import { getBranch } from '@/util/helpers'
 
   export default {
-    inheritAttrs: false,
-
     data: () => ({
-      migration: '',
+      migration: ' ',
       branch: undefined,
     }),
 
     mounted () {
       this.branch = getBranch()
-      fetch(`https://api.github.com/repos/vuetifyjs/vuetify/contents/MIGRATION.md?ref=${this.branch}`, {
+      // replace with this when ready to push pr
+      // fetch(`https://api.github.com/repos/vuetifyjs/vuetify/contents/MIGRATION.md?ref=${this.branch}`, {
+      fetch(`https://api.github.com/repos/vuetifyjs/vuetify/contents/MIGRATION.md?ref=feat/migration-page`, {
         method: 'get',
         headers: { 'Content-Type': 'application/json' },
       })
         .then(res => res.json())
         .then(res => {
           const { content } = res
-          this.migration = content ? atob(content) : ''
+          this.migration = content ? Buffer.from(content, 'base64').toString() : ' '
         })
         .catch(err => console.log(err))
     },
