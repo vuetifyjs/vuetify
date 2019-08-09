cd74a30cfff6018a32979ded1f1baedb60092ff4
nekosaur <albert@kaaman.nu>
2019-08-09 20:27:32 +0200

    feat(VDataTable): added filterable property to header items
    if filterable is set to false on header item, it is no longer searchable
    
    closes #8242

--- packages/vuetify/src/components/VDataTable/mixins/header.ts
+++ packages/vuetify/src/components/VDataTable/mixins/header.ts
@@ -13,6 +13,7 @@ export interface TableHeader {
   value: string
   align?: 'start' | 'center' | 'end'
   sortable?: boolean
+  filterable?: boolean
   divider?: boolean
   class?: string | string[]
   width?: string | number
