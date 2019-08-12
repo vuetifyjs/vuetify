678cd239821dbec67c20a1051c382c2da3d3955e
John Leider <john.j.leider@gmail.com>
2019-08-12 11:45:27 -0400

    docs(Elevation): update page

---
+++ packages/docs/src/examples/elevation/simple/dynamic.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text--primary">
+    <!-- Using the elevation prop -->
+    <v-hover>
+      <template v-slot="{ hover }">
+        <v-card
+          :elevation="hover ? 24 : 6"
+          class="mx-auto pa-6"
+        >
+          Prop based elevation
+        </v-card>
+      </template>
+    </v-hover>
+
+    <div class="my-6"></div>
+
+    <!-- Using a dynamic class -->
+    <v-hover>
+      <template v-slot="{ hover }">
+        <div
+          :class="`elevation-${hover ? 24 : 6}`"
+          class="mx-auto pa-6 transition-swing"
+        >
+          Class based elevation
+        </div>
+      </template>
+    </v-hover>
+  </div>
+</template>
