67a2379d07c7f83f0ffc27a110b6989d825841bd
Mohamed Elbahja <mohamed.jss11@gmail.com>
2019-08-31 00:29:50 +0100

    make dense text fields single except outlined

--- packages/vuetify/src/components/VTextField/VTextField.ts
+++ packages/vuetify/src/components/VTextField/VTextField.ts
@@ -136,7 +136,7 @@ export default baseMixins.extend<options>().extend({
       return this.isDirty || dirtyTypes.includes(this.type)
     },
     isSingle (): boolean {
-      return this.isSolo || this.singleLine || this.fullWidth
+      return this.isSolo || this.singleLine || this.fullWidth || (this.dense && !this.outlined)
     },
     isSolo (): boolean {
       return this.solo || this.soloInverted
