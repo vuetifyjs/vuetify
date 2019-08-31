3140f724e2befc62025dc80be73eedc3c7c28a65
Jacek Karczmarczyk <jk@drukcd.pl>
2019-08-31 22:03:10 +0200

    docs(VBtn): added x-small/x-large to sizing example

--- packages/docs/src/examples/buttons/simple/sizing.vue
+++ packages/docs/src/examples/buttons/simple/sizing.vue
@@ -4,6 +4,9 @@
       <v-col cols="12" sm="6">
         <div class="text-center">
           <div class="my-2">
+            <v-btn x-small color="secondary" dark>Extra small Button</v-btn>
+          </div>
+          <div class="my-2">
             <v-btn small color="primary" dark>Small Button</v-btn>
           </div>
           <div class="my-2">
@@ -12,10 +15,18 @@
           <div class="my-2">
             <v-btn color="error" dark large>Large Button</v-btn>
           </div>
+          <div class="my-2">
+            <v-btn x-large color="success" dark>Extra large Button</v-btn>
+          </div>
         </div>
       </v-col>
       <v-col cols="12" sm="6">
         <div class="text-center">
+          <div class="my-2">
+            <v-btn color="secondary" fab x-small dark>
+              <v-icon>mdi-television</v-icon>
+            </v-btn>
+          </div>
           <div class="my-2">
             <v-btn color="primary" fab small dark>
               <v-icon>mdi-pencil</v-icon>
@@ -29,6 +40,11 @@
           <div class="my-2">
             <v-btn color="error" fab large dark>
               <v-icon>mdi-alarm</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="success" fab x-large dark>
+              <v-icon>mdi-domain</v-icon>
             </v-btn>
           </div>
         </div>
