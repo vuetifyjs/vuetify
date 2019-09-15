94961fc1371c4a6374b4c6c8a103c262bc45fadf
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-09-14 21:32:01 -0400

    docs: wrap single select comps w/ multiple

--- packages/docs/src/examples/combobox/intermediate/no-data.vue
+++ packages/docs/src/examples/combobox/intermediate/no-data.vue
@@ -1,25 +1,27 @@
 <template>
-  <v-combobox
-    v-model="model"
-    :items="items"
-    :search-input.sync="search"
-    hide-selected
-    hint="Maximum of 5 tags"
-    label="Add some tags"
-    multiple
-    persistent-hint
-    small-chips
-  >
-    <template v-slot:no-data>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>
-            No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </template>
-  </v-combobox>
+  <v-container fluid>
+    <v-combobox
+      v-model="model"
+      :items="items"
+      :search-input.sync="search"
+      hide-selected
+      hint="Maximum of 5 tags"
+      label="Add some tags"
+      multiple
+      persistent-hint
+      small-chips
+    >
+      <template v-slot:no-data>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-combobox>
+  </v-container>
 </template>
 
 <script>
