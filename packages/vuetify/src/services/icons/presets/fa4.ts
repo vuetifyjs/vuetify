26e8a25b711af5d158917736c5e77ebeb9798b44
Ben New <ben@leftclick.com.au>
2019-09-18 17:37:35 +0800

    Use correct icon for unchecked checkbox using fontawesome
    Fixes #8998

--- packages/vuetify/src/services/icons/presets/fa4.ts
+++ packages/vuetify/src/services/icons/presets/fa4.ts
@@ -13,7 +13,7 @@ const icons: VuetifyIcons = {
   prev: 'fa fa-chevron-left',
   next: 'fa fa-chevron-right',
   checkboxOn: 'fa fa-check-square',
-  checkboxOff: 'fa fa-square-o', // note 'far'
+  checkboxOff: 'far fa-square', // note 'far'
   checkboxIndeterminate: 'fa fa-minus-square',
   delimiter: 'fa fa-circle', // for carousel
   sort: 'fa fa-sort-up',
