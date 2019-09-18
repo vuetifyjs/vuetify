b705982dce056243085ea782472402d8f7e4f649
Bassel Ahmed <bassel.ahmmed@gmail.com>
2019-09-18 17:28:31 +0200

    refactor(fasvg): import icons from existing fa file

--- packages/vuetify/src/services/icons/presets/fa-svg.ts
+++ packages/vuetify/src/services/icons/presets/fa-svg.ts
@@ -1,39 +1,6 @@
 import { VuetifyIcon, VuetifyIcons } from 'vuetify/types/services/icons'
 import { Component } from 'vue'
-
-const faIcons: VuetifyIcons = {
-  complete: 'fas fa-check',
-  cancel: 'fas fa-times-circle',
-  close: 'fas fa-times',
-  delete: 'fas fa-times-circle', // delete (e.g. v-chip close)
-  clear: 'fas fa-times-circle', // delete (e.g. v-chip close)
-  success: 'fas fa-check-circle',
-  info: 'fas fa-info-circle',
-  warning: 'fas fa-exclamation',
-  error: 'fas fa-exclamation-triangle',
-  prev: 'fas fa-chevron-left',
-  next: 'fas fa-chevron-right',
-  checkboxOn: 'fas fa-check-square',
-  checkboxOff: 'far fa-square', // note 'far'
-  checkboxIndeterminate: 'fas fa-minus-square',
-  delimiter: 'fas fa-circle', // for carousel
-  sort: 'fas fa-sort-up',
-  expand: 'fas fa-chevron-down',
-  menu: 'fas fa-bars',
-  subgroup: 'fas fa-caret-down',
-  dropdown: 'fas fa-caret-down',
-  radioOn: 'far fa-dot-circle',
-  radioOff: 'far fa-circle',
-  edit: 'fas fa-edit',
-  ratingEmpty: 'far fa-star',
-  ratingFull: 'fas fa-star',
-  ratingHalf: 'fas fa-star-half',
-  loading: 'fas fa-sync',
-  first: 'fas fa-step-backward',
-  last: 'fas fa-step-forward',
-  unfold: 'fas fa-arrows-alt-v',
-  file: 'fas fa-paperclip',
-}
+import icons from './fa'
 
 export function convertToComponentDeclarations (
   component: Component | string,
@@ -53,4 +20,4 @@ export function convertToComponentDeclarations (
   return result as VuetifyIcons
 }
 
-export default convertToComponentDeclarations('font-awesome-icon', faIcons)
+export default convertToComponentDeclarations('font-awesome-icon', icons)
