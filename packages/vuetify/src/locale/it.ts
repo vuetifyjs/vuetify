b17ab6b7326ec4b5a8c1de27977fa62c12725a5c
rt <rtrapletti@orionedp.com>
2019-09-18 17:32:05 +0200

    fix(locale): update locale it.ts

--- packages/vuetify/src/locale/it.ts
+++ packages/vuetify/src/locale/it.ts
@@ -1,40 +1,42 @@
 export default {
-  close: 'Close',
+  close: "Chiudi",
   dataIterator: {
-    pageText: '{0}-{1} di {2}',
-    noResultsText: 'Nessun risultato trovato',
-    loadingText: 'Loading item...',
+    pageText: "{0}-{1} di {2}",
+    noResultsText: "Nessun risultato trovato",
+    loadingText: "Caricamento in corso..."
   },
   dataTable: {
-    itemsPerPageText: 'Righe per pagina:',
+    itemsPerPageText: "Righe per pagina:",
     ariaLabel: {
-      sortDescending: ': Sorted descending. Activate to remove sorting.',
-      sortAscending: ': Sorted ascending. Activate to sort descending.',
-      sortNone: ': Not sorted. Activate to sort ascending.',
+      sortDescending:
+        ": Ordinati in ordine decrescente. Attiva per rimuovere l'ordinamento.",
+      sortAscending:
+        ": Ordinati in ordine crescente. Attiva per ordinare in ordine decrescente.",
+      sortNone: ": Non ordinato. Attiva per ordinare in ordine crescente."
     },
-    sortBy: 'Ordina per',
+    sortBy: "Ordina per"
   },
   dataFooter: {
-    itemsPerPageText: 'Elementi per pagina:',
-    itemsPerPageAll: 'Tutti',
-    nextPage: 'Pagina seguente',
-    prevPage: 'Pagina precedente',
-    firstPage: 'Pagina prima',
-    lastPage: 'Pagina ultima',
+    itemsPerPageText: "Elementi per pagina:",
+    itemsPerPageAll: "Tutti",
+    nextPage: "Pagina seguente",
+    prevPage: "Pagina precedente",
+    firstPage: "Pagina prima",
+    lastPage: "Pagina ultima"
   },
   datePicker: {
-    itemsSelected: '{0} selezionati',
+    itemsSelected: "{0} selezionati"
   },
-  noDataText: 'Nessun elemento disponibile',
+  noDataText: "Nessun elemento disponibile",
   carousel: {
-    prev: 'Precedente visivo',
-    next: 'Next visual',
+    prev: "Vista precedente",
+    next: "Vista successiva"
   },
   calendar: {
-    moreEvents: '{0} di più',
+    moreEvents: "{0} di più"
   },
   fileInput: {
-    counter: '{0} files',
-    counterSize: '{0} files ({1} in total)',
-  },
-}
+    counter: "{0} files",
+    counterSize: "{0} files ({1} in totale)"
+  }
+};
