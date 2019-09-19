8baac016c9c6542a562a8c81be65c17265e0ead1
tensor2flow <tensor2flow@gmail.com>
2019-09-19 14:44:45 +0500

    translation to uzbek

---
+++ packages/vuetify/src/locale/uz-Latin.ts
@@ -0,0 +1,41 @@
+export default {
+    close: 'Yopish',
+    dataIterator: {
+      pageText: '{0}-{1} dan {2}',
+      noResultsText: 'Mos kelovchi natijalar topilmadi',
+      loadingText: 'Yozib olinmoqda...',
+    },
+    dataTable: {
+      itemsPerPageText: 'Bir saxifadagi satrlar:',
+      ariaLabel: {
+        sortDescending: ': Kamayish tartibida saralangan. Ko\'tarilish tartibda saralashni faollashtirish..',
+        sortAscending: ': Ko\'tarilish tartibida saralangan. Kamayish tartibda saralashni faollashtirish.',
+        sortNone: ': Saralanmagan. Ko\'tarilish tartibida saralashni faollashtirish.',
+      },
+      sortBy: 'Saralash',
+    },
+    dataFooter: {
+      itemsPerPageText: 'Bir saxifadagi yozuvlar:',
+      itemsPerPageAll: 'Barchasi',
+      nextPage: 'Keyingi saxifa',
+      prevPage: 'Oldingi saxifa',
+      firstPage: 'Birinchi saxifa',
+      lastPage: 'Oxirgi saxifi',
+    },
+    datePicker: {
+      itemsSelected: '{0} tanlandi',
+    },
+    noDataText: 'Ma\'lumotlar yo\'q',
+    carousel: {
+      prev: 'Oldingi tasvir',
+      next: 'Keyingi tasvir',
+    },
+    calendar: {
+      moreEvents: 'Yana {0}',
+    },
+    fileInput: {
+      counter: 'Fayllar: {0}',
+      counterSize: 'Fayllar: {0} (barchasi: {1})',
+    },
+  }
+  
