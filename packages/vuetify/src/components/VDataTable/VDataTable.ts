a476b985ceda80c61e8a12346afa62b1625a63f7
Albert Kaaman <albert@kaaman.nu>
2019-09-24 03:28:48 +0200

    [Bug] fix(VDataTable): show no-results when custom filter returns zero rows (#8284)
    * fix(VDataTable): show no-results when custom filter returns zero rows
    
    closes #8131
    
    * fix(VDataIterator): updated comparison logic

--- packages/vuetify/src/components/VDataTable/VDataTable.ts
+++ packages/vuetify/src/components/VDataTable/VDataTable.ts
@@ -288,7 +288,7 @@ export default VDataIterator.extend({
       ])
     },
     genItems (items: any[], props: DataProps) {
-      const empty = this.genEmpty(props.pagination.itemsLength)
+      const empty = this.genEmpty(props.originalItemsLength, props.pagination.itemsLength)
       if (empty) return [empty]
 
       return props.groupedItems
