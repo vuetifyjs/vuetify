e8ff63d3465061d188e703dabfec29b318fc88c1
John Leider <john.j.leider@gmail.com>
2019-09-30 19:37:04 -0400

    refactor: final pass

--- packages/api-generator/src/maps/v-mutate.js
+++ packages/api-generator/src/maps/v-mutate.js
@@ -7,6 +7,26 @@ module.exports = {
         type: 'boolean',
       },
       {
+        name: 'modifiers.attr',
+        default: true,
+        type: 'boolean',
+      },
+      {
+        name: 'modifiers.char',
+        default: true,
+        type: 'boolean',
+      },
+      {
+        name: 'modifiers.child',
+        default: true,
+        type: 'boolean',
+      },
+      {
+        name: 'modifiers.sub',
+        default: true,
+        type: 'boolean',
+      },
+      {
         name: 'value',
         default: 'undefined',
         type: 'function',
