db8cf9004cbbb1f9c8f0b170055acf4fca5ae658
John Leider <john.j.leider@gmail.com>
2019-09-30 14:44:19 -0400

    refactor(v-mutate): improve code-style

--- packages/vuetify/src/directives/mutate/index.ts
+++ packages/vuetify/src/directives/mutate/index.ts
@@ -6,52 +6,41 @@ interface MutateVNodeDirective extends VNodeDirective {
 
 function inserted (el: HTMLElement, binding: MutateVNodeDirective) {
   const modifiers = binding.modifiers || /* istanbul ignore next */ {}
-  const value = binding.value!
-  let callback = (mutations: MutationRecord[]) => {}
-  let options = {
-    attributes: modifiers.attr || false,
-    childList: modifiers.child || false,
-    subtree: modifiers.sub || false,
-    characterData: modifiers.char || false,
-  }
+  const value = binding.value
+  const isObject = typeof value === 'object'
+  const callback = isObject ? value.handler : value
+  const { once, ...modifierKeys } = modifiers
+  const hasModifiers = Object.keys(modifierKeys).length > 0
+  const hasOptions = isObject && value.object
 
-  if (Object.keys(modifiers).filter(x => (
-    x === 'attr' ||
-    x === 'child' ||
-    x === 'sub' ||
-    x === 'char'
-  )).length === 0) { // There are no option modifiers
-    options = {
+  // Options take top priority
+  const options = hasOptions ? value.options : hasModifiers
+    // If we have modifiers, use only those provided
+    ? {
+      attributes: modifierKeys.attr,
+      childList: modifierKeys.child,
+      subtree: modifierKeys.sub,
+      characterData: modifierKeys.char,
+    }
+    // Defaults to everything on
+    : {
       attributes: true,
       childList: true,
       subtree: true,
       characterData: true,
     }
-  }
 
-  if (typeof value === 'object') {
-    callback = value.handler
-    options = {
-      ...options,
-      ...value.options,
-    }
-  } else {
-    callback = value
-  }
-
-  const callbackWrapper = (mutations: MutationRecord[]) => {
+  const observer = new MutationObserver((mutations: MutationRecord[]) => {
     /* istanbul ignore if */
     if (!el._mutate) return // Just in case, should never fire
 
     callback(mutations)
 
     // If has the once modifier, unbind
-    if (modifiers.once) unbind(el)
-  }
+    once && unbind(el)
+  })
 
-  const observer = new MutationObserver(callbackWrapper)
   observer.observe(el, options)
-
   el._mutate = { observer }
 }
 
