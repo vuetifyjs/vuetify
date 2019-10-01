1e62a4e89e75cee349408a27b8818902ebe64605
Mohamed Elbahja <8259014+melbahja@users.noreply.github.com>
2019-10-01 17:44:46 +0100

    fix(VTextField): show label on dense (#9116)
    fixes #9105 
    
    * remove default single line
    
    * fix label on dense
    
    * add dense margin bottom for v-input__slot
    
    * fix dense rules
    
    * recover non dense mt

--- packages/vuetify/src/components/VTextField/VTextField.ts
+++ packages/vuetify/src/components/VTextField/VTextField.ts
@@ -136,7 +136,7 @@ export default baseMixins.extend<options>().extend({
       return this.isDirty || dirtyTypes.includes(this.type)
     },
     isSingle (): boolean {
-      return this.isSolo || this.singleLine || this.fullWidth || (this.dense && !this.outlined)
+      return this.isSolo || this.singleLine || this.fullWidth
     },
     isSolo (): boolean {
       return this.solo || this.soloInverted
