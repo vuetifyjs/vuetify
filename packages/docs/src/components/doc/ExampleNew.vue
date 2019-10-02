91658ce9e7d3c9452948b7fe1b6bf0c527be31e5
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-10-02 11:30:29 -0400

    docs(ExampleNew): add theme toggle

--- packages/docs/src/components/doc/ExampleNew.vue
+++ packages/docs/src/components/doc/ExampleNew.vue
@@ -29,6 +29,7 @@
           <v-sheet
             height="300"
             tile
+            :dark="dark"
           >
             <div class="fill-height pa-6" data-app="true">
               <component :is="component" :attrs="attrs$" />
@@ -43,13 +44,28 @@
       </v-col>
 
       <v-col cols="12" md="3">
-        <v-responsive
-          class="title font-weight-regular d-flex align-center px-3 grey lighten-3"
-          height="48"
-        >
-          Options
-        </v-responsive>
-
+        <div class="d-flex grey lighten-3">
+          <v-responsive
+            class="title font-weight-regular align-center px-3"
+            height="48"
+          >
+            Options
+          </v-responsive>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                aria-label="Invert playground colors"
+                class="flex-shrink-1 mx-2 mt-2"
+                icon
+                @click="dark = !dark"
+                v-on="on"
+              >
+                <v-icon>mdi-invert-colors</v-icon>
+              </v-btn>
+            </template>
+            Invert playground colors
+          </v-tooltip>
+        </div>
         <v-divider />
 
         <v-responsive max-height="300" class="overflow-y-auto py-3">
@@ -161,6 +177,7 @@
       return {
         booleans: setupData(this.value.booleans),
         component: null,
+        dark: false,
         selects: setupData(this.value.selects),
         sliders: setupData(this.value.sliders),
         tab: 0,
