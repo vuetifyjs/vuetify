079904031b93b17e7c6bcd2a14405bc2bf4e91a4
John Leider <john.j.leider@gmail.com>
2019-10-10 17:10:48 -0400

    fix(VCard): resolved clashing border-radius, adjust to match md2 spec
    created new component to handle md2 subtitle, updated all examples, added usage playground

--- packages/docs/src/examples/cards/complex/custom-actions.vue
+++ packages/docs/src/examples/cards/complex/custom-actions.vue
@@ -9,21 +9,24 @@
     ></v-img>
 
     <v-card-title>
-      <div>Top western road trips</div>
-      <span class="grey--text subtitle-1">1,000 miles of wonder</span>
+      Top western road trips
     </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
 
     <v-card-actions>
       <v-btn text>Share</v-btn>
 
       <v-btn
-        text
         color="purple"
+        text
       >
         Explore
       </v-btn>
 
-      <div class="flex-grow-1"></div>
+      <v-spacer></v-spacer>
 
       <v-btn
         icon
@@ -35,6 +38,8 @@
 
     <v-expand-transition>
       <div v-show="show">
+        <v-divider></v-divider>
+
         <v-card-text>
           I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
         </v-card-text>
--- packages/docs/src/examples/cards/intermediate/media-with-text.vue
+++ packages/docs/src/examples/cards/intermediate/media-with-text.vue
@@ -4,31 +4,32 @@
     max-width="400"
   >
     <v-img
-      class="white--text"
+      class="white--text align-end"
       height="200px"
       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
     >
-      <v-card-title class="align-end fill-height">Top 10 Australian beaches</v-card-title>
+      <v-card-title>Top 10 Australian beaches</v-card-title>
     </v-img>
 
-    <v-card-text>
-      <span>Number 10</span><br>
-      <span class="text--primary">
-        <span>Whitehaven Beach</span><br>
-        <span>Whitsunday Island, Whitsunday Islands</span>
-      </span>
+    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
     </v-card-text>
 
     <v-card-actions>
       <v-btn
-        text
         color="orange"
+        text
       >
         Share
       </v-btn>
+
       <v-btn
-        text
         color="orange"
+        text
       >
         Explore
       </v-btn>
