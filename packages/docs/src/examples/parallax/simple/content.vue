e13ad1a7dc09449326664e68c1138a49e1acfb14
Vinícius Rufine <vsrufine@gmail.com>
2019-10-18 11:30:11 -0300

    Update content.vue

--- packages/docs/src/examples/parallax/simple/content.vue
+++ packages/docs/src/examples/parallax/simple/content.vue
@@ -7,7 +7,7 @@
       align="center"
       justify="center"
     >
-      <v-col class="text-center" xs="12">
+      <v-col class="text-center" cols="12">
         <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
         <h4 class="subheading">Build your application today!</h4>
       </v-col>
