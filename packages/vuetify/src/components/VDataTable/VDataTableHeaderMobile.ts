665ebe34473606be487b7d55ec0ccaf0fc856fed
Tim van den Eijnden <TimvdEijnden@users.noreply.github.com>
2019-10-31 03:32:09 +0100

    feat(VDataTable): hide sort by select on mobile if not needed (#9215)
    * feature(datatable): hide sort by select on mobile if not needed
    
    resolves #9209
    
    * feature(datatable): fix eslint error

--- packages/vuetify/src/components/VDataTable/VDataTableHeaderMobile.ts
+++ packages/vuetify/src/components/VDataTable/VDataTableHeaderMobile.ts
@@ -2,7 +2,7 @@ import { VNode, VNodeChildrenArrayContents } from 'vue'
 import mixins from '../../util/mixins'
 import VSelect from '../VSelect/VSelect'
 import VChip from '../VChip'
-import header from './mixins/header'
+import header, { TableHeader } from './mixins/header'
 import { wrapInArray } from '../../util/helpers'
 
 export default mixins(header).extend({
@@ -43,17 +43,14 @@ export default mixins(header).extend({
         },
       }, children)
     },
-    genSortSelect () {
-      const sortHeaders = this.headers.filter(h => h.sortable !== false && h.value !== 'data-table-select')
-
+    genSortSelect (items: any[]) {
       return this.$createElement(VSelect, {
         props: {
           label: this.$vuetify.lang.t(this.sortByText),
-          items: sortHeaders,
+          items,
           hideDetails: true,
           multiple: this.options.multiSort,
           value: this.options.multiSort ? this.options.sortBy : this.options.sortBy[0],
-          disabled: sortHeaders.length === 0 || this.disableSort,
         },
         on: {
           change: (v: string | string[]) => this.$emit('sort', v),
@@ -81,7 +78,10 @@ export default mixins(header).extend({
       }, [this.genSelectAll()]))
     }
 
-    children.push(this.genSortSelect())
+    const sortHeaders: TableHeader[] = this.headers.filter(h => h.sortable !== false && h.value !== 'data-table-select')
+    if (!this.disableSort && sortHeaders.length) {
+      children.push(this.genSortSelect(sortHeaders))
+    }
 
     const th = h('th', [h('div', { staticClass: 'v-data-table-header-mobile__wrapper' }, children)])
 
