7ae69a5867c058a8075129b97dff708b00efecfd
John Leider <john.j.leider@gmail.com>
2019-10-31 10:03:16 -0500

    chore: modify release scripts to target stable tag

--- scripts/parse-npm-tag.js
+++ scripts/parse-npm-tag.js
@@ -6,11 +6,3 @@ if (!semver.valid(version)) {
   console.error(`Error: '${version}' is not a valid NPM version string`)
   process.exit(9)
 }
-
-const prerelease = semver.prerelease(version)
-
-if (prerelease == null) {
-  console.log('latest')
-} else {
-  console.log('next')
-}
