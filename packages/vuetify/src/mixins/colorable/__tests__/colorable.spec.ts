c72bccf2074dfb0e3d25178e7b622874c253a50c
simonmclean <simonmclean1988@gmail.com>
2019-11-09 15:25:20 +0000

    improvement(colorable): add support for CSS custom props in colorable
    The "isCssColor" function now also matches on "var(--" in order to allow for the use of CSS custom
    properties

--- packages/vuetify/src/mixins/colorable/__tests__/colorable.spec.ts
+++ packages/vuetify/src/mixins/colorable/__tests__/colorable.spec.ts
@@ -82,4 +82,26 @@ describe('colorable.ts', () => {
       },
     })
   })
+
+  it('should compute data with text from a css custom property', async () => {
+    const wrapper = mountFunction()
+
+    expect(wrapper.vm.setTextColor('var(--foo-color)', {})).toEqual({
+      style: {
+        color: 'var(--foo-color)',
+        'caret-color': 'var(--foo-color)',
+      },
+    })
+  })
+
+  it('should compute data with background from a css custom property', async () => {
+    const wrapper = mountFunction()
+
+    expect(wrapper.vm.setBackgroundColor('var(--foo-color)', {})).toEqual({
+      style: {
+        'background-color': 'var(--foo-color)',
+        'border-color': 'var(--foo-color)',
+      },
+    })
+  })
 })
