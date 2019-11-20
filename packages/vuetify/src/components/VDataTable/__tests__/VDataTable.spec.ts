cbcd8e4dbde89b5252533c0b009500a37d58673c
Albert Kaaman <albert@kaaman.nu>
2019-11-20 20:55:33 +0100

    fix(VDataTable): items-per-page event should not emit immediately (#9615)
    closes #9599

--- packages/vuetify/src/components/VDataTable/__tests__/VDataTable.spec.ts
+++ packages/vuetify/src/components/VDataTable/__tests__/VDataTable.spec.ts
@@ -509,45 +509,51 @@ describe('VDataTable.ts', () => {
 
   // https://github.com/vuetifyjs/vuetify/issues/8184
   it('should default to first option in itemsPerPageOptions if it does not include itemsPerPage', async () => {
-    const itemsPerPage = jest.fn()
-    const options = jest.fn()
     const wrapper = mountFunction({
       propsData: {
         headers: testHeaders,
         items: testItems,
         footerProps: {
-          itemsPerPageOptions: [5, 6],
+          itemsPerPageOptions: [6, 7],
         },
-      },
-      listeners: {
-        'update:items-per-page': itemsPerPage,
-        'update:options': options,
       },
     })
 
-    expect(itemsPerPage).toHaveBeenCalledWith(5)
-    expect(options).toHaveBeenCalledWith(expect.objectContaining({
-      itemsPerPage: 5,
-    }))
+    expect(wrapper.html()).toMatchSnapshot()
   })
 
   // https://github.com/vuetifyjs/vuetify/issues/8817
   it('should handle object when checking if it should default to first option in itemsPerPageOptions', async () => {
-    const itemsPerPage = jest.fn()
     const wrapper = mountFunction({
       propsData: {
         headers: testHeaders,
         items: testItems,
         itemsPerPage: -1,
         footerProps: {
-          itemsPerPageOptions: [5, 6, { text: 'All', value: -1 }],
+          itemsPerPageOptions: [6, { text: 'All', value: -1 }],
         },
-      },
-      listeners: {
-        'update:items-per-page': itemsPerPage,
       },
     })
 
-    expect(itemsPerPage).toHaveBeenCalledWith(-1)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  // https://github.com/vuetifyjs/vuetify/issues/9599
+  it('should not immediately emit items-per-page', async () => {
+    const itemsPerPage = jest.fn()
+    const wrapper = mountFunction({
+      propsData: {
+        headers: testHeaders,
+        items: testItems,
+        footerProps: {
+          itemsPerPageOptions: [6, 7],
+        },
+      },
+      listeners: {
+        'update:itemsPerPage': itemsPerPage,
+      },
+    })
+
+    expect(itemsPerPage).not.toHaveBeenCalled()
   })
 })
