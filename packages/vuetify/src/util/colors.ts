77b464e5c084a23b9e398fd4a65b2f7178056790
Weijing Lin <weijing.lin@reeftechnology.com>
2019-11-27 11:22:50 -0800

    add material variable for red color

--- packages/vuetify/src/util/colors.ts
+++ packages/vuetify/src/util/colors.ts
@@ -13,6 +13,20 @@ const red = Object.freeze({
   accent2: '#ff5252',
   accent3: '#ff1744',
   accent4: '#d50000',
+  50: '#ffebee',
+  100: '#ffcdd2',
+  200: '#ef9a9a',
+  300: '#e57373',
+  400: '#ef5350',
+  500: '#F44336',
+  600: '#e53935',
+  700: '#d32f2f',
+  800: '#c62828',
+  900: '#b71c1c',
+  A100: '#ff8a80',
+  A200: '#ff5252',
+  A400: '#ff1744',
+  A700: '#d50000',
 })
 
 const pink = Object.freeze({
