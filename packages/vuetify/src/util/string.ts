2b859277cb264464f9968f9ee207acf1a7622b21
Sebastian Niemann <SebastianNiemann@users.noreply.github.com>
2019-11-29 03:09:50 +0100

    Delete string.ts

--- packages/vuetify/src/util/string.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Camelize a hyphen-delimited string.
- */
-const camelizeRE = /-(\w)/g
-export const camelize = (str: string): string => {
-  return str.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '')
-}
