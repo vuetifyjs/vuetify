b0a7c300009ee07c2f93b53bf7e7a92b83026037
Vojtěch Srdečný <srdecny@users.noreply.github.com>
2019-11-30 21:39:24 +0100

    feat(VSelect): add prop for disabling keyboard lookup (#9297)
    closes #9271
    
    Currently, there's no way to disable keyboard lookup when using VSelect.

--- packages/vuetify/src/components/VSelect/VSelect.ts
+++ packages/vuetify/src/components/VSelect/VSelect.ts
@@ -73,6 +73,7 @@ export default baseMixins.extend<options>().extend({
     chips: Boolean,
     clearable: Boolean,
     deletableChips: Boolean,
+    disableLookup: Boolean,
     eager: Boolean,
     hideSelected: Boolean,
     items: {
@@ -570,7 +571,8 @@ export default baseMixins.extend<options>().extend({
     onKeyPress (e: KeyboardEvent) {
       if (
         this.multiple ||
-        this.readonly
+        this.readonly ||
+        this.disableLookup
       ) return
 
       const KEYBOARD_LOOKUP_THRESHOLD = 1000 // milliseconds
