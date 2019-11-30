9c389fb3d44fe6013748d0cd313d83035fba162c
John Leider <john.j.leider@gmail.com>
2019-11-29 21:00:02 -0500

    refactor(VThemeProvider): add unit tests, clean-up theme page

--- packages/vuetify/src/components/VThemeProvider/VThemeProvider.ts
+++ packages/vuetify/src/components/VThemeProvider/VThemeProvider.ts
@@ -19,6 +19,7 @@ export default Themeable.extend({
   },
 
   render (): VNode {
+    /* istanbul ignore next */
     return (
       this.$slots.default! &&
       this.$slots.default!.find(node => !node.isComment && node.text !== ' ')!
