39face8abd9124071b98557298c0e64b60cd3291
Andrew Henry <AMajesticPotatoe@gmail.com>
2019-12-10 19:34:00 -0500

    docs(SASS): add variable api (#9780)
    * docs(api): add sass var export to api-gen
    
    * docs(sass): add basic variable api section
    
    * docs(SASS): reorder and fix headers/desc
    
    * docs(SASS):add globals and default them on mount:
    
    * docs(api): update sass var parser
    
    * style: tweak global var formatting for parser
    
    * style: remove arbitrary comments
    
    * docs: general code cleanup
    
    * docs(API): add sass var tab
    
    * docs(SASS): cleanup api section code
    
    * docs(SASS): fix variable api description

---
+++ packages/docs/src/components/doc/VariableApi.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <doc-heading>Generic.Pages.variableApi</doc-heading>
+
+    <doc-text>Customization.SassVariables.variableApiDesc</doc-text>
+
+    <v-card outlined>
+      <v-toolbar
+        class="px-2 py-3"
+        color="primary"
+        dark
+        flat
+        height="auto"
+      >
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-autocomplete
+              v-model="selectedComponent"
+              :class="$vuetify.breakpoint.mdAndUp ? '' : 'mb-6'"
+              :items="components"
+              :menu-props="{offsetY: true, contentClass: 'primary'}"
+              label="Available Component(s)"
+              hide-details
+              outlined
+              prepend-inner-icon="mdi-view-dashboard"
+            />
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+            offset-md="4"
+          >
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              clearable
+              hide-details
+              label="Search..."
+              outlined
+              single-line
+              type="search"
+            />
+          </v-col>
+        </v-row>
+      </v-toolbar>
+
+      <v-data-iterator
+        :items="variables"
+        :items-per-page="-1"
+        :search="search"
+        class="component-parameters"
+        hide-default-footer
+        sort-by="name"
+      >
+        <template v-slot="{ items }">
+          <div>
+            <template v-for="(item, i) in items">
+              <doc-api-item
+                :key="item.name"
+                :headers="headers"
+                :item="item"
+              />
+
+              <v-divider
+                v-if="i + 1!== items.length"
+                :key="`divider-${i}`"
+              />
+            </template>
+          </div>
+        </template>
+      </v-data-iterator>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  // Utilities
+  import variableApi from '@vuetify/api-generator/dist/variables'
+
+  export default {
+    name: 'DocVariableApi',
+
+    props: { value: String },
+
+    data: () => ({
+      components: [],
+      search: '',
+      selectedComponent: '',
+      variables: [],
+      headers: [
+        {
+          value: 'name',
+          class: 'xs12',
+        },
+        {
+          value: 'default',
+          type: 'sass',
+          class: 'xs12 mt-2',
+        },
+      ],
+    }),
+
+    watch: {
+      selectedComponent (component) {
+        this.variables = (variableApi && variableApi[component]) || []
+      },
+    },
+
+    mounted () {
+      this.components = Object.keys(variableApi) || []
+      this.selectedComponent = 'globals'
+    },
+  }
+</script>
