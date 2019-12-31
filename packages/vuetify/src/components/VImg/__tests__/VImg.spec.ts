vuetifyjs/vuetify

commit e92d6b79115c28b6cae71b7bc70bbb295c12ef89

fix(VResponsive): restructure elements to work with Firefox (#10027)
* fix(VResponsive): refactor VResponsive to work with Firefox

fixes #9659

* fix: use sizer element as a wrapper

Co-authored-by: Kael <kaelwd@gmail.com>

packages/vuetify/src/components/VImg/VImg.ts
@@ -127,6 +127,14 @@ export default VResponsive.extend({
         },
       }, [image])
     },
+    contentAspectStyle (): object | undefined {
+      const styles = VResponsive.options.computed.contentAspectStyle.call(this)
+
+      return {
+        ...styles,
+        ...(this.naturalWidth && { width: `${this.naturalWidth}px` }),
+      }
+    },
   },
 
   watch: {
@@ -224,16 +232,6 @@ export default VResponsive.extend({
 
       poll()
     },
-    genContent () {
-      const content: VNode = VResponsive.options.methods.genContent.call(this)
-      if (this.naturalWidth) {
-        this._b(content.data!, 'div', {
-          style: { width: `${this.naturalWidth}px` },
-        })
-      }
-
-      return content
-    },
     __genPlaceholder (): VNode | void {
       if (this.$slots.placeholder) {
         const placeholder = this.isLoading
@@ -274,7 +272,6 @@ export default VResponsive.extend({
     }
 
     node.children = [
-      this.__cachedSizer,
       this.__cachedImage,
       this.__genPlaceholder(),
       this.genContent(),

packages/vuetify/src/components/VAppBar/__tests__/__snapshots__/VAppBar.spec.ts.snap
@@ -14,7 +14,9 @@ exports[`AppBar.ts should render with background 1`] = `
            mode="in-out"
       >
       </div>
-      <div class="v-responsive__content">
+      <div class="v-responsive__sizer">
+        <div class="v-responsive__content">
+        </div>
       </div>
     </div>
   </div>

packages/vuetify/src/components/VImg/__tests__/__snapshots__/VImg.spec.ts.snap
@@ -13,7 +13,9 @@ exports[`VImg.ts should display placeholders 1`] = `
       loading...
     </div>
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
@@ -31,7 +33,9 @@ exports[`VImg.ts should display placeholders 2`] = `
       loading...
     </div>
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
@@ -47,7 +51,9 @@ exports[`VImg.ts should have aria attributes 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
@@ -60,7 +66,9 @@ exports[`VImg.ts should load 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
@@ -73,24 +81,28 @@ exports[`VImg.ts should load 2`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
 
 exports[`VImg.ts should override vuetify-loader values 1`] = `
 <div class="v-responsive v-image">
-  <div class="v-responsive__sizer"
-       style="padding-bottom: 50%;"
-  >
-  </div>
   <div class="v-image__image v-image__image--preload v-image__image--cover"
        style="background-image: url(lazySrc_manual); background-position: center center;"
        name="fade-transition"
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer"
+       style="padding-top: 50%;"
+  >
+    <div class="v-responsive__content"
+         style="margin-top: -50%;"
+    >
+    </div>
   </div>
 </div>
 `;
@@ -103,26 +115,28 @@ exports[`VImg.ts should update src 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
 
 exports[`VImg.ts should update src 2`] = `
 <div class="v-responsive v-image">
-  <div class="v-responsive__sizer"
-       style="padding-bottom: 56.25%;"
-  >
-  </div>
   <div class="v-image__image v-image__image--cover"
        style="background-image: url(LOAD_SUCCESS_SRC1); background-position: center center;"
        name="fade-transition"
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content"
-       style="width: 1600px;"
+  <div class="v-responsive__sizer"
+       style="padding-top: 56.25%;"
   >
+    <div class="v-responsive__content"
+         style="width: 1600px; margin-top: -56.25%;"
+    >
+    </div>
   </div>
 </div>
 `;
@@ -135,43 +149,47 @@ exports[`VImg.ts should update src while still loading 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;
 
 exports[`VImg.ts should update src while still loading 2`] = `
 <div class="v-responsive v-image">
-  <div class="v-responsive__sizer"
-       style="padding-bottom: 56.25%;"
-  >
-  </div>
   <div class="v-image__image v-image__image--cover"
        style="background-image: url(LOAD_SUCCESS_SRC1); background-position: center center;"
        name="fade-transition"
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content"
-       style="width: 1600px;"
+  <div class="v-responsive__sizer"
+       style="padding-top: 56.25%;"
   >
+    <div class="v-responsive__content"
+         style="width: 1600px; margin-top: -56.25%;"
+    >
+    </div>
   </div>
 </div>
 `;
 
 exports[`VImg.ts should use vuetify-loader data 1`] = `
 <div class="v-responsive v-image">
-  <div class="v-responsive__sizer"
-       style="padding-bottom: 100%;"
-  >
-  </div>
   <div class="v-image__image v-image__image--preload v-image__image--cover"
        style="background-image: url(lazySrc_auto); background-position: center center;"
        name="fade-transition"
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer"
+       style="padding-top: 100%;"
+  >
+    <div class="v-responsive__content"
+         style="margin-top: -100%;"
+    >
+    </div>
   </div>
 </div>
 `;

packages/vuetify/src/components/VResponsive/VResponsive.sass
@@ -7,10 +7,14 @@
   max-width: 100%
   display: flex
 
+  &__sizer
+    display: flex
+    transition: padding-top 0.2s map-get($transition, 'swing')
+
+  &__content
+    transition: margin-top 0.2s map-get($transition, 'swing')
+
+  &__sizer,
   &__content
     flex: 1 0 0px
     max-width: 100%
-
-  &__sizer
-    transition: padding-bottom 0.2s map-get($transition, 'swing')
-    flex: 0 0 0px

packages/vuetify/src/components/VResponsive/VResponsive.ts
@@ -21,26 +21,29 @@ export default mixins(Measurable).extend({
     computedAspectRatio (): number {
       return Number(this.aspectRatio)
     },
-    aspectStyle (): object | undefined {
+    sizerAspectStyle (): object | undefined {
       return this.computedAspectRatio
-        ? { paddingBottom: (1 / this.computedAspectRatio) * 100 + '%' }
+        ? { paddingTop: (1 / this.computedAspectRatio) * 100 + '%' }
         : undefined
     },
-    __cachedSizer (): VNode | [] {
-      if (!this.aspectStyle) return []
-
-      return this.$createElement('div', {
-        style: this.aspectStyle,
-        staticClass: 'v-responsive__sizer',
-      })
+    contentAspectStyle (): object | undefined {
+      return this.computedAspectRatio
+        ? { marginTop: -1 * (1 / this.computedAspectRatio) * 100 + '%' }
+        : undefined
     },
   },
 
   methods: {
     genContent (): VNode {
       return this.$createElement('div', {
-        staticClass: 'v-responsive__content',
-      }, this.$slots.default)
+        staticClass: 'v-responsive__sizer',
+        style: this.sizerAspectStyle,
+      }, [
+        this.$createElement('div', {
+          staticClass: 'v-responsive__content',
+          style: this.contentAspectStyle,
+        }, this.$slots.default),
+      ])
     },
   },
 
@@ -50,7 +53,6 @@ export default mixins(Measurable).extend({
       style: this.measurableStyles,
       on: this.$listeners,
     }, [
-      this.__cachedSizer,
       this.genContent(),
     ])
   },

packages/vuetify/src/components/VResponsive/__tests__/__snapshots__/VResponsive.spec.ts.snap
@@ -3,19 +3,23 @@
 exports[`VResponsive.ts should force aspect ratio 1`] = `
 <div class="v-responsive">
   <div class="v-responsive__sizer"
-       style="padding-bottom: 56.25%;"
+       style="padding-top: 56.25%;"
   >
-  </div>
-  <div class="v-responsive__content">
+    <div class="v-responsive__content"
+         style="margin-top: -56.25%;"
+    >
+    </div>
   </div>
 </div>
 `;
 
 exports[`VResponsive.ts should render content 1`] = `
 <div class="v-responsive">
-  <div class="v-responsive__content">
-    <div>
-      content
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+      <div>
+        content
+      </div>
     </div>
   </div>
 </div>
@@ -25,7 +29,9 @@ exports[`VResponsive.ts should set height 1`] = `
 <div class="v-responsive"
      style="height: 100px; max-height: 200px;"
 >
-  <div class="v-responsive__content">
+  <div class="v-responsive__sizer">
+    <div class="v-responsive__content">
+    </div>
   </div>
 </div>
 `;

commit 155086241f575f02fbcf3c3c0b9417cd4cf12074

fix(VImg): image entry animation
changes made in e92d6b7 caused the padding of v-responsive to animate when an image loaded

packages/vuetify/src/components/VImg/VImg.ts
@@ -188,6 +188,11 @@ export default VResponsive.extend({
       )
       this.$emit('error', this.src)
     },
+    genContent (): VNode | undefined {
+      return this.computedAspectRatio != null
+        ? VResponsive.options.methods.genContent.call(this)
+        : undefined
+    },
     getSrc () {
       /* istanbul ignore else */
       if (this.image) this.currentSrc = this.image.currentSrc || this.image.src

packages/vuetify/src/components/VAppBar/__tests__/__snapshots__/VAppBar.spec.ts.snap
@@ -14,10 +14,6 @@ exports[`AppBar.ts should render with background 1`] = `
            mode="in-out"
       >
       </div>
-      <div class="v-responsive__sizer">
-        <div class="v-responsive__content">
-        </div>
-      </div>
     </div>
   </div>
   <div class="v-toolbar__content"

packages/vuetify/src/components/VImg/__tests__/VImg.spec.ts
@@ -190,4 +190,18 @@ describe('VImg.ts', () => {
 
     expect(wrapper.html()).toMatchSnapshot()
   })
+
+  // Changes made in https://github.com/vuetifyjs/vuetify/pull/10027
+  // caused the container padding to animate when an image loaded.
+  it('should not render content until computedAspectRatio is defined', () => {
+    const wrapper = mountFunction()
+
+    expect(wrapper.vm.computedAspectRatio).toBeUndefined()
+    expect(wrapper.vm.genContent()).toBeUndefined()
+
+    wrapper.setData({ calculatedAspectRatio: 1 })
+
+    expect(wrapper.vm.computedAspectRatio).toBeDefined()
+    expect(wrapper.vm.genContent()).toBeDefined()
+  })
 })

packages/vuetify/src/components/VImg/__tests__/__snapshots__/VImg.spec.ts.snap
@@ -13,10 +13,6 @@ exports[`VImg.ts should display placeholders 1`] = `
       loading...
     </div>
   </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
-  </div>
 </div>
 `;
 
@@ -33,10 +29,6 @@ exports[`VImg.ts should display placeholders 2`] = `
       loading...
     </div>
   </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
-  </div>
 </div>
 `;
 
@@ -51,10 +43,6 @@ exports[`VImg.ts should have aria attributes 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
-  </div>
 </div>
 `;
 
@@ -66,10 +54,6 @@ exports[`VImg.ts should load 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
-  </div>
 </div>
 `;
 
@@ -80,10 +64,6 @@ exports[`VImg.ts should load 2`] = `
        name="fade-transition"
        mode="in-out"
   >
-  </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
   </div>
 </div>
 `;
@@ -115,10 +95,6 @@ exports[`VImg.ts should update src 1`] = `
        mode="in-out"
   >
   </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
-  </div>
 </div>
 `;
 
@@ -134,7 +110,7 @@ exports[`VImg.ts should update src 2`] = `
        style="padding-top: 56.25%;"
   >
     <div class="v-responsive__content"
-         style="width: 1600px; margin-top: -56.25%;"
+         style="margin-top: -56.25%; width: 1600px;"
     >
     </div>
   </div>
@@ -148,10 +124,6 @@ exports[`VImg.ts should update src while still loading 1`] = `
        name="fade-transition"
        mode="in-out"
   >
-  </div>
-  <div class="v-responsive__sizer">
-    <div class="v-responsive__content">
-    </div>
   </div>
 </div>
 `;
@@ -168,7 +140,7 @@ exports[`VImg.ts should update src while still loading 2`] = `
        style="padding-top: 56.25%;"
   >
     <div class="v-responsive__content"
-         style="width: 1600px; margin-top: -56.25%;"
+         style="margin-top: -56.25%; width: 1600px;"
     >
     </div>
   </div>