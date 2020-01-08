64848655527fc61987bbdb635f73f7c4f44d5f0b
John Leider <john.j.leider@gmail.com>
2020-01-08 13:05:52 -0500

    style(install): refactor code styling

--- packages/vuetify/src/install.ts
+++ packages/vuetify/src/install.ts
@@ -1,31 +1,41 @@
 import { App } from 'vue'
-import { VuetifyUseOptions } from 'types'
 import Vuetify, { VuetifySymbol } from './framework'
+import { ComponentOrPack, VuetifyUseOptions } from 'types'
 
 export function install (app: App, args: VuetifyUseOptions = {}) {
   const components = args.components || {}
   const directives = args.directives || {}
 
-  for (const name in directives) {
-    const directive = directives[name]
+  for (const key in directives) {
+    const directive = directives[key]
 
-    app.directive(name, directive)
+    app.directive(key, directive)
   }
 
-  (function registerComponents (components: any) {
-    if (components) {
-      for (const key in components) {
-        const component = components[key]
-        if (component && !registerComponents(component.$_vuetify_subcomponents)) {
-          app.component(key, component)
-        }
+  (function registerComponents (components: Dictionary<ComponentOrPack>) {
+    if (!components) return false
+
+    for (const key in components) {
+      const component = components[key]
+
+      // Check for and register subcomponents
+      if (
+        component &&
+        component.$_vuetify_subcomponents
+      ) {
+        registerComponents(component.$_vuetify_subcomponents)
+
+        continue
       }
-      return true
+
+      app.component(key, component)
     }
-    return false
+
+    return true
   })(components)
 
   const vuetify = new Vuetify(args)
+
   vuetify.init()
 
   app.provide(VuetifySymbol, vuetify.framework)
