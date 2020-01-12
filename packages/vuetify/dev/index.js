174ffaef1d322e588e2fcd97f20530734f4568ab
John Leider <john.j.leider@gmail.com>
2020-01-11 21:00:15 -0500

    chore: clean-up dev index

--- packages/vuetify/dev/index.js
+++ packages/vuetify/dev/index.js
@@ -1,10 +1,12 @@
 import '@mdi/font/css/materialdesignicons.css'
+
 import App from './App'
-import { createApp } from 'vue'
 import Vuetify from 'vuetify'
+import { createApp } from 'vue'
 
 const app = createApp()
+const options = {}
 
-app.use(Vuetify)
+app.use(Vuetify, options)
 
 app.mount(App, '#app')
