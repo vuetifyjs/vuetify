29b6d6f4ceb457e5451044f499fad76124fd24c9
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2020-01-15 15:01:00 -0500

    docs: fix group change events
    resolves #10227

--- packages/api-generator/src/maps/v-radio-group.js
+++ packages/api-generator/src/maps/v-radio-group.js
@@ -1,5 +1,11 @@
 const { VInput } = require('../helpers/variables')
 
+const ClickIndex = VInput.events.findIndex(e => e.name === 'click')
+VInput.events.splice(ClickIndex, 1, {
+  name: 'change',
+  value: 'any',
+})
+
 module.exports = {
   'v-radio-group': VInput,
 }
