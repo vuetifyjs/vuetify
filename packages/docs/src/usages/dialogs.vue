52632fbe90a744a1b8f65e5cf1ecf0a4af9e0ae6
Yiping <Yiping.Ruan@outlook.com>
2020-01-17 11:23:25 +0800

    Dialogs booleans

--- packages/docs/src/usages/dialogs.vue
+++ packages/docs/src/usages/dialogs.vue
@@ -1,11 +1,57 @@
 <template>
-  <v-dialog v-bind="attrs" />
-</template>
+  <v-row justify="center">
+    <v-btn
+      color="primary"
+      dark
+      @click.stop="dialog = true"
+    >
+      Open Dialog
+    </v-btn>
 
+    <v-dialog
+      v-model="dialog"
+      max-width="290"
+      v-bind="attrs"
+    >
+      <v-card>
+        <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+        <v-card-text>
+          Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Disagree
+          </v-btn>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
 <script>
   import Usage from './usage'
 
   export default {
     mixins: [Usage],
+    data () {
+      return {
+        dialog: false,
+      }
+    },
   }
 </script>
