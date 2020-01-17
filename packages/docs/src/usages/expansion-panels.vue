fd6561e6db7abfdf7c0ba458df736ed7ec162cb3
Yiping <Yiping.Ruan@outlook.com>
2020-01-17 14:16:48 +0800

    ExpansionPanels booleans

--- packages/docs/src/usages/expansion-panels.vue
+++ packages/docs/src/usages/expansion-panels.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-expansion-panels v-bind="attrs" />
+  <v-expansion-panels v-bind="attrs">
+    <v-expansion-panel
+      v-for="(item,i) in 5"
+      :key="i"
+    >
+      <v-expansion-panel-header>Item</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
