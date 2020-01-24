1d794e760ad6bb7c0387a97dea228f2eeccf960a
John Leider <john.j.leider@gmail.com>
2020-01-24 17:43:33 -0500

    docs(home): improve home page styling

--- packages/docs/src/views/home/components/Hero.vue
+++ packages/docs/src/views/home/components/Hero.vue
@@ -4,7 +4,11 @@
     :class="$vuetify.theme.dark && 'theme--dark'"
     class="white--text py-4 py-md-12"
   >
-    <v-responsive min-height="30vh">
+    <v-responsive
+      :max-width="1280 + 24"
+      class="mx-auto pa-6"
+      min-height="30vh"
+    >
       <v-container class="pa-0">
         <v-row align="center">
           <v-col
