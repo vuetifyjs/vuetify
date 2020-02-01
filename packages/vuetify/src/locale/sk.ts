59b8422b3a3cd5048f990c1ccd636f0f5b89cd16
Igor Špánik <igor.spanik@softpoint.sk>
2020-02-01 20:30:15 +0100

    feat(locale): add Slovak translation (#10328)

---
+++ packages/vuetify/src/locale/sk.ts
@@ -0,0 +1,48 @@
+export default {
+  badge: 'Odznak',
+  close: 'Zavrieť',
+  dataIterator: {
+    noResultsText: 'Neboli nájdené žiadne záznamy',
+    loadingText: 'Načítavam položky...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Počet riadkov na stránku:',
+    ariaLabel: {
+      sortDescending: ': Usporiadané zostupne. Aktivováním vypnete usporiadanie.',
+      sortAscending: ': Usporiadané vzostupne. Aktivováním usporiadate zostupne.',
+      sortNone: ': Neusporiadené. Aktivováním usporiadate vzostupne.',
+    },
+    sortBy: 'Usporiadať podľa',
+  },
+  dataFooter: {
+    itemsPerPageText: 'Počet položiek na stránku:',
+    itemsPerPageAll: 'Všetko',
+    nextPage: 'Dalšia strana',
+    prevPage: 'Predchádzajúca strana',
+    firstPage: 'Prvá strana',
+    lastPage: 'Posledná strana',
+    pageText: '{0}-{1} z {2}',
+  },
+  datePicker: {
+    itemsSelected: '{0} vybrané',
+  },
+  noDataText: 'Nie sú dostupné žiadne dáta',
+  carousel: {
+    prev: 'Predchádzajúci obrázok',
+    next: 'Další obrázok',
+    ariaLabel: {
+      delimiter: 'Snímka {0} z {1}',
+    },
+  },
+  calendar: {
+    moreEvents: '{0} ďaľších',
+  },
+  fileInput: {
+    counter: '{0} súborov',
+    counterSize: '{0} súborov ({1} celkom)',
+  },
+  timePicker: {
+    am: 'AM',
+    pm: 'PM',
+  },
+}
