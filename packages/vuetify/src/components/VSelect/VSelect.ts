vuetifyjs/vuetify

commit e9a816dd38a0ebe2c683e2bf4fac92cc44dcf6ff

fix(VMenu/Selects): properly configure a11y (#7385)
* chore(routable): remove invalid attribute

* refactor(activatable): pass aria attributes through scoped slot

* fix(VMenu): update for aria support

* chore: experimenting with interpretted aria attributes

* fix: update all inputs using v-menu for aria support

* refactor: improve interaction with selects

* chore(VSelect): remove dev code

* refactor: improve native feel of select inputs

* fix(VSelect): update from feedback

* test: fix unit tests from improvments

* refactor(VSelect): change computeId logic and change default id

* test(VListItem): improve a11y coverage

* style(VListeItem): fix spec lint

* test(VMenu): expand a11y coverage

* test(VSelect): expand a11y coverage

* test(VAutocomplete): expand a11y coverage

* Update packages/vuetify/src/components/VAutocomplete/__tests__/VAutocomplete3.spec.ts

Co-Authored-By: Dmitry Sharshakov <d3dx12.xx@gmail.com>

* Update packages/vuetify/src/components/VAutocomplete/__tests__/VAutocomplete3.spec.ts

Co-Authored-By: Dmitry Sharshakov <d3dx12.xx@gmail.com>

* Update packages/vuetify/src/components/VList/__tests__/VListItem.spec.ts

Co-Authored-By: Dmitry Sharshakov <d3dx12.xx@gmail.com>

* style(VAutocomplete): fix lint

* Update packages/vuetify/src/components/VBtn/VBtn.ts

Co-Authored-By: Dmitry Sharshakov <d3dx12.xx@gmail.com>

* chore(VSelect): tweaks from review

* fix(VBtn): only add disabled attr when tag is button

* refactor(VMenu): improve keyboard functionality

* fix(VMenu): update tab functionality to match wai-aria

* refactor(VSelect): avoid iterating menu items if no selections

* refactor(VSelect): remove tab as a selection key for native behavior

packages/vuetify/src/components/VSelect/VSelect.ts
@@ -30,6 +30,7 @@ export const defaultMenuProps = {
   eager: true,
   closeOnClick: false,
   closeOnContentClick: false,
+  disableKeys: true,
   openOnClick: false,
   maxHeight: 300,
 }
@@ -111,7 +112,6 @@ export default baseMixins.extend<options>().extend({
 
   data () {
     return {
-      attrsInput: { role: 'combobox' },
       cachedItems: this.cacheItems ? this.items : [],
       content: null as any,
       isBooted: false,
@@ -143,6 +143,11 @@ export default baseMixins.extend<options>().extend({
         'v-select--chips--small': this.smallChips,
         'v-select--is-menu-active': this.isMenuActive,
       }
+    },
+    computedId (): string {
+      if (this.$attrs.id) return this.$attrs.id
+
+      return `computed-id-${this._uid}`
     },
     /* Used by other components to overwrite */
     computedItems (): object[] {
@@ -176,10 +181,15 @@ export default baseMixins.extend<options>().extend({
     },
     listData (): object {
       const scopeId = this.$vnode && (this.$vnode.context!.$options as { [key: string]: any })._scopeId
+      const attrs = scopeId ? {
+        [scopeId]: true,
+      } : {}
+
       return {
-        attrs: scopeId ? {
-          [scopeId]: true,
-        } : null,
+        attrs: {
+          ...attrs,
+          id: this.computedId,
+        },
         props: {
           action: this.multiple,
           color: this.color,
@@ -248,6 +258,8 @@ export default baseMixins.extend<options>().extend({
       })
     },
     isMenuActive (val) {
+      this.$nextTick(() => this.onMenuActiveChange(val))
+
       if (!val) return
 
       this.isBooted = true
@@ -284,6 +296,12 @@ export default baseMixins.extend<options>().extend({
     },
     /** @public */
     activateMenu () {
+      if (
+        this.disabled ||
+        this.readonly ||
+        this.isMenuActive
+      ) return
+
       this.isMenuActive = true
     },
     clearableCallback () {
@@ -401,10 +419,23 @@ export default baseMixins.extend<options>().extend({
 
       input.data!.domProps!.value = null
       input.data!.attrs!.readonly = true
-      input.data!.attrs!['aria-readonly'] = String(this.readonly)
+      input.data!.attrs!['aria-readonly'] = true
       input.data!.on!.keypress = this.onKeyPress
 
       return input
+    },
+    genInputSlot (): VNode {
+      const render = VTextField.options.methods.genInputSlot.call(this)
+
+      render.data!.attrs = {
+        ...render.data!.attrs,
+        role: 'button',
+        'aria-haspopup': 'listbox',
+        'aria-expanded': String(this.isMenuActive),
+        'aria-owns': this.computedId,
+      }
+
+      return render
     },
     genList (): VNode {
       // If there's no slots, we can use a cached VNode to improve performance
@@ -445,6 +476,7 @@ export default baseMixins.extend<options>().extend({
       }
 
       return this.$createElement(VMenu, {
+        attrs: { role: undefined },
         props,
         on: {
           input: (val: boolean) => {
@@ -530,9 +562,6 @@ export default baseMixins.extend<options>().extend({
         this.$emit('focus')
       }
     },
-    onEnterDown (e: Event) {
-      this.onBlur(e)
-    },
     onEscDown (e: Event) {
       e.preventDefault()
       if (this.isMenuActive) {
@@ -551,7 +580,11 @@ export default baseMixins.extend<options>().extend({
       this.keyboardLookupPrefix += e.key.toLowerCase()
       this.keyboardLookupLastTime = now
 
-      const index = this.allItems.findIndex(item => this.getText(item).toLowerCase().startsWith(this.keyboardLookupPrefix))
+      const index = this.allItems.findIndex(item => {
+        const text = (this.getText(item) || '').toString()
+
+        return text.toLowerCase().startsWith(this.keyboardLookupPrefix)
+      })
       const item = this.allItems[index]
       if (index !== -1) {
         this.setValue(this.returnObject ? item : this.getValue(item))
@@ -560,24 +593,60 @@ export default baseMixins.extend<options>().extend({
     },
     onKeyDown (e: KeyboardEvent) {
       const keyCode = e.keyCode
+      const menu = this.$refs.menu
 
-      // If enter, space, up, or down is pressed, open menu
-      if (!this.readonly && !this.isMenuActive && [
+      // If enter, space, open menu
+      if ([
         keyCodes.enter,
         keyCodes.space,
-        keyCodes.up, keyCodes.down,
       ].includes(keyCode)) this.activateMenu()
 
-      if (this.isMenuActive && this.$refs.menu) (this.$refs.menu as { [key: string]: any }).changeListIndex(e)
+      if (!menu) return
 
-      // This should do something different
-      if (keyCode === keyCodes.enter) return this.onEnterDown(e)
+      // If menu is active, allow default
+      // listIndex change from menu
+      if (this.isMenuActive && keyCode !== keyCodes.tab) {
+        menu.changeListIndex(e)
+      }
+
+      // If menu is not active, up and down can do
+      // one of 2 things. If multiple, opens the
+      // menu, if not, will cycle through all
+      // available options
+      if (
+        !this.isMenuActive &&
+        [keyCodes.up, keyCodes.down].includes(keyCode)
+      ) return this.onUpDown(e)
 
       // If escape deactivate the menu
       if (keyCode === keyCodes.esc) return this.onEscDown(e)
 
       // If tab - select item or close menu
       if (keyCode === keyCodes.tab) return this.onTabDown(e)
+
+      // If space preventDefault
+      if (keyCode === keyCodes.space) return this.onSpaceDown(e)
+    },
+    onMenuActiveChange (val: boolean) {
+      // If menu is closing and mulitple
+      // or menuIndex is already set
+      // skip menu index recalculation
+      if (
+        (this.multiple && !val) ||
+        this.getMenuIndex() > -1
+      ) return
+
+      const menu = this.$refs.menu
+
+      if (!menu || !this.isDirty) return
+
+      // When menu opens, set index of first active item
+      for (let i = 0; i < menu.tiles.length; i++) {
+        if (menu.tiles[i].getAttribute('aria-selected') === 'true') {
+          this.setMenuIndex(i)
+          break
+        }
+      }
     },
     onMouseUp (e: MouseEvent) {
       if (this.hasMouseDown) {
@@ -618,29 +687,53 @@ export default baseMixins.extend<options>().extend({
         }
       }
     },
+    onSpaceDown (e: KeyboardEvent) {
+      e.preventDefault()
+    },
     onTabDown (e: KeyboardEvent) {
-      const menuIndex = this.getMenuIndex()
+      const menu = this.$refs.menu
 
-      const listTile = (this.$refs.menu as { [key: string]: any }).tiles[menuIndex]
+      if (!menu) return
+
+      const activeTile = menu.activeTile
 
       // An item that is selected by
       // menu-index should toggled
       if (
-        listTile &&
-        listTile.className.indexOf('v-list-item--highlighted') > -1 &&
-        this.isMenuActive &&
-        menuIndex > -1
+        !this.multiple &&
+        activeTile &&
+        this.isMenuActive
       ) {
         e.preventDefault()
         e.stopPropagation()
 
-        listTile.click()
+        activeTile.click()
       } else {
         // If we make it here,
         // the user has no selected indexes
         // and is probably tabbing out
         this.blur(e)
       }
+    },
+    onUpDown (e: KeyboardEvent) {
+      const menu = this.$refs.menu
+
+      if (!menu) return
+
+      e.preventDefault()
+
+      // Multiple selects do not cycle their value
+      // when pressing up or down, instead activate
+      // the menu
+      if (this.multiple) return this.activateMenu()
+
+      const keyCode = e.keyCode
+
+      // Cycle through available values to achieve
+      // select native behavior
+      menu.getTiles()
+      keyCodes.up === keyCode ? menu.prevTile() : menu.nextTile()
+      menu.activeTile && menu.activeTile.click()
     },
     selectItem (item: object) {
       if (!this.multiple) {
@@ -662,6 +755,17 @@ export default baseMixins.extend<options>().extend({
           this.$refs.menu &&
             (this.$refs.menu as { [key: string]: any }).updateDimensions()
         })
+
+        // We only need to reset list index for multiple
+        // to keep highlight when an item is toggled
+        // on and off
+        if (!this.multiple) return
+
+        const listIndex = this.getMenuIndex()
+
+        this.setMenuIndex(-1)
+
+        this.$nextTick(() => this.setMenuIndex(listIndex))
       }
     },
     setMenuIndex (index: number) {

commit 3da2132d28e7357850f83b3843dc95e07bf77621

fix(VSelect): set correct aria-readonly value
broken by #7385

packages/vuetify/src/components/VSelect/VSelect.ts
@@ -420,7 +420,7 @@ export default baseMixins.extend<options>().extend({
         attrs: {
           readonly: true,
           type: 'text',
-          'aria-readonly': true,
+          'aria-readonly': String(this.readonly),
           'aria-activedescendant': this.$refs.menu && this.$refs.menu.activeTile && this.$refs.menu.activeTile.getAttribute('aria-labelledby'),
           autocomplete: input.data!.attrs!.autocomplete || 'off',
         },

packages/vuetify/src/components/VDataIterator/__tests__/__snapshots__/VDataFooter.spec.ts.snap
@@ -21,7 +21,7 @@ exports[`VDataFooter.ts should disable last page button if no items 1`] = `
                      id="input-60"
                      readonly="readonly"
                      type="text"
-                     aria-readonly="true"
+                     aria-readonly="false"
                      autocomplete="off"
               >
             </div>
@@ -122,7 +122,7 @@ exports[`VDataFooter.ts should render first & last icons with showFirstLastPage 
                      id="input-26"
                      readonly="readonly"
                      type="text"
-                     aria-readonly="true"
+                     aria-readonly="false"
                      autocomplete="off"
               >
             </div>
@@ -219,7 +219,7 @@ exports[`VDataFooter.ts should render in RTL mode 1`] = `
                      id="input-12"
                      readonly="readonly"
                      type="text"
-                     aria-readonly="true"
+                     aria-readonly="false"
                      autocomplete="off"
               >
             </div>
@@ -316,7 +316,7 @@ exports[`VDataFooter.ts should render with custom itemsPerPage 1`] = `
                      id="input-2"
                      readonly="readonly"
                      type="text"
-                     aria-readonly="true"
+                     aria-readonly="false"
                      autocomplete="off"
               >
             </div>
@@ -392,7 +392,7 @@ exports[`VDataFooter.ts should show current page if has showCurrentPage 1`] = `
                      id="input-50"
                      readonly="readonly"
                      type="text"
-                     aria-readonly="true"
+                     aria-readonly="false"
                      autocomplete="off"
               >
             </div>

packages/vuetify/src/components/VDataIterator/__tests__/__snapshots__/VDataIterator.spec.ts.snap
@@ -33,7 +33,7 @@ exports[`VDataIterator.ts should render and match snapshot 1`] = `
                        id="input-4"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -124,7 +124,7 @@ exports[`VDataIterator.ts should render and match snapshot with data 1`] = `
                        id="input-17"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -206,7 +206,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
                        id="input-29"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -288,7 +288,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
                        id="input-29"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -370,7 +370,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
                        id="input-29"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>

packages/vuetify/src/components/VDataTable/__tests__/__snapshots__/VDataTable.spec.ts.snap
@@ -437,7 +437,7 @@ exports[`VDataTable.ts should default to first option in itemsPerPageOptions if 
                        id="input-429"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -826,7 +826,7 @@ exports[`VDataTable.ts should handle object when checking if it should default t
                        id="input-448"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -1116,7 +1116,7 @@ exports[`VDataTable.ts should limit page to current page count if not using serv
                        id="input-410"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -1310,7 +1310,7 @@ exports[`VDataTable.ts should not limit page to current item count when using se
                        id="input-351"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -1599,7 +1599,7 @@ exports[`VDataTable.ts should not limit page to current item count when using se
                        id="input-351"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -1993,7 +1993,7 @@ exports[`VDataTable.ts should not search column with filterable set to false 1`]
                        id="input-371"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -2196,7 +2196,7 @@ exports[`VDataTable.ts should not search column with filterable set to false 2`]
                        id="input-371"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -2452,7 +2452,7 @@ exports[`VDataTable.ts should not search column with filterable set to false and
                        id="input-391"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -2848,7 +2848,7 @@ exports[`VDataTable.ts should not search column with filterable set to false and
                        id="input-391"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3116,7 +3116,7 @@ exports[`VDataTable.ts should not select item that is not selectable 1`] = `
                        id="input-525"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3215,7 +3215,7 @@ exports[`VDataTable.ts should pass kebab-case footer props correctly 1`] = `
                        id="input-298"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3314,7 +3314,7 @@ exports[`VDataTable.ts should render 1`] = `
                        id="input-6"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3410,7 +3410,7 @@ exports[`VDataTable.ts should render footer.page-text slot content 1`] = `
                        id="input-332"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3538,7 +3538,7 @@ exports[`VDataTable.ts should render loading state 1`] = `
                        id="input-246"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3744,7 +3744,7 @@ exports[`VDataTable.ts should render loading state 2`] = `
                        id="input-265"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -3935,7 +3935,7 @@ exports[`VDataTable.ts should render with body slot 1`] = `
                        id="input-45"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -4224,7 +4224,7 @@ exports[`VDataTable.ts should render with data 1`] = `
                        id="input-25"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -4408,7 +4408,7 @@ exports[`VDataTable.ts should render with group scoped slot 1`] = `
                        id="input-230"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -4835,7 +4835,7 @@ exports[`VDataTable.ts should render with group.summary scoped slot 1`] = `
                        id="input-155"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -5208,7 +5208,7 @@ exports[`VDataTable.ts should render with grouped rows 1`] = `
                        id="input-210"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -5412,7 +5412,7 @@ exports[`VDataTable.ts should render with item scoped slot 1`] = `
                        id="input-176"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -5726,7 +5726,7 @@ exports[`VDataTable.ts should render with item.expanded scoped slot 1`] = `
                        id="input-115"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -6066,7 +6066,7 @@ exports[`VDataTable.ts should render with showExpand 1`] = `
                        id="input-69"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -6406,7 +6406,7 @@ exports[`VDataTable.ts should render with showExpand 2`] = `
                        id="input-69"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>
@@ -6768,7 +6768,7 @@ exports[`VDataTable.ts should render with showSelect 1`] = `
                        id="input-94"
                        readonly="readonly"
                        type="text"
-                       aria-readonly="true"
+                       aria-readonly="false"
                        autocomplete="off"
                 >
               </div>

packages/vuetify/src/components/VDataTable/__tests__/__snapshots__/VDataTableHeader.spec.ts.snap
@@ -540,7 +540,7 @@ exports[`VDataTableHeader.ts mobile should render 1`] = `
                   <input id="input-37"
                          readonly="readonly"
                          type="text"
-                         aria-readonly="true"
+                         aria-readonly="false"
                          autocomplete="off"
                   >
                 </div>
@@ -601,7 +601,7 @@ exports[`VDataTableHeader.ts mobile should render with data-table-select header 
                   <input id="input-74"
                          readonly="readonly"
                          type="text"
-                         aria-readonly="true"
+                         aria-readonly="false"
                          autocomplete="off"
                   >
                 </div>
@@ -664,7 +664,7 @@ exports[`VDataTableHeader.ts mobile should work with multiSort 1`] = `
                   <input id="input-49"
                          readonly="readonly"
                          type="text"
-                         aria-readonly="true"
+                         aria-readonly="false"
                          autocomplete="off"
                   >
                 </div>
@@ -716,7 +716,7 @@ exports[`VDataTableHeader.ts mobile should work with showGroupBy 1`] = `
                   <input id="input-43"
                          readonly="readonly"
                          type="text"
-                         aria-readonly="true"
+                         aria-readonly="false"
                          autocomplete="off"
                   >
                 </div>
@@ -779,7 +779,7 @@ exports[`VDataTableHeader.ts mobile should work with sortBy correctly 1`] = `
                   <input id="input-57"
                          readonly="readonly"
                          type="text"
-                         aria-readonly="true"
+                         aria-readonly="false"
                          autocomplete="off"
                   >
                 </div>
@@ -844,7 +844,7 @@ exports[`VDataTableHeader.ts mobile should work with sortDesc correctly 1`] = `
                   <input id="input-65"
                          readonly="readonly"
                          type="text"
-                         aria-readonly="true"
+                         aria-readonly="false"
                          autocomplete="off"
                   >
                 </div>

packages/vuetify/src/components/VSelect/__tests__/__snapshots__/VSelect.spec.ts.snap
@@ -230,7 +230,7 @@ exports[`VSelect.ts should render v-select correctly when not using scope slot 1
           <input id="input-35"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>
@@ -275,7 +275,7 @@ exports[`VSelect.ts should render v-select correctly when not using v-list-item 
           <input id="input-28"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>
@@ -320,7 +320,7 @@ exports[`VSelect.ts should render v-select correctly when using v-list-item in i
           <input id="input-20"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>

packages/vuetify/src/components/VSelect/__tests__/__snapshots__/VSelect2.spec.ts.snap
@@ -17,7 +17,7 @@ exports[`VSelect.ts should be clearable with prop, dirty and multi select 1`] = 
           <input id="input-106"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>
@@ -77,7 +77,7 @@ exports[`VSelect.ts should be clearable with prop, dirty and single select 1`] =
           <input id="input-97"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>
@@ -137,7 +137,7 @@ exports[`VSelect.ts should use scoped slot for selection generation 1`] = `
           <input id="input-26"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>

packages/vuetify/src/components/VSelect/__tests__/__snapshots__/VSelect3.spec.ts.snap
@@ -17,7 +17,7 @@ exports[`VSelect.ts should add color to selected index 1`] = `
           <input id="input-57"
                  readonly="readonly"
                  type="text"
-                 aria-readonly="true"
+                 aria-readonly="false"
                  autocomplete="off"
           >
         </div>