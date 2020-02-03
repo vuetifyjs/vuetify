e68338532c8ba06eabc6e4caecd3f21bfce0874d
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-02-03 11:08:35 -0800

    docs(v-simple-checkbox): add example and description

---
+++ packages/docs/src/examples/data-tables/intermediate/simple-checkbox.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      class="elevation-1"
+    >
+      <template v-slot:item.glutenFree="{ item }">
+        <v-checkbox v-model="item.glutenFree" disabled></v-checkbox>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+            glutenFree: true,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+            glutenFree: false,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+            glutenFree: false,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+            glutenFree: true,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+            glutenFree: true,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+            glutenFree: false,
+          },
+        ],
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+          { text: 'Gluten-Free', value: 'glutenFree' },
+        ],
+      }
+    },
+  }
+</script>
