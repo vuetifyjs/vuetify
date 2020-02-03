2e658c187d6520dc2bfe146a5af6699b9f53c2ce
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-02-03 20:44:28 +0300

    refactor(touch): refactor tests

--- packages/vuetify/src/directives/touch/__tests__/touch.spec.ts
+++ packages/vuetify/src/directives/touch/__tests__/touch.spec.ts
@@ -14,79 +14,78 @@ import Touch from '../'
 // Utilities
 import { touch } from '../../../../test'
 
+// Types
+import { TouchValue } from '../../../../types'
+
 describe('touch.ts', () => {
-  let mountFunction: (value?: object) => HTMLElement
   const el = document.createElement('div')
+  const mountFunction = (value: TouchValue): HTMLElement => {
+    const Test = defineComponent(() => () => withDirectives(h('div', { class: 'test' }), [ [ Touch, value ] ]))
+    const app = createApp()
 
-  beforeEach(() => {
-    mountFunction = (value = {}) => {
-      const Test = defineComponent(() => () => withDirectives(h('div', { class: 'test' }), [ [ Touch, value ] ]))
-      const app = createApp()
-
-      app.mount(Test, el)
-      return el
-    }
-  })
+    app.mount(Test, el)
+    return el.querySelector('.test')
+  }
 
   it('should call directive handlers', () => {
     const down = jest.fn()
-    touch(mountFunction({ down }).querySelector('.test')).start(0, 0).end(0, 20)
+    touch(mountFunction({ down })).start(0, 0).end(0, 20)
     expect(down).toHaveBeenCalled()
 
     const up = jest.fn()
-    touch(mountFunction({ up }).querySelector('.test')).start(0, 0).end(0, -20)
+    touch(mountFunction({ up })).start(0, 0).end(0, -20)
     expect(up).toHaveBeenCalled()
 
     const left = jest.fn()
-    touch(mountFunction({ left }).querySelector('.test')).start(0, 0).end(-20, 0)
+    touch(mountFunction({ left })).start(0, 0).end(-20, 0)
     expect(left).toHaveBeenCalled()
 
     const right = jest.fn()
-    touch(mountFunction({ right }).querySelector('.test')).start(0, 0).end(20, 0)
+    touch(mountFunction({ right })).start(0, 0).end(20, 0)
     expect(right).toHaveBeenCalled()
 
     const start = jest.fn()
-    touch(mountFunction({ start }).querySelector('.test')).start(0, 0)
+    touch(mountFunction({ start })).start(0, 0)
     expect(start).toHaveBeenCalled()
 
     const move = jest.fn()
-    touch(mountFunction({ move }).querySelector('.test')).move(0, 0)
+    touch(mountFunction({ move })).move(0, 0)
     expect(move).toHaveBeenCalled()
 
     const end = jest.fn()
-    touch(mountFunction({ end }).querySelector('.test')).end(0, 0)
+    touch(mountFunction({ end })).end(0, 0)
     expect(end).toHaveBeenCalled()
   })
 
   it('should call directive handlers if not straight down/up/right/left', async () => {
     const nope = jest.fn()
     const down = jest.fn()
-    touch(mountFunction({ down, right: nope }).querySelector('.test')).start(0, 0).end(5, 20)
+    touch(mountFunction({ down, right: nope })).start(0, 0).end(5, 20)
     expect(nope).not.toHaveBeenCalled()
     expect(down).toHaveBeenCalled()
   })
 
   it('should not call directive handlers if distance is too small ', async () => {
     const down = jest.fn()
-    touch(mountFunction({ down }).querySelector('.test')).start(0, 0).end(0, 10)
+    touch(mountFunction({ down })).start(0, 0).end(0, 10)
     expect(down).not.toHaveBeenCalled()
 
     const up = jest.fn()
-    touch(mountFunction({ up }).querySelector('.test')).start(0, 0).end(0, -10)
+    touch(mountFunction({ up })).start(0, 0).end(0, -10)
     expect(up).not.toHaveBeenCalled()
 
     const left = jest.fn()
-    touch(mountFunction({ left }).querySelector('.test')).start(0, 0).end(-10, 0)
+    touch(mountFunction({ left })).start(0, 0).end(-10, 0)
     expect(left).not.toHaveBeenCalled()
 
     const right = jest.fn()
-    touch(mountFunction({ right }).querySelector('.test')).start(0, 0).end(10, 0)
+    touch(mountFunction({ right })).start(0, 0).end(10, 0)
     expect(right).not.toHaveBeenCalled()
   })
 
   it('should unmount', async () => {
     const start = jest.fn()
-    const el = mountFunction({ start }).querySelector('.test') as HTMLElement
+    const el = mountFunction({ start })
 
     Touch.unmounted(el, { value: {} } as DirectiveBinding, h('div'), null)
 
