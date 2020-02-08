f377755effdadcd812423af3fb8bbc102eb64481
fpdrozd <fpdrozd@gmail.com>
2020-02-08 16:39:35 +0100

    fix(vripple): add unit tests

--- packages/vuetify/src/directives/ripple/__tests__/ripple.spec.ts
+++ packages/vuetify/src/directives/ripple/__tests__/ripple.spec.ts
@@ -10,22 +10,30 @@ import {
 } from '@vue/test-utils'
 
 describe('ripple.ts', () => {
-  it('Ripple with no value should render element with ripple enabled', () => {
-    const testComponent = Vue.component('test', {
-      directives: {
-        Ripple,
-      },
-      render (h) {
-        const data = {
-          directives: [{
-            name: 'ripple',
-          }],
-        }
-        return h('div', data)
-      },
-    })
+  let mountFunction: (options?: object) => Wrapper<Vue>
 
-    const wrapper = mount(testComponent)
+  beforeEach(() => {
+    mountFunction = (options = {}) => {
+      const testComponent = Vue.component('test', {
+        directives: {
+          Ripple,
+        },
+        render (h) {
+          const data = {
+            directives: [{
+              name: 'ripple',
+            }],
+          }
+          return h('div', data)
+        },
+      })
+
+      return mount(testComponent, options)
+    }
+  })
+
+  it('Ripple with no value should render element with ripple enabled', () => {
+    const wrapper = mountFunction()
 
     const div = wrapper.find('div')
     expect(div.element['_ripple'].enabled).toBe(true)
@@ -65,5 +73,56 @@ describe('ripple.ts', () => {
 
     wrapper.setProps({ ripple: true })
     expect(div.element['_ripple'].enabled).toBe(true)
+  })
+
+  it('Ripple should be triggered by the mousedown event', () => {
+    jest.useFakeTimers()
+
+    const wrapper = mountFunction()
+
+    const mousedownEvent = new MouseEvent('mousedown', { detail: 1 })
+    wrapper.element.dispatchEvent(mousedownEvent)
+
+    expect(wrapper.find('.v-ripple__container').exists()).toBe(true)
+
+    const mouseupEvent = new MouseEvent('mouseup', { detail: 1 })
+    wrapper.element.dispatchEvent(mouseupEvent)
+
+    jest.runAllTimers()
+    expect(wrapper.find('.v-ripple__container').exists()).toBe(false)
+  })
+
+  it('Ripple should be triggered by the enter key', () => {
+    jest.useFakeTimers()
+
+    const wrapper = mountFunction()
+
+    const keydownEvent = new KeyboardEvent('keydown', { keyCode: 13 })
+    wrapper.element.dispatchEvent(keydownEvent)
+
+    expect(wrapper.find('.v-ripple__container').exists()).toBe(true)
+
+    const keyupEvent = new KeyboardEvent('keyup')
+    wrapper.element.dispatchEvent(keyupEvent)
+
+    jest.runAllTimers()
+    expect(wrapper.find('.v-ripple__container').exists()).toBe(false)
+  })
+
+  it('Ripple should be triggered by the space key', () => {
+    jest.useFakeTimers()
+
+    const wrapper = mountFunction()
+
+    const keydownEvent = new KeyboardEvent('keydown', { keyCode: 32 })
+    wrapper.element.dispatchEvent(keydownEvent)
+
+    expect(wrapper.find('.v-ripple__container').exists()).toBe(true)
+
+    const keyupEvent = new KeyboardEvent('keyup')
+    wrapper.element.dispatchEvent(keyupEvent)
+
+    jest.runAllTimers()
+    expect(wrapper.find('.v-ripple__container').exists()).toBe(false)
   })
 })
