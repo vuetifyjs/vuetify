a738ef752a968e53410a1555877efbc3f374cfae
Jacek Karczmarczyk <jk@drukcd.pl>
2020-02-09 09:51:49 +0700

    fix(VEditDialog): large prop not working
    fixes #10468

--- packages/vuetify/src/components/VDataTable/VEditDialog.ts
+++ packages/vuetify/src/components/VDataTable/VEditDialog.ts
@@ -133,8 +133,10 @@ export default mixins(Returnable, Themeable).extend({
         },
       },
     }, [
-      this.genContent(),
-      this.large ? this.genActions() : null,
+      h('div', [
+        this.genContent(),
+        this.large ? this.genActions() : null,
+      ]),
     ])
   },
 })
