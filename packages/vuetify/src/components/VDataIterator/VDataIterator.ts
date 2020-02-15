edcabcf7218f884cf0b5a666ab54e6568304f816
Frank <fj@move-elevator.de>
2020-02-15 13:25:46 +0100

    feat(VDataIterator) emit the page-count event from the inner VData component like VDataTable

--- packages/vuetify/src/components/VDataIterator/VDataIterator.ts
+++ packages/vuetify/src/components/VDataIterator/VDataIterator.ts
@@ -295,6 +295,7 @@ export default Themeable.extend({
           this.internalCurrentItems = v
           this.$emit('current-items', v)
         },
+        'page-count': (v: number) => this.$emit('page-count', v),
       },
       scopedSlots: {
         default: this.genDefaultScopedSlot,
