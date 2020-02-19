500fc0d014f4cbd774ec9274adbb9e8956aae674
Jacek Karczmarczyk <jk@drukcd.pl>
2020-02-19 16:32:36 +0700

    Update babel.config.js

--- packages/docs/babel.config.js
+++ packages/docs/babel.config.js
@@ -9,15 +9,11 @@ module.exports = {
   plugins: [
     [
       '@babel/plugin-proposal-optional-chaining',
-      {
-        loose: true,
-      },
+      { loose: true },
     ],
     [
       '@babel/plugin-proposal-nullish-coalescing-operator',
-      {
-        loose: true,
-      },
+      { loose: true },
     ],
   ]
 }
