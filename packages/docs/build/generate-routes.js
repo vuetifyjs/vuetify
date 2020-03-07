c961df6065464a54173a8a2f604b9f91aebf07d4
Kael <kaelwd@gmail.com>
2020-03-07 21:39:18 +1100

    chore(prerender): use worker_threads, add progress bar

--- packages/docs/build/generate-routes.js
+++ packages/docs/build/generate-routes.js
@@ -13,31 +13,27 @@ const languages = require('../src/data/i18n/languages.json')
   .filter(lang => lang !== 'eo-UY')
 
 module.exports = function generateRoutes () {
-  return new Promise((resolve, reject) => {
-    glob(DATA_PATH_JSON_FILES, (err, files) => {
-      if (err) return reject(err)
+  const files = glob.sync(DATA_PATH_JSON_FILES)
 
-      const paths = ['/']
-      const routes = [{ locale: '', path: '', fullPath: '/' }]
+  const paths = ['/']
+  const routes = [{ locale: '', path: '', fullPath: '/' }]
 
-      for (const file of files) {
-        const route = file
-          .split('/pages/')
-          .pop()
-          .split('/')
-          .map(i => kebabCase(i.replace(/json/, '')).toLowerCase())
-          .join('/')
+  for (const file of files) {
+    const route = file
+      .split('/pages/')
+      .pop()
+      .split('/')
+      .map(i => kebabCase(i.replace(/json/, '')).toLowerCase())
+      .join('/')
 
-        paths.push(`/${route}`)
-      }
+    paths.push(`/${route}`)
+  }
 
-      for (const locale of languages) {
-        for (const path of paths) {
-          routes.push({ locale, path, fullPath: `/${locale}${path}` })
-        }
-      }
+  for (const locale of languages) {
+    for (const path of paths) {
+      routes.push({ locale, path, fullPath: `/${locale}${path}` })
+    }
+  }
 
-      resolve(routes)
-    })
-  })
+  return routes
 }
--- packages/docs/build/prerender.js
+++ packages/docs/build/prerender.js
@@ -1,14 +1,22 @@
-os = require('os')
+const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
+const mkdirp = require('mkdirp')
+const { performance } = require('perf_hooks')
+const {
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData,
+} = require('worker_threads')
+
+const ProgressBar = require('progress')
+const { createBundleRenderer } = require('vue-server-renderer')
 const generateRoutes = require('./generate-routes')
-const { DynamicPool } = require('node-worker-threads-pool')
 
 const threads = os.cpus().length
 const resolve = file => path.resolve(__dirname, file)
-
-const pool = new DynamicPool(threads)
 
 function chunk (arr, chunkSize) {
   const chunks = []
@@ -23,79 +31,110 @@ function chunk (arr, chunkSize) {
 function readFile (file) {
   return fs.readFileSync(resolve(file), 'utf-8')
 }
+const writeFile = util.promisify(fs.writeFile)
 
-generateRoutes().then(routes => {
+/**
+ * Call cb for each item in arr, waiting for a returned
+ * promise to resolve before calling on the next item
+ *
+ * @param {any[]} arr
+ * @param {function(*): Promise<void>} cb
+ * @return {Promise<void>}
+ */
+function forEachSequential (arr, cb) {
+  return arr.reduce((p, val) => {
+    return p.then(() => {
+      return cb(val)
+    })
+  }, Promise.resolve())
+}
+
+if (isMainThread) {
+  const routes = generateRoutes()
   const template = readFile('../src/index.template.html')
   const bundle = JSON.parse(readFile('../dist/vue-ssr-server-bundle.json'))
   const clientManifest = JSON.parse(readFile('../dist/vue-ssr-client-manifest.json'))
 
+  const bar = new ProgressBar('[:bar] :percent | ETA: :etas | :current/:total | :timems | :lastFile', {
+    total: routes.length,
+    width: 64,
+  })
+
   chunk(routes, Math.round(routes.length / threads)).forEach((routes, index) => {
-    pool.exec({
+    const worker = new Worker(__filename, {
       workerData: { routes, template, bundle, clientManifest, index },
-      task () {
-        const { routes, template, bundle, clientManifest, index } = this.workerData
-
-        const fs = require('fs')
-        const path = require('path')
-        const util = require('util')
-        const mkdirp = require('mkdirp')
-        const { performance } = require('perf_hooks')
-        const { createBundleRenderer } = require('vue-server-renderer')
-
-        const writeFile = util.promisify(fs.writeFile)
-
-        const renderer = createBundleRenderer(bundle, {
-          runInNewContext: false,
-          clientManifest,
-          shouldPrefetch: () => false,
-          template,
-        })
-
-        console.info(`Renderer ${index} created`)
-
-        /**
-         * Call cb for each item in arr, waiting for a returned
-         * promise to resolve before calling on the next item
-         *
-         * @param {any[]} arr
-         * @param {function(*): Promise<void>} cb
-         * @return {Promise<void>}
-         */
-        function forEachSequential (arr, cb) {
-          return arr.reduce((p, val) => {
-            return p.then(() => {
-              return cb(val)
-            })
-          }, Promise.resolve())
-        }
-
-        forEachSequential(routes, route => {
-          const start = performance.now()
-
-          const context = {
-            crowdin: '',
-            hostname: 'https://vuetifyjs.com', // TODO
-            hreflangs: '', // TODO
-            // hreflangs: availableLanguages.reduce((acc, lang) => {
-            //   return acc + `<link rel="alternate" hreflang="${lang}" href="https://${req.hostname}/${lang}${encodeURI(req.params[1])}" />`
-            // }, ''),
-            lang: route.locale,
-            scripts: '',
-            title: 'Vuetify', // default title
-            url: route.fullPath,
-          }
-
-          return renderer.renderToString(context).then(html => {
-            const dir = path.join('./dist/', route.fullPath)
-
-            return mkdirp(dir).then(() =>
-              writeFile(path.join(dir, 'index.html'), html, { encoding: 'utf-8' })
-            ).then(() => {
-              console.info(`${index}: (${Math.round(performance.now() - start)}ms) ${route.fullPath}`)
-            })
-          }).catch(err => console.error(`${index}: (${Math.round(performance.now() - start)}ms) ${route.fullPath}`, err))
-        }).then(() => process.exit(0))
-      },
+      stdout: true,
+    })
+    worker.on('message', ({ message, error, lastFile, time }) => {
+      if (message) {
+        bar.interrupt(message)
+      }
+      if (error) {
+        bar.interrupt('\n' + lastFile + '\n' + error)
+      }
+      if (lastFile) {
+        bar.tick({ lastFile, time })
+      }
     })
   })
-})
+} else {
+  const { routes, template, bundle, clientManifest, index } = workerData
+
+  const renderer = createBundleRenderer(bundle, {
+    runInNewContext: false,
+    clientManifest,
+    shouldPrefetch: () => false,
+    template,
+  })
+
+  parentPort.postMessage({
+    message: `Renderer ${index} created`
+  })
+
+  // Redirect console.log to the main thread
+  let currentRoute
+  const write = process.stdout.write
+  process.stdout.write = data => {
+    parentPort.postMessage({
+      message: '\n' + currentRoute.fullPath + '\n' + data.toString()
+    })
+    return write.call(process.stdout, data)
+  }
+
+  forEachSequential(routes, route => {
+    currentRoute = route
+    const start = performance.now()
+
+    const context = {
+      crowdin: '',
+      hostname: 'https://vuetifyjs.com', // TODO
+      hreflangs: '', // TODO
+      // hreflangs: availableLanguages.reduce((acc, lang) => {
+      //   return acc + `<link rel="alternate" hreflang="${lang}" href="https://${req.hostname}/${lang}${encodeURI(req.params[1])}" />`
+      // }, ''),
+      lang: route.locale,
+      scripts: '',
+      title: 'Vuetify', // default title
+      url: route.fullPath,
+    }
+
+    return renderer.renderToString(context).then(html => {
+      const dir = path.join('./dist/', route.fullPath)
+
+      return mkdirp(dir).then(() =>
+        writeFile(path.join(dir, 'index.html'), html, { encoding: 'utf-8' })
+      ).then(() => {
+        parentPort.postMessage({
+          lastFile: route.fullPath,
+          time: Math.round(performance.now() - start),
+        })
+      })
+    }).catch(err => {
+      parentPort.postMessage({
+        error: err.stack,
+        lastFile: route.fullPath,
+        time: Math.round(performance.now() - start),
+      })
+    })
+  }).then(() => process.exit(0))
+}
