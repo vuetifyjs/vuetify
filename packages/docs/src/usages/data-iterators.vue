d31b21a30bfab2a4718c0b334d6c4743987dee28
Lee Marlow <lee@yuzu.systems>
2020-03-07 19:10:03 -0800

    docs(vdataiterator): updated usage layout

--- packages/docs/src/usages/data-iterators.vue
+++ packages/docs/src/usages/data-iterators.vue
@@ -68,7 +68,7 @@
             v-for="item in props.items"
             :key="item.name"
             cols="12"
-            sm="4"
+            sm="6"
           >
             <v-card>
               <v-card-title>
@@ -77,32 +77,74 @@
               <v-divider />
               <v-list dense>
                 <v-list-item>
-                  <v-list-item-content>Calories:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.calories }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Calories:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.calories }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-content>Fat:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.fat }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Fat:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.fat }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-content>Carbs:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.carbs }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Carbs:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.carbs }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-content>Protein:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.protein }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Protein:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.protein }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-content>Sodium:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.sodium }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Sodium:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.sodium }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-content>Calcium:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.calcium }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Calcium:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.calcium }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-content>Iron:</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.iron }}</v-list-item-content>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <v-list-item-content>Iron:</v-list-item-content>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-list-item-content>{{ item.iron }}</v-list-item-content>
+                    </v-col>
+                  </v-row>
                 </v-list-item>
               </v-list>
             </v-card>
