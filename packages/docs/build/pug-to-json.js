c51ee535f2ef09d349a3ad3ee0cc291a45028a1e
Kael <kaelwd@gmail.com>
2020-03-30 00:53:22 +1100

    refactor: reduce nesting, add comments

--- packages/docs/build/pug-to-json.js
+++ packages/docs/build/pug-to-json.js
@@ -6,6 +6,20 @@ const compiler = require('vue-template-compiler')
 module.exports = function (content) {
   this.cacheable()
 
+  let ast
+  pug.render(content, {
+    compileDebug: false,
+    filename: this.resourcePath,
+    plugins: [{
+      preFilters (_ast) {
+        ast = _ast
+
+        // Skip fully compiling the template
+        return { type: 'Block', nodes: [], line: 0 }
+      }
+    }]
+  })
+
   const page = { children: [] }
   let pageSection
 
@@ -17,64 +31,56 @@ module.exports = function (content) {
     page.children.push(pageSection)
   }
 
-  pug.render(content, {
-    compileDebug: false,
-    filename: this.resourcePath,
-    filters: {
-      code: text => text
-    },
-    plugins: [{
-      preFilters (ast) {
-        const parents = []
-        walk(ast, node => {
-          if (!pageSection || ['h2', 'up-next', 'usage', 'usage-new', 'api', 'examples'].includes(node.name)) {
-            startSection()
-          }
-          switch (node.type) {
-            case 'Tag':
-              if (node.name === 'h2') {
-                pageSection.children.push({
-                  type: 'heading',
-                  lang: node.block.nodes[0].val
-                })
-              } else if (node.name === 'code') {
-                const lang = eval(node.attrs.find(attr => attr.name === 'lang').val)
-                const src = eval(node.attrs.find(attr => attr.name === 'src').val)
-                // TODO: inline code
-                pageSection.children.push({
-                  type: 'markup',
-                  val: `${lang}_${src}`
-                })
-              } else {
-                const attrs = node.attrs.reduce((acc, attr) => acc + `${attr.name}="${eval(`(${attr.val})`)}" `, '')
-                const parsedAttrs = (compiler.compile(`<${node.name} ${attrs}/>`)
-                  .ast.attrs || []).reduce((acc, attr) => (acc[attr.name] = eval(`(${attr.value})`), acc), {})
-                const lang = node.block.nodes[0] && node.block.nodes[0].val
-                const tag = { type: node.name, lang, ...parsedAttrs }
-                pageSection.children.push(tag)
-              }
-              return false
+  const parents = []
+  walk(ast, node => {
+    if (!pageSection || ['h2', 'up-next', 'usage', 'usage-new', 'api', 'examples'].includes(node.name)) {
+      startSection()
+    }
+    switch (node.type) {
+      case 'Tag':
+        if (node.name === 'h2') {
+          pageSection.children.push({
+            type: 'heading',
+            lang: node.block.nodes[0].val
+          })
+        } else if (node.name === 'code') {
+          const lang = eval(node.attrs.find(attr => attr.name === 'lang').val)
+          const src = eval(node.attrs.find(attr => attr.name === 'src').val)
+          // TODO: inline code
+          pageSection.children.push({
+            type: 'markup',
+            val: `${lang}_${src}`
+          })
+        } else {
+          // Pug doesn't deal with attrs in a way that I like, so we're going
+          // to pass them through vue's compiler to deal with binding for us
+          // First concat them back into a name="val" string (would be easier if we had proper location data on the AST)
+          const attrs = node.attrs.reduce((acc, attr) => acc + `${attr.name}="${eval(`(${attr.val})`)}" `, '')
+          // Run that through vue-template-compiler
+          const parsedAttrs = (compiler.compile(`<${node.name} ${attrs}/>`)
+            // Then eval() a bound object or array into an actual object instead of a string
+            // This is like JSON.parse() but with JS instead of JSON
+            .ast.attrs || []).reduce((acc, attr) => (acc[attr.name] = eval(`(${attr.value})`), acc), {})
+          const lang = node.block.nodes[0] && node.block.nodes[0].val
+          const tag = { type: node.name, lang, ...parsedAttrs }
+          pageSection.children.push(tag)
+        }
+        return false
 
-            case 'Text':
-              pageSection.children.push({
-                type: 'text',
-                lang: node.val,
-              })
-              break
+      case 'Text':
+        pageSection.children.push({
+          type: 'text',
+          lang: node.val,
+        })
+        break
 
-            case 'Block':
-              break
+      case 'Block':
+        break
 
-            default:
-              throw new Error(`Unexpected token "${node.type}" on line ${node.line}`)
-          }
-        }, null, { parents })
-
-        // Skip fully compiling the template
-        return { type: 'Block', nodes: [], line: 0 }
-      }
-    }]
-  })
+      default:
+        throw new Error(`Unexpected token "${node.type}" on line ${node.line}`)
+    }
+  }, null, { parents })
 
   return 'module.exports = ' + JSON.stringify(page, null, 2)
 }
