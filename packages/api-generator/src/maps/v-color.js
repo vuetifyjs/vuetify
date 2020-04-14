536b7810a905e69ad37126ffcaf1529dd8050216
fpdrozd <fpdrozd@gmail.com>
2020-04-14 15:17:27 +0200

    docs(directives): add page for color directive

---
+++ packages/api-generator/src/maps/v-color.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'v-color': {
+    options: [
+      {
+        name: 'arg:border',
+        default: false,
+        type: 'string',
+      },
+      {
+        name: 'arg:gradient',
+        default: false,
+        type: 'string',
+      },
+      {
+        name: 'arg:text',
+        default: false,
+        type: 'string',
+      },
+      {
+        name: 'modifiers.top.right.bottom.left',
+        default: false,
+        type: 'boolean',
+      },
+      {
+        name: 'value',
+        default: false,
+        type: 'string',
+      },
+    ],
+  },
+}
---
+++ packages/docs/src/examples/color/usage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="3">
+        <div
+          v-color:text="'primary'"
+          class="elem pa-6 text-center grey lighten-2"
+        >
+          Text
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div
+          v-color="'primary'"
+          class="elem pa-6 text-center white--text"
+        >
+          Background
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div
+          v-color="'grey lighten-2'"
+          v-color:border="'primary'"
+          class="elem pa-6 text-center black--text"
+        >
+          Border
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div
+          v-color:gradient="'to top, blue, blue darken-3'"
+          class="elem pa-6 text-center white--text"
+        >
+          Gradient
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.elem {
+  border: solid 2px white;
+}
+</style>
