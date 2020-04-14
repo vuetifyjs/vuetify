375ae47a19c0ccddea38a32680e3ec395464abad
John Leider <john.j.leider@gmail.com>
2020-04-14 00:11:53 -0400

    docs: add documentation page; tweak styles

---
+++ packages/docs/src/examples/border-radius/simple/rounded.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col
+        v-for="value in ['-sm', '', '-lg', '-xl']"
+        :key="value"
+        cols="12"
+        md="2"
+      >
+        <div
+          class="pa-6 text-center grey lighten-2"
+          :class="`rounded${value}`"
+          v-text="`.rounded${value}`"
+        ></div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ packages/docs/src/examples/border-radius/simple/separately.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col
+        v-for="value in ['tl', 'tr', 'br', 'bl']"
+        :key="value"
+        cols="12"
+        md="2"
+      >
+        <div
+          class="pa-6 text-center grey lighten-2"
+          :class="`rounded-${value}-xl`"
+          v-text="`.rounded-${value}-xl`"
+        ></div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
