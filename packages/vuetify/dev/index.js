0b51cd129749a58d262972b7d3374730dfc29c2c
John Leider <john.j.leider@gmail.com>
2020-04-16 13:46:01 -0400

    chore: update development playground

--- packages/vuetify/dev/index.js
+++ packages/vuetify/dev/index.js
@@ -1,19 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-import Vuetify from 'vuetify'
-import * as locales from '../src/locale'
 import router from './router'
+import vuetify from './vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.performance = true
-
-Vue.use(Vuetify)
-
-const vuetify = new Vuetify({
-  lang: {
-    locales,
-  },
-})
 
 const vm = new Vue({
   data: () => ({ isLoaded: document.readyState === 'complete' }),
