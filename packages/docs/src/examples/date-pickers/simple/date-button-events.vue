8dfa20a79e96d4350421ebeb6c1d9d97c2556839
Jacek Karczmarczyk <jk@drukcd.pl>
2020-04-16 15:18:55 +0200

    docs

---
+++ packages/docs/src/examples/date-pickers/simple/date-button-events.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6" class="my-2 px-1">
+      <v-date-picker
+        v-model="date"
+        @dblclick:date="dblClick"
+        @mouseenter:month="mouseEnter"
+        @mouseleave:month="mouseLeave"
+        @contextmenu:year="contextMenu"
+      ></v-date-picker>
+    </v-col>
+    <v-col cols="12" sm="6" class="my-2 px-1">
+      <div class="body-1 mb-2"><v-icon small>{{ done[0] ? '$checkboxOn' : '$checkboxOff' }} </v-icon> Double click on any date</div>
+      <div class="body-1"><v-icon small>{{ done[1] ? '$checkboxOn' : '$checkboxOff' }} </v-icon> Move mouse cursor over any month button</div>
+      <div class="title mb-2">Mouse pointer is located on: {{ mouseMonth || '-' }}</div>
+      <div class="body-1"><v-icon small>{{ done[2] ? '$checkboxOn' : '$checkboxOff' }} </v-icon> Right click on any year</div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      date: new Date().toISOString().substr(0, 10),
+      mouseMonth: null,
+      done: [false, false, false],
+    }),
+    methods: {
+      dblClick (date) {
+        this.$set(this.done, 0, true)
+        alert(`You have just double clicked the following date: ${date}`)
+      },
+      mouseEnter (month) {
+        this.$set(this.done, 1, true)
+        this.mouseMonth = month
+      },
+      mouseLeave () {
+        this.mouseMonth = null
+      },
+      contextMenu (year, event) {
+        this.$set(this.done, 2, true)
+        event.preventDefault()
+        alert(`You have activated context menu for year ${year}`)
+      },
+    },
+  }
+</script>
