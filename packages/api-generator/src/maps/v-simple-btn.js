f994d4629655479e23c23f280013e4d753eb0013
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-04-19 15:18:37 -0700

    docs(vsimplebtn): start component docs
    Add component to api gen with missing event and slot. Create lang file and add component props.

---
+++ packages/api-generator/src/maps/v-simple-btn.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'v-simple-btn': {
+    slots: [
+      {
+        name: 'default',
+        props: undefined,
+      },
+    ],
+    events: [
+      {
+        name: 'click',
+        value: 'Event',
+      },
+    ],
+  },
+}
