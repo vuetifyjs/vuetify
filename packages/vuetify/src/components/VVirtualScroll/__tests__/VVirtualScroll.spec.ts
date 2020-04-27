4793235367580002164c95e9cef7b6a5ddf9bf7a
Alberto De Agostini <albertodeagostini88@gmail.com>
2020-04-27 13:50:16 +0200

    fix(vvirtualscroll): fix broken test

--- packages/vuetify/src/components/VVirtualScroll/__tests__/VVirtualScroll.spec.ts
+++ packages/vuetify/src/components/VVirtualScroll/__tests__/VVirtualScroll.spec.ts
@@ -71,7 +71,7 @@ describe('VVirtualScroll.ts', () => {
   it('should render right items on scroll and match snapshot', async () => {
     const wrapper = mountFunction({
       propsData: {
-        height: '100px',
+        height: `${elementHeight}px`,
         items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
         itemHeight: 50,
       },
