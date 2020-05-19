4f685fbe622500cc93b0e49d6bddc3961120038c
chewy94 <smkimbal@gmail.com>
2020-05-19 11:43:23 -0700

    docs(vmenu): add doc example for vmenu roundable prop

---
+++ packages/docs/src/examples/menus/simple/rounded.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="text-center d-flex justify-space-around">
+    <v-menu top rounded="0">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          class="rounded-0"
+          color="primary"
+          dark
+          v-on="on"
+        >
+          rounded="0"
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-menu top :rounded="true">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="primary"
+          dark
+          v-on="on"
+        >
+          :rounded="true"
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-menu top rounded="lg">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          class="rounded-lg"
+          color="primary"
+          dark
+          v-on="on"
+        >
+          rounded="lg"
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+      offset: true,
+    }),
+  }
+</script>
