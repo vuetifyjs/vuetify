d649efd14afeeada22afffcc3a49a23c4fc6e011
John Leider <john.j.leider@gmail.com>
2020-05-21 15:09:21 -0500

    chore(app.js): remove dev code

--- packages/docs-next/src/plugins/app.js
+++ packages/docs-next/src/plugins/app.js
@@ -2,7 +2,7 @@
  * plugins/app.js
  *
  * Automatically loads and bootstraps files
- * in the `./src/components/app` folder.
+ * in the `./src/components/` folder.
  */
 
 // Imports
@@ -13,8 +13,6 @@ const requireComponent = require.context('@/components', true, /\.vue$/)
 
 for (const file of requireComponent.keys()) {
   const componentConfig = requireComponent(file)
-
-  console.log(componentConfig.default.name)
 
   Vue.component(
     componentConfig.default.name,
