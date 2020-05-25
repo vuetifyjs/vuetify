2277dd7b0effb27a91ae3ba2709a57e0334ae386
Kael <kaelwd@gmail.com>
2020-05-26 01:43:10 +1000

    docs: add click-outside directive page

---
+++ packages/docs/src/examples/click-outside/usage.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list>
+    <v-list-item v-click-outside="() => clickOutside1 = false" @click="clickOutside1 = true">
+      <v-list-item-title>Click outside - handler only</v-list-item-title>
+      <v-list-item-action>
+        <v-icon :color="clickOutside1 ? 'green' : 'red'">mdi-record</v-icon>
+      </v-list-item-action>
+    </v-list-item>
+
+    <v-list-item v-click-outside="{ handler: () => clickOutside2 = false, closeConditional }" @click="clickOutside2 = true">
+      <v-list-item-title>Click outside - with closeConditional</v-list-item-title>
+      <v-list-item-action>
+        <v-icon :color="clickOutside2 ? 'green' : 'red'">mdi-record</v-icon>
+      </v-list-item-action>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      clickOutside1: true,
+      clickOutside2: true,
+    }),
+    methods: {
+      closeConditional (e) {
+        return this.clickOutside2
+      },
+    },
+  }
+</script>
