2b6d124964f199bd6bec8de90fefd894a7195639
John Leider <john.j.leider@gmail.com>
2020-05-26 19:18:27 -0500

    style(VClickOutside): add examples, update language, clean-up code styling

--- packages/docs/src/examples/click-outside/usage.vue
+++ packages/docs/src/examples/click-outside/usage.vue
@@ -1,30 +1,30 @@
-<template>
-  <v-list>
-    <v-list-item v-click-outside="() => clickOutside1 = false" @click="clickOutside1 = true">
-      <v-list-item-title>Click outside - handler only</v-list-item-title>
-      <v-list-item-action>
-        <v-icon :color="clickOutside1 ? 'green' : 'red'">mdi-record</v-icon>
-      </v-list-item-action>
-    </v-list-item>
 
-    <v-list-item v-click-outside="{ handler: () => clickOutside2 = false, closeConditional }" @click="clickOutside2 = true">
-      <v-list-item-title>Click outside - with closeConditional</v-list-item-title>
-      <v-list-item-action>
-        <v-icon :color="clickOutside2 ? 'green' : 'red'">mdi-record</v-icon>
-      </v-list-item-action>
-    </v-list-item>
-  </v-list>
+<template>
+  <v-card
+    v-click-outside="onClickOutside"
+    :color="active ? 'primary' : undefined"
+    :dark="active"
+    class="mx-auto"
+    height="256"
+    rounded="xl"
+    width="256"
+    @click="active = true"
+  >
+    <div class="text-h6 text-md-h4 fill-height d-flex align-center justify-center">
+      {{ active ? 'Click Outside' : 'Click Me' }}
+    </div>
+  </v-card>
 </template>
 
 <script>
   export default {
     data: () => ({
-      clickOutside1: true,
-      clickOutside2: true,
+      active: false,
     }),
+
     methods: {
-      closeConditional (e) {
-        return this.clickOutside2
+      onClickOutside () {
+        this.active = false
       },
     },
   }
