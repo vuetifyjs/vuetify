246778d0092bf19807b16ffba1828bfe3d0274c5
MajesticPotatoe <amajesticpotatoe@gmail.com>
2020-05-29 13:42:08 -0400

    docs: add placeholder usage component

---
+++ packages/docs-next/src/components/Usage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-alert
+    text
+    border="left"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </v-alert>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+  }
+</script>
