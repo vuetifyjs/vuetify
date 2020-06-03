b55e7807464068c5b3c263b43218d0f196671336
MajesticPotatoe <amajesticpotatoe@gmail.com>
2020-06-03 18:43:11 -0400

    docs: update alert component

---
+++ packages/docs-next/src/examples/v-alert/prop-transition.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="text-center mb-4">
+      <v-btn
+        color="primary"
+        @click="alert = !alert"
+      >
+        Toggle
+      </v-btn>
+    </div>
+    <v-alert
+      :value="alert"
+      color="pink"
+      dark
+      border="top"
+      icon="mdi-home"
+      transition="scale-transition"
+    >
+      Phasellus tempus. Fusce ac felis sit amet ligula pharetra condimentum. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Pellentesque posuere. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo.
+
+      Phasellus nec sem in justo pellentesque facilisis. Phasellus magna. Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo. In hac habitasse platea dictumst. Praesent turpis.
+    </v-alert>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        alert: true,
+      }
+    },
+  }
+</script>
--- packages/docs-next/src/examples/v-alert/simple/dense.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-alert
-    dense
-    type="info"
-  >
-    I'm a dense alert with a <strong>type</strong> of info
-  </v-alert>
-</template>
