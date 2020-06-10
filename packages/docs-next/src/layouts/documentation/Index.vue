73138d96ee63dfcde7e6843882128c0547543df0
John Leider <john.j.leider@gmail.com>
2020-06-10 14:40:00 -0500

    docs(DocumentationLayout): automatically add unspecified nav items to drawer

--- packages/docs-next/src/layouts/documentation/Index.vue
+++ packages/docs-next/src/layouts/documentation/Index.vue
@@ -70,38 +70,78 @@
         ] = [...await Promise.all(pending)]
 
         this.pages = { ...pages.default, ...api.default }
-        this.nav = nav.map(item => this.genItem(item, `/${val}/`))
+        // Map provided nav groups using
+        // the provided language, val
+        this.nav = nav.map(item => {
+          // Build group string used
+          // for list groups
+          // e.g. /en/
+          const group = `/${val}/`
+
+          return this.genItem(item, group)
+        })
         this.tocs = toc.default
       },
       findItems (group) {
         const pages = []
 
+        // Iterate through the imported pages and
+        // map keys to the generated page routes
+        // e.g. /en/components/chip-groups/
         for (const key in this.pages) {
-          if (!key.startsWith(group) || group === key) continue
+          // Skip if the key doesn't match
+          // e.g. !'/en/components/alerts'.startsWith('/en/')
+          if (!key.startsWith(group)) continue
 
+          // Create a new inferred page route
+          // using the key/value from pages
           pages.push({
+            href: undefined,
+            icon: undefined,
+            items: undefined,
             title: this.pages[key],
             to: key,
           })
         }
 
-        return pages.length > 0 ? pages : undefined
+        return pages.length ? pages : undefined
       },
       genItems (item, ...args) {
-        return item.items
-          ? item.items.map(i => this.genItem(i, ...args))
-          : this.findItems(...args)
-      },
-      genItem (item, parent) {
-        const path = kebabCase(item.to || item.title)
-        const to = `${parent}${path}/`
-        let title = this.pages[to]
+        const foundItems = this.findItems(...args)
 
-        if (!title) {
-          title = this.$i18n.te(item.title)
+        if (!item.items) return foundItems
+
+        const keys = []
+        const items = []
+
+        // Generate explicitly provided
+        // items and their keys from src/data/nav.json
+        for (const child of item.items) {
+          keys.push(child.title)
+          items.push(this.genItem(child, ...args))
+        }
+
+        for (const found of foundItems) {
+          const title = kebabCase(found.title)
+
+          // Don't include found item
+          // if already provided
+          if (keys.includes(title)) continue
+
+          keys.push(title)
+          items.push(found)
+        }
+
+        return items.length ? items : undefined
+      },
+      genItem (item, group) {
+        const path = kebabCase(item.to || item.title)
+        const to = `${group}${path}/`
+        const title = this.pages[to] || (
+          this.$i18n.te(item.title)
             ? this.$i18n.t(item.title)
             : item.title
-        }
+        )
 
         return {
           href: item.href || undefined,
