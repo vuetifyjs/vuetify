e933e8007505deea6dbc629a6e625100778d19ec
MajesticPotatoe <amajesticpotatoe@gmail.com>
2020-06-17 15:22:37 -0400

    docs(API): add dummy api files for lang conversions

--- packages/docs-next/build/api-gen/index.js
+++ packages/docs-next/build/api-gen/index.js
@@ -1,13 +1,42 @@
 // Imports
-const { readFileSync } = require('fs')
+const { readFileSync, readdirSync } = require('fs')
 const { resolve } = require('path')
 
 // pseudo code to mock api-gen
 function generateAPI (comp, locale) {
   const api = {}
-  const compMap = require(`./maps/${comp}`)[comp]
+
+  // defaults if file doesn't exist
+  let compMap = {
+    props: [{ name: 'empty', type: '', default: '', source: '' }],
+    mixins: ['empty'],
+    slots: [{ name: 'empty', props: '' }],
+    events: [{ name: 'empty', value: '' }],
+    functions: [{ name: 'empty', signature: '' }],
+    sass: [{ name: 'empty', default: '' }],
+  }
+  let compLocale = {
+    props: { empty: '' },
+    slots: { empty: '' },
+    events: { empty: '' },
+    functions: { empty: '' },
+    sass: { empty: '' },
+  }
+
+  try {
+    const mapFile = require(`./maps/${comp}`)[comp]
+    compMap = mapFile
+    console.log(comp)
+  } catch (e) {
+    console.log(`map: ${comp} not found`)
+  }
   const localePath = resolve(__dirname, `./locale/${locale}/${comp}.json`)
-  const compLocale = JSON.parse(readFileSync(localePath, 'utf8'))
+  try {
+    const localeFile = JSON.parse(readFileSync(localePath, 'utf8'))
+    compLocale = localeFile
+  } catch (e) {
+    console.log(`locale: ${locale} not found`)
+  }
 
   for (const [section, sValue] of Object.entries(compMap)) {
     const items = []
@@ -31,7 +60,13 @@ function generateAPI (comp, locale) {
 }
 
 function generateCompList () {
-  return ['v-alert']
+  const mapPath = resolve(__dirname, './locale/en')
+  const files = readdirSync(mapPath, 'utf8')
+  const compList = []
+  for (const item of files) {
+    compList.push(item.split('.')[0])
+  }
+  return compList
 }
 
 function generateLocaleList () {
