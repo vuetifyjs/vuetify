069791d81c48f3c42d759d5f89269bc391600749
John Leider <john.j.leider@gmail.com>
2020-06-22 07:35:11 -0500

    docs(Alert): add support for md

--- packages/docs-next/src/components/Alert.vue
+++ packages/docs-next/src/components/Alert.vue
@@ -2,14 +2,22 @@
   <v-alert
     :class="$vuetify.theme.dark ? undefined : 'grey lighten-5'"
     border="left"
+    class="v-alert--md"
     colored-border
     v-bind="$attrs"
     v-on="$listeners"
   >
-    <slot />
+    <app-md><slot /></app-md>
   </v-alert>
 </template>
 
 <script>
   export default { name: 'Alert' }
 </script>
+
+<style lang="sass">
+  .v-alert--md .v-markdown
+    p:last-child
+      margin-bottom: 0
+
+</style>
