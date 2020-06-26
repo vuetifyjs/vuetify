9a3a8972b73802189ddb19e8963b84708d0cb015
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-06-26 19:14:29 +0300

    docs(VChip): migrate the page (#40)
    * docs(VChip): migrate the page
    
    * docs(VChip): add relates, alphabetize
    
    * docs(VChip): update usage
    
    Co-authored-by: MajesticPotatoe <amajesticpotatoe@gmail.com>

---
+++ packages/docs-next/src/examples/v-chip/prop-closable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="text-center">
+    <v-btn
+      v-if="!chip1 && !chip2 && !chip3 && !chip4"
+      close
+      color="primary"
+      dark
+      @click="chip1 = true, chip2 = true, chip3 = true, chip4= true"
+    >
+      Reset Chips
+    </v-btn>
+
+    <v-chip
+      v-if="chip1"
+      class="ma-2"
+      close
+      @click:close="chip1 = false"
+    >
+      Closable
+    </v-chip>
+
+    <v-chip
+      v-if="chip2"
+      class="ma-2"
+      close
+      color="red"
+      text-color="white"
+      @click:close="chip2 = false"
+    >
+      Remove
+    </v-chip>
+
+    <v-chip
+      v-if="chip3"
+      class="ma-2"
+      close
+      color="green"
+      outlined
+      @click:close="chip3 = false"
+    >
+      Success
+    </v-chip>
+
+    <v-chip
+      v-if="chip4"
+      class="ma-2"
+      close
+      color="orange"
+      label
+      outlined
+      @click:close="chip4 = false"
+    >
+      Complete
+    </v-chip>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        chip1: true,
+        chip2: true,
+        chip3: true,
+        chip4: true,
+      }
+    },
+  }
+</script>
---
+++ packages/docs-next/src/examples/v-chip/prop-colored.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center">
+    <v-chip
+      class="ma-2"
+    >
+      Default
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="primary"
+    >
+      Primary
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="secondary"
+    >
+      Secondary
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="red"
+      text-color="white"
+    >
+      Red Chip
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="green"
+      text-color="white"
+    >
+      Green Chip
+    </v-chip>
+  </div>
+</template>
