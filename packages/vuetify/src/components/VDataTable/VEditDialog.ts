ed444629da25ed4323f9e199bc0e3371224e2faf
Adam Lynch <adamlynchmob@gmail.com>
2020-06-27 17:27:57 -0400

    fix(veditdialog): fixes bug where save returns input value
    Fixes a bug where save returns the input value instead of the returnValue. This also updates the
    test to ensure cancel and save are working as intended.
    
    fix #11736

--- packages/vuetify/src/components/VDataTable/VEditDialog.ts
+++ packages/vuetify/src/components/VDataTable/VEditDialog.ts
@@ -88,10 +88,9 @@ export default mixins(Returnable, Themeable).extend({
         staticClass: 'v-small-dialog__content',
         on: {
           keydown: (e: KeyboardEvent) => {
-            const input = (this.$refs.content as Element).querySelector('input')
             e.keyCode === keyCodes.esc && this.cancel()
-            if (e.keyCode === keyCodes.enter && input) {
-              this.save(input.value)
+            if (e.keyCode === keyCodes.enter) {
+              this.save(this.returnValue)
               this.$emit('save')
             }
           },
