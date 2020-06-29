b88d7176c08d7478364ec888fa8c3b9a01b1989b
John Leider <john.j.leider@gmail.com>
2020-06-29 16:11:24 -0500

    docs(accessibility.md): port page from old docs

---
+++ packages/docs-next/src/examples/accessibility/menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="text-center">
+    <v-menu>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click me
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item @click="onClick">
+          <v-list-item-title>Option 1</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item disabled>
+          <v-list-item-title>Option 2</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item @click="onClick">
+          <v-list-item-title>Option 3</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onClick () {
+        // Perform an action
+      },
+    },
+  }
+</script>
