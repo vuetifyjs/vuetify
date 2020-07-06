41bfe072458734789c421ae626fe90c886a02ad1
Valentin Gavran <valentin.gavran.99@gmail.com>
2020-07-06 18:40:47 +0200

    docs(data-tables): migrate page (#94)
    * docs(contributing): migrate page
    
    * docs(contributing): fix related section
    
    * docs(data-tables): import examples
    
    * docs(data-tables): change category of few examples
    
    From "prop" to "misc"
    
    * docs(data-tables): rename example file for better understanding
    
    * docs(data-tables): migrate page
    
    * docs(data-tables): delete unused example file
    
    * docs(data-tables): import needed example icons and use them
    
    * Revert "docs(data-tables): import needed example icons and use them"
    
    This reverts commit 4b06c543ae2b0f3bbc6163f677902706fd4c945a.
    
    * docs(data-tables): lint fix
    
    * docs(data-tables): rename example folder
    
    * docs(data-tables): organize and cleanup examples
    
    * docs(api): migrate data-table lang
    
    Co-authored-by: MajesticPotatoe <amajesticpotatoe@gmail.com>

---
+++ packages/docs-next/src/examples/v-data-table/misc-server-side-paginate-and-sort.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :options.sync="options"
+      :server-items-length="totalDesserts"
+      :loading="loading"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        totalDesserts: 0,
+        desserts: [],
+        loading: true,
+        options: {},
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+      }
+    },
+    watch: {
+      options: {
+        handler () {
+          this.getDataFromApi()
+            .then(data => {
+              this.desserts = data.items
+              this.totalDesserts = data.total
+            })
+        },
+        deep: true,
+      },
+    },
+    mounted () {
+      this.getDataFromApi()
+        .then(data => {
+          this.desserts = data.items
+          this.totalDesserts = data.total
+        })
+    },
+    methods: {
+      getDataFromApi () {
+        this.loading = true
+        return new Promise((resolve, reject) => {
+          const { sortBy, sortDesc, page, itemsPerPage } = this.options
+
+          let items = this.getDesserts()
+          const total = items.length
+
+          if (sortBy.length === 1 && sortDesc.length === 1) {
+            items = items.sort((a, b) => {
+              const sortA = a[sortBy[0]]
+              const sortB = b[sortBy[0]]
+
+              if (sortDesc[0]) {
+                if (sortA < sortB) return 1
+                if (sortA > sortB) return -1
+                return 0
+              } else {
+                if (sortA < sortB) return -1
+                if (sortA > sortB) return 1
+                return 0
+              }
+            })
+          }
+
+          if (itemsPerPage > 0) {
+            items = items.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+          }
+
+          setTimeout(() => {
+            this.loading = false
+            resolve({
+              items,
+              total,
+            })
+          }, 1000)
+        })
+      },
+      getDesserts () {
+        return [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ]
+      },
+    },
+  }
+</script>
