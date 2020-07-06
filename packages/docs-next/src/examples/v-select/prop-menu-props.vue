6e15f1c3c2686a46facbe0305221e6f79cfdded2
paichi81 <paichi81@mac.com>
2020-07-06 23:07:33 +0900

    docs(selects): migrate page (#88)
    * docs(Selects): migrate page
    
    * docs(selects): cleanup examples
    
    Co-authored-by: MajesticPotatoe <amajesticpotatoe@gmail.com>

---
+++ packages/docs-next/src/examples/v-select/prop-menu-props.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row align="center">
+    <v-col cols="12">
+      <v-select
+        :items="items"
+        :menu-props="{ top: true, offsetY: true }"
+        label="Label"
+      ></v-select>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+    }),
+  }
+</script>
