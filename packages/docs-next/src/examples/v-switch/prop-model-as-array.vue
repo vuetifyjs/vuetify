430deaa3529f5886a28eafdea31f2041c97f690d
FoxyHutch <kester.lotz@gmail.com>
2020-07-08 02:00:15 +0200

    docs(selection-controls): migrate page (#113)
    * docs(selection-controls): docs migrated
    
    * docs(checkbox): split from Selection Controlo
    
    * docs(simple-checkbox): split from selection control
    
    * docs(radio): break up selection control
    
    * docs(radio-groups): break up selection control
    
    * docs(checkbox): update slot example
    
    * docs(radio-group): add radio as sub component
    
    * docs(switch): break up selection control
    
    * docs(selection-controls): cleanup selection controls and simple checkbox
    
    * docs(radio): edit radio group and radio slot examples
    
    * docs(nav): update nav drawer pages
    
    * docs(radio-groups): update page
    
    * docs(radio-buttons): combine radio and radio-group
    
    * docs(switch): update page and examples
    
    * docs(checkboxes): update page
    
    Co-authored-by: Kester Lotz <kester.lotz@posteo.de>
    Co-authored-by: MajesticPotatoe <amajesticpotatoe@gmail.com>

---
+++ packages/docs-next/src/examples/v-switch/prop-model-as-array.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <p>{{ people }}</p>
+    <v-switch
+      v-model="people"
+      label="John"
+      value="John"
+    ></v-switch>
+    <v-switch
+      v-model="people"
+      label="Jacob"
+      value="Jacob"
+    ></v-switch>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        people: ['John'],
+      }
+    },
+  }
+</script>
