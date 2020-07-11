2221c482c55614923ba7717fc7ea968536db94ea
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-07-11 19:02:12 +0300

    docs(professional-support): migrate business page (#131)

---
+++ packages/docs-next/src/components/doc/BecomeSponsor.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    large
+    href="https://github.com/users/johnleider/sponsorship"
+  >
+    <v-icon left>
+      $mdiGithub
+    </v-icon>
+    {{ $i18n.t('become-a-sponsor') }}
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    name: 'BecomeSponsor',
+  }
+</script>
