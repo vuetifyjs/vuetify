1030f1d4594ade57f9acbc65dd0e79445139d1d2
John Leider <john.j.leider@gmail.com>
2020-07-20 19:16:10 -0500

    chore(i18n): add root index for messages
    needed a better standard for handling vue-i18n

--- packages/docs-next/src/i18n/index.js
+++ packages/docs-next/src/i18n/index.js
@@ -1,6 +1,9 @@
+// Imports
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import messages from '@/i18n/messages/en'
+
+// Messages
+import { en } from '@/i18n/messages'
 
 Vue.use(VueI18n)
 
@@ -9,9 +12,7 @@ export function createI18n () {
   const i18n = new VueI18n({
     locale: 'en',
     fallbackLocale: 'en',
-    messages: {
-      en: messages,
-    },
+    messages: { en },
   })
 
   return i18n
