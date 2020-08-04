c98ad8253871e2213d40b179a4e379312eabb502
MajesticPotatoe <amajesticpotatoe@gmail.com>
2020-08-03 20:38:13 -0400

    docs(examples): fix textareas signup form error

--- packages/docs-next/src/examples/v-textarea/misc-signup-form.vue
+++ packages/docs-next/src/examples/v-textarea/misc-signup-form.vue
@@ -166,9 +166,9 @@
       password: undefined,
       phone: undefined,
       rules: {
-        email: v => (v || '').match(/@/) || 'Please enter a valid email',
+        email: v => !!(v || '').match(/@/) || 'Please enter a valid email',
         length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
-        password: v => (v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
+        password: v => !!(v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
           'Password must contain an upper case letter, a numeric character, and a special character',
         required: v => !!v || 'This field is required',
       },
