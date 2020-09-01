9cf5e3ab11cff21a8ebf996eb08f1f70012adb72
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2020-09-01 15:38:59 -0400

    docs(locales): remove untranslated langs

--- packages/docs/src/i18n/locales.js
+++ packages/docs/src/i18n/locales.js
@@ -4,36 +4,6 @@ module.exports = [
     locale: 'en',
   },
   {
-    title: 'Deutsch',
-    locale: 'de-DE',
-    alternate: 'de',
-  },
-  {
-    title: 'Français',
-    locale: 'fr-FR',
-    alternate: 'fr',
-  },
-  {
-    title: 'Русский',
-    locale: 'ru-RU',
-    alternate: 'ru',
-  },
-  {
-    title: '简体中文',
-    locale: 'zh-CN',
-    alternate: 'zh-Hans',
-  },
-  {
-    title: '한국어',
-    locale: 'ko-KR',
-    alternate: 'ko',
-  },
-  {
-    title: '日本語',
-    locale: 'ja-JP',
-    alternate: 'ja',
-  },
-  {
     title: 'Help translate',
     locale: 'eo-UY',
   },
