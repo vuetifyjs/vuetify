34d81fb22697e90a6f2ea84eade18ac807de6607
Soham Dev <sohamdevs@yahoo.com>
2020-09-13 06:59:14 +0530

    docs: removed hard coded color
    The toolbar background in "CRUD Actions" section in "en\components\data-tables" is hard coded with
    "white", which is not respecting dark theme. Removed it.

--- packages/docs/src/examples/data-tables/complex/crud.vue
+++ packages/docs/src/examples/data-tables/complex/crud.vue
@@ -6,7 +6,7 @@
     class="elevation-1"
   >
     <template v-slot:top>
-      <v-toolbar flat color="white">
+      <v-toolbar flat>
         <v-toolbar-title>My CRUD</v-toolbar-title>
         <v-divider
           class="mx-4"
