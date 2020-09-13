668d8903b56b2d385d74e424abc3e0980f77461a
Soham Dev <sohamdevs@yahoo.com>
2020-09-13 14:43:48 +0530

    docs: fixed broken icons

--- packages/docs/src/examples/v-card/misc-twitter-card.vue
+++ packages/docs/src/examples/v-card/misc-twitter-card.vue
@@ -23,7 +23,8 @@
       <v-list-item class="grow">
         <v-list-item-avatar color="grey darken-3">
           <v-img
-            class="elevation-6" alt=""
+            class="elevation-6"
+            alt=""
             src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
           ></v-img>
         </v-list-item-avatar>
