644bb7b3153f8c427d2dbfecd2c6244e64d5f862
John Leider <john.j.leider@gmail.com>
2020-09-22 16:01:31 -0500

    docs(prop-two-line-and-subheader.vue): add missing icons

--- packages/docs/src/examples/v-list/prop-two-line-and-subheader.vue
+++ packages/docs/src/examples/v-list/prop-two-line-and-subheader.vue
@@ -23,65 +23,62 @@
     </v-toolbar>
 
     <v-list
-      two-line
       subheader
+      two-line
     >
-      <v-subheader inset>
-        Folders
-      </v-subheader>
+      <v-subheader inset>Folders</v-subheader>
 
       <v-list-item
-        v-for="item in items"
-        :key="item.title"
+        v-for="folder in folders"
+        :key="folder.title"
       >
         <v-list-item-avatar>
           <v-icon
-            :class="[item.iconClass]"
-            v-text="item.icon"
-          ></v-icon>
+            class="grey lighten-1"
+            dark
+          >
+            mdi-folder
+          </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+          <v-list-item-title v-text="folder.title"></v-list-item-title>
+
+          <v-list-item-subtitle v-text="folder.subtitle"></v-list-item-subtitle>
         </v-list-item-content>
 
         <v-list-item-action>
           <v-btn icon>
-            <v-icon color="grey lighten-1">
-              mdi-information
-            </v-icon>
+            <v-icon color="grey lighten-1">mdi-information</v-icon>
           </v-btn>
         </v-list-item-action>
       </v-list-item>
 
       <v-divider inset></v-divider>
 
-      <v-subheader inset>
-        Files
-      </v-subheader>
+      <v-subheader inset>Files</v-subheader>
 
       <v-list-item
-        v-for="item in items2"
-        :key="item.title"
+        v-for="file in files"
+        :key="file.title"
       >
         <v-list-item-avatar>
           <v-icon
-            :class="[item.iconClass]"
-            v-text="item.icon"
+            :class="file.color"
+            dark
+            v-text="file.icon"
           ></v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+          <v-list-item-title v-text="file.title"></v-list-item-title>
+
+          <v-list-item-subtitle v-text="file.subtitle"></v-list-item-subtitle>
         </v-list-item-content>
 
         <v-list-item-action>
           <v-btn icon>
-            <v-icon color="grey lighten-1">
-              mdi-information
-            </v-icon>
+            <v-icon color="grey lighten-1">mdi-information</v-icon>
           </v-btn>
         </v-list-item-action>
       </v-list-item>
@@ -92,14 +89,33 @@
 <script>
   export default {
     data: () => ({
-      items: [
-        { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Photos', subtitle: 'Jan 9, 2014' },
-        { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Recipes', subtitle: 'Jan 17, 2014' },
-        { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Work', subtitle: 'Jan 28, 2014' },
+      files: [
+        {
+          color: 'blue',
+          icon: 'mdi-clipboard-text',
+          subtitle: 'Jan 20, 2014',
+          title: 'Vacation itinerary',
+        },
+        {
+          color: 'amber',
+          icon: 'mdi-gesture-tap-button',
+          subtitle: 'Jan 10, 2014',
+          title: 'Kitchen remodel',
+        },
       ],
-      items2: [
-        { icon: 'assignment', iconClass: 'blue white--text', title: 'Vacation itinerary', subtitle: 'Jan 20, 2014' },
-        { icon: 'call_to_action', iconClass: 'amber white--text', title: 'Kitchen remodel', subtitle: 'Jan 10, 2014' },
+      folders: [
+        {
+          subtitle: 'Jan 9, 2014',
+          title: 'Photos',
+        },
+        {
+          subtitle: 'Jan 17, 2014',
+          title: 'Recipes',
+        },
+        {
+          subtitle: 'Jan 28, 2014',
+          title: 'Work',
+        },
       ],
     }),
   }
