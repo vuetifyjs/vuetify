f89ff1a5150906608a4133e07cc6b39f507c1c4c
John Leider <john.j.leider@gmail.com>
2020-09-23 23:41:46 -0500

    docs(TideliftImg): update properties, set default height

--- packages/docs/src/components/doc/support/tidelift/TideliftImg.vue
+++ packages/docs/src/components/doc/support/tidelift/TideliftImg.vue
@@ -2,8 +2,9 @@
   <app-img
     alt="Vuetify + Tidelift"
     class="mb-8 mx-auto"
+    contain
+    height="210"
     src="https://cdn.vuetifyjs.com/images/affiliates/vuetify-tidelift.png"
-    width="250"
   />
 </template>
 
