8e8d20bc187636c3cb8d103e7b54cbc8b395b214
John Leider <john.j.leider@gmail.com>
2020-09-25 13:44:57 -0500

    docs(DefaultDrawer): scroll active list-item into view on load

--- packages/docs/src/layouts/default/Drawer.vue
+++ packages/docs/src/layouts/default/Drawer.vue
@@ -17,6 +17,7 @@
     <keep-alive>
       <default-list
         :key="key"
+        v-mutate.once.child="onMutate"
         :items="items"
       />
     </keep-alive>
@@ -26,13 +27,16 @@
 </template>
 
 <script>
-  // Utilities
-  import { get, sync } from 'vuex-pathify'
-
   // Components
   import DefaultBecomeASponsor from './BecomeASponsor'
   import DefaultDrawerPrepend from './DrawerPrepend'
   import DefaultList from './List'
+
+  // Utilities
+  import { get, sync } from 'vuex-pathify'
+
+  // Data
+  import { IN_BROWSER } from '@/util/globals'
 
   export default {
     name: 'DefaultDrawer',
@@ -59,6 +63,55 @@
       },
       items () {
         return !this.ualphabetical ? this.nav : this.alphabetical
+      },
+    },
+
+    methods: {
+      findActiveItem (children = []) {
+        return [...children].reduce((acc, cur) => {
+          // If we've found an item, just return it
+          if (acc) {
+            return acc
+          // If the current item is active, return as accumulator
+          } else if (cur.classList.contains('v-list-item--active')) {
+            return cur
+          }
+
+          return this.findActiveItem(cur.children)
+        }, null)
+      },
+      onMutate (mutationsList, observer) {
+        // Cancel if drawer is not visible
+        if (
+          !this.drawer ||
+          !IN_BROWSER ||
+          typeof document.documentElement.scrollIntoView !== 'function'
+        ) return
+
+        /* eslint-disable no-labels */
+        found:
+          // Iterate through all mutated items
+          for (const record of mutationsList) {
+            const nodes = [...record.addedNodes]
+
+            // Find the active list group
+            for (const node of nodes) {
+              if (
+                !node.classList.contains('v-list-group--active') ||
+                node.children.length < 2
+              ) continue
+
+              // Find the active child
+              const child = this.findActiveItem(node.children[1].children)
+
+              if (!child) continue
+
+              child.scrollIntoView()
+
+              // Break loop
+              break found
+            }
+          }
       },
     },
   }
