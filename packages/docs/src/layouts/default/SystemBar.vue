c20f485cad333fee89b3919ab642b9b67cb95809
John Leider <john.j.leider@gmail.com>
2020-09-26 01:48:53 -0500

    docs(DefaultSystemBar): add close icon

--- packages/docs/src/layouts/default/SystemBar.vue
+++ packages/docs/src/layouts/default/SystemBar.vue
@@ -1,11 +1,16 @@
 <template>
   <v-system-bar
+    v-if="bar"
     app
     class="justify-center"
     color="grey darken-4"
     dark
     window
   >
+    <v-responsive max-width="24" />
+
+    <v-spacer />
+
     <a
       class="text-subtitle-1 white--text text-decoration-none text-body-2"
       href="https://v2.vuetifyjs.com"
@@ -16,9 +21,19 @@
 
       <v-icon small>$mdiOpenInNew</v-icon>
     </a>
+
+    <v-spacer />
+
+    <v-icon @click="bar = false">
+      $close
+    </v-icon>
   </v-system-bar>
 </template>
 
 <script>
-  export default { name: 'HomeSystemBar' }
+  export default {
+    name: 'HomeSystemBar',
+
+    data: () => ({ bar: true }),
+  }
 </script>
