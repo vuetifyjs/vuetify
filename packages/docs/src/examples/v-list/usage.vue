f211f5792b422fa406494201228c024db795c0fa
AkioSarkiz <42392852+AkioSarkiz@users.noreply.github.com>
2020-10-05 18:44:18 +0300

    Update usage.vue

--- packages/docs/src/examples/v-list/usage.vue
+++ packages/docs/src/examples/v-list/usage.vue
@@ -1,32 +1,36 @@
-<template>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-    tile
-  >
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Single-line item</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+<div id="app">
+  <v-app id="inspire">
+    <v-card
+      class="mx-auto"
+      max-width="400"
+      tile
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Single-line item</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title>Two-line item</v-list-item-title>
-        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Two-line item</v-list-item-title>
+            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title>Three-line item</v-list-item-title>
-        <v-list-item-subtitle>
-          Secondary line text Lorem ipsum dolor sit amet,
-        </v-list-item-subtitle>
-        <v-list-item-subtitle>
-          consectetur adipiscing elit.
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-  </v-card>
-</template>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>Three-line item</v-list-item-title>
+            <v-list-item-subtitle>
+              Secondary line text Lorem ipsum dolor sit amet,
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              consectetur adipiscing elit.
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-app>
+</div>
