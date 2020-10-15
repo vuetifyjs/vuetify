32253e0a8d8b844d7f7e2f2e7e6b134486b41c15
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-10-15 10:56:14 +0300

    refactor(resize): update for v3 (#10419)
    Co-authored-by: John Leider <john@vuetifyjs.com>
    Co-authored-by: Kael <kaelwd@gmail.com>

--- packages/vuetify/src/directives/index.ts
+++ packages/vuetify/src/directives/index.ts
@@ -2,7 +2,7 @@
 // export { Color } from './color'
 export { Intersect } from './intersect'
 // export { Mutate } from './mutate'
-// export { Resize } from './resize'
+export { Resize } from './resize'
 // export { Ripple } from './ripple'
 // export { Scroll } from './scroll'
 // export { Touch } from './touch'
