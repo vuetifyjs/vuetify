c3e184e6e6b0d5a45c617989d5525917ca8cb121
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-10-16 09:13:34 +0200

    docs: fix tab-items example

--- packages/docs/src/examples/v-tabs/misc-tab-items.vue
+++ packages/docs/src/examples/v-tabs/misc-tab-items.vue
@@ -11,18 +11,17 @@
       >
         {{ item.tab }}
       </v-tab>
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item.tab"
+        >
+          <v-card flat>
+            <v-card-text>{{ item.content }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
     </v-tabs>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-        v-for="item in items"
-        :key="item.tab"
-      >
-        <v-card flat>
-          <v-card-text>{{ item.content }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
   </v-card>
 </template>
 
