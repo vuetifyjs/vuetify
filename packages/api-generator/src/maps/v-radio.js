b1dd188691092f876002cc9cd062239c932cf292
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-10-16 10:25:57 +0200

    docs: remove append and prepend slot from VRadio

--- packages/api-generator/src/maps/v-radio.js
+++ packages/api-generator/src/maps/v-radio.js
@@ -1,5 +1,7 @@
 const { VInput } = require('../helpers/variables')
 
+VInput.slots = VInput.slots.filter(slot => slot.name !== 'append' && slot.name !== 'prepend')
+
 module.exports = {
   'v-radio': {
     ...VInput,
