fd655168c698fc8d688f1e08cdf89573ed66073c
nekosaur <albert@kaaman.nu>
2020-10-23 23:06:39 +0200

    chore: move composable, update tests

---
+++ packages/vuetify/src/composables/__tests__/size.spec.ts
@@ -0,0 +1,37 @@
+import { mount } from '@vue/test-utils'
+import { makeSizeProps, useSizeClasses } from '../size'
+
+describe('size', () => {
+  it('should warn if value is incorrect', () => {
+    const TestComponent = {
+      props: makeSizeProps(),
+      template: '<div/>',
+    }
+
+    mount(TestComponent, {
+      props: {
+        // @ts-expect-error
+        size: 'foo',
+      },
+    })
+
+    expect('[Vue warn]: Invalid prop: custom validator check failed for prop "size".').toHaveBeenTipped()
+  })
+
+  it.each([
+    [undefined, null],
+    [null, null],
+    [0 as any, null],
+    ['' as any, null],
+    [{ size: undefined }, null],
+    [{ size: 'x-small' }, 'v-size--x-small'],
+    [{ size: 'small' }, 'v-size--small'],
+    [{ size: 'default' }, 'v-size--default'],
+    [{ size: 'large' }, 'v-size--large'],
+    [{ size: 'x-large' }, 'v-size--x-large'],
+  ])('should return the correct class given value %p', (input, expected) => {
+    const { sizeClasses } = useSizeClasses(input)
+
+    expect(sizeClasses.value).toStrictEqual(expected)
+  })
+})
