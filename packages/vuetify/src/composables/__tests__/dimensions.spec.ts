0be182ac6472926ca73dc94eaabb5c6e645ce07b
nekosaur <albert@kaaman.nu>
2020-10-24 10:08:27 +0200

    test: remove unnecessary describe

--- packages/vuetify/src/composables/__tests__/dimensions.spec.ts
+++ packages/vuetify/src/composables/__tests__/dimensions.spec.ts
@@ -4,144 +4,142 @@ import {
   dimensionsFactory,
 } from '../dimensions'
 
-describe('dimensions.ts', () => {
-  describe('dimensionsFactory', () => {
-    it('should take a single prop to use', () => {
-      const { useDimensions } = dimensionsFactory('width')
+describe('dimensionsFactory', () => {
+  it('should take a single prop to use', () => {
+    const { useDimensions } = dimensionsFactory('width')
 
-      const props = { width: 100 }
-      const { dimensions } = useDimensions(props)
+    const props = { width: 100 }
+    const { dimensions } = useDimensions(props)
 
-      expect(dimensions.value.style.width).toBeDefined()
-    })
+    expect(dimensions.value.style.width).toBeDefined()
+  })
 
-    it('should take a multiple props to use', () => {
-      const { useDimensions } = dimensionsFactory('width', 'height')
+  it('should take a multiple props to use', () => {
+    const { useDimensions } = dimensionsFactory('width', 'height')
 
-      const props = { width: 100, height: 200 }
-      const { dimensions } = useDimensions(props)
+    const props = { width: 100, height: 200 }
+    const { dimensions } = useDimensions(props)
 
-      expect(dimensions.value.style.width).toBeDefined()
-      expect(dimensions.value.style.height).toBeDefined()
-    })
+    expect(dimensions.value.style.width).toBeDefined()
+    expect(dimensions.value.style.height).toBeDefined()
+  })
 
-    it('should default to all props when given no props', () => {
-      const { useDimensions } = dimensionsFactory()
+  it('should default to all props when given no props', () => {
+    const { useDimensions } = dimensionsFactory()
 
-      const props = { width: 100, height: 200, maxWidth: 300, maxHeight: 400, minWidth: 500, minHeight: 600 }
-      const { dimensions } = useDimensions(props)
+    const props = { width: 100, height: 200, maxWidth: 300, maxHeight: 400, minWidth: 500, minHeight: 600 }
+    const { dimensions } = useDimensions(props)
 
-      expect(dimensions.value.style.width).toBeDefined()
-      expect(dimensions.value.style.height).toBeDefined()
-      expect(dimensions.value.style.maxWidth).toBeDefined()
-      expect(dimensions.value.style.maxHeight).toBeDefined()
-      expect(dimensions.value.style.minWidth).toBeDefined()
-      expect(dimensions.value.style.minHeight).toBeDefined()
+    expect(dimensions.value.style.width).toBeDefined()
+    expect(dimensions.value.style.height).toBeDefined()
+    expect(dimensions.value.style.maxWidth).toBeDefined()
+    expect(dimensions.value.style.maxHeight).toBeDefined()
+    expect(dimensions.value.style.minWidth).toBeDefined()
+    expect(dimensions.value.style.minHeight).toBeDefined()
+  })
+})
+
+describe('useDimensions', () => {
+  it.each([
+    [{ width: 100 }, { width: '100px' }],
+    [{ width: 100, height: '' }, { width: '100px' }],
+    [{ width: 100, height: null }, { width: '100px' }],
+    [{ width: 100, height: '200' }, { width: '100px', height: '200px' }],
+  ])('should have proper styles', (input, expected) => {
+    const { useDimensions } = dimensionsFactory()
+
+    expect(useDimensions(input).dimensions.value).toEqual({
+      style: expected,
     })
   })
 
-  describe('useDimensions', () => {
-    it.each([
-      [{ width: 100 }, { width: '100px' }],
-      [{ width: 100, height: '' }, { width: '100px' }],
-      [{ width: 100, height: null }, { width: '100px' }],
-      [{ width: 100, height: '200' }, { width: '100px', height: '200px' }],
-    ])('should have proper styles', (input, expected) => {
-      const { useDimensions } = dimensionsFactory()
+  it('should be reactive', () => {
+    const { useDimensions } = dimensionsFactory()
+    const props: { width?: number | string, height?: number | string } = reactive({ width: 100 })
+    const { dimensions } = useDimensions(props)
 
-      expect(useDimensions(input).dimensions.value).toEqual({
-        style: expected,
-      })
+    expect(dimensions.value).toEqual({
+      style: {
+        width: '100px',
+      },
     })
 
-    it('should be reactive', () => {
-      const { useDimensions } = dimensionsFactory()
-      const props: { width?: number | string, height?: number | string } = reactive({ width: 100 })
-      const { dimensions } = useDimensions(props)
+    props.width = 200
 
-      expect(dimensions.value).toEqual({
-        style: {
-          width: '100px',
-        },
-      })
+    expect(dimensions.value).toEqual({
+      style: {
+        width: '200px',
+      },
+    })
 
-      props.width = 200
+    delete props.width
 
-      expect(dimensions.value).toEqual({
-        style: {
-          width: '200px',
-        },
-      })
+    expect(dimensions.value).toEqual({
+      style: {},
+    })
 
-      delete props.width
+    props.height = 300
 
-      expect(dimensions.value).toEqual({
-        style: {},
-      })
+    expect(dimensions.value).toEqual({
+      style: {
+        height: '300px',
+      },
+    })
+  })
+})
 
-      props.height = 300
+describe('makeDimensionsProps', () => {
+  it('should have correct structure', () => {
+    expect(dimensionsFactory('width').makeDimensionsProps()).toEqual({
+      width: {
+        type: [Number, String],
+      },
+    })
 
-      expect(dimensions.value).toEqual({
-        style: {
-          height: '300px',
-        },
-      })
+    expect(dimensionsFactory('width', 'minWidth').makeDimensionsProps()).toEqual({
+      width: {
+        type: [Number, String],
+      },
+      minWidth: {
+        type: [Number, String],
+      },
+    })
+
+    expect(dimensionsFactory().makeDimensionsProps()).toEqual({
+      height: {
+        type: [Number, String],
+      },
+      width: {
+        type: [Number, String],
+      },
+      maxHeight: {
+        type: [Number, String],
+      },
+      maxWidth: {
+        type: [Number, String],
+      },
+      minWidth: {
+        type: [Number, String],
+      },
+      minHeight: {
+        type: [Number, String],
+      },
     })
   })
 
-  describe('makeDimensionsProps', () => {
-    it('should have correct structure', () => {
-      expect(dimensionsFactory('width').makeDimensionsProps()).toEqual({
-        width: {
-          type: [Number, String],
-        },
-      })
-
-      expect(dimensionsFactory('width', 'minWidth').makeDimensionsProps()).toEqual({
-        width: {
-          type: [Number, String],
-        },
-        minWidth: {
-          type: [Number, String],
-        },
-      })
-
-      expect(dimensionsFactory().makeDimensionsProps()).toEqual({
-        height: {
-          type: [Number, String],
-        },
-        width: {
-          type: [Number, String],
-        },
-        maxHeight: {
-          type: [Number, String],
-        },
-        maxWidth: {
-          type: [Number, String],
-        },
-        minWidth: {
-          type: [Number, String],
-        },
-        minHeight: {
-          type: [Number, String],
-        },
-      })
+  it('should allow setting default values', () => {
+    expect(dimensionsFactory('width').makeDimensionsProps({ width: 100 })).toEqual({
+      width: {
+        type: [Number, String],
+        default: 100,
+      },
     })
 
-    it('should allow setting default values', () => {
-      expect(dimensionsFactory('width').makeDimensionsProps({ width: 100 })).toEqual({
-        width: {
-          type: [Number, String],
-          default: 100,
-        },
-      })
-
-      expect(dimensionsFactory('width').makeDimensionsProps({ width: '200' })).toEqual({
-        width: {
-          type: [Number, String],
-          default: '200',
-        },
-      })
+    expect(dimensionsFactory('width').makeDimensionsProps({ width: '200' })).toEqual({
+      width: {
+        type: [Number, String],
+        default: '200',
+      },
     })
   })
 })
