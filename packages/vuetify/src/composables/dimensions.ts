76b17c36aa347985ce978fad089021fc4465aa80
Albert Kaaman <albert@kaaman.nu>
2020-10-24 11:45:15 +0200

    feat(dimensions): create composition function (#10410)
    Co-authored-by: Kael <kaelwd@gmail.com>

---
+++ packages/vuetify/src/composables/dimensions.ts
@@ -0,0 +1,62 @@
+// Setup
+import type { Prop } from 'vue'
+import { computed } from 'vue'
+import { convertToUnit } from '../util/helpers'
+
+// Props
+const allDimensionsProps = {
+  height: {
+    type: [Number, String],
+  },
+  maxHeight: {
+    type: [Number, String],
+  },
+  maxWidth: {
+    type: [Number, String],
+  },
+  minHeight: {
+    type: [Number, String],
+  },
+  minWidth: {
+    type: [Number, String],
+  },
+  width: {
+    type: [Number, String],
+  },
+}
+
+// Types
+type PropValue = string | number | null | undefined
+type PropNames = keyof typeof allDimensionsProps
+
+// Effect
+export function dimensionsFactory<S extends PropNames> (...possibleProps: S[]) {
+  const selectedProps = possibleProps.length ? possibleProps : Object.keys(allDimensionsProps) as S[]
+
+  const makeDimensionsProps = (defaults?: Partial<Record<S, PropValue>>) => selectedProps.reduce((obj, prop) => {
+    obj[prop] = {
+      ...allDimensionsProps[prop],
+      default: defaults?.[prop],
+    }
+    return obj
+  }, {} as Record<S, Prop<PropValue>>)
+
+  const useDimensions = (props: Partial<Record<S, PropValue>>) => {
+    const dimensions = computed(() => {
+      return selectedProps.reduce((obj, key) => {
+        const value: PropValue = props[key]
+        if (value) {
+          obj.style[key] = convertToUnit(value)
+        }
+        return obj
+      }, { style: {} } as { style: Record<S, string | undefined> })
+    })
+
+    return { dimensions }
+  }
+
+  return {
+    makeDimensionsProps,
+    useDimensions,
+  }
+}
