917c1c506b1424863e7e1fa0b1762618860da440
Kael <kaelwd@gmail.com>
2020-10-26 02:27:36 +1100

    refactor: use propsFactory in existing composables

--- packages/vuetify/src/composables/__tests__/elevation.spec.ts
+++ packages/vuetify/src/composables/__tests__/elevation.spec.ts
@@ -7,14 +7,14 @@ import {
 
 describe('elevation.ts', () => {
   it('should have the correct class', () => {
-    const values: [ElevationProps['elevation'], Record<string, boolean>][] = [
+    const values = [
       [1, { 'elevation-1': true }],
       [undefined, {}],
       [null, {}],
       [20, { 'elevation-20': true }],
       [0, { 'elevation-0': true }],
       ['14', { 'elevation-14': true }],
-    ]
+    ] as const
 
     for (const [elevation, equal] of values) {
       const props = { elevation }
@@ -29,6 +29,7 @@ describe('elevation.ts', () => {
   })
 
   it('should only allow numeric values between 0 and 24', () => {
+    // @ts-expect-error
     const { elevation: { validator } } = makeElevationProps()
     const validValues = [1, '24']
     const invalidValues = [-1, '25', false, true]
