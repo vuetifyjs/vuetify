5affdd7dfddfe7d735a0c4935ca81ce5308db7eb
Kael <kaelwd@gmail.com>
2020-10-31 01:31:22 +1100

    test(propsFactory): different type and default types

--- packages/vuetify/src/util/__tests__/propsFactory.spec.ts
+++ packages/vuetify/src/util/__tests__/propsFactory.spec.ts
@@ -3,6 +3,7 @@ import propsFactory from '../propsFactory'
 test.each([
   [String, undefined, String],
   [String, '', { type: String, default: '' }],
+  [String, false, { type: String, default: false }],
   [{ type: String }, '', { type: String, default: '' }],
   [null, undefined, null],
 ] as const)('propsFactory %#', (definition, defaults, result) => {
