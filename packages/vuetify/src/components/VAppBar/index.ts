8c5ee0268dcc78d4cde57f36a90bc68ec1656639
Kael <kaelwd@gmail.com>
2020-11-03 20:02:05 +1100

    feat(VAppBar): don't truncate title with shrink-on-scroll (#12516)
    closes #12514

--- packages/vuetify/src/components/VAppBar/index.ts
+++ packages/vuetify/src/components/VAppBar/index.ts
@@ -1,11 +1,13 @@
 import VAppBar from './VAppBar'
 import VAppBarNavIcon from './VAppBarNavIcon'
+import VAppBarTitle from './VAppBarTitle'
 
-export { VAppBar, VAppBarNavIcon }
+export { VAppBar, VAppBarNavIcon, VAppBarTitle }
 
 export default {
   $_vuetify_subcomponents: {
     VAppBar,
     VAppBarNavIcon,
+    VAppBarTitle,
   },
 }
