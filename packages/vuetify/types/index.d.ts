fef55280685673f713502b01fa27ac78aca53c41
Nathan <nquinn@medonesystems.com>
2020-11-04 16:24:02 -0500

    fixed types file

--- packages/vuetify/types/index.d.ts
+++ packages/vuetify/types/index.d.ts
@@ -1,73 +1,68 @@
-import Vue, {
-  Component,
-  PluginFunction,
-  VueConstructor,
-  DirectiveOptions
-} from "vue";
-import "./lib";
-import "./alacarte";
-import "./colors";
+import Vue, { Component, PluginFunction, VueConstructor, DirectiveOptions } from 'vue'
+import './lib'
+import './alacarte'
+import './colors'
 
 // Services
-import { Application } from "./services/application";
-import { Breakpoint } from "./services/breakpoint";
-import { Icons } from "./services/icons";
-import { Lang } from "./services/lang";
-import { Theme } from "./services/theme";
-import { Presets, UserVuetifyPreset, VuetifyPreset } from "./services/presets";
+import { Application } from './services/application'
+import { Breakpoint } from './services/breakpoint'
+import { Icons } from './services/icons'
+import { Lang } from './services/lang'
+import { Theme } from './services/theme'
+import { Presets, UserVuetifyPreset, VuetifyPreset } from './services/presets'
 
 // Service Options
-import { GoToOptions } from "./services/goto";
+import { GoToOptions } from './services/goto'
 
-declare const Vuetify: Vuetify;
-export default Vuetify;
+declare const Vuetify: Vuetify
+export default Vuetify
 export interface Vuetify {
-  framework: Framework;
-  install: PluginFunction<VuetifyUseOptions>;
-  preset: VuetifyPreset;
-  userPreset: UserVuetifyPreset;
-  version: string;
-  config: Config;
-  new (preset?: Partial<UserVuetifyPreset>): Vuetify;
+  framework: Framework
+  install: PluginFunction<VuetifyUseOptions>
+  preset: VuetifyPreset
+  userPreset: UserVuetifyPreset
+  version: string
+  config: Config
+  new (preset?: Partial<UserVuetifyPreset>): Vuetify
 }
 
 export interface Config {
-  silent: boolean;
+  silent: boolean
 }
 
-export { Presets, VuetifyPreset, UserVuetifyPreset } from "./services/presets";
+export { Presets, VuetifyPreset, UserVuetifyPreset } from './services/presets'
 
 export type ComponentOrPack = Component & {
-  $_vuetify_subcomponents?: Record<string, ComponentOrPack>;
-};
+  $_vuetify_subcomponents?: Record<string, ComponentOrPack>
+}
 
 export interface VuetifyUseOptions {
-  transitions?: Record<string, VueConstructor>;
-  directives?: Record<string, DirectiveOptions>;
-  components?: Record<string, ComponentOrPack>;
+  transitions?: Record<string, VueConstructor>
+  directives?: Record<string, DirectiveOptions>
+  components?: Record<string, ComponentOrPack>
 }
 
 export interface Framework {
-  readonly breakpoint: Breakpoint;
+  readonly breakpoint: Breakpoint
   readonly goTo: <T extends string | number | HTMLElement | Vue>(
     target: T,
     options?: GoToOptions
-  ) => Promise<T>;
-  application: Application;
-  theme: Theme;
-  icons: Icons;
-  lang: Lang;
-  presets: Presets;
-  rtl: boolean;
+  ) => Promise<T>
+  application: Application
+  theme: Theme
+  icons: Icons
+  lang: Lang
+  presets: Presets
+  rtl: boolean
 }
 
-declare module "vue/types/vue" {
+declare module 'vue/types/vue' {
   export interface Vue {
-    $vuetify: Framework;
+    $vuetify: Framework
   }
 }
 
-declare module "vue/types/options" {
+declare module 'vue/types/options' {
   export interface ComponentOptions<
     V extends Vue,
     Data = DefaultData<V>,
@@ -76,7 +71,7 @@ declare module "vue/types/options" {
     PropsDef = PropsDefinition<DefaultProps>,
     Props = DefaultProps
   > {
-    vuetify?: Vuetify;
+    vuetify?: Vuetify
   }
 }
 
@@ -85,64 +80,64 @@ export type TreeviewItemFunction = (
   item: object,
   search: string,
   textKey: string
-) => boolean;
+) => boolean
 
 export type SelectItemKey =
   | string
   | (string | number)[]
-  | ((item: object, fallback?: any) => any);
+  | ((item: object, fallback?: any) => any)
 
 export interface ItemGroup<T> {
-  name: string;
-  items: T[];
+  name: string
+  items: T[]
 }
 
 export interface DataOptions {
-  page: number;
-  itemsPerPage: number;
-  sortBy: string[];
-  sortDesc: boolean[];
-  groupBy: string[];
-  groupDesc: boolean[];
-  multiSort: boolean;
-  mustSort: boolean;
+  page: number
+  itemsPerPage: number
+  sortBy: string[]
+  sortDesc: boolean[]
+  groupBy: string[]
+  groupDesc: boolean[]
+  multiSort: boolean
+  mustSort: boolean
 }
 
 export interface DataPagination {
-  page: number;
-  itemsPerPage: number;
-  pageStart: number;
-  pageStop: number;
-  pageCount: number;
-  itemsLength: number;
+  page: number
+  itemsPerPage: number
+  pageStart: number
+  pageStop: number
+  pageCount: number
+  itemsLength: number
 }
 
 export interface DataItemProps {
-  item: any;
-  select: (v: boolean) => void;
-  isSelected: boolean;
-  expand: (v: boolean) => void;
-  isExpanded: boolean;
-  isMobile: boolean;
+  item: any
+  select: (v: boolean) => void
+  isSelected: boolean
+  expand: (v: boolean) => void
+  isExpanded: boolean
+  isMobile: boolean
 }
 
 export interface DataTableItemProps extends DataItemProps {
-  headers: DataTableHeader[];
+  headers: DataTableHeader[]
 }
 
 export interface DataScopeProps {
-  originalItemsLength: number;
-  items: any[];
-  pagination: DataPagination;
-  options: DataOptions;
-  updateOptions: (obj: any) => void;
-  sort: (value: string) => void;
-  sortArray: (sortBy: string[]) => void;
-  group: (value: string) => void;
-  groupedItems: ItemGroup<any>[] | null;
+  originalItemsLength: number
+  items: any[]
+  pagination: DataPagination
+  options: DataOptions
+  updateOptions: (obj: any) => void
+  sort: (value: string) => void
+  sortArray: (sortBy: string[]) => void
+  group: (value: string) => void
+  groupedItems: ItemGroup<any>[] | null
 }
 
-export type DataTableCompareFunction<T = any> = (a: T, b: T) => number;
+export type DataTableCompareFunction<T = any> = (a: T, b: T) => number
 
 export type DataSortFunction<T extends any = any> = (
   items: T[],
@@ -150,149 +145,149 @@ export type DataSortFunction<T extends any = any> = (
   sortDesc: boolean[],
   locale: string,
   customSorters?: Record<string, DataTableCompareFunction<T>>
-) => T[];
+) => T[]
 
 export type DataGroupFunction<T extends any = any> = (
   items: T[],
   groupBy: string[],
   groupDesc: boolean[]
-) => ItemGroup<T>[];
+) => ItemGroup<T>[]
 
 export type DataSearchFunction<T extends any = any> = (
   items: T[],
   search: string
-) => T[];
+) => T[]
 
-export type DatePickerFormatter = (date: string) => string;
+export type DatePickerFormatter = (date: string) => string
 
-export type DatePickerAllowedDatesFunction = (date: string) => boolean;
+export type DatePickerAllowedDatesFunction = (date: string) => boolean
 
-export type DatePickerEventColorValue = string | string[];
+export type DatePickerEventColorValue = string | string[]
 
 export type DatePickerEvents =
   | string[]
   | ((date: string) => boolean | DatePickerEventColorValue)
-  | Record<string, DatePickerEventColorValue>;
+  | Record<string, DatePickerEventColorValue>
 
 export type DatePickerEventColors =
   | DatePickerEventColorValue
   | Record<string, DatePickerEventColorValue>
-  | ((date: string) => DatePickerEventColorValue);
+  | ((date: string) => DatePickerEventColorValue)
 
-export type DatePickerType = "date" | "month";
+export type DatePickerType = 'date' | 'month'
 
-export type DatePickerMultipleFormatter = (date: string[]) => string;
+export type DatePickerMultipleFormatter = (date: string[]) => string
 
 export interface TouchHandlers {
-  start?: (wrapperEvent: TouchEvent & TouchWrapper) => void;
-  end?: (wrapperEvent: TouchEvent & TouchWrapper) => void;
-  move?: (wrapperEvent: TouchEvent & TouchWrapper) => void;
-  left?: (wrapper: TouchWrapper) => void;
-  right?: (wrapper: TouchWrapper) => void;
-  up?: (wrapper: TouchWrapper) => void;
-  down?: (wrapper: TouchWrapper) => void;
+  start?: (wrapperEvent: TouchEvent & TouchWrapper) => void
+  end?: (wrapperEvent: TouchEvent & TouchWrapper) => void
+  move?: (wrapperEvent: TouchEvent & TouchWrapper) => void
+  left?: (wrapper: TouchWrapper) => void
+  right?: (wrapper: TouchWrapper) => void
+  up?: (wrapper: TouchWrapper) => void
+  down?: (wrapper: TouchWrapper) => void
 }
 
 export interface TouchWrapper extends TouchHandlers {
-  touchstartX: number;
-  touchstartY: number;
-  touchmoveX: number;
-  touchmoveY: number;
-  touchendX: number;
-  touchendY: number;
-  offsetX: number;
-  offsetY: number;
+  touchstartX: number
+  touchstartY: number
+  touchmoveX: number
+  touchmoveY: number
+  touchendX: number
+  touchendY: number
+  offsetX: number
+  offsetY: number
 }
 
 export type TouchValue = TouchHandlers & {
-  parent?: boolean;
-  options?: AddEventListenerOptions;
-};
+  parent?: boolean
+  options?: AddEventListenerOptions
+}
 
-export type InputValidationRule = (value: any) => string | boolean;
+export type InputValidationRule = (value: any) => string | boolean
 
-export type InputMessage = string | string[];
+export type InputMessage = string | string[]
 
-export type InputValidationRules = (InputValidationRule | string)[];
+export type InputValidationRules = (InputValidationRule | string)[]
 
 export type CalendarCategory =
   | string
   | {
-      name?: string;
-      categoryName?: string;
-      [key: string]: any;
-    };
+      name?: string
+      categoryName?: string
+      [key: string]: any
+    }
 
 export type CalendarCategoryTextFunction = (
   category: CalendarCategory
-) => string;
+) => string
 
 export interface CalendarTimestamp {
-  date: string;
-  time: string;
-  year: number;
-  month: number;
-  day: number;
-  weekday: number;
-  hour: number;
-  minute: number;
-  hasDay: boolean;
-  hasTime: boolean;
-  past: boolean;
-  present: boolean;
-  future: boolean;
-  category?: CalendarCategory;
+  date: string
+  time: string
+  year: number
+  month: number
+  day: number
+  weekday: number
+  hour: number
+  minute: number
+  hasDay: boolean
+  hasTime: boolean
+  past: boolean
+  present: boolean
+  future: boolean
+  category?: CalendarCategory
 }
 
 export type CalendarFormatter = (
   timestamp: CalendarTimestamp,
   short: boolean
-) => string;
+) => string
 
 export interface CalendarEvent {
-  [prop: string]: any;
+  [prop: string]: any
 }
 
 export interface CalendarEventParsed {
-  input: CalendarEvent;
-  start: CalendarTimestamp;
-  startIdentifier: number;
-  startTimestampIdentifier: number;
-  end: CalendarTimestamp;
-  endIdentifier: number;
-  endTimestampIdentifier: number;
-  allDay: boolean;
-  index: number;
-  category: string | false;
+  input: CalendarEvent
+  start: CalendarTimestamp
+  startIdentifier: number
+  startTimestampIdentifier: number
+  end: CalendarTimestamp
+  endIdentifier: number
+  endTimestampIdentifier: number
+  allDay: boolean
+  index: number
+  category: string | false
 }
 
 export interface CalendarEventVisual {
-  event: CalendarEventParsed;
-  columnCount: number;
-  column: number;
-  left: number;
-  width: number;
+  event: CalendarEventParsed
+  columnCount: number
+  column: number
+  left: number
+  width: number
 }
 
 export interface CalendarDaySlotScope extends CalendarTimestamp {
-  outside: boolean;
-  index: number;
-  week: CalendarTimestamp[];
-  category: CalendarCategory;
+  outside: boolean
+  index: number
+  week: CalendarTimestamp[]
+  category: CalendarCategory
 }
 
 export type CalendarTimeToY = (
   time: CalendarTimestamp | number | string,
   clamp?: boolean
-) => number;
+) => number
 
 export type CalendarTimeDelta = (
   time: CalendarTimestamp | number | string
-) => number | false;
+) => number | false
 
 export interface CalendarDayBodySlotScope extends CalendarDaySlotScope {
-  timeToY: CalendarTimeToY;
-  timeDelta: CalendarTimeDelta;
+  timeToY: CalendarTimeToY
+  timeDelta: CalendarTimeDelta
 }
 
 export type CalendarEventOverlapMode = (
@@ -304,47 +299,47 @@ export type CalendarEventOverlapMode = (
   dayEvents: CalendarEventParsed[],
   timed: boolean,
   reset: boolean
-) => CalendarEventVisual[];
+) => CalendarEventVisual[]
 
-export type CalendarEventColorFunction = (event: CalendarEvent) => string;
+export type CalendarEventColorFunction = (event: CalendarEvent) => string
 
-export type CalendarEventTimedFunction = (event: CalendarEvent) => boolean;
+export type CalendarEventTimedFunction = (event: CalendarEvent) => boolean
 
-export type CalendarEventCategoryFunction = (event: CalendarEvent) => string;
+export type CalendarEventCategoryFunction = (event: CalendarEvent) => string
 
 export type CalendarEventNameFunction = (
   event: CalendarEventParsed,
   timedEvent: boolean
-) => string;
+) => string
 
 export type DataTableFilterFunction = (
   value: any,
   search: string | null,
   item: any
-) => boolean;
+) => boolean
 
 export interface DataTableHeader<T extends any = any> {
-  text: string;
-  value: string;
-  align?: "start" | "center" | "end";
-  sortable?: boolean;
-  filterable?: boolean;
-  groupable?: boolean;
-  divider?: boolean;
-  class?: string | string[];
-  cellClass?: string | string[];
-  width?: string | number;
-  filter?: (value: any, search: string | null, item: any) => boolean;
-  sort?: DataTableCompareFunction<T>;
+  text: string
+  value: string
+  align?: 'start' | 'center' | 'end'
+  sortable?: boolean
+  filterable?: boolean
+  groupable?: boolean
+  divider?: boolean
+  class?: string | string[]
+  cellClass?: string | string[]
+  width?: string | number
+  filter?: (value: any, search: string | null, item: any) => boolean
+  sort?: DataTableCompareFunction<T>
 }
 
 export type DataItemsPerPageOption =
   | number
   | {
-      text: string;
-      value: number;
-    };
+      text: string
+      value: number
+    }
 
 export type RowClassFunction = (
   item: any
-) => null | undefined | string | string[] | Record<string, boolean>;
+) => null | undefined | string | string[] | Record<string, boolean>
