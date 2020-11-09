9c820f93b64edc26389af498efca9d87cce1b8f4
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-11-09 14:16:42 -0800

    refactor(vcard): card-reveal
    update example.

--- packages/docs/src/examples/v-card/misc-card-reveal.vue
+++ packages/docs/src/examples/v-card/misc-card-reveal.vue
@@ -1,61 +1,50 @@
 <template>
   <v-card
     class="mx-auto"
-    color="grey lighten-4"
-    max-width="600"
+    max-width="344"
   >
-    <v-img
-      :aspect-ratio="16/9"
-      src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
-    >
-    </v-img>
-    <v-card-text
-      class="pt-6"
-      style="position: relative;"
-    >
-      <v-btn
-        absolute
-        color="orange"
-        icon
-        large
-        outlined
-        right
-        top
-        @click.prevent="reveal = true"
-      >
-        <v-icon>mdi-cart</v-icon>
-      </v-btn>
-      <div class="font-weight-light grey--text title mb-2">
-        For the perfect meal
-      </div>
-      <h3 class="display-1 font-weight-light orange--text mb-2">
-        QW cooking utensils
-      </h3>
-      <div class="font-weight-light title mb-2">
-        Our Vintage kitchen utensils delight any chef.<br>
-        Made of bamboo by hand
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="display-1 text--primary">
+        el·ee·mos·y·nar·y
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        relating to or dependent on charity; charitable.<br>
+        "an eleemosynary educational institution."
       </div>
     </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+
     <v-expand-transition>
       <v-card
         v-if="reveal"
-        class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal"
+        class="transition-fast-in-fast-out v-card--reveal"
         style="height: 100%;"
       >
-        <v-btn
-          absolute
-          large
-          right
-          top
-          icon
-          dark
-          color="white"
-          @click="reveal = false"
-        >
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-
-        <v-card-title class="white--text display-1">$14.99</v-card-title>
+        <v-card-text class="pb-0">
+          <p class="display-1 text--primary">
+            Origin
+          </p>
+          <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="reveal = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-expand-transition>
   </v-card>
@@ -71,10 +60,8 @@
 
 <style>
 .v-card--reveal {
-  align-items: center;
   bottom: 0;
-  justify-content: center;
-  opacity: .7 !important;
+  opacity: 1 !important;
   position: absolute;
   width: 100%;
 }
