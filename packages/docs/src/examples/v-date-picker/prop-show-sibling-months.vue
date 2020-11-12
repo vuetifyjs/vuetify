aa36a183c43ea4f5d312b4a6c8513c8c73ae6b32
Jacek Karczmarczyk <jk@drukcd.pl>
2020-11-12 17:35:13 +0100

    feat(VDatePicker): **show-sibling-month** prop
    fixes #12237

---
+++ packages/docs/src/examples/v-date-picker/prop-show-sibling-months.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-date-picker
+    v-model="picker"
+    show-sibling-months
+  ></v-date-picker>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        picker: null,
+      }
+    },
+  }
+</script>
