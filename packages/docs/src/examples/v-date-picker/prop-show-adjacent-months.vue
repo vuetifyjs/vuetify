8f1cf64691d05c673306563366e5ee794e8b75de
Jacek Karczmarczyk <jk@drukcd.pl>
2020-11-17 14:12:11 +0100

    feat(VDatePicker): `show-adjacent-months` prop (#12603)
    * feat(VDatePicker): **show-sibling-month** prop
    
    resolves #12237
    
    * refactor: rename prop to showAdjacentMonths

---
+++ packages/docs/src/examples/v-date-picker/prop-show-adjacent-months.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-date-picker
+    v-model="picker"
+    show-adjacent-months
+  ></v-date-picker>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        picker: null,
+      }
+    },
+  }
+</script>
