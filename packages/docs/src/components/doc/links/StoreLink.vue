ba87c38b21288d7c82494ce70276a6d55b9e4fd5
John Leider <john.j.leider@gmail.com>
2020-12-01 09:47:31 -0600

    docs(StoreLink): remove badge

--- packages/docs/src/components/doc/links/StoreLink.vue
+++ packages/docs/src/components/doc/links/StoreLink.vue
@@ -1,26 +1,11 @@
 <template>
   <app-tooltip-btn
     href="https://store.vuetifyjs.com/?utm_source=vuetifyjs.com&utm_medium=toolbar"
-    icon="$mdiCartOutline"
+    icon="$mdiShoppingOutline"
     path="store"
     rel="noopener"
     target="_blank"
-  >
-    <template #icon>
-      <v-badge
-        :value="true"
-        color="#ED561B"
-        dot
-        left
-        overlap
-      >
-        <v-icon
-          class="mx-1"
-          v-text="'$mdiShoppingOutline'"
-        />
-      </v-badge>
-    </template>
-  </app-tooltip-btn>
+  />
 </template>
 
 <script>
