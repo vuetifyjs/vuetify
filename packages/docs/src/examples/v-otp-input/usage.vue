759070872c5d1cf6f3cbdd3cbb0898c026c4d07c
Ohad Shitrit <ohadsh535@gmail.com>
2020-12-21 02:56:06 +0200

    docs(VOtpInput): Added documentation examples and api.

---
+++ packages/docs/src/examples/v-otp-input/usage.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <div class="text-center">
+        <div class="ma-auto" style="max-width: 320px">
+          <v-otp-input
+            v-model="otp"
+            :on-finish="onOtpFinished"
+            v-bind="$attrs"
+            v-on="$listeners"
+          ></v-otp-input>
+        </div>
+      </div>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    props: {
+      maxWidth: {
+        type: Number,
+        default: 300,
+      },
+    },
+    data: () => ({
+      defaults: {
+        disabled: false,
+        length: 6,
+      },
+      otp: '',
+      options: {
+        booleans: ['disabled', 'dark'],
+        selects: {
+          type: ['text', 'password', 'number'],
+        },
+        sliders: { length: [2, 8, 2] },
+      },
+      tabs: ['default'],
+    }),
+    methods: {
+      onOtpFinished (rsp) {
+        // OTP ready to process.
+        console.log('Completed input with', rsp)
+      },
+    },
+  }
+</script>
