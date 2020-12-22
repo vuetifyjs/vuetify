bae707c211eb1572d3f2d8a16bfeac48576d0f6b
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-12-22 15:23:52 -0800

    feat(VBtn): add new styling property plain (#12814)
    resolves #3026 
    
    Co-authored-by: John Leider <john.j.leider@gmail.com>

--- packages/vuetify/src/components/VBtn/VBtn.ts
+++ packages/vuetify/src/components/VBtn/VBtn.ts
@@ -54,6 +54,7 @@ export default baseMixins.extend<options>().extend({
     icon: Boolean,
     loading: Boolean,
     outlined: Boolean,
+    plain: Boolean,
     retainFocusOnClick: Boolean,
     rounded: Boolean,
     tag: {
@@ -81,16 +82,19 @@ export default baseMixins.extend<options>().extend({
         'v-btn--absolute': this.absolute,
         'v-btn--block': this.block,
         'v-btn--bottom': this.bottom,
-        'v-btn--contained': this.contained,
-        'v-btn--depressed': (this.depressed) || this.outlined,
+        'v-btn--contained': this.isElevated, // TODO: remove v3
+        'v-btn--depressed': (this.depressed) || this.outlined, // TODO: remove v3
         'v-btn--disabled': this.disabled,
+        'v-btn--is-elevated': this.isElevated,
         'v-btn--fab': this.fab,
         'v-btn--fixed': this.fixed,
-        'v-btn--flat': this.isFlat,
+        'v-btn--flat': !this.isElevated, // TODO: remove v3,
+        'v-btn--has-bg': this.hasBg,
         'v-btn--icon': this.icon,
         'v-btn--left': this.left,
         'v-btn--loading': this.loading,
         'v-btn--outlined': this.outlined,
+        'v-btn--plain': this.plain,
         'v-btn--right': this.right,
         'v-btn--round': this.isRound,
         'v-btn--rounded': this.rounded,
@@ -104,26 +108,23 @@ export default baseMixins.extend<options>().extend({
         ...this.sizeableClasses,
       }
     },
-    contained (): boolean {
-      return Boolean(
-        !this.isFlat &&
-        !this.depressed &&
-        // Contained class only adds elevation
-        // is not needed if user provides value
-        !this.elevation
-      )
-    },
     computedRipple (): RippleOptions | boolean {
       const defaultRipple = this.icon || this.fab ? { circle: true } : true
       if (this.disabled) return false
       else return this.ripple ?? defaultRipple
     },
-    isFlat (): boolean {
+    hasBg (): boolean {
+      return this.isElevated || this.depressed
+    },
+    isElevated (): boolean {
       return Boolean(
-        this.icon ||
-        this.text ||
-        this.outlined
-      )
+        !this.icon &&
+        !this.text &&
+        !this.outlined &&
+        !this.depressed &&
+        !this.disabled &&
+        !this.plain
+      ) || Number(this.elevation) > 0
     },
     isRound (): boolean {
       return Boolean(
@@ -182,8 +183,10 @@ export default baseMixins.extend<options>().extend({
       this.genContent(),
       this.loading && this.genLoader(),
     ]
-    const setColor = !this.isFlat ? this.setBackgroundColor : this.setTextColor
     const { tag, data } = this.generateRouteLink()
+    const setColor = (this.isElevated || this.depressed)
+      ? this.setBackgroundColor
+      : this.setTextColor
 
     if (tag === 'button') {
       data.attrs!.type = this.type
