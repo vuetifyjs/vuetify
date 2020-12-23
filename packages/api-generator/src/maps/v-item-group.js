f1702aebf1509aa96be46a98ee85dc6e0c038009
Kael <kaelwd@gmail.com>
2020-12-24 03:01:53 +1100

    chore(api): remove redundant prop definition

--- packages/api-generator/src/maps/v-item-group.js
+++ packages/api-generator/src/maps/v-item-group.js
@@ -1,12 +1,5 @@
 module.exports = {
   'v-item-group': {
-    props: [
-      {
-        name: 'tag',
-        type: 'string',
-        default: "'div'",
-      },
-    ],
     events: [
       {
         name: 'change',
