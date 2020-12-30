31a0f6cf167e7798183e2b44010e00792e182fd2
CHIRAG YADAV <chirag.yadav@miqdigital.com>
2020-12-30 22:31:13 +0530

    fix(VDatePicker): scroll not honoring min and max tableDate

--- packages/vuetify/src/components/VDatePicker/VDatePicker.ts
+++ packages/vuetify/src/components/VDatePicker/VDatePicker.ts
@@ -19,6 +19,7 @@ import {
   createItemTypeListeners,
   createNativeLocaleFormatter,
   pad,
+  sanitizeDateString,
 } from './util'
 
 // Types
@@ -40,13 +41,6 @@ type DatePickerValue = string | string[] | undefined
 interface Formatters {
   year: DatePickerFormatter
   titleDate: DatePickerFormatter | DatePickerMultipleFormatter
-}
-
-// Adds leading zero to month/day if necessary, returns 'YYYY' if type = 'year',
-// 'YYYY-MM' if 'month' and 'YYYY-MM-DD' if 'date'
-function sanitizeDateString (dateString: string, type: 'date' | 'month' | 'year'): string {
-  const [year, month = 1, date = 1] = dateString.split('-')
-  return `${year}-${pad(month)}-${pad(date)}`.substr(0, { date: 10, month: 7, year: 4 }[type])
 }
 
 export default mixins(
