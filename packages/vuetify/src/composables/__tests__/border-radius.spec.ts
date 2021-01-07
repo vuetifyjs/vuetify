8b3c79cfd0dfa033413b972ea034e8e2990f8080
Renoir Boulanger <contribs@renoirboulanger.com>
2021-01-06 22:48:28 -0300

    task: what was part of another Draft PR #12801 and missing here

--- packages/vuetify/src/composables/__tests__/border-radius.spec.ts
+++ packages/vuetify/src/composables/__tests__/border-radius.spec.ts
@@ -20,11 +20,27 @@ describe('border-radius.ts', () => {
   })
 
   it.each([
+    // When should return nothing
     [{}, []],
-    [{ rounded: 'tile' }, ['rounded-0']],
+    [{ rounded: null }, []],
+    [{ rounded: 1 }, []],
+    [{ rounded: { toString: () => 'pill' } }, []],
+    // Rounded only
     [{ rounded: true }, ['rounded']],
+    [{ rounded: '' }, ['rounded']],
+    // Rounded with 0
+    [{ rounded: 'tile' }, ['rounded-0']],
     [{ rounded: '0' }, ['rounded-0']],
     [{ rounded: false }, ['rounded-0']],
+    [{ rounded: 0 }, ['rounded-0']],
+    // Rounded with a word
+    [{ rounded: 'circle' }, ['rounded-circle']],
+    [{ rounded: 'shaped' }, ['rounded-shaped']],
+    [{ rounded: 'pill' }, ['rounded-pill']],
+    // All strings are accepted?
+    [{ rounded: 'foo-bar-bazz-buzz' }, ['rounded-foo-bar-bazz-buzz']],
+    [{ rounded: '!' }, ['rounded-!']],
+    [{ rounded: 'sm-3 4' }, ['rounded-sm-3', 'rounded-4']],
     [{ rounded: 'tr-xl br-lg' }, ['rounded-tr-xl', 'rounded-br-lg']],
   ] as BorderRadiusProps[])(
     'should return correct rounded classes',
