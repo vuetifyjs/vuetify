325908f09097dac97b7cfa20543091fd57044025
John Leider <john.j.leider@gmail.com>
2021-01-20 09:04:27 -0600

    chore: init commit

--- packages/vuetify/src/components/VDivider/VDivider.ts
+++ packages/vuetify/src/components/VDivider/VDivider.ts
@@ -1,42 +1,43 @@
-// @ts-nocheck
-/* eslint-disable */
-
 // Styles
 import './VDivider.sass'
 
-// Types
-import { VNode } from 'vue'
+// Utilities
+import { computed, defineComponent, h } from 'vue'
 
-// Mixins
-import Themeable from '../../mixins/themeable'
+// Composables
+import { useTheme } from '@/composables'
 
-export default Themeable.extend({
-  name: 'v-divider',
+export default defineComponent({
+  name: 'VDivider',
 
   props: {
     inset: Boolean,
     vertical: Boolean,
   },
 
-  render (h): VNode {
-    // WAI-ARIA attributes
-    let orientation
-    if (!this.$attrs.role || this.$attrs.role === 'separator') {
-      orientation = this.vertical ? 'vertical' : 'horizontal'
-    }
-    return h('hr', {
-      class: {
-        'v-divider': true,
-        'v-divider--inset': this.inset,
-        'v-divider--vertical': this.vertical,
-        ...this.themeClasses,
-      },
-      attrs: {
-        role: 'separator',
-        'aria-orientation': orientation,
-        ...this.$attrs,
-      },
-      on: this.$listeners,
+  setup (props, { attrs }) {
+    const { themeClasses } = useTheme()
+    const classes = computed(() => {
+      return {
+        'v-divider--inset': props.inset,
+        'v-divider--vertical': props.vertical,
+      }
     })
+    const role = attrs.role ?? 'separator'
+    const ariaOrientation = role === 'separator'
+      ? props.vertical ? 'vertical' : 'horizontal'
+      : undefined
+
+    return () => (
+      h('hr', {
+        class: [
+          'v-divider',
+          classes.value,
+          themeClasses.value,
+        ],
+        ariaOrientation,
+        role,
+      })
+    )
   },
 })
