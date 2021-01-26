5e0bf6ac5cdd8b3c9a62898d30b1790a286948f2
John Leider <john.j.leider@gmail.com>
2021-01-26 14:01:20 -0600

    feat(VSheet): port to v3

--- packages/vuetify/src/components/VSheet/VSheet.ts
+++ packages/vuetify/src/components/VSheet/VSheet.ts
@@ -1,70 +1,61 @@
-// @ts-nocheck
-/* eslint-disable */
-
 // Styles
 import './VSheet.sass'
 
-// Mixins
-import BindsAttrs from '../../mixins/binds-attrs'
-import Colorable from '../../mixins/colorable'
-import Elevatable from '../../mixins/elevatable'
-import Measurable from '../../mixins/measurable'
-import Roundable from '../../mixins/roundable'
-import Themeable from '../../mixins/themeable'
+// Utilities
+import { defineComponent, h } from 'vue'
+import makeProps from '@/util/makeProps'
 
-// Helpers
-import mixins from '../../util/mixins'
+// Composables
+import {
+  makeBorderProps,
+  makeBorderRadiusProps,
+  makeDimensionProps,
+  makeElevationProps,
+  makePositionProps,
+  makeTagProps,
+  useBorder,
+  useBorderRadius,
+  useDimension,
+  useElevation,
+  usePosition,
+  useTheme,
+} from '@/composables'
 
-// Types
-import { VNode } from 'vue'
+export default defineComponent({
+  name: 'VSheet',
 
-/* @vue/component */
-export default mixins(
-  BindsAttrs,
-  Colorable,
-  Elevatable,
-  Measurable,
-  Roundable,
-  Themeable
-).extend({
-  name: 'v-sheet',
+  props: makeProps({
+    ...makeBorderProps(),
+    ...makeBorderRadiusProps(),
+    ...makeDimensionProps(),
+    ...makeElevationProps(),
+    ...makePositionProps(),
+    ...makeTagProps(),
+  }),
 
-  props: {
-    outlined: Boolean,
-    shaped: Boolean,
-    tag: {
-      type: String,
-      default: 'div',
-    },
-  },
+  setup (props, { slots }) {
+    const { themeClasses } = useTheme()
+    const { borderClasses } = useBorder(props)
+    const { borderRadiusClasses } = useBorderRadius(props)
+    const { dimensionStyles } = useDimension(props)
+    const { elevationClasses } = useElevation(props)
+    const { positionClasses, positionStyles } = usePosition(props)
 
-  computed: {
-    classes (): object {
-      return {
-        'v-sheet': true,
-        'v-sheet--outlined': this.outlined,
-        'v-sheet--shaped': this.shaped,
-        ...this.themeClasses,
-        ...this.elevationClasses,
-        ...this.roundedClasses,
-      }
-    },
-    styles (): object {
-      return this.measurableStyles
-    },
-  },
-
-  render (h): VNode {
-    const data = {
-      class: this.classes,
-      style: this.styles,
-      on: this.listeners$,
-    }
-
-    return h(
-      this.tag,
-      this.setBackgroundColor(this.color, data),
-      this.$slots.default
+    return () => (
+      h(props.tag, {
+        class: [
+          'v-sheet',
+          themeClasses.value,
+          borderClasses.value,
+          borderRadiusClasses.value,
+          elevationClasses.value,
+          positionClasses.value,
+        ],
+        style: [
+          dimensionStyles.value,
+          positionStyles.value,
+        ],
+      }, slots)
     )
   },
 })
