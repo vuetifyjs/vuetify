89fce52f65f7bebda8f6ebdc2935dc68a5779bda
John Leider <john.j.leider@gmail.com>
2021-01-26 12:53:24 -0600

    refactor(VDivider): remove border and dimension composables
    replace with 2 new properties, length and thickness

--- packages/vuetify/src/components/VDivider/VDivider.ts
+++ packages/vuetify/src/components/VDivider/VDivider.ts
@@ -2,39 +2,48 @@
 import './VDivider.sass'
 
 // Utilities
-import { defineComponent, h } from 'vue'
+import { computed, defineComponent, h } from 'vue'
+import { convertToUnit } from '@/util/helpers'
 import makeProps from '@/util/makeProps'
 
 // Composables
-import {
-  makeBorderProps,
-  makeDimensionProps,
-  useBorder,
-  useDimension,
-  useTheme,
-} from '@/composables'
+import { useTheme } from '@/composables'
+
+// Types
+type DividerKey = 'maxHeight' | 'maxWidth' | 'minHeight' | 'minWidth'
+type DividerStyles = Partial<Record<DividerKey, string>>
 
 export default defineComponent({
   name: 'VDivider',
 
   props: {
-    ...makeBorderProps(),
-    ...makeDimensionProps({
-      height: undefined,
-      maxHeight: undefined,
-      maxWidth: undefined,
-      width: undefined,
-    }),
     ...makeProps({
       inset: Boolean,
+      length: [Number, String],
+      thickness: [Number, String],
       vertical: Boolean,
     }),
   },
 
   setup (props, { attrs }) {
-    const { borderClasses } = useBorder(props)
-    const { dimensionStyles } = useDimension(props)
     const { themeClasses } = useTheme()
+    const dividerStyles = computed(() => {
+      const styles: DividerStyles = {}
+
+      if (props.length) {
+        const length = convertToUnit(props.length)
+
+        styles[props.vertical ? 'maxHeight' : 'maxWidth'] = length
+      }
+
+      if (props.thickness) {
+        const length = convertToUnit(props.thickness)
+
+        styles[props.vertical ? 'minWidth' : 'minHeight'] = length
+      }
+
+      return styles
+    })
 
     return () => (
       h('hr', {
@@ -45,9 +54,10 @@ export default defineComponent({
             'v-divider--vertical': props.vertical,
           },
           themeClasses.value,
-          borderClasses.value,
         ],
-        style: [dimensionStyles.value],
+        style: [
+          dividerStyles.value,
+        ],
         ariaOrientation: !attrs.role || attrs.role === 'separator'
           ? props.vertical ? 'vertical' : 'horizontal'
           : undefined,
