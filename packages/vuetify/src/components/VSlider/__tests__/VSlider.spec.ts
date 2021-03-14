03ae9b4112c3eb2a78dececd5d93efe0bd646caa
inottn <inottn@outlook.com>
2021-03-14 15:52:07 +0800

    fix(VSlider): support touch and drag

--- packages/vuetify/src/components/VSlider/__tests__/VSlider.spec.ts
+++ packages/vuetify/src/components/VSlider/__tests__/VSlider.spec.ts
@@ -294,14 +294,17 @@ describe('VSlider.ts', () => {
     expect(wrapper.vm.thumbPressed).toBe(false)
     expect(wrapper.vm.isActive).toBe(false)
 
-    container.trigger('mousedown', {
-      touches: [],
+    container.trigger('touchstart', {
+      touches: [{}],
     })
 
     await wrapper.vm.$nextTick()
 
-    expect(wrapper.vm.thumbPressed).toBe(true)
     expect(wrapper.vm.isActive).toBe(true)
+
+    el.dispatchEvent(new Event('touchend'))
+
+    expect(wrapper.vm.isActive).toBe(false)
   })
 
   it('should return a rounded value', () => {
