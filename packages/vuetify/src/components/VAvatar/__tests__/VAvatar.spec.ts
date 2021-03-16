1c466ffcba8f49b737f7101b463f3830a9c47826
John Leider <john.j.leider@gmail.com>
2021-03-15 19:11:16 -0500

    test(VAvatar): remove redundant tests

--- packages/vuetify/src/components/VAvatar/__tests__/VAvatar.spec.ts
+++ packages/vuetify/src/components/VAvatar/__tests__/VAvatar.spec.ts
@@ -1,73 +1,24 @@
 // Components
-import { VAvatar } from '../'
+import { VAvatar } from '..'
 
 // Utilities
-import { h } from 'vue'
 import { createVuetify } from '@/framework'
 import { mount } from '@vue/test-utils'
 
 describe('VAvatar', () => {
   const vuetify = createVuetify()
-  const mountFunction = (options = {}) => mount(VAvatar, {
-    ...options,
-    global: {
-      plugins: [vuetify],
-    },
-  })
 
-  it('should have an v-avatar class', () => {
+  function mountFunction (options = {}) {
+    return mount(VAvatar, {
+      global: {
+        plugins: [vuetify],
+      },
+      ...options,
+    })
+  }
+
+  it('should match a snapshot', () => {
     const wrapper = mountFunction()
-
-    expect(wrapper.classes()).toContain('v-avatar')
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('should respect size prop', () => {
-    const wrapper = mountFunction({
-      props: {
-        size: 'large',
-      },
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('should respect rounded prop', () => {
-    const wrapper = mountFunction({
-      props: {
-        rounded: 0,
-      },
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('should respect color prop', () => {
-    const wrapper = mountFunction({
-      props: {
-        color: 'error',
-      },
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('should respect tag prop', () => {
-    const wrapper = mountFunction({
-      props: {
-        tag: 'span',
-      },
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('should render default slot content', () => {
-    const wrapper = mountFunction({
-      slots: {
-        default: () => h('span', 'foo'),
-      },
-    })
 
     expect(wrapper.html()).toMatchSnapshot()
   })
