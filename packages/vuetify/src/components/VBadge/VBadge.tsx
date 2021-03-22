5ae983f8b08221af5b6ca67c513595f83d2c2bf6
John Leider <john.j.leider@gmail.com>
2021-03-22 15:36:47 -0500

    style(VBadge): change border-radius to rounded composable

--- packages/vuetify/src/components/VBadge/VBadge.tsx
+++ packages/vuetify/src/components/VBadge/VBadge.tsx
@@ -5,7 +5,7 @@ import './VBadge.sass'
 import { VIcon } from '@/components/VIcon'
 
 // Composables
-import { makeBorderRadiusProps, useBorderRadius } from '@/composables/border-radius'
+import { makeRoundedProps, useRounded } from '@/composables/rounded'
 import { makeTagProps } from '@/composables/tag'
 import { makeTransitionProps } from '@/composables/transition'
 import { useBackgroundColor, useTextColor } from '@/composables/color'
@@ -54,14 +54,14 @@ export default defineComponent({
     offsetX: [Number, String],
     offsetY: [Number, String],
     textColor: String,
-    ...makeBorderRadiusProps(),
+    ...makeRoundedProps(),
     ...makeTagProps(),
     ...makeTransitionProps({ transition: 'scale-rotate-transition' }),
   },
 
   setup (props, ctx) {
     const { backgroundColorClasses, backgroundColorStyles } = useBackgroundColor(toRef(props, 'color'))
-    const { borderRadiusClasses } = useBorderRadius(props)
+    const { roundedClasses } = useRounded(props, 'v-badge')
     const { textColorClasses, textColorStyles } = useTextColor(toRef(props, 'textColor'))
 
     const position = computed(() => {
@@ -132,7 +132,7 @@ export default defineComponent({
                   class={[
                     'v-badge__badge',
                     backgroundColorClasses.value,
-                    borderRadiusClasses.value,
+                    roundedClasses.value,
                     textColorClasses.value,
                   ]}
                   style={[
