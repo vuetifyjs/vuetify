34924004993c32fb7bb31d98e6e918546eb6bc6e
Kael <kaelwd@gmail.com>
2021-04-19 21:03:56 +1000

    chore(ci): publish with --no-verify-access
    see https://github.com/lerna/lerna/issues/2788

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 npm config set //registry.npmjs.org/:_authToken ${NPM_API_KEY:?}
 npm whoami
-yarn lerna publish from-git --npm-tag $(node ./scripts/parse-npm-tag.js ${TAG_NAME:?}) --yes
+yarn lerna publish from-git --npm-tag $(node ./scripts/parse-npm-tag.js ${TAG_NAME:?}) --yes --no-verify-access
