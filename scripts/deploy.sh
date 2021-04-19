b36af80aa671491a993dff84abcfaa81cb46162d
Kael <kaelwd@gmail.com>
2021-04-19 20:47:10 +1000

    chore(ci): print npm user before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 npm config set //registry.npmjs.org/:_authToken ${NPM_API_KEY:?}
+npm whoami
 yarn lerna publish from-git --npm-tag $(node ./scripts/parse-npm-tag.js ${TAG_NAME:?}) --yes
