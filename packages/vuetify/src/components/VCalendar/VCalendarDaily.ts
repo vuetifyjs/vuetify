d051b0e39ec16af1cd70cacfff2e4090a2ee45ac
yuchao <yuchao.sydneyuni@gmail.com>
2021-04-21 16:03:28 +1000

    Rollback all VCalendarDaily changes

--- packages/vuetify/src/components/VCalendar/VCalendarDaily.ts
+++ packages/vuetify/src/components/VCalendar/VCalendarDaily.ts
@@ -129,6 +129,14 @@ export default CalendarWithIntervals.extend({
       return this.$createElement('div', {
         staticClass: 'v-calendar-daily__body',
       }, [
+        this.genScrollArea(),
+      ])
+    },
+    genScrollArea (): VNode {
+      return this.$createElement('div', {
+        ref: 'scrollArea',
+        staticClass: 'v-calendar-daily__scroll-area',
+      }, [
         this.genPane(),
       ])
     },
