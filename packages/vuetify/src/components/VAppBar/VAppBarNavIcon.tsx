bbbbff96ba411164f70a236810111aec7949cad4
John Leider <john.j.leider@gmail.com>
2021-05-10 15:02:20 -0500

    chore(VAppBarNavIcon): remove dev comments

--- packages/vuetify/src/components/VAppBar/VAppBarNavIcon.tsx
+++ packages/vuetify/src/components/VAppBar/VAppBarNavIcon.tsx
@@ -19,21 +19,4 @@ export default defineComponent({
       </VBtn>
     )
   },
-
-  // functional: true,
-
-  // render (h, { slots, listeners, props, data }) {
-  //   const d = Object.assign(data, {
-  //     staticClass: (`v-app-bar__nav-icon ${data.staticClass || ''}`).trim(),
-  //     props: {
-  //       ...props,
-  //       icon: true,
-  //     },
-  //     on: listeners,
-  //   })
-
-  //   const defaultSlot = slots().default
-
-  //   return h(VBtn, d, defaultSlot || [h(VIcon, '$menu')])
-  // },
 })
