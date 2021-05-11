1ef1536e5b975bbe28d79ee0cf90d06a15abe973
John Leider <john.j.leider@gmail.com>
2021-05-11 16:00:48 -0500

    docs(VAppBar): update docs page, update example, add nav item

---
+++ packages/docs/src/examples/v-app-bar/prop-density.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar density="compact">
+    <v-app-bar-title>Vuetify</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>Developer</v-btn>
+
+    <v-btn class="mr-3" text>Help</v-btn>
+
+    <v-divider inset vertical></v-divider>
+
+    <v-btn
+      class="mr-4 ml-5"
+      color="primary"
+      plain
+    >
+      <v-icon left icon="mdi-handshake-outline"></v-icon>
+
+      <span>John Leider</span>
+    </v-btn>
+  </v-app-bar>
+</template>
