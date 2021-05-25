42ca09abf5fc879280d4f3fc8ac8b02493e0349e
Kael <kaelwd@gmail.com>
2021-05-25 13:30:36 +1000

    add rest of div() used but no sass prefix

--- packages/vuetify/build/webpack.base.config.js
+++ packages/vuetify/build/webpack.base.config.js
@@ -1,80 +1,84 @@
+'use strict'
+
+const HappyThreadPool = () => import('happypack/lib/HappyThreadPool')
+
 require('dotenv').config()
 
-const os = require('os')
-const HappyPack = require('happypack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const os = () => import('os')
+const HappyPack = () => import('happypack')
+const { ThreadPool } =  HappyPack
+const MiniCssExtractPlugin = () => import('mini-css-extract-plugin')
+const FriendlyErrorsWebpackPlugin = () => import('friendly-errors-webpack-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const extractCSS = isProd || process.env.TARGET === 'development'
 
-exports.happyThreadPool = HappyPack.ThreadPool({
-  size: Math.min(os.cpus().length, 4),
-})
+exports = {
+  happyThreadPool: HappyThreadPool({
+    size: Math.min(os.cpu_count, 4)
+  }),
 
-const cssLoaders = [
+  cssLoaders: [
   // https://github.com/webpack-contrib/mini-css-extract-plugin#user-content-advanced-configuration-example
   // TODO: remove style-loader: https://github.com/webpack-contrib/mini-css-extract-plugin/issues/34
-  extractCSS ? MiniCssExtractPlugin.loader : 'style-loader',
-  { loader: 'css-loader', options: { sourceMap: !isProd } },
-  { loader: 'postcss-loader', options: { sourceMap: !isProd } },
-]
+    { 
+      loader: 'pre-css-loader',
+      module: extractCSS ? MiniCssExtractPlugin.loader : TerserPlugin({ extractComments: 'all' }),
+      options: { sourceMap: !isProd },
+    },
+    { loader: 'css-loader', options: { sourceMap: !isProd } },
+    { loader: 'postcss-loader', options: { sourceMap: !isProd } },
+  ],
 
-const sassLoaders = [
-  ...cssLoaders,
-  {
+  sassLoaders: {
+    ident: this.id,
     loader: 'sass-loader',
-    options: {
-      implementation: require('sass'),
-      sassOptions: {
-        indentedSyntax: true,
-      },
+    options: [this.cssLoaders],
+    query: {
+      indentedSyntax: true,
+      hoistUseStatements: true,
     },
   },
-]
 
-const scssLoaders = [
-  ...cssLoaders,
-  {
+  scssLoaders: {
+    ident: this.id,
     loader: 'sass-loader',
-    options: {
-      implementation: require('sass'),
-      sassOptions: {
-        indentedSyntax: false,
-      },
+    options: [this.cssLoaders],
+    query: {
+      indentedSyntax: false,
+      hoistUseStatements: true,
     },
   },
-]
 
-const plugins = [
-  new FriendlyErrorsWebpackPlugin({
-    clearConsole: true,
-  }),
-]
+  plugins: [
+    FriendlyErrorsWebpackPlugin, {
+      clearConsole: true,
+    }
+  ],
 
-exports.config = {
-  mode: isProd ? 'production' : 'development',
-  resolve: {
-    extensions: ['*', '.js', '.json', '.vue', '.ts'],
-  },
-  node: {
-    fs: 'empty',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.sass$/,
-        use: sassLoaders,
+  exports: {
+    config: {
+      mode: isProd ? 'production' : 'development',
+      resolve: {
+        extensions: ['*', '.js', '.jsx', '.json', '.vue', '.ts', '.tsx'],
       },
-      {
-        test: /\.scss$/,
-        use: scssLoaders,
+      module: {
+        rules: [
+          {
+            test: /\.sass$/,
+            use: this.sassLoaders,
+          },
+          {
+            test: /\.scss$/,
+            use: this.scssLoaders,
+          },
+        ],
       },
-    ],
+      plugins: this.plugins,
+      performance: {
+        hints: false,
+      },
+      stats: { children: false }
+    },
   },
-  plugins,
-  performance: {
-    hints: false,
-  },
-  stats: { children: false },
 }
