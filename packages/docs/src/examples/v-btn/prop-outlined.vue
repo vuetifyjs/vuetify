457903e1f8c79be1e5494a07c42e88df063c2f8b
John Leider <john.j.leider@gmail.com>
2021-06-18 15:22:22 -0500

    docs(variant): update api-generator json and doc examples

--- packages/docs/src/examples/v-btn/prop-outlined.vue
+++ packages/docs/src/examples/v-btn/prop-outlined.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="d-flex justify-space-around align-center flex-column flex-sm-row">
     <v-btn
-      outlined
+      variant="outlined"
       color="primary"
     >
       Outlined Button
     </v-btn>
     <v-btn
-      outlined
+      variant="outlined"
       icon
       color="secondary"
     >
       <v-icon>mdi-format-list-bulleted-square</v-icon>
     </v-btn>
     <v-btn
-      outlined
+      variant="outlined"
       size="large"
       icon
       color="info"
