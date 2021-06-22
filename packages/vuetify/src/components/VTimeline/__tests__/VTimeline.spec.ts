a66619ee22d2d709a9da07a3158a0d3389545f48
Kael <kaelwd@gmail.com>
2021-06-23 01:01:35 +1000

    test: update snapshots

--- packages/vuetify/src/components/VTimeline/__tests__/VTimeline.spec.ts
+++ packages/vuetify/src/components/VTimeline/__tests__/VTimeline.spec.ts
@@ -69,7 +69,7 @@ describe('VTimeline', () => {
   it('should show all items on single side', () => {
     const wrapper = mountFunction({
       props: {
-        side: 'after',
+        side: 'end',
       },
       slots: {
         default: () => createRange(3).map(() => h(VTimelineItem, {}, () => 'item content')),
