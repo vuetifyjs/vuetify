d06d1b4c239c47f01b7aeb097ddacfc30c61c08a
Kael <kaelwd@gmail.com>
2021-07-20 02:42:54 +1000

    chore: update vite

--- packages/vuetify/dev/index.js
+++ packages/vuetify/dev/index.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
-import viteSSR from 'vite-ssr'
+import viteSSR from 'vite-ssr/vue'
 import App from './App.vue'
 import vuetify from './vuetify'
 import { routes } from './router'
