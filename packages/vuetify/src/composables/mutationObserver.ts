fbfe119f65bf706faa31f833de3563f26f65f99f
John Leider <john.j.leider@gmail.com>
2021-07-21 15:54:36 -0500

    chore: iterating implementation

--- packages/vuetify/src/composables/mutationObserver.ts
+++ packages/vuetify/src/composables/mutationObserver.ts
@@ -1,27 +1,25 @@
 // Utilities
 import { onBeforeUnmount, ref, watch } from 'vue'
 
-export function useMutationObserver (callback?: MutationCallback) {
-  const mutationRef = ref<HTMLElement>()
-  // const options = typeof value === 'object' && value.options
-  //   ? value.options
-  //   : hasModifiers
-  //     // If we have modifiers, use only those provided
-  //     ? {
-  //       attributes: modifierKeys.attr,
-  //       childList: modifierKeys.child,
-  //       subtree: modifierKeys.sub,
-  //       characterData: modifierKeys.char,
-  //     }
-  //     // Defaults to everything on
-  //     : {
-  //       attributes: true,
-  //       childList: true,
-  //       subtree: true,
-  //       characterData: true,
-  //     }
+export interface MutationComposableBinding {
+  attr?: boolean
+  char?: boolean
+  child?: boolean
+  once?: boolean
+  quiet?: boolean
+  sub?: boolean
+}
 
-  const observer = new MutationObserver((mutations: MutationRecord[], observer: MutationObserver) => {
+export function useMutationObserver (
+  options?: MutationComposableBinding,
+  callback?: MutationCallback
+) {
+  const mutationRef = ref<HTMLElement>()
+
+  const observer = new MutationObserver((
+    mutations: MutationRecord[],
+    observer: MutationObserver
+  ) => {
     callback?.(mutations, observer)
   })
 
@@ -30,13 +28,22 @@ export function useMutationObserver (callback?: MutationCallback) {
   })
 
   watch(mutationRef, (newValue, oldValue) => {
+    console.log(newValue, oldValue)
     if (oldValue) {
       observer.disconnect()
     }
 
-    if (newValue) observer.observe(newValue)
+    if (newValue) {
+      observer.observe(newValue, {
+        attributes: options?.attr ?? true,
+        characterData: options?.char ?? true,
+        childList: options?.child ?? true,
+        subtree: options?.sub ?? true,
+      })
+    }
   }, {
-    flush: 'post',
+    // flush: 'post',
+    deep: true,
   })
 
   return { mutationRef }
