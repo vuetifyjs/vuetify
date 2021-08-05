c2db36d6b6517c096ef169996103563b5f1dedfc
John Leider <john.j.leider@gmail.com>
2021-08-05 11:06:18 -0500

    fix(validation): cancel validation iteration once errorCount is met

--- packages/vuetify/src/composables/validation.ts
+++ packages/vuetify/src/composables/validation.ts
@@ -1,9 +1,11 @@
-import { propsFactory, wrapInArray, wrapInPromise } from '@/util'
 // Utilities
-import type { PropType, Ref } from 'vue'
-import { ref, watch } from 'vue'
+import { propsFactory, wrapInPromise } from '@/util'
+import { ref, unref, watch } from 'vue'
 
 // Types
+import type { MaybeRef } from '@/util/helpers'
+import type { PropType } from 'vue'
+
 export type ValidationRule = string | ((value: any) => string | boolean)
 export type ValidationRules = ValidationRule[]
 export type ValidationResult = string | boolean | Promise<string | boolean>
@@ -24,48 +26,50 @@ export const makeValidationProps = propsFactory({
   },
 })
 
-export function useValidation (props: ValidationProps, value: Ref<any>) {
+export function useValidation (props: ValidationProps, value: MaybeRef<any>) {
   const errorMessages = ref<string[]>([])
   const isPristine = ref(true)
   const isValid = ref<null | boolean>(null)
   const isValidating = ref(false)
+  const errorCount = ref(0)
+  const unwrefed = unref(value)
 
   watch(isValid, () => isPristine.value = false)
 
   async function validate () {
     await reset()
 
-    const rules = wrapInArray(props.rules)
-
     isValidating.value = true
-    for (const rule of rules) {
+    for (const rule of props.rules) {
       const handler = typeof rule === 'function' ? rule : () => rule
-      const valid = await wrapInPromise<ValidationResult>(handler(value))
+      const valid = await wrapInPromise<ValidationResult>(handler(unwrefed.value))
 
       if (valid === true) continue
 
-      isValid.value = !!valid
+      errorCount.value += 1
 
       if (typeof valid === 'string') {
         errorMessages.value.push(valid)
       }
 
-      if (errorMessages.value.length >= props.errorCount) {
+      if (errorCount.value >= (props.errorCount || 1)) {
         break
       }
     }
+    isValid.value = errorCount.value > 0
     isValidating.value = false
 
     isValid.value = errorMessages.value.length === 0
   }
 
   async function reset () {
+    errorCount.value = 0
     errorMessages.value = []
     isValid.value = null
-    isValidating.value = false
   }
 
   return {
+    errorCount,
     errorMessages,
     isPristine,
     isValid,
