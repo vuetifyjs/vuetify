ca38b59501836dfbee80e366cb83450adb69446d
John Leider <john.j.leider@gmail.com>
2021-08-06 20:05:45 -0500

    refactor(filter.ts): improve typings, unit tests, and process

---
+++ packages/vuetify/src/composables/__tests__/filter.spec.ts
@@ -0,0 +1,74 @@
+// Utilities
+import { defaultFilter, useFilter } from '../filter'
+import { describe, expect, it } from '@jest/globals'
+import { ref } from 'vue'
+
+describe('filter.ts', () => {
+  describe('defaultFilter', () => {
+    it.each([
+      [null, null, true],
+      ['foo', null, true],
+      ['foo', 'foo', true],
+      ['foo', 'f', true],
+      [null, 'foo', false],
+      ['foo', 'bar', false],
+      [1, '1', false],
+      ['1', 1, true],
+    ])('should compare %s to %s and return a boolean', (text: any, query: any, expected: boolean) => {
+      expect(defaultFilter(text, query)).toBe(expected)
+    })
+  })
+
+  describe('useFilter', () => {
+    const items = ref(Array.from({ length: 50 }, (v, k) => `item-${k}`))
+
+    it.each([
+      ['', items.value.length],
+      [null, items.value.length],
+      ['1', 14],
+      [1, 14],
+      ['0', 5],
+      ['14', 1],
+      ['foo', 0],
+    ])('should return a array of filtered items from value %s', (text: any, expected: number) => {
+      const { filtered } = useFilter({}, items, ref(text))
+
+      expect(filtered.value).toHaveLength(expected)
+    })
+
+    it('should warn of inproper value', () => {
+      const { filtered } = useFilter({}, ref([{}]), ref('foo'))
+
+      expect(filtered.value).toHaveLength(0)
+
+      // eslint-disable-next-line max-len
+      expect('[Vuetify] The default filter function expects a string | number, received [object Object]. Update the item value or use a custom filter.').toHaveBeenTipped()
+    })
+
+    it('should accept a custom filter function', () => {
+      function filterFn (item: Dictionary<string>, query?: string) {
+        if (typeof query !== 'string') return true
+
+        return item.text.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      }
+      const query = ref('zz')
+      const items = ref([
+        { text: 'foo' },
+        { text: 'bar' },
+        { text: 'fizz' },
+        { text: 'buzz' },
+      ])
+      const { filtered } = useFilter({ filterFn }, items, query)
+
+      expect(filtered.value).toHaveLength(2)
+
+      query.value = 'foo'
+
+      expect(filtered.value).toHaveLength(1)
+
+      items.value.push({ text: 'foobar' })
+
+      expect(filtered.value).toHaveLength(2)
+    })
+  })
+})
--- packages/vuetify/src/composables/filter.ts
+++ packages/vuetify/src/composables/filter.ts
@@ -1,26 +1,60 @@
 // Utilities
 import { computed } from 'vue'
+import { consoleWarn, propsFactory } from '@/util'
 
 // Types
-import type { Ref } from 'vue'
+import type { PropType, Ref } from 'vue'
 
-export interface FilterProps {
-  filter?: (text: string, query: any) => boolean
+export function defaultFilter (text: string, query?: string) {
+  if (typeof query !== 'string') return true
+  if (typeof text !== 'string') return false
+
+  return text.toLocaleLowerCase().includes(query.toLocaleLowerCase())
 }
 
-export function useFilter (props: FilterProps, items: Ref<any[]>, query: Ref<any>) {
-  function filter (text: string, query: any) {
-    if (typeof props.filter === 'function') return props.filter(text, query)
+export interface FilterProps {
+  filterFn?: (item: any, query?: string) => boolean
+}
 
-    if (query == null || query === false) return true
+// Composables
+export const makeFilterProps = propsFactory({
+  filterFn: Function as unknown as PropType<FilterProps['filterFn']>,
+}, 'filter')
 
-    return text.toLocaleLowerCase().includes(query.value.toLocaleLowerCase())
-  }
+export function useFilter (
+  props: FilterProps,
+  items: Ref<unknown[]>,
+  query?: Ref<string | number>
+) {
+  const strQuery = computed(() => (
+    typeof query?.value !== 'string' &&
+    typeof query?.value !== 'number'
+  ) ? '' : String(query.value))
 
-  const filtered = computed(() => items.value.filter((item: any) => filter(item, query)))
+  const filtered = computed(() => {
+    if (!strQuery.value) return items.value
 
-  return {
-    filtered,
-    filter,
-  }
+    const array: (typeof items.value) = []
+
+    for (const item of items.value) {
+      if (props.filterFn) {
+        if (props.filterFn(item, strQuery.value)) {
+          array.push(item)
+        }
+      } else if (
+        typeof item === 'string' ||
+        typeof item === 'number'
+      ) {
+        if (defaultFilter(String(item), strQuery.value)) {
+          array.push(item)
+        }
+      } else {
+        consoleWarn(`The default filter function expects a string | number, received ${item}. Update the item value or use a custom filter.`)
+      }
+    }
+
+    return array
+  })
+
+  return { filtered }
 }
