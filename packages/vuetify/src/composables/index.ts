d4b13b8a171985085af290960a8df1f5409e838a
Glen <glen.84@gmail.com>
2021-08-09 10:38:02 +0200

    Export DisplayBreakpoint

--- packages/vuetify/src/composables/index.ts
+++ packages/vuetify/src/composables/index.ts
@@ -1,3 +1,3 @@
-export { useDisplay } from './display'
+export { DisplayBreakpoint, useDisplay } from './display'
 export { IconAliases, IconProps, IconSet } from './icons'
 export { ThemeDefinition } from './theme'
