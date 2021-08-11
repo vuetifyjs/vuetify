eb383371f6ff1b997643efd18d4bc53ce210af2c
Kael <kaelwd@gmail.com>
2021-08-12 03:59:04 +1000

    fix(convertToUnit): check isNaN before isFinite

--- packages/vuetify/src/util/helpers.ts
+++ packages/vuetify/src/util/helpers.ts
@@ -118,10 +118,12 @@ export function filterObjectOnKeys<T, K extends keyof T> (obj: T, keys: K[]): { 
 export function convertToUnit (str: number, unit?: string): string
 export function convertToUnit (str: string | number | null | undefined, unit?: string): string | undefined
 export function convertToUnit (str: string | number | null | undefined, unit = 'px'): string | undefined {
-  if (str == null || str === '' || !isFinite(+str!)) {
+  if (str == null || str === '') {
     return undefined
   } else if (isNaN(+str!)) {
     return String(str)
+  } else if (!isFinite(+str!)) {
+    return undefined
   } else {
     return `${Number(str)}${unit}`
   }
