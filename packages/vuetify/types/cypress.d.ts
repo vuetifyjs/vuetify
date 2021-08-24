0816782120c316a19147757ca60d6e2c207b83fd
Kael <kaelwd@gmail.com>
2021-08-25 00:28:43 +1000

    chore(test): allow mount to take a VNode

--- packages/vuetify/types/cypress.d.ts
+++ packages/vuetify/types/cypress.d.ts
@@ -1,11 +1,12 @@
 import 'cypress-file-upload'
 import type { mount as cyMount } from '@cypress/vue'
 import type { SnapshotOptions } from '@percy/core'
+import type { MountingOptions } from '@vue/test-utils'
 
 declare global {
   namespace Cypress {
     export interface Chainable {
-      mount: typeof cyMount
+      mount: typeof cyMount & ((component: JSX.Element, options?: MountingOptions<any>) => Cypress.Chainable)
       setProps (props: Record<string, unknown>): Cypress.Chainable
       getBySel (dataTestAttribute: string, args?: any): Chainable<Element>
       percySnapshot (
