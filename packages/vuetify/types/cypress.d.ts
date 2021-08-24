42e2a5d7d17586c6eac4f040b44a47a30d3ab0c9
Jessica Sachs <jess@jessicasachs.io>
2021-08-24 11:08:03 -0400

    test: add additional v-btn tests (#13692)
    Co-authored-by: Lachlan Miller <lachlan.miller.1990@outlook.com>
    Co-authored-by: Kael <kaelwd@gmail.com>

--- packages/vuetify/types/cypress.d.ts
+++ packages/vuetify/types/cypress.d.ts
@@ -1,13 +1,15 @@
 import 'cypress-file-upload'
 import type { mount as cyMount } from '@cypress/vue'
 import type { SnapshotOptions } from '@percy/core'
+import type { MountingOptions } from '@vue/test-utils'
 
 declare global {
   namespace Cypress {
     export interface Chainable {
-      mount: typeof cyMount
-      getBySel(dataTestAttribute: string, args?: any): Chainable<Element>
-      percySnapshot(
+      mount: typeof cyMount & ((component: JSX.Element, options?: MountingOptions<any>) => Cypress.Chainable)
+      setProps (props: Record<string, unknown>): Cypress.Chainable
+      getBySel (dataTestAttribute: string, args?: any): Chainable<Element>
+      percySnapshot (
         name?: string,
         options?: SnapshotOptions
       ): Chainable
