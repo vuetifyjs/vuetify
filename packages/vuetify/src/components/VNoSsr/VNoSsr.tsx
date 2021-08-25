e84f00246143b18d767f555f2e08cfa7548398c7
Blaine Landowski <blalan05@gmail.com>
2021-08-25 07:32:58 -0500

    fix(VNoSsr): Only return slot

--- packages/vuetify/src/components/VNoSsr/VNoSsr.tsx
+++ packages/vuetify/src/components/VNoSsr/VNoSsr.tsx
@@ -1,5 +1,3 @@
-// Composables
-import { makeTagProps } from '@/composables/tag'
 
 // Utilities
 import { onMounted, ref } from 'vue'
@@ -8,24 +6,14 @@ import { defineComponent } from '@/util'
 export default defineComponent({
   name: 'VNoSsr',
 
-  props: {
-    ...makeTagProps({ tag: 'div' }),
-  },
-
-  setup (props, { attrs, slots }) {
+  setup (props, { slots }) {
     const show = ref(false)
 
     onMounted(() => {
       show.value = true
     })
 
-    return () => {
-      return show.value && (
-        <props.tag>
-          { slots.default?.() }
-        </props.tag>
-      )
-    }
+    return () => show.value && slots.default?.()
   },
 
 })
