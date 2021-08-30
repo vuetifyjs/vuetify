3f3ecea417ce5f93bcc7323670b04d88136306d7
Kael <kaelwd@gmail.com>
2021-08-31 04:52:03 +1000

    feat: define package exports

--- packages/vuetify/build/rollup.types.config.js
+++ packages/vuetify/build/rollup.types.config.js
@@ -37,4 +37,12 @@ export default [
   createTypesConfig('entry-bundler.d.ts', 'dist/vuetify.d.ts'),
   createTypesConfig('components/index.d.ts', 'lib/components/index.d.ts'),
   createTypesConfig('directives/index.d.ts', 'lib/directives/index.d.ts'),
+  createTypesConfig('composables/index.d.ts', 'lib/composables/index.d.ts'),
+  createTypesConfig('locale/index.d.ts', 'lib/locale/index.d.ts'),
+  createTypesConfig('iconsets/fa.d.ts', 'lib/iconsets/fa.d.ts'),
+  createTypesConfig('iconsets/fa4.d.ts', 'lib/iconsets/fa4.d.ts'),
+  createTypesConfig('iconsets/fa-svg.d.ts', 'lib/iconsets/fa-svg.d.ts'),
+  createTypesConfig('iconsets/md.d.ts', 'lib/iconsets/md.d.ts'),
+  createTypesConfig('iconsets/mdi.d.ts', 'lib/iconsets/mdi.d.ts'),
+  createTypesConfig('iconsets/mdi-svg.d.ts', 'lib/iconsets/mdi-svg.d.ts'),
 ]
