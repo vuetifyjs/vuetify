425380f769d361ee8da32c17e4336e8d1809069f
Blaine Landowski <blalan05@gmail.com>
2021-09-01 07:01:00 -0500

    fix(VHydration): don't call for element twice

--- packages/vuetify/src/composables/hydration.ts
+++ packages/vuetify/src/composables/hydration.ts
@@ -4,7 +4,7 @@ import { getCurrentInstance, onMounted } from 'vue'
 export function useHydration (callback: () => void) {
   if (!IN_BROWSER) return
   const vm = getCurrentInstance()
-  const elId = vm?.root?.appContext?.app?._container?.id
-  // @ts-expect-error
-  return document.querySelector(`#${elId}`)?.__vue_app__ ? callback() : onMounted(callback)
+  const rootEl = vm?.root?.appContext?.app?._container
+
+  return rootEl?.__vue_app__ ? callback() : onMounted(callback)
 }
