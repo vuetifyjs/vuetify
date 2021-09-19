6da07e0f8e4a7ca52512010471d8b40f4866c3c9
John Leider <john.j.leider@gmail.com>
2021-09-18 22:43:48 -0500

    feat(VTextarea): port to v3

---
+++ packages/vuetify/src/components/VTextarea/VTextarea.tsx
@@ -0,0 +1,211 @@
+// Styles
+import './VTextarea.sass'
+
+// Components
+import { makeVFieldProps } from '@/components/VField/VField'
+import { VCounter } from '@/components/VCounter'
+import { VField } from '@/components/VField'
+
+// Composables
+import { useProxiedModel } from '@/composables/proxiedModel'
+
+// Directives
+import Intersect from '@/directives/intersect'
+
+// Utilities
+import { computed, nextTick, onMounted, ref } from 'vue'
+import { convertToUnit, defineComponent, pick, useRender } from '@/util'
+
+// Types
+import type { PropType } from 'vue'
+import type { VFieldSlot } from '@/components/VField/VField'
+
+export const VTextarea = defineComponent({
+  name: 'VTextarea',
+
+  directives: { Intersect },
+
+  inheritAttrs: false,
+
+  props: {
+    autoGrow: Boolean,
+    autofocus: Boolean,
+    counter: [Boolean, Number, String] as PropType<true | number | string>,
+    counterValue: Function as PropType<(value: any) => number>,
+    prefix: String,
+    placeholder: String,
+    persistentPlaceholder: Boolean,
+    persistentCounter: Boolean,
+    noResize: Boolean,
+    rowHeight: {
+      type: [Number, String],
+      default: 24,
+      validator: (v: any) => !isNaN(parseFloat(v)),
+    },
+    rows: {
+      type: [Number, String],
+      default: 5,
+      validator: (v: any) => !isNaN(parseInt(v, 10)),
+    },
+    suffix: String,
+
+    ...makeVFieldProps(),
+  },
+
+  setup (props, { attrs, slots }) {
+    const model = useProxiedModel(props, 'modelValue')
+
+    const controlHeight = ref('auto')
+    const internalDirty = ref(false)
+    const isDirty = computed(() => {
+      return internalDirty.value || !!model.value
+    })
+
+    const counterValue = computed(() => {
+      return typeof props.counterValue === 'function'
+        ? props.counterValue(model.value)
+        : (model.value || '').toString().length
+    })
+    const max = computed(() => {
+      if (attrs.maxlength) return attrs.maxlength as undefined
+
+      if (
+        !props.counter ||
+        (typeof props.counter !== 'number' &&
+        typeof props.counter !== 'string')
+      ) return undefined
+
+      return props.counter
+    })
+
+    function onIntersect (
+      isIntersecting: boolean,
+      entries: IntersectionObserverEntry[]
+    ) {
+      if (!props.autofocus || !isIntersecting) return
+
+      (entries[0].target as HTMLInputElement)?.focus?.()
+    }
+
+    const fieldRef = ref<VField>()
+    function focus () {
+      fieldRef.value?.inputRef?.focus()
+    }
+    function blur () {
+      fieldRef.value?.inputRef?.blur()
+    }
+
+    function calculateInputHeight () {
+      if (!props.autoGrow) return
+
+      controlHeight.value = 'auto'
+
+      nextTick(() => {
+        const height = fieldRef?.value?.inputRef?.scrollHeight
+        const minHeight = parseInt(props.rows, 10) * parseFloat(props.rowHeight)
+
+        controlHeight.value = convertToUnit(Math.max(minHeight, height ?? 0))
+      })
+    }
+
+    onMounted(calculateInputHeight)
+
+    useRender(() => {
+      const hasCounter = (slots.counter || props.counter || props.counterValue)
+      const [_, restAttrs] = pick(attrs, ['class'])
+
+      return (
+        <VField
+          ref={ fieldRef }
+          class={[
+            'v-textarea',
+            {
+              'v-textarea--prefixed': props.prefix,
+              'v-textarea--suffixed': props.suffix,
+              'v-textarea--auto-grow': props.autoGrow,
+              'v-textarea--no-resize': props.noResize || props.autoGrow,
+            },
+            attrs.class,
+          ]}
+          style={{
+            '--v-input-control-height': controlHeight.value,
+          }}
+          active={ isDirty.value }
+          onUpdate:active={ val => internalDirty.value = val }
+          onClick:control={ focus }
+          onClick:clear={ e => {
+            e.stopPropagation()
+
+            model.value = ''
+          }}
+          role="textbox"
+          { ...attrs }
+          { ...props }
+          v-slots={{
+            ...slots,
+            default: ({
+              isActive,
+              isDisabled,
+              isReadonly,
+              inputRef,
+              props: { class: fieldClass, ...slotProps },
+            }: VFieldSlot) => {
+              const showPlaceholder = isActive || props.persistentPlaceholder
+              return (
+                <>
+                  { props.prefix && (
+                    <span class="v-textarea__prefix" style={{ opacity: showPlaceholder ? undefined : '0' }}>
+                      { props.prefix }
+                    </span>
+                  ) }
+
+                  <textarea
+                    class={ fieldClass }
+                    style={{ opacity: showPlaceholder ? undefined : '0' }} // can't this just be a class?
+                    v-model={ model.value }
+                    v-intersect={[{
+                      handler: onIntersect,
+                    }, null, ['once']]}
+                    ref={ inputRef }
+                    autofocus={ props.autofocus }
+                    readonly={ isReadonly }
+                    disabled={ isDisabled }
+                    placeholder={ props.placeholder }
+                    onInput={ calculateInputHeight }
+                    rows={ props.rows }
+                    { ...slotProps }
+                    { ...restAttrs }
+                  />
+
+                  { props.suffix && (
+                    <span class="v-textarea__suffix" style={{ opacity: showPlaceholder ? undefined : '0' }}>
+                      { props.suffix }
+                    </span>
+                  ) }
+                </>
+              )
+            },
+            details: hasCounter && (({ isFocused }: VFieldSlot) => (
+              <>
+                <span />
+
+                <VCounter
+                  active={ props.persistentCounter || isFocused }
+                  value={ counterValue.value }
+                  max={ max.value }
+                  v-slots={ slots.counter }
+                />
+              </>
+            )),
+          }}
+        />
+      )
+    })
+
+    return {
+      fieldRef,
+      focus,
+      blur,
+    }
+  },
+})
--- packages/vuetify/src/components/VTextarea/__tests__/VTextarea.spec.ts
+++ packages/vuetify/src/components/VTextarea/__tests__/VTextarea.spec.ts
@@ -1,150 +1,150 @@
-// @ts-nocheck
-/* eslint-disable */
+// // @ts-nocheck
+// /* eslint-disable */
 
-// import { keyCodes } from '../../../util/helpers'
-// import VTextarea from '../VTextarea'
-import {
-  mount,
-  MountOptions,
-  Wrapper,
-} from '@vue/test-utils'
-// import { wait } from '../../../../test'
+// // import { keyCodes } from '../../../util/helpers'
+// // import VTextarea from '../VTextarea'
+// import {
+//   mount,
+//   MountOptions,
+//   Wrapper,
+// } from '@vue/test-utils'
+// // import { wait } from '../../../../test'
 
-describe.skip('VTextarea.ts', () => {
-  type Instance = InstanceType<typeof VTextarea>
-  let mountFunction: (options?: MountOptions<Instance>) => Wrapper<Instance>
-  beforeEach(() => {
-    mountFunction = (options?: MountOptions<Instance>) => {
-      return mount(VTextarea, options)
-    }
-  })
+// describe.skip('VTextarea.ts', () => {
+//   type Instance = InstanceType<typeof VTextarea>
+//   let mountFunction: (options?: MountOptions<Instance>) => Wrapper<Instance>
+//   beforeEach(() => {
+//     mountFunction = (options?: MountOptions<Instance>) => {
+//       return mount(VTextarea, options)
+//     }
+//   })
 
-  it('should calculate element height when using auto-grow prop', async () => {
-    const wrapper = mountFunction({
-      attachToDocument: true,
-      propsData: {
-        value: '',
-        autoGrow: true,
-      },
-    })
-    const input = jest.fn(value => wrapper.setProps({ value }))
-    wrapper.vm.$on('input', input)
+//   it('should calculate element height when using auto-grow prop', async () => {
+//     const wrapper = mountFunction({
+//       attachToDocument: true,
+//       propsData: {
+//         value: '',
+//         autoGrow: true,
+//       },
+//     })
+//     const input = jest.fn(value => wrapper.setProps({ value }))
+//     wrapper.vm.$on('input', input)
 
-    const el = wrapper.findAll('textarea').at(0)
+//     const el = wrapper.findAll('textarea').at(0)
 
-    el.trigger('focus')
-    await wrapper.vm.$nextTick()
-    el.element.value = 'this is a really long text that should hopefully make auto-grow kick in. maybe?'.replace(/\s/g, '\n')
-    el.trigger('input')
-    await wrapper.vm.$nextTick()
+//     el.trigger('focus')
+//     await wrapper.vm.$nextTick()
+//     el.element.value = 'this is a really long text that should hopefully make auto-grow kick in. maybe?'.replace(/\s/g, '\n')
+//     el.trigger('input')
+//     await wrapper.vm.$nextTick()
 
-    // TODO: switch to e2e, jest doesn't do inline styles
-    expect(wrapper.html()).toMatchSnapshot()
-    expect(el.element.style.getPropertyValue('height')).not.toHaveLength(0)
-  })
+//     // TODO: switch to e2e, jest doesn't do inline styles
+//     expect(wrapper.html()).toMatchSnapshot()
+//     expect(el.element.style.getPropertyValue('height')).not.toHaveLength(0)
+//   })
 
-  it('should watch lazy value', async () => {
-    const wrapper = mountFunction()
+//   it('should watch lazy value', async () => {
+//     const wrapper = mountFunction()
 
-    const calculateInputHeight = jest.fn()
-    wrapper.setMethods({ calculateInputHeight })
+//     const calculateInputHeight = jest.fn()
+//     wrapper.setMethods({ calculateInputHeight })
 
-    wrapper.vm.lazyValue = 'foo'
+//     wrapper.vm.lazyValue = 'foo'
 
-    expect(calculateInputHeight).not.toHaveBeenCalled()
+//     expect(calculateInputHeight).not.toHaveBeenCalled()
 
-    wrapper.setProps({ autoGrow: true })
+//     wrapper.setProps({ autoGrow: true })
 
-    wrapper.vm.lazyValue = 'bar'
+//     wrapper.vm.lazyValue = 'bar'
 
-    // wait for watcher
-    await wrapper.vm.$nextTick()
+//     // wait for watcher
+//     await wrapper.vm.$nextTick()
 
-    expect(calculateInputHeight).toHaveBeenCalled()
-  })
+//     expect(calculateInputHeight).toHaveBeenCalled()
+//   })
 
-  it('should calculate height on mounted', async () => {
-    const calculateInputHeight = jest.fn()
+//   it('should calculate height on mounted', async () => {
+//     const calculateInputHeight = jest.fn()
 
-    mountFunction({
-      attachToDocument: true,
-      propsData: {
-        autoGrow: true,
-      },
-      methods: { calculateInputHeight },
-    })
+//     mountFunction({
+//       attachToDocument: true,
+//       propsData: {
+//         autoGrow: true,
+//       },
+//       methods: { calculateInputHeight },
+//     })
 
-    await wait()
-    expect(calculateInputHeight).toHaveBeenCalled()
-  })
+//     await wait()
+//     expect(calculateInputHeight).toHaveBeenCalled()
+//   })
 
-  it('should stop propagation', async () => {
-    const wrapper = mountFunction()
+//   it('should stop propagation', async () => {
+//     const wrapper = mountFunction()
 
-    const stopPropagation = jest.fn()
-    const onKeyDown = {
-      keyCode: keyCodes.enter,
-      stopPropagation,
-    }
-    wrapper.vm.onKeyDown(onKeyDown)
+//     const stopPropagation = jest.fn()
+//     const onKeyDown = {
+//       keyCode: keyCodes.enter,
+//       stopPropagation,
+//     }
+//     wrapper.vm.onKeyDown(onKeyDown)
 
-    expect(stopPropagation).not.toHaveBeenCalled()
+//     expect(stopPropagation).not.toHaveBeenCalled()
 
-    wrapper.setData({ isFocused: true })
+//     wrapper.setData({ isFocused: true })
 
-    wrapper.vm.onKeyDown(onKeyDown)
+//     wrapper.vm.onKeyDown(onKeyDown)
 
-    expect(stopPropagation).toHaveBeenCalled()
-  })
+//     expect(stopPropagation).toHaveBeenCalled()
+//   })
 
-  it('should render no-resize the same if already auto-grow', () => {
-    const wrappers = [
-      { autoGrow: true, outlined: false },
-      { autoGrow: true, outlined: true },
-    ].map(propsData => mountFunction({ propsData }))
+//   it('should render no-resize the same if already auto-grow', () => {
+//     const wrappers = [
+//       { autoGrow: true, outlined: false },
+//       { autoGrow: true, outlined: true },
+//     ].map(propsData => mountFunction({ propsData }))
 
-    wrappers.forEach(async wrapper => {
-      await wrapper.vm.$nextTick()
-      const html1 = wrapper.html()
+//     wrappers.forEach(async wrapper => {
+//       await wrapper.vm.$nextTick()
+//       const html1 = wrapper.html()
 
-      wrapper.setProps({ noResize: true })
-      // will still pass without this, do not remove
-      await wrapper.vm.$nextTick()
-      const html2 = wrapper.html()
+//       wrapper.setProps({ noResize: true })
+//       // will still pass without this, do not remove
+//       await wrapper.vm.$nextTick()
+//       const html2 = wrapper.html()
 
-      expect(html2).toBe(html1)
-    })
-  })
+//       expect(html2).toBe(html1)
+//     })
+//   })
 
-  it('should emit keydown event', () => {
-    const wrapper = mountFunction()
-    const keydown = jest.fn()
-    const textarea = wrapper.find('textarea')
-    wrapper.vm.$on('keydown', keydown)
+//   it('should emit keydown event', () => {
+//     const wrapper = mountFunction()
+//     const keydown = jest.fn()
+//     const textarea = wrapper.find('textarea')
+//     wrapper.vm.$on('keydown', keydown)
 
-    textarea.trigger('focus')
-    textarea.element.value = 'foobar'
-    textarea.trigger('input')
-    textarea.trigger('keydown.enter')
+//     textarea.trigger('focus')
+//     textarea.element.value = 'foobar'
+//     textarea.trigger('input')
+//     textarea.trigger('keydown.enter')
 
-    expect(keydown).toHaveBeenCalled()
-  })
+//     expect(keydown).toHaveBeenCalled()
+//   })
 
-  it('should dynamically adjust row-height', async () => {
-    const wrapper = mountFunction({
-      propsData: {
-        autoGrow: true,
-      },
-    })
+//   it('should dynamically adjust row-height', async () => {
+//     const wrapper = mountFunction({
+//       propsData: {
+//         autoGrow: true,
+//       },
+//     })
 
-    await wait()
+//     await wait()
 
-    expect(wrapper.vm.$refs.input.style.height).toBe('120px')
+//     expect(wrapper.vm.$refs.input.style.height).toBe('120px')
 
-    wrapper.setProps({ rowHeight: 30 })
+//     wrapper.setProps({ rowHeight: 30 })
 
-    await wrapper.vm.$nextTick()
+//     await wrapper.vm.$nextTick()
 
-    expect(wrapper.vm.$refs.input.style.height).toBe('150px')
-  })
-})
+//     expect(wrapper.vm.$refs.input.style.height).toBe('150px')
+//   })
+// })
