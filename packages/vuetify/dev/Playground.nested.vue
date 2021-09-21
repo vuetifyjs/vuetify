51cb28ebf0c3d4800cd461b01243b37f771620a6
nekosaur <albert@kaaman.nu>
2021-09-21 18:03:59 +0200

    wip: add playground

--- packages/vuetify/dev/Playground.nested.vue
+++ packages/vuetify/dev/Playground.nested.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <div class="pa-10 d-flex justify-space-around">
-      <v-nested>
+      <!-- <v-nested>
         <v-nested-item value="a">a</v-nested-item>
         <v-nested-group value="group1">
           <template v-slot:header>
@@ -23,9 +23,9 @@
           <v-nested-item value="c">c</v-nested-item>
           <v-nested-item value="d">d</v-nested-item>
         </v-nested-group>
-      </v-nested>
+      </v-nested>-->
 
-      <v-nested v-model:selected="foo" :items="items" select-strategy="independent" />
+      <!-- <v-nested v-model:selected="foo" :items="items" select-strategy="independent" /> -->
 
       <v-nested :items="loadedItems" :open-strategy="loadItems" :opened="opened" />
 
@@ -34,6 +34,89 @@
     <div>
       {{ foo }}
     </div>
+
+    <v-list v-model:selected="test" v-model:opened="open">
+      <v-list-item active-color="primary" to="/" exact>foo</v-list-item>
+      <v-list-item active-color="primary" to="/page1">bar</v-list-item>
+      <v-list-group>
+        <template #header="props">
+          <v-list-item active-color="primary" prepend-icon="$close" v-bind="props" :active="$route.path.startsWith('/nested')">
+            header
+          </v-list-item>
+        </template>
+        <v-list-item active-color="primary" to="/nested/page1">baz</v-list-item>
+        <v-list-item active-color="primary" to="/nested/page2">bro</v-list-item>
+      </v-list-group>
+    </v-list>
+    <div>
+      {{ test }}
+    </div>
+    <div>
+      {{ open }}
+    </div>
+
+    <v-list v-model:selected="test2" v-model:opened="open2">
+      <v-list-item value="/">foo</v-list-item>
+      <v-list-item value="/page1">bar</v-list-item>
+      <v-list-group>
+        <template #header="props">
+          <v-list-item prepend-icon="$close" v-bind="props">
+            header
+          </v-list-item>
+        </template>
+        <v-list-item value="/nested/page1">baz</v-list-item>
+        <v-list-item value="/nested/page2">bro</v-list-item>
+        <v-list-group>
+          <template #header="props">
+            <v-list-item prepend-icon="$close" v-bind="props">
+              header 2
+            </v-list-item>
+          </template>
+          <v-list-item value="/nested/foo/page1">baz 2</v-list-item>
+          <v-list-item value="/nested/foo/page2">bro 2</v-list-item>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
+    <div>
+      {{ test2 }}
+    </div>
+    <div>
+      {{ open2 }}
+    </div>
+
+    <v-list open-on-select v-model:selected="openOnSelect" select-strategy="leaf">
+      <v-list-group>
+        <template #header="props">
+          <v-list-item v-bind="props">group 1</v-list-item>
+        </template>
+        <v-list-item value="a">a</v-list-item>
+        <v-list-item value="b">b</v-list-item>
+      </v-list-group>
+      <v-list-group>
+        <template #header="props">
+          <v-list-item v-bind="props">group 2</v-list-item>
+        </template>
+        <v-list-item value="c">c</v-list-item>
+        <v-list-item value="d">d</v-list-item>
+      </v-list-group>
+      <v-list-group>
+        <template #header="props">
+          <v-list-item v-bind="props">group 3</v-list-item>
+        </template>
+        <v-list-item value="e">e</v-list-item>
+        <v-list-item value="f">f</v-list-item>
+        <v-list-group>
+          <template #header="props">
+            <v-list-item v-bind="props">group 4</v-list-item>
+          </template>
+          <v-list-item value="g">g</v-list-item>
+          <v-list-item value="h">h</v-list-item>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
+    <v-btn @click="openOnSelect = ['a', 'g']">select</v-btn>
+
+    <router-view />
   </v-app>
 </template>
 
@@ -41,7 +124,12 @@
   export default {
     name: 'Playground',
     data: () => ({
+      openOnSelect: null,
       foo: null,
+      open: null,
+      test: null,
+      open2: null,
+      test2: ['/nested/foo/page2'],
       items: [
         {
           value: 'a',
@@ -100,6 +188,7 @@
     }),
     mounted () {
       window.addEventListener('click', this.ctrlHandler, { passive: true })
+      console.log(this.$route, this.$route.path.startsWith('/nested'))
     },
     beforeUnmount () {
       window.removeEventListener('click', this.ctrlHandler, { passive: true })
@@ -179,5 +268,9 @@
   .v-nested-group
     .items
       margin-left: calc(20px)
+
+  .v-list-group__header.v-list-item--active
+    .v-list-item__overlay
+      display: none
 
 </style>
