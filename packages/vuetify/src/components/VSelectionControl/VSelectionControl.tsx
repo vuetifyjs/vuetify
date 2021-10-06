ea6eb726ad7e0d7c42fd7a7bd1828fd8d7f18818
John Leider <john.j.leider@gmail.com>
2021-10-06 14:34:20 -0500

    feat(VSelectionControl): create new component

---
+++ packages/vuetify/src/components/VSelectionControl/VSelectionControl.tsx
@@ -0,0 +1,170 @@
+// Styles
+import './VSelectionControl.sass'
+
+// Components
+import { VIcon } from '@/components/VIcon'
+import { VFieldLabel } from '@/components/VField/VFieldLabel'
+
+// Composables
+import { useTextColor } from '@/composables/color'
+import { useProxiedModel } from '@/composables/proxiedModel'
+import { makeThemeProps, useTheme } from '@/composables/theme'
+import { makeValidationProps, useValidation } from '@/composables/validation'
+
+// Directives
+import { Ripple } from '@/directives/ripple'
+
+// Utilities
+import { computed, ref } from 'vue'
+import { genericComponent, getUid, SUPPORTS_FOCUS_VISIBLE, useRender } from '@/util'
+
+// Types
+import type { ComputedRef, WritableComputedRef } from 'vue'
+import type { MakeSlots } from '@/util'
+
+export interface VSelectionControlSlot {
+  model: WritableComputedRef<any>
+  isReadonly: ComputedRef<boolean>
+  isDisabled: ComputedRef<boolean>
+  props: {
+    onBlur: (e: Event) => void
+    onFocus: (e: FocusEvent) => void
+    id: string
+  }
+}
+
+export const VSelectionControl = genericComponent<new <T>() => {
+  $props: {
+    modelValue?: T
+    'onUpdate:modelValue'?: (val: T) => any
+  }
+  $slots: MakeSlots<{
+    default: [VSelectionControlSlot]
+  }>
+}>()({
+  name: 'VSelectionControl',
+
+  directives: { Ripple },
+
+  inheritAttrs: false,
+
+  props: {
+    color: String,
+    id: String,
+    label: String,
+    offIcon: {
+      type: String,
+      required: true,
+    },
+    onIcon: {
+      type: String,
+      required: true,
+    },
+
+    ...makeThemeProps(),
+    ...makeValidationProps(),
+  },
+
+  emits: {
+    'update:modelValue': (val: any) => true,
+  },
+
+  setup (props, { attrs, slots }) {
+    const { errorMessages, isDisabled, isReadonly, isValid, validationClasses } = useValidation(props, 'v-checkbox')
+    const model = useProxiedModel(props, 'modelValue')
+    const uid = getUid()
+    const { textColorClasses, textColorStyles } = useTextColor(computed(() => {
+      return isValid.value || model.value ? props.color : undefined
+    }))
+    const icon = computed(() => {
+      return model.value ? props.onIcon : props.offIcon
+    })
+    const id = computed(() => props.id || `input-${uid}`)
+    const isFocused = ref(false)
+    const isFocusVisible = ref(false)
+
+    function onFocus (e: FocusEvent) {
+      isFocused.value = true
+      if (
+        !SUPPORTS_FOCUS_VISIBLE ||
+        (SUPPORTS_FOCUS_VISIBLE && (e.target as HTMLElement).matches(':focus-visible'))
+      ) {
+        isFocusVisible.value = true
+      }
+    }
+
+    function onBlur () {
+      isFocused.value = false
+      isFocusVisible.value = false
+    }
+
+    function onClick () {
+      if (isDisabled.value || isReadonly.value) return
+
+      model.value = !model.value
+    }
+
+    useRender(() => {
+      const label = slots.label
+        ? slots.label({
+          label: props.label,
+          props: { for: id.value },
+        })
+        : props.label
+
+      return (
+        <div
+          class={[
+            'v-selection-control',
+            {
+              'v-selection-control--focused': isFocused.value,
+              'v-selection-control--focus-visible': isFocusVisible.value,
+            },
+          ]}
+          { ...attrs }
+        >
+          <div
+            class={[
+              'v-selection-control__input',
+              textColorClasses.value,
+            ]}
+            style={ textColorStyles.value }
+            v-ripple={[
+              !isDisabled.value && !isReadonly.value,
+              null,
+              ['center', 'circle'],
+            ]}
+          >
+            <VIcon
+              icon={ icon.value }
+              onClick={ onClick }
+            />
+
+            { slots.default?.({
+              model,
+              isReadonly,
+              isDisabled,
+              props: {
+                onFocus,
+                onBlur,
+                id: id.value,
+              },
+            } as VSelectionControlSlot) }
+          </div>
+
+          <VFieldLabel
+            for={ id.value }
+            style="position: static; pointer-events: auto; cursor: pointer;"
+            onClick={ onClick }
+          >
+            { label }
+          </VFieldLabel>
+        </div>
+      )
+    })
+
+    return {}
+  },
+})
+
+export type VSelectionControl = InstanceType<typeof VSelectionControl>
