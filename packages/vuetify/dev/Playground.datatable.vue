c5c383b0ff2ddc1388c8ead355d3913d72e46d0e
nekosaur <albert@kaaman.nu>
2021-11-25 00:19:10 +0100

    add playground

---
+++ packages/vuetify/dev/Playground.datatable.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-data-table :headers="headers" :items="items"></v-data-table>
+  </v-app>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    data: () => ({
+      headers: [
+        [
+          {
+            name: 'Group',
+            colspan: 2,
+          },
+          {
+            id: 'end',
+            name: 'Two rows',
+            rowspan: 2,
+          },
+        ],
+        [
+          {
+            id: 'one',
+            name: 'One',
+          },
+          {
+            id: 'two',
+            name: 'Two',
+          },
+          {
+            id: 'three',
+            name: 'Three',
+          },
+        ],
+      ],
+      items: [
+        {
+          one: 'one',
+          two: 'two',
+          end: 'end',
+          three: 'three',
+        },
+      ],
+    }),
+  })
+</script>
