78c915cc113847412ba1d5024fb78721d5ef7c7c
Kael <kaelwd@gmail.com>
2021-12-01 01:31:58 +1100

    Update packages/vuetify/src/components/VCalendar/VCalendarWeekly.ts

--- packages/vuetify/src/components/VCalendar/VCalendarWeekly.ts
+++ packages/vuetify/src/components/VCalendar/VCalendarWeekly.ts
@@ -186,7 +186,7 @@ export default CalendarBase.extend({
         on: this.getMouseEventHandlers({
           'click:date': { event: 'click', stop: true },
           'contextmenu:date': { event: 'contextmenu', stop: true, prevent: true, result: false },
-        }, nativeEvent => { return { nativeEvent, ...day } }),
+        }, nativeEvent => ({ nativeEvent, ...day })),
       }, hasMonth
         ? this.monthFormatter(day, this.shortMonths) + ' ' + this.dayFormatter(day, false)
         : this.dayFormatter(day, false)
