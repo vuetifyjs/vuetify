444df2151dd27ff86b98765bd234d5d1d5ae5c1b
Kael <kaelwd@gmail.com>
2021-12-01 17:34:09 +1100

    chore: remove deprecated mixins and services

--- packages/vuetify/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-/* eslint-disable */
-
-export * from './application'
-export * from './breakpoint'
-export * from './goto'
-export * from './icons'
-export * from './lang'
-export * from './theme'
