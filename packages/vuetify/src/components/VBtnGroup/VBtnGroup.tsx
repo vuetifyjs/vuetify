3a49ef203099dcd61a073c14be75260b082f8e76
John Leider <john.j.leider@gmail.com>
2021-12-03 15:20:56 -0600

    refactor(VBtnGroup): implement VBtnToggle styles

--- packages/vuetify/src/components/VBtnGroup/VBtnGroup.tsx
+++ packages/vuetify/src/components/VBtnGroup/VBtnGroup.tsx
@@ -1,22 +1,65 @@
 import './VBtnGroup.sass'
 
-import { defineComponent } from '@/util'
+// Components
+import { makeTagProps } from '@/composables/tag'
+import { makeThemeProps, useTheme } from '@/composables/theme'
+
+// Composables
+import { makeBorderProps, useBorder } from '@/composables/border'
+import { makeElevationProps, useElevation } from '@/composables/elevation'
+import { makeRoundedProps, useRounded } from '@/composables/rounded'
+import { useBackgroundColor } from '@/composables/color'
+
+// Utility
+import { computed } from 'vue'
+import { defineComponent, useRender } from '@/util'
+import { makeDensityProps, useDensity } from '@/composables/density'
+
+// Types
 
 export const VBtnGroup = defineComponent({
   name: 'VBtnGroup',
 
   props: {
-    tag: {
-      type: String,
-      default: 'div',
-    },
+    color: String,
+    // probably need to use variants
+    text: Boolean,
+
+    ...makeBorderProps(),
+    ...makeDensityProps(),
+    ...makeElevationProps(),
+    ...makeRoundedProps(),
+    ...makeTagProps(),
+    ...makeThemeProps(),
   },
 
   setup (props, { slots }) {
-    return () => (
-      <props.tag class="v-btn-group">
-        { slots.default?.() }
-      </props.tag>
-    )
+    const { themeClasses } = useTheme(props)
+    const { densityClasses } = useDensity(props, 'v-btn-group')
+    const { backgroundColorClasses, backgroundColorStyles } = useBackgroundColor(computed(() => props.color))
+    const { borderClasses } = useBorder(props, 'v-btn-group')
+    const { elevationClasses } = useElevation(props)
+    const { roundedClasses } = useRounded(props, 'v-btn-group')
+
+    useRender(() => (
+      <props.tag
+        class={[
+          'v-btn-group',
+          {
+            'v-btn-group--text': props.text,
+          },
+          themeClasses.value,
+          backgroundColorClasses.value,
+          borderClasses.value,
+          densityClasses.value,
+          elevationClasses.value,
+          roundedClasses.value,
+        ]}
+        style={{
+          ...backgroundColorStyles.value,
+        }}
+        v-slots={ slots }
+      />
+    ))
   },
 })
