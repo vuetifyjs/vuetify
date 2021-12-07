cac2a31c3cc43894a25731feac043af42ca4dacb
nekosaur <albert@kaaman.nu>
2021-12-08 00:25:52 +0100

    fix(VSlider): clicking on slider track when thumbs have equal value

--- packages/vuetify/src/components/VRangeSlider/VRangeSlider.tsx
+++ packages/vuetify/src/components/VRangeSlider/VRangeSlider.tsx
@@ -40,12 +40,15 @@ export const VRangeSlider = defineComponent({
     const inputRef = ref<VInput>()
 
     function getActiveThumb (e: MouseEvent | TouchEvent) {
-      const thumbs = inputRef.value?.$el.querySelectorAll('.v-slider-thumb')
+      if (!startThumbRef.value || !stopThumbRef.value) return
 
-      const a = Math.abs(getOffset(e, thumbs[0], props.direction))
-      const b = Math.abs(getOffset(e, thumbs[1], props.direction))
+      const startOffset = getOffset(e, startThumbRef.value.$el, props.direction)
+      const stopOffset = getOffset(e, stopThumbRef.value.$el, props.direction)
 
-      return a < b ? startThumbRef.value?.$el : stopThumbRef.value?.$el
+      const a = Math.abs(startOffset)
+      const b = Math.abs(stopOffset)
+
+      return (a < b || (a === b && startOffset < 0)) ? startThumbRef.value?.$el : stopThumbRef.value?.$el
     }
 
     const {
@@ -155,9 +158,11 @@ export const VRangeSlider = defineComponent({
 
                     // Make sure second thumb is focused if
                     // the thumbs are on top of each other
+                    // and they are both at minimum value
                     // but only if focused from outside.
                     if (
                       model.value[0] === model.value[1] &&
+                      model.value[1] === min.value &&
                       e.relatedTarget !== stopThumbRef.value?.$el
                     ) stopThumbRef.value?.$el.focus()
                   } }
@@ -178,9 +183,21 @@ export const VRangeSlider = defineComponent({
                   focused={ isFocused && focusedThumb.value === stopThumbRef.value }
                   modelValue={ model.value[1] }
                   onUpdate:modelValue={ v => (model.value = [model.value[0], v]) }
-                  onFocus={ () => {
+                  onFocus={ (e: FocusEvent) => {
                     focus()
                     focusedThumb.value = stopThumbRef.value
+
+                    // Make sure first thumb is focused if
+                    // the thumbs are on top of each other
+                    // and they are both at maximum value
+                    // but only if focused from outside.
+                    if (
+                      model.value[0] === model.value[1] &&
+                      model.value[0] === max.value &&
+                      e.relatedTarget !== startThumbRef.value?.$el
+                    ) {
+                      startThumbRef.value?.$el.focus()
+                    }
                   } }
                   onBlur={ () => {
                     blur()
