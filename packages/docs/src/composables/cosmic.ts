b9d599dd0a90aee731af3bbcc0c8670c04eee92b
John Leider <john.j.leider@gmail.com>
2021-12-07 17:34:18 -0600

    docs(cosmic.ts): assign default getObjects fn
    avoid console errors when env vars aren't defined

--- packages/docs/src/composables/cosmic.ts
+++ packages/docs/src/composables/cosmic.ts
@@ -1,11 +1,15 @@
+/* eslint-disable camelcase */
 import Cosmic from 'cosmicjs'
 
 export const useCosmic = () => {
   const api = Cosmic()
-  // eslint-disable-next-line camelcase
+  let bucket = { getObjects: () => Promise.resolve([]) }
   const read_key = import.meta.env.VITE_COSMIC_BUCKET_READ_KEY
   const slug = import.meta.env.VITE_COSMIC_BUCKET_SLUG
-  const bucket = api.bucket({ slug, read_key })
+
+  if (read_key && slug) {
+    bucket = api.bucket({ slug, read_key })
+  }
 
   return { bucket }
 }
