4e04462f89a4ba4ef695020211833e70261a8a66
Kael <kaelwd@gmail.com>
2021-12-07 23:30:46 +1100

    feat(VNavigationDrawer): add smooth touch dragging

--- packages/vuetify/src/components/VNavigationDrawer/VNavigationDrawer.tsx
+++ packages/vuetify/src/components/VNavigationDrawer/VNavigationDrawer.tsx
@@ -11,9 +11,10 @@ import { useDisplay } from '@/composables/display'
 import { useProxiedModel } from '@/composables/proxiedModel'
 import { makeThemeProps, useTheme } from '@/composables/theme'
 import { useBackgroundColor } from '@/composables/color'
+import { useVelocity } from '@/composables/touch'
 
 // Utilities
-import { computed, onBeforeMount, ref, toRef, watch } from 'vue'
+import { computed, onBeforeMount, onBeforeUnmount, onMounted, ref, toRef, watch } from 'vue'
 import { defineComponent } from '@/util'
 
 // Types
@@ -73,18 +74,10 @@ export const VNavigationDrawer = defineComponent({
     const isHovering = ref(false)
     const width = computed(() => {
       return (props.rail && props.expandOnHover && isHovering.value)
-        ? props.width
+        ? Number(props.width)
         : Number(props.rail ? props.railWidth : props.width)
     })
     const isTemporary = computed(() => !props.permanent && (mobile.value || props.temporary))
-    const layoutStyles = useLayoutItem(
-      props.name,
-      toRef(props, 'priority'),
-      toRef(props, 'position'),
-      computed(() => isTemporary.value ? 0 : props.rail && props.expandOnHover ? Number(props.railWidth) : width.value),
-      width,
-      isActive,
-    )
 
     if (!props.disableResizeWatcher) {
       watch(mobile, val => !props.permanent && (isActive.value = !val))
@@ -100,11 +93,96 @@ export const VNavigationDrawer = defineComponent({
       isActive.value = props.permanent || !mobile.value
     })
 
+    const rootEl = ref<HTMLElement>()
+    onMounted(() => {
+      window.addEventListener('touchstart', onTouchstart, { passive: true })
+      window.addEventListener('touchmove', onTouchmove, { passive: false })
+      window.addEventListener('touchend', onTouchend, { passive: true })
+    })
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('touchstart', onTouchstart)
+      window.removeEventListener('touchmove', onTouchmove)
+      window.removeEventListener('touchend', onTouchend)
+    })
+
+    const { addMovement, endTouch, getVelocity } = useVelocity()
+    const dragging = ref(false)
+    const dragProgress = ref(0)
+    const offset = ref(0)
+    function onTouchstart (e: TouchEvent) {
+      if (
+        e.changedTouches[0].clientX < 100 ||
+        (isActive.value && e.changedTouches[0].clientX < width.value) ||
+        (isActive.value && isTemporary.value)
+      ) {
+        dragging.value = true
+        offset.value = isActive.value ? e.changedTouches[0].clientX - width.value : e.changedTouches[0].clientX
+        dragProgress.value = Math.min(1, (e.changedTouches[0].clientX - offset.value) / width.value)
+        endTouch(e)
+        addMovement(e)
+      }
+    }
+
+    function onTouchmove (e: TouchEvent) {
+      if (!dragging.value) return
+
+      e.preventDefault()
+      addMovement(e)
+
+      const progress = (e.changedTouches[0].clientX - offset.value) / width.value
+      dragProgress.value = Math.max(0, Math.min(1, progress))
+
+      if (progress > 1) {
+        offset.value = e.changedTouches[0].clientX - width.value
+      }
+    }
+
+    function onTouchend (e: TouchEvent) {
+      if (!dragging.value) return
+
+      addMovement(e)
+
+      dragging.value = false
+
+      const velocity = getVelocity(e.changedTouches[0].identifier)
+      if (velocity.polar.radius > 300 && ['left', 'right'].includes(velocity.direction)) {
+        isActive.value = velocity.direction === 'right'
+      } else {
+        isActive.value = dragProgress.value > 0.5
+      }
+    }
+
+    const layoutSize = computed(() => {
+      const size = isTemporary.value ? 0
+        : props.rail && props.expandOnHover ? Number(props.railWidth)
+        : width.value
+
+      return dragging.value ? size * dragProgress.value : size
+    })
+    const layoutStyles = useLayoutItem(
+      props.name,
+      toRef(props, 'priority'),
+      toRef(props, 'position'),
+      layoutSize,
+      width,
+      computed(() => isActive.value || dragging.value),
+      computed(() => dragging.value)
+    )
+
+    const dragStyles = computed(() => {
+      return dragging.value ? {
+        transform: `translateX(calc(-100% + ${dragProgress.value * width.value}px))`,
+        // transition: 'none',
+      } : undefined
+    })
+
     return () => {
       const hasImage = (slots.image || props.image)
 
       return (
         <props.tag
+          ref={ rootEl }
           onMouseenter={ () => (isHovering.value = true) }
           onMouseleave={ () => (isHovering.value = false) }
           class={[
@@ -129,6 +207,7 @@ export const VNavigationDrawer = defineComponent({
           style={[
             backgroundColorStyles.value,
             layoutStyles.value,
+            dragStyles.value,
           ]}
         >
           { hasImage && (
