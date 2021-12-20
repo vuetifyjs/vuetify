0bcc285e7ae50899d935455273dea90bafea9e66
Kael <kaelwd@gmail.com>
2021-12-21 01:53:17 +1100

    feat(theme): support ssr with @vueuse/head

--- packages/vuetify/dev/index.js
+++ packages/vuetify/dev/index.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
 import viteSSR from 'vite-ssr/vue'
+import { createHead } from '@vueuse/head'
 import App from './App.vue'
 import vuetify from './vuetify'
 import { routes } from './router'
@@ -11,6 +12,11 @@ import { routes } from './router'
 library.add(fas)
 
 export default viteSSR(App, { routes }, ({ app }) => {
+  const head = createHead()
+
+  app.use(head)
   app.use(vuetify)
   app.component('FontAwesomeIcon', FontAwesomeIcon)
+
+  return { head }
 })
