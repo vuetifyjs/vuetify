1d6a9d47a0dbd3f15edc8d80d636537f787fa761
Adil Khan <adil.khan@portable.com.au>
2022-01-12 09:09:49 +0500

    feat(VDataTable): Reverted line 214 from ripple/index.ts. feat #14412

--- packages/vuetify/src/directives/ripple/index.ts
+++ packages/vuetify/src/directives/ripple/index.ts
@@ -211,7 +211,7 @@ function rippleHide (e: Event) {
     element._ripple.showTimerCommit = null
 
     // re-queue ripple hiding
-    element._ripple.showTimer = window.setTimeout(() => {
+    element._ripple.showTimer = setTimeout(() => {
       rippleHide(e)
     })
     return
