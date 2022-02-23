845915fc412ed7898beb1f84e5e2cbb62df0e334
John Leider <john.j.leider@gmail.com>
2022-02-23 11:24:41 -0600

    docs(VAlert): update border/type examples

--- packages/docs/src/examples/v-alert/prop-border.vue
+++ packages/docs/src/examples/v-alert/prop-border.vue
@@ -6,21 +6,21 @@
     >
       I'm an alert with a top border and primary color
     </v-alert>
-
+    <br>
     <v-alert
       border="end"
       color="secondary"
     >
       I'm an alert with an end border and secondary color
     </v-alert>
-
+    <br>
     <v-alert
       border="bottom"
       color="success"
     >
       I'm an alert with a bottom border and success color
     </v-alert>
-
+    <br>
     <v-alert
       border="start"
       color="error"
