50c63bf8842f5938620fa284b0516980f118a92b
John Leider <john.j.leider@gmail.com>
2022-02-23 14:08:18 -0600

    docs: add components index page (all)

---
+++ packages/docs/src/components/components/ListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-col cols="4">
+    <v-card
+      :image="`https://cdn.vuetifyjs.com/docs/images/components-temp/${name}.png`"
+      :to="rpath(`/components/${name}`)"
+      height="164"
+      class="mb-3"
+      elevation="0"
+    />
+
+    <h2 class="text-h6">
+      <span class="text-capitalize">{{ name }}</span>
+    </h2>
+
+    <slot />
+  </v-col>
+</template>
+
+<script lang="ts">
+  // Utilities
+  import { defineComponent } from 'vue'
+  import { rpath } from '@/util/routes'
+
+  export default defineComponent({
+    name: 'ComponentsListItem',
+
+    props: {
+      name: String,
+    },
+
+    setup () {
+      return { rpath }
+    },
+  })
+</script>
