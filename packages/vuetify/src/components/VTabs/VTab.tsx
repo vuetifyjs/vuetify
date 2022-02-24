a8ac49b540c57ad6cad26e7856a3143044abce71
Kael <kaelwd@gmail.com>
2022-02-24 16:06:31 +1100

    feat: replace slider with a static element

--- packages/vuetify/src/components/VTabs/VTab.tsx
+++ packages/vuetify/src/components/VTabs/VTab.tsx
@@ -3,14 +3,14 @@ import './VTab.sass'
 // Mixins
 
 // Utilities
-import { makeRouterProps, useLink } from '@/composables/router'
+import { makeRouterProps } from '@/composables/router'
 import { makeTagProps } from '@/composables/tag'
-import { defineComponent, pick } from '@/util'
+import { defineComponent, pick, standardEasing } from '@/util'
 import { VBtn } from '..'
 import { provideDefaults } from '@/composables/defaults'
 import { makeGroupItemProps, useGroupItem } from '@/composables/group'
 import { VTabsSymbol } from './VTabs'
-import { computed, toRef } from 'vue'
+import { computed, ref, toRef, watch } from 'vue'
 import { makeThemeProps } from '@/composables/theme'
 
 // Types
@@ -57,6 +57,37 @@ export const VTab = defineComponent({
       scoped: true,
     })
 
+    const rootEl = ref<VBtn>()
+    const sliderEl = ref<HTMLElement>()
+    watch(isSelected, isSelected => {
+      if (isSelected) {
+        const prevEl: HTMLElement | undefined = rootEl.value?.$el.parentElement?.querySelector('.v-tab--selected .v-tab__slider')
+        const nextEl = sliderEl.value
+
+        if (!prevEl || !nextEl) return
+
+        const prevBox = prevEl.getBoundingClientRect()
+        const nextBox = nextEl.getBoundingClientRect()
+
+        const delta = prevBox.x - nextBox.x
+        const origin =
+          Math.sign(delta) > 0 ? 'right'
+          : Math.sign(delta) < 0 ? 'left'
+          : 'center'
+        const width = Math.abs(delta) + (origin === 'left' ? prevBox.width : nextBox.width)
+        const scale = width / nextBox.width
+
+        const sigma = 1.5
+        nextEl.animate({
+          transform: [`translateX(${delta}px)`, `translateX(${delta / sigma}px) scaleX(${(scale - 1) / sigma + 1})`, ''],
+          transformOrigin: Array(3).fill(origin),
+        }, {
+          duration: 225,
+          easing: standardEasing,
+        })
+      }
+    })
+
     return () => {
       const [btnProps] = pick(props, [
         'href',
@@ -73,6 +104,7 @@ export const VTab = defineComponent({
 
       return (
         <VBtn
+          ref={ rootEl }
           class={[
             'v-tab',
             selectedClass.value,
@@ -81,6 +113,7 @@ export const VTab = defineComponent({
           { ...btnProps }
         >
           { slots.default ? slots.default() : props.title }
+          <div ref={ sliderEl } class="v-tab__slider"></div>
         </VBtn>
       )
     }
--- packages/vuetify/src/components/VTabs/VTabs.tsx
+++ packages/vuetify/src/components/VTabs/VTabs.tsx
@@ -3,23 +3,19 @@ import './VTabs.sass'
 
 // Components
 import { VTab } from './VTab'
-import { VTabsSlider } from './VTabsSlider'
 
 // Directives
-import Resize from '../../directives/resize'
 
 // Utilities
-import { convertToUnit } from '../../util/helpers'
 import { defineComponent } from '@/util'
 import { makeTagProps } from '@/composables/tag'
-import { computed, ref, toRef, watch } from 'vue'
+import { computed, toRef } from 'vue'
 
 // Types
 import type { InjectionKey, PropType } from 'vue'
 import { provideDefaults } from '@/composables/defaults'
 import type { GroupProvide } from '@/composables/group'
-import { makeGroupProps, useGroup } from '@/composables/group'
-import { useResizeObserver } from '@/composables/resizeObserver'
+import { makeGroupProps } from '@/composables/group'
 import { useSlideGroup } from '@/composables/slideGroup'
 import { makeDensityProps, useDensity } from '@/composables/density'
 
@@ -98,8 +94,6 @@ export const VTabs = defineComponent({
       containerListeners,
       contentRef,
       contentStyles,
-      items,
-      selected,
     } = useSlideGroup(props, VTabsSymbol)
 
     provideDefaults({
@@ -110,43 +104,8 @@ export const VTabs = defineComponent({
       },
     })
 
-    const sliderStyles = ref({})
-
-    function updateSliderStyles (el: HTMLElement) {
-      if (!selected.value.length) return
-
-      const index = items.value.findIndex(item => item.id === selected.value[0])
-
-      if (index < 0 || !el) return
-
-      const selectedElement = el.querySelectorAll('.v-tab')[index] as HTMLElement
-
-      if (props.direction === 'horizontal') {
-        sliderStyles.value = {
-          left: convertToUnit(selectedElement.offsetLeft),
-          width: convertToUnit(selectedElement.offsetWidth),
-        }
-      } else {
-        sliderStyles.value = {
-          top: convertToUnit(selectedElement.offsetTop),
-          height: convertToUnit(selectedElement.offsetHeight),
-        }
-      }
-    }
-
-    const { resizeRef } = useResizeObserver(entries => {
-      if (!entries.length) return
-
-      updateSliderStyles(entries[0].target as HTMLElement)
-    })
-
-    watch(selected, () => resizeRef.value && updateSliderStyles(resizeRef.value as HTMLElement), {
-      flush: 'post',
-    })
-
     return () => (
       <props.tag
-        ref={ resizeRef }
         class={[
           'v-tabs',
           `v-tabs--${props.direction}`,
@@ -170,10 +129,6 @@ export const VTabs = defineComponent({
             { slots.default ? slots.default() : parsedItems.value.map(item => (
               <VTab { ...item } key={ item.text } />
             )) }
-            <VTabsSlider
-              style={ sliderStyles.value }
-              color={ props.color }
-            />
           </div>
         </div>
       </props.tag>
