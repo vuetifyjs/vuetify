780031a37d5fdfd6d0c7932e2fae8dfbc2d5d24f
John Leider <john.j.leider@gmail.com>
2022-03-01 12:48:29 -0600

    test(VAppBar): fix cypress tests

--- packages/vuetify/src/components/VAppBar/__tests__/VAppBar.spec.cy.tsx
+++ packages/vuetify/src/components/VAppBar/__tests__/VAppBar.spec.cy.tsx
@@ -23,6 +23,10 @@ describe('VAppBar', () => {
 
     cy.get('.v-app-bar').should('have.css', 'height', '64px')
 
+    cy.vue().then(wrapper => wrapper.setProps({ density: 'prominent' }))
+
+    cy.get('.v-app-bar').should('have.css', 'height', '128px')
+
     cy.vue().then(wrapper => wrapper.setProps({ density: 'comfortable' }))
 
     cy.get('.v-app-bar').should('have.css', 'height', '56px')
@@ -30,19 +34,5 @@ describe('VAppBar', () => {
     cy.vue().then(wrapper => wrapper.setProps({ density: 'compact' }))
 
     cy.get('.v-app-bar').should('have.css', 'height', '48px')
-  })
-
-  it('should change height when using prominent', () => {
-    cy.mount(({ prominentHeight }: any) => (
-      <VLayout>
-        <VAppBar prominentHeight={ prominentHeight } prominent />
-      </VLayout>
-    ))
-
-    cy.get('.v-app-bar').should('have.css', 'height', '128px')
-
-    cy.vue().then(wrapper => wrapper.setProps({ prominentHeight: 200 }))
-
-    cy.get('.v-app-bar').should('have.css', 'height', '200px')
   })
 })
