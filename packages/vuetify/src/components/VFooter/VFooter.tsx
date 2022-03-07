57760d31b8550df1721c5b44b4002c16bf6c763c
John Leider <john.j.leider@gmail.com>
2022-03-06 19:07:08 -0600

    feat(VFooter): add color support

--- packages/vuetify/src/components/VFooter/VFooter.tsx
+++ packages/vuetify/src/components/VFooter/VFooter.tsx
@@ -9,14 +9,18 @@ import { makePositionProps, usePosition } from '@/composables/position'
 import { makeRoundedProps, useRounded } from '@/composables/rounded'
 import { makeTagProps } from '@/composables/tag'
 import { makeThemeProps, provideTheme } from '@/composables/theme'
+import { useBackgroundColor } from '@/composables/color'
 
 // Utilities
 import { defineComponent } from '@/util'
+import { toRef } from 'vue'
 
 export const VFooter = defineComponent({
   name: 'VFooter',
 
   props: {
+    color: String,
+
     ...makeBorderProps(),
     ...makeDimensionProps(),
     ...makeElevationProps(),
@@ -29,6 +33,7 @@ export const VFooter = defineComponent({
 
   setup (props, { slots }) {
     const { themeClasses } = provideTheme(props)
+    const { backgroundColorClasses, backgroundColorStyles } = useBackgroundColor(toRef(props, 'color'))
     const { borderClasses } = useBorder(props)
     const { dimensionStyles } = useDimension(props)
     const { elevationClasses } = useElevation(props)
@@ -40,13 +45,14 @@ export const VFooter = defineComponent({
         class={[
           'v-footer',
           themeClasses.value,
+          backgroundColorClasses.value,
           borderClasses.value,
           elevationClasses.value,
           positionClasses.value,
           roundedClasses.value,
         ]}
         style={[
-
+          backgroundColorStyles,
           dimensionStyles.value,
           positionStyles.value,
         ]}
