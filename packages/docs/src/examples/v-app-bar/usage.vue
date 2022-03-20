b34b1b3cb9c4b9cdfeca93c46a43cd7af11ccd3f
John Leider <john.j.leider@gmail.com>
2022-03-19 20:31:59 -0500

    docs(VAppBar): improve usage for dark themes

--- packages/docs/src/examples/v-app-bar/usage.vue
+++ packages/docs/src/examples/v-app-bar/usage.vue
@@ -11,30 +11,31 @@
         }
       }"
     >
-      <v-layout
-        class="elevation-2 rounded mx-auto bg-grey-lighten-4"
-        style="max-width: 448px;"
-      >
-        <v-app-bar :model-value="model !== 'hidden'">
-          <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          </template>
+      <v-sheet color="surface-variant" class="pa-4 mx-auto" max-width="448" rounded>
+        <v-layout>
+          <v-app-bar :model-value="model !== 'hidden'" rounded="t">
+            <template v-slot:prepend>
+              <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
 
-          <v-app-bar-title>Application Bar</v-app-bar-title>
+            <v-app-bar-title>Application Bar</v-app-bar-title>
 
-          <template v-slot:append>
-            <v-btn icon="mdi-heart"></v-btn>
+            <template v-slot:append>
+              <v-btn icon="mdi-heart"></v-btn>
 
-            <v-btn icon="mdi-magnify"></v-btn>
+              <v-btn icon="mdi-magnify"></v-btn>
 
-            <v-btn icon="mdi-dots-vertical"></v-btn>
-          </template>
-        </v-app-bar>
+              <v-btn icon="mdi-dots-vertical"></v-btn>
+            </template>
+          </v-app-bar>
 
-        <v-main>
-          <div style="height: 250px;"></div>
-        </v-main>
-      </v-layout>
+          <v-main
+            class="bg-surface rounded"
+            style="height: 250px;"
+          >
+          </v-main>
+        </v-layout>
+      </v-sheet>
     </v-defaults-provider>
   </usage-example>
 </template>
