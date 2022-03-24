7aae04cf3003da8b7b4d328c07d05f7045d1d870
Kael <kaelwd@gmail.com>
2022-03-25 01:20:39 +1100

    feat(VOverlay): add "none" scroll strategy
    resolves #14292

--- packages/vuetify/src/components/VOverlay/scrollStrategies.ts
+++ packages/vuetify/src/components/VOverlay/scrollStrategies.ts
@@ -15,6 +15,7 @@ export interface ScrollStrategyData {
 }
 
 const scrollStrategies = {
+  none: null,
   close: closeScrollStrategy,
   block: blockScrollStrategy,
   reposition: repositionScrollStrategy,
