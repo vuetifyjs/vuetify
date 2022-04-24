7ce28b889236f9a288693344d96ec11de443794c
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-24 19:05:45 +0900

    fixup

--- packages/vuetify/src/util/globals.ts
+++ packages/vuetify/src/util/globals.ts
@@ -1,6 +1,6 @@
 export const IS_NODE = typeof process !== 'undefined'
 export const IN_BROWSER = typeof window !== 'undefined'
-process.env ??= Object.create(null);
+if (IS_NODE) process.env ??= Object.create(null);
 export const IS_DEBUG = IS_NODE && process.env.DEBUG === 'true'
 export const IS_PROD = IS_NODE && process.env.NODE_ENV === 'production'
 export const SUPPORTS_INTERSECTION = IN_BROWSER && 'IntersectionObserver' in window
