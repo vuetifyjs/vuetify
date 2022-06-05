f1b76fdd5037283429dee501e8ad7801bd1ce2f9
Kael <kaelwd@gmail.com>
2022-06-06 02:49:30 +1000

    docs: parse emoji and github links in markdown

--- packages/docs/src/components/app/Markdown.vue
+++ packages/docs/src/components/app/Markdown.vue
@@ -6,11 +6,101 @@
   // Utilities
   import { computed } from 'vue'
   import MarkdownIt from 'markdown-it'
+  import Emoji from 'markdown-it-emoji/bare'
 
   const md = MarkdownIt({
     html: true,
     linkify: true,
     typographer: true,
+  })
+
+  md.use(Emoji, {
+    defs: {
+      rocket: '🚀',
+      wrench: '🔧',
+      microscope: '🔬',
+      arrows_counterclockwise: '🔄',
+      fire: '🔥',
+    },
+  })
+
+  md.core.ruler.after('linkify', 'gh_links', state => {
+    const blockTokens = state.tokens
+
+    for (let j = 0; j < blockTokens.length; j++) {
+      if (blockTokens[j].type !== 'inline') continue
+
+      const tokens = blockTokens[j].children
+
+      for (let i = tokens.length - 1; i >= 0; i--) {
+        const currentToken = tokens[i]
+
+        // Skip content of markdown links
+        if (currentToken.type === 'link_close') {
+          i--
+          while (tokens[i].level !== currentToken.level && tokens[i].type !== 'link_open') {
+            i--
+          }
+          continue
+        }
+
+        if (currentToken.type === 'text') {
+          const text = currentToken.content
+
+          const links = [
+            ...text.matchAll(/(?<commits>[a-f0-9]{7,40})(?:\D|$)/gm),
+            ...text.matchAll(/(?<issues>#(\d{3,5}))(?:\D|$)/gm),
+          ]
+          links.sort((a, b) => a.index - b.index)
+
+          const nodes = []
+          let level = currentToken.level
+          let lastPos = 0
+
+          links.forEach(link => {
+            const url = `https://github.com/vuetifyjs/vuetify/${Object.keys(link.groups)[0]}/${link.at(-1)}`
+            const linkText = link[1]
+            const pos = link.index
+
+            if (pos > lastPos) {
+              const token = new state.Token('text', '', 0)
+              token.content = text.slice(lastPos, pos)
+              token.level = level
+              nodes.push(token)
+            }
+
+            let token = new state.Token('link_open', 'a', 1)
+            token.attrs = [['href', url], ['_target', 'blank']]
+            token.level = level++
+            nodes.push(token)
+
+            if (link.groups.commits) {
+              token = new state.Token('code_inline', 'code', 0)
+              token.attrSet('class', 'text-subtitle-1')
+            } else {
+              token = new state.Token('text', '', 0)
+            }
+            token.content = linkText
+            token.level = level
+            nodes.push(token)
+
+            token = new state.Token('link_close', 'a', -1)
+            token.level = --level
+            nodes.push(token)
+
+            lastPos = link.index + link[1].length
+          })
+          if (lastPos < text.length) {
+            const token = new state.Token('text', '', 0)
+            token.content = text.slice(lastPos)
+            token.level = level
+            nodes.push(token)
+          }
+
+          tokens.splice(i, 1, ...nodes)
+        }
+      }
+    }
   })
 
   export default {
@@ -29,27 +119,5 @@
         markdown: computed(() => md.render(props.content, {})),
       }
     },
-
-    // render (h, { children: nodes = [], data, props, slots }) {
-    //   const children = []
-    //   const node = nodes[0] || {}
-
-    //   if (node.children) {
-    //     children.push(...node.children)
-    //   } else if (nodes.length > 1) {
-    //     children.push(nodes)
-    //   } else {
-    //     const text = node.text || data.domProps.textContent || ''
-
-    //     data.domProps = {
-    //       ...data.domProps,
-    //       innerHTML: md.render(text, {}),
-    //     }
-    //   }
-
-    //   data.staticClass = `v-markdown ${data.staticClass || ''}`.trim()
-
-    //   return h(props.tag, data, children)
-    // },
   }
 </script>
