55a0eb8a7f738fe1454c211a5ef18b6ec9d338d2
John Leider <john.j.leider@gmail.com>
2022-06-15 09:02:06 -0500

    test(VDivider): update cy boilerplate

--- packages/vuetify/src/components/VDivider/__tests__/VDivider.spec.cy.tsx
+++ packages/vuetify/src/components/VDivider/__tests__/VDivider.spec.cy.tsx
@@ -3,39 +3,12 @@
 import { VDivider } from '..'
 import { generate } from '@/../cypress/templates'
 
-const defaultColors = ['success', 'info', 'warning', 'error', 'invalid']
-
-const props = {
-  color: defaultColors,
-  icon: ['mdi-vuetify'],
-  modelValue: true,
-}
+const props = {}
 
 const stories = {
-  'Default divider': <VDivider />,
-  'Icon divider': <VDivider icon="mdi-vuetify" />,
+  Default: <VDivider />,
 }
-
 // Tests
 describe('VDivider', () => {
-  describe('color', () => {
-    it('supports default color props', () => {
-      cy.mount(() => (
-        <>
-          {defaultColors.map((color, idx) => (
-            <VDivider color={ color }>
-              { color } divider
-            </VDivider>
-          )) }
-        </>
-      ))
-        .get('.v-divider')
-        .should('have.length', defaultColors.length)
-    })
-  })
-})
-
-// Useful to preview all of the variants and pre-made examples
-describe('Showcase', { viewportHeight: 1130, viewportWidth: 700 }, () => {
   generate({ stories, props, component: VDivider })
 })
