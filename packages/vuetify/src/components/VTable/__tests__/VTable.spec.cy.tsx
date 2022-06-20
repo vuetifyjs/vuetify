01d1c3a361410186a515eab1b78a438f9a6afb0c
John Leider <john.j.leider@gmail.com>
2022-06-20 11:44:19 -0500

    test(VTable): update baseline cypress structure

--- packages/vuetify/src/components/VTable/__tests__/VTable.spec.cy.tsx
+++ packages/vuetify/src/components/VTable/__tests__/VTable.spec.cy.tsx
@@ -6,14 +6,9 @@ import { generate } from '@/../cypress/templates'
 const props = {}
 
 const stories = {
-  'Default table': <VTable />,
+  Default: <VTable />,
 }
-
 // Tests
 describe('VTable', () => {
-  // TODO
-
-  describe('Showcase', { viewportHeight: 1130, viewportWidth: 700 }, () => {
-    generate({ stories, props, component: VTable })
-  })
+  generate({ stories, props, component: VTable })
 })
