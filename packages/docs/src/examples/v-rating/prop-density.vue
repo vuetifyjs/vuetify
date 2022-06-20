abaef4c561202b4bbaaa4915fbc7052050ae8309
Andrey Kniazev <worldpwn@hotmail.com>
2022-06-20 23:39:31 +0200

    docs(VRating): In density example make rating selectable

--- packages/docs/src/examples/v-rating/prop-density.vue
+++ packages/docs/src/examples/v-rating/prop-density.vue
@@ -2,20 +2,26 @@
   <div class="d-flex flex-column align-center justify-center">
     <v-rating
       class="ma-2"
-      :model-value="3"
+      v-model="rating"
       density="default"
     ></v-rating>
 
     <v-rating
       class="ma-2"
-      :model-value="3"
+      v-model="rating"
       density="comfortable"
     ></v-rating>
 
     <v-rating
       class="ma-2"
-      :model-value="3"
+      v-model="rating"
       density="compact"
     ></v-rating>
   </div>
 </template>
+
+<script>
+  export default {
+    data: () => ({ rating: 3 }),
+  }
+</script>
