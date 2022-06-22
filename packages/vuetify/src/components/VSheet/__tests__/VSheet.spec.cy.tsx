8538481e8c1e23b4830728a2344e24ab660612a9
John Leider <john.j.leider@gmail.com>
2022-06-22 14:49:08 -0500

    test(VSheet): update cypress test to baseline

--- packages/vuetify/src/components/VSheet/__tests__/VSheet.spec.cy.tsx
+++ packages/vuetify/src/components/VSheet/__tests__/VSheet.spec.cy.tsx
@@ -1,29 +1,14 @@
 /// <reference types="../../../../types/cypress" />
 
 import { VSheet } from '..'
+import { generate } from '@/../cypress/templates'
 
-const defaultColors = ['success', 'info', 'warning', 'error', 'invalid']
+const props = {}
 
+const stories = {
+  Default: <VSheet />,
+}
 // Tests
 describe('VSheet', () => {
-  describe('color', () => {
-    it('supports default color props', () => {
-      cy.mount(() => (
-        <>
-          {defaultColors.map((color, idx) => (
-            <VSheet color={ color }>
-              { color } sheet
-            </VSheet>
-          )) }
-        </>
-      ))
-        .get('.v-sheet')
-        .should('have.length', defaultColors.length)
-        .then(subjects => {
-          Array.from(subjects).forEach((subject, idx) => {
-            expect(subject).to.contain(defaultColors[idx])
-          })
-        })
-    })
-  })
+  generate({ stories, props, component: VSheet })
 })
