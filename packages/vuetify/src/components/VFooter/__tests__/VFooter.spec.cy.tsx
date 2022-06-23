865f22cd45860032b2a292e69717c54482e57910
John Leider <john.j.leider@gmail.com>
2022-06-23 11:46:25 -0500

    test(VFooter): reset cypress test to baseline

--- packages/vuetify/src/components/VFooter/__tests__/VFooter.spec.cy.tsx
+++ packages/vuetify/src/components/VFooter/__tests__/VFooter.spec.cy.tsx
@@ -1,39 +1,14 @@
 /// <reference types="../../../../types/cypress" />
 
 import { VFooter } from '..'
-import { VLayout } from '@/components'
 import { generate } from '@/../cypress/templates'
 
-const defaultColors = ['success', 'info', 'warning', 'error', 'invalid']
-
-const props = {
-  color: defaultColors,
-}
+const props = {}
 
 const stories = {
-  'Default footer': <VFooter />,
+  Default: <VFooter />,
 }
-
 // Tests
 describe('VFooter', () => {
-  describe('color', () => {
-    it('supports default color props', () => {
-      cy.mount(() => (
-        <VLayout>
-          {defaultColors.map((color, idx) => (
-            <VFooter color={ color }>
-              { color } footer
-            </VFooter>
-          )) }
-        </VLayout>
-      ))
-        .get('.v-footer')
-        .should('have.length', defaultColors.length)
-        .then(subjects => {
-          Array.from(subjects).forEach((subject, idx) => {
-            expect(subject).to.contain(defaultColors[idx])
-          })
-        })
-    })
-  })
+  generate({ stories, props, component: VFooter })
 })
