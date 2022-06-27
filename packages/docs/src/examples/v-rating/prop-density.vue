68d325495fea273fd43510a7333ab4b3bcdc14c9
Andrey Kniazev <worldpwn@hotmail.com>
2022-06-27 13:10:35 +0200

    Fix style

--- packages/docs/src/examples/v-rating/prop-density.vue
+++ packages/docs/src/examples/v-rating/prop-density.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="d-flex flex-column align-center justify-center">
     <v-rating
-      class="ma-2"
       v-model="rating"
+      class="ma-2"
       density="default"
     ></v-rating>
 
     <v-rating
-      class="ma-2"
       v-model="rating"
+      class="ma-2"
       density="comfortable"
     ></v-rating>
 
     <v-rating
-      class="ma-2"
       v-model="rating"
+      class="ma-2"
       density="compact"
     ></v-rating>
   </div>
