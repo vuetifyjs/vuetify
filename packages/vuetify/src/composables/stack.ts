63a2c6cf2531305bb5ed8352c3a1f3e092948772
Kael <kaelwd@gmail.com>
2022-07-09 20:28:47 +1000

    fix(VOverlay): compare uid instead of object reference in isTop
    fixes #15380
    fixes #15412

--- packages/vuetify/src/composables/stack.ts
+++ packages/vuetify/src/composables/stack.ts
@@ -1,31 +1,31 @@
 import { useToggleScope } from '@/composables/toggleScope'
 
-import { computed, onScopeDispose, reactive, readonly, ref, toRaw, watchEffect } from 'vue'
+import { computed, onScopeDispose, reactive, readonly, ref, watchEffect } from 'vue'
 import { getCurrentInstance } from '@/util'
 
 // Types
-import type { ComponentInternalInstance, Ref } from 'vue'
+import type { Ref } from 'vue'
 
-const stack = reactive<[vm: ComponentInternalInstance, zIndex: number][]>([])
+const stack = reactive<[uid: number, zIndex: number][]>([])
 
 export function useStack (isActive: Readonly<Ref<boolean>>, zIndex: Readonly<Ref<string | number>>) {
   const vm = getCurrentInstance('useStack')
 
   const _zIndex = ref(+zIndex.value)
   useToggleScope(isActive, () => {
-    const lastZIndex = stack[stack.length - 1]?.[1]
+    const lastZIndex = stack.at(-1)?.[1]
     _zIndex.value = lastZIndex ? lastZIndex + 10 : +zIndex.value
-    stack.push([vm, _zIndex.value])
+    stack.push([vm.uid, _zIndex.value])
 
     onScopeDispose(() => {
-      const idx = stack.findIndex(v => v[0] === vm)
+      const idx = stack.findIndex(v => v[0] === vm.uid)
       stack.splice(idx, 1)
     })
   })
 
   const isTop = ref(true)
   watchEffect(() => {
-    const _isTop = toRaw(stack[stack.length - 1]?.[0]) === vm
+    const _isTop = stack.at(-1)?.[0] === vm.uid
     setTimeout(() => isTop.value = _isTop)
   })
 
