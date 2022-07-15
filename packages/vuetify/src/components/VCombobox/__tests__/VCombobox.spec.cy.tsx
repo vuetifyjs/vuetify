f80a35e37fd103894be879deb7577f8067cef2f9
antoninbeaufort <antonin.beaufort@gmail.com>
2022-07-15 16:23:50 +0200

    test(VCombobox): add tests for filter using search

---
+++ packages/vuetify/src/components/VCombobox/__tests__/VCombobox.spec.cy.tsx
@@ -0,0 +1,50 @@
+/// <reference types="../../../../types/cypress" />
+
+import { VCombobox } from '../VCombobox'
+
+describe('VCombobox', () => {
+  describe('search', () => {
+    it('should find one entry and put it in list item', () => {
+      const items = [
+        'Item 1',
+        'Item 2',
+      ]
+      cy.mount(() => (
+        <VCombobox items={items} />
+      ))
+        .get('input')
+        .click()
+        .type('1')
+        .get('.v-list-item')
+        .should('have.length', 1)
+    })
+    it('should find all entries and put them in list item', () => {
+      const items = [
+        'Item 1',
+        'Item 2',
+      ]
+      cy.mount(() => (
+        <VCombobox items={items} />
+      ))
+        .get('input')
+        .click()
+        .type('Item')
+        .get('.v-list-item')
+        .should('have.length', 2)
+    })
+    it('should not find unexisting entry', () => {
+      const items = [
+        'Item 1',
+        'Item 2',
+      ]
+      cy.mount(() => (
+        <VCombobox items={items} />
+      ))
+        .get('input')
+        .click()
+        .type('3')
+        .get('.v-list-item')
+        .should('have.length', 0)
+    })
+  })
+})
