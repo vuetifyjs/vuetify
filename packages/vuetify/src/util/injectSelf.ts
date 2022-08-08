57335b446a3e95d185a51cd5b8b6650d082f880b
Kael <kaelwd@gmail.com>
2022-08-09 02:28:07 +1000

    refactor: only allow one call before subcomponentDefaults

---
+++ packages/vuetify/src/util/injectSelf.ts
@@ -0,0 +1,12 @@
+import { getCurrentInstance } from '@/util/getCurrentInstance'
+import type { InjectionKey } from 'vue'
+
+export function injectSelf<T>(key: InjectionKey<T> | string): T | undefined
+export function injectSelf (key: InjectionKey<any> | string) {
+  const { provides } = getCurrentInstance('injectSelf')
+
+  if (provides && (key as string | symbol) in provides) {
+    // TS doesn't allow symbol as index type
+    return provides[key as string]
+  }
+}
