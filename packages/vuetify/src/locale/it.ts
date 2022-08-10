17040ed1fe1028b3bfc482181ec2a9a5ca5570ce
Kael <kaelwd@gmail.com>
2022-08-10 21:34:01 +1000

    feat(VInput): add locale keys for append/prepend buttons

--- packages/vuetify/src/locale/it.ts
+++ packages/vuetify/src/locale/it.ts
@@ -44,6 +44,11 @@ export default {
   calendar: {
     moreEvents: '{0} di più',
   },
+  input: {
+    clear: 'Clear {0}',
+    prependAction: '{0} prepended action',
+    appendAction: '{0} appended action',
+  },
   fileInput: {
     counter: '{0} file',
     counterSize: '{0} file ({1} in totale)',
