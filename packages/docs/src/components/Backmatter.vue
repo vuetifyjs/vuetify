f4b584da10bbd5237d52934a709c8c8393247518
John Leider <john@vuetifyjs.com>
2022-08-23 13:31:20 -0500

    docs(RelatedPages): fix missing links

--- packages/docs/src/components/Backmatter.vue
+++ packages/docs/src/components/Backmatter.vue
@@ -22,31 +22,7 @@
       </template>
     </i18n-t>
 
-    <v-row dense>
-      <v-col
-        v-for="(item, i) in related"
-        :key="i"
-        cols="12"
-        sm="6"
-        md="4"
-      >
-        <app-sheet>
-          <v-list-item
-            :to="item.href"
-            :title="item.title"
-            :subtitle="item.subtitle"
-          >
-            <template #prepend>
-              <v-icon
-                :icon="item.icon"
-                :color="item.color"
-                class="mr-5 ml-2"
-              />
-            </template>
-          </v-list-item>
-        </app-sheet>
-      </v-col>
-    </v-row>
+    <related-pages />
 
     <app-divider class="mt-7 mb-5" />
 
@@ -58,85 +34,15 @@
   </section>
 </template>
 
-<script lang="ts">
-  import { computed, defineComponent } from 'vue'
-  import { useI18n } from 'vue-i18n'
-  import { useRoute } from 'vue-router'
-  import { generatedRoutes, rpath } from '@/util/routes'
+<script setup>
+  // Components
+  import RelatedPages from '@/components/doc/RelatedPages.vue'
 
-  const categoryIcons: Record<string, { icon: string, color: string }> = {
-    api: {
-      icon: 'mdi-flask-outline',
-      color: 'orange',
-    },
-    components: {
-      icon: 'mdi-view-dashboard-outline',
-      color: 'indigo-darken-1',
-    },
-    features: {
-      icon: 'mdi-image-edit-outline',
-      color: 'red',
-    },
-    directives: {
-      icon: 'mdi-function',
-      color: 'blue-grey',
-    },
-    'getting-started': {
-      icon: 'mdi-speedometer',
-      color: 'teal',
-    },
-    introduction: {
-      icon: 'mdi-script-text-outline',
-      color: 'green',
-    },
-    about: {
-      icon: 'mdi-vuetify',
-      color: 'primary',
-    },
-    resources: {
-      icon: 'mdi-human-male-board-poll',
-      color: 'pink',
-    },
-    styles: {
-      icon: 'mdi-palette-outline',
-      color: 'deep-purple-accent-4',
-    },
-    themes: {
-      icon: 'mdi-script-text-outline',
-      color: 'pink',
-    },
-  }
+  // Composables
+  import { useI18n } from 'vue-i18n'
 
   // Utilities
-  export default defineComponent({
-    name: 'Backmatter',
+  import { rpath } from '@/util/routes'
 
-    setup () {
-      const route = useRoute()
-      const { t, locale } = useI18n()
-
-      const related = computed(() => ((route.meta.related as string[]) || []).flatMap(href => {
-        href = href.replace(/\/$/, '')
-        const path = `/${locale.value}${href}`
-        const route = generatedRoutes.find((route: any) => route.path === path)
-
-        if (!route) return []
-
-        const category = href.split('/')[1]
-
-        return {
-          title: (route.meta?.nav ?? route.meta?.title ?? href) as string | undefined,
-          subtitle: category,
-          ...categoryIcons[category],
-          href: path,
-        }
-      }))
-
-      return {
-        related,
-        t,
-        rpath,
-      }
-    },
-  })
+  const { t } = useI18n()
 </script>
--- packages/docs/src/store/app.ts
+++ packages/docs/src/store/app.ts
@@ -9,5 +9,47 @@ export const useAppStore = defineStore({
   state: () => ({
     drawer: null,
     settings: false,
+    categories: {
+      api: {
+        icon: 'mdi-flask-outline',
+        color: 'orange',
+      },
+      components: {
+        icon: 'mdi-view-dashboard-outline',
+        color: 'indigo-darken-1',
+      },
+      features: {
+        icon: 'mdi-image-edit-outline',
+        color: 'red',
+      },
+      directives: {
+        icon: 'mdi-function',
+        color: 'blue-grey',
+      },
+      'getting-started': {
+        icon: 'mdi-speedometer',
+        color: 'teal',
+      },
+      introduction: {
+        icon: 'mdi-script-text-outline',
+        color: 'green',
+      },
+      about: {
+        icon: 'mdi-vuetify',
+        color: 'primary',
+      },
+      resources: {
+        icon: 'mdi-human-male-board',
+        color: 'pink',
+      },
+      styles: {
+        icon: 'mdi-palette-outline',
+        color: 'deep-purple-accent-4',
+      },
+      themes: {
+        icon: 'mdi-script-text-outline',
+        color: 'pink',
+      },
+    },
   } as RootState),
 })
