7ce0fec0f36beae630e24209f10ffebcacc8637d
Urata Daiki <7nohe@users.noreply.github.com>
2022-10-24 17:48:49 +0900

    test: add tests for closable chips (#15925)

--- packages/vuetify/src/components/VCombobox/__tests__/VCombobox.spec.cy.tsx
+++ packages/vuetify/src/components/VCombobox/__tests__/VCombobox.spec.cy.tsx
@@ -3,6 +3,33 @@
 import { VCombobox } from '../VCombobox'
 
 describe('VCombobox', () => {
+  describe('closableChips', () => {
+    it('should close only first chip', () => {
+      const items = [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ]
+
+      const selectedItems = [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+      ]
+
+      cy.mount(() => (
+        <VCombobox items={items} modelValue={selectedItems} multiple closableChips chips />
+      ))
+        .get('.v-chip__close')
+        .eq(0)
+        .click()
+        .get('input')
+        .get('.v-chip')
+        .should('have.length', 2)
+    })
+  })
+
   describe('search', () => {
     it('should filter items', () => {
       const items = [
--- packages/vuetify/src/components/VSelect/__tests__/VSelect.spec.cy.tsx
+++ packages/vuetify/src/components/VSelect/__tests__/VSelect.spec.cy.tsx
@@ -55,4 +55,26 @@ describe('VSelect', () => {
     ))
       .get('.v-list-item').last().invoke('text').should('equal', 'Foo')
   })
+
+  it('should close only first chip', () => {
+    const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
+
+    const selectedItems = ['Item 1', 'Item 2', 'Item 3']
+
+    cy.mount(() => (
+      <VSelect
+        items={items}
+        modelValue={selectedItems}
+        chips
+        closableChips
+        multiple
+      />
+    ))
+      .get('.v-chip__close')
+      .eq(0)
+      .click()
+      .get('input')
+      .get('.v-chip')
+      .should('have.length', 2)
+  })
 })
