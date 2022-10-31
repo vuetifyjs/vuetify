7f83d4cbfc564afad7b1e8dbcf9624a156244714
Kael <kaelwd@gmail.com>
2022-10-31 19:19:43 +1100

    docs(api): exclude type and hideSpinButtons props from some components
    closes #15997

--- packages/api-generator/src/maps/v-textarea.js
+++ packages/api-generator/src/maps/v-textarea.js
@@ -1,5 +1,10 @@
 const { VTextField } = require('../helpers/variables')
 
 module.exports = {
-  'v-textarea': VTextField,
+  'v-textarea': {
+    ...VTextField,
+    exclude: {
+      props: ['type', 'hide-spin-buttons'],
+    },
+  },
 }
