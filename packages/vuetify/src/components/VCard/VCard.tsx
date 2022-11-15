d9aec8d54ae70f88fa50cf452d476e8ed9a0a007
Paul Thebaud <paul.thebaud29@gmail.com>
2022-11-16 00:37:07 +0100

    revert(VCard): remove v-if on VCard loader
    ref: dab4adc603d7bc72e5f5ae4f36f484862ea7d7ee

--- packages/vuetify/src/components/VCard/VCard.tsx
+++ packages/vuetify/src/components/VCard/VCard.tsx
@@ -146,7 +146,6 @@ export const VCard = defineComponent({
           ) }
 
           <LoaderSlot
-            v-if={props.loading !== undefined || slots.loader}
             name="v-card"
             active={ !!props.loading }
             color={ typeof props.loading === 'boolean' ? undefined : props.loading }
