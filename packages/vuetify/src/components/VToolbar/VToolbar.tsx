ceaffe530eb21e40cee7f9b8c45aafdc60f94718
John Leider <john.j.leider@gmail.com>
2022-11-29 16:43:09 -0600

    fix(VTab/VTabs): use --v-tabs-height as default for --v-btn-height
    fixes #14863
    
    this wasn't actually fixed in 86b2cf1

--- packages/vuetify/src/components/VToolbar/VToolbar.tsx
+++ packages/vuetify/src/components/VToolbar/VToolbar.tsx
@@ -13,7 +13,6 @@ import { makeElevationProps, useElevation } from '@/composables/elevation'
 import { makeRoundedProps, useRounded } from '@/composables/rounded'
 import { makeTagProps } from '@/composables/tag'
 import { makeThemeProps, provideTheme } from '@/composables/theme'
-import { provideDefaults } from '@/composables/defaults'
 import { useBackgroundColor } from '@/composables/color'
 
 // Utilities
@@ -98,12 +97,6 @@ export const VToolbar = genericComponent<new () => {
       : 0
     )
 
-    provideDefaults({
-      VBtn: {
-        variant: 'text',
-      },
-    })
-
     useRender(() => {
       const hasTitle = !!(props.title || slots.title)
       const hasImage = !!(slots.image || props.image)
@@ -147,41 +140,57 @@ export const VToolbar = genericComponent<new () => {
             </div>
           ) }
 
-          <div
-            class="v-toolbar__content"
-            style={{ height: convertToUnit(contentHeight.value) }}
+          <VDefaultsProvider
+            defaults={{
+              VTabs: {
+                height: convertToUnit(contentHeight.value),
+              },
+            }}
           >
-            { slots.prepend && (
-              <div class="v-toolbar__prepend">
-                { slots.prepend?.() }
-              </div>
-            ) }
+            <div
+              class="v-toolbar__content"
+              style={{ height: convertToUnit(contentHeight.value) }}
+            >
+              { slots.prepend && (
+                <div class="v-toolbar__prepend">
+                  { slots.prepend?.() }
+                </div>
+              ) }
 
-            { hasTitle && (
-              <VToolbarTitle key="title" text={ props.title }>
-                {{ text: slots.title }}
-              </VToolbarTitle>
-            ) }
+              { hasTitle && (
+                <VToolbarTitle key="title" text={ props.title }>
+                  {{ text: slots.title }}
+                </VToolbarTitle>
+              ) }
 
-            { slots.default?.() }
+              { slots.default?.() }
 
-            { slots.append && (
-              <div class="v-toolbar__append">
-                { slots.append?.() }
-              </div>
-            ) }
-          </div>
+              { slots.append && (
+                <div class="v-toolbar__append">
+                  { slots.append?.() }
+                </div>
+              ) }
+            </div>
+          </VDefaultsProvider>
 
-          <VExpandTransition>
-            { isExtended.value && (
-              <div
-                class="v-toolbar__extension"
-                style={{ height: convertToUnit(extensionHeight.value) }}
-              >
-                { extension }
-              </div>
-            ) }
-          </VExpandTransition>
+          <VDefaultsProvider
+            defaults={{
+              VTabs: {
+                height: convertToUnit(extensionHeight.value),
+              },
+            }}
+          >
+            <VExpandTransition>
+              { isExtended.value && (
+                <div
+                  class="v-toolbar__extension"
+                  style={{ height: convertToUnit(extensionHeight.value) }}
+                >
+                  { extension }
+                </div>
+              ) }
+            </VExpandTransition>
+          </VDefaultsProvider>
         </props.tag>
       )
     })
