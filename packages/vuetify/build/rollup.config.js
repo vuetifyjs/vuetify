866126d9c3141f3e44988b49bbe57444611c2124
John Leider <9064066+johnleider@users.noreply.github.com>
2022-12-28 10:36:03 -0600

    feat(labs): add labs export (#16341)
    Co-authored-by: Kael <kaelwd@gmail.com>

--- packages/vuetify/build/rollup.config.js
+++ packages/vuetify/build/rollup.config.js
@@ -26,153 +26,240 @@ function fixWindowsPath(path) {
   return path.replace(/^[^:]+:\\/, '\\').replaceAll('\\', '/')
 }
 
-export default {
-  input: 'src/entry-bundler.ts',
-  output: [
-    {
-      file: 'dist/vuetify.esm.js',
-      format: 'es',
-      sourcemap: true,
-      banner,
-    },
-    {
-      file: 'dist/vuetify.js',
-      name: 'Vuetify',
-      format: 'umd',
-      globals: { vue: 'Vue' },
-      sourcemap: true,
-      banner,
-    },
-    {
-      file: 'dist/vuetify.min.js',
-      name: 'Vuetify',
-      format: 'umd',
-      globals: { vue: 'Vue' },
-      plugins: [terser({
-        format: { comments: /^!/, ecma: 2015, semicolons: false },
-      })],
-      sourcemap: true,
-      banner,
-    },
-  ],
-  external: ['vue'],
-  plugins: [
-    nodeResolve({ extensions }),
-    babel({
-      extensions,
-      babelHelpers: 'inline',
-    }),
-    sass({
-      options: {
-        charset: false,
+export default [
+  {
+    input: 'src/entry-bundler.ts',
+    output: [
+      {
+        file: 'dist/vuetify.esm.js',
+        format: 'es',
+        sourcemap: true,
+        banner,
       },
-      output (styles, styleNodes) {
-        // Complete CSS bundle
-        mkdirp(path.resolve(__dirname, '../dist')).then(() => {
-          return Promise.all([
-            postcss([autoprefixer]).process(styles, { from: 'src' }),
-            postcss([autoprefixer, cssnano({
-              preset: 'default',
-              postcssZindex: false,
-              reduceIdents: false,
-            })]).process(styles, { from: 'src' }),
-          ])
-        }).then(result => {
-          writeFile(path.resolve(__dirname, '../dist/vuetify.css'), banner + result[0].css, 'utf8')
-          writeFile(path.resolve(__dirname, '../dist/vuetify.min.css'), banner + result[1].css, 'utf8')
-        })
-
-        // Individual CSS files
-        for (const { id, content } of styleNodes) {
-          const out = path.parse(fixWindowsPath(id).replace(
-            path.resolve(__dirname, '../src'),
-            path.resolve(__dirname, '../lib')
-          ))
-          mkdirp(out.dir).then(() => {
-            writeFile(path.join(out.dir, out.name + '.css'), content, 'utf8')
+      {
+        file: 'dist/vuetify.js',
+        name: 'Vuetify',
+        format: 'umd',
+        globals: { vue: 'Vue' },
+        sourcemap: true,
+        banner,
+      },
+      {
+        file: 'dist/vuetify.min.js',
+        name: 'Vuetify',
+        format: 'umd',
+        globals: { vue: 'Vue' },
+        plugins: [terser({
+          format: { comments: /^!/, ecma: 2015, semicolons: false },
+        })],
+        sourcemap: true,
+        banner,
+      },
+    ],
+    external: ['vue'],
+    plugins: [
+      nodeResolve({ extensions }),
+      babel({
+        extensions,
+        babelHelpers: 'inline',
+      }),
+      sass({
+        options: {
+          charset: false,
+        },
+        output (styles, styleNodes) {
+          // Complete CSS bundle
+          mkdirp(path.resolve(__dirname, '../dist')).then(() => {
+            return Promise.all([
+              postcss([autoprefixer]).process(styles, { from: 'src' }),
+              postcss([autoprefixer, cssnano({
+                preset: 'default',
+                postcssZindex: false,
+                reduceIdents: false,
+              })]).process(styles, { from: 'src' }),
+            ])
+          }).then(result => {
+            writeFile(path.resolve(__dirname, '../dist/vuetify.css'), banner + result[0].css, 'utf8')
+            writeFile(path.resolve(__dirname, '../dist/vuetify.min.css'), banner + result[1].css, 'utf8')
           })
-        }
-      },
-    }),
-    alias({
-      entries: [
-        { find: /^@\/(.*)/, replacement: path.resolve(__dirname, '../src/$1') },
-      ],
-    }),
-    {
-      async buildEnd () {
-        const components = Object.create(null)
-        const directives = []
-        const variables = []
 
-        { // Components
-          const { importedIds } = this.getModuleInfo(
-            (await this.resolve('src/components/index.ts')).id
-          )
-          await Promise.all(importedIds.map(async id => {
-            // Fix for Windows
-            const importFrom = path.relative(path.resolve(__dirname, '../src'), fixWindowsPath(id)).replace(/\.ts$/, '.mjs')
+          // Individual CSS files
+          for (const { id, content } of styleNodes) {
+            const out = path.parse(fixWindowsPath(id).replace(
+              path.resolve(__dirname, '../src'),
+              path.resolve(__dirname, '../lib')
+            ))
+            mkdirp(out.dir).then(() => {
+              writeFile(path.join(out.dir, out.name + '.css'), content, 'utf8')
+            })
+          }
+        },
+      }),
+      alias({
+        entries: [
+          { find: /^@\/(.*)/, replacement: path.resolve(__dirname, '../src/$1') },
+        ],
+      }),
+      {
+        async buildEnd () {
+          const components = Object.create(null)
+          const directives = []
+          const variables = []
 
-            if (await this.resolve(path.join(id, '../_variables.scss')) != null) {
-              variables.push(id)
-            }
+          { // Components
+            const { importedIds } = this.getModuleInfo(
+              (await this.resolve('src/components/index.ts')).id
+            )
+            await Promise.all(importedIds.map(async id => {
+              // Fix for Windows
+              const importFrom = path.relative(path.resolve(__dirname, '../src'), fixWindowsPath(id)).replace(/\.ts$/, '.mjs')
 
-            const { ast } = this.getModuleInfo(id)
+              if (await this.resolve(path.join(id, '../_variables.scss')) != null) {
+                variables.push(id)
+              }
+
+              const { ast } = this.getModuleInfo(id)
+              walk(ast, {
+                ExportNamedDeclaration (node) {
+                  node.specifiers.forEach(node => {
+                    components[node.exported.name] = importFrom
+                  })
+                  node.declaration?.declarations.forEach(node => {
+                    components[node.id.name] = importFrom
+                  })
+                },
+              })
+            }))
+          }
+
+          { // Directives
+            const { ast, importedIds } = this.getModuleInfo(
+              (await this.resolve('src/directives/index.ts')).id
+            )
+            await Promise.all(importedIds.map(async id => {
+              if (await this.resolve(path.join(id, '../_variables.scss')) != null) {
+                variables.push(id)
+              }
+            }))
             walk(ast, {
               ExportNamedDeclaration (node) {
                 node.specifiers.forEach(node => {
-                  components[node.exported.name] = importFrom
-                })
-                node.declaration?.declarations.forEach(node => {
-                  components[node.id.name] = importFrom
+                  directives.push(node.exported.name)
                 })
               },
             })
-          }))
-        }
+          }
 
-        { // Directives
-          const { ast, importedIds } = this.getModuleInfo(
-            (await this.resolve('src/directives/index.ts')).id
-          )
-          await Promise.all(importedIds.map(async id => {
-            if (await this.resolve(path.join(id, '../_variables.scss')) != null) {
-              variables.push(id)
-            }
-          }))
-          walk(ast, {
-            ExportNamedDeclaration (node) {
-              node.specifiers.forEach(node => {
-                directives.push(node.exported.name)
-              })
-            },
+          this.emitFile({
+            type: 'asset',
+            fileName: 'json/importMap.json',
+            source: JSON.stringify({
+              components: Object.fromEntries(Object.entries(components).map(entry => [entry[0], {
+                from: entry[1],
+                styles: [], // TODO
+              }])),
+              directives,
+            }, null, 2),
           })
-        }
 
-        this.emitFile({
-          type: 'asset',
-          fileName: 'json/importMap.json',
-          source: JSON.stringify({
-            components: Object.fromEntries(Object.entries(components).map(entry => [entry[0], {
-              from: entry[1],
-              styles: [], // TODO
-            }])),
-            directives,
-          }, null, 2),
-        })
-
-        this.emitFile({
-          type: 'asset',
-          fileName: '_component-variables.sass',
-          source: variables.map(id => {
-            return `@forward '` + path.join(
-              '../lib',
-              path.relative(path.resolve(__dirname, '../src'), id),
-              '../_variables.scss'
-            ) + `'`
-          }).sort().join('\n'),
-        })
+          this.emitFile({
+            type: 'asset',
+            fileName: '_component-variables.sass',
+            source: variables.map(id => {
+              return `@forward '` + path.join(
+                '../lib',
+                path.relative(path.resolve(__dirname, '../src'), id),
+                '../_variables.scss'
+              ) + `'`
+            }).sort().join('\n'),
+          })
+        },
+      },
+    ],
+  },
+  {
+    input: 'src/labs/index.ts',
+    output: {
+      file: 'dist/vuetify-labs.js',
+      name: 'Vuetify',
+      format: 'umd',
+      globals: {
+        vue: 'Vue',
       },
     },
-  ],
-}
+    external: ['vue'],
+    plugins: [
+      nodeResolve({ extensions }),
+      babel({
+        extensions,
+        babelHelpers: 'inline',
+      }),
+      sass({
+        options: {
+          charset: false,
+        },
+        output () {},
+      }),
+      alias({
+        entries: [
+          { find: /^@\/(.*)/, replacement: path.resolve(__dirname, '../src/$1') },
+        ],
+      }),
+      {
+        async buildEnd () {
+          const components = Object.create(null)
+          const variables = []
+
+          { // Components
+            const { importedIds } = this.getModuleInfo(
+              (await this.resolve('src/labs/components.ts')).id
+            )
+            await Promise.all(importedIds.map(async id => {
+              // Fix for Windows
+              const importFrom = path.relative(path.resolve(__dirname, '../src'), fixWindowsPath(id)).replace(/\.ts$/, '.mjs')
+
+              if (await this.resolve(path.join(id, '../_variables.scss')) != null) {
+                variables.push(id)
+              }
+
+              const { ast } = this.getModuleInfo(id)
+              walk(ast, {
+                ExportNamedDeclaration (node) {
+                  node.specifiers.forEach(node => {
+                    components[node.exported.name] = importFrom
+                  })
+                  node.declaration?.declarations.forEach(node => {
+                    components[node.id.name] = importFrom
+                  })
+                },
+              })
+            }))
+          }
+
+          this.emitFile({
+            type: 'asset',
+            fileName: 'json/importMap-labs.json',
+            source: JSON.stringify({
+              components: Object.fromEntries(Object.entries(components).map(entry => [entry[0], {
+                from: entry[1],
+                styles: [], // TODO
+              }])),
+            }, null, 2),
+          })
+
+          this.emitFile({
+            type: 'asset',
+            fileName: '_component-variables-labs.sass',
+            source: variables.map(id => {
+              return `@forward '` + path.join(
+                '../lib',
+                path.relative(path.resolve(__dirname, '../src'), id),
+                '../_variables.scss'
+              ) + `'`
+            }).sort().join('\n'),
+          })
+        }
+      }
+    ],
+  },
+]
