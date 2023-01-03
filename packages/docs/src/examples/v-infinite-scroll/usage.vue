45f5e48ad22a9f7c74a95decc87cc624c40c1503
John Leider <john@vuetifyjs.com>
2023-01-03 11:31:31 -0600

    docs(VInfiniteScroll): add script section to usage code example

--- packages/docs/src/examples/v-infinite-scroll/usage.vue
+++ packages/docs/src/examples/v-infinite-scroll/usage.vue
@@ -1,42 +1,95 @@
 <template>
-  <v-infinite-scroll
-    v-bind="$attrs"
-    height="300"
-    :load="load"
+  <usage-example
+    v-model="model"
+    :code="code"
+    :script="script"
+    :name="name"
+    :options="options"
   >
-    <template v-for="(item, index) in items" :key="item">
-      <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
-        Item number {{ item }}
-      </div>
-    </template>
-  </v-infinite-scroll>
+    <div>
+      <v-infinite-scroll
+        v-bind="props"
+        :items="items.value"
+        :load="load"
+      >
+        <template v-for="(item, index) in items" :key="item">
+          <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+            Item number {{ item }}
+          </div>
+        </template>
+      </v-infinite-scroll>
+    </div>
+  </usage-example>
 </template>
 
-<script>
-  export default {
-    name: 'Usage',
+<script setup>
+  // Utilities
+  import { computed, ref } from 'vue'
+  import { propsToString } from '@/util/helpers'
 
-    inheritAttrs: false,
+  const name = 'v-infinite-scroll'
+  const model = ref('default')
+  const options = []
+  const items = ref(Array.from({ length: 50 }, (k, v) => v + 1))
 
-    data: () => ({
-      items: Array.from({ length: 50 }, (k, v) => v + 1),
-      defaults: {
-      },
-      options: {
-      },
-      tabs: [],
-    }),
-
-    methods: {
-      load () {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            this.items.push(...Array.from({ length: 10 }, (k, v) => v + this.items.at(-1) + 1))
-
-            resolve('ok')
-          }, 1000)
-        })
-      },
-    },
+  function api () {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1))
+      }, 1000)
+    })
   }
+
+  async function load () {
+    // Perform API call
+    const res = await api()
+
+    items.value.push(...res)
+  }
+
+  const props = computed(() => {
+    return {
+      height: 300,
+      ':load': 'load',
+      ':items': 'items',
+    }
+  })
+
+  const slots = computed(() => {
+    return `
+  <template v-for="(item, index) in items" :key="item">
+    <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+      Item #{{ item }}
+    </div>
+  </template>
+`
+  })
+
+  const code = computed(() => {
+    return `<v-infinite-scroll${propsToString(props.value)}>${slots.value}</v-infinite-scroll>`
+  })
+
+  const script = computed(() => {
+    return `export default {
+  data: () => ({
+    items: [],
+  }),
+
+  methods: {
+    async api () {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1))
+        }, 1000)
+      })
+    },
+    async load () {
+      // Perform API call
+      const res = await this.api()
+
+      this.items.push(...res)
+    },
+  },
+}`
+  })
 </script>
