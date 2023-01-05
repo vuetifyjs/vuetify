4f46c85fbae88758b0812a6ac5050fd1652ddaed
Azimov Dilovar <dilovar09@gmail.com>
2023-01-05 15:26:04 +0300

    Created Tajik locale

---
+++ packages/vuetify/src/locale/tj.ts
@@ -0,0 +1,69 @@
+export default {
+  badge: 'аломат',
+  close: 'Пушидан',
+  dataIterator: {
+    noResultsText: 'Сабти мувофик пайдо нагардид',
+    loadingText: 'Сабт бор шуда истодааст...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Сатр на саҳифа:',
+    ariaLabel: {
+      sortDescending: 'Батартибдарорӣ аз рӯи камшавӣ.',
+      sortAscending: 'Батартибдарорӣ аз рӯи афзоиш.',
+      sortNone: 'Ба тартиб дароварда нашудааст.',
+      activateNone: 'Барои нест кардани батартибдарорӣ фаъол кунед.',
+      activateDescending: 'Барои батартибдарорӣ аз рӯи камшави фаъол кунед.',
+      activateAscending: 'Барои батартибдарорӣ аз рӯи афзоиш фаъол кунед.',
+    },
+    sortBy: 'Батартибдарорӣ аз рӯи',
+  },
+  dataFooter: {
+    itemsPerPageText: 'Сабт дар саҳифа:',
+    itemsPerPageAll: 'Ҳама',
+    nextPage: 'Саҳифаи навбатӣ',
+    prevPage: 'Саҳифаи гузашта',
+    firstPage: 'Саҳифаи аввал',
+    lastPage: 'Саҳифаи охир',
+    pageText: '{0}-{1} аз {2}',
+  },
+  datePicker: {
+    itemsSelected: '{0} интихоб шуд',
+    nextMonthAriaLabel: 'Моҳи оянда',
+    nextYearAriaLabel: 'Соли оянда',
+    prevMonthAriaLabel: 'Моҳи гузашта',
+    prevYearAriaLabel: 'Соли гузашта',
+  },
+  noDataText: 'Маъумот мавҷуд нест',
+  carousel: {
+    prev: 'Слайди гузашта',
+    next: 'Слайди навбатӣ',
+    ariaLabel: {
+      delimiter: 'Слайди {0} аз {1}',
+    },
+  },
+  calendar: {
+    moreEvents: 'Боз {0} - и дигар',
+  },
+  fileInput: {
+    counter: 'Файлҳо: {0}',
+    counterSize: 'Файлҳо: {0} (ҳамаги {1})',
+  },
+  timePicker: {
+    am: 'AM',
+    pm: 'PM',
+  },
+  pagination: {
+    ariaLabel: {
+      wrapper: 'Навигатсияи саҳифаҳо',
+      next: 'Саҳифаи навбатӣ',
+      previous: 'Саҳифаи гузашта',
+      page: 'Гузаштан ба саҳифаи {0}',
+      currentPage: 'Саҳифаи ҷорӣ, Саҳифаи {0}',
+    },
+  },
+  rating: {
+    ariaLabel: {
+      icon: 'Рейтинг {0} аз {1}',
+    },
+  },
+}
