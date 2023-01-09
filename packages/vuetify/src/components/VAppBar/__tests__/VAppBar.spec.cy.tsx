bdba7424dacfdd8d942ca62b9dca3e9a1f15f813
Carson McManus <carson.mcmanus1@gmail.com>
2023-01-09 16:01:59 -0500

    test(VAppBar): tests for hide scrolling behavior

--- packages/vuetify/src/components/VAppBar/__tests__/VAppBar.spec.cy.tsx
+++ packages/vuetify/src/components/VAppBar/__tests__/VAppBar.spec.cy.tsx
@@ -35,4 +35,71 @@ describe('VAppBar', () => {
 
     cy.get('.v-app-bar').should('have.css', 'height', '48px')
   })
+
+  it('should support hide scroll behavior', () => {
+    cy.mount(() => (
+      <VLayout>
+        <VAppBar scroll-behavior="hide" style="position: fixed" />
+        <div class="content" style="min-height: 200vh; font-size: 64px">
+          <div>content</div>
+          <div>content</div>
+        </div>
+      </VLayout>
+    ))
+
+    // HACK: force cypress to wait for the page to be scrollable
+    cy.get('.content').should('be.visible')
+
+    cy.get('.v-app-bar').should('be.visible')
+
+    cy.scrollTo(0, 500, { ensureScrollable: true, duration: 200 })
+
+    cy.get('.v-app-bar').should('not.be.visible')
+  })
+
+  it('should support hide scroll behavior with inverted', () => {
+    cy.mount(() => (
+      <VLayout>
+        <VAppBar scroll-behavior="hide inverted" scroll-threshold="200" />
+        <div class="content" style="min-height: 200vh; font-size: 64px">
+          <div>content</div>
+          <div>content</div>
+        </div>
+      </VLayout>
+    ))
+
+    // HACK: force cypress to wait for the page to be scrollable
+    cy.get('.content').should('be.visible')
+
+    cy.get('.v-app-bar').should('not.be.visible')
+
+    cy.scrollTo(0, 500, { ensureScrollable: true, duration: 200 })
+
+    cy.get('.v-app-bar').should('be.visible')
+  })
+
+  it('should not wait until scroll to update active state', () => {
+    cy.mount(({ invertedScroll }: any) => (
+      <VLayout>
+        <VAppBar scroll-behavior={'hide' + (invertedScroll ? ' inverted' : '')} scroll-threshold="100" />
+        <div class="content" style="min-height: 200vh; font-size: 64px">
+          <div>content</div>
+          <div>content</div>
+        </div>
+      </VLayout>
+    ))
+
+    // HACK: force cypress to wait for the page to be scrollable
+    cy.get('.content').should('be.visible')
+
+    cy.get('.v-app-bar').should('be.visible')
+
+    cy.scrollTo(0, 200, { ensureScrollable: true, duration: 200 })
+
+    cy.get('.v-app-bar').should('not.be.visible')
+
+    cy.vue().then(wrapper => wrapper.setProps({ invertedScroll: true }))
+
+    cy.get('.v-app-bar').should('be.visible')
+  })
 })
