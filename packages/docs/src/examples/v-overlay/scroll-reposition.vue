1a8bc3c18a53d5164f0aa58d4afb5d7c8274065d
John Leider <john@vuetifyjs.com>
2023-01-10 21:58:22 -0600

    docs(overlays): update examples, resolve log warnings

--- packages/docs/src/examples/v-overlay/scroll-reposition.vue
+++ packages/docs/src/examples/v-overlay/scroll-reposition.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="d-flex justify-center">
     <v-btn>
-      reposition
+      Reposition
+
       <v-overlay
         activator="parent"
         location-strategy="connected"
