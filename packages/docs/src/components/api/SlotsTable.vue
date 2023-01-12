1b99ecaff969226d894041a3da8f8681a823191a
John Leider <john@vuetifyjs.com>
2023-01-12 16:31:27 -0600

    docs(ApiTable): normalize the various tables under new component

--- packages/docs/src/components/api/SlotsTable.vue
+++ packages/docs/src/components/api/SlotsTable.vue
@@ -1,42 +1,22 @@
 <template>
-  <app-sheet>
-    <v-table
-      class="api-table"
-    >
-      <tbody>
-        <template v-for="item in items" :key="item.name">
-          <tr :class="theme.dark ? 'bg-grey-darken-3' : 'bg-grey-lighten-4'">
-            <NameCell section="props" :name="item.name" />
-          </tr>
-          <tr v-if="item.formatted !== 'never'">
-            <app-markup :code="getType(item)" language="ts" :rounded="false" />
-          </tr>
-          <tr>
-            <td colspan="3" class="text-mono">
-              <app-markdown v-if="item.description" :content="item.description" />
-            </td>
-          </tr>
-        </template>
-      </tbody>
-    </v-table>
-  </app-sheet>
+  <ApiTable>
+    <template #row="{ props, ...item }">
+      <tr v-bind="props">
+        <NameCell section="slots" :name="item.name" />
+      </tr>
+
+      <tr v-if="item.formatted !== 'never'">
+        <app-markup :code="getType(item)" language="ts" :rounded="false" />
+      </tr>
+    </template>
+  </ApiTable>
 </template>
 
 <script setup lang="ts">
   // Components
+  import ApiTable from './ApiTable.vue'
   import NameCell from './NameCell.vue'
 
   // Utilities
   import { getType } from './utils'
-  import { PropType } from 'vue'
-  import { useTheme } from 'vuetify'
-
-  defineProps({
-    items: {
-      type: Array as PropType<any[]>,
-      default: () => [],
-    },
-  })
-
-  const { current: theme } = useTheme()
 </script>
