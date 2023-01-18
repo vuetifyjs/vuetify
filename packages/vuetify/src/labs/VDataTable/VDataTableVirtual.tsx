3e248d9a6fe899704be8e7feaa03271da1786f45
Kael <kaelwd@gmail.com>
2023-01-19 00:45:53 +1100

    fix(VCombobox): filter transformed items
    fixes #16403
    closes #16411

--- packages/vuetify/src/labs/VDataTable/VDataTableVirtual.tsx
+++ packages/vuetify/src/labs/VDataTable/VDataTableVirtual.tsx
@@ -60,7 +60,8 @@ export const VDataTableVirtual = defineComponent({
     })
     const { items } = useDataTableItems(props, columns)
 
-    const { filteredItems } = useFilter<DataTableItem>(props, items, toRef(props, 'search'))
+    const filterKeys = computed(() => columns.value.map(c => 'columns.' + c.key))
+    const { filteredItems } = useFilter<DataTableItem>(props, items, toRef(props, 'search'), { filterKeys })
 
     const { sortBy } = createSort(props)
     const { sortByWithGroups, opened, extractRows } = createGroupBy(props, groupBy, sortBy)
