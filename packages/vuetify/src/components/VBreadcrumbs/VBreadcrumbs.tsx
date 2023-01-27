a6b1b87bcc351ceaf8f31882ba4f2156f1eec6f4
sameh doush <samehdev95@gmail.com>
2023-01-27 21:03:15 +0300

    Update VBreadcrumbs.tsx

--- packages/vuetify/src/components/VBreadcrumbs/VBreadcrumbs.tsx
+++ packages/vuetify/src/components/VBreadcrumbs/VBreadcrumbs.tsx
@@ -25,122 +25,122 @@ import type { PropType } from 'vue'
 import type { SlotsToProps } from '@/util'
 
 export type BreadcrumbItem = string | (LinkProps & {
-  text: string
-  disabled?: boolean
+ text: string
+ disabled?: boolean
 })
 
 export const VBreadcrumbs = genericComponent<new <T>() => {
-  $props: {
-    items?: T[]
-  } & SlotsToProps<{
-    prepend: []
-    title: [{ item: T, index: number }]
-    divider: [{ item: T, index: number }]
-    default: []
-  }>
+ $props: {
+  items?: T[]
+ } & SlotsToProps<{
+  prepend: []
+  title: [{ item: T, index: number }]
+  divider: [{ item: T, index: number }]
+  default: []
+ }>
 }>()({
-  name: 'VBreadcrumbs',
+ name: 'VBreadcrumbs',
 
-  props: {
-    activeClass: String,
-    activeColor: String,
-    bgColor: String,
-    color: String,
-    disabled: Boolean,
-    divider: {
-      type: String,
-      default: '/',
-    },
-    icon: IconValue,
-    items: {
-      type: Array as PropType<BreadcrumbItem[]>,
-      default: () => ([]),
-    },
-
-    ...makeDensityProps(),
-    ...makeRoundedProps(),
-    ...makeTagProps({ tag: 'ul' }),
+ props: {
+  activeClass: String,
+  activeColor: String,
+  bgColor: String,
+  color: String,
+  disabled: Boolean,
+  divider: {
+   type: String,
+   default: '/',
+  },
+  icon: IconValue,
+  items: {
+   type: Array as PropType<BreadcrumbItem[]>,
+   default: () => ([]),
   },
 
-  setup (props, { slots }) {
-    const { backgroundColorClasses, backgroundColorStyles } = useBackgroundColor(toRef(props, 'bgColor'))
-    const { densityClasses } = useDensity(props)
-    const { roundedClasses } = useRounded(props)
+  ...makeDensityProps(),
+  ...makeRoundedProps(),
+  ...makeTagProps({ tag: 'ul' }),
+ },
 
-    provideDefaults({
-      VBreadcrumbsDivider: {
-        divider: toRef(props, 'divider'),
-      },
-      VBreadcrumbsItem: {
-        activeClass: toRef(props, 'activeClass'),
-        activeColor: toRef(props, 'activeColor'),
-        color: toRef(props, 'color'),
-        disabled: toRef(props, 'disabled'),
-      },
-    })
+ setup(props, { slots }) {
+  const { backgroundColorClasses, backgroundColorStyles } = useBackgroundColor(toRef(props, 'bgColor'))
+  const { densityClasses } = useDensity(props)
+  const { roundedClasses } = useRounded(props)
 
-    useRender(() => {
-      const hasPrepend = !!(slots.prepend || props.icon)
+  provideDefaults({
+   VBreadcrumbsDivider: {
+    divider: toRef(props, 'divider'),
+   },
+   VBreadcrumbsItem: {
+    activeClass: toRef(props, 'activeClass'),
+    activeColor: toRef(props, 'activeColor'),
+    color: toRef(props, 'color'),
+    disabled: toRef(props, 'disabled'),
+   },
+  })
 
-      return (
-        <props.tag
-          class={[
-            'v-breadcrumbs',
-            backgroundColorClasses.value,
-            densityClasses.value,
-            roundedClasses.value,
-          ]}
-          style={ backgroundColorStyles.value }
-        >
-          { hasPrepend && (
-            <VDefaultsProvider
-              key="prepend"
-              defaults={{
-                VIcon: {
-                  icon: props.icon,
-                  start: true,
-                },
-              }}
-            >
-              <div class="v-breadcrumbs__prepend">
-                { slots.prepend
-                  ? slots.prepend()
-                  : props.icon && (<VIcon />)
-                }
-              </div>
-            </VDefaultsProvider>
-          ) }
+  useRender(() => {
+   const hasPrepend = !!(slots.prepend || props.icon)
 
-          { props.items.map((item, index, array) => (
-            <>
-             { slots.item?.({ item, index }) ?? (
-                  <VBreadcrumbsItem
-                    key={ index }
-                    disabled={ index >= array.length - 1 }
-                    { ...(typeof item === 'string' ? { title: item } : item)}
-                    v-slots={{
-                      default:  slots.title?.({ item, index }) ?? undefined,
-                    } }
-                  />
-                )
-              }
-              { index < array.length - 1 && (
-                <VBreadcrumbsDivider
-                  v-slots={{
-                    default: slots.divider ? () => slots.divider?.({ item, index }) : undefined,
-                  }}
-                />
-              ) }
-            </>
-          )) }
+   return (
+    <props.tag
+     class={[
+      'v-breadcrumbs',
+      backgroundColorClasses.value,
+      densityClasses.value,
+      roundedClasses.value,
+     ]}
+     style={backgroundColorStyles.value}
+    >
+     {hasPrepend && (
+      <VDefaultsProvider
+       key="prepend"
+       defaults={{
+        VIcon: {
+         icon: props.icon,
+         start: true,
+        },
+       }}
+      >
+       <div class="v-breadcrumbs__prepend">
+        {slots.prepend
+         ? slots.prepend()
+         : props.icon && (<VIcon />)
+        }
+       </div>
+      </VDefaultsProvider>
+     )}
 
-          { slots.default?.() }
-        </props.tag>
-      )
-    })
+     {props.items.map((item, index, array) => (
+      <>
+       {slots.item?.({ item, index }) ?? (
+        <VBreadcrumbsItem
+         key={index}
+         disabled={index >= array.length - 1}
+         {...(typeof item === 'string' ? { title: item } : item)}
+         v-slots={{
+          default: slots.title ? () => slots.title?.({ item, index }) : undefined,
+         }}
+        />
+       )
+       }
+       {index < array.length - 1 && (
+        <VBreadcrumbsDivider
+         v-slots={{
+          default: slots.divider ? () => slots.divider?.({ item, index }) : undefined,
+         }}
+        />
+       )}
+      </>
+     ))}
 
-    return {}
-  },
+     {slots.default?.()}
+    </props.tag>
+   )
+  })
+
+  return {}
+ },
 })
 
 export type VBreadcrumbs = InstanceType<typeof VBreadcrumbs>
