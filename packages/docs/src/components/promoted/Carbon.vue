573a912038c3d78df394878dcb13fd6ae4ddf484
Kael <kaelwd@gmail.com>
2023-02-05 20:04:39 +1100

    docs: fix SSR build

--- packages/docs/src/components/promoted/Carbon.vue
+++ packages/docs/src/components/promoted/Carbon.vue
@@ -12,7 +12,6 @@
     >
       <promoted-script
         id="carbonads"
-
         script-id="_carbonads_js"
         src="//cdn.carbonads.com/carbon.js?serve=CKYI5KQY&placement=vuetifyjscom"
         @script:error="error = true"
@@ -31,8 +30,9 @@
 
   // Utiltlies
   import { ref } from 'vue'
+  import { IN_BROWSER } from '@/util/globals'
 
-  const error = ref(!!document.getElementById('carbonads'))
+  const error = ref(IN_BROWSER && !!document.getElementById('carbonads'))
   const route = useRoute()
 </script>
 
