e9fe9013ae225bf6768fb45059a4eb113d575449
John Leider <john@vuetifyjs.com>
2023-02-10 11:22:48 -0600

    docs(AppDrawer): scroll to active item onMounted

--- packages/docs/src/components/app/drawer/Drawer.vue
+++ packages/docs/src/components/app/drawer/Drawer.vue
@@ -19,5 +19,23 @@
   // Composables
   import { useAppStore } from '@/store/app'
 
+  // Utilities
+  import { onMounted } from 'vue'
+  import { wait } from '@/util/helpers'
+
   const app = useAppStore()
+
+  onMounted(async () => {
+    await wait(1000)
+
+    const element = document.querySelector('#app-drawer .v-list-item--active:not(.v-list-group__header)')
+
+    if (!element) return
+
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'center',
+    })
+  })
 </script>
