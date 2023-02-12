940f9c161af6f77928ce1508f6b4f653b8b4cd8b
Kael <kaelwd@gmail.com>
2023-02-12 21:56:44 +1100

    chore(docs): don't track events during ssg build

--- packages/docs/src/plugins/gtag.ts
+++ packages/docs/src/plugins/gtag.ts
@@ -1,10 +1,13 @@
 // Imports
 import VueGtag, { trackRouter } from 'vue-gtag-next'
+import { IN_BROWSER } from '@/util/globals'
 
 // Types
 import type { ViteSSGContext } from '@vuetify/vite-ssg'
 
 export const useGtag = (ctx: ViteSSGContext) => {
+  if (!IN_BROWSER) return
+
   // vue-gtag-next doesn't support esm properly
   const install = VueGtag.install ?? (VueGtag as any).default.install
   ctx.app.use(install, {
