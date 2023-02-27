839ac07971ec6c0bea5d7c2ebf94f32865569f78
Peter Hauke <90506472+peterhauke@users.noreply.github.com>
2023-02-27 21:30:13 +0100

    docs: add v-icon closing tags
    Add v-icon closing tags
    Add blank line between similar components (is this the correct style for this project?)

--- packages/docs/src/examples/v-icon/prop-color.vue
+++ packages/docs/src/examples/v-icon/prop-color.vue
@@ -4,31 +4,36 @@
       size="large"
       color="green-darken-2"
       icon="mdi-domain"
-    />
+    ></v-icon>
+  
     <v-icon
       size="large"
       color="blue-darken-2"
       icon="mdi-message-text"
-    />
+    ></v-icon>
+    
     <v-icon
       size="large"
       color="purple-darken-2"
       icon="mdi-dialpad"
-    />
+    ></v-icon>
+    
     <v-icon
       size="large"
       color="teal-darken-2"
       icon="mdi-email"
-    />
+    ></v-icon>
+    
     <v-icon
       size="large"
       color="blue-grey-darken-2"
       icon="mdi-call-split"
-    />
+    ></v-icon>
+    
     <v-icon
       size="large"
       color="orange-darken-2"
       icon="mdi-arrow-up-bold-box-outline"
-    />
+    ></v-icon>
   </v-row>
 </template>
