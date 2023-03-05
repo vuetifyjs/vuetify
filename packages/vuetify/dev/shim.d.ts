d6b823cd78e8853b5c4cfb077dd9a82276cf7d1c
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2023-03-06 10:13:47 +1100

    move script to a separate PR

--- packages/vuetify/dev/shim.d.ts
+++
@@ -1,9 +0,0 @@
-import { VCol } from 'vuetify/src/components/VGrid/VCol'
-import { VRow } from 'vuetify/src/components/VGrid/VRow'
-
-declare module 'vue' {
-  export interface GlobalComponents {
-    VCol: typeof VCol;
-    VRow: typeof VRow;
-  }
-}
