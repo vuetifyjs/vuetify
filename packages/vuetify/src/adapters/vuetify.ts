5deaa307df7ef8c02d2856bedaba2395f592e031
nekosaur <albert@kaaman.nu>
2023-03-06 19:54:31 +0100

    wip: locale?

--- packages/vuetify/src/adapters/vuetify.ts
+++ packages/vuetify/src/adapters/vuetify.ts
@@ -1,4 +1,5 @@
 // Types
+import { createRange } from '@/util'
 import type { DateAdapter } from './date-adapter'
 
 function getWeekArray (date: Date) {
@@ -55,36 +56,197 @@ function date (value?: any): Date | null {
   return null
 }
 
-function getWeekdays () {
-  return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const firstDay: Record<string, number> = {
+  '001': 1,
+  AD: 1,
+  AE: 6,
+  AF: 6,
+  AG: 0,
+  AI: 1,
+  AL: 1,
+  AM: 1,
+  AN: 1,
+  AR: 1,
+  AS: 0,
+  AT: 1,
+  AU: 0,
+  AX: 1,
+  AZ: 1,
+  BA: 1,
+  BD: 0,
+  BE: 1,
+  BG: 1,
+  BH: 6,
+  BM: 1,
+  BN: 1,
+  BR: 0,
+  BS: 0,
+  BT: 0,
+  BW: 0,
+  BY: 1,
+  BZ: 0,
+  CA: 0,
+  CH: 1,
+  CL: 1,
+  CM: 1,
+  CN: 0,
+  CO: 0,
+  CR: 1,
+  CY: 1,
+  CZ: 1,
+  DE: 1,
+  DJ: 6,
+  DK: 1,
+  DM: 0,
+  DO: 0,
+  DZ: 6,
+  EC: 1,
+  EE: 1,
+  EG: 6,
+  ES: 1,
+  ET: 0,
+  FI: 1,
+  FJ: 1,
+  FO: 1,
+  FR: 1,
+  GB: 1,
+  'GB-alt-variant': 0,
+  GE: 1,
+  GF: 1,
+  GP: 1,
+  GR: 1,
+  GT: 0,
+  GU: 0,
+  HK: 0,
+  HN: 0,
+  HR: 1,
+  HU: 1,
+  ID: 0,
+  IE: 1,
+  IL: 0,
+  IN: 0,
+  IQ: 6,
+  IR: 6,
+  IS: 1,
+  IT: 1,
+  JM: 0,
+  JO: 6,
+  JP: 0,
+  KE: 0,
+  KG: 1,
+  KH: 0,
+  KR: 0,
+  KW: 6,
+  KZ: 1,
+  LA: 0,
+  LB: 1,
+  LI: 1,
+  LK: 1,
+  LT: 1,
+  LU: 1,
+  LV: 1,
+  LY: 6,
+  MC: 1,
+  MD: 1,
+  ME: 1,
+  MH: 0,
+  MK: 1,
+  MM: 0,
+  MN: 1,
+  MO: 0,
+  MQ: 1,
+  MT: 0,
+  MV: 5,
+  MX: 0,
+  MY: 1,
+  MZ: 0,
+  NI: 0,
+  NL: 1,
+  NO: 1,
+  NP: 0,
+  NZ: 1,
+  OM: 6,
+  PA: 0,
+  PE: 0,
+  PH: 0,
+  PK: 0,
+  PL: 1,
+  PR: 0,
+  PT: 0,
+  PY: 0,
+  QA: 6,
+  RE: 1,
+  RO: 1,
+  RS: 1,
+  RU: 1,
+  SA: 0,
+  SD: 6,
+  SE: 1,
+  SG: 0,
+  SI: 1,
+  SK: 1,
+  SM: 1,
+  SV: 0,
+  SY: 6,
+  TH: 0,
+  TJ: 1,
+  TM: 1,
+  TR: 1,
+  TT: 0,
+  TW: 0,
+  UA: 1,
+  UM: 0,
+  US: 0,
+  UY: 1,
+  UZ: 1,
+  VA: 1,
+  VE: 0,
+  VI: 0,
+  VN: 1,
+  WS: 0,
+  XK: 1,
+  YE: 0,
+  ZA: 0,
+  ZW: 0,
 }
 
-function format (value: Date, formatString: string): string {
-  const date = new Date(value)
-  const options = {
-    weekday: getWeekdays(),
-    shortWeekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    month: [
-      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-      'September', 'October', 'November', 'December',
-    ],
-    shortMonth: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  }
+const sundayJanuarySecond2000 = new Date(2000, 0, 2)
 
+function getWeekdays (locale: string) {
+  const daysFromSunday = firstDay[locale.slice(-2).toUpperCase()]
+
+  return createRange(7).map(i => {
+    const weekday = new Date(sundayJanuarySecond2000)
+    weekday.setDate(sundayJanuarySecond2000.getDate() + daysFromSunday + i)
+    return new Intl.DateTimeFormat(locale, { weekday: 'long' }).format(weekday)
+  })
+}
+
+function format (value: Date, formatString: string, locale: string): string {
+  const date = new Date(value)
+
+  let options: Intl.DateTimeFormatOptions = {}
   switch (formatString) {
     case 'fullDateWithWeekday':
-      return `${options.weekday[date.getDay()]}, ${date.getDate()} ${options.month[date.getMonth()]} ${date.getFullYear()}`
+      options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
+      break
     case 'normalDateWithWeekday':
-      return `${options.shortWeekday[date.getDay()]}, ${date.getDate()} ${options.shortMonth[date.getMonth()]} ${date.getFullYear()}`
+      options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' }
+      break
     case 'keyboardDate':
-      return `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate()}/${date.getFullYear()}`
+      options = {}
+      break
     case 'monthAndDate':
-      return `${options.month[date.getMonth()]} ${date.getDate()}`
+      options = { month: 'long', day: 'numeric' }
+      break
     case 'monthAndYear':
-      return `${options.month[date.getMonth()]} ${date.getFullYear()}`
+      options = { month: 'long', year: 'numeric' }
+      break
     default:
-      return date.toLocaleDateString()
+      options = { timeZone: 'UTC', timeZoneName: 'short' }
   }
+
+  return new Intl.DateTimeFormat(locale, options).format(date)
 }
 
 function addDays (date: Date, amount: number) {
@@ -190,7 +352,9 @@ function setYear (date: Date, year: number) {
   return d
 }
 
-export default class DateFnsAdapter implements DateAdapter<Date> {
+export default class VuetifyDateAdapter implements DateAdapter<Date> {
+  constructor (private locale: string) {}
+
   date (value?: any) {
     return date(value)
   }
@@ -216,7 +380,7 @@ export default class DateFnsAdapter implements DateAdapter<Date> {
   }
 
   format (date: Date, formatString: string) {
-    return format(date, formatString)
+    return format(date, formatString, this.locale)
   }
 
   isEqual (date: Date, comparing: Date) {
@@ -256,7 +420,7 @@ export default class DateFnsAdapter implements DateAdapter<Date> {
   }
 
   getWeekdays () {
-    return getWeekdays()
+    return getWeekdays(this.locale)
   }
 
   getYear (date: Date) {
