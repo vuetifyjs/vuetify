bf4e0bb6d0f5fbc9a4aa7610fa6818c583aedf50
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2023-03-14 11:24:00 +1100

    Add cy test against VCheckbox

---
+++ packages/vuetify/src/components/VCheckbox/__tests__/VCheckbox.spec.cy.tsx
@@ -0,0 +1,32 @@
+/// <reference types="../../../../types/cypress" />
+
+import { VDefaultsProvider } from '@/components'
+import { VCheckbox } from '..'
+
+describe('VCheckbox', () => {
+  describe('global configuration', () => {
+    it('should only apply \'v-checkbox\' class to root element and also apply global config class/style', () => {
+      cy.mount(() => (
+        <VDefaultsProvider defaults={ {
+          global: {
+            class: 'v-global-class',
+            style: {
+              opacity: 0.5,
+            },
+          },
+        } }
+        >
+
+          <VCheckbox />
+        </VDefaultsProvider>
+      ))
+
+      cy.get('.v-checkbox')
+        .should('have.length', 1)
+        // assert it's the root element
+        .should('have.class', 'v-input')
+        .should('have.class', 'v-global-class')
+        .should('have.css', 'opacity', '0.5')
+    })
+  })
+})
