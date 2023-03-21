176541414edb9522d90e4c6e7467390d9b135732
John Leider <john@vuetifyjs.com>
2023-03-21 14:09:05 -0500

    chore(VCombobox): remove dev code

--- packages/vuetify/src/components/VCombobox/VCombobox.tsx
+++ packages/vuetify/src/components/VCombobox/VCombobox.tsx
@@ -93,7 +93,6 @@ export const VCombobox = genericComponent<new <
     ...makeFilterProps({ filterKeys: ['title'] }),
     ...makeSelectProps({ hideNoData: true, returnObject: true }),
     ...omit(makeVTextFieldProps({
-      focusOnClear: false,
       modelValue: null,
     }), ['validationValue', 'dirty', 'appendInnerIcon']),
     ...makeTransitionProps({ transition: false }),
