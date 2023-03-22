e4e7415b8bbf9c4e1cccb48162d6f3ec299e03d6
John Leider <john@vuetifyjs.com>
2023-03-22 12:53:41 -0500

    refactor(VSkeletonLoader): partially implement v2 logic

--- packages/vuetify/src/components/VSkeletonLoader/VSkeletonLoader.tsx
+++ packages/vuetify/src/components/VSkeletonLoader/VSkeletonLoader.tsx
@@ -6,37 +6,102 @@ import './VSkeletonLoader.sass'
 import { VSkeletonBone } from './VSkeletonBone'
 
 // Utilities
-import { genericComponent, useRender } from '@/util'
+import { genericComponent, useRender, wrapInArray } from '@/util'
+import type { PropType } from 'vue'
+import { computed } from 'vue'
 
 const rootTypes = ['card']
+
+export const types = {
+  actions: ['button@2'],
+  article: ['heading', 'paragraph'],
+  avatar: 'avatar',
+  button: 'button',
+  card: ['image', 'card-heading'],
+  'card-avatar': ['image', 'list-item-avatar'],
+  'card-heading': 'heading',
+  chip: 'chip',
+  'date-picker': ['list-item', 'card-heading', 'divider', 'date-picker-options', 'date-picker-days', 'actions'],
+  'date-picker-options': ['text', 'avatar@2'],
+  'date-picker-days': ['avatar@28'],
+  heading: 'heading',
+  image: 'image',
+  'list-item': 'text',
+  'list-item-avatar': ['avatar', 'text'],
+  'list-item-two-line': ['sentences'],
+  'list-item-avatar-two-line': ['avatar', 'sentences'],
+  'list-item-three-line': ['paragraph'],
+  'list-item-avatar-three-line': ['avatar', 'paragraph'],
+  paragraph: ['text@3'],
+  sentences: ['text@2'],
+  table: ['table-heading', 'table-thead', 'table-tbody', 'table-tfoot'],
+  'table-heading': ['heading', 'text'],
+  'table-thead': ['heading@6'],
+  'table-tbody': ['table-row-divider@6'],
+  'table-row-divider': ['table-row', 'divider'],
+  'table-row': ['table-cell@6'],
+  'table-cell': 'text',
+  'table-tfoot': ['text@2', 'avatar@2'],
+  text: 'text',
+} as const
+
+function processElement (element: string | { type: string, children?: any[] }): any[] {
+  const [item, repeat] = (typeof element === 'string' ? element : element.type).split('@')
+  const itemCount = repeat ? parseInt(repeat, 10) : 1
+  const flatItems = Array(itemCount).fill(flattenTypes(item)).flat()
+  const children = typeof element === 'object' && element.children ? flattenTypes(element.children) : []
+
+  return [...flatItems, ...children]
+}
+
+function flattenTypes (key: any): any[] {
+  if (typeof key === 'string') {
+    const value = types[key]
+
+    if (key === value) {
+      return [{ type: key }]
+    }
+
+    return wrapInArray(value).flatMap(processElement)
+  }
+
+  if (Array.isArray(key)) {
+    return key.flatMap(processElement)
+  }
+
+  throw new Error('Invalid argument type for flattenTypes function')
+}
 
 export const VSkeletonLoader = genericComponent()({
   name: 'VSkeletonLoader',
 
   props: {
     type: {
-      type: String,
+      type: [String, Array] as PropType<string | string[]>,
       validator: (val: string) => rootTypes.includes(val),
       default: 'card',
     },
   },
 
-  setup (props) {
+  setup (props, { slots }) {
+    const items = computed(() => {
+      return flattenTypes(props.type as any)
+    })
+
     useRender(() => (
       <div
         class={[
           'v-skeleton-loader',
         ]}
       >
-        { props.type === 'card' && (
-          <>
-            <VSkeletonBone type="image" />
+        { items.value.map((item, i) => (
+          <VSkeletonBone
+            key={i}
+            type={item.type}
+          />
+        )) }
 
-            <VSkeletonBone type="sheet">
-              <VSkeletonBone type="heading" />
-            </VSkeletonBone>
-          </>
-        ) }
+        { slots.default?.() }
       </div>
     ))
 
