e98d14e1ae9ff6967a5d899dc270d8e497180b7b
John Leider <9064066+johnleider@users.noreply.github.com>
2023-03-22 16:22:40 -0500

    docs(buttons): add guide and update page content / examples (#16822)

--- packages/docs/src/examples/v-btn/prop-rounded.vue
+++ packages/docs/src/examples/v-btn/prop-rounded.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="d-flex justify-space-around align-center flex-column flex-md-row fill-height">
-    <v-btn
-      color="primary"
-    >
-      Normal Button
-    </v-btn>
+  <v-container class="text-center">
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4">
+        <v-btn block rounded="0" size="x-large">Rounded 0</v-btn>
+      </v-col>
 
-    <v-btn
-      rounded="lg"
-      color="primary"
-    >
-      Rounded Button
-    </v-btn>
+      <v-col cols="12" sm="6" md="4">
+        <v-btn block rounded="xs" size="x-large">Rounded xs</v-btn>
+      </v-col>
 
-    <v-btn
-      :rounded="0"
-      color="primary"
-    >
-      Tile Button
-    </v-btn>
+      <v-col cols="12" sm="6" md="4">
+        <v-btn block rounded="sm" size="x-large">Rounded sm</v-btn>
+      </v-col>
 
-    <v-btn
-      rounded="pill"
-      color="primary"
-    >
-      Pill Button
-    </v-btn>
-  </div>
+      <v-col cols="12" sm="6" md="4">
+        <v-btn block size="x-large">Button</v-btn>
+      </v-col>
+
+      <v-col cols="12" sm="6" md="4">
+        <v-btn block rounded="lg" size="x-large">Rounded lg</v-btn>
+      </v-col>
+
+      <v-col cols="12" sm="6" md="4">
+        <v-btn block rounded="xl" size="x-large">Rounded xl</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
