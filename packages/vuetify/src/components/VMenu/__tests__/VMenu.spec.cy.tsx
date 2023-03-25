43e46a80a795c469d9cfe35d987cd7f39e1a9454
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2023-03-25 13:56:26 +1100

    Try to set fixed size container to avoid ResizeObserver loop limit exceeded

--- packages/vuetify/src/components/VMenu/__tests__/VMenu.spec.cy.tsx
+++ packages/vuetify/src/components/VMenu/__tests__/VMenu.spec.cy.tsx
@@ -1,29 +1,34 @@
 /// <reference types="../../../../types/cypress" />
 
 import { VBtn, VDefaultsProvider } from '@/components'
+import { Application, CenteredGrid } from '@/../cypress/templates'
 import { VMenu } from '../VMenu'
 
 describe('VMenu', () => {
   describe('global configuration', () => {
     it('should only apply \'v-menu\' class to root element and also apply global config class/style', () => {
       cy.mount(() => (
-        <VDefaultsProvider defaults={ {
-          global: {
-            class: 'v-global-class',
-            style: {
-              opacity: 0.5,
-            },
-          },
-        } }
-        >
-          <VBtn
-            id="menu-activator"
-          >
-            Open Menu
-          </VBtn>
-          <VMenu activator="#menu-activator">
-          </VMenu>
-        </VDefaultsProvider>
+        <Application>
+          <CenteredGrid width="360px">
+            <VDefaultsProvider defaults={ {
+              global: {
+                class: 'v-global-class',
+                style: {
+                  opacity: 0.5,
+                },
+              },
+            } }
+            >
+              <VBtn
+                id="menu-activator"
+              >
+                Open Menu
+              </VBtn>
+              <VMenu activator="#menu-activator">
+              </VMenu>
+            </VDefaultsProvider>
+          </CenteredGrid>
+        </Application>
       ))
 
       cy.get('.v-btn').click()
