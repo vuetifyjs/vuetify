4396441f0ef662c93e054719b26a3ca677b26820
Kael <kaelwd@gmail.com>
2023-04-05 02:04:06 +1000

    test: use stories

--- packages/vuetify/src/components/VSwitch/__tests__/VSwitch.spec.cy.tsx
+++ packages/vuetify/src/components/VSwitch/__tests__/VSwitch.spec.cy.tsx
@@ -1,62 +1,28 @@
 /// <reference types="../../../../types/cypress" />
 
 import { VSwitch } from '../VSwitch'
+import { generate, gridOn } from '@/../cypress/templates'
+
+const contextColor = 'rgb(0, 0, 255)'
+const color = 'rgb(255, 0, 0)'
+const stories = {
+  'Explicit color': gridOn([undefined], [true, false], (_, active) => (
+    <div style={{ color: contextColor }}>
+      <VSwitch modelValue={ active } color={ color } />
+    </div>
+  )),
+  'Inherited color': gridOn([undefined], [true, false], (_, active) => (
+    <div style={{ color: contextColor }}>
+      <VSwitch modelValue={ active } />
+    </div>
+  )),
+  'No color': gridOn([undefined], [true, false], (_, active) => (
+    <VSwitch modelValue={ active } />
+  )),
+}
 
 describe('VSwitch', () => {
-  it('should display color correctly with explicit `color` prop', () => {
-    const contextColor = 'rgb(200, 200, 200)'
-    const color = 'rgb(255, 0, 0)'
-
-    cy.mount(() => (
-
-      <div style={{ color: contextColor }}>
-        <VSwitch color={color} />
-      </div>
-    ))
-      .get('.v-switch__thumb')
-      .should('have.css', 'color', contextColor)
-      .get('.v-switch__track')
-      .should('have.css', 'color', contextColor)
-
-    cy.percySnapshot('VSwitch - color prop - OFF')
-
-    // turn switch ON
-    cy.get('#switch-0').click()
-
-    cy.get('.v-switch__thumb')
-      .should('have.css', 'color', color)
-
-      .get('.v-switch__track')
-      .should('have.css', 'color', color)
-
-    cy.percySnapshot('VSwitch - color prop - ON')
-  })
-
-  it('should display color correctly without `color` prop defined', () => {
-    const contextColor = 'rgb(250, 250, 250)'
-
-    cy.mount(() => (
-
-      <div style={{ color: contextColor }}>
-        <VSwitch />
-      </div>
-    ))
-      .get('.v-switch__thumb')
-      .should('have.css', 'color', contextColor)
-      .get('.v-switch__track')
-      .should('have.css', 'color', contextColor)
-
-    cy.percySnapshot('VSwitch - color prop - OFF')
-
-    // turn switch ON
-    cy.get('#switch-0').click()
-
-    cy.get('.v-switch__thumb')
-      .should('have.css', 'color', contextColor)
-
-      .get('.v-switch__track')
-      .should('have.css', 'color', contextColor)
-
-    cy.percySnapshot('VSwitch - color prop - ON')
+  describe('Showcase', () => {
+    generate({ stories, props: {}, component: VSwitch })
   })
 })
