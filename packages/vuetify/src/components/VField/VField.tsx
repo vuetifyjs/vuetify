d0b41e409139567ce69b334c52df3e6bc45b0715
John Leider <9064066+johnleider@users.noreply.github.com>
2023-04-18 16:11:44 -0400

    feat(VField): add solo-inverted variant (#16794)

--- packages/vuetify/src/components/VField/VField.tsx
+++ packages/vuetify/src/components/VField/VField.tsx
@@ -10,6 +10,7 @@ import { VFieldLabel } from './VFieldLabel'
 import { IconValue } from '@/composables/icons'
 import { LoaderSlot, makeLoaderProps, useLoader } from '@/composables/loader'
 import { makeFocusProps, useFocus } from '@/composables/focus'
+import { makeRoundedProps, useRounded } from '@/composables/rounded'
 import { makeThemeProps, provideTheme } from '@/composables/theme'
 import { useBackgroundColor, useTextColor } from '@/composables/color'
 
@@ -35,7 +36,7 @@ import type { MakeSlots, SlotsToProps } from '@/util'
 import type { PropType, Ref } from 'vue'
 import type { VInputSlot } from '@/components/VInput/VInput'
 
-const allowedVariants = ['underlined', 'outlined', 'filled', 'solo', 'plain'] as const
+const allowedVariants = ['underlined', 'outlined', 'filled', 'solo', 'solo-inverted', 'solo-filled', 'plain'] as const
 type Variant = typeof allowedVariants[number]
 
 export interface DefaultInputSlot {
@@ -63,6 +64,7 @@ export const makeVFieldProps = propsFactory({
   dirty: Boolean,
   disabled: Boolean,
   error: Boolean,
+  flat: Boolean,
   label: String,
   persistentClear: Boolean,
   prependInnerIcon: IconValue,
@@ -80,6 +82,7 @@ export const makeVFieldProps = propsFactory({
 
   ...makeThemeProps(),
   ...makeLoaderProps(),
+  ...makeRoundedProps(),
 }, 'v-field')
 
 export type VFieldSlots = MakeSlots<{
@@ -118,6 +121,7 @@ export const VField = genericComponent<new <T>() => {
     const { loaderClasses } = useLoader(props)
     const { focusClasses, isFocused, focus, blur } = useFocus(props)
     const { InputIcon } = useInputIcon(props)
+    const { roundedClasses } = useRounded(props)
 
     const isActive = computed(() => props.dirty || props.active)
     const hasLabel = computed(() => !props.singleLine && !!(props.label || slots.label))
@@ -218,6 +222,7 @@ export const VField = genericComponent<new <T>() => {
               'v-field--disabled': props.disabled,
               'v-field--dirty': props.dirty,
               'v-field--error': props.error,
+              'v-field--flat': props.flat,
               'v-field--has-background': !!props.bgColor,
               'v-field--persistent-clear': props.persistentClear,
               'v-field--prepended': hasPrepend,
@@ -230,6 +235,7 @@ export const VField = genericComponent<new <T>() => {
             backgroundColorClasses.value,
             focusClasses.value,
             loaderClasses.value,
+            roundedClasses.value,
           ]}
           style={[
             backgroundColorStyles.value,
@@ -258,7 +264,7 @@ export const VField = genericComponent<new <T>() => {
           )}
 
           <div class="v-field__field" data-no-activator="">
-            {['solo', 'filled'].includes(props.variant) && hasLabel.value && (
+            {['filled', 'solo', 'solo-inverted', 'solo-filled'].includes(props.variant) && hasLabel.value && (
               <VFieldLabel
                 key="floating-label"
                 ref={ floatingLabelRef }
