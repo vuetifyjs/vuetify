dd8d98e179decb9f31b887dfa5cf5d9e096740ec
joel-wenzel <wenzje07@gmail.com>
2023-04-18 18:50:15 -0500

    fix(VInput): null check message length

--- packages/vuetify/src/components/VInput/VInput.tsx
+++ packages/vuetify/src/components/VInput/VInput.tsx
@@ -123,7 +123,7 @@ export const VInput = genericComponent<VInputSlots>()({
     useRender(() => {
       const hasPrepend = !!(slots.prepend || props.prependIcon)
       const hasAppend = !!(slots.append || props.appendIcon)
-      const hasMessages = messages.value.length > 0
+      const hasMessages = messages.value?.length > 0
       const hasDetails = !props.hideDetails || (
         props.hideDetails === 'auto' &&
         (hasMessages || !!slots.details)
