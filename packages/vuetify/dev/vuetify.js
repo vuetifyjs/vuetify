8f662dffa4e482db860bf8c22d17cb5028207a6e
John Leider <john@vuetifyjs.com>
2023-04-18 22:13:47 -0500

    chore(dev): remove declared adapter from vuetify playground

--- packages/vuetify/dev/vuetify.js
+++ packages/vuetify/dev/vuetify.js
@@ -5,14 +5,10 @@ import { aliases, mdi } from 'vuetify/src/iconsets/mdi'
 import { fa } from 'vuetify/src/iconsets/fa-svg'
 import { ar, en, ja, sv } from 'vuetify/src/locale'
 import * as directives from 'vuetify/src/directives'
-import DateFnsAdapter from 'vuetify/src/adapters/vuetify'
 
 export default createVuetify({
   directives,
   ssr: !!process.env.VITE_SSR,
-  date: {
-    adapter: DateFnsAdapter,
-  },
   locale: {
     messages: {
       en,
