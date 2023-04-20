919dab25e8fd88764c5f7b48cc00d6bf20212e9c
John Leider <9064066+johnleider@users.noreply.github.com>
2023-04-20 09:09:51 -0500

    feat(date): add new composable (#16691)
    Co-authored-by: nekosaur <albert@kaaman.nu>

--- packages/vuetify/src/util/helpers.ts
+++ packages/vuetify/src/util/helpers.ts
@@ -349,6 +349,10 @@ export function padEnd (str: string, length: number, char = '0') {
   return str + char.repeat(Math.max(0, length - str.length))
 }
 
+export function padStart (str: string, length: number, char = '0') {
+  return char.repeat(Math.max(0, length - str.length)) + str
+}
+
 export function chunk (str: string, size = 1) {
   const chunked: string[] = []
   let index = 0
