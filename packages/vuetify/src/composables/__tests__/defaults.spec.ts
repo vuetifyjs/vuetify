370cbfb91fc2f7d7d907fcb9277587f525a29539
John Leider <john@vuetifyjs.com>
2023-04-26 11:01:29 -0500

    test(defaults): add spec test

---
+++ packages/vuetify/src/composables/__tests__/defaults.spec.ts
@@ -0,0 +1,29 @@
+// Components
+import { VBtn } from '@/components/VBtn'
+
+// Utilities
+import { createVuetify } from '@/framework'
+import { describe, expect, it } from '@jest/globals'
+import { mount } from '@vue/test-utils'
+
+describe('defaults.ts', () => {
+  it('should apply global default classes', () => {
+    const vuetify = createVuetify({
+      defaults: {
+        VBtn: {
+          class: 'foobar',
+          style: 'color: red;',
+        },
+      },
+    })
+
+    const wrapper = mount(VBtn, {
+      global: {
+        plugins: [vuetify],
+      },
+    })
+
+    expect(wrapper.classes()).toContain('foobar')
+    expect(wrapper.attributes('style')).toContain('color: red;')
+  })
+})
