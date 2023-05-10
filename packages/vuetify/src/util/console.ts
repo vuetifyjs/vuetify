c7257d87c38a62ad31df06583bcf85342d527c7a
Kael <kaelwd@gmail.com>
2023-05-10 23:42:58 +1000

    comment unused

--- packages/vuetify/src/util/console.ts
+++ packages/vuetify/src/util/console.ts
@@ -17,8 +17,8 @@ export function deprecate (original: string, replacement: string | string[]) {
   warn(`[Vuetify UPGRADE] '${original}' is deprecated, use ${replacement} instead.`)
 }
 export function breaking (original: string, replacement: string) {
-  warn(`[Vuetify BREAKING] '${original}' has been removed, use '${replacement}' instead. For more information, see the upgrade guide https://github.com/vuetifyjs/vuetify/releases/tag/v2.0.0#user-content-upgrade-guide`)
+  // warn(`[Vuetify BREAKING] '${original}' has been removed, use '${replacement}' instead. For more information, see the upgrade guide https://github.com/vuetifyjs/vuetify/releases/tag/v2.0.0#user-content-upgrade-guide`)
 }
 export function removed (original: string) {
-  warn(`[Vuetify REMOVED] '${original}' has been removed. You can safely omit it.`)
+  // warn(`[Vuetify REMOVED] '${original}' has been removed. You can safely omit it.`)
 }
