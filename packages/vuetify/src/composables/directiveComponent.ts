eab8a175677b9b46be8b9426671008394563f277
Kael <kaelwd@gmail.com>
2023-05-17 16:15:19 +1000

    fix: inherit provides

--- packages/vuetify/src/composables/directiveComponent.ts
+++ packages/vuetify/src/composables/directiveComponent.ts
@@ -1,62 +1,19 @@
+import { h, mergeProps, render, resolveComponent } from 'vue'
 import type {
-  AppContext,
   Component,
   ComponentPublicInstance,
   ConcreteComponent,
   DirectiveBinding,
   ObjectDirective,
-  VNode,
 } from 'vue'
-import { h, render, resolveComponent } from 'vue'
-
-// Function to render the main component
-const renderComponent = (
-  component: Component,
-  children: VNode[] | string,
-  props: any,
-  appContext: AppContext
-) => {
-  const result = h(component as Component, props, children)
-  result.appContext = appContext
-  return result
-}
-
-// Function to render VLayout component if necessary
-const renderVLayoutComponent = (
-  component: Component,
-  layoutComponent: Component,
-  children: VNode
-) => {
-  // Check if the component has a 'name' prop that includes 'layout'
-  if (
-    (component as ConcreteComponent).props?.name &&
-    ((component as ConcreteComponent).props?.name.source as string).includes(
-      'layout'
-    )
-  ) {
-    // Render the component within VLayout
-    const result = h(layoutComponent, null, {
-      default: () => [children],
-    })
-    // Set the appContext to maintain reactivity
-    result.appContext = children.appContext
-    return result
-  }
-
-  return children
-}
 
 export const useDirectiveComponent = (
   component: string | Component,
   props?: any
 ): ObjectDirective => {
-  // Resolve the component if it's a string
-  if (typeof component === 'string') {
-    component = resolveComponent(component)
-  }
-
-  // Resolve the VLayout component
-  const VLayout = resolveComponent('VLayout')
+  const concreteComponent = (typeof component === 'string'
+    ? resolveComponent(component)
+    : component) as ConcreteComponent
 
   return {
     mounted (el: HTMLElement, binding: DirectiveBinding) {
@@ -65,22 +22,14 @@ export const useDirectiveComponent = (
       // Get the children from the props or directive value, or the element's children
       const children = props.text || value.text || el.innerHTML
 
-      // Render the main component with the children, props, and appContext
-      let element = renderComponent(
-        component as Component,
-        children,
-        { ...props, ...value },
-        (binding.instance as ComponentPublicInstance).$.appContext
+      const node = h(concreteComponent, mergeProps(props, value), children)
+      node.appContext = Object.assign(
+        Object.create(null),
+        (binding.instance as ComponentPublicInstance).$.appContext,
+        { provides: binding.instance!.$.provides }
       )
 
-      // Render the component within VLayout if necessary
-      element = renderVLayoutComponent(
-        component as Component,
-        VLayout as Component,
-        element
-      )
-
-      render(element, el)
+      render(node, el)
     },
     unmounted (el: HTMLElement) {
       render(null, el)
