82b7aaeb99f1dc12df5befcdca6b5239bab72c3d
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2023-05-30 16:56:38 +1000

    Add cy test

--- packages/vuetify/src/labs/VInfiniteScroll/VInfiniteScroll.tsx
+++ packages/vuetify/src/labs/VInfiniteScroll/VInfiniteScroll.tsx
@@ -195,6 +195,7 @@ export const VInfiniteScroll = genericComponent<VInfiniteScrollSlots>()({
               window.requestAnimationFrame(() => {
                 intersecting(side)
               })
+
             })
 
           }
