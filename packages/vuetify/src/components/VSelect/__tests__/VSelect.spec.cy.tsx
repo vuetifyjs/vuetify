821218ff3fb98f810d9aaba31d69aa1f2ee5aaac
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2023-06-13 23:17:50 +1000

    chore: improve stack logic to better serve a convincible cy test

--- packages/vuetify/src/components/VSelect/__tests__/VSelect.spec.cy.tsx
+++ packages/vuetify/src/components/VSelect/__tests__/VSelect.spec.cy.tsx
@@ -9,6 +9,7 @@ import { VListItem } from '@/components/VList'
 import { cloneVNode, ref } from 'vue'
 import { generate } from '../../../../cypress/templates'
 import { keyValues } from '@/util'
+import { VAutocomplete, VCol, VCombobox, VRow } from '@/components'
 
 const variants = ['underlined', 'outlined', 'filled', 'solo', 'plain'] as const
 const densities = ['default', 'comfortable', 'compact'] as const
@@ -434,6 +435,46 @@ describe('VSelect', () => {
       .should('have.class', 'v-select--active-menu')
       .trigger('keydown', { key: keyValues.esc })
       .should('not.have.class', 'v-select--active-menu')
+  })
+
+  // https://github.com/vuetifyjs/vuetify/issues/17488
+  it('should close its open menu when the menu of another select component is opened via a click', () => {
+    cy
+      .mount(() => (
+        <VRow>
+          <VCol>
+            <VSelect
+              items={['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']}
+            />
+          </VCol>
+          <VCol>
+            <VAutocomplete
+              items={['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']}
+            />
+          </VCol>
+          <VCol>
+            <VCombobox
+              items={['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']}
+            />
+          </VCol>
+        </VRow> 
+      ))
+      cy.get('.v-select')
+        .trigger('keydown', { key: keyValues.down, waitForAnimations: false })
+        .trigger('keydown', { key: keyValues.down, waitForAnimations: false })
+        .click()
+        .get('.v-overlay__content.v-select__content')
+        .should('exist')
+
+      cy.get('.v-autocomplete')
+        .click()
+        .trigger('keydown', { key: keyValues.down, waitForAnimations: false })
+        .trigger('keydown', { key: keyValues.down, waitForAnimations: false })
+        .get('.v-overlay__content.v-select__content')
+        .should('not.exist')
+        .get('.v-overlay__content.v-autocomplete__content')
+        .should('exist')
+
   })
 
   describe('Showcase', () => {
