cff649c2b6971a32e2b0af896142ac7bb12bbb87
John Leider <john@vuetifyjs.com>
2023-06-13 14:02:12 -0500

    chore(VDataTableVirtual): tweak code

--- packages/vuetify/src/labs/VDataTable/VDataTableVirtual.tsx
+++ packages/vuetify/src/labs/VDataTable/VDataTableVirtual.tsx
@@ -180,27 +180,25 @@ export const VDataTableVirtual = genericComponent<VDataTableVirtualSlots>()({
                     >
                       {{
                         ...slots,
-                        item: itemSlotProps => {
-                          return (
-                            <VVirtualScrollItem
-                              key={ itemSlotProps.item.index }
-                              dynamicHeight
-                              renderless
-                              onUpdate:height={ height => handleItemResize(itemSlotProps.item.index, height) }
-                            >
-                              { slotProps => (
-                                slots.item?.({ ...itemSlotProps, props: { ...itemSlotProps.props, ...slotProps?.props } }) ?? (
-                                  <VDataTableRow
-                                    { ...itemSlotProps.props }
-                                    { ...slotProps?.props }
-                                    key={ itemSlotProps.item.index }
-                                    v-slots={ slots }
-                                  />
-                                )
-                              )}
-                            </VVirtualScrollItem>
-                          )
-                        },
+                        item: itemSlotProps => (
+                          <VVirtualScrollItem
+                            key={ itemSlotProps.item.index }
+                            dynamicHeight
+                            renderless
+                            onUpdate:height={ height => handleItemResize(itemSlotProps.item.index, height) }
+                          >
+                            { slotProps => (
+                              slots.item?.({ ...itemSlotProps, props: { ...itemSlotProps.props, ...slotProps?.props } }) ?? (
+                                <VDataTableRow
+                                  { ...itemSlotProps.props }
+                                  { ...slotProps?.props }
+                                  key={ itemSlotProps.item.index }
+                                  v-slots={ slots }
+                                />
+                              )
+                            )}
+                          </VVirtualScrollItem>
+                        ),
                       }}
                     </VDataTableRows>
 
