a36926d7b584ccaf802155d5fad255c985d3c99b
John Leider <john@vuetifyjs.com>
2023-06-26 15:58:00 -0500

    docs(slot-error): fix lint

--- packages/docs/src/examples/v-img/slot-error.vue
+++ packages/docs/src/examples/v-img/slot-error.vue
@@ -7,12 +7,11 @@
   >
     <template v-slot:error>
       <v-img
-      class="mx-auto"
-      height="300"
-      max-width="500"
-      src="https://picsum.photos/500/300?image=232"
-      >
-      </v-img>
+        class="mx-auto"
+        height="300"
+        max-width="500"
+        src="https://picsum.photos/500/300?image=232"
+      ></v-img>
     </template>
   </v-img>
 </template>
