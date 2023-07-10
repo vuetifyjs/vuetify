0ced830d15add015cd0fdf85d0e2beb07181e6fa
John Leider <john@vuetifyjs.com>
2023-07-10 16:03:23 -0500

    fix(VStepperWindow): avoid registering with parent VStepper as an item

--- packages/vuetify/src/labs/VStepper/VStepperWindow.tsx
+++ packages/vuetify/src/labs/VStepper/VStepperWindow.tsx
@@ -2,18 +2,19 @@
 import { makeVWindowProps, VWindow } from '@/components/VWindow/VWindow'
 
 // Composables
-import { makeGroupItemProps, useGroupItem } from '@/composables/group'
 import { useProxiedModel } from '@/composables/proxiedModel'
 
 // Utilities
-import { computed } from 'vue'
+import { computed, inject } from 'vue'
 import { genericComponent, propsFactory, useRender } from '@/util'
 
 // Types
-import { VStepperSymbol } from './VStepper'
+import type { InjectionKey } from 'vue'
+import type { GroupProvide } from '@/composables/group'
+
+export const VStepperSymbol: InjectionKey<GroupProvide> = Symbol.for('vuetify:v-stepper')
 
 export const makeVStepperWindowProps = propsFactory({
-  ...makeGroupItemProps(),
   ...makeVWindowProps(),
 }, 'VStepperWindow')
 
@@ -23,20 +24,19 @@ export const VStepperWindow = genericComponent()({
   props: makeVStepperWindowProps(),
 
   emits: {
-    'group:selected': (val: { value: boolean }) => true,
     'update:modelValue': (v: unknown) => true,
   },
 
   setup (props, { slots }) {
-    const group = useGroupItem(props, VStepperSymbol, false)
+    const group = inject(VStepperSymbol, null)
     const _model = useProxiedModel(props, 'modelValue')
 
     const model = computed({
       get () {
-        return group?.group.selected.value ?? _model.value
+        return group?.selected.value ?? _model.value
       },
       set (val) {
-        group?.group.select(val[0], true)
+        group?.select(val[0], true)
         _model.value = val
       },
     })
