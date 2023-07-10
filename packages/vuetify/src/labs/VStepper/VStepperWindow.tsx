d3957ed9360d0f1d54bc4f07a5a1e1a43261d1be
John Leider <john@vuetifyjs.com>
2023-07-10 15:30:49 -0500

    feat(VStepperWindow): inject parent model if available

--- packages/vuetify/src/labs/VStepper/VStepperWindow.tsx
+++ packages/vuetify/src/labs/VStepper/VStepperWindow.tsx
@@ -2,12 +2,18 @@
 import { makeVWindowProps, VWindow } from '@/components/VWindow/VWindow'
 
 // Composables
+import { makeGroupItemProps, useGroupItem } from '@/composables/group'
 import { useProxiedModel } from '@/composables/proxiedModel'
 
 // Utilities
+import { computed } from 'vue'
 import { genericComponent, propsFactory, useRender } from '@/util'
 
+// Types
+import { VStepperSymbol } from './VStepper'
+
 export const makeVStepperWindowProps = propsFactory({
+  ...makeGroupItemProps(),
   ...makeVWindowProps(),
 }, 'VStepperWindow')
 
@@ -17,11 +23,23 @@ export const VStepperWindow = genericComponent()({
   props: makeVStepperWindowProps(),
 
   emits: {
+    'group:selected': (val: { value: boolean }) => true,
     'update:modelValue': (v: unknown) => true,
   },
 
   setup (props, { slots }) {
-    const model = useProxiedModel(props, 'modelValue')
+    const group = useGroupItem(props, VStepperSymbol, false)
+    const _model = useProxiedModel(props, 'modelValue')
+
+    const model = computed({
+      get () {
+        return group?.group.selected.value ?? _model.value
+      },
+      set (val) {
+        group?.group.select(val[0], true)
+        _model.value = val
+      },
+    })
 
     useRender(() => {
       const [windowProps] = VWindow.filterProps(props)
@@ -35,6 +53,8 @@ export const VStepperWindow = genericComponent()({
         />
       )
     })
+
+    return {}
   },
 })
 
