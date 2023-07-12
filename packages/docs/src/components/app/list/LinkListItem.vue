e3ebde514e35e38b82710ed1b3da86ead5987f30
John Leider <john@vuetifyjs.com>
2023-07-12 15:32:07 -0500

    docs(AppDrawer/Settings): remove github login, other tweaks

---
+++ packages/docs/src/components/app/list/LinkListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="text-medium-emphasis text-caption py-2 px-3 d-flex align-center">
+    <div class="d-inline-flex align-center">
+      <v-icon v-if="prependIcon" :icon="prependIcon" start />
+
+      {{ label }}
+    </div>
+
+    <v-btn
+      v-bind="$attrs"
+      class="text-none px-2 ms-auto"
+      density="compact"
+      variant="text"
+    >
+      {{ title }}
+
+      <v-icon v-if="appendIcon" :icon="appendIcon" size="xs" end />
+    </v-btn>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    label: String,
+    title: String,
+    prependIcon: String,
+    appendIcon: String,
+  })
+</script>
+
+<script>
+  export default {
+    inheritAttrs: false,
+  }
+</script>
--- packages/docs/src/components/app/settings/Append.vue
+++ packages/docs/src/components/app/settings/Append.vue
@@ -1,54 +1,23 @@
 <template>
-  <v-list-item v-if="GITHUB_SHA" class="text-caption">
-    Build
-    <a
-      :href="`https://github.com/vuetifyjs/vuetify/commit/${GITHUB_SHA}`"
-      target="_blank"
-      rel="noopener noreferrer"
-    >{{ GITHUB_SHA }}</a>
-  </v-list-item>
+  <v-divider v-if="GITHUB_SHA" />
 
-  <v-divider />
-
-  <v-list-item
-    :prepend-avatar="user?.picture"
-    :prepend-icon="isAuthenticated ? undefined : 'mdi-github'"
-    :title="isAuthenticated ? user?.name : 'Login w/ GitHub'"
-    :subtitle="user?.email"
-    :link="!isAuthenticated"
-    lines="two"
-    @click="onClick"
-  >
-    <template #append>
-      <v-icon
-        v-if="isAuthenticated"
-        icon="mdi-logout-variant"
-        @click="onClickLogout"
-      />
-
-      <v-icon
-        v-else
-        icon="mdi-login-variant"
-      />
-    </template>
-  </v-list-item>
+  <app-link-list-item
+    v-if="GITHUB_SHA"
+    :href="`https://github.com/vuetifyjs/vuetify/commit/${GITHUB_SHA}`"
+    :label="t('documentation-build')"
+    :title="GITHUB_SHA"
+    prepend-icon="mdi-package"
+    append-icon="mdi-open-in-new"
+    rel="noopener noreferrer"
+    target="_blank"
+  />
 </template>
 
 <script setup>
   // Composables
-  import { useAuth0 } from '@auth0/auth0-vue'
+  import { useI18n } from 'vue-i18n'
 
   const GITHUB_SHA = import.meta.env.VITE_GITHUB_SHA
 
-  const { loginWithPopup, user, isAuthenticated, logout } = useAuth0()
-
-  function onClick () {
-    if (isAuthenticated.value) return
-
-    loginWithPopup()
-  }
-
-  function onClickLogout () {
-    logout({ logoutParams: { returnTo: window.location.origin } })
-  }
+  const { t } = useI18n()
 </script>
