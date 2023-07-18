9335fa10fb0434c0cb2e130dd778d23a30cb6200
John Leider <john@vuetifyjs.com>
2023-07-18 15:55:08 -0500

    chore(cosmic): add new key names

--- packages/docs/src/composables/cosmic.ts
+++ packages/docs/src/composables/cosmic.ts
@@ -2,8 +2,8 @@
 import { createBucketClient } from '@cosmicjs/sdk'
 
 export function useCosmic<T> (
-  bucketSlug = import.meta.env.VITE_COSMIC_BUCKET_SLUG as string | undefined,
-  readKey = import.meta.env.VITE_COSMIC_BUCKET_READ_KEY as string | undefined,
+  bucketSlug = import.meta.env.VITE_COSMIC_2_BUCKET_SLUG as string | undefined,
+  readKey = import.meta.env.VITE_COSMIC_2_BUCKET_READ_KEY as string | undefined,
 ) {
   return {
     bucket: (readKey && bucketSlug)
