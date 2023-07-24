f4460c5b40d0370342e27f6f0b1498345dc16733
gitemad <61329625+gitemad@users.noreply.github.com>
2023-07-24 23:44:53 +0330

    fix(VFileInput): prevent file dialog when readonly (#17853)
    fixes #17851
    
    Co-authored-by: John Leider <john@vuetifyjs.com>

--- packages/vuetify/src/components/VFileInput/VFileInput.tsx
+++ packages/vuetify/src/components/VFileInput/VFileInput.tsx
@@ -223,6 +223,8 @@ export const VFileInput = genericComponent<VFileInputSlots>()({
                         onClick={ e => {
                           e.stopPropagation()
 
+                          if (isReadonly.value) e.preventDefault()
+
                           onFocus()
                         }}
                         onChange={ e => {
