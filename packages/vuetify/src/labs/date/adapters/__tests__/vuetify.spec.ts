faaa60db155ee25841b01f787bd73435640aabfc
nekosaur <albert@kaaman.nu>
2023-07-26 13:07:23 +0200

    fix(date): parse string dates in local time zone

--- packages/vuetify/src/labs/date/adapters/__tests__/vuetify.spec.ts
+++ packages/vuetify/src/labs/date/adapters/__tests__/vuetify.spec.ts
@@ -1,5 +1,6 @@
 // Utilities
 import { describe, expect, it } from '@jest/globals'
+import timezoneMock, { TimeZone } from 'timezone-mock'
 import { VuetifyDateAdapter } from '../vuetify'
 
 describe('vuetify date adapter', () => {
@@ -21,5 +22,31 @@ describe('vuetify date adapter', () => {
     instance = new VuetifyDateAdapter({ locale: 'sv-SE' })
 
     expect(instance.format(new Date(2000, 0, 1), 'fullDateWithWeekday')).toBe('lördag 1 januari 2000')
+  })
+
+  it.each([
+    'UTC',
+    'US/Pacific',
+    'Europe/London',
+    'Brazil/East',
+    'Australia/Adelaide',
+    'Etc/GMT-2',
+    'Etc/GMT-4',
+    'Etc/GMT+4',
+  ])('should handle timezone %s when parsing date without time', (timezone) => {
+    // locale option here has no impact on timezone
+    let instance = new VuetifyDateAdapter({ locale: 'en-us' })
+
+    const str = "2001-01-01"
+
+    timezoneMock.register(timezone as TimeZone)
+
+    const date = instance.date(str)
+
+    expect(date?.getFullYear()).toBe(2001)
+    expect(date?.getDate()).toBe(1)
+    expect(date?.getMonth()).toBe(0)
+
+    timezoneMock.unregister()
   })
 })
