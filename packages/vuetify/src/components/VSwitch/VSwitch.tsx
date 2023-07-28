f9748a61a809535e18aacd5d07a2b0945784d098
SonTT19 <tran.thanh.son.acc@gmail.com>
2023-07-28 11:39:08 +0700

    fix(VSwicth): use sass for default progress color

--- packages/vuetify/src/components/VSwitch/VSwitch.tsx
+++ packages/vuetify/src/components/VSwitch/VSwitch.tsx
@@ -60,7 +60,7 @@ export const VSwitch = genericComponent<VSwitchSlots>()({
     const loaderColor = computed(() => {
       return typeof props.loading === 'string' && props.loading !== ''
         ? props.loading
-        : (props.color || 'primary')
+        : props.color
     })
 
     const uid = getUid()
