47828613a0e67655a076b701f74e8d280c2a1d66
Kael <kaelwd@gmail.com>
2023-08-10 19:36:50 +1000

    docs: correct user preferences update merging logic

--- packages/docs/src/store/user.ts
+++ packages/docs/src/store/user.ts
@@ -8,7 +8,7 @@ import { defineStore } from 'pinia'
 import { reactive, toRefs } from 'vue'
 
 export type RootState = {
-  v: number
+  v: 2
   api: 'link-only' | 'inline'
   dev: boolean
   composition: ('options' | 'composition')
@@ -31,9 +31,64 @@ export type RootState = {
   }
 }
 
+type SavedState = {
+  api: boolean
+  drawer: { alphabetical: boolean, mini: boolean }
+  last: {
+    install: null | number
+    notification: null | number
+    promotion: null | number
+    jobs: null | number
+  }
+  pwaRefresh: boolean
+  rtl: boolean
+  theme: {
+    dark: boolean
+    system: boolean
+    mixed: boolean
+  }
+} | {
+  api: 'link-only' | 'inline'
+  pwaRefresh: boolean
+  theme: string
+  mixedTheme: boolean
+  direction: 'rtl' | 'ltr'
+  notifications: {
+    read: string[]
+    last: {
+      install: null | number
+      notification: null | number
+      promotion: null | number
+      jobs: null | number
+    }
+  }
+} | {
+  v: 1
+  api: 'link-only' | 'inline'
+  dev?: boolean
+  composition?: ('options' | 'composition') | ('options' | 'composition')[]
+  pwaRefresh: boolean
+  theme: string
+  mixedTheme: boolean
+  direction: 'rtl' | 'ltr'
+  quickbar?: boolean
+  notifications: {
+    show?: boolean
+    read: string[]
+    last: {
+      banner?: null | number | string[]
+      v2banner?: null | number
+      install: null | number
+      notification: null | number
+      promotion: null | number
+      jobs: null | number
+    }
+  }
+} | RootState
+
 export const useUserStore = defineStore('user', () => {
   const state = reactive<RootState>({
-    v: 1,
+    v: 2,
     api: 'link-only',
     dev: false,
     composition: 'options',
@@ -61,6 +116,7 @@ export const useUserStore = defineStore('user', () => {
 
     const stored = localStorage.getItem('vuetify@user')
     const data = stored ? JSON.parse(stored) : {}
+    const needsRefresh = data.v === state.v
 
     if (!data.v) {
       data.pwaRefresh = true
@@ -77,25 +133,29 @@ export const useUserStore = defineStore('user', () => {
           : data.theme.dark ? 'dark'
           : 'light'
       }
+      if (Array.isArray(data.notifications)) {
+        data.notifications = { read: data.notifications }
+      }
       if (typeof data.last === 'object') {
         data.notifications.last = data.last
         delete data.last
       }
     }
 
-    if (Array.isArray(data.notifications)) {
-      data.notifications = { read: data.notifications }
+    if (data.v === 1) {
+      if (Array.isArray(data.composition)) {
+        data.composition = 'composition'
+      }
+      if (!Array.isArray(data.notifications.last.banner)) {
+        data.notifications.last.banner = []
+      }
     }
 
-    if (Array.isArray(data.composition)) {
-      data.composition = 'composition'
-    }
-
-    if (!Array.isArray(data.notifications.last.banner)) {
-      data.notifications.last.banner = []
-    }
-
+    data.v = state.v
     Object.assign(state, merge(state, data))
+    if (needsRefresh) {
+      save()
+    }
   }
 
   function save () {
