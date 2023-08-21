be059b2ba5fd62bc77c419c09a8c226c7d07fb67
Kael <kaelwd@gmail.com>
2023-08-21 22:12:54 +1000

    fix(VSwitch): improve dark theme visibility

--- packages/vuetify/src/components/VSwitch/VSwitch.tsx
+++ packages/vuetify/src/components/VSwitch/VSwitch.tsx
@@ -12,7 +12,6 @@ import { makeVSelectionControlProps, VSelectionControl } from '@/components/VSel
 import { useFocus } from '@/composables/focus'
 import { LoaderSlot, useLoader } from '@/composables/loader'
 import { useProxiedModel } from '@/composables/proxiedModel'
-import { useTheme } from '@/composables/theme'
 
 // Utilities
 import { computed, ref } from 'vue'
@@ -85,7 +84,6 @@ export const VSwitch = genericComponent<VSwitchSlots>()({
       const [rootAttrs, controlAttrs] = filterInputAttrs(attrs)
       const [inputProps, _1] = VInput.filterProps(props)
       const [controlProps, _2] = VSelectionControl.filterProps(props)
-      const { current } = useTheme()
 
       return (
         <VInput
@@ -93,7 +91,6 @@ export const VSwitch = genericComponent<VSwitchSlots>()({
             'v-switch',
             { 'v-switch--inset': props.inset },
             { 'v-switch--indeterminate': indeterminate.value },
-            { 'v-switch--dark' : current.value.dark },
             loaderClasses.value,
             props.class,
           ]}
@@ -129,15 +126,26 @@ export const VSwitch = genericComponent<VSwitchSlots>()({
               >
                 {{
                   ...slots,
-                  default: () => (<div class="v-switch__track" onClick={ onTrackClick }></div>),
-                  input: ({ inputNode, icon }) => (
+                  default: ({ backgroundColorClasses, backgroundColorStyles }) => (
+                    <div
+                      class={[
+                        'v-switch__track',
+                        ...backgroundColorClasses.value,
+                      ]}
+                      style={ backgroundColorStyles.value }
+                      onClick={ onTrackClick }
+                    ></div>
+                  ),
+                  input: ({ inputNode, icon, backgroundColorClasses, backgroundColorStyles }) => (
                     <>
                       { inputNode }
                       <div
                         class={[
                           'v-switch__thumb',
                           { 'v-switch__thumb--filled': icon || props.loading },
+                          props.inset ? undefined : backgroundColorClasses.value,
                         ]}
+                        style={ props.inset ? undefined : backgroundColorStyles.value }
                       >
                         <VScaleTransition>
                           { !props.loading ? (
