71ceced325ab47a6e660b3d6420fb861cf72fafc
John Leider <john@vuetifyjs.com>
2023-09-28 12:35:34 -0500

    docs(user): update advanced options

--- packages/docs/src/components/app/settings/AdvancedOptions.vue
+++ packages/docs/src/components/app/settings/AdvancedOptions.vue
@@ -8,19 +8,40 @@
 
   <notifications-option />
 
+  <br>
+
   <div class="d-flex justify-end">
     <v-btn
+      :disabled="isDisabled"
       :text="t('reset-all')"
       color="error"
       size="small"
-      variant="flat"
-      @click="onResetAll"
+      variant="outlined"
+      @click="onResetAllNotifications"
     />
   </div>
 
   <v-divider class="my-3" />
 
+  <settings-header
+    title="dashboard.advanced-options.danger-zone"
+    text="dashboard.advanced-options.danger-zone-message"
+  />
+
   <developer-mode />
+
+  <br>
+
+  <div class="d-flex justify-end">
+    <v-btn
+      :text="t('reset-all-settings')"
+      color="error"
+      prepend-icon="mdi-alert-circle-outline"
+      size="small"
+      variant="flat"
+      @click="onResetAllSettings"
+    />
+  </div>
 </template>
 
 <script setup>
@@ -36,14 +57,25 @@
   // Stores
   import { useUserStore } from '@/store/user'
 
+  // Utilities
+  import { computed } from 'vue'
+
   const { t } = useI18n()
   const user = useUserStore()
 
-  function onResetAll () {
+  const isDisabled = computed(() => (
+    user.notifications.last.banner.length === 0 &&
+    user.notifications.read.length === 0
+  ))
+
+  function onResetAllNotifications () {
     user.banner = true
     user.notifications.read = []
     user.notifications.show = true
     user.notifications.last.banner = []
-    user.quickbar = true
+  }
+
+  function onResetAllSettings () {
+    user.reset()
   }
 </script>
