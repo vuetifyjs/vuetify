f710ccf4aad54517992303e23cbbf5596a793124
John Leider <john@vuetifyjs.com>
2023-10-05 16:19:45 -0500

    docs(AppDrawerAppend): add rail toggle for subscribers

--- packages/docs/src/components/app/drawer/Append.vue
+++ packages/docs/src/components/app/drawer/Append.vue
@@ -1,10 +1,26 @@
 <template>
   <v-divider />
 
-  <git-hub-login />
+  <v-hover>
+    <template #default="{ props: hoverProps, isHovering }">
+      <div v-bind="hoverProps">
+        <v-expand-transition v-if="auth.isSubscriber">
+          <drawer-toggle-rail v-if="isHovering" />
+        </v-expand-transition>
+
+        <git-hub-login />
+      </div>
+    </template>
+  </v-hover>
 </template>
 
 <script setup>
   // Components
+  import DrawerToggleRail from '@/components/app/drawer/DrawerToggleRail.vue'
   import GitHubLogin from '@/components/app/GitHubLogin.vue'
+
+  // Stores
+  import { useAuthStore } from '@/store/auth'
+
+  const auth = useAuthStore()
 </script>
