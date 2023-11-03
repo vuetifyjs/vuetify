vuetifyjs/vuetify

commit fc84e78648800902cbb41bb20910409273411c2e

feat(VDataTable): nested headers object structure (#18512)
closes #18484

Co-authored-by: Kael <kaelwd@gmail.com>

packages/vuetify/src/labs/VDataTable/composables/headers.ts
@@ -1,15 +1,16 @@
 // Utilities
 import { inject, provide, ref, watchEffect } from 'vue'
-import { createRange, propsFactory } from '@/util'
+import { consoleError, propsFactory } from '@/util'
 
 // Types
 import type { DeepReadonly, InjectionKey, PropType, Ref } from 'vue'
 import type { SortItem } from './sort'
 import type { DataTableHeader, InternalDataTableHeader } from '../types'
+import type { SelectItemKey } from '@/util'
 
 export const makeDataTableHeaderProps = propsFactory({
   headers: {
-    type: Array as PropType<DeepReadonly<DataTableHeader[] | DataTableHeader[][]>>,
+    type: Array as PropType<DeepReadonly<DataTableHeader[]>>,
     default: () => ([]),
   },
 }, 'DataTable-header')
@@ -20,7 +21,199 @@ export const VDataTableHeadersSymbol: InjectionKey<{
 }> = Symbol.for('vuetify:data-table-headers')
 
 type HeaderProps = {
-  headers: DeepReadonly<DataTableHeader[] | DataTableHeader[][]>
+  headers: DeepReadonly<DataTableHeader[]>
+}
+
+const defaultHeader = { title: '', sortable: false }
+const defaultActionHeader = { ...defaultHeader, width: 48 }
+
+function priorityQueue <T> (arr: T[] = []) {
+  const queue: { element: T, priority: number }[] = arr.map(element => ({ element, priority: 0 }))
+
+  return {
+    enqueue: (element: T, priority: number) => {
+      let added = false
+      for (let i = 0; i < queue.length; i++) {
+        const item = queue[i]
+        if (item.priority > priority) {
+          queue.splice(i, 0, { element, priority })
+          added = true
+          break
+        }
+      }
+
+      if (!added) queue.push({ element, priority })
+    },
+    size: () => queue.length,
+    count: () => {
+      let count = 0
+
+      if (!queue.length) return 0
+
+      const whole = Math.floor(queue[0].priority)
+      for (let i = 0; i < queue.length; i++) {
+        if (Math.floor(queue[i].priority) === whole) count += 1
+      }
+
+      return count
+    },
+    dequeue: () => {
+      return queue.shift()
+    },
+  }
+}
+
+function extractLeaves (item: InternalDataTableHeader, columns: InternalDataTableHeader[] = []) {
+  if (!item.children) {
+    columns.push(item)
+  } else {
+    for (const child of item.children) {
+      extractLeaves(child, columns)
+    }
+  }
+
+  return columns
+}
+
+function extractKeys (headers: DeepReadonly<DataTableHeader[]>, keys = new Set<string>()) {
+  for (const item of headers) {
+    if (item.key) keys.add(item.key)
+
+    if (item.children) {
+      extractKeys(item.children, keys)
+    }
+  }
+
+  return keys
+}
+
+function getDefaultItem (item: DeepReadonly<DataTableHeader>) {
+  if (!item.key) return undefined
+  if (item.key === 'data-table-group') return defaultHeader
+  if (['data-table-expand', 'data-table-select'].includes(item.key)) return defaultActionHeader
+  return undefined
+}
+
+function getDepth (item: InternalDataTableHeader, depth = 0): number {
+  if (!item.children) return depth
+
+  return Math.max(depth, ...item.children.map(child => getDepth(child, depth + 1)))
+}
+
+function parseFixedColumns (items: InternalDataTableHeader[]) {
+  let seenFixed = false
+  function setFixed (item: InternalDataTableHeader, parentFixed = false) {
+    if (!item) return
+
+    if (parentFixed) {
+      item.fixed = true
+    }
+
+    if (item.fixed) {
+      if (item.children) {
+        for (let i = item.children.length - 1; i >= 0; i--) {
+          setFixed(item.children[i], true)
+        }
+      } else {
+        if (!seenFixed) {
+          item.lastFixed = true
+        } else if (isNaN(+item.width!)) {
+          consoleError(`Multiple fixed columns should have a static width (key: ${item.key})`)
+        }
+        seenFixed = true
+      }
+    } else {
+      if (item.children) {
+        for (let i = item.children.length - 1; i >= 0; i--) {
+          setFixed(item.children[i])
+        }
+      } else {
+        seenFixed = false
+      }
+    }
+  }
+
+  for (let i = items.length - 1; i >= 0; i--) {
+    setFixed(items[i])
+  }
+
+  function setFixedOffset (item: InternalDataTableHeader, fixedOffset = 0) {
+    if (!item) return fixedOffset
+
+    if (item.children) {
+      item.fixedOffset = fixedOffset
+      for (const child of item.children) {
+        fixedOffset = setFixedOffset(child, fixedOffset)
+      }
+    } else if (item.fixed) {
+      item.fixedOffset = fixedOffset
+      fixedOffset += parseFloat(item.width || '0') || 0
+    }
+
+    return fixedOffset
+  }
+
+  let fixedOffset = 0
+  for (const item of items) {
+    fixedOffset = setFixedOffset(item, fixedOffset)
+  }
+}
+
+function parse (items: InternalDataTableHeader[], maxDepth: number) {
+  const headers: InternalDataTableHeader[][] = []
+  let currentDepth = 0
+  const queue = priorityQueue(items)
+
+  while (queue.size() > 0) {
+    let rowSize = queue.count()
+    const row: InternalDataTableHeader[] = []
+    let fraction = 1
+    while (rowSize > 0) {
+      const { element: item, priority } = queue.dequeue()!
+      const diff = maxDepth - currentDepth - getDepth(item)
+
+      row.push({
+        ...item,
+        rowspan: diff ?? 1,
+        colspan: item.children ? extractLeaves(item).length : 1,
+      })
+
+      if (item.children) {
+        for (const child of item.children) {
+          // This internally sorts items that are on the same priority "row"
+          const sort = priority % 1 + (fraction / Math.pow(10, currentDepth + 1))
+          queue.enqueue(child, currentDepth + diff + sort)
+        }
+      }
+
+      fraction += 1
+      rowSize -= 1
+    }
+    currentDepth += 1
+    headers.push(row)
+  }
+
+  const columns = items.map(item => extractLeaves(item)).flat()
+
+  return { columns, headers }
+}
+
+function convertToInternalHeaders (items: DeepReadonly<DataTableHeader[]>) {
+  const internalHeaders: InternalDataTableHeader[] = []
+  for (const item of items) {
+    const defaultItem = { ...getDefaultItem(item), ...item }
+    const internalItem: InternalDataTableHeader = {
+      ...defaultItem,
+      key: defaultItem.key ?? null,
+      value: (defaultItem.key ?? defaultItem.value ?? null) as any as SelectItemKey,
+      sortable: defaultItem.sortable ?? defaultItem.key != null,
+      children: defaultItem.children ? convertToInternalHeaders(defaultItem.children) : undefined,
+    }
+
+    internalHeaders.push(internalItem)
+  }
+
+  return internalHeaders
 }
 
 export function createHeaders (
@@ -35,80 +228,30 @@ export function createHeaders (
   const columns = ref<InternalDataTableHeader[]>([])
 
   watchEffect(() => {
-    const wrapped = !props.headers.length
-      ? []
-      : Array.isArray(props.headers[0])
-        ? props.headers as DataTableHeader[][]
-        : [props.headers as DataTableHeader[]]
-    const flat = wrapped.flatMap((row, index) => row.map(column => ({
-      column,
-      row: index,
-    })))
+    const items = props.headers.slice()
+    const keys = extractKeys(items)
 
-    const rowCount = wrapped.length
-    const defaultHeader = { title: '', sortable: false }
-    const defaultActionHeader = { ...defaultHeader, width: 48 }
-
-    if (options?.groupBy?.value.length) {
-      const index = flat.findIndex(({ column }) => column.key === 'data-table-group')
-      if (index < 0) flat.unshift({ column: { ...defaultHeader, key: 'data-table-group', title: 'Group', rowspan: rowCount }, row: 0 })
-      else flat.splice(index, 1, { column: { ...defaultHeader, ...flat[index].column }, row: flat[index].row })
+    if (options?.groupBy?.value.length && !keys.has('data-table-group')) {
+      items.unshift({ key: 'data-table-group', title: 'Group' })
     }
 
-    if (options?.showSelect?.value) {
-      const index = flat.findIndex(({ column }) => column.key === 'data-table-select')
-      if (index < 0) flat.unshift({ column: { ...defaultActionHeader, key: 'data-table-select', rowspan: rowCount }, row: 0 })
-      else flat.splice(index, 1, { column: { ...defaultActionHeader, ...flat[index].column }, row: flat[index].row })
+    if (options?.showSelect?.value && !keys.has('data-table-select')) {
+      items.unshift({ key: 'data-table-select' })
     }
 
-    if (options?.showExpand?.value) {
-      const index = flat.findIndex(({ column }) => column.key === 'data-table-expand')
-      if (index < 0) flat.push({ column: { ...defaultActionHeader, key: 'data-table-expand', rowspan: rowCount }, row: 0 })
-      else flat.splice(index, 1, { column: { ...defaultActionHeader, ...flat[index].column }, row: flat[index].row })
+    if (options?.showExpand?.value && !keys.has('data-table-expand')) {
+      items.push({ key: 'data-table-expand' })
     }
 
-    const fixedRows: InternalDataTableHeader[][] = createRange(rowCount).map(() => [])
-    const fixedOffsets = createRange(rowCount).fill(0)
+    const internalHeaders = convertToInternalHeaders(items)
 
-    flat.forEach(({ column, row }) => {
-      const key = column.key ?? (typeof column.value === 'string' ? column.value : null)
-      const value = column.value ?? column.key ?? null
-      for (let i = row; i <= row + (column.rowspan ?? 1) - 1; i++) {
-        fixedRows[i].push({
-          ...column,
-          key,
-          value,
-          fixedOffset: fixedOffsets[i],
-          sortable: column.sortable ?? key != null,
-        })
+    parseFixedColumns(internalHeaders)
 
-        fixedOffsets[i] += Number(column.width ?? 0)
-      }
-    })
+    const maxDepth = Math.max(...internalHeaders.map(item => getDepth(item))) + 1
+    const parsed = parse(internalHeaders, maxDepth)
 
-    fixedRows.forEach(row => {
-      for (let i = row.length; i--; i >= 0) {
-        if (row[i].fixed) {
-          row[i].lastFixed = true
-          return
-        }
-      }
-    })
-
-    const seen = new Set()
-    headers.value = fixedRows.map(row => {
-      const filtered = []
-      for (const column of row) {
-        if (!seen.has(column.key)) {
-          seen.add(column.key)
-          filtered.push(column)
-        }
-      }
-
-      return filtered
-    })
-
-    columns.value = fixedRows.at(-1) ?? []
+    headers.value = parsed.headers
+    columns.value = parsed.columns
   })
 
   const data = { headers, columns }

packages/docs/src/examples/v-data-table/headers-multiple.vue
@@ -92,27 +92,22 @@
     },
   ]
   const headers = [
-    [
-      {
-        title: 'Dessert (100g serving)',
-        align: 'start',
-        sortable: false,
-        key: 'name',
-        rowspan: 2,
-      },
-      {
-        title: 'Properties',
-        key: 'foo',
-        colspan: 5,
-      },
-    ],
-    [
-      { title: 'Calories', align: 'end', key: 'calories' },
-      { title: 'Fat (g)', align: 'end', key: 'fat' },
-      { title: 'Carbs (g)', align: 'end', key: 'carbs' },
-      { title: 'Protein (g)', align: 'end', key: 'protein' },
-      { title: 'Iron (%)', align: 'end', key: 'iron' },
-    ],
+    {
+      title: 'Dessert (100g serving)',
+      align: 'start',
+      sortable: false,
+      key: 'name',
+    },
+    {
+      title: 'Properties',
+      children: [
+        { title: 'Calories', align: 'end', key: 'calories' },
+        { title: 'Fat (g)', align: 'end', key: 'fat' },
+        { title: 'Carbs (g)', align: 'end', key: 'carbs' },
+        { title: 'Protein (g)', align: 'end', key: 'protein' },
+        { title: 'Iron (%)', align: 'end', key: 'iron' },
+      ],
+    },
   ]
 </script>
 
@@ -202,27 +197,23 @@
         },
       ],
       headers: [
-        [
-          {
-            title: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            key: 'name',
-            rowspan: 2,
-          },
-          {
-            title: 'Properties',
-            key: 'foo',
-            colspan: 5,
-          },
-        ],
-        [
-          { title: 'Calories', align: 'end', key: 'calories' },
-          { title: 'Fat (g)', align: 'end', key: 'fat' },
-          { title: 'Carbs (g)', align: 'end', key: 'carbs' },
-          { title: 'Protein (g)', align: 'end', key: 'protein' },
-          { title: 'Iron (%)', align: 'end', key: 'iron' },
-        ],
+        {
+          title: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          key: 'name',
+          rowspan: 2,
+        },
+        {
+          title: 'Properties',
+          children: [
+            { title: 'Calories', align: 'end', key: 'calories' },
+            { title: 'Fat (g)', align: 'end', key: 'fat' },
+            { title: 'Carbs (g)', align: 'end', key: 'carbs' },
+            { title: 'Protein (g)', align: 'end', key: 'protein' },
+            { title: 'Iron (%)', align: 'end', key: 'iron' },
+          ],
+        },
       ],
     }),
   }

packages/vuetify/src/components/VTable/VTable.tsx
@@ -34,7 +34,7 @@ export const VTable = genericComponent<VTableSlots>()({
 
   props: makeVTableProps(),
 
-  setup (props, { slots }) {
+  setup (props, { slots, emit }) {
     const { themeClasses } = provideTheme(props)
     const { densityClasses } = useDensity(props)
 

packages/vuetify/src/labs/VDataTable/VDataTable.sass
@@ -1,5 +1,6 @@
 @use '../../styles/settings'
 @use '../../styles/tools'
+@use '../../components/VTable/variables' as *
 @use './variables' as *
 
 .v-data-table
@@ -44,6 +45,9 @@
           > th
             align-items: center
 
+          > th.v-data-table__th--fixed
+              position: sticky
+
           > th.v-data-table__th--sortable:hover
               cursor: pointer
               color: rgba(var(--v-theme-on-surface), var(--v-high-emphasis-opacity))
@@ -57,8 +61,8 @@
                   opacity: 0.5
 
 .v-data-table-column--fixed
+  background: $table-background
   position: sticky
-  z-index: 2
   left: 0
 
 .v-data-table-column--last-fixed

packages/vuetify/src/labs/VDataTable/VDataTableHeaders.tsx
@@ -31,7 +31,6 @@ export type HeadersSlotProps = {
   toggleSort: ReturnType<typeof provideSort>['toggleSort']
   selectAll: ReturnType<typeof provideSelection>['selectAll']
   getSortIcon: (column: InternalDataTableHeader) => IconValue
-  getFixedStyles: (column: InternalDataTableHeader, y: number) => CSSProperties | undefined
   isSorted: ReturnType<typeof provideSort>['isSorted']
 }
 
@@ -74,19 +73,18 @@ export const VDataTableHeaders = genericComponent<VDataTableHeadersSlots>()({
 
   props: makeVDataTableHeadersProps(),
 
-  setup (props, { slots, emit }) {
+  setup (props, { slots }) {
     const { toggleSort, sortBy, isSorted } = useSort()
     const { someSelected, allSelected, selectAll, showSelectAll } = useSelection()
     const { columns, headers } = useHeaders()
     const { loaderClasses } = useLoader(props)
 
-    const getFixedStyles = (column: InternalDataTableHeader, y: number): CSSProperties | undefined => {
+    function getFixedStyles (column: InternalDataTableHeader, y: number): CSSProperties | undefined {
       if (!props.sticky && !column.fixed) return undefined
 
       return {
         position: 'sticky',
-        zIndex: column.fixed ? 4 : props.sticky ? 3 : undefined, // TODO: This needs to account for possible previous fixed columns.
-        left: column.fixed ? convertToUnit(column.fixedOffset) : undefined, // TODO: This needs to account for possible row/colspan of previous columns
+        left: column.fixed ? convertToUnit(column.fixedOffset) : undefined,
         top: props.sticky ? `calc(var(--v-table-header-height) * ${y})` : undefined,
       }
     }
@@ -111,7 +109,6 @@ export const VDataTableHeaders = genericComponent<VDataTableHeadersSlots>()({
       allSelected: allSelected.value,
       selectAll,
       getSortIcon,
-      getFixedStyles,
     } satisfies HeadersSlotProps))
 
     const VDataTableHeaderCell = ({ column, x, y }: { column: InternalDataTableHeader, x: number, y: number }) => {
@@ -126,6 +123,7 @@ export const VDataTableHeaders = genericComponent<VDataTableHeadersSlots>()({
             {
               'v-data-table__th--sortable': column.sortable,
               'v-data-table__th--sorted': isSorted(column),
+              'v-data-table__th--fixed': column.fixed,
             },
             loaderClasses.value,
           ]}

packages/vuetify/src/labs/VDataTable/__tests__/headers.spec.ts
@@ -1,0 +1,64 @@
+import { createHeaders } from '../composables/headers'
+
+// Utilities
+import { describe, expect, it } from '@jest/globals'
+
+describe('VDataTable headers', () => {
+  it('flattens 2d headers', () => {
+    const { headers, columns } = createHeaders({
+      headers: [
+        { key: 'foo' },
+        { key: 'bar', children: [{ key: 'fizz' }, { key: 'buzz' }] },
+        { key: 'baz' },
+      ],
+    })
+    expect(headers.value).toMatchObject([
+      [{ key: 'foo', rowspan: 2 }, { key: 'bar', colspan: 2 }, { key: 'baz', rowspan: 2 }],
+      [{ key: 'fizz' }, { key: 'buzz' }],
+    ] as any)
+    expect(columns.value).toMatchObject([{ key: 'foo' }, { key: 'fizz' }, { key: 'buzz' }, { key: 'baz' }])
+    expect('provide() can only be used inside setup()').toHaveBeenTipped()
+  })
+
+  it('orders sibling columns correctly', () => {
+    const { headers, columns } = createHeaders({
+      headers: [
+        {
+          key: 'left',
+          children: [
+            {
+              key: 'left_child',
+              children: [
+                {
+                  key: 'foo',
+                },
+                {
+                  key: 'bar',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          key: 'right',
+          children: [
+            {
+              key: 'fizz',
+            },
+            {
+              key: 'buzz',
+            },
+          ],
+        },
+      ],
+    })
+
+    expect(headers.value).toMatchObject([
+      [{ key: 'left', colspan: 2 }, { key: 'right', rowspan: 2, colspan: 2 }],
+      [{ key: 'left_child', colspan: 2 }],
+      [{ key: 'foo' }, { key: 'bar' }, { key: 'fizz' }, { key: 'buzz' }],
+    ] as any)
+    expect(columns.value).toMatchObject([{ key: 'foo' }, { key: 'bar' }, { key: 'fizz' }, { key: 'buzz' }])
+    expect('provide() can only be used inside setup()').toHaveBeenTipped()
+  })
+})

packages/vuetify/src/labs/VDataTable/types.ts
@@ -10,10 +10,7 @@ export type DataTableCompareFunction<T = any> = (a: T, b: T) => number
 export type DataTableHeader = {
   key?: 'data-table-group' | 'data-table-select' | 'data-table-expand' | (string & {})
   value?: SelectItemKey
-  title: string
-
-  colspan?: number
-  rowspan?: number
+  title?: string
 
   fixed?: boolean
   align?: 'start' | 'end' | 'center'
@@ -24,14 +21,19 @@ export type DataTableHeader = {
 
   sortable?: boolean
   sort?: DataTableCompareFunction
+
+  children?: DataTableHeader[]
 }
 
-export type InternalDataTableHeader = Omit<DataTableHeader, 'key' | 'value'> & {
+export type InternalDataTableHeader = Omit<DataTableHeader, 'key' | 'value' | 'children'> & {
   key: string | null
   value: SelectItemKey | null
   sortable: boolean
   fixedOffset?: number
   lastFixed?: boolean
+  colspan?: number
+  rowspan?: number
+  children?: InternalDataTableHeader[]
 }
 
 export interface DataTableItem<T = any> extends InternalItem<T>, GroupableItem<T>, SelectableItem {

commit f533a0e0eae0b459d69b582d06db19f16cedfe03

chore(VDataTable): correct column value logic
regression from #18512

packages/vuetify/src/labs/VDataTable/composables/headers.ts
@@ -6,7 +6,6 @@ import { consoleError, propsFactory } from '@/util'
 import type { DeepReadonly, InjectionKey, PropType, Ref } from 'vue'
 import type { SortItem } from './sort'
 import type { DataTableHeader, InternalDataTableHeader } from '../types'
-import type { SelectItemKey } from '@/util'
 
 export const makeDataTableHeaderProps = propsFactory({
   headers: {
@@ -202,10 +201,12 @@ function convertToInternalHeaders (items: DeepReadonly<DataTableHeader[]>) {
   const internalHeaders: InternalDataTableHeader[] = []
   for (const item of items) {
     const defaultItem = { ...getDefaultItem(item), ...item }
+    const key = defaultItem.key ?? (typeof defaultItem.value === 'string' ? defaultItem.value : null)
+    const value = defaultItem.value ?? key ?? null
     const internalItem: InternalDataTableHeader = {
       ...defaultItem,
-      key: defaultItem.key ?? null,
-      value: (defaultItem.key ?? defaultItem.value ?? null) as any as SelectItemKey,
+      key,
+      value,
       sortable: defaultItem.sortable ?? defaultItem.key != null,
       children: defaultItem.children ? convertToInternalHeaders(defaultItem.children) : undefined,
     }

packages/vuetify/src/util/helpers.ts
@@ -73,7 +73,7 @@ export function getObjectValueByPath (obj: any, path?: string | null, fallback?:
 export type SelectItemKey =
   | boolean | null | undefined // Ignored
   | string // Lookup by key, can use dot notation for nested objects
-  | (string | number)[] // Nested lookup by key, each array item is a key in the next level
+  | readonly (string | number)[] // Nested lookup by key, each array item is a key in the next level
   | ((item: Record<string, any>, fallback?: any) => any)
 
 export function getPropertyFromItem (