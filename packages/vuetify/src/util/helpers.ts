vuetifyjs/vuetify

commit a36dfb8c4764376ce2af0d994983238dbd96f5bf

refactor: make filterProps faster

packages/vuetify/src/util/helpers.ts
@@ -204,16 +204,32 @@ type MaybePick<
 // Array of keys
 export function pick<
   T extends object,
+  U extends Extract<keyof T, string>
+> (obj: T, paths: U[]): MaybePick<T, U> {
+  const found: any = {}
+
+  for (const path of paths) {
+    if (obj.hasOwnProperty(path)) {
+      found[path] = obj[path]
+    }
+  }
+
+  return found
+}
+
+// Array of keys
+export function pickWithRest<
+  T extends object,
   U extends Extract<keyof T, string>,
   E extends Extract<keyof T, string>
 > (obj: T, paths: U[], exclude?: E[]): [yes: MaybePick<T, Exclude<U, E>>, no: Omit<T, Exclude<U, E>>]
 // Array of keys or RegExp to test keys against
-export function pick<
+export function pickWithRest<
   T extends object,
   U extends Extract<keyof T, string>,
   E extends Extract<keyof T, string>
 > (obj: T, paths: (U | RegExp)[], exclude?: E[]): [yes: Partial<T>, no: Partial<T>]
-export function pick<
+export function pickWithRest<
   T extends object,
   U extends Extract<keyof T, string>,
   E extends Extract<keyof T, string>
@@ -343,9 +359,9 @@ export function isComposingIgnoreKey (e: KeyboardEvent): boolean {
  * attributes should be passed to the <input> element inside.
  */
 export function filterInputAttrs (attrs: Record<string, unknown>) {
-  const [events, props] = pick(attrs, [onRE])
+  const [events, props] = pickWithRest(attrs, [onRE])
   const inputEvents = omit(events, bubblingEvents)
-  const [rootAttrs, inputAttrs] = pick(props, ['class', 'style', 'id', /^data-/])
+  const [rootAttrs, inputAttrs] = pickWithRest(props, ['class', 'style', 'id', /^data-/])
   Object.assign(rootAttrs, events)
   Object.assign(inputAttrs, inputEvents)
   return [rootAttrs, inputAttrs]

packages/vuetify/src/components/VAppBar/VAppBar.tsx
@@ -133,7 +133,7 @@ export const VAppBar = genericComponent<VToolbarSlots>()({
     })
 
     useRender(() => {
-      const [toolbarProps] = VToolbar.filterProps(props)
+      const toolbarProps = VToolbar.filterProps(props)
 
       return (
         <VToolbar

packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
@@ -391,7 +391,7 @@ export const VAutocomplete = genericComponent<new <
         slots['no-data']
       )
       const isDirty = model.value.length > 0
-      const [textFieldProps] = VTextField.filterProps(props)
+      const textFieldProps = VTextField.filterProps(props)
 
       return (
         <VTextField

packages/vuetify/src/components/VBadge/VBadge.tsx
@@ -17,7 +17,7 @@ import { makeTransitionProps, MaybeTransition } from '@/composables/transition'
 
 // Utilities
 import { toRef } from 'vue'
-import { genericComponent, pick, propsFactory, useRender } from '@/util'
+import { genericComponent, pickWithRest, propsFactory, useRender } from '@/util'
 
 export type VBadgeSlots = {
   default: never
@@ -85,7 +85,7 @@ export const VBadge = genericComponent<VBadgeSlots>()({
         : value <= +props.max ? value
         : `${props.max}+`
 
-      const [badgeAttrs, attrs] = pick(ctx.attrs as Record<string, any>, [
+      const [badgeAttrs, attrs] = pickWithRest(ctx.attrs as Record<string, any>, [
         'aria-atomic',
         'aria-label',
         'aria-live',

packages/vuetify/src/components/VBottomSheet/VBottomSheet.tsx
@@ -34,7 +34,7 @@ export const VBottomSheet = genericComponent<OverlaySlots>()({
     const isActive = useProxiedModel(props, 'modelValue')
 
     useRender(() => {
-      const [dialogProps] = VDialog.filterProps(props)
+      const dialogProps = VDialog.filterProps(props)
 
       return (
         <VDialog

packages/vuetify/src/components/VBtnToggle/VBtnToggle.tsx
@@ -41,7 +41,7 @@ export const VBtnToggle = genericComponent<VBtnToggleSlots>()({
     const { isSelected, next, prev, select, selected } = useGroup(props, VBtnToggleSymbol)
 
     useRender(() => {
-      const [btnGroupProps] = VBtnGroup.filterProps(props)
+      const btnGroupProps = VBtnGroup.filterProps(props)
 
       return (
         <VBtnGroup

packages/vuetify/src/components/VCarousel/VCarousel.tsx
@@ -96,7 +96,7 @@ export const VCarousel = genericComponent<VCarouselSlots>()({
     }
 
     useRender(() => {
-      const [windowProps] = VWindow.filterProps(props)
+      const windowProps = VWindow.filterProps(props)
 
       return (
         <VWindow

packages/vuetify/src/components/VCarousel/VCarouselItem.tsx
@@ -22,8 +22,8 @@ export const VCarouselItem = genericComponent<VImgSlots>()({
 
   setup (props, { slots, attrs }) {
     useRender(() => {
-      const [imgProps] = VImg.filterProps(props)
-      const [windowItemProps] = VWindowItem.filterProps(props)
+      const imgProps = VImg.filterProps(props)
+      const windowItemProps = VWindowItem.filterProps(props)
 
       return (
         <VWindowItem

commit 048f3e8f87c8c4c26c6cec52462340b3507cca98

chore: correct keys check in pick()
regression from a36dfb8c4

packages/vuetify/src/util/helpers.ts
@@ -208,8 +208,9 @@ export function pick<
 > (obj: T, paths: U[]): MaybePick<T, U> {
   const found: any = {}
 
+  const keys = new Set(Object.keys(obj))
   for (const path of paths) {
-    if (obj.hasOwnProperty(path)) {
+    if (keys.has(path)) {
       found[path] = obj[path]
     }
   }