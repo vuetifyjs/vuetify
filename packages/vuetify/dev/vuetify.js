1211fe24c317104525ac3a58b064c03ddc644f24
John Leider <john@vuetifyjs.com>
2023-11-27 11:26:25 -0600

    chore: remove unnecessary changes

--- packages/vuetify/dev/vuetify.js
+++ packages/vuetify/dev/vuetify.js
@@ -3,17 +3,16 @@ import 'vuetify/src/styles/main.sass'
 
 import { createVuetify } from 'vuetify/src/framework'
 import * as directives from 'vuetify/src/directives'
-import { VuetifyDateAdapter } from 'vuetify/src/composables/date/adapters/vuetify'
+
+import date from './vuetify/date'
 import defaults from './vuetify/defaults'
 import icons from './vuetify/icons'
 import locale from './vuetify/locale'
 
 export default createVuetify({
-  date: {
-    adapter: VuetifyDateAdapter,
-  },
   directives,
   ssr: !!process.env.VITE_SSR,
+  date,
   defaults,
   icons,
   locale,
