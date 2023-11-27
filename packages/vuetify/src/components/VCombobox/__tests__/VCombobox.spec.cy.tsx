vuetifyjs/vuetify

commit 4a449998e987b21cd8daa18251a233b6639b188b

fix(VCombobox): remove duplicate selections
fixes #16100
closes #18515
closes #18728

packages/vuetify/src/components/VCombobox/VCombobox.tsx
@@ -342,22 +342,22 @@ export const VCombobox = genericComponent<new <
         vTextFieldRef.value?.focus()
       }
     }
-    function select (item: ListItem) {
+    function select (item: ListItem, add = true) {
       if (props.multiple) {
         const index = model.value.findIndex(selection => props.valueComparator(selection.value, item.value))
 
-        if (index === -1) {
-          model.value = [...model.value, item]
-        } else {
-          const value = [...model.value]
+        if (~index) {
+          const value = add ? [...model.value, item] : [...model.value]
           value.splice(index, 1)
           model.value = value
+        } else if (add) {
+          model.value = [...model.value, item]
         }
 
         search.value = ''
       } else {
-        model.value = [item]
-        _search.value = item.title
+        model.value = add ? [item] : []
+        _search.value = add ? item.title : ''
 
         // watch for search watcher to trigger
         nextTick(() => {
@@ -397,8 +397,7 @@ export const VCombobox = genericComponent<new <
       ) {
         select(displayItems.value[0])
       } else if (props.multiple && search.value) {
-        model.value = [...model.value, transformItem(props, search.value)]
-        search.value = ''
+        select(transformItem(props, search.value))
       }
     })
 
@@ -542,7 +541,7 @@ export const VCombobox = genericComponent<new <
                     e.stopPropagation()
                     e.preventDefault()
 
-                    select(item)
+                    select(item, false)
                   }
 
                   const slotProps = {

commit e47119244a33d02cfdde280b30c4e3a87cfe0dfe

chore(VCombobox): fix selection regression from 4a449998

packages/vuetify/src/components/VCombobox/VCombobox.tsx
@@ -342,9 +342,11 @@ export const VCombobox = genericComponent<new <
         vTextFieldRef.value?.focus()
       }
     }
-    function select (item: ListItem, add = true) {
+    /** @param set - null means toggle */
+    function select (item: ListItem, set: boolean | null = true) {
       if (props.multiple) {
         const index = model.value.findIndex(selection => props.valueComparator(selection.value, item.value))
+        const add = set == null ? !~index : set
 
         if (~index) {
           const value = add ? [...model.value, item] : [...model.value]
@@ -356,6 +358,7 @@ export const VCombobox = genericComponent<new <
 
         search.value = ''
       } else {
+        const add = set !== false
         model.value = add ? [item] : []
         _search.value = add ? item.title : ''
 
@@ -494,7 +497,7 @@ export const VCombobox = genericComponent<new <
                             ref: itemRef,
                             key: index,
                             active: (highlightFirst.value && index === 0) ? true : undefined,
-                            onClick: () => select(item),
+                            onClick: () => select(item, null),
                           })
 
                           return slots.item?.({

packages/vuetify/src/components/VCombobox/__tests__/VCombobox.spec.cy.tsx
@@ -616,6 +616,23 @@ describe('VCombobox', () => {
       .should('have.length', 6)
   })
 
+  it(`doesn't add duplicate values`, () => {
+    cy
+      .mount(() => (
+        <VCombobox multiple />
+      ))
+      .get('.v-combobox input')
+      .click()
+      .type('foo{enter}')
+      .type('bar{enter}')
+      .get('.v-combobox__selection')
+      .should('have.length', 2)
+      .get('.v-combobox input')
+      .type('foo{enter}')
+      .get('.v-combobox__selection')
+      .should('have.length', 2)
+  })
+
   describe('Showcase', () => {
     generate({ stories })
   })