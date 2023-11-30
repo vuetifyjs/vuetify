818385033c5ca96b8785abfd11ac736b980f711d
vcastro <victor.castro.cintas@gmail.com>
2023-11-30 16:19:57 +0100

    feat(rules): Add aliases/overwrite system

--- packages/vuetify/src/labs/rules/rules.ts
+++ packages/vuetify/src/labs/rules/rules.ts
@@ -7,90 +7,85 @@ import type { InjectionKey } from 'vue'
 import type { LocaleInstance } from '@/composables'
 import type { ValidationRule } from '@/composables/validation'
 
+export type ValidationRuleBuilderWithoutOptions = (err?: string) => ValidationRule
 export type ValidationRuleBuilderWithOptions<T> = (options: T, err?: string) => ValidationRule
-export type ValidationRuleBuilder = (err?: string) => ValidationRule
+export type ValidationRuleBuilder =
+  | ValidationRuleBuilderWithoutOptions
+  | ValidationRuleBuilderWithOptions<any>
 
-export interface RulesInstance {
-  required: ValidationRuleBuilder
-  email: ValidationRuleBuilder
-  number: ValidationRuleBuilder
-  integer: ValidationRuleBuilder
-  capital: ValidationRuleBuilder
+export interface RuleAliases {
+  [name: string]: ValidationRuleBuilder
+  required: ValidationRuleBuilderWithoutOptions
+  email: ValidationRuleBuilderWithoutOptions
+  number: ValidationRuleBuilderWithoutOptions
+  integer: ValidationRuleBuilderWithoutOptions
+  capital: ValidationRuleBuilderWithoutOptions
   maxLength: ValidationRuleBuilderWithOptions<number>
   minLength: ValidationRuleBuilderWithOptions<number>
   strictLength: ValidationRuleBuilderWithOptions<number>
   exclude: ValidationRuleBuilderWithOptions<string[]>
-  notEmpty: ValidationRuleBuilder
+  notEmpty: ValidationRuleBuilderWithoutOptions
   pattern: ValidationRuleBuilderWithOptions<RegExp>
 }
 
-export type InternalRulesOptions = {
-  locale: LocaleInstance
-}
-
-export type RulesOptions = Partial<InternalRulesOptions>
-
-const parseRulesOptions = (options?: RulesOptions) => {
-  return mergeDeep({
-  }, options)
+export type RulesOptions = {
+  aliases?: Partial<RuleAliases>
 }
 
 export function createRules (options: RulesOptions | undefined, locale: LocaleInstance) {
-  // @ts-expect-error
-  const _options = parseRulesOptions(options)
   const { t } = locale
 
-  const state: RulesInstance = {
-    required: (err?: string) => {
-      return (v: any) => !!v || t(err || '$vuetify.rules.required')
-    },
-    email: (err?: string) => {
-      return (v: any) => (!v || (typeof v === 'string' && /^.+@\S+\.\S+$/.test(v))) || t(err || '$vuetify.rules.email')
-    },
-    number: (err?: string) => {
-      return (v: string) => !!+v || t(err || '$vuetify.rules.number')
-    },
-    integer: (err?: string) => {
-      return (v: string) => (/^[\d]*$/.test(v)) || t(err || '$vuetify.rules.integer')
-    },
-    capital: (err?: string) => {
-      return (v: string) => (/^[A-Z]*$/.test(v)) || t(err || '$vuetify.rules.capital')
-    },
-    maxLength: (len: number, err?: string) => {
-      return (v: any) => (!v || v.length <= len) || t(err || '$vuetify.rules.maxLength', [len])
-    },
-    minLength: (len: number, err?: string) => {
-      return (v: any) => (!v || v.length >= len) || t(err || '$vuetify.rules.minLength', [len])
-    },
-    strictLength: (len: number, err?: string) => {
-      return (v: any) => (!v || v.length === len) || t(err || '$vuetify.rules.strictLength', [len])
-    },
-    exclude: (forbiddenCharacters: string[], err?: string) => {
-      return (v: string) => {
-        let error: string | true = true
-        for (const character of forbiddenCharacters) {
-          if (v.includes(character)) error = err || t('$vuetify.rules.exclude', character)
+  return mergeDeep({
+    aliases: {
+      required: (err?: string) => {
+        return (v: any) => !!v || t(err || '$vuetify.rules.required')
+      },
+      email: (err?: string) => {
+        return (v: any) => (!v || (typeof v === 'string' && /^.+@\S+\.\S+$/.test(v))) || t(err || '$vuetify.rules.email')
+      },
+      number: (err?: string) => {
+        return (v: string) => !!+v || t(err || '$vuetify.rules.number')
+      },
+      integer: (err?: string) => {
+        return (v: string) => (/^[\d]*$/.test(v)) || t(err || '$vuetify.rules.integer')
+      },
+      capital: (err?: string) => {
+        return (v: string) => (/^[A-Z]*$/.test(v)) || t(err || '$vuetify.rules.capital')
+      },
+      maxLength: (len: number, err?: string) => {
+        return (v: any) => (!v || v.length <= len) || t(err || '$vuetify.rules.maxLength', [len])
+      },
+      minLength: (len: number, err?: string) => {
+        return (v: any) => (!v || v.length >= len) || t(err || '$vuetify.rules.minLength', [len])
+      },
+      strictLength: (len: number, err?: string) => {
+        return (v: any) => (!v || v.length === len) || t(err || '$vuetify.rules.strictLength', [len])
+      },
+      exclude: (forbiddenCharacters: string[], err?: string) => {
+        return (v: string) => {
+          let error: string | true = true
+          for (const character of forbiddenCharacters) {
+            if (v.includes(character)) error = err || t('$vuetify.rules.exclude', character)
+          }
+          return error
         }
-        return error
-      }
+      },
+      notEmpty: (err?: string) => {
+        return (v: any) => (v && v.length > 0) || t(err || '$vuetify.rules.notEmpty')
+      },
+      pattern: (pattern: RegExp, err?: string) => {
+        return (v: any) => (!v || pattern.test(v) || t(err || '$vuetify.rules.pattern'))
+      },
     },
-    notEmpty: (err?: string) => {
-      return (v: any) => (v && v.length > 0) || t(err || '$vuetify.rules.notEmpty')
-    },
-    pattern: (pattern: RegExp, err?: string) => {
-      return (v: any) => (!v || pattern.test(v) || t(err || '$vuetify.rules.pattern'))
-    },
-  }
-
-  return state
+  }, options)
 }
 
-export const RulesSymbol: InjectionKey<RulesInstance> = Symbol.for('vuetify:rules')
+export const RulesSymbol: InjectionKey<RulesOptions> = Symbol.for('vuetify:rules')
 
 export function useRules () {
-  const rules = inject(RulesSymbol)
+  const ruleOptions = inject(RulesSymbol)
 
-  if (!rules) throw new Error('Could not find Vuetify rules injection')
+  if (!ruleOptions) throw new Error('Could not find Vuetify rules injection')
 
-  return rules
+  return ruleOptions.aliases
 }
