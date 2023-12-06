266f721e8a6bf5324e3bb73069eda73e9bedaff8
John Leider <john@vuetifyjs.com>
2023-12-05 20:42:45 -0600

    docs(Releases): fix lint

--- packages/docs/src/components/doc/Releases.vue
+++ packages/docs/src/components/doc/Releases.vue
@@ -292,28 +292,28 @@
     timeout = setTimeout(() => store.find(val), 500)
   }
 
-  function timeAgo (string: string): string {
-    const date = adapter.toJsDate(adapter.date(string))
-    const now = new Date()
-    const seconds = Math.floor((now.getTime() - date.getTime()) / 1000)
+  // function timeAgo (string: string): string {
+  //   const date = adapter.toJsDate(adapter.date(string))
+  //   const now = new Date()
+  //   const seconds = Math.floor((now.getTime() - date.getTime()) / 1000)
 
-    let interval = seconds / 31536000
-    if (interval > 1) return `${Math.floor(interval)} years ago`
+  //   let interval = seconds / 31536000
+  //   if (interval > 1) return `${Math.floor(interval)} years ago`
 
-    interval = seconds / 2592000
-    if (interval > 1) return `${Math.floor(interval)} months ago`
+  //   interval = seconds / 2592000
+  //   if (interval > 1) return `${Math.floor(interval)} months ago`
 
-    interval = seconds / 86400
-    if (interval > 1) return `${Math.floor(interval)} days ago`
+  //   interval = seconds / 86400
+  //   if (interval > 1) return `${Math.floor(interval)} days ago`
 
-    interval = seconds / 3600
-    if (interval > 1) return `${Math.floor(interval)} hours ago`
+  //   interval = seconds / 3600
+  //   if (interval > 1) return `${Math.floor(interval)} hours ago`
 
-    interval = seconds / 60
-    if (interval > 1) return `${Math.floor(interval)} minutes ago`
+  //   interval = seconds / 60
+  //   if (interval > 1) return `${Math.floor(interval)} minutes ago`
 
-    return `${Math.floor(seconds)} seconds ago`
-  }
+  //   return `${Math.floor(seconds)} seconds ago`
+  // }
 </script>
 
 <style lang="sass">
