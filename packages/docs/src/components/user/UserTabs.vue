fa7d17ea0a35ed0bf75d5132af6ea82d7c2fc881
John Leider <9064066+johnleider@users.noreply.github.com>
2023-12-20 13:44:08 -0600

    docs: add stripe subscriptions (#18882)

--- packages/docs/src/components/user/UserTabs.vue
+++ packages/docs/src/components/user/UserTabs.vue
@@ -1,36 +1,47 @@
 <template>
-  <v-defaults-provider
-    :defaults="{
-      VTab: {
-        class: 'text-none text-body-2 font-weight-regular',
-        color: 'medium-emphasis'
-      }
-    }"
-  >
-    <v-tabs color="primary">
-      <v-tab
-        v-for="(tab, i) in tabs"
-        :key="i"
-        v-bind="tab"
-      />
-    </v-tabs>
-  </v-defaults-provider>
+  <v-tabs color="primary">
+    <v-tab
+      v-for="(tab, i) in tabs"
+      :key="i"
+      class="text-none text-body-2 font-weight-regular"
+      v-bind="tab"
+    />
+  </v-tabs>
 </template>
 
 <script setup>
+  // Composables
+  import { useAuthStore, useUserStore } from '@vuetify/one'
+
   // Utilities
+  import { computed } from 'vue'
   import { rpath } from '@/util/routes'
 
-  const tabs = [
-    {
-      prependIcon: 'mdi-view-dashboard-outline',
-      text: 'Dashboard',
-      to: rpath('/user/dashboard/'),
-    },
-    {
-      prependIcon: 'mdi-cog-outline',
-      text: 'Options',
-      to: rpath('/user/options/'),
-    },
-  ]
+  const auth = useAuthStore()
+  const user = useUserStore()
+
+  const tabs = computed(() => {
+    const array = [
+      {
+        prependIcon: 'mdi-view-dashboard-outline',
+        text: 'Dashboard',
+        to: rpath('/user/dashboard/'),
+      },
+      {
+        prependIcon: 'mdi-cog-outline',
+        text: 'Options',
+        to: rpath('/user/options/'),
+      },
+    ]
+
+    if (auth.user && user.dev) {
+      array.push({
+        prependIcon: '$vuetify',
+        text: 'Subscriptions',
+        to: rpath('/user/subscriptions/'),
+      })
+    }
+
+    return array
+  })
 </script>
---
+++ packages/docs/src/pages/en/user/subscriptions.md
@@ -0,0 +1,18 @@
+---
+layout: user
+meta:
+  nav: Dashboard
+  title: Subscriptions
+  description: Subscriptions
+  keywords: Subscriptions
+---
+
+<script setup>
+import OneSubscription from '@/components/user/account/OneSubscription.vue'
+</script>
+
+### Vuetify One Subscription
+
+Support Vuetify and gain access to exclusive documentation perks and features for only $1 per month
+
+<OneSubscription />
