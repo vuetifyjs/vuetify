61456f9c0dd7ace74bdd61fa6d2e9c4a7b14e055
Viktor Denisov <websitevirtuoso@gmail.com>
2023-12-19 20:13:41 -0800

    fetchPriority property

--- packages/vuetify/src/components/VImg/VImg.tsx
+++ packages/vuetify/src/components/VImg/VImg.tsx
@@ -78,6 +78,7 @@ export const makeVImgProps = propsFactory({
     default: '',
   },
   crossorigin: String as PropType<'' | 'anonymous' | 'use-credentials'>,
+  fetchpriority: String as PropType<'' | 'high' | 'low'>,
   referrerpolicy: String as PropType<
     | 'no-referrer'
     | 'no-referrer-when-downgrade'
@@ -243,6 +244,7 @@ export const VImg = genericComponent<VImgSlots>()({
           srcset={ normalisedSrc.value.srcset }
           alt={ props.alt }
           crossorigin={ props.crossorigin }
+          fetchpriority={ props.fetchpriority }
           referrerpolicy={ props.referrerpolicy }
           draggable={ props.draggable }
           sizes={ props.sizes }
