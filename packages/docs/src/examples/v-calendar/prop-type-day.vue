08bb49792ab1849af9aac1dbb94f5034e051be61
John Leider <john@vuetifyjs.com>
2023-12-27 10:29:53 -0600

    docs(VCalendar): update example, remove slots

--- packages/docs/src/examples/v-calendar/prop-type-day.vue
+++ packages/docs/src/examples/v-calendar/prop-type-day.vue
@@ -2,19 +2,7 @@
   <v-row>
     <v-col>
       <v-sheet>
-        <v-calendar color="primary" type="day">
-          <template v-slot:day-header="{ present }">
-            <div v-if="present" class="text-center">
-              Today
-            </div>
-          </template>
-
-          <template v-slot:interval="{ hour }">
-            <div class="text-center">
-              {{ hour }} o'clock
-            </div>
-          </template>
-        </v-calendar>
+        <v-calendar color="primary"></v-calendar>
       </v-sheet>
     </v-col>
   </v-row>
