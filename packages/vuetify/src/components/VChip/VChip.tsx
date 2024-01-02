vuetifyjs/vuetify

commit 50e48d576401deb3b0a7f8720f4d1ad52450d07a

fix(VChip): use button element for closable button (#18571)
closes #18547

packages/vuetify/src/components/VChip/VChip.tsx
@@ -328,7 +328,7 @@ export const VChip = genericComponent<VChipSlots>()({
           )}
 
           { hasClose && (
-            <div
+            <button
               key="close"
               class="v-chip__close"
               { ...closeProps.value }
@@ -351,7 +351,7 @@ export const VChip = genericComponent<VChipSlots>()({
                   v-slots:default={ slots.close }
                 />
               )}
-            </div>
+            </button>
           )}
         </Tag>
       )

commit c287a29800999fd07e777852c057d18c5eeb6fdc

fix(VChip): prevent close button from submitting forms
regression from #18571

packages/vuetify/src/components/VChip/VChip.tsx
@@ -331,6 +331,7 @@ export const VChip = genericComponent<VChipSlots>()({
             <button
               key="close"
               class="v-chip__close"
+              type="button"
               { ...closeProps.value }
             >
               { !slots.close ? (