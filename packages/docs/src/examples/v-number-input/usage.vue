20782fdda3a1904e0a7312d42821b7ad956adb6a
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2024-01-06 21:31:17 +1100

    Tweaking usage example

--- packages/docs/src/examples/v-number-input/usage.vue
+++ packages/docs/src/examples/v-number-input/usage.vue
@@ -10,9 +10,15 @@
     </div>
 
     <template v-slot:configuration>
+      <v-select
+        v-model="controlVariant"
+        label="Control Variant"
+        :items="controlVariantOptions"
+      ></v-select>
+      <v-checkbox v-model="controlReverse" label="ControlReverse"></v-checkbox>
+      <v-checkbox v-model="inset" label="Inset"></v-checkbox>
+      <v-checkbox v-model="hideInput" label="HideInput"></v-checkbox>
       <v-text-field v-model="label" label="Label" clearable></v-text-field>
-      <v-checkbox v-model="disabled" label="Disabled"></v-checkbox>
-      <v-checkbox v-model="loading" label="Loading"></v-checkbox>
     </template>
   </usage-example>
 </template>
@@ -25,15 +31,24 @@
   const name = ref('v-number-input')
   const model = ref('outlined')
   const options = ['outlined', 'filled', 'solo', 'solo-inverted', 'solo-filled']
+  const controlVariantOptions = ['default', 'stacked', 'split']
+  const controlReverse = ref(false)
+  const controlVariant = ref('default')
   const disabled = ref(false)
   const loading = ref(false)
+  const inset = ref(false)
+  const hideInput = ref(false)
   const label = ref('')
 
   const props = computed(() => {
     return {
+      controlReverse: controlReverse.value,
+      controlVariant: controlVariant.value,
       disabled: disabled.value || undefined,
       label: label.value,
       loading: loading.value || undefined,
+      hideInput: hideInput.value,
+      inset: inset.value,
       variant: model.value !== 'default' ? model.value : undefined,
     }
   })
