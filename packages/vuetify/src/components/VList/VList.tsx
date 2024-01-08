412256b71f7f75788cefe1aba3b3516f66ed2be9
Zhaolin Liang <develop0304@outlook.com>
2024-01-09 00:57:00 +0800

    fix(VList): unexpected page scrolling when focusing (#18940)
    fixes #18223

--- packages/vuetify/src/components/VList/VList.tsx
+++ packages/vuetify/src/components/VList/VList.tsx
@@ -216,6 +216,10 @@ export const VList = genericComponent<new <
       e.preventDefault()
     }
 
+    function onMousedown (e: MouseEvent) {
+      isFocused.value = true
+    }
+
     function focus (location?: 'next' | 'prev' | 'first' | 'last') {
       if (contentRef.value) {
         return focusChild(contentRef.value, location)
@@ -254,6 +258,7 @@ export const VList = genericComponent<new <
           onFocusout={ onFocusout }
           onFocus={ onFocus }
           onKeydown={ onKeydown }
+          onMousedown={ onMousedown }
         >
           <VListChildren
             items={ items.value }
