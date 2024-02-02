3a3f949280d9fc8b48e4724673b4ab797a12cd07
John Leider <john@vuetifyjs.com>
2024-02-02 14:58:45 -0600

    fix(VListItemContent): add component helper
    this is to ease the pain of upgrading

---
+++ packages/vuetify/src/components/VList/VListItemContent.ts
@@ -0,0 +1,6 @@
+// Utilities
+import { createSimpleFunctional } from '@/util'
+
+export const VListItemContent = createSimpleFunctional('v-list-item-content')
+
+export type VListItemContent = InstanceType<typeof VListItemContent>
