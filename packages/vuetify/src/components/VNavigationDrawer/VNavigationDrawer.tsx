0296e5a0614e6c5e0a0f69e9d798176e42bb8d43
Albert Kaaman <albert@kaaman.nu>
2024-02-14 09:16:00 +0100

    fix(layout): use suspense to delay render of layout items (#15229)
    Co-authored-by: John Leider <john.j.leider@gmail.com>
    Co-authored-by: John Leider <john@vuetifyjs.com>
    Co-authored-by: Kael <kaelwd@gmail.com>

--- packages/vuetify/src/components/VNavigationDrawer/VNavigationDrawer.tsx
+++ packages/vuetify/src/components/VNavigationDrawer/VNavigationDrawer.tsx
@@ -26,7 +26,7 @@ import { makeThemeProps, provideTheme } from '@/composables/theme'
 import { useToggleScope } from '@/composables/toggleScope'
 
 // Utilities
-import { computed, nextTick, onBeforeMount, ref, shallowRef, toRef, Transition, watch } from 'vue'
+import { computed, nextTick, ref, shallowRef, toRef, Transition, watch } from 'vue'
 import { genericComponent, propsFactory, toPhysical, useRender } from '@/util'
 
 // Types
@@ -149,11 +149,9 @@ export const VNavigationDrawer = genericComponent<VNavigationDrawerSlots>()({
       if (val) isActive.value = true
     })
 
-    onBeforeMount(() => {
-      if (props.modelValue != null || isTemporary.value) return
-
+    if (props.modelValue == null && !isTemporary.value) {
       isActive.value = props.permanent || !mobile.value
-    })
+    }
 
     const { isDragging, dragProgress, dragStyles } = useTouch({
       isActive,
@@ -170,8 +168,7 @@ export const VNavigationDrawer = genericComponent<VNavigationDrawerSlots>()({
 
       return isDragging.value ? size * dragProgress.value : size
     })
-
-    const { layoutItemStyles, layoutItemScrimStyles } = useLayoutItem({
+    const { layoutItemStyles, layoutItemScrimStyles, layoutIsReady } = useLayoutItem({
       id: props.name,
       order: computed(() => parseInt(props.order, 10)),
       position: location,
@@ -310,9 +307,7 @@ export const VNavigationDrawer = genericComponent<VNavigationDrawerSlots>()({
       )
     })
 
-    return {
-      isStuck,
-    }
+    return layoutIsReady.then(() => ({ isStuck }))
   },
 })
 
