17428c2a21741894572c89c050731d50916a87e7
Yuchao <yuchao.sydneyuni@gmail.com>
2024-02-22 07:26:54 +1100

    fix(VMenu): do not call closeParents when still inside parent (#19239)
    fixes #17004
    fixes #19138

--- packages/vuetify/src/util/helpers.ts
+++ packages/vuetify/src/util/helpers.ts
@@ -721,4 +721,15 @@ export function eagerComputed<T> (fn: () => T, options?: WatchOptions): Readonly
   })
 
   return readonly(result)
+export function isClickInsideElement (event: MouseEvent, targetDiv: HTMLElement) {
+  const mouseX = event.clientX
+  const mouseY = event.clientY
+
+  const divRect = targetDiv.getBoundingClientRect()
+  const divLeft = divRect.left
+  const divTop = divRect.top
+  const divRight = divRect.right
+  const divBottom = divRect.bottom
+
+  return mouseX >= divLeft && mouseX <= divRight && mouseY >= divTop && mouseY <= divBottom
 }
