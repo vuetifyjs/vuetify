3afc91e844f5287366422787d0797e8cec78224d
John Leider <john@vuetifyjs.com>
2024-02-26 14:41:15 -0600

    refactor(VFab): design tweaks and changes

--- packages/vuetify/src/labs/VFab/VFab.tsx
+++ packages/vuetify/src/labs/VFab/VFab.tsx
@@ -2,16 +2,18 @@
 import './VFab.sass'
 
 // Components
+import { VFabTransition } from '@/components/transitions'
 import { makeVBtnProps, VBtn } from '@/components/VBtn/VBtn'
 
 // Composables
 import { makeLayoutItemProps, useLayoutItem } from '@/composables/layout'
 import { useProxiedModel } from '@/composables/proxiedModel'
 import { useResizeObserver } from '@/composables/resizeObserver'
+import { useToggleScope } from '@/composables/toggleScope'
 
 // Utilities
 import { computed, ref, shallowRef, toRef } from 'vue'
-import { genericComponent, propsFactory, useRender, omit } from '@/util'
+import { genericComponent, omit, propsFactory, useRender } from '@/util'
 
 // Types
 import type { ComputedRef, PropType } from 'vue'
@@ -25,14 +27,14 @@ export const makeVFabProps = propsFactory({
   location: {
     type: String as PropType<typeof locations[number]>,
     default: 'bottom end',
-    validator: (value: any) => locations.includes(value),
   },
+  offset: Boolean,
   modelValue: {
     type: Boolean,
     default: true,
   },
 
-  ...omit(makeVBtnProps(), ['location']),
+  ...omit(makeVBtnProps({ size: 56 }), ['location']),
   ...makeLayoutItemProps(),
 }, 'VFab')
 
@@ -48,28 +50,39 @@ export const VFab = genericComponent()({
   setup (props, { slots }) {
     const isActive = useProxiedModel(props, 'modelValue')
     const height = shallowRef(56)
+    const layoutItemStyles = ref()
+
     const { resizeRef } = useResizeObserver(entries => {
       if (!entries.length) return
       height.value = entries[0].target.clientHeight
     })
+
+    const hasPosition = computed(() => props.app || props.absolute)
+
     const position = computed(() => {
-      if (!props.app || !props.location) return false
+      if (!hasPosition.value) return false
 
       return props.location.split(' ').shift()
     }) as ComputedRef<Position>
+
     const orientation = computed(() => {
-      if (!props.app || !props.location) return false
+      if (!hasPosition.value) return false
 
       return props.location.split(' ')[1] ?? 'end'
     })
-    const { layoutItemStyles } = useLayoutItem({
-      id: props.name,
-      order: computed(() => parseInt(props.order, 10)),
-      position,
-      layoutSize: height,
-      elementSize: computed(() => height.value + 8),
-      active: computed(() => !!props.app && isActive.value),
-      absolute: toRef(props, 'absolute'),
+
+    useToggleScope(() => props.app, () => {
+      const layout = useLayoutItem({
+        id: props.name,
+        order: computed(() => parseInt(props.order, 10)),
+        position,
+        layoutSize: height,
+        elementSize: computed(() => height.value + 8),
+        active: computed(() => props.app && isActive.value),
+        absolute: toRef(props, 'absolute'),
+      })
+
+      layoutItemStyles.value = layout.layoutItemStyles
     })
 
     const vFabRef = ref()
@@ -83,9 +96,12 @@ export const VFab = genericComponent()({
           class={[
             'v-fab',
             {
+              'v-fab--absolute': props.absolute,
               'v-fab--app': !!props.app,
               'v-fab--extended': props.extended,
-              [`v-fab--${orientation.value}`]: !!props.app,
+              'v-fab--offset': props.offset,
+              [`v-fab--${position.value}`]: hasPosition.value,
+              [`v-fab--${orientation.value}`]: hasPosition.value,
             },
             props.class,
           ]}
@@ -93,18 +109,23 @@ export const VFab = genericComponent()({
             props.app ? {
               ...layoutItemStyles.value,
             } : {
-              height: undefined,
+              height: 'inherit',
               width: undefined,
             },
             props.style,
           ]}
         >
-          <VBtn
-            ref={ resizeRef }
-            { ...btnProps }
-            location={ undefined }
-            v-slots={ slots }
-          />
+          <div class="v-fab__container">
+            <VFabTransition>
+              <VBtn
+                v-show={ isActive.value }
+                ref={ resizeRef }
+                { ...btnProps }
+                location={ undefined }
+                v-slots={ slots }
+              />
+            </VFabTransition>
+          </div>
         </div>
       )
     })
