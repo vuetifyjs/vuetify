acccbb9d3e9481322522ba0a986809d615f3d23f
John Leider <john@vuetifyjs.com>
2024-02-29 14:12:54 -0600

    feat(VSpeedDial): port to v3

---
+++ packages/vuetify/src/labs/VSpeedDial/VSpeedDial.tsx
@@ -0,0 +1,57 @@
+// Components
+import { VDefaultsProvider } from '@/components/VDefaultsProvider'
+import { makeVMenuProps, VMenu } from '@/components/VMenu/VMenu'
+
+// Composables
+import { makeComponentProps } from '@/composables/component'
+import { makeTransitionProps, MaybeTransition } from '@/composables/transition'
+
+// Utilities
+import { genericComponent, propsFactory, useRender } from '@/util'
+
+export const makeVSpeedDialProps = propsFactory({
+  ...makeComponentProps(),
+  ...makeTransitionProps({ transition: 'fade-transition' }),
+  ...makeVMenuProps(),
+}, 'VSpeedDial')
+
+export const VSpeedDial = genericComponent()({
+  name: 'VSpeedDial',
+
+  props: makeVSpeedDialProps(),
+
+  setup (props, { slots }) {
+    useRender(() => {
+      const menuProps = VMenu.filterProps(props)
+
+      return (
+        <VMenu
+          { ...menuProps }
+          class={ props.class }
+          style={ props.style }
+        >
+          <VDefaultsProvider
+            defaults={{
+              VBtn: {
+                size: 'x-small',
+              },
+            }}
+          >
+            <MaybeTransition
+              appear
+              group
+              tag="div"
+              transition={ props.transition }
+            >
+              { slots.default?.() }
+            </MaybeTransition>
+          </VDefaultsProvider>
+        </VMenu>
+      )
+    })
+
+    return {}
+  },
+})
+
+export type VSpeedDial = InstanceType<typeof VSpeedDial>
