d7757ca422884f1fa8e00447144681516249f754
Kael <kaelwd@gmail.com>
2024-03-05 14:11:17 +1100

    add keys

--- packages/docs/src/examples/v-speed-dial/usage.vue
+++ packages/docs/src/examples/v-speed-dial/usage.vue
@@ -12,10 +12,10 @@
         <v-speed-dial
           v-bind="props"
         >
-          <v-btn icon="$success"></v-btn>
-          <v-btn icon="$info"></v-btn>
-          <v-btn icon="$warning"></v-btn>
-          <v-btn icon="$error"></v-btn>
+          <v-btn key="1" icon="$success"></v-btn>
+          <v-btn key="2" icon="$info"></v-btn>
+          <v-btn key="3" icon="$warning"></v-btn>
+          <v-btn key="4" icon="$error"></v-btn>
         </v-speed-dial>
       </v-fab>
     </v-sheet>
@@ -57,10 +57,10 @@
     ></v-fab>
   </template>
 
-  <v-btn icon="$success"></v-btn>
-  <v-btn icon="$info"></v-btn>
-  <v-btn icon="$warning"></v-btn>
-  <v-btn icon="$error"></v-btn>
+  <v-btn key="1" icon="$success"></v-btn>
+  <v-btn key="2" icon="$info"></v-btn>
+  <v-btn key="3" icon="$warning"></v-btn>
+  <v-btn key="4" icon="$error"></v-btn>
 `
   })
 
