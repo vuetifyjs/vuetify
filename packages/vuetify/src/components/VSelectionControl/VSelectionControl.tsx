00a6ba22af1484ee6fb2815c6c2f7f0954cc5b30
Count-Monte <edmund.mcgardner@gmail.com>
2024-03-14 15:47:07 -0500

    fix(VDataTable): checkboxes do not have labels

--- packages/vuetify/src/components/VSelectionControl/VSelectionControl.tsx
+++ packages/vuetify/src/components/VSelectionControl/VSelectionControl.tsx
@@ -231,6 +231,7 @@ export const VSelectionControl = genericComponent<new <T>(
           onFocus={ onFocus }
           onInput={ onInput }
           aria-disabled={ !!props.disabled }
+          aria-label={ label || '' }
           type={ props.type }
           value={ trueValue.value }
           name={ props.name }
