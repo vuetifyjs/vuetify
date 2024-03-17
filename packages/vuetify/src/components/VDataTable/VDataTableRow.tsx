8ceee42654907d69cb5154f6f262871a82f656dd
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-03-17 14:21:02 -0700

    move mobile view to be more streamline and reduce duplication

--- packages/vuetify/src/components/VDataTable/VDataTableRow.tsx
+++ packages/vuetify/src/components/VDataTable/VDataTableRow.tsx
@@ -46,7 +46,7 @@ export const VDataTableRow = genericComponent<new <T>(
   setup (props, { slots }) {
     const { isSelected, toggleSelect } = useSelection()
     const { isExpanded, toggleExpand } = useExpanded()
-    const { columns, headers } = useHeaders()
+    const { columns } = useHeaders()
 
     useRender(() => (
       <tr
@@ -54,7 +54,7 @@ export const VDataTableRow = genericComponent<new <T>(
           'v-data-table__tr',
           {
             'v-data-table__tr--clickable': !!(props.onClick || props.onContextmenu || props.onDblclick),
-            'v-data-table__tr--mobile': props.mobileView,
+            'v-data-table__mobile-tr': props.mobileView,
           },
         ]}
         onClick={ props.onClick as any }
@@ -62,105 +62,10 @@ export const VDataTableRow = genericComponent<new <T>(
         onDblclick={ props.onDblclick as any }
       >
         {
-          props.item && props.mobileView && headers.value.map((column, i) => {
+          props.item && columns.value.map((column, i) => {
             const item = props.item!
             const slotName = `item.${column.key}` as const
-            const slotProps = {
-              index: props.index!,
-              item: item.raw,
-              internalItem: item,
-              value: getObjectValueByPath(item.columns, column.key),
-              column,
-              isSelected,
-              toggleSelect,
-              isExpanded,
-              toggleExpand,
-            } satisfies ItemKeySlot<any>
-
-            const cellProps = typeof props.cellProps === 'function'
-              ? props.cellProps({
-                index: slotProps.index,
-                item: slotProps.item,
-                internalItem: slotProps.internalItem,
-                value: slotProps.value,
-                column,
-              })
-              : props.cellProps
-            const columnCellProps = typeof column.cellProps === 'function'
-              ? column.cellProps({
-                index: slotProps.index,
-                item: slotProps.item,
-                internalItem: slotProps.internalItem,
-                value: slotProps.value,
-              })
-              : column.cellProps
-
-            const cell = []
-
-            column.forEach((col, v) => {
-              const columnKey = col.key
-              const isSelectOrExpand = columnKey === 'data-table-select' || columnKey === 'data-table-expand'
-
-              cell.push(
-                <VDataTableColumn
-                  class={[
-                    'v-data-table__td-mobile',
-                    {
-                      'v-data-table__td-mobile-select-row': columnKey === 'data-table-select',
-                      'v-data-table__td-mobile-expanded-row': columnKey === 'data-table-expand',
-                      'v-data-table__td-mobile-expanded-row-expanded is-expanded': isExpanded(item) && columnKey === 'data-table-expand',
-                    }]}
-                  align={ column.align }
-                  noPadding={ isSelectOrExpand }
-                  width={ column.width }
-                  { ...cellProps }
-                  { ...columnCellProps }
-                >
-                  {{
-                    default: () => {
-                      if (slots[slotName]) return slots[slotName]!(slotProps)
-
-                      if (columnKey === 'data-table-select') {
-                        return slots['item.data-table-select']?.(slotProps) ?? (
-                        <VCheckboxBtn
-                          disabled={ !item.selectable }
-                          modelValue={ isSelected([item]) }
-                          onClick={ withModifiers(() => toggleSelect(item), ['stop']) }
-                        />
-                        )
-                      }
-
-                      if (columnKey === 'data-table-expand') {
-                        return slots['item.data-table-expand']?.(slotProps) ?? (
-                        <VBtn
-                          icon={ isExpanded(item) ? '$collapse' : '$expand' }
-                          size="small"
-                          variant="text"
-                          onClick={ withModifiers(() => toggleExpand(item), ['stop']) }
-                        />
-                        )
-                      }
-
-                      return (
-                        <>
-                          <div class="v-data-table__td-mobile-key">{ toDisplayString(col.title) }</div>
-                          <div class="v-data-table__td-mobile-value">{ toDisplayString(item.columns[columnKey]) }</div>
-                        </>
-                      )
-                    },
-                  }}
-                </VDataTableColumn>
-              )
-            })
-
-            return cell
-          })
-        }
-
-        {
-          props.item && !props.mobileView && columns.value.map((column, i) => {
-            const item = props.item!
-            const slotName = `item.${column.key}` as const
+            const columnKey = column.key
             const slotProps = {
               index: props.index!,
               item: item.raw,
@@ -194,10 +99,17 @@ export const VDataTableRow = genericComponent<new <T>(
             return (
               <VDataTableColumn
                 align={ column.align }
+                class={
+                  {
+                    'v-data-table__mobile-td': props.mobileView,
+                    'v-data-table__mobile-td-select-row': props.mobileView && columnKey === 'data-table-select',
+                    'v-data-table__mobile-td-expanded-row': props.mobileView && columnKey === 'data-table-expand',
+                    'is-expanded': isExpanded(item) && props.mobileView && columnKey === 'data-table-expand',
+                  }}
                 fixed={ column.fixed }
                 fixedOffset={ column.fixedOffset }
                 lastFixed={ column.lastFixed }
-                noPadding={ column.key === 'data-table-select' || column.key === 'data-table-expand' }
+                noPadding={ columnKey === 'data-table-select' || columnKey === 'data-table-expand' }
                 width={ column.width }
                 { ...cellProps }
                 { ...columnCellProps }
@@ -206,7 +118,7 @@ export const VDataTableRow = genericComponent<new <T>(
                   default: () => {
                     if (slots[slotName]) return slots[slotName]!(slotProps)
 
-                    if (column.key === 'data-table-select') {
+                    if (columnKey === 'data-table-select') {
                       return slots['item.data-table-select']?.(slotProps) ?? (
                         <VCheckboxBtn
                           disabled={ !item.selectable }
@@ -216,7 +128,7 @@ export const VDataTableRow = genericComponent<new <T>(
                       )
                     }
 
-                    if (column.key === 'data-table-expand') {
+                    if (columnKey === 'data-table-expand') {
                       return slots['item.data-table-expand']?.(slotProps) ?? (
                         <VBtn
                           icon={ isExpanded(item) ? '$collapse' : '$expand' }
@@ -227,7 +139,18 @@ export const VDataTableRow = genericComponent<new <T>(
                       )
                     }
 
-                    return toDisplayString(slotProps.value)
+                    const displayValue = toDisplayString(slotProps.value)
+
+                    if (props.mobileView) {
+                      return (
+                        <>
+                          <div class="v-data-table__mobile-td-title">{ column.title }</div>
+                          <div class="v-data-table__mobile-td-value">{ displayValue }</div>
+                        </>
+                      )
+                    }
+
+                    return displayValue
                   },
                 }}
               </VDataTableColumn>
