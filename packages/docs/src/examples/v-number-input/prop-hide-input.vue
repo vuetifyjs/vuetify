319b9c6373ef09d667154be3602ca60dcfe0dacf
Yuchao <yuchaoyj@gmail.com>
2024-03-20 06:43:29 +1100

    feat(VNumberInput): add new component to labs (#18162)
    Co-authored-by: John Leider <john@vuetifyjs.com>

---
+++ packages/docs/src/examples/v-number-input/prop-hide-input.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="auto">
+        <v-number-input hide-input></v-number-input>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
