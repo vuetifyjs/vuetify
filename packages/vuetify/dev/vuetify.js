5032265a616e5a0bb717e26a47a138ab251a8e1c
John Leider <john@vuetifyjs.com>
2024-03-25 09:16:12 -0500

    chore: revert changes to dev

--- packages/vuetify/dev/vuetify.js
+++ packages/vuetify/dev/vuetify.js
@@ -3,23 +3,16 @@ import 'vuetify/src/styles/main.sass'
 
 import { createVuetify } from 'vuetify/src/framework'
 import * as directives from 'vuetify/src/directives'
-import { VuetifyDateAdapter } from 'vuetify/src/composables/date/adapters/vuetify'
-import * as components from 'vuetify/src/components'
-import { VTimePicker } from 'vuetify/src/labs/components'
+
+import date from './vuetify/date'
 import defaults from './vuetify/defaults'
 import icons from './vuetify/icons'
 import locale from './vuetify/locale'
 
 export default createVuetify({
-  date: {
-    adapter: VuetifyDateAdapter,
-  },
-  components: {
-    ...components,
-    VTimePicker,
-  },
   directives,
   ssr: !!process.env.VITE_SSR,
+  date,
   defaults,
   icons,
   locale,
