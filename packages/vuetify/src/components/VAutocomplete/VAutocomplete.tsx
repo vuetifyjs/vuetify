384d89ecfe526b78219cbf4ff9004f25099dd799
Titi Asrat <titi@veoci.com>
2024-03-28 19:07:11 -0500

    fix(VSelects): focus / activation of chips and clearable w/ kb (#18838)
    fixes #18482
    
    Co-authored-by: John Leider <john@vuetifyjs.com>

--- packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
+++ packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
@@ -552,6 +552,14 @@ export const VAutocomplete = genericComponent<new <
 
                   const slotProps = {
                     'onClick:close': onChipClose,
+                    onKeydown (e: KeyboardEvent) {
+                      if (e.key !== 'Enter' && e.key !== ' ') return
+
+                      e.preventDefault()
+                      e.stopPropagation()
+
+                      onChipClose(e)
+                    },
                     onMousedown (e: MouseEvent) {
                       e.preventDefault()
                       e.stopPropagation()
