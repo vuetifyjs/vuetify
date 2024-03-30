9feeb1cb22ae9237f72bc0121ca7541f34b84d10
John Leider <john@vuetifyjs.com>
2024-03-30 00:34:42 -0500

    chore(VDataTableFooter): change isNaN comparison type

--- packages/vuetify/src/components/VDataTable/VDataTableFooter.tsx
+++ packages/vuetify/src/components/VDataTable/VDataTableFooter.tsx
@@ -92,7 +92,7 @@ export const VDataTableFooter = genericComponent<{ prepend: never }>()({
 
         return {
           ...option,
-          title: !isNaN(option.title as any) ? option.title : t(option.title),
+          title: !isNaN(Number(option.title)) ? option.title : t(option.title),
         }
       })
     ))
