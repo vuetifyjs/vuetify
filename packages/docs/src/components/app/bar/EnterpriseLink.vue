c4ab6c42293a69f1771faf78f4cfcb5bcebff7bf
John Leider <john@vuetifyjs.com>
2024-04-03 21:37:10 -0500

    docs(EnterpriseLink): fix duplicate translate function

--- packages/docs/src/components/app/bar/EnterpriseLink.vue
+++ packages/docs/src/components/app/bar/EnterpriseLink.vue
@@ -1,15 +1,14 @@
 <template>
   <AppBtn
-    :text="t('enterprise')"
     :to="rpath('/introduction/enterprise-support/')"
     class="ms-1"
     color="primary"
+    text="enterprise"
     variant="outlined"
     @click="gtagClick('app-bar', 'enterprise', name)"
   />
 </template>
 
 <script setup>
-  const { t } = useI18n()
   const { name } = useRoute()
 </script>
