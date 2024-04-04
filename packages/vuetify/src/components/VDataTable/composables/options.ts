3b8d78d08253c126a3826dd393a9259afc16b7f9
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-04-04 11:46:44 -0700

    fix(VDataTable): check for old search value type of undefined
    * fixed #17966

--- packages/vuetify/src/components/VDataTable/composables/options.ts
+++ packages/vuetify/src/components/VDataTable/composables/options.ts
@@ -34,7 +34,7 @@ export function useOptions ({
     if (deepEqual(oldOptions, options.value)) return
 
     // Reset page when searching
-    if (oldOptions?.search !== options.value.search) {
+    if (typeof oldOptions?.search !== 'undefined' && oldOptions?.search !== options.value.search) {
       page.value = 1
     }
 
