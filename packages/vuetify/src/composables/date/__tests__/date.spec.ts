9a8ae56cb039d24e6f74cbfaf6d36db05b4c74e5
Kael <kaelwd@gmail.com>
2024-04-16 01:16:54 +1000

    chore: revert default TDate change

--- packages/vuetify/src/composables/date/__tests__/date.spec.ts
+++ packages/vuetify/src/composables/date/__tests__/date.spec.ts
@@ -13,7 +13,7 @@ function expectAssignable<T, T2 extends T = T> (value: T2): void {}
 
 describe('date.ts', () => {
   // Cannot define properties that don't exist in date-io
-  expectAssignable<DateAdapter>({} as IUtils<Date, string>)
+  expectAssignable<DateAdapter>({} as IUtils<Date>)
   // @ts-expect-error Can implement a subset of date-io
   expectAssignable<IUtils<Date>>({} as DateAdapter)
 
