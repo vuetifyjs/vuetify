3c4d0b16acb64ba04b580ac0aae8424cb43cbedb
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2024-04-19 00:05:25 +1000

    fix(VSelect): auto scroll to the last model value item when menu open
    fixes #19635

--- packages/vuetify/src/components/VSelect/VSelect.tsx
+++ packages/vuetify/src/components/VSelect/VSelect.tsx
@@ -304,8 +304,8 @@ export const VSelect = genericComponent<new <
 
     watch([menu, model], () => {
       if (!props.hideSelected && menu.value && model.value.length) {
-        const index = displayItems.value.findLastIndex(
-          item => model.value.some(s => props.valueComparator(s.value, item.value))
+        const index = displayItems.value.findIndex(
+          item => props.valueComparator(model.value[model.value.length - 1]?.value, item.value)
         )
         IN_BROWSER && window.requestAnimationFrame(() => {
           index >= 0 && vVirtualScrollRef.value?.scrollToIndex(index)
