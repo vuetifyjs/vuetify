2d5e624e355b857efb597a6eb33d7d5f4c5e3fa4
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2024-04-25 18:44:20 +1000

    chore: remove redundant logics

--- packages/vuetify/src/labs/VPullToRefresh/VPullToRefresh.tsx
+++ packages/vuetify/src/labs/VPullToRefresh/VPullToRefresh.tsx
@@ -9,12 +9,10 @@ import { VProgressCircular } from '@/components/VProgressCircular'
 import { LoaderSlot } from '@/composables/loader'
 
 // Utilities
-import { computed, onMounted, ref, shallowRef, watchEffect } from 'vue'
+import { computed, onMounted, ref, shallowRef, watch } from 'vue'
 import { clamp, convertToUnit, genericComponent, getScrollParents, useRender } from '@/util'
 
 const PULL_DOWN_HEIGHT_PX = 64
-
-export type PullToRefreshStatus = 'ok' | 'error'
 
 export const VPullToRefresh = genericComponent()({
   name: 'VPullToRefresh',
@@ -23,7 +21,7 @@ export const VPullToRefresh = genericComponent()({
   },
 
   emits: {
-    load: (options: { done: (status: PullToRefreshStatus) => void }) => true,
+    load: (options: { done: () => void }) => true,
   },
 
   setup (props, { slots, emit }) {
@@ -31,11 +29,10 @@ export const VPullToRefresh = genericComponent()({
     let immediateScrollParent: HTMLElement | undefined
 
     const touchDiff = shallowRef(0)
-    const scrollContainerRef = ref<HTMLElement>()
+    const containerRef = ref<HTMLElement>()
 
     const refreshing = shallowRef(false)
     const touching = shallowRef(false)
-    const canRefresh = shallowRef(false)
 
     const topOffset = computed(() => clamp(touchDiff.value, 0, PULL_DOWN_HEIGHT_PX))
 
@@ -46,38 +43,36 @@ export const VPullToRefresh = genericComponent()({
     }
 
     function onTouchmove (e: TouchEvent) {
-      if (canRefresh.value || refreshing.value) return
+      if (refreshing.value) return
 
       const touchY = e.touches[0].clientY
 
       if (!immediateScrollParent!.scrollTop) {
         touchDiff.value = touchY - touchstartY
-        canRefresh.value = touchDiff.value >= PULL_DOWN_HEIGHT_PX
       }
     }
 
     function onTouchend (e: TouchEvent) {
       if (refreshing.value) return
       touching.value = false
-      if (canRefresh.value) {
-        function done (status: PullToRefreshStatus) {
+      if (touchDiff.value >= PULL_DOWN_HEIGHT_PX) {
+        function done () {
           if (!refreshing.value) return
-          if (status === 'ok') {
-            touchDiff.value = 0
-            refreshing.value = false
-          }
+          touchDiff.value = 0
+          refreshing.value = false
         }
         emit('load', { done })
         refreshing.value = true
-        canRefresh.value = false
+      } else {
+        touchDiff.value = 0
       }
     }
 
     onMounted(() => {
-      immediateScrollParent = getScrollParents(scrollContainerRef.value)[0]
+      immediateScrollParent = getScrollParents(containerRef.value)[0]
     })
 
-    watchEffect(() => {
+    watch([topOffset, refreshing], () => {
       if (immediateScrollParent) {
         immediateScrollParent.style.overflow = topOffset.value && !refreshing.value ? 'hidden' : 'auto'
       }
@@ -92,6 +87,7 @@ export const VPullToRefresh = genericComponent()({
           onTouchstart={ onTouchstart }
           onTouchmove={ onTouchmove }
           onTouchend={ onTouchend }
+          ref={ containerRef }
         >
           <div
             class={[
@@ -130,7 +126,6 @@ export const VPullToRefresh = genericComponent()({
                 'v-pull-to-refresh__scroll-container--touching': touching.value,
               },
             ]}
-            ref={ scrollContainerRef }
             style={{ top: convertToUnit(topOffset.value) }}
           >
             { slots.default?.() }
