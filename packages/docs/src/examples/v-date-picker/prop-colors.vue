7a78637ab46f15dcaa595bb1f08e88b717bb8135
SonTT19 <tran.thanh.son.acc@gmail.com>
2024-04-26 10:44:54 +0700

    feat(VDatePicker): add header-color props

--- packages/docs/src/examples/v-date-picker/prop-colors.vue
+++ packages/docs/src/examples/v-date-picker/prop-colors.vue
@@ -4,6 +4,11 @@
       <v-date-picker
         color="primary"
       ></v-date-picker>
+
+      <v-date-picker
+        color="primary"
+        header-color="secondary"
+      ></v-date-picker>
     </v-row>
   </v-container>
 </template>
