vuetifyjs/vuetify

commit 85ba4a800711b006c13fcfb18a5046a6195a5ead

fix(VMenu): restore menu focus on enter inside input (#19519)
fixes #19206

Co-authored-by: John Leider <john@vuetifyjs.com>

packages/vuetify/src/components/VMenu/VMenu.tsx
@@ -125,7 +125,9 @@ export const VMenu = genericComponent<OverlaySlots>()({
     function onKeydown (e: KeyboardEvent) {
       if (props.disabled) return
 
-      if (e.key === 'Tab') {
+      if (e.key === 'Tab' || (e.key === 'Enter' && !props.closeOnContentClick)) {
+        if (e.key === 'Enter') e.preventDefault()
+
         const nextElement = getNextElement(
           focusableChildren(overlay.value?.contentEl as Element, false),
           e.shiftKey ? 'prev' : 'next',

commit 054aea45ee6cfee82dc2faf2836779642819234a

fix(VMenu): allow enter keypress to work in textareas
Previously, when a `<v-menu />` with `:close-on-content-click="false"`
contained a textarea, an enter press inside the textarea would do
nothing and not produce a new line. Now, it will allow a new line to be
typed into the textarea when pressing enter.

This last worked in Vuetify v3.5.14 and was broken starting in v3.5.15.
The fix for #19519 in 85ba4a800 caused this issue.

fixes #19767

packages/vuetify/src/components/VMenu/VMenu.tsx
@@ -126,6 +126,7 @@ export const VMenu = genericComponent<OverlaySlots>()({
       if (props.disabled) return
 
       if (e.key === 'Tab' || (e.key === 'Enter' && !props.closeOnContentClick)) {
+        if (e.key === 'Enter' && e.target instanceof HTMLTextAreaElement) return
         if (e.key === 'Enter') e.preventDefault()
 
         const nextElement = getNextElement(