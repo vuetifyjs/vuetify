e4dd89479b216e3e9dedf7981b381b0b8a3bd4e0
Ken Kieu <minhcanhdn0704@gmail.com>
2024-05-09 13:53:19 -0700

    update usage

--- packages/docs/src/examples/v-confirm-directive/usage.vue
+++ packages/docs/src/examples/v-confirm-directive/usage.vue
@@ -1,26 +1,23 @@
 <template>
-  <ExamplesUsageExample
-    v-model="model"
-    :code="code"
-    :name="name"
-    :options="options"
-  >
+  <ExamplesUsageExample v-model=" model " :code=" code " :name=" name " :options=" options ">
     <div class="text-center">
       <v-btn
-        v-if="!showInput"
+        v-if=" !showInput "
         text="Save Change"
-        v-confirm="{ text: 'Are you sure?', onSubmit }"
+        v-confirm=" { text: 'Are you sure?', onSubmit: () => showAlert( 'Submit clicked' ) } "
       ></v-btn>
-      <v-btn
-        v-else
-        text="Change name"
-        v-confirm="{ text: 'Enter name', input, onSubmit }"
+      <div v-else>Username: <v-btn
+        :text=" input "
+        class="text-none"
+        variant="text"
+        v-confirm=" { text: 'Enter name', input, onSubmit: ( value ) => input = value } "
       ></v-btn>
+      </div>
+
     </div>
 
     <template v-slot:configuration>
-      <v-text-field v-model="text" label="Text"></v-text-field>
-      <v-checkbox v-model="showInput" label="Show Input"></v-checkbox>
+      <v-checkbox v-model=" showInput " label="Show Input"></v-checkbox>
     </template>
   </ExamplesUsageExample>
 </template>
@@ -28,16 +25,15 @@
 <script setup>
   const name = 'v-confirm'
   const model = ref('default')
-  const text = ref('Are you sure?')
   const showInput = ref(false)
-  const input = ref('John')
+  const input = ref('johnleider')
 
   const code = computed(() => {
     return !showInput.value ? `<v-btn v-confirm="{ text:'Are you sure?', onSubmit: () => alert('Submit clicked')}">Save Change</v-btn>`
       : `<v-btn v-confirm="{ text:'Enter name', input: 'John', onSubmit: (value) => alert(value)}">Change name</v-btn>`
   })
 
-  const onSubmit = value => {
-    alert(value || 'Submit clicked')
+  function showAlert (value) {
+    alert(value)
   }
 </script>
