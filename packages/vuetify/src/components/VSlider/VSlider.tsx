e6aa07fb5f70f65a87c1586e899bdad6cbeded13
Tilen Pirih <33714934+tilenpirih@users.noreply.github.com>
2024-05-24 17:05:33 +0200

    Update packages/vuetify/src/components/VSlider/VSlider.tsx
    Co-authored-by: John Leider <9064066+johnleider@users.noreply.github.com>

--- packages/vuetify/src/components/VSlider/VSlider.tsx
+++ packages/vuetify/src/components/VSlider/VSlider.tsx
@@ -168,7 +168,7 @@ export const VSlider = genericComponent<VSliderSlots>()({
                   onFocus={ focus }
                   onBlur={ blur }
                   ripple={ props.ripple }
-                  name = { props.name}
+                  name={ props.name }
                 >
                   {{ 'thumb-label': slots['thumb-label'] }}
                 </VSliderThumb>
