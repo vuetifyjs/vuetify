a2104a1e9b2f33d7bcf1dcb6a7dd77d3592360bd
Joel Mandell <joelmandell@gmail.com>
2024-06-04 15:47:58 +0200

    fix linting

--- packages/vuetify/src/components/VOverlay/VOverlay.tsx
+++ packages/vuetify/src/components/VOverlay/VOverlay.tsx
@@ -154,8 +154,9 @@ export const VOverlay = genericComponent<OverlaySlots>()({
       contentEvents,
       scrimEvents,
     } = useActivator(props, { isActive, isTop: localTop })
-    const potentialShadowDomRoot = computed( () => (activatorEl?.value as Element)?.getRootNode() as Element);
-    const { teleportTarget } = useTeleport(computed(() => props.attach || props.contained || potentialShadowDomRoot.value instanceof ShadowRoot ? potentialShadowDomRoot.value : false))
+    const potentialShadowDomRoot = computed( () => (activatorEl?.value as Element)?.getRootNode() as Element)
+    const { teleportTarget } = useTeleport(computed(() => props.attach || props.contained 
+      || potentialShadowDomRoot.value instanceof ShadowRoot ? potentialShadowDomRoot.value : false))
     const { dimensionStyles } = useDimension(props)
     const isMounted = useHydration()
     const { scopeId } = useScopeId()
