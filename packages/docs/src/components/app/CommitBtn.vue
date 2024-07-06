f32ff105b8c4a3b8535822028089830002390c57
John Leider <john@vuetifyjs.com>
2024-07-06 18:26:35 -0500

    docs(commits): restructure store

--- packages/docs/src/components/app/CommitBtn.vue
+++ packages/docs/src/components/app/CommitBtn.vue
@@ -1,10 +1,10 @@
 <template>
   <v-btn
-    v-if="commits.latest"
     :href="`https://github.com/vuetifyjs/vuetify/commit/${commits.latest?.sha}`"
-    :text="commits.latest?.sha?.slice(0, 7)"
+    :prepend-icon="commits.latest ? 'mdi-source-commit' : undefined"
+    :readonly="!commits.latest"
+    :text="commits.latest?.sha.slice(0, 7)"
     class="text-caption"
-    prepend-icon="mdi-source-commit"
     rel="noopener noreferrer"
     size="small"
     target="_blank"
