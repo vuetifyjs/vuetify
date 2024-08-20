af9a492cadfaa0f6373cde8c584bf597551bea42
jsek <im68889094144@outlook.com>
2024-08-20 16:03:09 +0200

    feat(VNumberInput): clear zero's on focus

---
+++ packages/docs/src/examples/v-number-input/prop-clear-zero.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-number-input v-model="value" clear-zero></v-number-input>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+  const value = ref(0)
+</script>
