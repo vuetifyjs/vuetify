8272b7cd38f0a63bfa7a0dc6b17c7a48d900c68c
jsek <im68889094144@outlook.com>
2024-08-22 12:21:40 +0200

    chore: streamline VList example with selectable actions

--- packages/docs/src/examples/v-list/misc-actions.vue
+++ packages/docs/src/examples/v-list/misc-actions.vue
@@ -1,85 +1,58 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-  >
+  <v-card class="mx-auto" max-width="400">
     <v-toolbar color="purple">
       <v-btn icon="mdi-menu"></v-btn>
-
       <v-toolbar-title>Settings</v-toolbar-title>
-
       <v-spacer></v-spacer>
-
       <v-btn icon="mdi-magnify"></v-btn>
     </v-toolbar>
 
     <v-list lines="three">
       <v-list-subheader>User Controls</v-list-subheader>
-
-      <v-list-item>
-        <v-list-item-title>Content filtering</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Set the content filtering level to restrict appts that can be downloaded
-        </v-list-item-subtitle>
-      </v-list-item>
-
-      <v-list-item>
-        <v-list-item-title>Password</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Require password for purchase or use password to restrict purchase
-        </v-list-item-subtitle>
-      </v-list-item>
+      <v-list-item
+        v-for="item in userControls"
+        :key="item.value"
+        :subtitle="item.subtitle"
+        :title="item.title"
+      ></v-list-item>
     </v-list>
 
     <v-divider></v-divider>
 
-    <v-list lines="three" select-strategy="classic">
+    <v-list
+      v-model:selected="settingsSelection"
+      lines="three"
+      select-strategy="leaf"
+    >
       <v-list-subheader>General</v-list-subheader>
-
-      <v-list-item value="notifications">
-        <template v-slot:prepend="{ isActive }">
-
+      <v-list-item
+        v-for="item in settingsItems"
+        :key="item.value"
+        :subtitle="item.subtitle"
+        :title="item.title"
+        :value="item.value"
+      >
+        <template v-slot:prepend="{ isSelected }">
           <v-list-item-action start>
-            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+            <v-checkbox-btn :model-value="isSelected"></v-checkbox-btn>
           </v-list-item-action>
         </template>
-
-        <v-list-item-title>Notifications</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Notify me about updates to apps or games that I downloaded
-        </v-list-item-subtitle>
-      </v-list-item>
-
-      <v-list-item value="sound">
-        <template v-slot:prepend="{ isActive }">
-          <v-list-item-action start>
-            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-          </v-list-item-action>
-        </template>
-
-        <v-list-item-title>Sound</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Auto-update apps at any time. Data charges may apply
-        </v-list-item-subtitle>
-      </v-list-item>
-
-      <v-list-item value="widgets">
-        <template v-slot:prepend="{ isActive }">
-          <v-list-item-action start>
-            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-          </v-list-item-action>
-        </template>
-
-        <v-list-item-title>Auto-add widgets</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Automatically add home screen widgets when downloads complete
-        </v-list-item-subtitle>
       </v-list-item>
     </v-list>
   </v-card>
 </template>
+
+<script setup>
+  const userControls = [
+    { title: 'Content filtering', subtitle: 'Set the content filtering level to restrict appts that can be downloaded' },
+    { title: 'Password', subtitle: 'Require password for purchase or use password to restrict purchase' },
+  ]
+
+  const settingsItems = [
+    { value: 'notifications', title: 'Notifications', subtitle: 'Notify me about updates to apps or games that I downloaded' },
+    { value: 'sound', title: 'Sound', subtitle: 'Auto-update apps at any time. Data charges may apply' },
+    { value: 'widgets', title: 'Auto-add widgets', subtitle: 'Automatically add home screen widgets when downloads complete' },
+  ]
+
+  const settingsSelection = ref([])
+</script>
