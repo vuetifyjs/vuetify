cfd9146198e1b5ceb55d7cb078301ccbea254ea9
adrien charrier <adrien.charrier@outlook.fr>
2024-08-23 13:01:22 +0200

    fix(VIcon): don't set aria-hidden on the svg tag when it's set to false
    fixes #20382

--- packages/vuetify/src/components/VIcon/VIcon.ts
+++ packages/vuetify/src/components/VIcon/VIcon.ts
@@ -159,7 +159,7 @@ const VIcon = mixins(
           xmlns: 'http://www.w3.org/2000/svg',
           viewBox: '0 0 24 24',
           role: 'img',
-          'aria-hidden': true,
+          'aria-hidden': this.$attrs['aria-hidden'] !== 'false',
         },
       }
 
