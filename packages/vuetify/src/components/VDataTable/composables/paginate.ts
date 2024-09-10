acaa8429839e07713ed6def885bdea337fbf568c
J-Sek <J-Sek@users.noreply.github.com>
2024-09-10 13:22:10 +0200

    fix(VDataTable,VDataIterator): emit current items immediately

--- packages/vuetify/src/components/VDataTable/composables/paginate.ts
+++ packages/vuetify/src/components/VDataTable/composables/paginate.ts
@@ -128,7 +128,7 @@ export function usePaginatedItems <T> (options: {
 
   watch(paginatedItems, val => {
     vm.emit('update:currentItems', val)
-  })
+  }, { immediate: true })
 
   return { paginatedItems }
 }
