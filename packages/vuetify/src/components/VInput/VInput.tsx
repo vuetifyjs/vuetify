b3a183d5cdb2ab384ced379f804677428d579371
vikash8813 <vikashjangir2301@gmail.com>
2024-09-13 23:08:32 +0530

    fixed prepend icon click on file input

--- packages/vuetify/src/components/VInput/VInput.tsx
+++ packages/vuetify/src/components/VInput/VInput.tsx
@@ -147,6 +147,18 @@ export const VInput = genericComponent<new <T>(
       }
     })
 
+    function prependKeyDown(key: KeyboardEvent) {
+      console.warn('prependKeyDown')
+      if (key.key !== 'Enter' && key.key !== ' ') return
+
+      console.warn('prependKeyDown')
+      key.preventDefault()
+      key.stopPropagation()
+
+      props['onClick:prepend']?.(new MouseEvent('click'))
+    }
+
+
     useRender(() => {
       const hasPrepend = !!(slots.prepend || props.prependIcon)
       const hasAppend = !!(slots.append || props.appendIcon)
@@ -177,7 +189,7 @@ export const VInput = genericComponent<new <T>(
           ]}
         >
           { hasPrepend && (
-            <div key="prepend" class="v-input__prepend">
+            <div key="prepend" class="v-input__prepend" onKeypress={prependKeyDown}>
               { slots.prepend?.(slotProps.value) }
 
               { props.prependIcon && (
