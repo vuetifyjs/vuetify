4ee76d7ffd13ca53f95185806df3307162f034d1
Kael <kaelwd@gmail.com>
2024-09-22 18:10:01 +1000

    use screen

--- packages/vuetify/src/components/VCheckbox/__tests__/VCheckboxBtn.spec.browser.tsx
+++ packages/vuetify/src/components/VCheckbox/__tests__/VCheckboxBtn.spec.browser.tsx
@@ -1,65 +1,59 @@
 import { VCheckboxBtn } from '../'
 
 // Utilities
-import { render } from '@test'
-import { userEvent } from '@vitest/browser/context'
+import { render, userEvent, screen } from '@test'
 import { ref } from 'vue'
 
 describe('VCheckboxBtn', () => {
   it('should function without v-model', async () => {
-    const { container } = render(() => (
+    render(() => (
       <VCheckboxBtn />
     ))
 
-    const input = container.querySelector('input')
+    const input = screen.getByCSS('input')
 
-    await userEvent.click(input!)
-
+    await userEvent.click(input)
     expect(input).toBeChecked()
 
-    await userEvent.click(input!)
-
+    await userEvent.click(input)
     expect(input).not.toBeChecked()
   })
 
   it('should function with v-model', async () => {
     const model = ref(false)
-    const { container } = render(() => (
+    render(() => (
       <VCheckboxBtn v-model={ model.value } />
     ))
 
-    const input = container.querySelector('input')
+    const input = screen.getByCSS('input')
 
-    await userEvent.click(input!)
-
+    await userEvent.click(input)
     expect(input).toBeChecked()
     expect(model.value).toBe(true)
 
-    await userEvent.click(input!)
-
+    await userEvent.click(input)
     expect(input).not.toBeChecked()
     expect(model.value).toBe(false)
   })
 
   it('should display indeterminate status', async () => {
-    const { container } = render(() => (
+    render(() => (
       <VCheckboxBtn modelValue={ false } indeterminate />
     ))
 
-    const input = container.querySelector('input')
+    const input = screen.getByCSS('input')
     expect(input).toHaveAttribute('aria-checked', 'mixed')
   })
 
   it('should not update input checked state when it is readonly', async () => {
     const model = ref(false)
-    const { container } = render(() => (
+    render(() => (
       <VCheckboxBtn v-model={ model.value } readonly />
     ))
 
-    const input = container.querySelector('input')
+    const input = screen.getByCSS('input')
 
     await userEvent.click(input!)
-
     expect(input).not.toBeChecked()
     expect(model.value).toBe(false)
   })
