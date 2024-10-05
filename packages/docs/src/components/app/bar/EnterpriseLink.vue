6173caba27f03e325cc8c48c59603339de8c9b46
Kael <kaelwd@gmail.com>
2024-10-05 16:34:06 +1000

    docs: add umami analytics

--- packages/docs/src/components/app/bar/EnterpriseLink.vue
+++ packages/docs/src/components/app/bar/EnterpriseLink.vue
@@ -1,8 +1,11 @@
 <template>
   <AppBtn
+    :data-umami-event-value="name"
     :to="rpath('/introduction/enterprise-support/')"
     class="ms-1"
     color="primary"
+    data-umami-event="app-bar"
+    data-umami-event-type="enterprise"
     text="support"
     variant="outlined"
     @click="gtagClick('app-bar', 'enterprise', name)"
