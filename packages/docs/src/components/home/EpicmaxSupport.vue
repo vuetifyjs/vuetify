28b44c1c9c83f58ef4293b716e363685ad132b28
Vasyl <vasyl.hryha@epicmax.co>
2024-11-07 15:37:08 +0200

    - feat: add Epicmax support section to home page

---
+++ packages/docs/src/components/home/EpicmaxSupport.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="bg-light-blue-darken-4 py-8">
+    <h2 class="v-heading mb-2 text-h4 text-sm-h4">{{ t("epicmax.title") }}</h2>
+    <p class="mx-auto" style="width: 700px">{{ t("epicmax.message") }}</p>
+    <v-btn
+      :aria-label="t('epicmax.learn-more')"
+      class="px-16 text-light-blue-darken-4"
+      color="grey-lighten-5"
+      href="https://www.epicmax.co/?ref=vuetify"
+      rounded="0"
+      size="large"
+      variant="flat"
+    >
+      <span
+        class="text-capitalize font-weight-regular"
+        v-text="t('epicmax.learn-more')"
+      />
+    </v-btn>
+  </section>
+</template>
+
+<script setup>
+  const { t } = useI18n()
+</script>
