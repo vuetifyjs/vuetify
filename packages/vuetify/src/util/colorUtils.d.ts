63612436e2a6838c9dda776ff52d0886329fdb36
ROYER-ADNOT Remy <remy.royeradnot.pro@gmail.com>
2024-11-08 21:52:04 +1100

    Expose type definitions for colorUtils

---
+++ packages/vuetify/src/util/colorUtils.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for colorUtils
+
+export type XYZ = [number, number, number];
+export type LAB = [number, number, number];
+export type HSV = { h: number, s: number, v: number, a?: number };
+export type RGB = { r: number, g: number, b: number, a?: number };
+export type HEX = string;
+
+// Export functions and types included in colorUtils.ts
