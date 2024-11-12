f2cf3d058d3da9c1e50ab4b4424b8f565bd93993
Kael <kaelwd@gmail.com>
2024-11-12 18:00:09 +1100

    chore: replace umami with swetrix

---
+++ packages/docs/src/plugins/analytics.js
@@ -0,0 +1,7 @@
+import * as Swetrix from 'swetrix'
+
+Swetrix.init('IM114CSbiwdu', {
+  apiURL: 'https://swetrix-api.vuetifyjs.com/log'
+})
+Swetrix.trackViews()
+Swetrix.trackErrors()
--- packages/docs/src/plugins/index.js
+++ packages/docs/src/plugins/index.js
@@ -1,4 +1,5 @@
 // Make this dynamic
+import '@/plugins/analytics'
 import '@/plugins/no-ssr'
 import '@/plugins/rollbar'
 import '@/plugins/vuetify'
