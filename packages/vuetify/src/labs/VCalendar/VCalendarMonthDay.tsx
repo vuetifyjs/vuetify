f80d66b5971963411785039b6aed14c6f6e8f9ef
Jason Thomas <jason@openc3.com>
2024-12-02 14:24:34 -0700

    Fix linter errors

--- packages/vuetify/src/labs/VCalendar/VCalendarMonthDay.tsx
+++ packages/vuetify/src/labs/VCalendar/VCalendarMonthDay.tsx
@@ -93,7 +93,7 @@ export const VCalendarMonthDay = genericComponent<VCalendarMonthDaySlots>()({
                       : (
                         <VCalendarEvent
                           day={ props.day }
-                          event={ event } 
+                          event={ event }
                           onClick:event={ clickEvent }
                           onContextmenu:event={ contextmenuEvent }
                           allDay
