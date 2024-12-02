1e099f2c1858617a4447c308326cf204f9e9d112
J-Sek <J-Sek@users.noreply.github.com>
2024-12-02 08:24:02 +0100

    feat(VChip): add base-color prop (#20702)
    resolves #19678

--- packages/vuetify/src/components/VChipGroup/VChipGroup.tsx
+++ packages/vuetify/src/components/VChipGroup/VChipGroup.tsx
@@ -23,6 +23,7 @@ import type { GenericProps } from '@/util'
 export const VChipGroupSymbol = Symbol.for('vuetify:v-chip-group')
 
 export const makeVChipGroupProps = propsFactory({
+  baseColor: String,
   column: Boolean,
   filter: Boolean,
   valueComparator: {
@@ -69,6 +70,7 @@ export const VChipGroup = genericComponent<new <T>(
 
     provideDefaults({
       VChip: {
+        baseColor: toRef(props, 'baseColor'),
         color: toRef(props, 'color'),
         disabled: toRef(props, 'disabled'),
         filter: toRef(props, 'filter'),
