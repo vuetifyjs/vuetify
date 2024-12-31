85841bc8064b689941d8bac33cbc969e82bafa77
babu-ch <mashirohira@gmail.com>
2024-12-31 18:28:33 +0900

    fix(VField): prevent rendering clear button when disabled

--- packages/vuetify/src/components/VField/VField.tsx
+++ packages/vuetify/src/components/VField/VField.tsx
@@ -319,7 +319,7 @@ export const VField = genericComponent<new <T>(
             } as VFieldSlot)}
           </div>
 
-          { hasClear && (
+          { hasClear && !props.disabled && (
             <VExpandXTransition key="clear">
               <div
                 class="v-field__clearable"
