f00e96be6cf248cf33424ee3c9ac620108fca26c
J-Sek <J-Sek@users.noreply.github.com>
2025-01-23 20:31:58 +0100

    feat(VNumberInput): ability to hide controls

--- packages/docs/src/examples/v-number-input/prop-control-variant.vue
+++ packages/docs/src/examples/v-number-input/prop-control-variant.vue
@@ -18,6 +18,12 @@
 
         <v-number-input control-variant="split"></v-number-input>
       </v-col>
+
+      <v-col cols="12" md="4" sm="4">
+        <h5>Hidden</h5>
+
+        <v-number-input control-variant="hidden"></v-number-input>
+      </v-col>
     </v-row>
   </v-container>
 </template>
