b54db8ddb799ad6e4f1c99549f154c5e7e33b9ef
Kael <kaelwd@gmail.com>
2025-02-14 18:34:17 +1100

    fix(VTreeview): remove itemType and unused slots
    fixes #20980

--- packages/vuetify/src/labs/VTreeview/VTreeview.tsx
+++ packages/vuetify/src/labs/VTreeview/VTreeview.tsx
@@ -36,7 +36,7 @@ export const makeVTreeviewProps = propsFactory({
     collapseIcon: '$treeviewCollapse',
     expandIcon: '$treeviewExpand',
     slim: true,
-  }), ['nav', 'openStrategy']),
+  }), ['itemType', 'nav', 'openStrategy']),
   modelValue: {
     type: Array,
     default: () => ([]),
