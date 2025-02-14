bee1af36538e8cbf28c5990181f4761482a526c2
Andrew Henry <AMajesticPotatoe@gmail.com>
2025-02-14 11:26:22 -0500

    docs: fix lint

--- packages/docs/src/examples/v-parallax/misc-welcome.vue
+++ packages/docs/src/examples/v-parallax/misc-welcome.vue
@@ -7,11 +7,11 @@
         <v-col cols="12" md="6">
           <h1 class="text-h1 mb-8">John Doe</h1>
           <h3 class="text-h3 mb-8 font-weight-thin">Web Developer</h3>
-          <v-btn color="primary" class="elevation-4 rounded-xl mb-4">
+          <v-btn class="elevation-4 rounded-xl mb-4" color="primary">
             Contact Me
           </v-btn>
         </v-col>
-        <v-col cols="12" md="6" class="text-center">
+        <v-col class="text-center" cols="12" md="6">
           <v-avatar :size="300" class="elevation-12 mx-auto mb-8">
             <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
           </v-avatar>
