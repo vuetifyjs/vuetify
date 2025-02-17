fd7cb11bf1689e2051bbd7aa08ff239a7b2c0608
J-Sek <J-Sek@users.noreply.github.com>
2025-02-17 07:27:16 +0100

    feat(VNumberInput): strict precision (#20252)
    resolves #19898
    
    Co-authored-by: Kael <kaelwd@gmail.com>

--- packages/vuetify/src/labs/VNumberInput/__tests__/VNumberInput.spec.browser.tsx
+++ packages/vuetify/src/labs/VNumberInput/__tests__/VNumberInput.spec.browser.tsx
@@ -8,15 +8,15 @@ import { ref } from 'vue'
 
 describe('VNumberInput', () => {
   it.each([
-    { typing: '---', expected: '-' }, // "-" is only allowed once
-    { typing: '1-', expected: '1' }, // "-" is only at the start
-    { typing: '.', expected: '.' }, // "." is allowed at the start
-    { typing: '..', expected: '.' }, // "." is only allowed once
-    { typing: '1...0', expected: '1.0' }, // "." is only allowed once
-    { typing: '123.45.67', expected: '123.4567' }, // "." is only allowed once
-    { typing: 'ab-c8+.iop9', expected: '-8.9' }, // Only numbers, "-", "." are allowed to type in
-  ])('prevents NaN from arbitrary input', async ({ typing, expected }) => {
-    const { element } = render(VNumberInput)
+    { precision: 0, typing: '---', expected: '-' }, // "-" is only allowed once
+    { precision: 0, typing: '1-', expected: '1' }, // "-" is only at the start
+    { precision: 1, typing: '.', expected: '.' }, // "." is allowed at the start
+    { precision: 1, typing: '..', expected: '.' }, // "." is only allowed once
+    { precision: 1, typing: '1...0', expected: '1.0' }, // "." is only allowed once
+    { precision: 4, typing: '123.45.67', expected: '123.4567' }, // "." is only allowed once
+    { precision: 1, typing: 'ab-c8+.iop9', expected: '-8.9' }, // Only numbers, "-", "." are allowed to type in
+  ])('prevents NaN from arbitrary input', async ({ precision, typing, expected }) => {
+    const { element } = render(() => <VNumberInput precision={ precision } />)
     await userEvent.click(element)
     await userEvent.keyboard(typing)
     expect(screen.getByCSS('input')).toHaveValue(expected)
@@ -28,7 +28,6 @@ describe('VNumberInput', () => {
       <VNumberInput
         clearable
         v-model={ model.value }
-        readonly
       />
     ))
 
@@ -134,6 +133,7 @@ describe('VNumberInput', () => {
           <VNumberInput
             class="disabled-input-1"
             v-model={ value3.value }
+            precision={ 1 }
             min={ 0 }
             max={ 10 }
             disabled
@@ -141,6 +141,7 @@ describe('VNumberInput', () => {
           <VNumberInput
             class="disabled-input-2"
             v-model={ value4.value }
+            precision={ 1 }
             min={ 0 }
             max={ 10 }
             disabled
@@ -181,6 +182,7 @@ describe('VNumberInput', () => {
       render(() => (
         <VNumberInput
           step={ 0.03 }
+          precision={ 2 }
           v-model={ model.value }
         />
       ))
