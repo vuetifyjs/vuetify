820f1353f1ed49157f34a3b91df7089472b2ce27
John Leider <john@vuetifyjs.com>
2025-02-24 13:30:53 -0600

    feat(VTreeview): add slot to replace expand toggle btn

--- packages/vuetify/src/components/VTreeview/shared.ts
+++ packages/vuetify/src/components/VTreeview/shared.ts
@@ -1,8 +1,13 @@
 // Types
 import type { ComputedRef, InjectionKey } from 'vue'
+import type { ListItemSlot } from '@/components/VList/VListItem'
 
 export interface TreeViewProvide {
   visibleIds: ComputedRef<Set<unknown> | null>
+}
+
+export type ToggleListItemSlot = ListItemSlot & {
+  props: { onClick: (e: PointerEvent) => void }
 }
 
 export const VTreeviewSymbol: InjectionKey<TreeViewProvide> = Symbol.for('vuetify:v-treeview')
