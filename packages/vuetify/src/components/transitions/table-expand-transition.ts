f233be39db651af11f784d38e5c9c0770f68eea1
Kael <kaelwd@gmail.com>
2025-03-06 16:52:03 +1100

    refactor: remove duplicate code

--- packages/vuetify/src/components/transitions/table-expand-transition.ts
+++ packages/vuetify/src/components/transitions/table-expand-transition.ts
@@ -1,6 +1,7 @@
-import { createJavascriptTransition } from './createTransition'
 import { ExpandTransitionGenerator } from './expand-transition'
-import { getCurrentInstance, h, mergeProps, onMounted, Transition } from 'vue'
+
+// Utilities
+import { h, Transition } from 'vue'
 import { defineComponent } from '@/util'
 
 const expandBase = ExpandTransitionGenerator()
@@ -16,101 +17,63 @@ export const VTableExpandTransition = defineComponent({
   },
 
   setup (props, { slots }) {
-    const vm = getCurrentInstance()!
-
-    function onEnter (el: Element, done: () => void) {
-      const tr = document.createElement('tr')
-      const td = document.createElement('td')
-      td.setAttribute('colspan', String(props.columns))
-      td.style.padding = '0'
-      td.style.height = 'auto'
-      const wrapperRoot = document.createElement('div')
-      const density = el.closest('.v-table')!.className.match(/v-table--density-([a-z]+)/)![1]
-      wrapperRoot.classList.add('v-table', `v-table--density-${density}`)
-      const wrapper = document.createElement('div')
-      wrapper.classList.add('v-table__wrapper')
-      const wrapperTable = document.createElement('table')
-      const tbody = document.createElement('tbody')
-      tr.append(td)
-      td.append(wrapperRoot)
-      wrapperRoot.append(wrapper)
-      wrapper.append(wrapperTable)
-      wrapperTable.append(tbody)
-      wrapperRoot.classList.add('expand-transition-enter-active', 'expand-transition-enter-from')
-      expandBase.onBeforeEnter(wrapperRoot)
-      el.parentNode!.insertBefore(tr, el)
-      tbody.append(el)
-      function onEnd () {
-        tr.parentNode!.insertBefore(el, tr)
-        tr.remove()
-        done()
+    function createHook (type: 'enter' | 'leave') {
+      return function (el: Element, done: () => void) {
+        const placeholder = document.createComment('placeholder')
+        const tr = document.createElement('tr')
+        const td = document.createElement('td')
+        td.setAttribute('colspan', String(props.columns))
+        td.style.padding = '0'
+        td.style.height = 'auto'
+        const wrapperRoot = document.createElement('div')
+        const density = el.closest('.v-table')!.className.match(/v-table--density-([a-z]+)/)![1]
+        wrapperRoot.classList.add('v-table', `v-table--density-${density}`)
+        const wrapper = document.createElement('div')
+        wrapper.classList.add('v-table__wrapper')
+        const wrapperTable = document.createElement('table')
+        const tbody = document.createElement('tbody')
+        tr.append(td)
+        td.append(wrapperRoot)
+        wrapperRoot.append(wrapper)
+        wrapper.append(wrapperTable)
+        wrapperTable.append(tbody)
+        if (type === 'enter') {
+          wrapperRoot.classList.add('expand-transition-enter-active', 'expand-transition-enter-from')
+          expandBase.onBeforeEnter(wrapperRoot)
+        } else {
+          wrapperRoot.classList.add('expand-transition-leave-active', 'expand-transition-leave-from')
+        }
+        el.parentNode!.insertBefore(placeholder, el)
+        el.parentNode!.insertBefore(tr, placeholder)
+        tbody.append(el)
+        function onEnd () {
+          placeholder.parentNode!.insertBefore(el, placeholder)
+          tr.remove()
+          placeholder.remove()
+          done()
+        }
+        wrapperRoot.addEventListener('transitionend', onEnd)
+        wrapperRoot.addEventListener('transitioncancel', onEnd)
+        requestAnimationFrame(() => {
+          if (type === 'enter') {
+            wrapperRoot.classList.remove('expand-transition-enter-from')
+            wrapperRoot.classList.add('expand-transition-enter-to')
+            expandBase.onEnter(wrapperRoot)
+          } else {
+            wrapperRoot.classList.remove('expand-transition-leave-from')
+            wrapperRoot.classList.add('expand-transition-leave-to')
+            expandBase.onLeave(wrapperRoot)
+          }
+        })
       }
-      wrapperRoot.addEventListener('transitionend', onEnd)
-      wrapperRoot.addEventListener('transitioncancel', onEnd)
-      requestAnimationFrame(() => {
-        wrapperRoot.classList.remove('expand-transition-enter-from')
-        wrapperRoot.classList.add('expand-transition-enter-to')
-        expandBase.onEnter(wrapperRoot)
-      })
-    }
-
-    function onLeave (el: Element, done: () => void) {
-      const tr = document.createElement('tr')
-      const td = document.createElement('td')
-      td.setAttribute('colspan', String(props.columns))
-      td.style.padding = '0'
-      td.style.height = 'auto'
-      const wrapperRoot = document.createElement('div')
-      const density = el.closest('.v-table')!.className.match(/v-table--density-([a-z]+)/)![1]
-      wrapperRoot.classList.add('v-table', `v-table--density-${density}`)
-      const wrapper = document.createElement('div')
-      wrapper.classList.add('v-table__wrapper')
-      const wrapperTable = document.createElement('table')
-      const tbody = document.createElement('tbody')
-      tr.append(td)
-      td.append(wrapperRoot)
-      wrapperRoot.append(wrapper)
-      wrapper.append(wrapperTable)
-      wrapperTable.append(tbody)
-      wrapperRoot.classList.add('expand-transition-leave-active', 'expand-transition-leave-from')
-      el.parentNode!.insertBefore(tr, el)
-      tbody.append(el)
-      function onEnd () {
-        tr.parentNode!.insertBefore(el, tr)
-        tr.remove()
-        done()
-      }
-      wrapperRoot.addEventListener('transitionend', onEnd)
-      wrapperRoot.addEventListener('transitioncancel', onEnd)
-      requestAnimationFrame(() => {
-        wrapperRoot.classList.remove('expand-transition-leave-from')
-        wrapperRoot.classList.add('expand-transition-leave-to')
-        expandBase.onLeave(wrapperRoot)
-      })
     }
 
     return () => (
       h(Transition, {
         css: false,
-        onEnter,
-        onLeave,
+        onEnter: createHook('enter'),
+        onLeave: createHook('leave'),
       }, slots.default)
     )
   },
 })
-
-// export const VTableExpandTransition = createJavascriptTransition('table-expand-transition', {
-//   ...expandBase,
-//   appear: true,
-//   css: false,
-//   onEnter (el, done) {
-//     const wrapper = document.createElement('div')
-//     wrapper.style.width = '100%'
-//     el.parentNode.insertBefore(wrapper, el)
-//     wrapper.appendChild(el)
-//     expandBase.onEnter(wrapper)
-//   },
-//   onAfterEnter (el) {
-//     console.log(el)
-//   },
-// })
