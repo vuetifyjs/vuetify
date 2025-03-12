8a59806c6a0a7adc407d71444eab7b313813d182
Kael <kaelwd@gmail.com>
2025-03-12 23:10:41 +1100

    replace class selectors with testid

--- packages/vuetify/src/components/VDatePicker/__tests__/VDatePicker.spec.browser.tsx
+++ packages/vuetify/src/components/VDatePicker/__tests__/VDatePicker.spec.browser.tsx
@@ -2,37 +2,22 @@
 import { VDatePicker } from '..'
 
 // Utilities
-import { render, screen } from '@test'
-import { fireEvent, waitFor } from '@testing-library/vue'
+import { render, screen, userEvent } from '@test'
 import { ref } from 'vue'
 
 describe('VDatePicker', () => {
-  const selectDay = async (dayIndex: number, buttonClass = '.v-date-picker-month__day-btn') => {
-    const days = screen.getAllByCSS(buttonClass)
-    const dayButton = days[dayIndex]
-    await fireEvent.click(dayButton)
-  }
-
-  const selectMonth = async (monthName: string) => {
-    const monthButton = screen.getByCSS('.v-date-picker-controls__month-btn')
-    await fireEvent.click(monthButton)
-    const month = screen.getByText(monthName, { exact: false })
-    await fireEvent.click(month)
-  }
-
   it('selects a range of dates', async () => {
     const model = ref<unknown[]>([])
     render(() => (
-        <VDatePicker v-model={ model.value } multiple="range" />
+      <VDatePicker v-model={ model.value } multiple="range" />
     ))
 
     // Select two days in the same month (e.g., 10th and 20th)
-    await selectDay(10)
-    await selectDay(20)
+    await userEvent.click(await screen.findByText(10))
+    await userEvent.click(await screen.findByText(20))
 
-    await waitFor(() => {
-      expect(model.value).toHaveLength(11) // Expect a 11-day range to be selected
-    })
+    // Expect a 11-day range to be selected
+    await expect.poll(() => model.value).toHaveLength(11)
   })
 
   it('selects a range of dates across month boundary', async () => {
@@ -41,16 +26,18 @@ describe('VDatePicker', () => {
       <VDatePicker v-model={ model.value } multiple="range" />
     ))
 
-    // Select day 7 in January
-    await selectMonth('Jan')
-    await selectDay(7)
+    // Select 2025-01-07
+    await userEvent.click(await screen.findByTestId('year-btn'))
+    await userEvent.click(await screen.findByText('2025'))
+    await userEvent.click(await screen.findByTestId('month-btn'))
+    await userEvent.click(await screen.findByText('Jan'))
+    await userEvent.click(await screen.findByText(7))
 
-    // Switch to February and select day 7
-    await selectMonth('Feb')
-    await selectDay(7)
+    // Select 2025-02-07
+    await userEvent.click(await screen.findByTestId('next-month'))
+    await userEvent.click(await screen.findByText(7))
 
-    await waitFor(() => {
-      expect(model.value).toHaveLength(32) // Expect a 32-day range spanning across two months
-    })
+    // Expect a 32-day range spanning across two months
+    await expect.poll(() => model.value).toHaveLength(32)
   })
 })
