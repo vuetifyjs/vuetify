d605f8749b693b0eadef65f6bf1cfba162e961df
Kael <kaelwd@gmail.com>
2025-03-14 17:32:42 +1100

    chore(VDefaultsProvider): update cypress tests to vitest

---
+++ packages/vuetify/src/components/VDefaultsProvider/__tests__/VDefaultsProvider.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@test'
+
+// Components
+import { VDefaultsProvider } from '../VDefaultsProvider'
+import { VCard } from '@/components/VCard'
+
+describe('VDefaultsProvider', () => {
+  it('should apply new defaults', () => {
+    render(() => (
+      <VDefaultsProvider defaults={{ global: { elevation: 10 }, VCard: { color: 'primary' } }}>
+        <VCard title="foo" subtitle="bar" />
+      </VDefaultsProvider>
+    ))
+
+    const card = screen.getByCSS('.v-card')
+    expect(card.classList).toContain('elevation-10')
+    expect(card.classList).toContain('bg-primary')
+  })
+})
