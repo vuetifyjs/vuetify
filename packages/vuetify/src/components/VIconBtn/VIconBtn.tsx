9af9cfa8f28132f1bc5cb04b92ce3175403871b2
John Leider <john@vuetifyjs.com>
2025-03-17 10:40:12 -0500

    feat(VIconBtn): create new component

---
+++ packages/vuetify/src/components/VIconBtn/VIconBtn.tsx
@@ -0,0 +1,98 @@
+// Styles
+import './VIconBtn.scss'
+
+// Components
+import { VIcon } from '@/components/VIcon'
+
+// Composables
+import { makeBorderProps, useBorder } from '@/composables/border'
+import { useBackgroundColor, useTextColor } from '@/composables/color'
+import { makeComponentProps } from '@/composables/component'
+import { makeElevationProps, useElevation } from '@/composables/elevation'
+import { makeRoundedProps, useRounded } from '@/composables/rounded'
+import { makeTagProps } from '@/composables/tag'
+import { makeThemeProps, provideTheme } from '@/composables/theme'
+
+// Utilities
+import { toRef, toRefs } from 'vue'
+import { convertToUnit, genericComponent, propsFactory, useRender } from '@/util'
+
+// Types
+import type { PropType } from 'vue'
+import type { IconValue } from '@/composables/icons'
+
+export const makeVIconBtnProps = propsFactory({
+  bgColor: String,
+  color: String,
+  loading: Boolean,
+  disabled: Boolean,
+  readonly: Boolean,
+  icon: [String, Function, Object] as PropType<IconValue>,
+  rotate: [Number, String],
+  size: [Number, String],
+  text: {
+    type: [String, Number, Boolean],
+    default: undefined,
+  },
+
+  ...makeBorderProps(),
+  ...makeComponentProps(),
+  ...makeElevationProps(),
+  ...makeRoundedProps(),
+  ...makeTagProps(),
+  ...makeThemeProps(),
+}, 'VIconBtn')
+
+export const VIconBtn = genericComponent()({
+  name: 'VIconBtn',
+
+  props: makeVIconBtnProps(),
+
+  setup (props, { slots }) {
+    const { color, bgColor } = toRefs(props)
+
+    const { themeClasses } = provideTheme(props)
+    const { textColorClasses, textColorStyles } = useTextColor(color)
+    const { backgroundColorClasses, backgroundColorStyles } = useBackgroundColor(bgColor)
+    const { borderClasses } = useBorder(props)
+    const { elevationClasses } = useElevation(props)
+    const { roundedClasses } = useRounded(props)
+
+    useRender(() => (
+      <props.tag
+        class={[
+          'v-icon-btn',
+          themeClasses.value,
+          backgroundColorClasses.value,
+          borderClasses.value,
+          elevationClasses.value,
+          roundedClasses.value,
+          textColorClasses.value,
+          props.class,
+        ]}
+        style={[
+          backgroundColorStyles.value,
+          textColorStyles.value,
+          props.style,
+          {
+            '--v-icon-btn-rotate: ': props.rotate,
+            '--v-icon-btn-size': convertToUnit(props.size),
+          },
+        ]}
+      >
+        { slots.default?.() ?? (
+          !props.icon ? props.text : (
+            <VIcon
+              key="icon"
+              icon={ props.icon }
+            />
+          )
+        )}
+      </props.tag>
+    ))
+
+    return {}
+  },
+})
+
+export type VIconBtn = InstanceType<typeof VIconBtn>
