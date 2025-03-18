5d57df291a4674d90454e3e5bb2dc6dcbb1d04db
Kael <kaelwd@gmail.com>
2025-03-18 18:51:55 +1100

    mjs local rules

--- eslint-local-rules.cjs
+++ eslint-local-rules.cjs
@@ -1,11 +1,10 @@
 'use strict'
 
-module.exports = {
-  'no-render-string-reference': require('./scripts/rules/no-render-string-reference.cjs'),
-  'no-components-index': require('./scripts/rules/no-components-index.cjs'),
-  'jsx-condition-key': require('./scripts/rules/jsx-condition-key.cjs'),
-  'jsx-curly-spacing': require('./scripts/rules/jsx-curly-spacing.cjs'),
-  'vitest-global-imports': require('./scripts/rules/vitest-global-imports.cjs'),
-  'sort-imports': require('./scripts/rules/sort-imports.cjs'),
-  'no-nullish-coalescing-in-condition': require('./scripts/rules/no-nullish-coalescing-in-condition.cjs'),
-}
+const path = require('node:path')
+const glob = require('glob')
+
+module.exports = Object.fromEntries(
+  glob.sync('./scripts/rules/*', { cwd: __dirname, dotRelative: true }).map(name => (
+    [path.parse(name).name, require(name).default]
+  ))
+)
