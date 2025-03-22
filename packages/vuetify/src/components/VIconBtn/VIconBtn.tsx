b9681465567e1995c6c639c5f408bf49e29beb50
John Leider <john@vuetifyjs.com>
2025-03-22 00:32:33 -0500

    feat(VIconBtn): add defaults provider support

--- packages/vuetify/src/components/VIconBtn/VIconBtn.tsx
+++ packages/vuetify/src/components/VIconBtn/VIconBtn.tsx
@@ -2,6 +2,7 @@
 import './VIconBtn.scss'
 
 // Components
+import { VDefaultsProvider } from '@/components/VDefaultsProvider'
 import { VIcon } from '@/components/VIcon'
 import { VProgressCircular } from '@/components/VProgressCircular'
 
@@ -15,7 +16,7 @@ import { makeTagProps } from '@/composables/tag'
 import { makeThemeProps, provideTheme } from '@/composables/theme'
 
 // Utilities
-import { toRefs } from 'vue'
+import { toDisplayString, toRefs } from 'vue'
 import { convertToUnit, genericComponent, propsFactory, useRender } from '@/util'
 
 // Types
@@ -65,61 +66,76 @@ export const VIconBtn = genericComponent<VIconBtnSlots>()({
     const { elevationClasses } = useElevation(props)
     const { roundedClasses } = useRounded(props)
 
-    useRender(() => (
-      <props.tag
-        class={[
-          {
-            'v-icon-btn': true,
-            'v-icon-btn--disabled': props.disabled,
-            'v-icon-btn--readonly': props.readonly,
-            'v-icon-btn--loading': props.loading,
-          },
-          themeClasses.value,
-          backgroundColorClasses.value,
-          borderClasses.value,
-          elevationClasses.value,
-          roundedClasses.value,
-          textColorClasses.value,
-          props.class,
-        ]}
-        style={[
-          {
-            '--v-icon-btn-rotate': props.rotate ? convertToUnit(props.rotate, 'deg') : undefined,
-            '--v-icon-btn-height': props.size ? convertToUnit(props.size) : undefined,
-            '--v-icon-btn-width': props.size ? convertToUnit(props.size) : undefined,
-          },
-          backgroundColorStyles.value,
-          textColorStyles.value,
-          props.style,
-        ]}
-        tabindex={ props.disabled || props.readonly ? -1 : 0 }
-      >
-        <div class="v-icon-btn__content">
-          { slots.default?.() ?? (
-            !props.icon ? props.text : (
+    useRender(() => {
+      const hasIcon = !!(props.icon)
+
+      return (
+        <props.tag
+          class={[
+            {
+              'v-icon-btn': true,
+              'v-icon-btn--disabled': props.disabled,
+              'v-icon-btn--readonly': props.readonly,
+              'v-icon-btn--loading': props.loading,
+            },
+            themeClasses.value,
+            backgroundColorClasses.value,
+            borderClasses.value,
+            elevationClasses.value,
+            roundedClasses.value,
+            textColorClasses.value,
+            props.class,
+          ]}
+          style={[
+            {
+              '--v-icon-btn-rotate': props.rotate ? convertToUnit(props.rotate, 'deg') : undefined,
+              '--v-icon-btn-height': props.size ? convertToUnit(props.size) : undefined,
+              '--v-icon-btn-width': props.size ? convertToUnit(props.size) : undefined,
+            },
+            backgroundColorStyles.value,
+            textColorStyles.value,
+            props.style,
+          ]}
+          tabindex={ props.disabled || props.readonly ? -1 : 0 }
+        >
+          <div class="v-icon-btn__content" data-no-activator="">
+            { (!slots.default && hasIcon) ? (
               <VIcon
-                key="icon"
+                key="content-icon"
                 icon={ props.icon }
                 opacity={ props.opacity }
               />
-            )
-          )}
-        </div>
-
-        { !!props.loading && (
-          <span key="loader" class="v-icon-btn__loader">
-            { slots.loader?.() ?? (
-              <VProgressCircular
-                color={ typeof props.loading === 'boolean' ? undefined : props.loading }
-                indeterminate="disable-shrink"
-                width="2"
-                size="20"
-              />
+            ) : (
+              <VDefaultsProvider
+                key="content-defaults"
+                disabled={ !hasIcon }
+                defaults={{
+                  VIcon: {
+                    icon: props.icon,
+                    opacity: props.opacity,
+                  },
+                }}
+              >
+                { slots.default?.() ?? toDisplayString(props.text) }
+              </VDefaultsProvider>
             )}
-          </span>
-        )}
-      </props.tag>
-    ))
+          </div>
+
+          { !!props.loading && (
+            <span key="loader" class="v-icon-btn__loader">
+              { slots.loader?.() ?? (
+                <VProgressCircular
+                  color={ typeof props.loading === 'boolean' ? undefined : props.loading }
+                  indeterminate="disable-shrink"
+                  width="2"
+                  size="20"
+                />
+              )}
+            </span>
+          )}
+        </props.tag>
+      )
+    })
 
     return {}
   },
