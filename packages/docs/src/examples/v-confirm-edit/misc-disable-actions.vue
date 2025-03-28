4173d0ab78b76e34cda91738be1366d89f7ee8bd
John Leider <john@vuetifyjs.com>
2025-03-28 11:49:22 -0500

    docs(misc-disable-actions): clean-up example

--- packages/docs/src/examples/v-confirm-edit/misc-disable-actions.vue
+++ packages/docs/src/examples/v-confirm-edit/misc-disable-actions.vue
@@ -1,54 +1,85 @@
 <template>
   <div>
-    <v-btn-toggle>
-      <v-btn
-        :active="Array.isArray(disabled) && disabled?.includes('cancel')"
-        text="Toggle cancel"
-        @click="onClick('cancel')"
-      ></v-btn>
-      <v-btn
-        :active="Array.isArray(disabled) && disabled?.includes('save')"
-        text="Toggle save"
-        @click="onClick('save')"
-      ></v-btn>
-      <v-btn
-        :active="typeof disabled === 'boolean'"
-        text="Toggle Boolean"
-        @click="disabled = !disabled"
-      ></v-btn>
-      <v-btn
-        :active="disabled === undefined"
-        text="Default"
-        @click="disabled = undefined"
-      ></v-btn>
-    </v-btn-toggle>
+    <div class="d-flex justify-center">
+      <v-btn-toggle
+        density="comfortable"
+        rounded="lg"
+        border
+        divided
+      >
+        <v-btn
+          :active="Array.isArray(disabled) && disabled?.includes('cancel')"
+          text="Toggle cancel"
+          @click="onClick('cancel')"
+        ></v-btn>
 
-    <div class="ma-3">
-      disabled <pre>{{ disabled === undefined ? 'undefined' : disabled }}</pre>
+        <v-btn
+          :active="Array.isArray(disabled) && disabled?.includes('save')"
+          text="Toggle save"
+          @click="onClick('save')"
+        ></v-btn>
+
+        <v-btn
+          :active="typeof disabled === 'boolean'"
+          text="Toggle Boolean"
+          @click="disabled = !disabled"
+        ></v-btn>
+
+        <v-btn
+          :active="disabled === undefined"
+          text="Default"
+          @click="disabled = undefined"
+        ></v-btn>
+      </v-btn-toggle>
     </div>
 
-    <v-confirm-edit v-model="value" :disabled="disabled">
-      <template v-slot:default="{ model: proxyModel, actions }">
-        <v-card class="mx-auto" title="Update Field">
+    <div class="d-flex justify-center align-center py-4 ga-2">
+      <strong>Disabled:</strong>
+      <span
+        class="bg-surface-light rounded rounded-md pa-1"
+        size="small"
+        v-text="disabled === undefined ? 'undefined' : disabled"
+      ></span>
+    </div>
+
+    <v-sheet
+      class="pa-4"
+      color="surface-light"
+      rounded="lg"
+    >
+      <v-confirm-edit
+        v-slot="{ model: proxyModel, actions }"
+        v-model="value"
+        :disabled="disabled"
+      >
+        <v-card class="mx-auto" max-width="400" rounded="lg" title="Update Field">
           <template v-slot:text>
-            <v-text-field v-model="proxyModel.value"></v-text-field>
+            <v-text-field
+              v-model="proxyModel.value"
+              label="Name"
+              prepend-icon="$vuetify"
+              variant="outlined"
+            ></v-text-field>
           </template>
+
+          <v-divider></v-divider>
 
           <template v-slot:actions>
             <v-spacer></v-spacer>
+
             <component :is="actions"></component>
           </template>
         </v-card>
-      </template>
-    </v-confirm-edit>
+      </v-confirm-edit>
+    </v-sheet>
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { ref, shallowRef } from 'vue'
 
   const disabled = ref([])
-  const value = ref('Egg Plant')
+  const value = shallowRef('My Beach Vacation')
 
   function onClick (action) {
     if (!Array.isArray(disabled.value)) {
@@ -68,7 +99,7 @@
     data () {
       return {
         disabled: [],
-        value: 'Egg Plant',
+        value: 'My Beach Vacation',
       }
     },
 
