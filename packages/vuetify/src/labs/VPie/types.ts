9a76d152db2a264605b684b784d9c2aac59a7fa4
J-Sek <J-Sek@users.noreply.github.com>
2025-04-01 19:12:28 +0200

    rename `series` and `segments` to `items`

--- packages/vuetify/src/labs/VPie/types.ts
+++ packages/vuetify/src/labs/VPie/types.ts
@@ -1,7 +1,9 @@
-export interface VPieSeries {
+export interface VPieItem {
   id: any
   color: string
   value: number
   title: string
   pattern?: string
 }
+
+export type TextTemplate<T> = string | ((v: T) => string)
