e139a24bb95ef1bcba8e495ed2bf99a5f4dee3ad
MajesticPotatoe <amajesticpotatoe@gmail.com>
2025-04-02 23:47:04 -0400

    docs(api): reorganize, cleanup descriptions

--- packages/api-generator/src/vscode.ts
+++ packages/api-generator/src/vscode.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { /*capitalize,*/ kebabCase } from './helpers/text'
+import { /* capitalize, */ kebabCase } from './helpers/text'
 import type { ComponentData, DirectiveData } from './types'
 import pkg from '../package.json' with { type: 'json' }
 
@@ -7,105 +7,92 @@ export const createVSCodeApi = (componentData: ComponentData[], directiveData: D
   const getDocUrl = (cmp: string, heading?: string) =>
     `https://vuetifyjs.com/api/${cmp}` + (heading ? `#${heading}` : '')
 
-  /*
-  const createTypedEntity = (name: string, type: string) => {
-    return {
-      name,
-      type,
-    }
-  }
-  */
+  const descriptionHeader = '**Description**\n'
+  const noDescription = '*no description*'
+  const templateAttributes: Record<string, any> = {}
 
   const createTag = (component: ComponentData) => {
     const createTagSlot = ([name, slot]: [string, any]) => {
-      return {
-        name: `#${kebabCase(name)}`,
-        description: slot.description.en || '',
-        references: [
-          {
-            name: `#${kebabCase(name)} API docs`,
-            url: getDocUrl(component.pathName, `slots-${kebabCase(name)}`)
-          }
-        ],
-        // not in schema
-        /*
-        pattern: undefined,
-        'vue-properties': slot.properties && Object.entries(slot.properties ?? {}).map(([name, prop]) => createTypedEntity(name, (prop as any).formatted)),
-        */
-      }
+      return ['#', 'v-slot:'].map(prefix => (
+        {
+          name: `${prefix}${kebabCase(name)}`,
+          description: [
+            descriptionHeader,
+            `>${slot.description.en || noDescription}\n`,
+            '**Properties:**\n',
+            ...slot.properties
+              ? Object.entries(slot.properties ?? {}).map(([name, prop]) => `>**${name}:**, ${(prop as any).formatted}`)
+              : ['>*no properties*'],
+          ].join('\n') || '',
+          references: [
+            {
+              name: `${prefix}${kebabCase(name)} API docs`,
+              url: getDocUrl(component.pathName, `slots-${kebabCase(name)}`),
+            },
+          ],
+        }
+      ))
     }
 
     const createTagEvent = ([name, event]: [string, any]) => {
+      const argument = event.formatted
       return {
-        name:`@${kebabCase(name)}`,
-        description: event.description.en || '',
+        name: `@${kebabCase(name)}`,
+        description: [
+          descriptionHeader,
+          `>${event.description.en || noDescription}`,
+          ...argument ? ['', `* **argument:** \`${argument}\``] : [],
+        ].join('\n') || '',
         references: [
           {
             name: `@${kebabCase(name)} API docs`,
-            url: getDocUrl(component.pathName, `events-${kebabCase(name)}`)
-          }
+            url: getDocUrl(component.pathName, `events-${kebabCase(name)}`),
+          },
         ],
-        // not in schema
-        // arguments: [createTypedEntity('argument', event.formatted)],
       }
     }
 
-    /*
-    const createTagValue = (type: string) => {
-      return {
-        kind: 'expression',
-        type: type?.trim(),
-      }
-    }
-    */
-
-    const createTagAttribute = ([name, prop]: [string, any]) => {
+    const createTagProp = ([name, prop]: [string, any]) => {
+      const defaultValue = typeof prop.default !== 'string' ? JSON.stringify(prop.default) : prop.default
       return {
         name: kebabCase(name),
-        description: prop.description.en || '',
+        description: [
+          descriptionHeader,
+          `>${prop.description.en || noDescription}\n`,
+          `* **default:** \`${defaultValue}\``,
+        ].join('\n') || '',
         references: [
           {
             name: `${kebabCase(name)} API docs`,
-            url: getDocUrl(component.fileName, `props-${kebabCase(name)}`)
-          }
+            url: getDocUrl(component.fileName, `props-${kebabCase(name)}`),
+          },
         ],
-        // not in schema - work into description or toss
-        /*
-        default: typeof prop.default !== 'string' ? JSON.stringify(prop.default) : prop.default,
-        required: undefined, // TODO: implement this
-        value: createTagValue(prop.formatted),
-        type: prop.formatted === 'boolean' ? 'boolean' : undefined, // this is deprecated but should be const 'boolean' for compatibility with 2019.2
-        */
       }
     }
 
-    return {
-      name: component.displayName,
-      description: '', // TODO: we should probably include component description in locale files
-      attributes: [
-        ...Object.entries(component.props ?? {}).map(createTagAttribute), // props
-        ...Object.entries(component.events ?? {}).map(createTagEvent), // events
-        ...Object.entries(component.slots ?? {}).map(createTagSlot) // slots
-      ],
-      references: [
-        {
-          name: `${component.displayName} docs`,
-          url: getDocUrl(component.pathName)
-        }
-      ],
-      // not in schema - work into description or toss
-      /*
-      aliases: undefined, // TODO: are we using this? deprecated name changes?
-      source: {
-        module: './src/components/index.ts',
-        symbol: component.displayName,
-      },
-      'vue-model': { // TODO: we should expose this in api data if we can
-        prop: 'modelValue',
-        event: 'update:modelValue',
-      },
-      */
+    // slots
+    for (const [name, slot] of Object.entries(component.slots ?? {})) {
+      if (!templateAttributes?.[name]) { templateAttributes[name] = createTagSlot([name, slot]) }
     }
+    return [
+      ...[component.displayName, kebabCase(component.displayName)].map(propName => (
+        {
+          name: propName,
+          description: noDescription, // TODO: include component description in locale files
+          attributes: [
+            ...Object.entries(component.props ?? {}).map(createTagProp), // props
+            ...Object.entries(component.events ?? {}).map(createTagEvent), // events
+          ],
+          // TODO: add reference to api page
+          references: [
+            {
+              name: `${propName} docs`,
+              url: getDocUrl(component.pathName),
+            },
+          ],
+        }
+      )),
+    ]
   }
 
   const createAttribute = (directive: DirectiveData) => {
@@ -127,12 +114,15 @@ export const createVSCodeApi = (componentData: ComponentData[], directiveData: D
     const createAttributeVueModifier = ([name, modifier]: [string, any]) => {
       return {
         name,
-        description: modifier.description.en || '',
+        description: [
+          descriptionHeader,
+          `>${modifier.description.en || noDescription}`,
+        ].join('\n') || '',
         references: [
           {
             name: `${name} API docs`,
-            url: getDocUrl(directive.pathName, 'modifiers')
-          }
+            url: getDocUrl(directive.pathName, 'modifiers'),
+          },
         ],
       }
     }
@@ -148,18 +138,21 @@ export const createVSCodeApi = (componentData: ComponentData[], directiveData: D
 
     return {
       name: directive.displayName,
-      description: directive?.value?.description?.en || '',
+      description: [
+        descriptionHeader,
+        `>${directive?.value?.description?.en || noDescription}`,
+      ].join('\n') || '',
       references: [
         {
           name: `${directive.displayName} docs`,
-          url: getDocUrl(directive.pathName)
-        }
+          url: getDocUrl(directive.pathName),
+        },
       ],
       attributes: [
         // will need to add to description
-        //...(directive.argument ? [createAttributeVueArgument(directive.argument)] : []),
-        ...(directive.modifiers ? Object.entries(directive.modifiers).map(createAttributeVueModifier) : [])
-      ]
+        // ...(directive.argument ? [createAttributeVueArgument(directive.argument)] : []),
+        ...(directive.modifiers ? Object.entries(directive.modifiers).map(createAttributeVueModifier) : []),
+      ],
       // not in schema - work into description or toss
       /*
       aliases: undefined,
@@ -174,7 +167,17 @@ export const createVSCodeApi = (componentData: ComponentData[], directiveData: D
     }
   }
 
-  const tags = componentData.map(createTag)
+  const tags = []
+  for (const component of componentData) {
+    tags.push(...createTag(component))
+  }
+  tags.push({
+    name: 'template',
+    attributes: Object.values(templateAttributes).reduce((k, a) => {
+      a.push(...k)
+      return a
+    }, []),
+  })
   const globalAttributes = directiveData.map(createAttribute)
 
   const vscode = {
@@ -184,6 +187,24 @@ export const createVSCodeApi = (componentData: ComponentData[], directiveData: D
     version: pkg.version,
     tags,
     globalAttributes,
+    // value set example structure, can be applied via valueSet / values on props
+    /*
+    valueSets: [
+      {
+        name: 'x',
+        values: [
+          {
+            name: 'xvals',
+            description: 'x values',
+          },
+          {
+            name: 'xval',
+            description: 'x value',
+          },
+        ],
+      },
+    ],
+    */
   }
 
   fs.writeFileSync('dist/vscode.json', JSON.stringify(vscode, null, 2))
