3a2c846c9ce1985647aff78100de325efe6982db
J-Sek <J-Sek@users.noreply.github.com>
2025-05-07 12:19:15 +0200

    fix(VAutocomplete): re-evaluate dirty on external change (#21344)
    fixes #20718

--- packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
+++ packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
@@ -403,6 +403,12 @@ export const VAutocomplete = genericComponent<new <
       }
     })
 
+    watch(model, value => {
+      if (!props.multiple && !hasSelectionSlot.value) {
+        search.value = value[0]?.title ?? ''
+      }
+    })
+
     useRender(() => {
       const hasList = !!(
         (!props.hideNoData || displayItems.value.length) ||
