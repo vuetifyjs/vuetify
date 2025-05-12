99331d6a9a0a661723761c80cd1570b2bf9b9d1c
J-Sek <J-Sek@users.noreply.github.com>
2025-05-12 20:48:27 +0200

    fix(VDateInput): accept value when picker is hidden (#21273)

--- packages/vuetify/src/labs/VDateInput/__tests__/VDateInput.spec.browser.tsx
+++ packages/vuetify/src/labs/VDateInput/__tests__/VDateInput.spec.browser.tsx
@@ -2,7 +2,9 @@
 import { VDateInput } from '../VDateInput'
 
 // Utilities
+import { commands, render, screen, userEvent } from '@test'
 import { mount } from '@vue/test-utils'
+import { ref } from 'vue'
 import { createVuetify } from '@/framework'
 
 describe('VDateInput', () => {
@@ -20,6 +22,27 @@ describe('VDateInput', () => {
       ...options,
     })
   }
+
+  it('accepts keyboard input even if the picker is hidden', async () => {
+    const model = ref<Date | null>(null)
+    const { element } = render(() => <VDateInput v-model={ model.value } />)
+
+    await userEvent.click(element)
+    await commands.waitStable('.v-picker')
+    expect(screen.getByCSS('.v-picker')).toBeVisible()
+
+    await userEvent.keyboard('{Escape}') // hide picker, but keep the focus
+
+    await commands.waitStable('.v-picker')
+    expect(screen.getByCSS('.v-picker')).not.toBeVisible()
+
+    const input = screen.getByCSS('input') as HTMLInputElement
+    await userEvent.type(input, '02/20/2022{Enter}')
+
+    expect(model.value).toBeDefined()
+    const formatter = new Intl.DateTimeFormat('en-US', { dateStyle: 'medium' })
+    expect(formatter.format(model.value!)).toBe('Feb 20, 2022')
+  })
 
   describe('parseDateString', () => {
     const testCases = [
