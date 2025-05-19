cf35c2b0d9b33b00f4fb6fa5e0b22ff8f3f13d13
J-Sek <J-Sek@users.noreply.github.com>
2025-05-20 01:06:36 +0200

    few more tests

--- packages/vuetify/src/labs/VPie/__tests__/pie-arc.spec.ts
+++ packages/vuetify/src/labs/VPie/__tests__/pie-arc.spec.ts
@@ -6,8 +6,10 @@ import { ref } from 'vue'
 
 describe('usePieArc', () => {
   it.each([
-    [{ innerCut: 0, value: 25 }, { x: 100, y: 50, arcWidth: 25, sliceRadius: 0 }],
-    [{ innerCut: 0, value: 66 }, { x: 7.78, y: 76.79, arcWidth: 25, sliceRadius: 0 }],
+    [{ innerCut: 0, value: 25 }, { x: 100, y: 50, arcWidth: 50, sliceRadius: 0 }],
+    [{ innerCut: 25, value: 25 }, { x: 100, y: 50, arcWidth: 37.5, sliceRadius: 6.25 }],
+    [{ innerCut: 75, value: 25 }, { x: 100, y: 50, arcWidth: 12.5, sliceRadius: 18.75 }],
+    [{ innerCut: 0, value: 66 }, { x: 7.78, y: 76.79, arcWidth: 50, sliceRadius: 0 }],
   ])('should correctly calculate arc properties', (props, expected) => {
     const isHovering = ref(false)
     const { x, y, arcWidth, sliceRadius } = usePieArc({
