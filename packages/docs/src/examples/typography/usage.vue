ed5265a217f6806358f9e58ba53724f088b4f756
Toure Maka (DIN) <maka.toure@etat.ge.ch>
2025-05-20 15:10:20 +0200

    feat: add VTypography component

---
+++ packages/docs/src/examples/typography/usage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-flex flex-column gap-4">
+    <VTypography
+      text="display-large"
+      color="primary"
+    >
+      Display Large
+    </VTypography>
+
+    <VTypography
+      text="headline-large"
+      color="secondary"
+    >
+      Headline Large
+    </VTypography>
+
+    <VTypography
+      text="body-medium"
+      color="error"
+    >
+      Body Medium
+    </VTypography>
+
+    <VTypography
+      text="label-small"
+      color="success"
+    >
+      Label Small
+    </VTypography>
+  </div>
+</template>
+
+<script setup>
+import { VTypography } from 'vuetify/components'
+</script>
