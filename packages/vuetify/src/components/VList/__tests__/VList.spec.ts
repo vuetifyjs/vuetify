139eada51b73bcbed52f96eb433392416bc06122
Ishan Subedi <ikushum@gmail.com>
2025-05-21 00:34:35 +0545

    chore(VList): add test

--- packages/vuetify/src/components/VList/__tests__/VList.spec.ts
+++ packages/vuetify/src/components/VList/__tests__/VList.spec.ts
@@ -20,4 +20,17 @@ describe('VList', () => {
 
     expect(wrapper.html()).toMatchSnapshot()
   })
+
+  it.each([
+    { selectable: true, activatable: false, expected: 'listbox' },
+    { selectable: false, activatable: true, expected: 'listbox' },
+    { selectable: true, activatable: true, expected: 'listbox' },
+    { selectable: false, activatable: false, expected: 'list' },
+  ])('should have role="$expected" when selectable is $selectable and activatable is $activatable', ({ selectable, activatable, expected }) => {
+    const wrapper = mountFunction({
+      props: { selectable, activatable },
+    })
+
+    expect(wrapper.attributes('role')).toBe(expected)
+  })
 })
