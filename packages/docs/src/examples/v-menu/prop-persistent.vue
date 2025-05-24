a0fffee1f6b7c346a32a8b1ae69226adc4aed8ab
briones@ualberta.ca <briones@ualberta.ca>
2025-05-24 00:01:05 -0600

    docs(VMenu): more examples and fixes for non responsive examples

---
+++ packages/docs/src/examples/v-menu/prop-persistent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center">
+    <v-menu persistent>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props"> Dropdown </v-btn>
+      </template>
+
+      <v-list :items="items"></v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script setup>
+  const items = [
+    { title: 'Menu Item 1', value: 1 },
+    { title: 'Menu Item 2', value: 2 },
+    { title: 'Menu Item 3', value: 3 },
+    { title: 'Menu Item 4', value: 4 },
+  ]
+</script>
