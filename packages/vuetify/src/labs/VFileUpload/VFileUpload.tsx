fidelity-contributions/vuetifyjs-vuetify

fix(VFileUpload): only filter if accept type is passed
fix(VFileInput): same with VFileUpload filter if accept type passed
refactor(helper): return empty if no files passed
refactor(helper): return files as array if no accept type passed

packages/vuetify/src/labs/VFileUpload/VFileUpload.tsx
@@ -147,7 +147,7 @@ export const VFileUpload = genericComponent<VFileUploadSlots>()({
       }
 
       if (acceptType) {
-        filesArray = filterFilesByAcceptType(acceptType, files)
+        filesArray = filterFilesByAcceptType(files, acceptType)
       }
 
       if (!props.multiple) {
@@ -183,9 +183,9 @@ export const VFileUpload = genericComponent<VFileUploadSlots>()({
       const acceptType = inputRef.value?.accept
       const files = (e.target as HTMLInputElement)?.files
 
-      if (!files || !acceptType) return
+      if (!files) return
 
-      model.value = filterFilesByAcceptType(acceptType, files)
+      model.value = filterFilesByAcceptType(files, acceptType)
     }
 
     useRender(() => {

vuetifyjs/vuetify

packages/vuetify/src/labs/VFileUpload/VFileUpload.tsx
@@ -134,7 +134,7 @@ export const VFileUpload = genericComponent<VFileUploadSlots>()({
       if (!initialFiles || initialFiles.length === 0 || !inputRef.value) return
 
       if (acceptType) {
-        files = filterFilesByAcceptType(acceptType, initialFiles)
+        files = filterFilesByAcceptType(initialFiles, acceptType)
       }
 
       const dataTransfer = new DataTransfer()
@@ -169,9 +169,9 @@ export const VFileUpload = genericComponent<VFileUploadSlots>()({
       const acceptType = inputRef.value?.accept
       const files = (e.target as HTMLInputElement)?.files
 
-      if (!files || !acceptType) return
+      if (!files) return
 
-      model.value = filterFilesByAcceptType(acceptType, files)
+      model.value = filterFilesByAcceptType(files, acceptType)
     }
 
     useRender(() => {