ff466635dd0731d98da46c7f3b6821411bc44c77
Ishan Subedi <ikushum@gmail.com>
2025-06-05 12:31:10 +0545

    docs(VMaskInput): add docs for VMaskInput

---
+++ packages/docs/src/examples/v-mask-input/card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-mask-input
+      v-model="model"
+      label="Credit Card"
+      mask="credit-card"
+      placeholder="#### - #### - #### - ####"
+    ></v-mask-input>
+  </div>
+</template>
+
+<script setup>
+  import { shallowRef } from 'vue'
+
+  const model = shallowRef(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      model: null,
+    }),
+  }
+</script>
