dacd8b890f1eca167509a763f8b60fb7bd353c6c
Jens Müller <jensi.mueller74@gmail.con>
2025-06-09 16:52:04 +0200

    Apply icon color to clearable icon

--- packages/vuetify/src/components/VField/VField.tsx
+++ packages/vuetify/src/components/VField/VField.tsx
@@ -362,7 +362,7 @@ export const VField = genericComponent<new <T>(
                       onFocus={ focus }
                       onBlur={ blur }
                       tabindex={ -1 }
-                      color={ iconColor.value}
+                      color={ iconColor.value }
                     />
                   )}
                 </VDefaultsProvider>
