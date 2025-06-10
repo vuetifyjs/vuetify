be871d456437f877773471999f027cabbe06018c
Jens Müller <jensi.mueller74@gmail.con>
2025-06-10 20:15:44 +0200

    Add aria-label attribute to Vtooltip

--- packages/vuetify/src/components/VTooltip/VTooltip.tsx
+++ packages/vuetify/src/components/VTooltip/VTooltip.tsx
@@ -109,7 +109,7 @@ export const VTooltip = genericComponent<OverlaySlots>()({
           persistent
           role="tooltip"
           activatorProps={ activatorProps.value }
-          aria-label={ props.text}
+          aria-label={ props.text }
           _disableGlobalStack
           { ...scopeId }
         >
