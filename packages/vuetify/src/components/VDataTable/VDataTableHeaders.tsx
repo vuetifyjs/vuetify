415c267d057ad49334d9801c4fdb0c6aa9dcdcd1
J-Sek <J-Sek@users.noreply.github.com>
2025-07-03 14:22:47 +0200

    feat(VDataTable): support `fixed: 'end'` columns (#21665)
    resolves #20020
    resolves #21153

--- packages/vuetify/src/components/VDataTable/VDataTableHeaders.tsx
+++ packages/vuetify/src/components/VDataTable/VDataTableHeaders.tsx
@@ -61,7 +61,6 @@ export const makeVDataTableHeadersProps = propsFactory({
   color: String,
   disableSort: Boolean,
   fixedHeader: Boolean,
-  lastFixed: Boolean,
   multiSort: Boolean,
   sortAscIcon: {
     type: IconValue,
@@ -95,12 +94,16 @@ export const VDataTableHeaders = genericComponent<VDataTableHeadersSlots>()({
     const { loaderClasses } = useLoader(props)
 
     function getFixedStyles (column: InternalDataTableHeader, y: number): CSSProperties | undefined {
-      if (!(props.sticky || props.fixedHeader) && !(column.fixed || column.lastFixed)) return undefined
+      if (!(props.sticky || props.fixedHeader) && !column.fixed) return undefined
+
+      const fixedSide = typeof column.fixed === 'string' ? column.fixed
+        : column.fixed ? 'start'
+        : 'none'
 
       return {
         position: 'sticky',
-        left: column.fixed || column.lastFixed ? convertToUnit(column.fixedOffset) : undefined,
-        right: column.lastFixed ? convertToUnit(column.fixedOffset ?? 0) : undefined,
+        left: fixedSide === 'start' ? convertToUnit(column.fixedOffset) : undefined,
+        right: fixedSide === 'end' ? convertToUnit(column.fixedEndOffset) : undefined,
         top: (props.sticky || props.fixedHeader) ? `calc(var(--v-table-header-height) * ${y})` : undefined,
       }
     }
@@ -169,6 +172,7 @@ export const VDataTableHeaders = genericComponent<VDataTableHeadersSlots>()({
           fixed={ column.fixed }
           nowrap={ column.nowrap }
           lastFixed={ column.lastFixed }
+          firstFixedEnd={ column.firstFixedEnd }
           noPadding={ noPadding }
           tabindex={ column.sortable ? 0 : undefined }
           onClick={ column.sortable ? () => toggleSort(column) : undefined }
