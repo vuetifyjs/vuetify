ea7d80b5fbd9bdb82e41417aadd5719cb1850fd7
Kael <kaelwd@gmail.com>
2025-07-17 18:50:41 +1000

    chore(ripple): fix tests

--- packages/vuetify/src/directives/ripple/__tests__/ripple.spec.ts
+++ packages/vuetify/src/directives/ripple/__tests__/ripple.spec.ts
@@ -56,11 +56,11 @@ describe('v-ripple', () => {
     expect(wrapper.find('.v-ripple__container').exists()).toBe(false)
   })
 
-  it.each(['enter', 'space'] as const)('should trigger ripple on %s key press', key => {
+  it.each(['Enter', 'Space'] as const)('should trigger ripple on %s key press', key => {
     vi.useFakeTimers()
     const wrapper = mount(testComponent)
 
-    const keydownEvent = new KeyboardEvent('keydown', { keyCode: keyCodes[key] })
+    const keydownEvent = new KeyboardEvent('keydown', { key })
     wrapper.element.dispatchEvent(keydownEvent)
 
     expect(wrapper.find('.v-ripple__container').exists()).toBe(true)
@@ -97,7 +97,7 @@ describe('v-ripple', () => {
   it('should hide ripple on blur if keyboardRipple is true', () => {
     vi.useFakeTimers()
     const wrapper = mount(testComponent)
-    const keydownEvent = new KeyboardEvent('keydown', { keyCode: 13 })
+    const keydownEvent = new KeyboardEvent('keydown', { key: 'Enter' })
     wrapper.element.dispatchEvent(keydownEvent)
 
     expect(wrapper.find('.v-ripple__container').exists()).toBe(true)
