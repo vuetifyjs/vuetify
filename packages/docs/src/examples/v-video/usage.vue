62b12a09c414efb42b4cf1acd22e274dc6b9b62e
J-Sek <J-Sek@users.noreply.github.com>
2025-07-23 13:44:52 +0200

    docs: include `no-fullscreen` in usage example

--- packages/docs/src/examples/v-video/usage.vue
+++ packages/docs/src/examples/v-video/usage.vue
@@ -13,8 +13,9 @@
       <v-select v-model="theme" :items="['light', 'dark']" label="Theme" clearable></v-select>
       <v-select v-model="color" :items="colorOptions" label="Color" clearable></v-select>
       <v-select v-model="elevation" :items="[4, 8, 12]" label="Elevation" clearable></v-select>
-      <v-checkbox v-model="hidePlay" :disabled="isHidden" label="Hide play"></v-checkbox>
-      <v-checkbox v-model="hideVolume" :disabled="isHidden" label="Hide volume"></v-checkbox>
+      <v-checkbox v-if="!isHidden" v-model="hidePlay" label="Hide play"></v-checkbox>
+      <v-checkbox v-if="!isHidden" v-model="hideVolume" label="Hide volume"></v-checkbox>
+      <v-checkbox v-if="!isHidden" v-model="noFullscreen" label="No fullscreen"></v-checkbox>
     </template>
   </ExamplesUsageExample>
 </template>
@@ -26,6 +27,7 @@
   const model = shallowRef('default')
   const hidePlay = shallowRef(false)
   const hideVolume = shallowRef(false)
+  const noFullscreen = shallowRef(false)
   const theme = shallowRef(null)
   const color = shallowRef(null)
   const elevation = shallowRef(null)
@@ -46,6 +48,7 @@
       elevation: elevation.value || undefined,
       'hide-play': (!isHidden.value && hidePlay.value) || undefined,
       'hide-volume': (!isHidden.value && hideVolume.value) || undefined,
+      noFullscreen: (!isHidden.value && noFullscreen.value) || undefined,
       'controls-variant': controlsVariants.includes(model.value) ? model.value : undefined,
       image: 'https://jsek.work/vt-sunflowers.jpg',
       src: 'https://jsek.work/vt-sunflowers.mp4',
