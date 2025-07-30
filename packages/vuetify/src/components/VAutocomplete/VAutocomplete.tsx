b8fdfd4b3e97228f2739c8fa58aec1612b122cb3
Tommaso Gangemi <gangemi.tommaso@gmail.com>
2025-07-30 17:02:43 +0200

    fix(VAutocomplete): correctly filter using filterKeys prop
    Closes #21799

--- packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
+++ packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
@@ -144,7 +144,12 @@ export const VAutocomplete = genericComponent<new <
         : model.value.length
     })
     const form = useForm(props)
-    const { filteredItems, getMatches } = useFilter(props, items, () => isPristine.value ? '' : search.value)
+    const { filteredItems, getMatches } = useFilter(
+      props,
+      items,
+      () => isPristine.value ? '' : search.value,
+      { transform: item => item.raw }
+    )
 
     const displayItems = computed(() => {
       if (props.hideSelected) {
