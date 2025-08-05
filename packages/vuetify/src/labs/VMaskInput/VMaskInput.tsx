a687f7a38f4377421f47660564fe7f02d6e0231c
Ishan Subedi <17315781+ikushum@users.noreply.github.com>
2025-08-06 01:12:41 +0545

    feat(mask): create useMask composable (#21736)

--- packages/vuetify/src/labs/VMaskInput/VMaskInput.tsx
+++ packages/vuetify/src/labs/VMaskInput/VMaskInput.tsx
@@ -3,11 +3,11 @@ import { makeVTextFieldProps, VTextField } from '@/components/VTextField/VTextFi
 
 // Composables
 import { forwardRefs } from '@/composables/forwardRefs'
-import { makeMaskProps, useMask } from '@/composables/mask'
+import { isMaskDelimiter, makeMaskProps, useMask } from '@/composables/mask'
 import { useProxiedModel } from '@/composables/proxiedModel'
 
 // Utilities
-import { computed, onBeforeMount, ref, toRef } from 'vue'
+import { computed, onBeforeMount, ref, shallowRef, toRef } from 'vue'
 import { genericComponent, propsFactory, useRender } from '@/util'
 
 // Types
@@ -16,6 +16,7 @@ import type { VTextFieldSlots } from '@/components/VTextField/VTextField'
 export type VMaskInputSlots = VTextFieldSlots
 
 export const makeVMaskInputProps = propsFactory({
+  returnMaskedValue: Boolean,
   ...makeVTextFieldProps(),
   ...makeMaskProps(),
 }, 'VMaskInput')
@@ -32,7 +33,10 @@ export const VMaskInput = genericComponent<VMaskInputSlots>()({
   setup (props, { slots, emit }) {
     const vTextFieldRef = ref<VTextField>()
 
-    const { maskText, updateRange, unmaskText } = useMask(props, vTextFieldRef)
+    const selection = shallowRef(0)
+    const lazySelection = shallowRef(0)
+
+    const mask = useMask(props)
     const returnMaskedValue = computed(() => props.mask && props.returnMaskedValue)
 
     const model = useProxiedModel(
@@ -40,33 +44,66 @@ export const VMaskInput = genericComponent<VMaskInputSlots>()({
       'modelValue',
       undefined,
       // Always display masked value in input when mask is applied
-      val => props.mask ? maskText(unmaskText(val)) : val,
+      val => props.mask ? mask.mask(mask.unmask(val)) : val,
       val => {
         if (props.mask) {
-          const valueBeforeChange = unmaskText(model.value)
+          const valueBeforeChange = mask.unmask(model.value)
           // E.g. mask is #-# and the input value is '2-23'
           // model-value should be enforced to '2-2'
-          const enforcedMaskedValue = maskText(unmaskText(val))
-          const newUnmaskedValue = unmaskText(enforcedMaskedValue)
+          const enforcedMaskedValue = mask.mask(mask.unmask(val))
+          const newUnmaskedValue = mask.unmask(enforcedMaskedValue)
 
           if (newUnmaskedValue === valueBeforeChange) {
             vTextFieldRef.value!.value = enforcedMaskedValue
           }
           val = newUnmaskedValue
           updateRange()
-          return returnMaskedValue.value ? maskText(val) : val
+          return returnMaskedValue.value ? mask.mask(val) : val
         }
         return val
       },
     )
 
-    const validationValue = toRef(() => returnMaskedValue.value ? model.value : unmaskText(model.value))
+    const validationValue = toRef(() => returnMaskedValue.value ? model.value : mask.unmask(model.value))
 
     onBeforeMount(() => {
       if (props.returnMaskedValue) {
         emit('update:modelValue', model.value)
       }
     })
+
+    function setCaretPosition (newSelection: number) {
+      selection.value = newSelection
+      vTextFieldRef.value && vTextFieldRef.value.setSelectionRange(selection.value, selection.value)
+    }
+
+    function resetSelections () {
+      if (!vTextFieldRef.value?.selectionEnd) return
+
+      selection.value = vTextFieldRef.value.selectionEnd
+      lazySelection.value = 0
+
+      for (let index = 0; index < selection.value; index++) {
+        isMaskDelimiter(vTextFieldRef.value.value[index]) || lazySelection.value++
+      }
+    }
+
+    function updateRange () {
+      if (!vTextFieldRef.value) return
+      resetSelections()
+
+      let selection = 0
+      const newValue = vTextFieldRef.value.value
+
+      if (newValue) {
+        for (let index = 0; index < newValue.length; index++) {
+          if (lazySelection.value <= 0) break
+          isMaskDelimiter(newValue[index]) || lazySelection.value--
+          selection++
+        }
+      }
+      setCaretPosition(selection)
+    }
 
     useRender(() => {
       const textFieldProps = VTextField.filterProps(props)
