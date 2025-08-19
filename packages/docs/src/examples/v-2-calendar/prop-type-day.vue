6e8225c44b0ab52b87690ab36f687c2ba15915bc
Kael <kaelwd@gmail.com>
2025-08-19 17:56:03 +1000

    add docs

---
+++ packages/docs/src/examples/v-2-calendar/prop-type-day.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-sheet height="400">
+        <v2-calendar
+          color="primary"
+          type="day"
+        >
+          <template v-slot:day-header="{ present }">
+            <div v-if="present" class="text-center">
+              Today
+            </div>
+          </template>
+
+          <template v-slot:interval="{ hour }">
+            <div class="text-center">
+              {{ hour }} o'clock
+            </div>
+          </template>
+        </v2-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({}),
+  }
+</script>
