9d7fdd12d2ac2b1996b0fc945e8caf3d4724c1ca
J-Sek <J-Sek@users.noreply.github.com>
2025-08-30 23:00:51 +0200

    chore: improved usage example

--- packages/docs/src/examples/v-command-palette/usage.vue
+++ packages/docs/src/examples/v-command-palette/usage.vue
@@ -1,33 +1,49 @@
 <template>
-  <div class="text-center">
-    <v-btn
-      color="primary"
-      @click="dialog = true"
-    >
-      Open Command Palette
-    </v-btn>
+  <ExamplesUsageExample
+    :code="code"
+    :name="name"
+    :options="[]"
+    :script="script"
+  >
+    <div class="text-center">
+      <v-btn
+        color="primary"
+        text="Open"
+        @click="open = true"
+      ></v-btn>
+      <br>
+      <br>
+      <small><v-hotkey :keys="hotkey" inline></v-hotkey></small>
 
-    <v-command-palette
-      v-model="dialog"
-      :items="items"
-    ></v-command-palette>
+      <v-command-palette
+        v-model="open"
+        :hotkey="hotkey"
+        :items="items"
+      ></v-command-palette>
 
-    <v-snackbar v-model="snackbar" :timeout="2000">
-      {{ snackbarText }}
-    </v-snackbar>
-  </div>
+      <v-snackbar :key="snackbarId" v-model="snackbar" :timeout="2000">
+        {{ snackbarText }}
+      </v-snackbar>
+    </div>
+    <template v-slot:configuration>
+      <v-text-field v-model="hotkey" label="Hotkey"></v-text-field>
+    </template>
+  </ExamplesUsageExample>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  const name = 'v-command-palette'
+  const hotkey = shallowRef('alt+g')
 
-  const dialog = ref(false)
-  const snackbar = ref(false)
-  const snackbarText = ref('')
+  const open = shallowRef(false)
+  const snackbar = shallowRef(false)
+  const snackbarText = shallowRef('')
+  const snackbarId = shallowRef(0)
 
   function showSnackbar (text) {
     snackbarText.value = text
     snackbar.value = true
+    snackbarId.value++
   }
 
   const items = [
@@ -50,50 +66,46 @@
       handler: () => showSnackbar('Showing Help'),
     },
   ]
-</script>
 
-<script>
-  import { ref } from 'vue'
+  const script = computed(() => {
+    return `
+<script setup>
+  import { shallowRef } from 'vue'
 
-  export default {
-    setup () {
-      const dialog = ref(false)
-      const snackbar = ref(false)
-      const snackbarText = ref('')
+  const open = shallowRef(false)
 
-      function showSnackbar (text) {
-        snackbarText.value = text
-        snackbar.value = true
-      }
-
-      const items = [
-        {
-          id: 'home',
-          title: 'Home',
-          prependIcon: 'mdi-home',
-          handler: () => showSnackbar('Navigating to Home'),
-        },
-        {
-          id: 'settings',
-          title: 'Settings',
-          prependIcon: 'mdi-cog',
-          handler: () => showSnackbar('Opening Settings'),
-        },
-        {
-          id: 'help',
-          title: 'Help',
-          prependIcon: 'mdi-help-circle',
-          handler: () => showSnackbar('Showing Help'),
-        },
-      ]
-
-      return {
-        dialog,
-        snackbar,
-        snackbarText,
-        showSnackbar,
-        items,
-      }
+  const items = [
+    {
+      id: 'home',
+      title: 'Home',
+      prependIcon: 'mdi-home',
+      handler: () => console.log('Navigating to Home'),
     },
-  }
+    {
+      id: 'settings',
+      title: 'Settings',
+      prependIcon: 'mdi-cog',
+      handler: () => console.log('Opening Settings'),
+    },
+    {
+      id: 'help',
+      title: 'Help',
+      prependIcon: 'mdi-help-circle',
+      handler: () => console.log('Showing Help'),
+    },
+  ]
+<` + '/script>'
+  })
+
+  const code = computed(() => {
+    return `<div class="text-center">
+  <v-btn color="primary" text="Open" @click="open = true"></v-btn>
+
+  <v-command-palette
+    v-model="open"
+    hotkey="${hotkey.value}"
+    :items="items"
+  ></v-command-palette>
+</div>`
+  })
 </script>
