140ee999fd67a9a9e722b1f98c51ad06e349b2d8
antonhansson0624 <anton0424handsson@gmail.com>
2025-09-02 15:52:46 -0700

    docs: add renderless mode example for v-virtual-scroll with v-table

---
+++ packages/docs/src/examples/v-virtual-scroll/renderless.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-table>
+        <thead>
+          <tr>
+            <th>Col 1</th>
+            <th>Col 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <v-virtual-scroll :items="items" renderless>
+            <template v-slot:default="{ item, itemRef }">
+              <tr :ref="itemRef">
+                <td>{{ item }}</td>
+                <td>{{ item }}</td>
+              </tr>
+            </template>
+          </v-virtual-scroll>
+        </tbody>
+      </v-table>
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const items = ref(Array(1000).fill('col'))
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      items: Array(1000).fill('col'),
+    }),
+  }
+</script>
