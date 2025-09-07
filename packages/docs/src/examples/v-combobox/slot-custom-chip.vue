3d6ebcb2d98f495ec5fc4c1b61a97f8ef383121d
Capstone0624 <anton0424handsson@gmail.com>
2025-09-07 12:07:05 -0700

    docs(VCombobox): example with correct use of chip slot (#22024)
    Co-authored-by: J-Sek <J-Sek@users.noreply.github.com>
    Resolves #18600

---
+++ packages/docs/src/examples/v-combobox/slot-custom-chip.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-combobox
+    v-model="selected"
+    :items="items"
+    item-title="name"
+    item-value="name"
+    chips
+    closable-chips
+    multiple
+  >
+    <template v-slot:chip="{ props, item }">
+      <v-chip v-bind="props" label>
+        <template v-slot:prepend>
+          <div class="me-1">{{ item.raw.symbol }}</div>
+        </template>
+        <template v-slot:close>
+          <v-icon icon="$close" size="14"></v-icon>
+        </template>
+      </v-chip>
+    </template>
+  </v-combobox>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const items = [
+    { symbol: '🍎', name: 'Apple' },
+    { symbol: '🍌', name: 'Banana' },
+    { symbol: '🍇', name: 'Grapes' },
+    { symbol: '🍉', name: 'Watermelon' },
+    { symbol: '🍓', name: 'Strawberry' },
+    { symbol: '🥝', name: 'Kiwi' },
+  ]
+  const selected = ref(['Apple', 'Kiwi', 'Grapes']
+    .map(v => items.find(item => item.name === v)))
+</script>
