0db3cda33e5d9021c3a112c3fb7f05c9aea4f8a7
antonhansson0624 <anton0424handsson@gmail.com>
2025-09-11 11:53:14 -0700

    docs: simplify template structure for center-active prop example

--- packages/docs/src/examples/v-chip-group/prop-centeractive.vue
+++ packages/docs/src/examples/v-chip-group/prop-centeractive.vue
@@ -1,18 +1,16 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-chip-group
-        v-model="selected"
-        selected-class="text-red"
-        center-active
-        filter
-      >
-        <v-slide-group-item v-for="n in 25" :key="n">
-          <v-chip :value="n">Chip {{ n }}</v-chip>
-        </v-slide-group-item>
-      </v-chip-group>
-    </v-container>
-  </v-app>
+  <v-sheet class="mx-auto" max-width="600">
+    <v-chip-group
+      v-model="selected"
+      selected-class="text-red"
+      center-active
+      filter
+    >
+      <v-slide-group-item v-for="n in 25" :key="n">
+        <v-chip :value="n">Chip {{ n }}</v-chip>
+      </v-slide-group-item>
+    </v-chip-group>
+  </v-sheet>
 </template>
 
 <script setup>
