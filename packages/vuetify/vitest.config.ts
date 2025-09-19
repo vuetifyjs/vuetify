af9a9bfc63fa44721f89076431fbe004e70d7c1c
Kael <kaelwd@gmail.com>
2025-09-19 19:23:11 +1000

    chore: update vite dependencies

--- packages/vuetify/vitest.config.ts
+++ packages/vuetify/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 import AutoImport from 'unplugin-auto-import/vite'
 import { fileURLToPath } from 'node:url'
+import { commands } from './test/setup/browser-commands'
 
 const IS_RUN = process.argv.slice(2).some(v => v === 'run')
 
@@ -56,6 +57,59 @@ export default defineConfig(configEnv => {
           clean: true,
           reportsDirectory: '../coverage',
         },
+        projects: [
+          {
+            extends: true,
+            resolve: {
+              alias: {
+                // Vite logs a warning for this even if we just re-export it without using anything
+                '@vitest/browser/context': fileURLToPath(new URL('test/contextStub.ts', import.meta.url)),
+              },
+            },
+            test: {
+              name: 'unit',
+              include: ['**/*.spec.{ts,tsx}'],
+              setupFiles: ['../test/setup/unit-setup.ts'],
+              environment: 'jsdom',
+            },
+          },
+          {
+            extends: true,
+            test: {
+              name: 'browser',
+              include: ['**/*.spec.browser.{ts,tsx}'],
+              setupFiles: ['../test/setup/browser-setup.ts'],
+              bail: process.env.TEST_BAIL ? 1 : undefined,
+              browser: {
+                enabled: true,
+                provider: 'webdriverio',
+                ui: false,
+                headless: !process.env.TEST_BAIL,
+                screenshotDirectory: '../test/__screenshots__',
+                commands,
+                instances: [{
+                  browser: 'chrome',
+                  capabilities: {
+                    browserVersion: '136',
+                    'goog:chromeOptions': {
+                      args: [
+                        '--start-maximized',
+                        process.env.TEST_BAIL && '--auto-open-devtools-for-tabs',
+                        // I have no idea why this is needed, it throws "WebDriverError: session
+                        // not created: probably user data directory is already in use" without it
+                        process.env.CI && '--no-sandbox',
+                      ].filter(v => !!v) as string[],
+                    },
+                  },
+                }],
+                viewport: {
+                  width: 1280,
+                  height: 800,
+                },
+              },
+            },
+          },
+        ],
       },
     })
   )
