5defa79ad83207217b601768eace743b13e80224
antonhansson0624 <anton0424handsson@gmail.com>
2025-09-25 15:23:35 -0700

    docs(transitions): add guidance on using padding with expand transitions

---
+++ packages/docs/src/examples/transitions/misc-expand-x-padding.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-btn class="ma-2" color="primary" @click="expand = !expand">
+        Expand Transition
+      </v-btn>
+
+      <v-expand-transition>
+        <div v-show="expand">
+          <v-alert text="Smooth transition here" type="info" />
+        </div>
+      </v-expand-transition>
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const expand = ref(false)
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      expand: false,
+    }),
+  }
+</script>
