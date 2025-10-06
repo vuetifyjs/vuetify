e0758868e03411f26c8b9911cdc1a76b3861a456
J-Sek <J-Sek@users.noreply.github.com>
2025-10-06 16:04:19 +0200

    docs: fix unnecessary VApp wrapper

--- packages/docs/src/examples/v-virtual-scroll/prop-renderless.vue
+++ packages/docs/src/examples/v-virtual-scroll/prop-renderless.vue
@@ -1,34 +1,32 @@
 <template>
-  <v-app>
-    <v-container>
-      <p class="text-caption opacity-80">
-        💡 The <code>height</code> prop should be a plain number,
-        or a value with units like <code>px</code> and <code>vh</code>, but not the percentage
-        (<code>%</code>).
-      </p>
+  <v-container>
+    <p class="text-caption opacity-80">
+      💡 The <code>height</code> prop should be a plain number,
+      or a value with units like <code>px</code> and <code>vh</code>, but not the percentage
+      (<code>%</code>).
+    </p>
 
-      <v-table height="300" fixed-header>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Icon</th>
-          </tr>
-        </thead>
-        <tbody>
-          <v-virtual-scroll :items="items" renderless>
-            <template v-slot:default="{ item, index, itemRef }">
-              <tr :ref="itemRef">
-                <td>{{ index + 1 }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.icon }}</td>
-              </tr>
-            </template>
-          </v-virtual-scroll>
-        </tbody>
-      </v-table>
-    </v-container>
-  </v-app>
+    <v-table height="300" fixed-header>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Icon</th>
+        </tr>
+      </thead>
+      <tbody>
+        <v-virtual-scroll :items="items" renderless>
+          <template v-slot:default="{ item, index, itemRef }">
+            <tr :ref="itemRef">
+              <td>{{ index + 1 }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.icon }}</td>
+            </tr>
+          </template>
+        </v-virtual-scroll>
+      </tbody>
+    </v-table>
+  </v-container>
 </template>
 
 <script setup>
