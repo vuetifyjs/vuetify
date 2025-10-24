dc02b351faecf64c0571370cf35efb840290bc36
J-Sek <J-Sek@users.noreply.github.com>
2025-10-24 17:16:37 +0200

    fix(group): avoid errors when comparing recursive items
    fixes #19322

--- packages/vuetify/src/util/helpers.ts
+++ packages/vuetify/src/util/helpers.ts
@@ -45,7 +45,30 @@ export function getNestedValue (obj: any, path: (string | number)[], fallback?: 
   return obj[path[last]] === undefined ? fallback : obj[path[last]]
 }
 
-export function deepEqual (a: any, b: any): boolean {
+function updateRecursionCache (a: any, b: any, cache: WeakMap<any, any>, result: boolean) {
+  if (!cache || isPrimitive(a) || isPrimitive(b)) return
+
+  const visitedObject = cache.get(a)
+  if (visitedObject) {
+    visitedObject.set(b, result)
+  } else {
+    const newCacheItem = new WeakMap()
+    newCacheItem.set(b, result)
+    cache.set(a, newCacheItem)
+  }
+}
+
+function findCachedComparison (a: any, b: any, cache: WeakMap<any, any>): boolean | null {
+  if (!cache || isPrimitive(a) || isPrimitive(b)) return null
+
+  const r1 = cache.get(a)?.get(b)
+  if (typeof r1 === 'boolean') return r1
+  const r2 = cache.get(b)?.get(a)
+  if (typeof r2 === 'boolean') return r2
+  return null
+}
+
+export function deepEqual (a: any, b: any, recursionCache = new WeakMap()): boolean {
   if (a === b) return true
 
   if (
@@ -69,7 +92,14 @@ export function deepEqual (a: any, b: any): boolean {
     return false
   }
 
-  return props.every(p => deepEqual(a[p], b[p]))
+  const cachedComparisonResult = findCachedComparison(a, b, recursionCache)
+  if (cachedComparisonResult) {
+    return cachedComparisonResult
+  }
+
+  updateRecursionCache(a, b, recursionCache, true)
+
+  return props.every(p => deepEqual(a[p], b[p], recursionCache))
 }
 
 export function getObjectValueByPath (obj: any, path?: string | null, fallback?: any): any {
