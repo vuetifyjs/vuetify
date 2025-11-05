0e1c09ef92ae066a2fc3bbede9d97c5d9529b3b7
Henry Aviles <haviles04@gmail.com>
2025-11-05 08:20:54 -0500

    feat: added icons

--- packages/docs/src/components/one/OtherSubs.vue
+++ packages/docs/src/components/one/OtherSubs.vue
@@ -1,13 +1,23 @@
 <template>
   <v-container class=" d-flex flex-column justify-center align-center text-center py-10">
-    <h2 class="text-h4 mb-6">Other ways to subscribe</h2>
+    <h2 class="text-h4 mb-6 font-weight-bold">Other ways to subscribe</h2>
 
     <div class="d-flex ga-6 justify-center align-center flex-wrap">
-      <v-card v-for="sponsor in sponsorships" :key="sponsor" class="pa-8" elevation="6" max-width="400">
+      <v-card
+        v-for="sponsor in sponsorships"
+        :key="sponsor"
+        class="pa-6"
+        elevation="6"
+        max-width="400"
+      >
         <v-card-title class="text-h5 font-weight-bold mb-4">{{ sponsor.name }}</v-card-title>
 
+        <v-icon aria-hidden="false" role="img" size="48">
+          {{ sponsor.icon }}
+        </v-icon>
+
         <v-card-text class="text-body-1 mb-6">
-          Support Vuetify development directly through {{ sponsor.name }} Sponsors and gain access to exclusive benefits and updates.
+          Support Vuetify development directly through {{ sponsor.name }} and gain access to exclusive benefits and updates.
         </v-card-text>
 
         <v-btn
@@ -32,14 +42,17 @@
     {
       name: 'GitHub',
       link: 'https://github.com/sponsors/johnleider',
+      icon: 'mdi-github',
     },
     {
       name: 'Discord',
       link: 'https://discord.com/servers/vuetify-340160225338195969',
+      icon: 'mdi-discord',
     },
     {
       name: 'Open Collective',
       link: 'https://opencollective.com/vuetify',
+      icon: 'mdi-account-group-outline',
     },
   ]
 
