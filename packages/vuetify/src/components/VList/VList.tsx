9d5a1d40a880b95ef3320bb63dd61748bd12b9ca
J-Sek <J-Sek@users.noreply.github.com>
2025-11-12 03:06:15 +0100

    fix(VTreeview): faster interactions with large trees (#22255)
    Co-authored-by: Kael <kaelwd@gmail.com>
    fixes #21720

--- packages/vuetify/src/components/VList/VList.tsx
+++ packages/vuetify/src/components/VList/VList.tsx
@@ -172,7 +172,8 @@ export const VList = genericComponent<new <
     const { dimensionStyles } = useDimension(props)
     const { elevationClasses } = useElevation(props)
     const { roundedClasses } = useRounded(props)
-    const { children, open, parents, select, getPath } = useNested(props)
+    const { children, open, parents, select, getPath } = useNested(props, items, () => props.returnObject)
+
     const lineClasses = toRef(() => props.lines ? `v-list--${props.lines}-line` : undefined)
     const activeColor = toRef(() => props.activeColor)
     const baseColor = toRef(() => props.baseColor)
--- packages/vuetify/src/composables/nested/nested.ts
+++ packages/vuetify/src/composables/nested/nested.ts
@@ -38,6 +38,7 @@ import type { InjectionKey, MaybeRefOrGetter, PropType, Ref } from 'vue'
 import type { ActiveStrategy } from './activeStrategies'
 import type { OpenStrategy } from './openStrategies'
 import type { SelectStrategy } from './selectStrategies'
+import type { ListItem } from '@/composables/list-items'
 import type { EventProp } from '@/util'
 
 export type ActiveStrategyProp =
@@ -57,6 +58,7 @@ export type SelectStrategyProp =
   | SelectStrategy
   | ((mandatory: boolean) => SelectStrategy)
 export type OpenStrategyProp = 'single' | 'multiple' | 'list' | OpenStrategy
+export type ItemsRegistrationType = 'props' | 'render'
 
 export interface NestedProps {
   activatable: boolean
@@ -68,6 +70,7 @@ export interface NestedProps {
   selected: any
   opened: any
   mandatory: boolean
+  itemsRegistration: ItemsRegistrationType
   'onUpdate:activated': EventProp<[any]> | undefined
   'onUpdate:selected': EventProp<[any]> | undefined
   'onUpdate:opened': EventProp<[any]> | undefined
@@ -86,6 +89,7 @@ type NestedProvide = {
     activated: Ref<Set<unknown>>
     selected: Ref<Map<unknown, 'on' | 'off' | 'indeterminate'>>
     selectedValues: Ref<unknown[]>
+    itemsRegistration: Ref<ItemsRegistrationType>
     register: (id: unknown, parentId: unknown, isDisabled: boolean, isGroup?: boolean) => void
     unregister: (id: unknown) => void
     open: (id: unknown, value: boolean, event?: Event) => void
@@ -101,6 +105,7 @@ export const VNestedSymbol: InjectionKey<NestedProvide> = Symbol.for('vuetify:ne
 export const emptyNested: NestedProvide = {
   id: shallowRef(),
   root: {
+    itemsRegistration: ref('render'),
     register: () => null,
     unregister: () => null,
     children: ref(new Map()),
@@ -130,9 +135,13 @@ export const makeNestedProps = propsFactory({
   activated: null,
   selected: null,
   mandatory: Boolean,
+  itemsRegistration: {
+    type: String as PropType<ItemsRegistrationType>,
+    default: 'render',
+  },
 }, 'nested')
 
-export const useNested = (props: NestedProps) => {
+export const useNested = (props: NestedProps, items: Ref<ListItem[]>, returnObject: MaybeRefOrGetter<boolean>) => {
   let isUnmounted = false
   const children = shallowRef(new Map<unknown, unknown[]>())
   const parents = shallowRef(new Map<unknown, unknown>())
@@ -227,6 +236,48 @@ export const useNested = (props: NestedProps) => {
     })
   }, 100)
 
+  watch(() => [items.value, toValue(returnObject)], () => {
+    if (props.itemsRegistration === 'props') {
+      updateInternalMaps()
+    }
+  }, { immediate: true })
+
+  function updateInternalMaps () {
+    const _parents = new Map()
+    const _children = new Map()
+    const _disabled = new Set()
+
+    const getValue = toValue(returnObject)
+      ? (item: ListItem) => toRaw(item.raw)
+      : (item: ListItem) => item.value
+
+    const stack = [...items.value]
+    let i = 0
+    while (i < stack.length) {
+      const item = stack[i++]
+      const itemValue = getValue(item)
+
+      if (item.children) {
+        const childValues = []
+        for (const child of item.children) {
+          const childValue = getValue(child)
+          _parents.set(childValue, itemValue)
+          childValues.push(childValue)
+          stack.push(child)
+        }
+        _children.set(itemValue, childValues)
+      }
+
+      if (item.props.disabled) {
+        _disabled.add(itemValue)
+      }
+    }
+
+    children.value = _children
+    parents.value = _parents
+    disabled.value = _disabled
+  }
+
   const nested: NestedProvide = {
     id: shallowRef(),
     root: {
@@ -244,6 +295,7 @@ export const useNested = (props: NestedProps) => {
 
         return arr
       }),
+      itemsRegistration: toRef(() => props.itemsRegistration),
       register: (id, parentId, isDisabled, isGroup) => {
         if (nodeIds.has(id)) {
           const path = getPath(id).map(String).join(' -> ')
@@ -391,26 +443,23 @@ export const useNestedItem = (id: MaybeRefOrGetter<unknown>, isDisabled: MaybeRe
   }
 
   onBeforeMount(() => {
-    if (!parent.isGroupActivator) {
-      nextTick(() => {
-        parent.root.register(computedId.value, parent.id.value, toValue(isDisabled), isGroup)
-      })
-    }
+    if (parent.isGroupActivator || parent.root.itemsRegistration.value === 'props') return
+    nextTick(() => {
+      parent.root.register(computedId.value, parent.id.value, toValue(isDisabled), isGroup)
+    })
   })
 
   onBeforeUnmount(() => {
-    if (!parent.isGroupActivator) {
-      parent.root.unregister(computedId.value)
-    }
+    if (parent.isGroupActivator || parent.root.itemsRegistration.value === 'props') return
+    parent.root.unregister(computedId.value)
   })
 
   watch(computedId, (val, oldVal) => {
-    if (!parent.isGroupActivator) {
-      parent.root.unregister(oldVal)
-      nextTick(() => {
-        parent.root.register(val, parent.id.value, toValue(isDisabled), isGroup)
-      })
-    }
+    if (parent.isGroupActivator || parent.root.itemsRegistration.value === 'props') return
+    parent.root.unregister(oldVal)
+    nextTick(() => {
+      parent.root.register(val, parent.id.value, toValue(isDisabled), isGroup)
+    })
   })
 
   isGroup && provide(VNestedSymbol, item)
