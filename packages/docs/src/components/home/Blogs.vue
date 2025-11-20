29e8a66a3c2c540a84b5a63b0ace25220632d0f1
Ishan Subedi <ikushum@gmail.com>
2025-11-20 14:37:35 +0545

    blog with image

--- packages/docs/src/components/home/Blogs.vue
+++ packages/docs/src/components/home/Blogs.vue
@@ -13,16 +13,25 @@
       <v-row align="center" class="text-left" justify="space-between">
         <v-col v-if="latestBlog" cols="12" md="7">
           <v-card
-            class="pr-10"
+            class="pr-10 overflow-hidden"
             color="transparent"
-            rounded="xl"
             flat
           >
-            <v-card-subtitle class="pt-5 text-subtitle-2 pl-0">
+            <v-img
+              v-if="latestBlog.image"
+              :src="latestBlog.image"
+              gradient="to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0)"
+              max-height="150"
+              rounded="lg"
+              width="100%"
+              cover
+            />
+
+            <v-card-subtitle class="pt-3 pb-1 text-subtitle-2 pl-0">
               {{ latestBlog.date }}
             </v-card-subtitle>
 
-            <v-card-title class="text-pre-wrap mb-2 pl-0">
+            <v-card-title class="text-pre-wrap pl-0 pt-0">
               {{ latestBlog.title }}
             </v-card-title>
 
@@ -30,7 +39,7 @@
               {{ latestBlog.shortDescription }}
             </div>
 
-            <v-card-text class="pl-0">
+            <v-card-text class="pl-0 pt-2">
               <div class="d-flex align-center ga-2 text-subtitle-2 text-medium-emphasis">
                 <v-icon size="small">mdi-circle-edit-outline</v-icon>
                 {{ latestBlog.personName }}
@@ -46,10 +55,7 @@
           </v-card>
         </v-col>
 
-        <v-col
-          cols="12"
-          md="5"
-        >
+        <v-col cols="12" md="5">
           <v-card
             v-for="(item , index) in remainingBlogs"
             :key="index"
@@ -66,7 +72,7 @@
               {{ item.title }}
             </v-card-title>
 
-            <v-card-text class="text-medium-emphasis text-subtitle-1 text-truncate py-0">
+            <v-card-text :class="{ 'text-truncate': smAndDown}" class="text-medium-emphasis text-subtitle-1 py-0">
               {{ item.shortDescription }}
             </v-card-text>
 
@@ -106,6 +112,7 @@
 
   const items = [
     {
+      image: 'https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       title: 'August 2025 Update',
       shortDescription: 'August marks a pivotal moment in Vuetify’s evolution as we prepare to release the pre-alpha of Vuetify0 (v0), launch our redesigned issues page, and continue delivering powerful components and improvements. This month brings exciting developments including the “Mastering Vuetify Theming',
       date: 'Apr 5, 2023',
