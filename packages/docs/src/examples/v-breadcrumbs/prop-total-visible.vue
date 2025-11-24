b940a03ca1b8f5fd4efc176c45688b3ef5ff4d53
J-Sek <J-Sek@users.noreply.github.com>
2025-11-24 13:43:09 +0100

    rework main example

--- packages/docs/src/examples/v-breadcrumbs/prop-total-visible.vue
+++ packages/docs/src/examples/v-breadcrumbs/prop-total-visible.vue
@@ -1,70 +1,65 @@
 <template>
-  <v-breadcrumbs :items="items" :total-visible="4">
-  </v-breadcrumbs>
-  <v-breadcrumbs :items="items" :total-visible="3" collapse-in-menu>
-  </v-breadcrumbs>
+  <v-container max-width="600">
+    <v-breadcrumbs
+      :items="items"
+      :total-visible="limit"
+    ></v-breadcrumbs>
+
+    <v-breadcrumbs
+      :items="items"
+      :total-visible="limit"
+      collapse-in-menu
+    ></v-breadcrumbs>
+
+    <div class="border-t pa-6">
+      <v-slider v-model="count" label="Items count:" max="10" step="1" thumb-label="always"></v-slider>
+      <v-slider v-model="limit" label="Collapse when exceeding:" max="10" step="1" thumb-label="always"></v-slider>
+    </div>
+  </v-container>
 </template>
 
 <script setup>
-  const items = [
+  import { shallowRef, toRef } from 'vue'
+
+  const count = shallowRef(6)
+  const limit = shallowRef(3)
+
+  const items = toRef(() => [
     {
       title: 'Dashboard',
       disabled: false,
       href: 'breadcrumbs_dashboard',
     },
-    {
-      title: 'Link 1',
-      disabled: false,
-      href: 'breadcrumbs_link_1',
-    },
-    {
-      title: 'Link 2',
-      disabled: false,
-      href: 'breadcrumbs_link_2',
-    },
-    {
-      title: 'Link 3',
-      disabled: false,
-      href: 'breadcrumbs_link_3',
-    },
-    {
-      title: 'Link 4',
-      disabled: true,
-      href: 'breadcrumbs_link_4',
-    },
-  ]
+    ...Array.from({ length: count.value - 1 }, (_, i) => i + 1)
+      .map(i => ({
+        title: `Link ${i}`,
+        disabled: i === count.value - 1,
+        href: `breadcrumbs_link_${i}`,
+      })),
+  ])
 </script>
 
 <script>
   export default {
     data: () => ({
-      items: [
-        {
-          title: 'Dashboard',
-          disabled: false,
-          href: 'breadcrumbs_dashboard',
-        },
-        {
-          title: 'Link 1',
-          disabled: false,
-          href: 'breadcrumbs_link_1',
-        },
-        {
-          title: 'Link 2',
-          disabled: false,
-          href: 'breadcrumbs_link_2',
-        },
-        {
-          title: 'Link 3',
-          disabled: false,
-          href: 'breadcrumbs_link_3',
-        },
-        {
-          title: 'Link 4',
-          disabled: true,
-          href: 'breadcrumbs_link_4',
-        },
-      ],
+      count: 6,
+      limit: 3,
     }),
+    computed: {
+      items () {
+        return [
+          {
+            title: 'Dashboard',
+            disabled: false,
+            href: 'breadcrumbs_dashboard',
+          },
+          ...Array.from({ length: count.value - 1 }, (_, i) => ({
+            title: `Link ${i}`,
+            disabled: i === count.value - 2,
+            href: `breadcrumbs_link_${i}`,
+          })),
+        ]
+      },
+    },
   }
 </script>
