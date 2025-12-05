06720cbd177b4c2878f70c7871b43e68a566cb1b
J-Sek <J-Sek@users.noreply.github.com>
2025-12-05 15:57:57 +0100

    fix(VDataTable): selecting group should respect selectable prop

--- packages/vuetify/src/components/VDataTable/VDataTableGroupHeaderRow.tsx
+++ packages/vuetify/src/components/VDataTable/VDataTableGroupHeaderRow.tsx
@@ -83,13 +83,15 @@ export const VDataTableGroupHeaderRow = genericComponent<VDataTableGroupHeaderRo
               </VDataTableColumn>
             )
           } else if (column.key === 'data-table-select') {
-            const modelValue = isSelected(rows.value)
-            const indeterminate = isSomeSelected(rows.value) && !modelValue
-            const selectGroup = (v: boolean) => select(rows.value, v)
+            const selectableRows = rows.value.filter(x => x.selectable)
+            const modelValue = selectableRows.length > 0 && isSelected(selectableRows)
+            const indeterminate = isSomeSelected(selectableRows) && !modelValue
+            const selectGroup = (v: boolean) => select(selectableRows, v)
             return slots['data-table-select']?.({ props: { modelValue, indeterminate, 'onUpdate:modelValue': selectGroup } }) ?? (
               <VDataTableColumn class="v-data-table__td--select-row" noPadding>
                 <VCheckboxBtn
                   density={ props.density }
+                  disabled={ selectableRows.length === 0 }
                   modelValue={ modelValue }
                   indeterminate={ indeterminate }
                   onUpdate:modelValue={ selectGroup }
