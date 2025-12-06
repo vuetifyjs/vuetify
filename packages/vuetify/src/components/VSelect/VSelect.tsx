291586ba3fc3f0597f34a6eaef285785040ca96f
Henry Aviles <haviles04@gmail.com>
2025-12-06 09:24:32 -0500

    fix: update slot location

--- packages/vuetify/src/components/VSelect/VSelect.tsx
+++ packages/vuetify/src/components/VSelect/VSelect.tsx
@@ -464,6 +464,13 @@ export const VSelect = genericComponent<new <
                   onAfterLeave={ onAfterLeave }
                   { ...computedMenuProps.value }
                 >
+
+                  { slots['list-header'] && (
+                    <div class="v-select__list-header">
+                      { slots['list-header']() }
+                    </div>
+                  )}
+
                   { hasList && (
                     <VList
                       ref={ listRef }
@@ -482,12 +489,6 @@ export const VSelect = genericComponent<new <
                       { ...props.listProps }
                     >
                       { slots['prepend-item']?.() }
-
-                      { slots['list-header'] && (
-                        <div class="v-select__list-header">
-                          { slots['list-header']() }
-                        </div>
-                      )}
 
                       { !displayItems.value.length && !props.hideNoData && (slots['no-data']?.() ?? (
                         <VListItem key="no-data" title={ t(props.noDataText) } />
