e811d2bb23ded518bda26af0ac0203a6195e1e40
Mayra valentina velasquez <56451875+mvalentinavo@users.noreply.github.com>
2025-12-17 09:45:15 -0500

    docs: add Figma links to example components (#22452)

--- packages/docs/src/examples/v-range-slider/slot-thumb-label.vue
+++ packages/docs/src/examples/v-range-slider/slot-thumb-label.vue
@@ -63,3 +63,8 @@
     },
   }
 </script>
+<example-meta lang="json">
+  {
+    "figma": "https://www.figma.com/design/5f4g4pbbBsk9TTWX4Xvlx1/PRO-v3.0---Official-Vuetify-3-UI-Kit?node-id=2073-37216&t=tC3y53U3XKPv8ZyJ-4"
+  }
+</example-meta>
