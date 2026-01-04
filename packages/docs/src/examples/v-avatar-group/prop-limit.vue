1e0556a452ff72714d2b3321c8ec71d6bd782f2a
J-Sek <J-Sek@users.noreply.github.com>
2026-01-04 20:50:13 +0100

    feat(VAvatarGroup): add new component

---
+++ packages/docs/src/examples/v-avatar-group/prop-limit.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="d-flex justify-center">
+    <v-avatar-group :items="items" :limit="3"></v-avatar-group>
+  </v-container>
+</template>
+
+<script setup>
+  const items = [
+    { text: 'BK', color: 'primary' },
+    { text: 'RF', color: 'secondary' },
+    { text: 'CA', color: '#E91E63' },
+    { text: 'PE', color: '#9C27B0' },
+    { text: 'WA', color: '#00BCD4' },
+  ]
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { text: 'BK', color: 'primary' },
+        { text: 'RF', color: 'secondary' },
+        { text: 'CA', color: '#E91E63' },
+        { text: 'PE', color: '#9C27B0' },
+        { text: 'WA', color: '#00BCD4' },
+      ],
+    }),
+  }
+</script>
