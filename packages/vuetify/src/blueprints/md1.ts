58c436c947386194fa37cf38cf3963199a07bb8b
J-Sek <J-Sek@users.noreply.github.com>
2026-01-26 13:13:35 +0100

    chore: md1 blueprint

--- packages/vuetify/src/blueprints/md1.ts
+++ packages/vuetify/src/blueprints/md1.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // Icons
 import { mdi } from '@/iconsets/mdi'
 
@@ -19,6 +20,7 @@ export const md1: Blueprint = {
       color: 'primary',
     },
     VBtn: {
+      class: 'text-uppercase',
       color: 'primary',
       rounded: 0,
     },
@@ -91,6 +93,36 @@ export const md1: Blueprint = {
           accent: '#009688',
         },
       },
+    },
+  },
+  typography: {
+    merge: false,
+    variables: {
+      'font-heading': '"Roboto", sans-serif',
+      'font-body': '"Roboto", sans-serif',
+
+      'display-large': 'var:display-2-*',
+      'headline-large': 'var:display-1-*',
+      'headline-medium': 'var:headline-*',
+      'headline-small': 'var:title-*',
+      'title-large': 'var:title-*',
+      'body-large': 'var:body-1-*',
+      'body-medium': 'var:body-2-*',
+      'label-large': 'var:button-*',
+      'label-small': 'var:caption-*',
+    },
+    variants: {
+      'display-4': { fontFamily: 'var:font-heading', fontSize: '7rem', lineHeight: 1, fontWeight: 300 },
+      'display-3': { fontFamily: 'var:font-heading', fontSize: '3.5rem', lineHeight: 1, fontWeight: 400 },
+      'display-2': { fontFamily: 'var:font-heading', fontSize: '2.8125rem', lineHeight: 1.05, fontWeight: 400 },
+      'display-1': { fontFamily: 'var:font-heading', fontSize: '2.125rem', lineHeight: 1.175, fontWeight: 400 },
+      headline: { fontFamily: 'var:font-heading', fontSize: '1.5rem', lineHeight: 1.333, fontWeight: 400 },
+      title: { fontFamily: 'var:font-heading', fontSize: '1.25rem', lineHeight: 1.6, fontWeight: 500 },
+      subheading: { fontFamily: 'var:font-body', fontSize: '1rem', lineHeight: 1.75, fontWeight: 400 },
+      'body-2': { fontFamily: 'var:font-body', fontSize: '.875rem', lineHeight: 1.5, fontWeight: 400 },
+      'body-1': { fontFamily: 'var:font-body', fontSize: '.8125rem', lineHeight: 1.425, fontWeight: 400 },
+      button: { fontFamily: 'var:font-body', fontSize: '.875rem', lineHeight: 2.6, fontWeight: 500, textTransform: 'uppercase' },
+      caption: { fontFamily: 'var:font-body', fontSize: '.75rem', lineHeight: 1.667, fontWeight: 400 },
     },
   },
 }
