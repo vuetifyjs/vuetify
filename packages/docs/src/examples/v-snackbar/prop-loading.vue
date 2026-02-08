1c8ff76b7b0cebabc0703f24c1307ac90f925ec1
J-Sek <J-Sek@users.noreply.github.com>
2026-02-08 19:39:23 +0100

    docs: VSnackbar examples

---
+++ packages/docs/src/examples/v-snackbar/prop-loading.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout min-height="100">
+    <v-snackbar
+      v-model="loadingSnackbar"
+      :timeout="-1"
+      text="Uploading file..."
+      contained
+      loading
+    ></v-snackbar>
+    <v-snackbar
+      v-model="successSnackbar"
+      :timeout="2000"
+      color="success"
+      prepend-icon="$complete"
+      text="Uploading successful!"
+      contained
+    ></v-snackbar>
+  </v-layout>
+</template>
+
+<script setup>
+  import { shallowRef, watch } from 'vue'
+
+  const loadingSnackbar = shallowRef(true)
+  const successSnackbar = shallowRef(false)
+
+  watch(loadingSnackbar, val => {
+    if (val) {
+      setTimeout(() => {
+        loadingSnackbar.value = false
+        successSnackbar.value = true
+      }, 2000)
+    } else {
+      setTimeout(() => {
+        loadingSnackbar.value = true
+      }, 3000)
+    }
+  }, { immediate: true })
+</script>
---
+++ packages/docs/src/examples/v-snackbar/prop-timer-position.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-layout height="150">
+    <v-container class="d-flex flex-wrap ga-3 justify-center">
+      <v-btn text="Timer Top" @click="current = 1"></v-btn>
+      <v-btn text="Timer bottom" @click="current = 2"></v-btn>
+      <v-btn text="Top Reversed" @click="current = 3"></v-btn>
+      <v-btn text="Bottom Reversed" @click="current = 4"></v-btn>
+    </v-container>
+
+    <v-snackbar
+      :model-value="current === 1"
+      text="Timer at the top"
+      timer="top"
+      timer-color="green"
+      contained
+      @update:model-value="current = -1"
+    ></v-snackbar>
+
+    <v-snackbar
+      :model-value="current === 2"
+      text="Timer at the bottom"
+      timer="bottom"
+      timer-color="info"
+      contained
+      @update:model-value="current = -1"
+    ></v-snackbar>
+
+    <v-snackbar
+      :model-value="current === 3"
+      text="Reversed timer"
+      timer-color="warning"
+      contained
+      reverse-timer
+      timer
+      @update:model-value="current = -1"
+    ></v-snackbar>
+
+    <v-snackbar
+      :model-value="current === 4"
+      text="Reversed bottom timer"
+      timer="bottom"
+      timer-color="error"
+      contained
+      reverse-timer
+      @update:model-value="current = -1"
+    ></v-snackbar>
+  </v-layout>
+</template>
+
+<script setup>
+  import { shallowRef } from 'vue'
+
+  const current = ref(-1)
+</script>
