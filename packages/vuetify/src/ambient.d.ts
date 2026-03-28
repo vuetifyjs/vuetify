f484098cd4e7f9735db03182af5f23f1921a6df6
hellozzm <hellozzm@users.noreply.github.com>
2026-03-28 18:15:42 +0800

    fix(types): add module declarations for side-effect style imports
    Add type declarations for 'vuetify/styles' and its sub-paths to fix
    TS2882 error when importing styles with TypeScript strict mode.
    
    Fixes vuetifyjs/vuetify#22766

--- packages/vuetify/src/ambient.d.ts
+++ packages/vuetify/src/ambient.d.ts
@@ -1,2 +1,9 @@
 declare module '*.sass' {}
 declare module '*.scss' {}
+
+// Type declarations for side-effect style imports
+// Fixes: https://github.com/vuetifyjs/vuetify/issues/22766
+declare module 'vuetify/styles'
+declare module 'vuetify/styles/core'
+declare module 'vuetify/styles/colors'
+declare module 'vuetify/styles/utilities'
