66bccefd88cf7f77fb903ea525f3957d104434ad
Vaishnavi Deshpande <VDeshpande7@slb.com>
2026-05-09 12:04:57 +0100

    fix(v-intersect): auto-detect scrollable ancestor as root

--- packages/vuetify/src/directives/intersect/index.ts
+++ packages/vuetify/src/directives/intersect/index.ts
@@ -1,5 +1,4 @@
 // Utilities
-import { nextTick } from 'vue'
 import { SUPPORTS_INTERSECTION } from '@/util'
 
 // Types
@@ -37,7 +36,7 @@ const scrollableRE = /auto|scroll/
 
 function getScrollableAncestor (el: HTMLElement): HTMLElement | null {
   let parent = el.parentElement
-  while (parent) {
+  while (parent && parent !== document.documentElement && parent !== document.body) {
     const { overflow, overflowY, overflowX } = getComputedStyle(parent)
     if (scrollableRE.test(overflow + overflowY + overflowX)) {
       return parent
@@ -56,49 +55,45 @@ function mounted (el: HTMLElement, binding: ObserveDirectiveBinding) {
     ? value
     : { handler: value, options: {} }
 
-  nextTick(() => {
-    const resolvedOptions: IntersectionObserverInit = {
-      ...options,
-      root: options?.root !== undefined
-        ? options.root
-        : getScrollableAncestor(el),
+  const resolvedOptions: IntersectionObserverInit = {
+    ...options,
+    root: options?.root !== undefined
+      ? options.root
+      : getScrollableAncestor(el),
+  }
+
+  const observer = new IntersectionObserver((
+    entries: IntersectionObserverEntry[] = [],
+    observer: IntersectionObserver
+  ) => {
+    const _observe = el._observe?.[binding.instance!.$.uid]
+    if (!_observe) return
+
+    const isIntersecting = entries.some(entry => entry.isIntersecting)
+
+    if (
+      handler && (
+        !modifiers.quiet ||
+        _observe.init
+      ) && (
+        !modifiers.once ||
+        isIntersecting ||
+        _observe.init
+      )
+    ) {
+      handler(isIntersecting, entries, observer)
     }
 
-    const observer = new IntersectionObserver((
-      entries: IntersectionObserverEntry[] = [],
-      observer: IntersectionObserver
-    ) => {
-      const _observe = el._observe?.[binding.instance!.$.uid]
-      if (!_observe) return // Just in case, should never fire
+    if (isIntersecting && modifiers.once) unmounted(el, binding)
+    else _observe.init = true
+  }, resolvedOptions)
 
-      const isIntersecting = entries.some(entry => entry.isIntersecting)
+  el._observe = Object(el._observe)
 
-      // If is not quiet or has already been
-      // initted, invoke the user callback
-      if (
-        handler && (
-          !modifiers.quiet ||
-          _observe.init
-        ) && (
-          !modifiers.once ||
-          isIntersecting ||
-          _observe.init
-        )
-      ) {
-        handler(isIntersecting, entries, observer)
-      }
+  const target = getObservableEl(el)
+  el._observe![binding.instance!.$.uid] = { init: false, observer, target }
 
-      if (isIntersecting && modifiers.once) unmounted(el, binding)
-      else _observe.init = true
-    }, resolvedOptions)
-
-    el._observe = Object(el._observe)
-
-    const target = getObservableEl(el)
-    el._observe![binding.instance!.$.uid] = { init: false, observer, target }
-
-    observer.observe(target)
-  })
+  observer.observe(target)
 }
 
 function unmounted (el: HTMLElement, binding: ObserveDirectiveBinding) {
