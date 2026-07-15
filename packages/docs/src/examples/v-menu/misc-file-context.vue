21ab87e0240178c022cb0530c06bda3e32aad729
J-Sek <J-Sek@users.noreply.github.com>
2026-07-15 19:06:06 +0200

    chore: update, cleanup, simplify

--- packages/docs/src/examples/v-menu/misc-file-context.vue
+++ packages/docs/src/examples/v-menu/misc-file-context.vue
@@ -1,44 +1,41 @@
 <template>
-    <div class="text-center">
-        <v-menu location="center">
-            <template v-slot:activator="{ props }">
-                <v-btn v-bind="props"> Open Menu </v-btn>
+  <div class="text-center">
+    <v-menu
+      :close-on-content-click="false"
+      location="bottom center"
+      offset="8"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props">Open Menu</v-btn>
+      </template>
+      <v-card min-width="200" rounded="lg">
+        <div class="d-flex justify-space-between pa-2">
+          <v-icon-btn icon="mdi-eye-outline" variant="text" v-tooltip:top="'Preview'" @click=""></v-icon-btn>
+          <v-icon-btn icon="mdi-download" variant="text" v-tooltip:top="'Download'" @click=""></v-icon-btn>
+          <v-icon-btn icon="mdi-content-copy" variant="text" v-tooltip:top="'Copy file name'" @click=""></v-icon-btn>
+          <v-icon-btn icon="mdi-rename-outline" variant="text" v-tooltip:top="'Rename'" @click=""></v-icon-btn>
+        </div>
+      </v-card>
+      <v-card class="mt-2" rounded="lg">
+        <v-list class="pa-1" density="compact" prepend-gap="12" nav>
+          <v-list-item prepend-icon="mdi-share-variant" title="Share" @click="">
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-information-outline" title="Details" @click="">
+            <template v-slot:append>
+              <v-hotkey keys="Alt+D" variant="text"></v-hotkey>
             </template>
-            <v-card min-width="200" rounded="lg">
-                <div class="d-flex justify-space-between pa-2">
-                    <v-btn icon="mdi-cogs" size="small" variant="text"></v-btn>
-                    <v-btn icon="mdi-content-copy" size="small" variant="text"></v-btn>
-                    <v-btn icon="mdi-pencil" size="small" variant="text"></v-btn>
-                    <v-btn icon="mdi-delete-outline" size="small" variant="text" color="error"></v-btn>
-                </div>
-            </v-card>
-            <v-card class="mt-2" rounded="lg">
-                <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-file-excel" title="Open With Excel">
-                    </v-list-item>
-                    <v-list-item prepend-icon="mdi-open-in-new" append-icon="mdi-menu-right" title="Open With">
-                    </v-list-item>
-                    <v-list-item prepend-icon="mdi-file-pdf-box" title="Export PDF">
-                    </v-list-item>
-                    <v-list-item prepend-icon="mdi-folder-zip-outline" title="Compress / zip">
-                        <template v-slot:title>
-                            Compress <v-icon>mdi-circle-small</v-icon> 7zip
-                        </template>
-                    </v-list-item>
-                    <v-divider></v-divider>
-                    <v-list-item prepend-icon="mdi-cloud-upload" title="Upload">
-                        <template v-slot:append>
-                            <v-chip label size="x-small" class="font-weight-medium text-grey border" variant="tonal">
-                                Ctrl + U
-                            </v-chip>
-                        </template>
-                    </v-list-item>
-                    <v-list-item prepend-icon="mdi-access-point" title="Sharepoint"></v-list-item>
-                    <v-divider></v-divider>
-                    <v-list-item prepend-icon="mdi-shield-star-outline" title="Protect">
-                    </v-list-item>
-                </v-list>
-            </v-card>
-        </v-menu>
-    </div>
+          </v-list-item>
+          <v-divider class="mt-1"></v-divider>
+          <v-list-item prepend-icon="mdi-delete-outline" title="Remove" @click="">
+            <template v-slot:prepend>
+              <v-icon color="error"></v-icon>
+            </template>
+            <template v-slot:append>
+              <v-hotkey keys="Delete" variant="text"></v-hotkey>
+            </template>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-menu>
+  </div>
 </template>
--- packages/docs/src/examples/v-menu/misc-use-in-text-field.vue
+++ packages/docs/src/examples/v-menu/misc-use-in-text-field.vue
@@ -1,65 +1,79 @@
 <template>
-    <v-layout>
-        <v-container class="mb-10">
-            <v-bottom-navigation :elevation="10">
-                <v-text-field variant="outlined" density="compact" hide-details class="pa-2 px-15">
-                    <template #append-inner>
-                        <div class="d-flex">
-                            <v-menu location="top" offset="25"
-                                transition="slide-x-transition">
-                                <template v-slot:activator="{ props }">
-                                    <v-btn size="" v-bind="props"><v-icon size="small" color="yellow">mdi-emoticon</v-icon></v-btn>
-                                </template>
+  <v-container>
+    <v-text-field class="mx-auto" max-width="400" variant="outlined" hide-details>
+      <template v-slot:append-inner>
+        <div class="d-flex">
+          <v-menu
+            :close-on-content-click="false"
+            location="top"
+            offset="25"
+            transition="slide-x-transition"
+          >
+            <template v-slot:activator="{ props }">
+              <v-icon-btn icon="mdi-emoticon" icon-color="yellow" v-bind="props"></v-icon-btn>
+            </template>
 
-                                <v-card>
-                                    <v-text-field hide-details append-inner-icon="mdi-magnify"
-                                        label="Search"></v-text-field>
-                                    <v-container>
+            <v-card max-width="300">
+              <v-text-field
+                append-inner-icon="mdi-magnify"
+                label="Search"
+                hide-details
+              ></v-text-field>
+              <v-container>
 
-                                        <div class="d-flex align-center justify-space-between  mt-4">
-                                            <span class="text-body-2 font-weight-bold"> Emoji's </span>
-                                            <v-btn variant="text" size="x-small">See all ></v-btn>
-                                        </div>
-                                        <div class="d-flex justify-space-between">
-                                            <v-btn variant="text" size="small" icon>✨</v-btn>
-                                            <v-btn variant="text" size="small" icon>🔥</v-btn>
-                                            <v-btn variant="text" size="small" icon>⭐</v-btn>
-                                            <v-btn variant="text" size="small" icon>😊</v-btn>
-                                            <v-btn variant="text" size="small" icon>💀</v-btn>
-                                            <v-btn variant="text" size="small" icon>😂</v-btn>
-                                        </div>
+                <div class="d-flex align-center justify-space-between  mt-4">
+                  <span class="text-body-2 font-weight-bold"> Emoji's </span>
+                  <v-btn size="x-small" variant="text">See all ></v-btn>
+                </div>
+                <div class="d-flex justify-space-between">
+                  <v-btn size="small" variant="text" icon>✨</v-btn>
+                  <v-btn size="small" variant="text" icon>🔥</v-btn>
+                  <v-btn size="small" variant="text" icon>⭐</v-btn>
+                  <v-btn size="small" variant="text" icon>😊</v-btn>
+                  <v-btn size="small" variant="text" icon>💀</v-btn>
+                  <v-btn size="small" variant="text" icon>😂</v-btn>
+                </div>
 
-                                        <div class="d-flex align-center justify-space-between  mt-4">
-                                            <span class="text-body-2 font-weight-bold"> Gifs </span>
-                                            <v-btn variant="text" size="x-small">See all ></v-btn>
-                                        </div>
+                <div class="d-flex align-center justify-space-between  mt-4">
+                  <span class="text-body-2 font-weight-bold"> Gifs </span>
+                  <v-btn size="x-small" variant="text">See all ></v-btn>
+                </div>
 
-                                        <v-row dense>
-                                            <v-col v-for="n in 3" cols="4" :key="`gifs-${n}`">
-                                                <v-sheet color="surface-variant-alt" height="96" rounded></v-sheet>
-                                            </v-col>
-                                        </v-row>
-                                        <div class="d-flex align-center justify-space-between mt-4">
-                                            <span class="text-body-2 font-weight-bold"> Stickers </span>
-                                            <v-btn variant="text" size="x-small">See all ></v-btn>
-                                        </div>
-                                        <v-row dense>
-                                            <v-col v-for="n in 3" cols="4" :key="`stickers-${n}`">
-                                                <v-sheet color="surface-variant-alt" height="96" rounded></v-sheet>
-                                            </v-col>
-                                        </v-row>
-                                    </v-container>
-                                    <div class="d-flex justify-end pb-2 px-4 text-caption">
-                                        ⚡by vuetify
-                                    </div>
-                                </v-card>
-                            </v-menu>
-                            <v-divider vertical class="mx-2 "></v-divider>
-                            <v-btn size=""><v-icon size="small" color="white">mdi-send</v-icon></v-btn>
-                        </div>
-                    </template>
-                </v-text-field>
-            </v-bottom-navigation>
-        </v-container>
-    </v-layout>
+                <v-slide-group show-arrows="never">
+                  <div class="d-flex ga-3">
+                    <v-skeleton-loader
+                      v-for="n in 7"
+                      :key="`gifs-${n}`"
+                      class="rounded-lg overflow-hidden"
+                      height="96"
+                      type="image"
+                      width="130"
+                    ></v-skeleton-loader>
+                  </div>
+                </v-slide-group>
+                <div class="d-flex align-center justify-space-between mt-4">
+                  <span class="text-body-2 font-weight-bold"> Stickers </span>
+                  <v-btn size="x-small" variant="text">See all ></v-btn>
+                </div>
+                <v-slide-group show-arrows="never">
+                  <div class="d-flex ga-3">
+                    <v-skeleton-loader
+                      v-for="n in 7"
+                      :key="`stickers-${n}`"
+                      class="rounded-lg overflow-hidden"
+                      height="96"
+                      type="image"
+                      width="80"
+                    ></v-skeleton-loader>
+                  </div>
+                </v-slide-group>
+              </v-container>
+            </v-card>
+          </v-menu>
+          <v-divider class="mx-2" vertical></v-divider>
+          <v-icon-btn icon="mdi-send" icon-size="20"></v-icon-btn>
+        </div>
+      </template>
+    </v-text-field>
+  </v-container>
 </template>
