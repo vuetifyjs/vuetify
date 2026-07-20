8e83dad0e8d8c35a4fcee9c097e84100ec3a571b
UllernJ <Ulrik.johannesen@gmail.com>
2026-07-20 22:28:28 +0200

    add transition flag to vmain

--- packages/vuetify/src/components/VMain/VMain.tsx
+++ packages/vuetify/src/components/VMain/VMain.tsx
@@ -13,6 +13,10 @@ import { genericComponent, propsFactory, useRender } from '@/util'
 
 export const makeVMainProps = propsFactory({
   scrollable: Boolean,
+  transition: {
+    type: Boolean,
+    default: true,
+  },
 
   ...makeComponentProps(),
   ...makeDimensionProps(),
@@ -38,7 +42,7 @@ export const VMain = genericComponent()({
         ]}
         style={[
           mainStyles.value,
-          ssrBootStyles.value,
+          props.transition ? ssrBootStyles.value : {},
           dimensionStyles.value,
           props.style,
         ]}
