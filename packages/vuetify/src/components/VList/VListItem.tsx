80c8e9b5a36db50fca92f102c0ad758e3fc5b3e3
J-Sek <J-Sek@users.noreply.github.com>
2026-07-23 14:57:04 +0200

    fix(VListItem/VChip/VDatePickerHeader): only attach click when interactive
    fixes #22854

--- packages/vuetify/src/components/VList/VListItem.tsx
+++ packages/vuetify/src/components/VList/VListItem.tsx
@@ -317,7 +317,7 @@ export const VListItem = genericComponent<VListItemSlots>()({
           tabindex={ props.tabindex ?? (isClickable.value ? (list ? -2 : 0) : undefined) }
           aria-selected={ ariaSelected.value }
           role={ role.value }
-          onClick={ onClick }
+          onClick={ (isClickable.value || props.onClick || props.onClickOnce) && onClick }
           onKeydown={ isClickable.value && !isLink.value && onKeyDown }
           v-ripple={ isClickable.value && rippleOptions.value }
         >
