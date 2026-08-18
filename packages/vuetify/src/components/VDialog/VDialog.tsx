017f122d265c3502238ba5175ec59178901f8276
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-18 07:18:50 +0500

    fix(VDialog): avoid error when unmounted while closing (#23054)
    fixes #23053

--- packages/vuetify/src/components/VDialog/VDialog.tsx
+++ packages/vuetify/src/components/VDialog/VDialog.tsx
@@ -68,7 +68,7 @@ export const VDialog = genericComponent<OverlaySlots>()({
     watch(isActive, async val => {
       if (!val) {
         await nextTick()
-        overlay.value!.activatorEl?.focus({ preventScroll: true })
+        overlay.value?.activatorEl?.focus({ preventScroll: true })
       }
     })
 
