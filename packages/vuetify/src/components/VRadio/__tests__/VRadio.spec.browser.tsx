f75053a507adf5938d4ebbdd62a06caf0ccdf713
userquin <userquin@gmail.com>
2026-08-27 15:09:16 +0200

    chore: update VRadio browser spec

--- packages/vuetify/src/components/VRadio/__tests__/VRadio.spec.browser.tsx
+++ packages/vuetify/src/components/VRadio/__tests__/VRadio.spec.browser.tsx
@@ -3,7 +3,7 @@ import { VRadio } from '../VRadio'
 import { VRadioGroup } from '@/components/VRadioGroup'
 
 // Utilities
-import { render, screen, userEvent, waitIdle } from '@test'
+import { page, render, screen, userEvent, waitIdle } from '@test'
 import { ref } from 'vue'
 
 describe('VRadio', () => {
@@ -40,7 +40,22 @@ describe('VRadio', () => {
     expect(screen.getByLabelText('two')).toBeChecked()
   })
 
-  it('should work standalone, outside a group and expose VSelectionControl API', async () => {
+  it('should work standalone, outside a group', async () => {
+    const model = ref()
+
+    const { element } = render(() => (
+      <VRadio id="test" label="Standalone" value="a" v-model={ model } />
+    ))
+
+    expect(element).toBeInTheDocument()
+
+    await page.getByRole('radio').click()
+    await waitIdle()
+
+    expect(model.value).toBe('a')
+  })
+
+  it('should expose VSelectionControl API', async () => {
     const cmp = ref<any>()
 
     render(() => <VRadio ref={ cmp } label="Standalone" value="a" />)
