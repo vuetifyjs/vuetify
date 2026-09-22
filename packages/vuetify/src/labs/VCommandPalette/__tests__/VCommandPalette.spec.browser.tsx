e4bcecf995e206855defe1db982421f2da83ac7d
J-Sek <J-Sek@users.noreply.github.com>
2026-09-22 14:59:47 +0200

    no need for a test

--- packages/vuetify/src/labs/VCommandPalette/__tests__/VCommandPalette.spec.browser.tsx
+++ packages/vuetify/src/labs/VCommandPalette/__tests__/VCommandPalette.spec.browser.tsx
@@ -486,23 +486,6 @@ describe('VCommandPalette', () => {
 
       expect(screen.getByTestId('no-data-slot')).toBeInTheDocument()
     })
-
-    it('should render the list.prepend slot', async () => {
-      const model = ref(true)
-      render(() => (
-        <VCommandPalette
-          v-model={ model.value }
-          items={ testItems }
-          v-slots={{
-            'list.prepend': () => <div data-testid="list-prepend-slot">List prepend</div>,
-          } as any}
-        />
-      ))
-
-      await screen.findByRole('dialog')
-
-      await expect(screen.findByTestId('list-prepend-slot')).resolves.toBeInTheDocument()
-    })
   })
 
   describe('Accessibility', () => {
